3da0d3ef83b6adc057d1170084ef5a5f4d016d36
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-02-04 11:53:45 +0100

    set aij_{sm} to non-zero

--- mpi-dpd/fsi.cu
+++ mpi-dpd/fsi.cu
@@ -31,7 +31,7 @@ ComputeFSI::ComputeFSI(MPI_Comm comm)
 
     //TODO: use CUDA_CHECK(cudaEventCreateWithFlags(&evuploaded, cudaEventDisableTiming));
 
-    KernelsFSI::Params params = {0.0, gammadpd, sigmaf};
+    KernelsFSI::Params params = {aij, gammadpd, sigmaf};
 
     CUDA_CHECK(cudaMemcpyToSymbol(KernelsFSI::params, &params, sizeof(params)));
 
