8c11b981104dc7942c766c93c87b9510b1c55b65
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-02-10 19:49:10 +0100

    Add some tests

---
+++ mpi-dpd/tests/locations/dpd-forces.cu
@@ -0,0 +1,56 @@
+#include "dpd-forces.h"
+#include "common.h"
+
+__device__ bool inbox(float x, float y, float z,
+        float xl=-3, float xh=3,
+        float yl=-3, float yh=3,
+        float zl=-3, float zh=3) {
+    return xl < x && x < xh && yl < y && y < yh  && zl < z && z < zh;
+}
+
+__device__ float3 compute_dpd_force_traced(int type1, int type2,
+        float3 pos1, float3 pos2, float3 vel1, float3 vel2, float myrandnr) {
+    /* return the DPD interaction force based on particle types
+     * type: 0 -- outer solvent, 1 -- inner solvent, 2 -- membrane, 3 -- wall */
+
+    const float _xr = pos1.x - pos2.x;
+    const float _yr = pos1.y - pos2.y;
+    const float _zr = pos1.z - pos2.z;
+
+    const float rij2 = _xr * _xr + _yr * _yr + _zr * _zr;
+    const float invrij = rsqrtf(rij2);
+    const float rij = rij2 * invrij;
+    if (rij2 >= 1)
+        return make_float3(0, 0, 0);
+
+    const float argwr = 1.f - rij;
+    const float wr = viscosity_function<-VISCOSITY_S_LEVEL>(argwr);
+
+    const float xr = _xr * invrij;
+    const float yr = _yr * invrij;
+    const float zr = _zr * invrij;
+
+    const float rdotv =
+        xr * (vel1.x - vel2.x) +
+        yr * (vel1.y - vel2.y) +
+        zr * (vel1.z - vel2.z);
+
+    // particle type dependent constants
+    const float gammadpd[4] = {56, 8, 56, 56};              // default: 4.5
+    const float aij[4] = {4 / RC_FX, 4 / RC_FX, 4 / RC_FX, 4 / RC_FX}; // default: 75*kBT/numberdensity -- Groot and Warren (1997)
+
+    const float aij_pair = 0.5 * (aij[type1] + aij[type2]);
+    const float gammadpd_pair = 0.5 * (gammadpd[type1] + gammadpd[type2]);
+    const float sigmaf_pair = sqrt(2*gammadpd_pair*kBT / dt);
+
+    /* printing */
+    if (inbox(pos1.x, pos1.y, pos1.z, -1, 1, -1, 1, -1, 1))
+        printf("%g %g %g %d :CDFT:\n", pos1.x, pos1.y, pos1.z, type1);
+    if (inbox(pos2.x, pos2.y, pos2.z, -1, 1, -1, 1, -1, 1))
+        printf("%g %g %g %d :CDFT:\n", pos2.x, pos2.y, pos2.z, type2);
+    /* end printing */
+
+    const float strength = aij_pair * argwr + (-gammadpd_pair * wr * rdotv + sigmaf_pair * myrandnr) * wr;
+
+    return make_float3(strength*xr, strength*yr, strength*zr);
+}
---
+++ mpi-dpd/tests/locations/main.cu
@@ -0,0 +1,249 @@
+/*
+ *  main.cu
+ *  Part of uDeviceX/mpi-dpd/
+ *
+ *  Created and authored by Diego Rossinelli on 2014-11-14.
+ *  Copyright 2015. All rights reserved.
+ *
+ *  Users are NOT authorized
+ *  to employ the present software for their own publications
+ *  before getting a written permission from the author of this file.
+ */
+
+#include <cstdio>
+#include <cassert>
+#include <csignal>
+#include <mpi.h>
+#include <errno.h>
+
+#include "argument-parser.h"
+#include "common.h"
+#include "containers.h"
+#include "solvent-exchange.h"
+#include "dpd.h"
+#include "wall.h"
+#include "solute-exchange.h"
+#include "fsi.h"
+#include "contact.h"
+#include "redistribute-particles.h"
+#include "redistribute-rbcs.h"
+#include "velcontroller.h"
+#include "simulation.h"
+
+float tend, desired_shrate;
+bool walls, pushtheflow, doublepoiseuille, rbcs, hdf5field_dumps, hdf5part_dumps, is_mps_enabled, adjust_message_sizes, contactforces;
+int steps_per_dump, steps_per_hdf5dump, wall_creation_stepid;
+
+float RBCx0, RBCp, RBCcq, RBCkb, RBCka, RBCkv, RBCgammaC, RBCkd, RBCtotArea, RBCtotVolume, RBCscale;
+
+LocalComm localcomm;
+
+namespace SignalHandling
+{
+    volatile sig_atomic_t graceful_exit = 0, graceful_signum = 0;
+
+    void signal_handler(int signum)
+    {
+        graceful_exit = 1;
+        graceful_signum = signum;
+    }
+
+    void setup()
+    {
+        struct sigaction action;
+        memset(&action, 0, sizeof(struct sigaction));
+        action.sa_handler = signal_handler;
+        sigaction(SIGUSR1, &action, NULL);
+    }
+
+    bool check_termination_request()
+    {
+        return graceful_exit;
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    int ranks[3];
+
+    //parsing of the positional arguments
+    if (argc < 4)
+    {
+        printf("usage: ./mpi-dpd <xranks> <yranks> <zranks>\n");
+        exit(-1);
+    }
+    else
+        for(int i = 0; i < 3; ++i)
+            ranks[i] = atoi(argv[1 + i]);
+
+    ArgumentParser argp(vector<string>(argv + 4, argv + argc));
+
+    tend = argp("-tend").asDouble(50);
+    walls = argp("-walls").asBool(false);
+    pushtheflow = argp("-pushtheflow").asBool(false);
+    doublepoiseuille = argp("-doublepoiseuille").asBool(false);
+    rbcs = argp("-rbcs").asBool(false);
+    hdf5field_dumps = argp("-hdf5field_dumps").asBool(false);
+    hdf5part_dumps = argp("-hdf5part_dumps").asBool(false);
+    steps_per_dump = argp("-steps_per_dump").asInt(1000);
+    steps_per_hdf5dump = argp("-steps_per_hdf5dump").asInt(2000);
+    wall_creation_stepid = argp("-wall_creation_stepid").asInt(5000);
+    adjust_message_sizes = argp("-adjust_message_sizes").asBool(false);
+    contactforces = argp("-contactforces").asBool(false);
+    hdf5field_dumps = argp("-hdf5field_dumps").asBool(false);
+
+    // desired shear rate in DPD units
+    desired_shrate = argp("-shrate").asDouble(1);
+    RBCx0 = argp("-RBCx0").asDouble(0.5);
+    RBCp = argp("-RBCp").asDouble(0.0045);
+    RBCka = argp("-RBCka").asDouble(4900);
+    RBCkb = argp("-RBCkb").asDouble(40);
+    RBCkd = argp("-RBCkd").asDouble(100);
+    RBCkv = argp("-RBCkv").asDouble(5000);
+    RBCgammaC = argp("-RBCgammaC").asDouble(30);
+    RBCtotArea = argp("-RBCtotArea").asDouble(124);
+    RBCtotVolume = argp("-RBCtotVolume").asDouble(90);
+
+    RBCscale = 1/RC_FX;
+
+#ifndef _NO_DUMPS_
+    const bool mpi_thread_safe = argp("-mpi_thread_safe").asBool(true);
+#else
+    const bool mpi_thread_safe = argp("-mpi_thread_safe").asBool(false);
+#endif
+
+    SignalHandling::setup();
+
+    CUDA_CHECK(cudaSetDevice(0));
+
+    CUDA_CHECK(cudaDeviceReset());
+
+    size_t sz;
+    CUDA_CHECK(cudaDeviceGetLimit(&sz, cudaLimitPrintfFifoSize));
+    printf("sz: %d\n", sz);
+    CUDA_CHECK(cudaDeviceSetLimit(cudaLimitPrintfFifoSize, sz*1000));
+
+    {
+        is_mps_enabled = false;
+
+        const char * mps_variables[] = {
+            "CRAY_CUDA_MPS",
+            "CUDA_MPS",
+            "CRAY_CUDA_PROXY",
+            "CUDA_PROXY"
+        };
+
+        for(int i = 0; i < 4; ++i)
+            is_mps_enabled |= getenv(mps_variables[i])!= NULL && atoi(getenv(mps_variables[i])) != 0;
+    }
+
+    int nranks, rank;
+
+    if (mpi_thread_safe)
+    {
+        //needed for the asynchronous data dumps
+        setenv("MPICH_MAX_THREAD_SAFETY", "multiple", 0);
+
+        int provided_safety_level;
+        MPI_CHECK( MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided_safety_level));
+
+        MPI_CHECK( MPI_Comm_size(MPI_COMM_WORLD, &nranks) );
+        MPI_CHECK( MPI_Comm_rank(MPI_COMM_WORLD, &rank) );
+
+        if (provided_safety_level != MPI_THREAD_MULTIPLE)
+        {
+            if (rank == 0)
+                printf("ooooooooops MPI thread safety level is just %d. Aborting now.\n", provided_safety_level);
+            abort();
+        }
+        else
+            if (rank == 0)
+                printf("I have set MPICH_MAX_THREAD_SAFETY=multiple\n");
+    }
+    else
+    {
+        MPI_CHECK(MPI_Init(&argc, &argv));
+        MPI_CHECK( MPI_Comm_size(MPI_COMM_WORLD, &nranks) );
+        MPI_CHECK( MPI_Comm_rank(MPI_COMM_WORLD, &rank) );
+
+        const char * env_thread_safety = getenv("MPICH_MAX_THREAD_SAFETY");
+
+        if (rank == 0 && env_thread_safety)
+            printf("I read MPICH_MAX_THREAD_SAFETY=%s", env_thread_safety);
+    }
+
+    MPI_Comm activecomm = MPI_COMM_WORLD;
+#if defined(CUSTOM_REORDERING)
+    activecomm = setup_reorder_comm(MPI_COMM_WORLD, rank, nranks);
+#endif
+
+    bool reordering = true;
+
+    const char * env_reorder = getenv("MPICH_RANK_REORDER_METHOD");
+
+    //reordering of the ranks according to the computational domain and environment variables
+    if (atoi(env_reorder ? env_reorder : "-1") == atoi("3"))
+    {
+        reordering = false;
+
+        const bool usefulrank = rank < ranks[0] * ranks[1] * ranks[2];
+
+        MPI_CHECK(MPI_Comm_split(MPI_COMM_WORLD, usefulrank, rank, &activecomm)) ;
+
+        MPI_CHECK(MPI_Barrier(activecomm));
+
+        if (!usefulrank)
+        {
+            printf("rank %d has been thrown away\n", rank);
+            fflush(stdout);
+
+            MPI_CHECK(MPI_Barrier(activecomm));
+
+            MPI_Finalize();
+
+            return 0;
+        }
+
+        MPI_CHECK(MPI_Barrier(activecomm));
+    }
+
+    MPI_Comm cartcomm;
+
+    int periods[] = {1, 1, 1};
+
+    MPI_CHECK( MPI_Cart_create(activecomm, 3, ranks, periods, (int)reordering, &cartcomm) );
+
+    activecomm = cartcomm;
+
+    //RAII
+    {
+        MPI_CHECK(MPI_Barrier(activecomm));
+
+        if (rank == 0)
+        {
+            argp.print_arguments();
+            fflush(stdout);
+        }
+
+        localcomm.initialize(activecomm);
+
+        MPI_CHECK(MPI_Barrier(activecomm));
+
+        Simulation simulation(cartcomm, activecomm, SignalHandling::check_termination_request);
+
+        simulation.run();
+    }
+
+    if (activecomm != cartcomm)
+        MPI_CHECK(MPI_Comm_free(&activecomm));
+
+    MPI_CHECK(MPI_Comm_free(&cartcomm));
+
+    MPI_CHECK(MPI_Finalize());
+
+    CUDA_CHECK(cudaDeviceSynchronize());
+
+    CUDA_CHECK(cudaDeviceReset());
+
+    return 0;
+}
