61d175d299467d16875390123582bf8f7377339e
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-02-14 12:33:55 +0100

    Refactoring

--- mpi-dpd/dpd-forces.cu
+++ mpi-dpd/dpd-forces.cu
@@ -13,6 +13,19 @@ __device__ float3 compute_dpd_force_traced(int type1, int type2,
     /* return the DPD interaction force based on particle types
      * type: 0 -- outer solvent, 1 -- inner solvent, 2 -- membrane, 3 -- wall */
 
+    /************** User-defined constants ***********/
+    // All particles: DPD interactions
+    const float gammadpd[4] = {8., 8., 8., 8.};  // default: 8
+    const float aij[4] = {4. / RC_FX, 4. / RC_FX, 4. / RC_FX, 4. / RC_FX};  // default: 4
+    const float gammadpd_pair = 0.5 * (gammadpd[type1] + gammadpd[type2]);
+    const float sigmaf_pair = sqrt(2*gammadpd_pair*kBT / dt);
+
+    // RBC-RBC: Lennard-Jones interactions
+    const float ljsigma = 0.3;  // default: 0.3
+    const float ljepsilon = 1.0 / (RC_FX*RC_FX);  // default: 1
+    /********** End of User-defined constants ********/
+
+
     const float _xr = pos1.x - pos2.x;
     const float _yr = pos1.y - pos2.y;
     const float _zr = pos1.z - pos2.z;
@@ -35,18 +48,6 @@ __device__ float3 compute_dpd_force_traced(int type1, int type2,
         yr * (vel1.y - vel2.y) +
         zr * (vel1.z - vel2.z);
 
-    // particle type dependent constants
-    const float gammadpd[4] = {8, 8, 8, 8};  // default: 8
-    const float aij[4] = {4 / RC_FX, 4 / RC_FX, 4 / RC_FX, 4 / RC_FX};  // default: 4
-
-    // RBC contact interactions
-    const float ljsigma = 0.3;
-    const float ljepsilon = 1.0 / (RC_FX*RC_FX);
-
-    const float aij_pair = 0.5 * (aij[type1] + aij[type2]);
-    const float gammadpd_pair = 0.5 * (gammadpd[type1] + gammadpd[type2]);
-    const float sigmaf_pair = sqrt(2*gammadpd_pair*kBT / dt);
-
     float strength = (-gammadpd_pair * wr * rdotv + sigmaf_pair * myrandnr) * wr;
     if (type1 == 2 && type2 == 2) {  // membrane contact
         const float invr2 = invrij * invrij;
@@ -54,9 +55,9 @@ __device__ float3 compute_dpd_force_traced(int type1, int type2,
         const float t4 = t2 * t2;
         const float t6 = t4 * t2;
         const float lj = min(1e4f, max(0.f, ljepsilon * 24.f * invrij * t6 * (2.f * t6 - 1.f)));
-
         strength += lj;
     } else {
+        const float aij_pair = 0.5 * (aij[type1] + aij[type2]);
         strength += aij_pair * argwr;
     }
 
