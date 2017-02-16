3f35154b7c9dbccf3a05ad7b42e7317b4177b67a
Sergey Litvinov <slitvinov@gmail.com>
2017-02-16 01:51:10 +0100

    Formatting in main.cu

--- mpi-dpd/main.cu
+++ mpi-dpd/main.cu
@@ -10,210 +10,168 @@
  *  before getting a written permission from the author of this file.
  */
 
-#include <cstdio>
-#include <csignal>
-#include <mpi.h>
-#include <errno.h>
-#include <dpd-rng.h>
-#include <map>
 #include "argument-parser.h"
 #include "common.h"
-#include "containers.h"
-#include "solvent-exchange.h"
-#include "dpd.h"
-#include "wall.h"
-#include "solute-exchange.h"
-#include "fsi.h"
 #include "contact.h"
+#include "containers.h"
+#include "dpd.h"
+#include "fsi.h"
 #include "redistribute-particles.h"
 #include "redistribute-rbcs.h"
-#include "velcontroller.h"
 #include "simulation.h"
+#include "solute-exchange.h"
+#include "solvent-exchange.h"
+#include "velcontroller.h"
+#include "wall.h"
+#include <csignal>
+#include <cstdio>
+#include <dpd-rng.h>
+#include <errno.h>
+#include <map>
+#include <mpi.h>
 
 float tend, desired_shrate;
-bool walls, pushtheflow, doublepoiseuille, rbcs, hdf5field_dumps, hdf5part_dumps, is_mps_enabled, adjust_message_sizes, contactforces;
+bool walls, pushtheflow, doublepoiseuille, rbcs, hdf5field_dumps,
+    hdf5part_dumps, is_mps_enabled, adjust_message_sizes, contactforces;
 int steps_per_dump, steps_per_hdf5dump, wall_creation_stepid;
 
-float RBCx0, RBCp, RBCcq, RBCkb, RBCka, RBCkv, RBCgammaC, RBCkd, RBCtotArea, RBCtotVolume, RBCscale;
+float RBCx0, RBCp, RBCcq, RBCkb, RBCka, RBCkv, RBCgammaC, RBCkd, RBCtotArea,
+    RBCtotVolume, RBCscale;
 
 LocalComm localcomm;
 
+int main(int argc, char **argv) {
+  int ranks[3];
 
-int main(int argc, char ** argv)
-{
-    int ranks[3];
+  // parsing of the positional arguments
+  if (argc < 4) {
+    printf("usage: ./mpi-dpd <xranks> <yranks> <zranks>\n");
+    exit(-1);
+  } else
+    for (int i = 0; i < 3; ++i)
+      ranks[i] = atoi(argv[1 + i]);
 
-    //parsing of the positional arguments
-    if (argc < 4)
-    {
-        printf("usage: ./mpi-dpd <xranks> <yranks> <zranks>\n");
-        exit(-1);
-    }
-    else
-        for(int i = 0; i < 3; ++i)
-            ranks[i] = atoi(argv[1 + i]);
+  ArgumentParser argp(vector<string>(argv + 4, argv + argc));
 
-    ArgumentParser argp(vector<string>(argv + 4, argv + argc));
+  tend = argp("-tend").asDouble(50);
+  walls = argp("-walls").asBool(false);
+  pushtheflow = argp("-pushtheflow").asBool(false);
+  doublepoiseuille = argp("-doublepoiseuille").asBool(false);
+  rbcs = argp("-rbcs").asBool(false);
+  hdf5field_dumps = argp("-hdf5field_dumps").asBool(false);
+  hdf5part_dumps = argp("-hdf5part_dumps").asBool(false);
+  steps_per_dump = argp("-steps_per_dump").asInt(1000);
+  steps_per_hdf5dump = argp("-steps_per_hdf5dump").asInt(2000);
+  wall_creation_stepid = argp("-wall_creation_stepid").asInt(5000);
+  adjust_message_sizes = argp("-adjust_message_sizes").asBool(false);
+  contactforces = argp("-contactforces").asBool(false);
+  hdf5field_dumps = argp("-hdf5field_dumps").asBool(false);
 
-    tend = argp("-tend").asDouble(50);
-    walls = argp("-walls").asBool(false);
-    pushtheflow = argp("-pushtheflow").asBool(false);
-    doublepoiseuille = argp("-doublepoiseuille").asBool(false);
-    rbcs = argp("-rbcs").asBool(false);
-    hdf5field_dumps = argp("-hdf5field_dumps").asBool(false);
-    hdf5part_dumps = argp("-hdf5part_dumps").asBool(false);
-    steps_per_dump = argp("-steps_per_dump").asInt(1000);
-    steps_per_hdf5dump = argp("-steps_per_hdf5dump").asInt(2000);
-    wall_creation_stepid = argp("-wall_creation_stepid").asInt(5000);
-    adjust_message_sizes = argp("-adjust_message_sizes").asBool(false);
-    contactforces = argp("-contactforces").asBool(false);
-    hdf5field_dumps = argp("-hdf5field_dumps").asBool(false);
+  // desired shear rate in DPD units
+  desired_shrate = argp("-shrate").asDouble(1);
+  RBCx0 = argp("-RBCx0").asDouble(0.5);
+  RBCp = argp("-RBCp").asDouble(0.0045);
+  RBCka = argp("-RBCka").asDouble(4900);
+  RBCkb = argp("-RBCkb").asDouble(40);
+  RBCkd = argp("-RBCkd").asDouble(100);
+  RBCkv = argp("-RBCkv").asDouble(5000);
+  RBCgammaC = argp("-RBCgammaC").asDouble(30);
+  RBCtotArea = argp("-RBCtotArea").asDouble(124);
+  RBCtotVolume = argp("-RBCtotVolume").asDouble(90);
 
-    // desired shear rate in DPD units
-    desired_shrate = argp("-shrate").asDouble(1);
-    RBCx0 = argp("-RBCx0").asDouble(0.5);
-    RBCp = argp("-RBCp").asDouble(0.0045);
-    RBCka = argp("-RBCka").asDouble(4900);
-    RBCkb = argp("-RBCkb").asDouble(40);
-    RBCkd = argp("-RBCkd").asDouble(100);
-    RBCkv = argp("-RBCkv").asDouble(5000);
-    RBCgammaC = argp("-RBCgammaC").asDouble(30);
-    RBCtotArea = argp("-RBCtotArea").asDouble(124);
-    RBCtotVolume = argp("-RBCtotVolume").asDouble(90);
-
-    RBCscale = 1/RC_FX;
+  RBCscale = 1 / RC_FX;
 
 #ifndef _NO_DUMPS_
-    const bool mpi_thread_safe = argp("-mpi_thread_safe").asBool(true);
+  const bool mpi_thread_safe = argp("-mpi_thread_safe").asBool(true);
 #else
-    const bool mpi_thread_safe = argp("-mpi_thread_safe").asBool(false);
+  const bool mpi_thread_safe = argp("-mpi_thread_safe").asBool(false);
 #endif
 
-    CUDA_CHECK(cudaSetDevice(0));
+  CUDA_CHECK(cudaSetDevice(0));
+  CUDA_CHECK(cudaDeviceReset());
 
-    CUDA_CHECK(cudaDeviceReset());
+  {
+    is_mps_enabled = false;
+    const char *mps_variables[] = {"CRAY_CUDA_MPS", "CUDA_MPS",
+                                   "CRAY_CUDA_PROXY", "CUDA_PROXY"};
+    for (int i = 0; i < 4; ++i)
+      is_mps_enabled |= getenv(mps_variables[i]) != NULL &&
+                        atoi(getenv(mps_variables[i])) != 0;
+  }
 
-    {
-        is_mps_enabled = false;
+  int nranks, rank;
+  if (mpi_thread_safe) {
+    // needed for the asynchronous data dumps
+    setenv("MPICH_MAX_THREAD_SAFETY", "multiple", 0);
+    int provided_safety_level;
+    MPI_CHECK(MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE,
+                              &provided_safety_level));
+    MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &nranks));
+    MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
 
-        const char * mps_variables[] = {
-            "CRAY_CUDA_MPS",
-            "CUDA_MPS",
-            "CRAY_CUDA_PROXY",
-            "CUDA_PROXY"
-        };
+    if (provided_safety_level != MPI_THREAD_MULTIPLE) {
+      if (rank == 0)
+        printf(
+            "ooooooooops MPI thread safety level is just %d. Aborting now.\n",
+            provided_safety_level);
+      abort();
+    } else if (rank == 0)
+      printf("I have set MPICH_MAX_THREAD_SAFETY=multiple\n");
+  } else {
+    MPI_CHECK(MPI_Init(&argc, &argv));
+    MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &nranks));
+    MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    const char *env_thread_safety = getenv("MPICH_MAX_THREAD_SAFETY");
+    if (rank == 0 && env_thread_safety)
+      printf("I read MPICH_MAX_THREAD_SAFETY=%s", env_thread_safety);
+  }
 
-        for(int i = 0; i < 4; ++i)
-            is_mps_enabled |= getenv(mps_variables[i])!= NULL && atoi(getenv(mps_variables[i])) != 0;
-    }
-
-    int nranks, rank;
-
-    if (mpi_thread_safe)
-    {
-        //needed for the asynchronous data dumps
-        setenv("MPICH_MAX_THREAD_SAFETY", "multiple", 0);
-
-        int provided_safety_level;
-        MPI_CHECK( MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided_safety_level));
-
-        MPI_CHECK( MPI_Comm_size(MPI_COMM_WORLD, &nranks) );
-        MPI_CHECK( MPI_Comm_rank(MPI_COMM_WORLD, &rank) );
-
-        if (provided_safety_level != MPI_THREAD_MULTIPLE)
-        {
-            if (rank == 0)
-                printf("ooooooooops MPI thread safety level is just %d. Aborting now.\n", provided_safety_level);
-            abort();
-        }
-        else
-            if (rank == 0)
-                printf("I have set MPICH_MAX_THREAD_SAFETY=multiple\n");
-    }
-    else
-    {
-        MPI_CHECK(MPI_Init(&argc, &argv));
-        MPI_CHECK( MPI_Comm_size(MPI_COMM_WORLD, &nranks) );
-        MPI_CHECK( MPI_Comm_rank(MPI_COMM_WORLD, &rank) );
-
-        const char * env_thread_safety = getenv("MPICH_MAX_THREAD_SAFETY");
-
-        if (rank == 0 && env_thread_safety)
-            printf("I read MPICH_MAX_THREAD_SAFETY=%s", env_thread_safety);
-    }
-
-    MPI_Comm activecomm = MPI_COMM_WORLD;
+  MPI_Comm activecomm = MPI_COMM_WORLD;
 #if defined(CUSTOM_REORDERING)
-    activecomm = setup_reorder_comm(MPI_COMM_WORLD, rank, nranks);
+  activecomm = setup_reorder_comm(MPI_COMM_WORLD, rank, nranks);
 #endif
 
-    bool reordering = true;
-
-    const char * env_reorder = getenv("MPICH_RANK_REORDER_METHOD");
-
-    //reordering of the ranks according to the computational domain and environment variables
-    if (atoi(env_reorder ? env_reorder : "-1") == atoi("3"))
-    {
-        reordering = false;
-
-        const bool usefulrank = rank < ranks[0] * ranks[1] * ranks[2];
-
-        MPI_CHECK(MPI_Comm_split(MPI_COMM_WORLD, usefulrank, rank, &activecomm)) ;
-
-        MPI_CHECK(MPI_Barrier(activecomm));
-
-        if (!usefulrank)
-        {
-            printf("rank %d has been thrown away\n", rank);
-            fflush(stdout);
-
-            MPI_CHECK(MPI_Barrier(activecomm));
-
-            MPI_Finalize();
-
-            return 0;
-        }
-
-        MPI_CHECK(MPI_Barrier(activecomm));
+  bool reordering = true;
+  const char *env_reorder = getenv("MPICH_RANK_REORDER_METHOD");
+  // reordering of the ranks according to the computational domain and
+  // environment variables
+  if (atoi(env_reorder ? env_reorder : "-1") == atoi("3")) {
+    reordering = false;
+    const bool usefulrank = rank < ranks[0] * ranks[1] * ranks[2];
+    MPI_CHECK(MPI_Comm_split(MPI_COMM_WORLD, usefulrank, rank, &activecomm));
+    MPI_CHECK(MPI_Barrier(activecomm));
+    if (!usefulrank) {
+      printf("rank %d has been thrown away\n", rank);
+      fflush(stdout);
+      MPI_CHECK(MPI_Barrier(activecomm));
+      MPI_Finalize();
+      return 0;
     }
 
-    MPI_Comm cartcomm;
+    MPI_CHECK(MPI_Barrier(activecomm));
+  }
 
-    int periods[] = {1, 1, 1};
-
-    MPI_CHECK( MPI_Cart_create(activecomm, 3, ranks, periods, (int)reordering, &cartcomm) );
-
-    activecomm = cartcomm;
-
-    //RAII
-    {
-        MPI_CHECK(MPI_Barrier(activecomm));
-
-        if (rank == 0)
-        {
-            argp.print_arguments();
-            fflush(stdout);
-        }
-
-        localcomm.initialize(activecomm);
-
-        MPI_CHECK(MPI_Barrier(activecomm));
-
-        Simulation simulation(cartcomm, activecomm);
-
-        simulation.run();
+  MPI_Comm cartcomm;
+  int periods[] = {1, 1, 1};
+  MPI_CHECK(MPI_Cart_create(activecomm, 3, ranks, periods, (int)reordering,
+                            &cartcomm));
+  activecomm = cartcomm;
+  // RAII
+  {
+    MPI_CHECK(MPI_Barrier(activecomm));
+    if (rank == 0) {
+      argp.print_arguments();
+      fflush(stdout);
     }
-
-    if (activecomm != cartcomm)
-        MPI_CHECK(MPI_Comm_free(&activecomm));
-
-    MPI_CHECK(MPI_Comm_free(&cartcomm));
-
-    MPI_CHECK(MPI_Finalize());
-
-    CUDA_CHECK(cudaDeviceSynchronize());
-
-    CUDA_CHECK(cudaDeviceReset());
-
-    return 0;
+    localcomm.initialize(activecomm);
+    MPI_CHECK(MPI_Barrier(activecomm));
+    Simulation simulation(cartcomm, activecomm);
+    simulation.run();
+  }
+  if (activecomm != cartcomm)
+    MPI_CHECK(MPI_Comm_free(&activecomm));
+  MPI_CHECK(MPI_Comm_free(&cartcomm));
+  MPI_CHECK(MPI_Finalize());
+  CUDA_CHECK(cudaDeviceSynchronize());
+  CUDA_CHECK(cudaDeviceReset());
 }
