f62a153ca5d192ab340ab4216d74e1cff94df114
Sergey Litvinov <slitvinov@gmail.com>
2017-02-17 11:03:11 +0100

    Move reference files; update tests

--- mpi-dpd/run_diag.sh
+++ mpi-dpd/run_diag.sh
@@ -7,31 +7,31 @@
 #  > atest run_diag.sh
 # add crap from test_data/* to git
 
-# sTEST: diag.t1
+# TEST: diag.t1
 # set -x
 # export PATH=../tools:$PATH
-# (cd ../..  && echo 0 0 0  1 0 0 8  0 1 0 8  0 0 1 8  0 0 0 1 > rbcs-ic.txt)
-# (cd ../..  && make clean && make -j && make -C ../tools)
-# (cd ../..  && rm -rf ply h5 diag.txt)
-# (cd ../..  && ./test 1 1 1 -rbcs -tend=0.5 -steps_per_dump=100)
-# (cd ../..  && awk '{print $2}' diag.txt    | fhash.awk -v tol=2) > diag.out.txt
+# echo 0 0 0  1 0 0 8  0 1 0 8  0 0 1 8  0 0 0 1 > rbcs-ic.txt
+# make clean && make -j && make -C ../tools
+# rm -rf ply h5 diag.txt
+# ./test 1 1 1 -rbcs -tend=0.5 -steps_per_dump=100
+# awk '{print $2}' diag.txt    | fhash.awk -v tol=2) > diag.out.txt
 #
-# sTEST: diag.t2
+# TEST: diag.t2
 # set -x
 # export PATH=../tools:$PATH
-# (cd ../..  && echo 0 0 0  1 0 0 8  0 1 0 8  0 0 1 8  0 0 0 1 > rbcs-ic.txt)
-# (cd ../..  && make clean && make -j && make -C ../tools)
-# (cd ../..  && rm -rf ply h5 diag.txt)
-# (cd ../..  && ./test 1 1 1 -rbcs -tend=0.5 -steps_per_dump=100)
-# (cd ../..  && ply2punto ply/rbcs-00009.ply | fhash.awk -v tol=2) > ply.out.txt
+# echo 0 0 0  1 0 0 8  0 1 0 8  0 0 1 8  0 0 0 1 > rbcs-ic.txt
+# make clean && make -j && make -C ../tools
+# rm -rf ply h5 diag.txt
+# ./test 1 1 1 -rbcs -tend=0.5 -steps_per_dump=100
+# ply2punto ply/rbcs-00009.ply | fhash.awk -v tol=2 > ply.out.txt
 #
-# cTEST: diag.t3
+# TEST: diag.t3
 # export PATH=../tools:$PATH
-# (cd ../..  && cp sdf/wall1/wall.dat                               sdf.dat)
+# cp sdf/wall1/wall.dat                               sdf.dat
 # x=0.75 y=8 z=12
-# (cd ../..  && echo 0 0 0  1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt)
-# (cd ../..  && make clean && make -j && make -C ../tools)
-# (cd ../..  && rm -rf ply h5 diag.txt)
-# (cd ../..  && ./test 1 1 1 -rbcs -tend=1.0 -steps_per_dump=300  -walls  -wall_creation_stepid=100 \
-#       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300)
-# (cd ../..  && ply2punto ply/rbcs-00009.ply | fhash.awk -v tol=2) > ply.out.txt
+# echo 0 0 0  1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
+# make clean && make -j && make -C ../tools
+# rm -rf ply h5 diag.txt
+# ./test 1 1 1 -rbcs -tend=1.0 -steps_per_dump=300  -walls  -wall_creation_stepid=100 \
+#       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300
+# ply2punto ply/rbcs-00009.ply | fhash.awk -v tol=2 > ply.out.txt
