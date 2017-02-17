e4bceaa7e62d59ab428b6f8d27ceb7a9ef62cd9f
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-02-17 15:19:23 +0100

    Add shear flow test (no rbcs)

--- mpi-dpd/run_diag.sh
+++ mpi-dpd/run_diag.sh
@@ -35,3 +35,12 @@
 # ./test 1 1 1 -rbcs -tend=1.0 -steps_per_dump=300  -walls  -wall_creation_stepid=100 \
 #       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300
 # ply2punto ply/rbcs-00009.ply | fhash.awk -v tol=2 > ply.out.txt
+#
+# TEST: diag.t4
+# export PATH=../tools:$PATH
+# cp sdf/wall1/wall.dat sdf.dat
+# make clean && make -j && make -C ../tools
+# rm -rf ply h5 diag.txt
+# ./test 1 1 1 -tend=1.0 -steps_per_dump=300 -walls -wall_creation_stepid=100 \
+#       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300
+# avg_h5.m h5/flowfields-0006.h5 | fhash.awk -v tol=2 > h5.out.txt
