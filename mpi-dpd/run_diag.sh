9b9c5efb5458e236d9c956624638b58000ec9124
Sergey Litvinov <slitvinov@gmail.com>
2017-02-18 21:18:26 +0100

    Making "around" test case

--- mpi-dpd/run_diag.sh
+++ mpi-dpd/run_diag.sh
@@ -71,14 +71,14 @@
 #       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300 -pushtheflow
 # mid_h5.m h5/flowfields-0026.h5 | fhash.awk -v tol=2 > h5.out.txt
 #
-# TEST: flow.around.t1
+# cTEST: flow.around.t1
 # export PATH=../tools:$PATH
 # cp .conf.around.h  .conf.h
 # cp sdf/cyl1/cyl.dat sdf.dat
-# x=0.75 y=8 z=11
+# x=0.75 y=8 z=9
 # echo 0 0 0  1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
 # make clean && make -j && make -C ../tools
 # rm -rf ply h5 diag.txt
-# ./test 1 1 1 -rbcs -tend=4.0 -steps_per_dump=5000 -walls -wall_creation_stepid=100 \
+# ./test 1 1 1 -rbcs -tend=4.0 -steps_per_dump=5000 -walls -wall_creation_stepid=1000 \
 #       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=5000 -pushtheflow
 # mid_h5.m h5/flowfields-0026.h5 | fhash.awk -v tol=2 > h5.out.txt
