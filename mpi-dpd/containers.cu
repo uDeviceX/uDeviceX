70474d8bda2b9d3fdecf7c74d9363db1bcb45ba5
Sergey Litvinov <slitvinov@gmail.com>
2017-02-19 18:07:11 +0100

    Preparing containers to move (III): cleanup

--- mpi-dpd/containers.cu
+++ mpi-dpd/containers.cu
@@ -1,4 +1,3 @@
-
 /*
  *  containers.cu
  *  Part of uDeviceX/mpi-dpd/
@@ -32,22 +31,22 @@ namespace ParticleKernels
 {
     __global__ void upd_stg1(bool rbcflag, Particle * p, Acceleration * a, int n, float dt,
 			     float _driving_acceleration, float threshold, bool doublePoiseuille) {
-        int pid = threadIdx.x + blockDim.x * blockIdx.x;
+	int pid = threadIdx.x + blockDim.x * blockIdx.x;
+	if (pid >= n) return;
 
-        if (pid >= n) return;
 	int type; float driving_acceleration, mass, vx = p[pid].u[0], y = p[pid].x[1];
 	if      ( rbcflag                            ) type = MEMB_TYPE;
 	else if (!rbcflag &&  last_bit_float::get(vx)) type =   IN_TYPE;
 	else if (!rbcflag && !last_bit_float::get(vx)) type =  OUT_TYPE;
 	mass                 = (type == MEMB_TYPE) ? rbc_mass : 1;
-        driving_acceleration = (type ==   IN_TYPE) ? 0        : _driving_acceleration;
+	driving_acceleration = (type ==   IN_TYPE) ? 0        : _driving_acceleration;
 	if (doublePoiseuille && y <= threshold) driving_acceleration *= -1;
 
-        for(int c = 0; c < 3; ++c) {
+	for(int c = 0; c < 3; ++c) {
 	  last_bit_float::Preserver up0(p[pid].u[0]);
 	  p[pid].u[c] += (a[pid].a[c]/mass + (c == 0 ? driving_acceleration : 0)) * dt * 0.5;
 	}
-        for(int c = 0; c < 3; ++c) p[pid].x[c] += p[pid].u[c] * dt;
+	for(int c = 0; c < 3; ++c) p[pid].x[c] += p[pid].u[c] * dt;
     }
 
     __global__ void upd_stg2_and_1(bool rbcflag, float2 * _pdata, float * _adata,
@@ -60,166 +59,166 @@ namespace ParticleKernels
 #else
 #define _ACCESS(x) (*(x))
 #endif
-        int warpid = threadIdx.x >> 5;
-        int base = 32 * (warpid + 4 * blockIdx.x);
-        int nsrc = min(32, nparticles - base);
+	int warpid = threadIdx.x >> 5;
+	int base = 32 * (warpid + 4 * blockIdx.x);
+	int nsrc = min(32, nparticles - base);
 
-        float2 *pdata = _pdata + 3 * base;
-        float *adata = _adata + 3 * base;
+	float2 *pdata = _pdata + 3 * base;
+	float *adata = _adata + 3 * base;
 
-        int laneid;
-        asm volatile ("mov.u32 %0, %%laneid;" : "=r"(laneid));
+	int laneid;
+	asm volatile ("mov.u32 %0, %%laneid;" : "=r"(laneid));
 
-        int nwords = 3 * nsrc;
+	int nwords = 3 * nsrc;
 
-        float2 s0, s1, s2;
-        float ax, ay, az;
+	float2 s0, s1, s2;
+	float ax, ay, az;
 
-        if (laneid < nwords)
-        {
-            s0 = _ACCESS(pdata + laneid);
-            ax = _ACCESS(adata + laneid);
-        }
+	if (laneid < nwords)
+	{
+	    s0 = _ACCESS(pdata + laneid);
+	    ax = _ACCESS(adata + laneid);
+	}
 
-        if (laneid + 32 < nwords)
-        {
-            s1 = _ACCESS(pdata + laneid + 32);
-            ay = _ACCESS(adata + laneid + 32);
-        }
+	if (laneid + 32 < nwords)
+	{
+	    s1 = _ACCESS(pdata + laneid + 32);
+	    ay = _ACCESS(adata + laneid + 32);
+	}
 
-        if (laneid + 64 < nwords)
-        {
-            s2 = _ACCESS(pdata + laneid + 64);
-            az = _ACCESS(adata + laneid + 64);
-        }
+	if (laneid + 64 < nwords)
+	{
+	    s2 = _ACCESS(pdata + laneid + 64);
+	    az = _ACCESS(adata + laneid + 64);
+	}
 
-        {
-            int srclane0 = (3 * laneid + 0) & 0x1f;
-            int srclane1 = (srclane0 + 1) & 0x1f;
-            int srclane2 = (srclane0 + 2) & 0x1f;
+	{
+	    int srclane0 = (3 * laneid + 0) & 0x1f;
+	    int srclane1 = (srclane0 + 1) & 0x1f;
+	    int srclane2 = (srclane0 + 2) & 0x1f;
 
-            int start = laneid % 3;
+	    int start = laneid % 3;
 
-            {
-                float t0 = __shfl(start == 0 ? s0.x : start == 1 ? s1.x : s2.x, srclane0);
-                float t1 = __shfl(start == 0 ? s2.x : start == 1 ? s0.x : s1.x, srclane1);
-                float t2 = __shfl(start == 0 ? s1.x : start == 1 ? s2.x : s0.x, srclane2);
+	    {
+		float t0 = __shfl(start == 0 ? s0.x : start == 1 ? s1.x : s2.x, srclane0);
+		float t1 = __shfl(start == 0 ? s2.x : start == 1 ? s0.x : s1.x, srclane1);
+		float t2 = __shfl(start == 0 ? s1.x : start == 1 ? s2.x : s0.x, srclane2);
 
-                s0.x = t0;
-                s1.x = t1;
-                s2.x = t2;
-            }
+		s0.x = t0;
+		s1.x = t1;
+		s2.x = t2;
+	    }
 
-            {
-                float t0 = __shfl(start == 0 ? s0.y : start == 1 ? s1.y : s2.y, srclane0);
-                float t1 = __shfl(start == 0 ? s2.y : start == 1 ? s0.y : s1.y, srclane1);
-                float t2 = __shfl(start == 0 ? s1.y : start == 1 ? s2.y : s0.y, srclane2);
+	    {
+		float t0 = __shfl(start == 0 ? s0.y : start == 1 ? s1.y : s2.y, srclane0);
+		float t1 = __shfl(start == 0 ? s2.y : start == 1 ? s0.y : s1.y, srclane1);
+		float t2 = __shfl(start == 0 ? s1.y : start == 1 ? s2.y : s0.y, srclane2);
 
-                s0.y = t0;
-                s1.y = t1;
-                s2.y = t2;
-            }
+		s0.y = t0;
+		s1.y = t1;
+		s2.y = t2;
+	    }
 
-            {
-                float t0 = __shfl(start == 0 ? ax : start == 1 ? ay : az, srclane0);
-                float t1 = __shfl(start == 0 ? az : start == 1 ? ax : ay, srclane1);
-                float t2 = __shfl(start == 0 ? ay : start == 1 ? az : ax, srclane2);
+	    {
+		float t0 = __shfl(start == 0 ? ax : start == 1 ? ay : az, srclane0);
+		float t1 = __shfl(start == 0 ? az : start == 1 ? ax : ay, srclane1);
+		float t2 = __shfl(start == 0 ? ay : start == 1 ? az : ax, srclane2);
 
-                ax = t0;
-                ay = t1;
-                az = t2;
-            }
-        }
+		ax = t0;
+		ay = t1;
+		az = t2;
+	    }
+	}
 
 	int type; float driving_acceleration, mass, vx = s1.y, y = s0.y;
 	if      (rbcflag                             ) type = MEMB_TYPE;
 	else if (!rbcflag &&  last_bit_float::get(vx)) type =  IN_TYPE;
 	else if (!rbcflag && !last_bit_float::get(vx)) type = OUT_TYPE;
 	mass                 = (type == MEMB_TYPE) ? rbc_mass : 1;
-        driving_acceleration = (type ==   IN_TYPE) ? 0        : _driving_acceleration;
-        if (doublePoiseuille && y <= threshold) driving_acceleration *= -1;
+	driving_acceleration = (type ==   IN_TYPE) ? 0        : _driving_acceleration;
+	if (doublePoiseuille && y <= threshold) driving_acceleration *= -1;
 
-        s1.y += (ax/mass + driving_acceleration) * dt;
-        s2.x += ay/mass * dt;
-        s2.y += az/mass * dt;
+	s1.y += (ax/mass + driving_acceleration) * dt;
+	s2.x += ay/mass * dt;
+	s2.y += az/mass * dt;
 
-        s0.x += s1.y * dt;
-        s0.y += s2.x * dt;
-        s1.x += s2.y * dt;
+	s0.x += s1.y * dt;
+	s0.y += s2.x * dt;
+	s1.x += s2.y * dt;
 
-        {
-            int srclane0 = (32 * ((laneid) % 3) + laneid) / 3;
-            int srclane1 = (32 * ((laneid + 1) % 3) + laneid) / 3;
-            int srclane2 = (32 * ((laneid + 2) % 3) + laneid) / 3;
+	{
+	    int srclane0 = (32 * ((laneid) % 3) + laneid) / 3;
+	    int srclane1 = (32 * ((laneid + 1) % 3) + laneid) / 3;
+	    int srclane2 = (32 * ((laneid + 2) % 3) + laneid) / 3;
 
-            int start = laneid % 3;
+	    int start = laneid % 3;
 
-            {
-                float t0 = __shfl(s0.x, srclane0);
-                float t1 = __shfl(s2.x, srclane1);
-                float t2 = __shfl(s1.x, srclane2);
+	    {
+		float t0 = __shfl(s0.x, srclane0);
+		float t1 = __shfl(s2.x, srclane1);
+		float t2 = __shfl(s1.x, srclane2);
 
-                s0.x = start == 0 ? t0 : start == 1 ? t2 : t1;
-                s1.x = start == 0 ? t1 : start == 1 ? t0 : t2;
-                s2.x = start == 0 ? t2 : start == 1 ? t1 : t0;
-            }
+		s0.x = start == 0 ? t0 : start == 1 ? t2 : t1;
+		s1.x = start == 0 ? t1 : start == 1 ? t0 : t2;
+		s2.x = start == 0 ? t2 : start == 1 ? t1 : t0;
+	    }
 
-            {
-                float t0 = __shfl(s0.y, srclane0);
-                float t1 = __shfl(s2.y, srclane1);
-                float t2 = __shfl(s1.y, srclane2);
+	    {
+		float t0 = __shfl(s0.y, srclane0);
+		float t1 = __shfl(s2.y, srclane1);
+		float t2 = __shfl(s1.y, srclane2);
 
-                s0.y = start == 0 ? t0 : start == 1 ? t2 : t1;
-                s1.y = start == 0 ? t1 : start == 1 ? t0 : t2;
-                s2.y = start == 0 ? t2 : start == 1 ? t1 : t0;
-            }
+		s0.y = start == 0 ? t0 : start == 1 ? t2 : t1;
+		s1.y = start == 0 ? t1 : start == 1 ? t0 : t2;
+		s2.y = start == 0 ? t2 : start == 1 ? t1 : t0;
+	    }
 
-            {
-                float t0 = __shfl(ax, srclane0);
-                float t1 = __shfl(az, srclane1);
-                float t2 = __shfl(ay, srclane2);
+	    {
+		float t0 = __shfl(ax, srclane0);
+		float t1 = __shfl(az, srclane1);
+		float t2 = __shfl(ay, srclane2);
 
-                ax = start == 0 ? t0 : start == 1 ? t2 : t1;
-                ay = start == 0 ? t1 : start == 1 ? t0 : t2;
-                az = start == 0 ? t2 : start == 1 ? t1 : t0;
-            }
-        }
+		ax = start == 0 ? t0 : start == 1 ? t2 : t1;
+		ay = start == 0 ? t1 : start == 1 ? t0 : t2;
+		az = start == 0 ? t2 : start == 1 ? t1 : t0;
+	    }
+	}
 
-        if (laneid < nwords) {
-            last_bit_float::Preserver up1(pdata[laneid].y);
-            pdata[laneid] = s0;
-        }
+	if (laneid < nwords) {
+	    last_bit_float::Preserver up1(pdata[laneid].y);
+	    pdata[laneid] = s0;
+	}
 
-        if (laneid + 32 < nwords) {
-            last_bit_float::Preserver up1(pdata[laneid + 32].y);
-            pdata[laneid + 32] = s1;
-        }
+	if (laneid + 32 < nwords) {
+	    last_bit_float::Preserver up1(pdata[laneid + 32].y);
+	    pdata[laneid + 32] = s1;
+	}
 
-        if (laneid + 64 < nwords) {
-            last_bit_float::Preserver up1(pdata[laneid + 64].y);
-            pdata[laneid + 64] = s2;
-        }
+	if (laneid + 64 < nwords) {
+	    last_bit_float::Preserver up1(pdata[laneid + 64].y);
+	    pdata[laneid + 64] = s2;
+	}
     }
 
     __global__ void clear_velocity(Particle *p, int n)  {
-        int pid = threadIdx.x + blockDim.x * blockIdx.x;
-        if (pid >= n) return;
-        last_bit_float::Preserver up(p[pid].u[0]);
-        for(int c = 0; c < 3; ++c)
-            p[pid].u[c] = 0;
+	int pid = threadIdx.x + blockDim.x * blockIdx.x;
+	if (pid >= n) return;
+	last_bit_float::Preserver up(p[pid].u[0]);
+	for(int c = 0; c < 3; ++c)
+	    p[pid].u[c] = 0;
     }
 }
 
 void ParticleArray::upd_stg1(bool rbcflag, float driving_acceleration, cudaStream_t stream) {
     if (size)
-        ParticleKernels::upd_stg1<<<(xyzuvw.size + 127) / 128, 128, 0, stream>>>(
-                rbcflag, xyzuvw.data, axayaz.data, xyzuvw.size, dt, driving_acceleration, globalextent.y * 0.5 - origin.y, doublepoiseuille);
+	ParticleKernels::upd_stg1<<<(xyzuvw.size + 127) / 128, 128, 0, stream>>>(
+		rbcflag, xyzuvw.data, axayaz.data, xyzuvw.size, dt, driving_acceleration, globalextent.y * 0.5 - origin.y, doublepoiseuille);
 }
 
 void  ParticleArray::upd_stg2_and_1(bool rbcflag, float driving_acceleration, cudaStream_t stream) {
     if (size)
-        ParticleKernels::upd_stg2_and_1<<<(xyzuvw.size + 127) / 128, 128, 0, stream>>>
-            (rbcflag, (float2 *)xyzuvw.data, (float *)axayaz.data, xyzuvw.size, dt, driving_acceleration, globalextent.y * 0.5 - origin.y, doublepoiseuille);
+	ParticleKernels::upd_stg2_and_1<<<(xyzuvw.size + 127) / 128, 128, 0, stream>>>
+	    (rbcflag, (float2 *)xyzuvw.data, (float *)axayaz.data, xyzuvw.size, dt, driving_acceleration, globalextent.y * 0.5 - origin.y, doublepoiseuille);
 }
 
 void ParticleArray::resize(int n) {
@@ -227,8 +226,8 @@ void ParticleArray::resize(int n) {
 
     // YTANG: need the array to be 32-padded for locally transposed storage of acceleration
     if ( n % 32 ) {
-        xyzuvw.preserve_resize( n - n % 32 + 32 );
-        axayaz.preserve_resize( n - n % 32 + 32 );
+	xyzuvw.preserve_resize( n - n % 32 + 32 );
+	axayaz.preserve_resize( n - n % 32 + 32 );
     }
     xyzuvw.resize(n);
     axayaz.resize(n);
@@ -246,12 +245,12 @@ void ParticleArray::preserve_resize(int n) {
     axayaz.preserve_resize(n);
 
     if (size > oldsize)
-        CUDA_CHECK(cudaMemset(axayaz.data + oldsize, 0, sizeof(Acceleration) * (size-oldsize)));
+	CUDA_CHECK(cudaMemset(axayaz.data + oldsize, 0, sizeof(Acceleration) * (size-oldsize)));
 }
 
 void ParticleArray::clear_velocity() {
     if (size)
-        ParticleKernels::clear_velocity<<<(xyzuvw.size + 127) / 128, 128 >>>(xyzuvw.data, xyzuvw.size);
+	ParticleKernels::clear_velocity<<<(xyzuvw.size + 127) / 128, 128 >>>(xyzuvw.data, xyzuvw.size);
 }
 
 void CollectionRBC::resize(int count) {
@@ -268,7 +267,7 @@ CollectionRBC::CollectionRBC(MPI_Comm cartcomm):
   cartcomm(cartcomm), ncells(0) {
   MPI_CHECK(MPI_Comm_rank(cartcomm, &myrank));
   MPI_CHECK( MPI_Cart_get(cartcomm, 3, dims, periods, coords) );
-  
+
   CudaRBC::get_triangle_indexing(indices, ntriangles);
   CudaRBC::Extent extent;
   CudaRBC::setup(nvertices, extent);
@@ -281,36 +280,28 @@ struct TransformedExtent {
 void CollectionRBC::setup(const char *path2ic) {
     vector<TransformedExtent> allrbcs;
     if (myrank == 0) {
-        //read transformed extent from file
-        FILE *f = fopen(path2ic, "r");
-        printf("READING FROM: <%s>\n", path2ic);
-        bool isgood = true;
+	//read transformed extent from file
+	FILE *f = fopen(path2ic, "r");
+	printf("READING FROM: <%s>\n", path2ic);
+	bool isgood = true;
+	while(isgood) {
+	    float tmp[19];
+	    for(int c = 0; c < 19; ++c) {
+		int retval = fscanf(f, "%f", tmp + c);
+		isgood &= retval == 1;
+	    }
 
-        while(isgood)
-        {
-            float tmp[19];
-            for(int c = 0; c < 19; ++c)
-            {
-                int retval = fscanf(f, "%f", tmp + c);
+	    if (isgood) {
+		TransformedExtent t;
 
-                isgood &= retval == 1;
-            }
+		for(int c = 0; c < 3; ++c) t.com[c] = tmp[c];
 
-            if (isgood)
-            {
-                TransformedExtent t;
-
-                for(int c = 0; c < 3; ++c)
-                    t.com[c] = tmp[c];
-
-                int ctr = 3;
-                for(int c = 0; c < 16; ++c, ++ctr)
-                    t.transform[c / 4][c % 4] = tmp[ctr];
-
-                allrbcs.push_back(t);
-            }
-        }
-        fclose(f);
+		int ctr = 3;
+		for(int c = 0; c < 16; ++c, ++ctr) t.transform[c / 4][c % 4] = tmp[ctr];
+		allrbcs.push_back(t);
+	    }
+	}
+	fclose(f);
 	printf("Instantiating %d CELLs from...<%s>\n", (int)allrbcs.size(), path2ic);
     } /* end of myrank == 0 */
 
@@ -328,22 +319,19 @@ void CollectionRBC::setup(const char *path2ic) {
     int L[3] = { XSIZE_SUBDOMAIN, YSIZE_SUBDOMAIN, ZSIZE_SUBDOMAIN };
 
     for(vector<TransformedExtent>::iterator it = allrbcs.begin(); it != allrbcs.end(); ++it) {
-        bool inside = true;
-
-        for(int c = 0; c < 3; ++c)
-            inside &= it->com[c] >= coords[c] * L[c] && it->com[c] < (coords[c] + 1) * L[c];
-
-        if (inside) {
-            for(int c = 0; c < 3; ++c)
-                it->transform[c][3] -= (coords[c] + 0.5) * L[c];
-            good.push_back(*it);
-        }
+	bool inside = true;
+	for(int c = 0; c < 3; ++c)
+	    inside &= it->com[c] >= coords[c] * L[c] && it->com[c] < (coords[c] + 1) * L[c];
+	if (inside) {
+	    for(int c = 0; c < 3; ++c)
+		it->transform[c][3] -= (coords[c] + 0.5) * L[c];
+	    good.push_back(*it);
+	}
     }
 
     resize(good.size());
-
     for(int i = 0; i < good.size(); ++i)
-        _initialize((float *)(xyzuvw.data + get_nvertices() * i), good[i].transform);
+	_initialize((float *)(xyzuvw.data + get_nvertices() * i), good[i].transform);
 }
 
 void CollectionRBC::_initialize(float *device_xyzuvw, float (*transform)[4]) {
@@ -354,20 +342,20 @@ void CollectionRBC::remove(int *entries, int nentries) {
     std::vector<bool > marks(ncells, true);
 
     for(int i = 0; i < nentries; ++i)
-        marks[entries[i]] = false;
+	marks[entries[i]] = false;
 
     std::vector< int > survivors;
     for(int i = 0; i < ncells; ++i)
-        if (marks[i])
-            survivors.push_back(i);
+	if (marks[i])
+	    survivors.push_back(i);
 
     int nsurvived = survivors.size();
 
     SimpleDeviceBuffer<Particle> survived(get_nvertices() *nsurvived);
 
     for(int i = 0; i < nsurvived; ++i)
-        CUDA_CHECK(cudaMemcpy(survived.data + get_nvertices() * i, data() + get_nvertices() * survivors[i],
-                    sizeof(Particle) * get_nvertices(), cudaMemcpyDeviceToDevice));
+	CUDA_CHECK(cudaMemcpy(survived.data + get_nvertices() * i, data() + get_nvertices() * survivors[i],
+		    sizeof(Particle) * get_nvertices(), cudaMemcpyDeviceToDevice));
 
     resize(nsurvived);
 
@@ -381,15 +369,15 @@ void CollectionRBC::_dump(const char *format4ply,
 
     //we fused VV stages so we need to recover the state before stage 1
     for(int i = 0; i < n; ++i) {
-        last_bit_float::Preserver up(p[i].u[0]);
-        for(int c = 0; c < 3; ++c) {
-            p[i].x[c] -= dt * p[i].u[c];
-            p[i].u[c] -= 0.5 * dt * a[i].a[c];
-        }
+	last_bit_float::Preserver up(p[i].u[0]);
+	for(int c = 0; c < 3; ++c) {
+	    p[i].x[c] -= dt * p[i].u[c];
+	    p[i].u[c] -= 0.5 * dt * a[i].a[c];
+	}
     }
     char buf[200];
     sprintf(buf, format4ply, ctr);
-    
+
     int rank;
     MPI_CHECK(MPI_Comm_rank(comm, &rank));
 
