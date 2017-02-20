26a59c2d99657845426570cd9dc4faa1a10078cc
Sergey Litvinov <slitvinov@gmail.com>
2017-02-20 20:50:15 +0100

    More formatting in solvent-exchange.h

--- mpi-dpd/solvent-exchange.h
+++ mpi-dpd/solvent-exchange.h
@@ -1,21 +1,30 @@
 class SolventExchange {
+public:
   MPI_Comm cartcomm;
   MPI_Request sendreq[26 * 2], recvreq[26], sendcellsreq[26], recvcellsreq[26],
       sendcountreq[26], recvcountreq[26];
-
   int recv_tags[26], recv_counts[26], nlocal, nactive;
-
   bool firstpost;
+  int myrank, nranks, dims[3], periods[3], coords[3], dstranks[26];
 
-protected:
+  // zero-copy allocation for acquiring the message offsets in the gpu send
+  // buffer
+  int *required_send_bag_size, *required_send_bag_size_host;
+
+  // plain copy of the offsets for the cpu (i speculate that reading multiple
+  // times the zero-copy entries is slower)
+  int nsendreq;
+  int3 halosize[26];
+  float safety_factor;
+  cudaEvent_t evfillall, evuploaded, evdownloaded;
+  const int basetag;
+
   struct SendHalo {
     int expected;
-
     SimpleDeviceBuffer<int> scattered_entries, tmpstart, tmpcount, dcellstarts;
     SimpleDeviceBuffer<Particle> dbuf;
     PinnedHostBuffer<int> hcellstarts;
     PinnedHostBuffer<Particle> hbuf;
-
     void setup(const int estimate, const int nhalocells) {
       adjust(estimate);
       dcellstarts.resize(nhalocells + 1);
@@ -30,23 +39,19 @@ protected:
       dbuf.resize(estimate);
       scattered_entries.resize(estimate);
     }
-
   } sendhalos[26];
 
   struct RecvHalo {
     int expected;
-
     PinnedHostBuffer<int> hcellstarts;
     PinnedHostBuffer<Particle> hbuf;
     SimpleDeviceBuffer<Particle> dbuf;
     SimpleDeviceBuffer<int> dcellstarts;
-
     void setup(const int estimate, const int nhalocells) {
       adjust(estimate);
       dcellstarts.resize(nhalocells + 1);
       hcellstarts.resize(nhalocells + 1);
     }
-
     void adjust(const int estimate) {
       expected = estimate;
       hbuf.resize(estimate);
@@ -55,44 +60,17 @@ protected:
 
   } recvhalos[26];
 
-  int myrank, nranks, dims[3], periods[3], coords[3], dstranks[26];
-
-  // zero-copy allocation for acquiring the message offsets in the gpu send
-  // buffer
-  int *required_send_bag_size, *required_send_bag_size_host;
-
-  // plain copy of the offsets for the cpu (i speculate that reading multiple
-  // times the zero-copy entries is slower)
-  int nsendreq;
-
-  int3 halosize[26];
-  float safety_factor;
-
   void post_expected_recv();
-
   void _pack_all(const Particle *const p, const int n,
-                 const bool update_baginfos, cudaStream_t stream);
-
+		 const bool update_baginfos, cudaStream_t stream);
   int nof_sent_particles();
-
-  cudaEvent_t evfillall, evuploaded, evdownloaded;
-
-  const int basetag;
-
   void _cancel_recv();
-
-public:
   SolventExchange(MPI_Comm cartcomm, const int basetag);
-
   void pack(const Particle *const p, const int n, const int *const cellsstart,
-            const int *const cellscount, cudaStream_t stream);
-
+	    const int *const cellscount, cudaStream_t stream);
   void post(const Particle *const p, const int n, cudaStream_t stream,
-            cudaStream_t downloadstream);
-
+	    cudaStream_t downloadstream);
   void recv(cudaStream_t stream, cudaStream_t uploadstream);
-
   void adjust_message_sizes(ExpectedMessageSizes sizes);
-
   virtual ~SolventExchange();
 };
