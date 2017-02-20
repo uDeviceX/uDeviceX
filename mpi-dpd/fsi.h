37969ecbd2041867a3c7c7fb62c7caa1261c197e
Sergey Litvinov <slitvinov@gmail.com>
2017-02-20 23:50:23 +0100

    Formatting

--- mpi-dpd/fsi.h
+++ mpi-dpd/fsi.h
@@ -1,18 +1,10 @@
 class ComputeFSI {
-  //TODO: use cudaEvent_t evuploaded;
-
   SolventWrap wsolvent;
-
   Logistic::KISS local_trunk;
-
- public:
-
-  void bind_solvent(SolventWrap wrap) { wsolvent = wrap; }
-
+public:
+  void bind_solvent(SolventWrap wrap) {wsolvent = wrap;}
   ComputeFSI(MPI_Comm comm);
-
   void bulk(std::vector<ParticlesWrap> wsolutes, cudaStream_t stream);
-
   /*override of SoluteExchange::Visitor::halo*/
   void halo(ParticlesWrap solutes[26], cudaStream_t stream);
 };
