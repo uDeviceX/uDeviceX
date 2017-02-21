1e1d70acbe21433a8f41a43bbf6c3f912c8c608c
Sergey Litvinov <slitvinov@gmail.com>
2017-02-21 15:01:41 +0100

    bipsbatch move (IV)

--- mpi-dpd/bipsbatch.decl.h
+++ mpi-dpd/bipsbatch.decl.h
@@ -1,21 +1,21 @@
 namespace BipsBatch {
-__constant__ unsigned int start[27];
+  __constant__ unsigned int start[27];
 
-enum HaloType { HALO_BULK = 0, HALO_FACE = 1, HALO_EDGE = 2, HALO_CORNER = 3 };
+  enum HaloType { HALO_BULK = 0, HALO_FACE = 1, HALO_EDGE = 2, HALO_CORNER = 3 };
 
-struct BatchInfo {
-  float *xdst;
-  float2 *xsrc;
-  float seed;
-  int ndst, nsrc, mask, *cellstarts, *scattered_entries, dx, dy, dz, xcells,
+  struct BatchInfo {
+    float *xdst;
+    float2 *xsrc;
+    float seed;
+    int ndst, nsrc, mask, *cellstarts, *scattered_entries, dx, dy, dz, xcells,
       ycells, zcells;
-  HaloType halotype;
-};
+    HaloType halotype;
+  };
 
-__constant__ BatchInfo batchinfos[26];
+  __constant__ BatchInfo batchinfos[26];
 
 
-bool firstcall = true;
+  bool firstcall = true;
 
-cudaEvent_t evhalodone;
+  cudaEvent_t evhalodone;
 }
