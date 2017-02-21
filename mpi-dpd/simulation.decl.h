95c31a069a2f28839fc49a5b8554385a8fb37c7d
Sergey Litvinov <slitvinov@gmail.com>
2017-02-21 23:52:38 +0100

    Formatting

--- mpi-dpd/simulation.decl.h
+++ mpi-dpd/simulation.decl.h
@@ -1,35 +1,35 @@
 namespace Sim {
-ParticleArray *particles, *newparticles;
-ParticleArray *rbcscoll;
+  ParticleArray *particles, *newparticles;
+  ParticleArray *rbcscoll;
 
-DeviceBuffer<float4 > *xyzouvwo;
-DeviceBuffer<ushort4> *xyzo_half;
+  DeviceBuffer<float4 > *xyzouvwo;
+  DeviceBuffer<ushort4> *xyzo_half;
 
-CellLists* cells;
+  CellLists* cells;
 
-bool wall_created = false;
-bool sim_is_done = false;
+  bool wall_created = false;
+  bool sim_is_done = false;
 
-MPI_Comm activecomm;
-cudaStream_t mainstream, uploadstream, downloadstream;
+  MPI_Comm activecomm;
+  cudaStream_t mainstream, uploadstream, downloadstream;
 
-size_t nsteps;
-float driving_acceleration = 0;
+  size_t nsteps;
+  float driving_acceleration = 0;
 
-pthread_t thread_datadump;
-pthread_mutex_t mutex_datadump;
-pthread_cond_t request_datadump, done_datadump;
-bool datadump_pending = false;
-int datadump_idtimestep, datadump_nsolvent, datadump_nrbcs;
-bool async_thread_initialized;
+  pthread_t thread_datadump;
+  pthread_mutex_t mutex_datadump;
+  pthread_cond_t request_datadump, done_datadump;
+  bool datadump_pending = false;
+  int datadump_idtimestep, datadump_nsolvent, datadump_nrbcs;
+  bool async_thread_initialized;
 
-PinnedHostBuffer<Particle>      *particles_datadump;
-PinnedHostBuffer<Acceleration>  *accelerations_datadump;
+  PinnedHostBuffer<Particle>      *particles_datadump;
+  PinnedHostBuffer<Acceleration>  *accelerations_datadump;
 
-cudaEvent_t evdownloaded;
+  cudaEvent_t evdownloaded;
 
 #define NPMAX 5000000 /* TODO: */
-float rbc_xx[NPMAX], rbc_yy[NPMAX], rbc_zz[NPMAX];
-float sol_xx[NPMAX], sol_yy[NPMAX], sol_zz[NPMAX];
-int iotags[NPMAX];
+  float rbc_xx[NPMAX], rbc_yy[NPMAX], rbc_zz[NPMAX];
+  float sol_xx[NPMAX], sol_yy[NPMAX], sol_zz[NPMAX];
+  int iotags[NPMAX];
 }
