a502e652c206aa1600a95728be15192dfba85e30
Sergey Litvinov <slitvinov@gmail.com>
2017-02-21 11:59:34 +0100

    Make a home for packinghalo

---
+++ mpi-dpd/packinghalo.decl.h
@@ -0,0 +1,17 @@
+namespace PackingHalo {
+  int ncells;
+  __constant__ int cellpackstarts[27];
+  struct CellPackSOA {
+    int *start, *count, *scan, size;
+    bool enabled;
+  };
+  __constant__ CellPackSOA cellpacks[26];
+  struct SendBagInfo {
+    int *start_src, *count_src, *start_dst;
+    int bagsize, *scattered_entries;
+    Particle *dbag, *hbag;
+  };
+
+  __constant__ SendBagInfo baginfos[26];
+  __constant__ int *srccells[26 * 2], *dstcells[26 * 2];
+}
