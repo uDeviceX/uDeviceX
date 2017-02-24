926bf814a3dbfa97311e132fdc23d6afd9b9b4f3
Sergey Litvinov <slitvinov@gmail.com>
2017-02-24 16:11:18 +0100

    Remove crap from StaticDeviceBuffer

--- mpi-dpd/common.tmp.h
+++ mpi-dpd/common.tmp.h
@@ -1,21 +1,10 @@
+#define MAX_PARTICLE_NUMBER 5000000
+
 /* container for the gpu particles during the simulation */
 template <typename T> struct StaticDeviceBuffer {
-  typedef T value_type;
-  /* `C': capacity; `S': size; `D' is for data*/
-  int C, S; T *D;
-
-  explicit StaticDeviceBuffer(int n = 0) : C(0), S(0), D(NULL) { resize(n); }
-  ~StaticDeviceBuffer() {
-    if (D != NULL) CC(cudaFree(D));
-    D = NULL;
-  }
-
-  void resize(int n) {
-    S = n;
-    if (C >= n) return;
-    if (D != NULL) CC(cudaFree(D));
-    int conservative_estimate = (int)ceil(1.1 * n);
-    C = 128 * ((conservative_estimate + 129) / 128);
-    CC(cudaMalloc(&D, sizeof(T) * C));
-  }
+  /* `S': size; `D' is for data*/
+  int S; T *D;
+  StaticDeviceBuffer() : S(0) {CC(cudaMalloc(&D, sizeof(T) * MAX_PARTICLE_NUMBER));}
+  ~StaticDeviceBuffer() {      CC(cudaFree(D));}
+  void resize(int n) {S = n;}
 };
