67b2ea1d2b584347bd6102a64eb0f19c00eac3eb
Sergey Litvinov <slitvinov@gmail.com>
2017-02-25 10:31:57 +0100

    Update tests; add a test with a simple RBC

--- mpi-dpd/run_diag.sh
+++ mpi-dpd/run_diag.sh
@@ -6,8 +6,10 @@
 # To update the test change TEST to cTEST and run
 #  > atest run_diag.sh
 # add crap from test_data/* to git
-#
-# TEST: diag.t1
+
+
+# Solvent in a periodic box
+# sTEST: diag.t1
 # set -x
 # export PATH=../tools:$PATH
 # cp .conf.test.h .conf.h
@@ -17,7 +19,21 @@
 # ./test 1 1 1 -rbcs -tend=0.5 -steps_per_dump=100
 # awk '{print 100*$2}' diag.txt    | fhash.awk -v tol=2 > diag.out.txt
 #
-# TEST: diag.t3
+
+# RBC in a periodic box
+# TEST: diag.t2
+# export PATH=../tools:$PATH
+# cp .conf.test.h .conf.h
+# x=0.75 y=8 z=12
+# echo 0 0 0  1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
+# make clean && make -j && make -C ../tools
+# rm -rf ply h5 diag.txt
+# ./test 1 1 1 -rbcs -tend=0.5 -steps_per_dump=300
+# ply2punto ply/rbcs-00003.ply | fround.awk -v tol=1 > ply.out.txt
+#
+
+# RBC with a wall
+# sTEST: diag.t3
 # export PATH=../tools:$PATH
 # cp .conf.test.h .conf.h
 # cp sdf/wall1/wall.dat                               sdf.dat
@@ -29,7 +45,8 @@
 #       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300
 # ply2punto ply/rbcs-00003.ply | fhash.awk -v tol=1 > ply.out.txt
 #
-# TEST: diag.t4
+
+# sTEST: diag.t4
 # export PATH=../tools:$PATH
 # cp .conf.test.h .conf.h
 # cp sdf/wall1/wall.dat sdf.dat
@@ -39,7 +56,7 @@
 #       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300
 # avg_h5.m h5/flowfields-0003.h5 | fhash.awk -v tol=1 > h5.out.txt
 #
-# TEST: diag.t5
+# sTEST: diag.t5
 # export PATH=../tools:$PATH
 # cp .conf.poiseuille.h .conf.h
 # cp sdf/wall1/wall.dat sdf.dat
@@ -49,7 +66,7 @@
 #       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300 -pushtheflow
 # avg_h5.m h5/flowfields-0013.h5 | fhash.awk -v tol=2 > h5.out.txt
 #
-# TEST: diag.t6
+# sTEST: diag.t6
 # export PATH=../tools:$PATH
 # cp .conf.poiseuille.h .conf.h
 # cp sdf/cyl1/cyl.dat sdf.dat
@@ -59,7 +76,7 @@
 #       -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300 -pushtheflow
 # mid_h5.m h5/flowfields-0026.h5 | fhash.awk -v tol=2 > h5.out.txt
 #
-# TEST: flow.around.t1
+# sTEST: flow.around.t1
 # export PATH=../tools:$PATH
 # cp .conf.around.h  .conf.h
 # cp sdf/cyl1/cyl.dat sdf.dat
@@ -72,7 +89,7 @@
 # mid_h5.m h5/flowfields-0001.h5 | fhash.awk -v tol=1 > h5.out.txt
 #
 # a test case with two RBCs around cylinder
-# TEST: flow.around.t2
+# sTEST: flow.around.t2
 # export PATH=../tools:$PATH
 # cp .conf.around.h  .conf.h
 # cp sdf/cyl1/cyl.dat sdf.dat
@@ -85,7 +102,7 @@
 # ply2punto ply/rbcs-00001.ply | fhash.awk -v tol=1 > ply.out.txt
 #
 ## a test case with two RBCs around cylinder with one RBC removed by the wall
-# TEST: flow.around.t3
+# sTEST: flow.around.t3
 # export PATH=../tools:$PATH
 # cp .conf.around.h  .conf.h
 # cp sdf/cyl1/cyl.dat sdf.dat
