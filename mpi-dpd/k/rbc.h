64f3d629d9b24e966c98d7c09a348b1ad7c36167
Sergey Litvinov <slitvinov@gmail.com>
2017-02-26 13:58:51 +0100

    Free rbc parameters (XV)

--- mpi-dpd/k/rbc.h
+++ mpi-dpd/k/rbc.h
@@ -63,31 +63,36 @@ __device__ __forceinline__ float3 _fvisc(float3 v1, float3 v2,
 template <int update>
 __device__ __forceinline__ float3 _fdihedral(float3 v1, float3 v2, float3 v3,
 					     float3 v4) {
-  float3 ksi = cross(v1 - v2, v1 - v3);
-  float3 dzeta = cross(v3 - v4, v2 - v4);
+  float overIksiI, overIdzeI, cosTheta, IsinThetaI2, sinTheta_1,
+    beta, b11, b12, phi, sint0kb, cost0kb;
 
-  float overIksiI = rsqrtf(dot(ksi, ksi));
-  float overIdzetaI = rsqrtf(dot(dzeta, dzeta));
+  float3 ksi = cross(v1 - v2, v1 - v3), dze = cross(v3 - v4, v2 - v4);
+  overIksiI = rsqrtf(dot(ksi, ksi));
+  overIdzeI = rsqrtf(dot(dze, dze));
 
-  float cosTheta = dot(ksi, dzeta) * overIksiI * overIdzetaI;
-  float IsinThetaI2 = 1.0f - cosTheta * cosTheta;
-  float sinTheta_1 = copysignf(
-      rsqrtf(max(IsinThetaI2, 1.0e-6f)),
-      dot(ksi - dzeta, v4 - v1)); // ">" because the normals look inside
-  float beta =
-      devParams.cost0kb - cosTheta * devParams.sint0kb * sinTheta_1;
+  cosTheta = dot(ksi, dze) * overIksiI * overIdzeI;
+  IsinThetaI2 = 1.0f - cosTheta * cosTheta;
 
-  float b11 = -beta * cosTheta * overIksiI * overIksiI;
-  float b12 = beta * overIksiI * overIdzetaI;
-  float b22 = -beta * cosTheta * overIdzetaI * overIdzetaI;
+  sinTheta_1 = copysignf
+    (rsqrtf(max(IsinThetaI2, 1.0e-6f)),
+     dot(ksi - dze, v4 - v1)); // ">" because the normals look inside
 
-  if (update == 1)
-    return cross(ksi, v3 - v2) * b11 + cross(dzeta, v3 - v2) * b12;
-  else if (update == 2)
-    return cross(ksi, v1 - v3) * b11 +
-	   (cross(ksi, v3 - v4) + cross(dzeta, v1 - v3)) * b12 +
-	   cross(dzeta, v3 - v4) * b22;
-  else
+  phi = RBCphi / 180.0 * M_PI;
+  sint0kb = sin(phi) * RBCkb;
+  cost0kb = cos(phi) * RBCkb;
+  beta = cost0kb - cosTheta * sint0kb * sinTheta_1;
+
+  b11 = -beta *  cosTheta * overIksiI * overIksiI;
+  b12 =  beta * overIksiI * overIdzeI;
+
+  if (update == 1) {
+    return cross(ksi, v3 - v2) * b11 + cross(dze, v3 - v2) * b12;
+  } else if (update == 2) {
+    float b22 = -beta * cosTheta * overIdzeI * overIdzeI;
+    return  cross(ksi, v1 - v3) * b11 +
+	   (cross(ksi, v3 - v4) + cross(dze, v1 - v3)) * b12 +
+	    cross(dze, v3 - v4) * b22;
+  } else
     return make_float3(0, 0, 0);
 }
 
