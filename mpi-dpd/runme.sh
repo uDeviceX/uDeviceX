519c8086cde5af675ffaf2df33d13a59110cb963
Sergey Litvinov <slitvinov@gmail.com>
2017-02-26 22:48:01 +0100

    Add comment to .cache.Makefile

--- mpi-dpd/runme.sh
+++ mpi-dpd/runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #
 #SBATCH --job-name="rbc_shear"
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=rbc_shear.%j.o
@@ -19,6 +19,6 @@ export HEX_COMM_FACTOR=2
 #srun --ntasks 1 --export ALL ./test 1 1 1 -rbcs -tend=10000 -steps_per_dump=1000 -shrate=1e-1 -RBCx0=0.4 -RBCp=5e-3 -RBCkb=40 -RBCka=4900 -RBCkd=100 -RBCkv=5000 -RBCgammaC=30 -RBCtotArea=124 -RBCtotVolume=90 -RBCfk=0
 
 rm -rf h5 ply
-srun --ntasks 1 --export ALL ./test 1 1 1 -rbcs -hdf5field_dumps -tend=100 -steps_per_dump=200 -steps_per_hdf5dump=200 -hdf5part_dumps -shrate=8
+srun --ntasks 1 --export ALL ./test 1 1 1
 
 #=====END====
