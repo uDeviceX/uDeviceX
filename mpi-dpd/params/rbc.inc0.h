7512a35b21ac1c78cf468b83d319e61f8417f478
Sergey Litvinov <slitvinov@gmail.com>
2017-02-26 02:59:47 +0100

    Add files with parameters

---
+++ mpi-dpd/params/rbc.inc0.h
@@ -0,0 +1,12 @@
+#define         RBCx0    0.5
+#define          RBCp 0.0045
+#define         RBCka   4900
+#define         RBCkb     40
+#define         RBCkd    100
+#define         RBCkv   5000
+#define     RBCgammaC     30
+#define    RBCtotArea    124
+#define  RBCtotVolume     90
+
+#define     RBCnv        498
+#define     RBCnt       1488
