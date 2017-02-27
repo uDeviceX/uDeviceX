c0e0a0d854b9540e59d3ddde306389e5d0a80374
Sergey Litvinov <slitvinov@gmail.com>
2017-02-27 22:05:00 +0100

    Add basic mpi tests

---
+++ mpi-dpd/test.mpi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#### RBC in a periodic box (mpi on falcon)
+# sTEST: mpi.t1
+# set -x
+# export PATH=../tools:$PATH
+# export PATH=/usr/lib64/mpich/bin:$PATH
+# echo 0 0 0  1 0 0 8  0 1 0 8  0 0 1 8  0 0 0 1 > rbcs-ic.txt
+# rm -rf ply h5 diag.txt
+# argp .conf.test.h -rbcs -tend=0.5 -steps_per_dump=100 > .conf.h
+# make clean && make -j && make -C ../tools
+# mpirun -n 2 ./test 2 1 1
+# awk '{print 100*$2}' diag.txt    | fhash.awk -v tol=2 > diag.out.txt
+
+#### Double poiseuille
+# TEST: mpi.t2
+# set -x
+# export PATH=../tools:$PATH
+# export PATH=/usr/lib64/mpich/bin:$PATH
+# rm -rf ply h5 diag.txt
+# argp .conf.double.poiseuille.h     \
+#   -tend=2.0 -steps_per_dump=100    \
+#   -pushtheflow -doublepoiseuille   \
+#   -hdf5field_dumps -hdf5part_dumps \
+#   -steps_per_hdf5dump=300 -pushtheflow > .conf.h
+# make clean && make -j && make -C ../tools
+# mpirun -n 2 ./test 2 1 1
+# avg_h52.m h5/flowfields-0013.h5 | fround.awk -v tol=2 > h5.out.txt
