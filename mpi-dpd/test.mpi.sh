169d24dab7bf78c192c984458e37b76ccf606972
Sergey Litvinov <slitvinov@gmail.com>
2017-02-27 22:14:28 +0100

    Add basic mpi tests (shear flow wall is not updated)

--- mpi-dpd/test.mpi.sh
+++ mpi-dpd/test.mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #### RBC in a periodic box (mpi on falcon)
-# sTEST: mpi.t1
+# TEST: mpi.t1
 # set -x
 # export PATH=../tools:$PATH
 # export PATH=/usr/lib64/mpich/bin:$PATH
@@ -26,3 +26,17 @@
 # make clean && make -j && make -C ../tools
 # mpirun -n 2 ./test 2 1 1
 # avg_h52.m h5/flowfields-0013.h5 | fround.awk -v tol=2 > h5.out.txt
+
+####
+# TEST: mpi.t3
+# export PATH=../tools:$PATH
+# export PATH=/usr/lib64/mpich/bin:$PATH
+# rm -rf ply h5 diag.txt
+# cp sdf/cyl1/cyl.dat sdf.dat
+# x=0.75 y=8 z=9; echo 0 0 0  1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
+# argp .conf.around.h \
+#    -rbcs -tend=4.0 -steps_per_dump=5000 -walls -wall_creation_stepid=1000 \
+#    -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=5000 -pushtheflow > .conf.h
+# make clean && make -j && make -C ../tools
+# mpirun -n 3 ./test 1 1 3
+# mid_h5.m h5/flowfields-0001.h5 | fhash.awk -v tol=1 > h5.out.txt
