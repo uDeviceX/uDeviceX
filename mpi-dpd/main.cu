79a9606494438616208596977fcc3125a8c305ff
Sergey Litvinov <slitvinov@gmail.com>
2017-02-28 06:16:30 +0100

    Clean up main from crap (III)

--- mpi-dpd/main.cu
+++ mpi-dpd/main.cu
@@ -11,25 +11,21 @@
 #include "bund.h"
 
 int main(int argc, char **argv) {
-  int rank, device = 0;
-  m::dims[0] = m::dims[1] = m::dims[2];
+  m::dims[0] = m::dims[1] = m::dims[2] = 1;
   for (int iarg = 1; iarg < argc && iarg <= 3; iarg++)
     m::dims[iarg - 1] = atoi(argv[iarg]);
 
+  int device = 0;
   CC(cudaSetDevice(device));
+  
   MC(MPI_Init(&argc, &argv));
-  MC(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+  MC(MPI_Comm_rank(MPI_COMM_WORLD, &m::rank));
+  MC(MPI_Cart_create(MPI_COMM_WORLD, m::d, m::dims, m::periods, 0, &m::cart));
 
-  MPI_Comm cartcomm, activecomm = MPI_COMM_WORLD;
-  MC(MPI_Cart_create(activecomm, 3, m::dims, periods, 0, &cartcomm));
-  activecomm = cartcomm;
-  MC(MPI_Barrier(activecomm));
-
-  sim::init(cartcomm, activecomm);
+  sim::init(m::cart, m::cart);
   sim::run();
   sim::close();
-
-  if (activecomm != cartcomm) MC(MPI_Comm_free(&activecomm));
-  MC(MPI_Comm_free(&cartcomm));
+  
+  MC(MPI_Comm_free(&m::cart));
   MC(MPI_Finalize());
 }
