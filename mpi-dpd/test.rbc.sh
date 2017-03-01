e62cc21a0855054ee9c2ab1c8ba5456a0396b129
Sergey Litvinov <slitvinov@gmail.com>
2017-03-02 00:53:54 +0100

    Add rotated RBC test

--- mpi-dpd/test.rbc.sh
+++ mpi-dpd/test.rbc.sh
@@ -8,7 +8,7 @@
 # add crap from test_data/* to git
 
 #### RBC in a periodic box
-# TEST: diag.t2
+# sTEST: diag.t2
 # export PATH=../tools:$PATH
 # rm -rf ply h5 diag.txt
 # x=0.75 y=8 z=12
@@ -18,8 +18,22 @@
 # ./test
 # ply2punto ply/rbcs-00003.ply | fround.awk -v tol=1 > ply.out.txt
 
+#### RBC initialy rotated
+# TEST: rotated.t1
+# export PATH=../tools:$PATH
+# rm -rf ply h5 diag.txt
+# x=0.75 y=8 z=12   c=0.5 s=0.866
+# echo $x $y $z    1  0   0 $x              \
+#                  0 $c -$s $y              \
+#                  0 $s  $c $z              \
+#                  0  0   0  1 > rbcs-ic.txt
+# argp .conf.test.h -rbcs -tend=0.5 -steps_per_dump=300 > .conf.h
+# make clean && make -j && make -C ../tools
+# ./test
+# ply2punto ply/rbcs-00003.ply | fround.awk -v tol=1 > ply.out.txt
+
 #### RBC with wall
-# TEST: diag.t3
+# sTEST: diag.t3
 # export PATH=../tools:$PATH
 # cp sdf/wall1/wall.dat                               sdf.dat
 # x=0.75 y=8 z=12; echo 0 0 0  1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
@@ -32,7 +46,7 @@
 # ply2punto ply/rbcs-00003.ply | fround.awk -v tol=1 > ply.out.txt
 
 #### one RBC around cylinder
-# TEST: flow.around.t1
+# sTEST: flow.around.t1
 # export PATH=../tools:$PATH
 # rm -rf ply h5 diag.txt
 # cp sdf/cyl1/cyl.dat sdf.dat
@@ -45,7 +59,7 @@
 # ply2punto ply/rbcs-00001.ply | fround.awk -v tol=1 > ply.out.txt
 
 ### two RBCs around cylinder
-# TEST: flow.around.t2
+# sTEST: flow.around.t2
 # export PATH=../tools:$PATH
 # rm -rf ply h5 diag.txt
 # x=0.75  y=3 z=9; echo 0 0 0  1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 >  rbcs-ic.txt
@@ -60,7 +74,7 @@
 # ply2punto ply/rbcs-00001.ply | fround.awk -v tol=0 > ply.out.txt
 
 ### two RBCs around cylinder with one RBC removed by the wall
-# TEST: flow.around.t3
+# sTEST: flow.around.t3
 # export PATH=../tools:$PATH
 # rm -rf ply h5 diag.txt
 # cp sdf/cyl1/cyl.dat sdf.dat
