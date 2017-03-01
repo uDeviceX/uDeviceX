eb3ba5e961a6458fc94eefd2beb31b49cab8b63e
Sergey Litvinov <slitvinov@gmail.com>
2017-03-01 18:36:47 +0100

    Formatting

--- mpi-dpd/k/wall.h
+++ mpi-dpd/k/wall.h
@@ -146,17 +146,16 @@ namespace k_wall {
   }
 
   __device__ void vell(float x, float y, float z,
-			    float *vxw, float *vyw, float *vzw) {
+		       float *vxw, float *vyw, float *vzw) {
     float z0 = glb::r0[2];
     *vxw = gamma_dot * (z - z0); *vyw = 0; *vzw = 0; /* velocity of the wall; */
   }
 
   __device__ void bounce_vel(float   xw, float   yw, float   zw, /* wall */
 			     float* vxp, float* vyp, float* vzp) {
-    float vx = *vxp,  vy = *vyp, vz = *vzp;
-
+    float vx = *vxp, vy = *vyp, vz = *vzp;
     float vxw, vyw, vzw; vell(xw, yw, zw, &vxw, &vyw, &vzw);
-
+    /* go to velocity relative to the wall; bounce; and go back */
     vx -= vxw; vx = -vx; vx += vxw;
     vy -= vyw; vy = -vy; vy += vyw;
     vz -= vzw; vz = -vz; vz += vzw;
