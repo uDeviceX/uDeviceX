40bc3d371bf74836e95873e5ddaa6fa41b346300
Sergey Litvinov <slitvinov@gmail.com>
2017-03-08 02:21:48 +0100

    Formatting

--- src/wall.impl.h
+++ src/wall.impl.h
@@ -5,27 +5,19 @@ namespace wall {
     setup_texture(k_wall::texWallCellCount, int);
     CC(cudaFuncSetCacheConfig(k_wall::interactions_3tpp, cudaFuncCachePreferL1));
 
-
-    if (m::rank == 0) printf("carving out wall particles...\n");
-
     thrust::device_vector<int> keys(n);
-
     k_sdf::fill_keys<<<k_cnf(n)>>>
       (pp, n, thrust::raw_pointer_cast(&keys[0]));
-
     thrust::sort_by_key(keys.begin(), keys.end(),
 			thrust::device_ptr<Particle>(pp));
-
     int nsurvived = thrust::count(keys.begin(), keys.end(), 0);
     int nbelt = thrust::count(keys.begin() + nsurvived, keys.end(), 1);
-
     thrust::device_vector<Particle> solid_local
       (thrust::device_ptr<Particle>(pp + nsurvived),
        thrust::device_ptr<Particle>(pp + nsurvived + nbelt));
 
     /* can't use halo-exchanger class because of MARGIN */
     if (m::rank == 0) printf("fetching remote wall particles...\n");
-
     DeviceBuffer<Particle> solid_remote;
 
     {
@@ -40,7 +32,6 @@ namespace wall {
 
 	int coordsneighbor[3];
 	for (int c = 0; c < 3; ++c) coordsneighbor[c] = m::coords[c] + d[c];
-
 	MC(MPI_Cart_rank(m::cart, coordsneighbor, dstranks + i));
       }
 
@@ -54,19 +45,16 @@ namespace wall {
 		       123 + recv_tags[i], m::cart, reqrecv + i));
 
 	int localsize = local.size();
-
 	MPI_Request reqsend[26];
 	for (int i = 0; i < 26; ++i)
 	  MC(MPI_Isend(&localsize, 1, MPI_INTEGER, dstranks[i], 123 + i,
 		       m::cart, reqsend + i));
-
 	MPI_Status statuses[26];
 	MC(MPI_Waitall(26, reqrecv, statuses));
 	MC(MPI_Waitall(26, reqsend, statuses));
       }
 
       std::vector<Particle> remote[26];
-
       // send local data - receive remote data
       {
 	for (int i = 0; i < 26; ++i) remote[i].resize(remsizes[i]);
@@ -76,7 +64,6 @@ namespace wall {
 	  MC(MPI_Irecv(remote[i].data(), remote[i].size() * 6, MPI_FLOAT,
 		       dstranks[i], 321 + recv_tags[i], m::cart,
 		       reqrecv + i));
-
 	MPI_Request reqsend[26];
 	for (int i = 0; i < 26; ++i)
 	  MC(MPI_Isend(local.data(), local.size() * 6, MPI_FLOAT,
@@ -91,21 +78,16 @@ namespace wall {
       std::vector<Particle> selected;
       int L[3] = {XS, YS, ZS};
       int MARGIN[3] = {XMARGIN_WALL, YMARGIN_WALL, ZMARGIN_WALL};
-      
+
       for (int i = 0; i < 26; ++i) {
 	int d[3] = {(i + 2) % 3 - 1, (i / 3 + 2) % 3 - 1, (i / 9 + 2) % 3 - 1};
-
 	for (int j = 0; j < remote[i].size(); ++j) {
 	  Particle p = remote[i][j];
-
 	  for (int c = 0; c < 3; ++c) p.r[c] += d[c] * L[c];
-
 	  bool inside = true;
-
 	  for (int c = 0; c < 3; ++c)
 	    inside &=
 	      p.r[c] >= -L[c] / 2 - MARGIN[c] && p.r[c] < L[c] / 2 + MARGIN[c];
-
 	  if (inside) selected.push_back(p);
 	}
       }
@@ -137,11 +119,8 @@ namespace wall {
     if (m::rank == 0) printf("consolidating wall particles...\n");
 
     if (w_n > 0)
-      k_sdf::strip_solid4<<<k_cnf(w_n)>>>
-	(solid, w_n, w_pp);
-
+      k_sdf::strip_solid4<<<k_cnf(w_n)>>>(solid, w_n, w_pp);
     CC(cudaFree(solid));
-
     return nsurvived;
   } /* end of ini */
 
@@ -171,7 +150,6 @@ namespace wall {
 
       k_wall::interactions_3tpp<<<k_cnf(3 * n)>>>
 	((float2 *)p, n, w_n, (float *)acc, trunk->get_float());
-
       CC(cudaUnbindTexture(k_wall::texWallParticles));
       CC(cudaUnbindTexture(k_wall::texWallCellStart));
       CC(cudaUnbindTexture(k_wall::texWallCellCount));
