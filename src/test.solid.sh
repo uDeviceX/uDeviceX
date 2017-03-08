fc94c34f45b733f2926dea6e0ac6acec2ab02b52
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-03-08 19:24:12 +0100

    Update tests

--- src/test.solid.sh
+++ src/test.solid.sh
@@ -37,3 +37,26 @@
 # make clean && make -j && make -C ../tools
 # ./udx
 # ply2punto ply/rbcs-00004.ply | fround.awk -v tol=1 > ply.out.txt
+#
+#### Double poiseuille rbc rotated
+# TEST: solid.t3
+# set -x
+# export PATH=../tools:$PATH
+# rm -rf ply h5 diag.txt
+# x=8 y=17 z=8   c=0.5 s=0.866
+# echo     1  0   0 $x              \
+#          0 $c -$s $y              \
+#          0 $s  $c $z              \
+#          0  0   0  1 > rbcs-ic.txt
+# cp bodies/rbc.off rbc.off
+# :
+# argp .conf.double.poiseuille.h     \
+#   -rbcs                            \
+#   -tend=1.0 -steps_per_dump=400    \
+#   -pushtheflow -doublepoiseuille   \
+#   -hdf5field_dumps -hdf5part_dumps \
+#   -steps_per_hdf5dump=400 > .conf.h
+# :
+# make clean && make -j && make -C ../tools
+# ./udx
+# ply2punto ply/rbcs-00004.ply | fround.awk -v tol=1 > ply.out.txt
