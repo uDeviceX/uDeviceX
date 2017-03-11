66507132c78cf72f8116e32f23c9650c957161f3
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-03-11 14:26:27 +0100

    Add h5part script

---
+++ tools/read_h5part.m
@@ -0,0 +1,17 @@
+#!/usr/bin/env octave-qf
+
+% Usage (octave-qf wrapper should be on the PATH):
+% > ./read_h5part.m test_data/all.h5part
+
+fn  = argv(){1}; % file name
+% fn  = 'test_data/all.h5part';
+D   = load('-hdf5', fn);
+
+% ts: timestep; f: field (x, y, ...)
+gs = @(ts)    getfield(D, sprintf('Step_%d', ts));
+gf = @(ts, f) getfield(gs(ts), f);
+
+ts = 0; xx0 = gf(0, "x"); yy0 = gf(0, "y"); zz0 = gf(0, "z");
+ts = 1; xx1 = gf(1, "x"); yy1 = gf(1, "y"); zz1 = gf(1, "z");
+
+dlmwrite(stdout(), [xx1', yy1', zz1'], ' ');
