8b530b230d58a2a642bf63353dce23179db02830
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-03-13 19:04:52 +0100

    Change averaging

--- tools/avg_h5.m
+++ tools/avg_h5.m
@@ -12,9 +12,10 @@ load(fn, 'u', 'v', 'w');
 vx = sq(u); vy = sq(v); vz = sq(w);
 nx = size(vx, X); ny = size(vy, Y); nz = size(vz, Z);
 xx = grd(nx); yy = grd(ny); zz = grd(nz);
+xxc = xx-nx/2; yyc = yy-ny/2; zzc = zz-nz/2;
 
 vx = sq(mean(vx, 1));
 vx = sq(mean(vx, 1));
 
 % dlmwrite('vx.dat', [zz' vx'], ' ');
-dlmwrite(stdout, vx', ' ');
+dlmwrite(stdout, [xxc; vx]', ' ');
