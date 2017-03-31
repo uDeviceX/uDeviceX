441a4734f3f9ff9368017fee4752915f480df0c9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-03-31 10:56:46 +0200

    bug fix for pp profile

--- run/circle_shear/profile.py
+++ run/circle_shear/profile.py
@@ -3,23 +3,35 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-assert len(sys.argv) == 2
+assert len(sys.argv) >= 2
 
-filename = sys.argv[1]
+for fid in range(1, len(sys.argv)):
+    filename = sys.argv[fid]
+    
+    f = h5py.File(filename, "r")
 
-f = h5py.File(filename, "r")
+    if fid == 1:
+        vx = np.array(f['u'])
+        vy = np.array(f['v'])
+    else:
+        vx += np.array(f['u'])
+        vy += np.array(f['v'])
 
-vx = np.array(f['u'])
-vy = np.array(f['v'])
+vx = vx * (1.0 / (len(sys.argv)-1))
+vy = vy * (1.0 / (len(sys.argv)-1))
+
+ZS, YS, XS, dummy = vx.shape
+
+vx = np.reshape(vx, (ZS, YS, XS,))
+vy = np.reshape(vy, (ZS, YS, XS,))
 
 # average in z direction
-vx = np.mean(vx, (2))
-vy = np.mean(vy, (2))
+vx = np.mean(vx, (0))
+vy = np.mean(vy, (0))
 
 def plot_at_x(v, i):
     #select border only (x = 0)
-    v_ = v[i,:,:]
-    v_ = v_.reshape((-1,))
+    v_ = v[:,i]
     
     yy = np.array(range(len(v_)))
     
@@ -39,7 +51,9 @@ def plot_at_x(v, i):
     
 plt.figure()
 
-for ix in [0, 7, 15]:
+indices = [0, XS/4-1, XS/2-1]
+
+for ix in indices:
      plot_at_x(vx, ix)
 
 plt.xlabel('y')
