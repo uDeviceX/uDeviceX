4e7ebb975c7b222d6bbe11a901589987195b766b
Sergey Litvinov <slitvinov@gmail.com>
2017-04-04 19:45:38 +0200

    Add h5to3d.m tool

---
+++ tools/h5to3d.m
@@ -0,0 +1,38 @@
+#!/usr/bin/env octave-qf
+
+% Usage (octave-qf wrapper should be on the PATH):
+% > ./hello_h5part.m test_data/all.h5part
+
+% convert h5part file to several 3D files
+
+# TEST: h5to3d.t1
+# ./h5to3d.m test_data/all.h5part 3d
+# cat 3d/000[01].3d  > h5to3d.out.txt
+
+fn = argv(){1}; % file name
+% fn = 'test_data/all.h5part'; % input file
+
+d = argv(){2};
+% d  = '3d'                   ; % output directory
+
+fmt = '%s/%04d.3d'          ; % format of output file
+D   = load('-hdf5', fn);
+
+% number of timesteps
+nt = length(fieldnames(D));
+
+gs = @(ts)    getfield(D, sprintf('Step_%d', ts)); % get string
+gf = @(ts, f) getfield(gs(ts), f); % get field
+fi = @(x, idx) x(idx); % filter by idx
+ou = @(ts)     sprintf(fmt, d, ts);
+
+mkdir(d)
+for ts=0:nt-1 % loop over timesteps
+  xx = gf(ts, 'x'); yy = gf(ts, 'y'); zz = gf(ts, 'z'); type = gf(ts, 'type');
+  idx = (type == 1);
+  xx = fi(xx, idx); yy = fi(yy, idx); zz = fi(zz, idx);
+  dlmwrite(ou(ts), [xx', yy', zz'], ' ');
+  fprintf(stderr(), '(h5to3d.m) writing %s\n', ou(ts))
+end
+
+
