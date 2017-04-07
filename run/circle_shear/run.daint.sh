a109ac7cdb760aae49078202c17b20fbdb55c1a8
amlucas <lucas.amoudruz@wanadoo.fr>
2017-04-07 17:50:46 +0200

    updated cache daint

--- run/circle_shear/run.daint.sh
+++ run/circle_shear/run.daint.sh
@@ -23,7 +23,7 @@ ZS=8
 
 argp .conf.test.h                                                        \
      -tend=1000.0 -steps_per_dump=1000 -walls -wall_creation_stepid=5000 \
-     -hdf5part_dumps -steps_per_hdf5dump=1000                            \
+     -hdf5field_dumps=false -hdf5part_dumps -steps_per_hdf5dump=1000     \
      -gamma_dot=${GDOT} -rbcs -rcyl=5 -pin_com=true -dt=1e-3 -shear_y    \
      -rbc_mass=1.f -XS=${XS} -YS=${YS} -ZS=${ZS}                         \
      > .conf.h
