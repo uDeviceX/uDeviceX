47a643befc1a178fd203c839a019e873a77b71e9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-04-07 14:20:54 +0200

    don t need hdf5 field

--- run/circle_shear/run.daint.sh
+++ run/circle_shear/run.daint.sh
@@ -21,11 +21,11 @@ XS=64
 YS=64
 ZS=8
 
-argp .conf.test.h                                                       \
+argp .conf.test.h                                                        \
      -tend=1000.0 -steps_per_dump=1000 -walls -wall_creation_stepid=5000 \
-     -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=1000          \
-     -gamma_dot=${GDOT} -rbcs -rcyl=5 -pin_com=true -dt=1e-3 -shear_y   \
-     -rbc_mass=1.f -XS=${XS} -YS=${YS} -ZS=${ZS}                        \
+     -hdf5part_dumps -steps_per_hdf5dump=1000                            \
+     -gamma_dot=${GDOT} -rbcs -rcyl=5 -pin_com=true -dt=1e-3 -shear_y    \
+     -rbc_mass=1.f -XS=${XS} -YS=${YS} -ZS=${ZS}                         \
      > .conf.h
 
 make clean && make -j && make -C ../tools
