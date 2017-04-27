9746d257e00dd4b25b6e9f0e0cee9ef8b751aaef
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-04-27 16:55:35 +0200

    mpi io

--- src/dump.impl.h
+++ src/dump.impl.h
@@ -8,25 +8,31 @@ namespace dump
     
     void parts(const Particle *pp, const long n, const char *name, const int step)
     {
-        // serial for now
-        if (m::rank != 0) exit(1);
-        
         char fname[256] = {0};
         sprintf(fname, "bop/%s-%05d.bop", name, step);
-        
-        FILE *f = fopen(fname, "w");
 
-        if (f == NULL)
+        MPI_File f;
+        MPI_Status status;
+        MPI_Offset base, offset = 0;
+        MPI_Offset len = n * sizeof(Particle);
+        
+        MC( MPI_File_open(m::cart, fname, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f) );
+
+        MC( MPI_File_set_size(f, 0) );
+        MC( MPI_File_get_position(f, &base) ); 
+
+        if (m::rank == 0)
         {
-            fprintf(stderr, "(dump) error opening <%s>\n", fname);
-            exit(1);
+            MC( MPI_File_write (f, &n, 1, MPI_LONG, &status));
+            len += sizeof(long);
+            base += sizeof(long);
         }
 
-        long nl = n;
-        fwrite(&nl, sizeof(long), 1, f);
-        fwrite(pp, sizeof(Particle), n, f);
+        MC( MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, m::cart) );
         
-        fclose(f);
+        MC( MPI_File_write_at_all(f, base + offset, pp, n, Particle::datatype(), &status) ); 
+        
+        MC( MPI_File_close(&f) );
     }
 
 }
