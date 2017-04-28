82a2a5c37b3087976be7971daf2794a54b17271a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-04-28 14:26:41 +0200

    bop2test in test.solid.sh

--- src/test.solid.sh
+++ src/test.solid.sh
@@ -1,5 +1,5 @@
 #### Couette pinned sphere
-# sTEST: solid.t1
+# TEST: solid.t1
 # export PATH=../tools:$PATH
 # rm -rf h5 diag.txt solid_diag.txt
 # cp sdf/wall1/wall.dat sdf.dat
@@ -15,7 +15,7 @@
 # avg_h5.m h5/flowfields-0001.h5 | fround.awk -v tol=1 > h5.out.txt
 
 #### Double poiseuille pinned sphere
-# sTEST: solid.t2
+# TEST: solid.t2
 # set -x
 # export PATH=../tools:$PATH
 # rm -rf h5 diag.txt solid_diag.txt
@@ -25,16 +25,16 @@
 #   -rbcs -rsph=4 -pin_com=false      \
 #   -tend=2.0 -steps_per_dump=100     \
 #   -pushtheflow -doublepoiseuille    \
-#   -hdf5field_dumps --part_dumps     \
+#   -hdf5field_dumps -part_dumps      \
 #   -steps_per_hdf5dump=100 > .conf.h
 # :
 # make clean && make -j && make -C ../tools
 # ./udx
-# read_h5part.m h5/s.h5part | fround.awk -v tol=1 > h5part.out.txt
+# bop2test.py bop/solid-00004.bop | fround.awk -v tol=1 > bop.out.txt
 #
 
 #### Double poiseuille pinned cylinder
-# sTEST: solid.t3
+# TEST: solid.t3
 # set -x
 # export PATH=../tools:$PATH
 # rm -rf h5 diag.txt solid_diag.txt debug.txt
@@ -42,18 +42,18 @@
 # :
 # argp .conf.double.poiseuille.h     \
 #   -rbcs -rcyl=4 -pin_com=true      \
-#   -tend=2.0 -steps_per_dump=100    \
+#   -tend=0.5 -steps_per_dump=100    \
 #   -pushtheflow -doublepoiseuille   \
 #   -hdf5field_dumps -part_dumps     \
 #   -steps_per_hdf5dump=100 > .conf.h
 # :
 # make clean && make -j && make -C ../tools
 # ./udx
-# read_h5part.m h5/s.h5part | fround.awk -v tol=1 > h5part.out.txt
+# bop2test.py bop/solid-00004.bop | fround.awk -v tol=1 > bop.out.txt
 #
 
 #### Couette pinned cylinder
-# sTEST: solid.t4
+# TEST: solid.t4
 # export PATH=../tools:$PATH
 # rm -rf h5 diag.txt solid_diag.txt
 # cp sdf/yplates1/yplates.dat sdf.dat
@@ -69,7 +69,7 @@
 # avg_h5.m h5/flowfields-0001.h5 | fround.awk -v tol=1 > h5.out.txt
 
 #### Double poiseuille pinned ellipse
-# sTEST: solid.t5
+# TEST: solid.t5
 # set -x
 # export PATH=../tools:$PATH
 # rm -rf h5 diag.txt solid_diag.txt debug.txt
@@ -85,5 +85,5 @@
 # :
 # make clean && make -j && make -C ../tools
 # ./udx
-# read_h5part.m h5/s.h5part | fround.awk -v tol=1 > h5part.out.txt
+# bop2test.py bop/solid-00004.bop | fround.awk -v tol=1 > bop.out.txt
 #
