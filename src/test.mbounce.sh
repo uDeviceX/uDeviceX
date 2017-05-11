f57a0b193ae255257597f6cfcbc0aa5d2a90db9e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-05-11 17:52:03 +0200

    updated test.mbounce

--- src/test.mbounce.sh
+++ src/test.mbounce.sh
@@ -5,14 +5,14 @@
 # cp sdf/wall1/wall.dat sdf.dat
 # echo -e "16 16 16" > ic_solid.txt
 # :
-# argp .conf.test.h                                                  \
-#   -tend=2.01 -steps_per_dump=500 -walls -wall_creation_stepid=100  \
-#   -hdf5field_dumps -part_dumps  -steps_per_hdf5dump=1000           \
+# argp .conf.test.h                                                \
+#   -tend=2.01 -steps_per_dump=500 -walls -wall_creation_stepid=0  \
+#   -hdf5field_dumps -part_dumps  -steps_per_hdf5dump=1000         \
 #   -rbcs -sbounce_back -pin_com -dt=1e-3 -shear_z > .conf.h
 # :
 # (make clean && make -j && make -C ../tools) > /dev/null
 # ./udx
-# bop2txt bop/solid-00004.bop | awk '{print $1, $2}' | uscale 10 > bop.out.txt
+# bop2txt bop/solid-00004.bop | awk '{print $1, $2}' > bop.out.txt
 
 #### Double poiseuille non pinned sphere
 # nTEST: mbounce.t2
