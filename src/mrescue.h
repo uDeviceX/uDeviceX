33cd4ad74a583345cb6ce4311ca4525f0517c13e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-05-19 10:39:10 +0200

    updated prototype

--- src/mrescue.h
+++ src/mrescue.h
@@ -3,6 +3,6 @@ namespace mrescue
     void init(int n);
     void close();
 
-    void rescue_hst(const int *tt, const int nt, const Particle *ipp, const int n, /**/ Particle *pp);
-    //void rescue_dev(const int *tt, const int nt, const Particle *ipp, const int n, /**/ Particle *pp);
+    void rescue_hst(const Mesh m, const Particle *i_pp, const int ns, const int n,
+                    const int *tcstarts, const int *tccounts, const int *tcids, /**/ Particle *pp);
 }
