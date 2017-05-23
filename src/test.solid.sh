f6b9f600aa86dc1248f9c6cdd8a470a9a93fc04e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-05-23 11:56:38 +0200

    repopulating test.solid

--- src/test.solid.sh
+++ src/test.solid.sh
@@ -1,28 +1,29 @@
 #### Couette pinned sphere
-# sTEST: solid.t1
+# nTEST: solid.t1
 # export PATH=../tools:$PATH
 # rm -rf h5 bop diag.txt solid_diag.txt
 # cp sdf/wall1/wall.dat sdf.dat
 # echo -e "16 16 16" > ic_solid.txt
+# cp data/sphere.ply mesh_solid.ply
 # :
-# argp .conf.test.h                                                  \
-#   -tend=2.01 -steps_per_dump=1000 -walls -wall_creation_stepid=100 \
-#   -hdf5field_dumps -part_dumps  -steps_per_hdf5dump=1000           \
-#   -rbcs -sbounce_back -rsph=5 -pin_com -dt=1e-3 -shear_z > .conf.h
+# argp .conf.test.h                                                \
+#   -tend=2.01 -steps_per_dump=1000 -walls -wall_creation_stepid=1 \
+#   -hdf5field_dumps -part_dumps -steps_per_hdf5dump=1000          \
+#   -rbcs -sbounce_back -pin_com -dt=1e-3 -shear_z > .conf.h
 # :
 # (make clean && make -j && make -C ../tools) > /dev/null
 # ./udx
 # avg_h5.m h5/flowfields-0001.h5 | sed -n '4,29p' | uscale 0.5 > h5.out.txt
 
 #### Double poiseuille non pinned sphere
-# sTEST: solid.t2
-# set -x
+# nTEST: solid.t2
 # export PATH=../tools:$PATH
 # rm -rf h5 bop diag.txt solid_diag.txt
 # echo -e "8 16 8" > ic_solid.txt
+# cp data/sphere.ply mesh_solid.ply
 # :
 # argp .conf.double.poiseuille.h      \
-#   -rbcs -rsph=4 -sbounce_back       \
+#   -rbcs -sbounce_back               \
 #   -tend=0.5 -steps_per_dump=100     \
 #   -pushtheflow -doublepoiseuille    \
 #   -hdf5field_dumps -part_dumps      \
@@ -35,18 +36,18 @@
 #
 
 #### Double poiseuille pinned cylinder
-# sTEST: solid.t3
-# set -x
+# nTEST: solid.t3
 # export PATH=../tools:$PATH
-# rm -rf h5 bop diag.txt solid_diag.txt debug.txt
+# rm -rf h5 bop diag.txt solid_diag.txt
 # echo -e "8 16 8" > ic_solid.txt
+# cp data/cyl.ply mesh_solid.ply
 # :
-# argp .conf.double.poiseuille.h              \
-#   -rbcs -rcyl=4 -pin_com -sbounce_back      \
-#   -tend=0.5 -steps_per_dump=100             \
-#   -pushtheflow -doublepoiseuille            \
-#   -hdf5field_dumps -part_dumps              \
-#   -wall_creation_stepid=0                   \
+# argp .conf.double.poiseuille.h        \
+#   -rbcs -spdir=2 -sbounce_back        \
+#   -tend=0.5 -steps_per_dump=100       \
+#   -pushtheflow -doublepoiseuille      \
+#   -hdf5field_dumps -part_dumps        \
+#   -wall_creation_stepid=0             \
 #   -steps_per_hdf5dump=100 > .conf.h
 # :
 # (make clean && make -j && make -C ../tools) > /dev/null
@@ -55,16 +56,17 @@
 #
 
 #### Couette pinned cylinder
-# sTEST: solid.t4
+# nTEST: solid.t4
 # export PATH=../tools:$PATH
 # rm -rf h5 bop diag.txt solid_diag.txt
 # cp sdf/yplates1/yplates.dat sdf.dat
 # echo -e "16 16 16" > ic_solid.txt
+# cp data/cyl.ply mesh_solid.ply
 # :
 # argp .conf.test.h  \
 #   -tend=2.01 -steps_per_dump=1000 -walls -wall_creation_stepid=100 \
 #   -hdf5field_dumps -part_dumps -steps_per_hdf5dump=1000            \
-#   -rbcs -rcyl=5 -pin_com -sbounce_back -dt=1e-3 -shear_y > .conf.h
+#   -rbcs -spdir=2 -sbounce_back -dt=1e-3 -shear_y > .conf.h
 # :
 # (make clean && make -j && make -C ../tools) > /dev/null
 # ./udx
