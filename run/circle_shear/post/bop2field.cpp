8414df4993b60d297b6b83fc57bf189e87a6f0c2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-05-23 18:19:20 +0200

    post in radial coords

--- run/circle_shear/post/bop2field.cpp
+++ run/circle_shear/post/bop2field.cpp
@@ -1,7 +1,57 @@
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
+#include <cmath>
 
 #include "reader.h"
+
+
+void radial_v(const float *com, const float Rmin, const float Rmax, const int nR,
+              const float *data, const long n, const int nvars, float *av, int *counts)
+{
+    const int nf = nvars - 3;
+    if (nf > 0) memset(av, 0, nR * nf * sizeof(float));
+    memset(counts, 0, nR*sizeof(int));
+
+    const float dr = (Rmax - Rmin) / nR;
+    
+    for (long i = 0; i < n; ++i)
+    {
+        const long base = i*nvars;
+        const float *ro = data + base;
+        const float *fo  = data + base + 3;
+
+        const float r[3] = {ro[0] - com[0], ro[1] - com[1], ro[2] - com[2]};
+        const float r_ = sqrt(r[0]*r[0] + r[1]*r[1]); // suppose cylinder in z direction
+
+        const int ir = (int) floor( (r_-Rmin) * dr );
+
+        if (ir < 0 || ir >= nR) continue;
+
+        ++counts[ir];
+
+        // transform velocity to radial coords
+
+        float f[nf];
+        memcpy(f, fo, nf*sizeof(float));
+        
+        if (nf >= 3)
+        {
+            const float sc = 1.f / r_;
+            
+            f[0] = sc * (r[0] * fo[0] + r[1] * fo[1]);
+            f[1] = sc * (r[1] * fo[1] - r[0] * fo[0]);
+        }
+        
+        for (int j = 0; j < nf; ++j)
+        av[ir * nf + j] += f[j];
+    }
+
+    for (int i = 0; i < nR; ++i)
+    for (int j = 0; j < nf; ++j)
+    av[i * nf + j] /= counts[i] > 0 ? counts[i] : 1;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -29,6 +79,16 @@ int main(int argc, char **argv)
     }
     summary(&d);
 
+    const int nr = 100;
+    const float rmin = 5.f;
+    const float rmax = 10.f;
+    const float com[3] = {32, 16, 8};
+
+    int *counts = new int[nr];
+    float *av = new float[nr * (d.nvars-3)];
+    
+    radial_v(com, rmin, rmax, nr, d.fdata, d.n, d.nvars, /**/ av, counts);
+        
     FILE *f = fopen(argv[1], "w");
     
     fclose(f);
