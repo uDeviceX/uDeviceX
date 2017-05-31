3c31424cc3292aa87aab3adc7241b47751957b6e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-05-31 14:13:12 +0200

    solid has a mass

--- src/common.h
+++ src/common.h
@@ -72,11 +72,12 @@ struct Particle {
 
 struct Solid {
     float Iinv[6],            /* moment of inertia            6        */
-        com[3],               /* [c]enter [o]f [m]ass         9        */
-        v[3], om[3],          /* linear and angular velocity  12 15    */
-        e0[3], e1[3], e2[3],  /* local referential            18 21 24 */
-        fo[3], to[3],         /* force, torque                27 30    */
-        id;                   /* id of the solid              31       */
+        mass,                 /* mass of the solid            7        */
+        com[3],               /* [c]enter [o]f [m]ass         10       */
+        v[3], om[3],          /* linear and angular velocity  13 16    */
+        e0[3], e1[3], e2[3],  /* local referential            19 22 25 */
+        fo[3], to[3],         /* force, torque                28 31    */
+        id;                   /* id of the solid              32       */
 
     static bool initialized;
     static MPI_Datatype mytype;
@@ -84,7 +85,7 @@ struct Solid {
     {
         if (!initialized)
         {
-            MC (MPI_Type_contiguous(31, MPI_FLOAT, &mytype));
+            MC (MPI_Type_contiguous(32, MPI_FLOAT, &mytype));
             MC (MPI_Type_commit(&mytype));
             initialized = true;
         }
