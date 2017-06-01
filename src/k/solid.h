28414524117077747f0425d69fcd7ea70da4a9db
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-01 16:42:20 +0200

    more static functions

--- src/k/solid.h
+++ src/k/solid.h
@@ -6,20 +6,20 @@ namespace k_solid
 
 #define _HD_ __host__ __device__
     
-    _HD_ float dot(const float *v, const float *u) {
+    static _HD_ float dot(const float *v, const float *u) {
         return v[X]*u[X] + v[Y]*u[Y] + v[Z]*u[Z];
     }
 
-    _HD_ void reject(/**/ float *v, float *u) {
+    static _HD_ void reject(/**/ float *v, float *u) {
         const float d = dot(v, u);
         v[X] -= d*u[X]; v[Y] -= d*u[Y]; v[Z] -= d*u[Z];
     }
 
-    _HD_ float norm(const float *v) {
+    static _HD_ float norm(const float *v) {
         return sqrt(v[X]*v[X] + v[Y]*v[Y] + v[Z]*v[Z]);
     }
 
-    _HD_ void normalize(/**/ float *v) {
+    static _HD_ void normalize(/**/ float *v) {
         const float s = 1.f / norm(v);
         v[X] *= s; v[Y] *= s; v[Z] *= s;
     }
@@ -61,7 +61,7 @@ namespace k_solid
         }
     }
     
-    __device__ void warpReduceSumf3(float *x)
+    static __device__ void warpReduceSumf3(float *x)
     {
         for (int offset = warpSize>>1; offset > 0; offset >>= 1)
         {
