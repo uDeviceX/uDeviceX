1c0107ca0bc40af4db77eb0049da3bf07294c7a1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-07 16:43:02 +0200

    fix test

--- src/test.rbc.sh
+++ src/test.rbc.sh
@@ -3,7 +3,7 @@
 #### RBC in a periodic box
 # sTEST: diag.t2
 # export PATH=../tools:$PATH
-# rm -rf diag.txt h5 o ply
+# rm -rf diag.txt h5 bop ply
 # x=0.75 y=8 z=12; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
 # :
 # argp .conf.couette.h -rbcs -tend=0.5 -part_freq=300 > .conf.h
@@ -15,7 +15,7 @@
 #### RBC initialy rotated
 # nTEST: rotated.t1
 # export PATH=../tools:$PATH
-# rm -rf diag.txt h5 o ply
+# rm -rf diag.txt h5 bop ply
 # x=0.75 y=8 z=12   c=0.5 s=0.866 # sin() and cos()
 # echo     1  0   0 $x              \
 #          0 $c -$s $y              \
@@ -24,7 +24,6 @@
 # :
 # argp .conf.couette.h -rbcs -tend=0.5 -part_freq=300 > .conf.h
 # :
-# { make clean && make ranks && make -j && make -C ../tools; } > /dev/null
-# udirs sr/p
-# ./u
+# { make clean && make -j && make -C ../tools; } > /dev/null
+# ./udx
 # ply2punto ply/rbcs-00003.ply | uscale 100 > ply.out.txt
