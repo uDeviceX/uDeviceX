fa5ed9773305fdc9c6d820f2ae23b9f512f52a2c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-07 17:27:41 +0200

    test around

--- src/test.rbc.sh
+++ src/test.rbc.sh
@@ -51,11 +51,9 @@
 # x=0.75 y=8 z=9; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
 # :
 # argp .conf.around.h \
-#    -acyl            \
 #    -rbcs -tend=3.0 -part_freq=5000 -walls -wall_creation=1000 \
 #    -field_dumps -part_dumps -field_freq=5000 -pushflow > .conf.h
 # :
-# { make clean && make ranks && make -j && make -C ../tools; } > /dev/null
-# udirs sr/p
-# ./u
+# { make clean && make -j && make -C ../tools; } > /dev/null
+# ./udx
 # ply2punto ply/rbcs-00001.ply | uscale 100 > ply.out.txt
