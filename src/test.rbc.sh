2713a5d88446416e1c8cb142f6268b916fe4e155
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-07 18:31:47 +0200

    adding rbc test

--- src/test.rbc.sh
+++ src/test.rbc.sh
@@ -44,7 +44,7 @@
 # ply2punto ply/rbcs-00003.ply | uscale 100 > ply.out.txt
 
 #### one RBC around cylinder
-# nTEST: flow.around.t1
+# sTEST: flow.around.t1
 # export PATH=../tools:$PATH
 # rm -rf diag.txt h5 o ply
 # cp sdf/cyl1/cyl.dat sdf.dat
@@ -57,3 +57,22 @@
 # { make clean && make -j && make -C ../tools; } > /dev/null
 # ./udx
 # ply2punto ply/rbcs-00001.ply | uscale 100 > ply.out.txt
+
+#### two RBCs around cylinder
+# nTEST: flow.around.t2
+# export PATH=../tools:$PATH
+# rm -rf diag.txt h5 o ply
+# x=0.75  y=3 z=9; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 >  rbcs-ic.txt
+# x=0.75 y=13 z=9; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 >> rbcs-ic.txt
+# cp sdf/cyl1/cyl.dat sdf.dat
+# :
+# argp .conf.around.h  -rbcs -tend=3.0 -part_freq=5000 \
+#        -acyl                             \
+#        -walls -wall_creation=1000 \
+#        -field_dumps -part_dumps  \
+#        -field_freq=5000 -pushflow > .conf.h
+# :
+# { make clean && make ranks && make -j && make -C ../tools; } > /dev/null
+# udirs sr/p
+# ./u
+# ply2punto ply/rbcs-00001.ply | uscale 10 > ply.out.txt
