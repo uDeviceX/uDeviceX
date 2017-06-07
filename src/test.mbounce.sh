0f46c82ce27abaf7ab52282edd76fa08db3eb000
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-07 09:12:28 +0200

    rbc/solid in test.mbounce

--- src/test.mbounce.sh
+++ src/test.mbounce.sh
@@ -6,7 +6,7 @@
 # cp data/sphere.ply mesh_solid.ply
 # :
 # argp .conf.double.poiseuille.h      \
-#   -rbcs  -sbounce_back              \
+#   -solids -sbounce_back             \
 #   -tend=0.5 -steps_per_dump=100     \
 #   -pushtheflow -doublepoiseuille    \
 #   -hdf5field_dumps -part_dumps      \
