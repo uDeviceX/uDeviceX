6d54bd8d8d8fa48e943dc75a4bdb51ed5033787b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-07 14:36:31 +0200

    adding common.macro

---
+++ src/common.macro.h
@@ -0,0 +1,7 @@
+
+
+enum {  /* used in sorting of bulk particle when wall is created */
+  W_BULK,  /* remains in bulk */
+  W_WALL,  /* becomes wall particle */
+  W_DEEP   /* deep inside the wall */
+};
