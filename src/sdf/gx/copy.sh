1da291d2ff1c403f53c0b723e58effc8b9669da7
Sergey Litvinov <slitvinov@gmail.com>
2017-06-07 22:51:17 +0200

    Copy rotated file

--- src/sdf/gx/copy.sh
+++ src/sdf/gx/copy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 d=panda:/scratch/googleX
+
 r=vessels_small_mirrored # remote and local name
 l=small                  #
-
 rsync -avz $d/$r.sdf $l.dat # (sic)
 rsync -avz $d/$r.h5  $l.h5
 rsync -avz $d/$r.xmf $l.xmf
 
-sdf.shufle yxz small.dat small.rot.dat # shufle x and y
+r=small.rot
+l=small.rot
+rsync -avz $d/$r.dat $l.dat
