dde3c0609e71d621eed6a8758e8b967d58ac031c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-07 17:42:16 +0200

    adapt + ref

--- src/test.contact.sh
+++ src/test.contact.sh
@@ -2,7 +2,7 @@
 #### contact force: two RBCs in double Poiseuille
 # nTEST: contact.t1
 # export PATH=../tools:$PATH
-# rm -rf diag.txt h5 o ply
+# rm -rf diag.txt h5 bop ply
 #  x=5 y=17 z=8; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 >  rbcs-ic.txt
 # x=11 y=15 z=8; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 >> rbcs-ic.txt
 # :
@@ -11,7 +11,6 @@
 #        -pushflow -doublepoiseuille \
 #        -contactforces > .conf.h
 # :
-# { make clean && make ranks && make -j && make -C ../tools; } > /dev/null
-# udirs sr/p
-# ./u
+# { make clean &&  make -j && make -C ../tools; } > /dev/null
+# ./udx
 # ply2punto ply/rbcs-00002.ply | uscale 100 > ply.out.txt
