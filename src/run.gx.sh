7753d559067cc2dd5d8ec137519adf330aaf14cf
Sergey Litvinov <slitvinov@gmail.com>
2017-06-09 00:48:25 +0200

    copy rbc.off from cells

--- src/run.gx.sh
+++ src/run.gx.sh
@@ -7,9 +7,7 @@ setup() {
 }
 
 pre() {
-    XS=40
-    YS=52
-    ZS=20
+    XS=40 YS=52 ZS=20
     df=1.0
 
     D="-XS=$XS -YS=$YS -ZS=$ZS"
@@ -22,6 +20,8 @@ pre() {
     rm -rf diag.txt h5 bop ply solid-ply solid_diag*txt
     cp sdf/gx/small.rot.dat sdf.dat
     cp data/sphere_R1.ply mesh_solid.ply
+    cp data/sphere_R1.ply mesh_solid.ply
+    cp cells/rbc.496.off  rbc.off
 
     argp .conf.gx.base.h $D                  \
          -rbcs -solids -contactforces        \
@@ -29,7 +29,6 @@ pre() {
          -walls -wall_creation=1000          \
          -pushflow -driving_force=$df        \
          -field_dumps -part_dumps -field_freq=1000 > .conf.h
-    
 }
 
 compile() {
