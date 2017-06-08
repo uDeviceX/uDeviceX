143ddc79238f8c98fc9eb4588a507759c9a96b61
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-08 09:43:12 +0200

    adding tests for both rbc and solid

---
+++ src/test.solid.rbc.sh
@@ -0,0 +1,35 @@
+
+#### contact force: RBC and sphere in double Poiseuille
+# cTEST: contact.mix.t1
+# export PATH=../tools:$PATH
+# rm -rf diag.txt h5 bop ply
+#  x=5 y=17 z=8; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
+# x=11 y=15 z=8; echo $x $y $z                              > ic_solid.txt
+# cp data/sphere.ply mesh_solid.ply
+# :
+# argp .conf.double.poiseuille.h -rbcs -tend=2.0 -part_freq=1500 \
+#        -solids -sbounce_back -wall_creation=1                  \
+#        -part_dumps -field_freq=1500                            \
+#        -pushflow -doublepoiseuille                             \
+#        -contactforces > .conf.h
+# :
+# { make clean &&  make -j && make -C ../tools; } > /dev/null
+# ./udx
+# ply2punto ply/rbcs-00002.ply | uscale 100 > ply.out.txt
+
+#### no contact force: RBC and sphere in double Poiseuille
+# sTEST: no.contact.mix.t1
+# export PATH=../tools:$PATH
+# rm -rf diag.txt h5 bop ply
+# x=4  y=17 z=8; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 >  rbcs-ic.txt
+# x=12 y=15 z=8; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 >> rbcs-ic.txt
+# :
+# argp .conf.double.poiseuille.h -rbcs -tend=2.0 -part_freq=1500 \
+#        -part_dumps -field_freq=1500 \
+#        -pushflow -doublepoiseuille \
+#                      > .conf.h
+# :
+# { make clean && make -j && make -C ../tools; } > /dev/null
+# ./udx
+# ply2punto ply/rbcs-00002.ply | uscale 100 > ply.out.txt
+#
