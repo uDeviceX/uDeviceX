b7255e75c67e5783e519afd86cd2f5e6128bf0ae
Athena Economides <eceva@ethz.ch>
2017-06-08 21:24:33 +0200

    run solids+rbcs

--- run/googleX/daint/run_daint.sh
+++ run/googleX/daint/run_daint.sh
@@ -2,28 +2,49 @@
 
 (
 cd ${RUNDIR}
+pwd
+
+#make -C ${SRCDIR}/tools/rbc install
+#make -C ${SRCDIR}/tools install
+#make -C ${SRCDIR}/post/build_smesh install
 
 # number of ranks
-if [[ -n "${NX}" ]]; then NX=${NX}; else NX=1; fi
-if [[ -n "${NY}" ]]; then NY=${NY}; else NY=1; fi
-if [[ -n "${NZ}" ]]; then NZ=${NZ}; else NZ=1; fi
+NX=8
+NY=8
+NZ=1
 NN=$((${NX}*${NY}*${NZ}))
 
+# domain sizes
+XS=`grep -w XS conf.h | awk '{print $3}'`
+YS=`grep -w YS conf.h | awk '{print $3}'`
+ZS=`grep -w ZS conf.h | awk '{print $3}'`
+LX=$((${NX}*${XS}))
+LY=$((${NY}*${YS}))
+LZ=$((${NZ}*${ZS}))
+
 # copy walls
-cp ${SRCDIR}/src/sdf/gx/vessels_mirrored.dat sdf.dat
+#cp ${SRCDIR}/src/sdf/gx/vessels_mirrored.dat sdf.dat
 #cp ${SRCDIR}/src/sdf/gx/vessels_small_mirrored.dat sdf.dat
+cp ${SRCDIR}/src/sdf/gx/small.rot.dat sdf.dat
 
-# solid mesh + position
-# /users/eceva/uDeviceX/uDX_gx/src/data/sphere.ply
-
-# doesnt run w/o this
+# rbc mesh
 cp ${SRCDIR}/src/rbc.off .
+
+# solid mesh
+cp ${SRCDIR}/data/sphere_R1.ply mesh_solid.ply
+
+# rbc+solid placement
+radius=3
+fraction=0.5
+sc=1
+ang=0
+plcmt.ro $LX $LY $LZ $radius $fraction $sc $ang ic_solid.txt rbcs-ic.txt
 
 # create sbatch script
  echo "#!/bin/bash -l
 #
-#SBATCH --job-name=gx
-#SBATCH --time=00:30:00
+#SBATCH --job-name=vessels
+#SBATCH --time=00:10:00
 #SBATCH --ntasks=${NN}
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=slurm_out.%j.o
