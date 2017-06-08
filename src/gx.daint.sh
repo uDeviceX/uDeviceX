5114f53f65ec6cf99a6dde3bd711288134393744
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-08 17:53:27 +0200

    add run on daint

---
+++ src/gx.daint.sh
@@ -0,0 +1,22 @@
+ini() {
+    module load cray-hdf5-parallel cray-mpich cudatoolkit
+}
+
+run () {
+    n=gx
+    
+    cat <<-EOF > runme.sh
+	#!/bin/bash -l
+	#SBATCH --job-name=$n
+	#SBATCH --time=00:10:00
+	#SBATCH --nodes=1
+	#SBATCH --ntasks-per-node=1
+	#SBATCH --output=output.txt
+	#SBATCH --error=error.txt
+	#SBATCH --constraint=gpu
+	:
+    module load cray-hdf5-parallel cray-mpich cudatoolkit
+	srun --export ALL ./udx
+EOF
+    sbatch runme.sh
+}
