802a18ffa02a08e2a4fa3151ef12199fafa9e0f6
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-08 17:48:22 +0200

    more generic run.gx

--- src/run.gx.sh
+++ src/run.gx.sh
@@ -1,30 +1,44 @@
 #!/usr/bin/sh
 
-export PATH=../tools:$PATH
+setup() {
+    make -C ../tools/rbc install    
+}
 
-XS=40
-YS=52
-ZS=20
-df=1.0
+pre() {
+    XS=40
+    YS=52
+    ZS=20
+    df=1.0
 
-D="-XS=$XS -YS=$YS -ZS=$ZS"
+    D="-XS=$XS -YS=$YS -ZS=$ZS"
 
-radius=3
-fraction=0.5
-sc=1 ang=0
-plcmt.ro $XS $YS $ZS $radius $fraction $sc $ang ic_solid.txt rbcs-ic.txt
+    radius=3
+    fraction=0.5
+    sc=1 ang=0
+    plcmt.ro $XS $YS $ZS $radius $fraction $sc $ang ic_solid.txt rbcs-ic.txt
 
-rm -rf diag.txt h5 bop ply solid-ply
-cp sdf/gx/small.rot.dat sdf.dat
-cp data/sphere_R1.ply mesh_solid.ply
+    rm -rf diag.txt h5 bop ply solid-ply
+    cp sdf/gx/small.rot.dat sdf.dat
+    cp data/sphere_R1.ply mesh_solid.ply
 
-argp .conf.gx.base.h $D                \
-   -rbcs -solids                       \
-   -tend=3000.0 -part_freq=1000        \
-   -walls -wall_creation=1000          \
-   -pushflow -driving_force=$df        \
-   -field_dumps -part_dumps -field_freq=1000 > .conf.h
+    argp .conf.gx.base.h $D                \
+         -rbcs -solids                       \
+         -tend=3000.0 -part_freq=1000        \
+         -walls -wall_creation=1000          \
+         -pushflow -driving_force=$df        \
+         -field_dumps -part_dumps -field_freq=1000 > .conf.h
+    
+}
 
-{ make clean && make -j ; } > /dev/null
-./udx
+compile() {
+    { make clean && make -j ; } > /dev/null
+}
 
+#. gx.panda.sh
+. gx.daint.sh
+
+setup
+pre
+ini
+compile
+run
