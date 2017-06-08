ed493352e58675274b447321a946b64352c64158
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-08 17:35:01 +0200

    playing with params

--- src/run.gx.sh
+++ src/run.gx.sh
@@ -5,7 +5,7 @@ export PATH=../tools:$PATH
 XS=40
 YS=52
 ZS=20
-df=0.5
+df=1.0
 
 D="-XS=$XS -YS=$YS -ZS=$ZS"
 
@@ -14,16 +14,16 @@ fraction=0.5
 sc=1 ang=0
 plcmt.ro $XS $YS $ZS $radius $fraction $sc $ang ic_solid.txt rbcs-ic.txt
 
-rm -rf diag.txt h5 bop ply
+rm -rf diag.txt h5 bop ply solid-ply
 cp sdf/gx/small.rot.dat sdf.dat
 cp data/sphere_R1.ply mesh_solid.ply
 
 argp .conf.gx.base.h $D                \
    -rbcs -solids                       \
-   -tend=3000.0 -part_freq=100         \
-   -walls -wall_creation=1             \
+   -tend=3000.0 -part_freq=1000        \
+   -walls -wall_creation=1000          \
    -pushflow -driving_force=$df        \
-   -field_dumps -part_dumps -field_freq=5000 > .conf.h
+   -field_dumps -part_dumps -field_freq=1000 > .conf.h
 
 { make clean && make -j ; } > /dev/null
 ./udx
