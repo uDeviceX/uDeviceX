9ed5cee494fa4234b5b2b1a133ef4c322fc597c1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-08 10:35:26 +0200

    indent

--- src/solid.h
+++ src/solid.h
@@ -1,20 +1,20 @@
 namespace solid
 {
-    void init(const Particle *pp, int n, float pmass, const float *com, const Mesh mesh, /**/ float *rr0, Solid *s);
+void init(const Particle *pp, int n, float pmass, const float *com, const Mesh mesh, /**/ float *rr0, Solid *s);
     
-    void reinit_ft_hst(const int nsolid, /**/ Solid *ss);
-    void reinit_ft_dev(const int nsolid, /**/ Solid *ss);
+void reinit_ft_hst(const int nsolid, /**/ Solid *ss);
+void reinit_ft_dev(const int nsolid, /**/ Solid *ss);
 
-    void update_hst(const Force *ff, const float *rr0, int n, int nsolid, /**/ Particle *pp, Solid *shst);    
-    void update_dev(const Force *ff, const float *rr0, int n, int nsolid, /**/ Particle *pp, Solid *sdev);
+void update_hst(const Force *ff, const float *rr0, int n, int nsolid, /**/ Particle *pp, Solid *shst);    
+void update_dev(const Force *ff, const float *rr0, int n, int nsolid, /**/ Particle *pp, Solid *sdev);
 
-    void generate_hst(const Solid *ss_hst, const int ns, const float *rr0, const int nps, /**/ Particle *pp);
-    void generate_dev(const Solid *ss_dev, const int ns, const float *rr0, const int nps, /**/ Particle *pp);
+void generate_hst(const Solid *ss_hst, const int ns, const float *rr0, const int nps, /**/ Particle *pp);
+void generate_dev(const Solid *ss_dev, const int ns, const float *rr0, const int nps, /**/ Particle *pp);
 
-    void mesh2pp_hst(const Solid *ss_hst, const int ns, const Mesh m, /**/ Particle *pp);
+void mesh2pp_hst(const Solid *ss_hst, const int ns, const Mesh m, /**/ Particle *pp);
 
-    void update_mesh_hst(const Solid *ss_hst, const int ns, const Mesh m, /**/ Particle *pp);
-    void update_mesh_dev(const Solid *ss_dev, const int ns, const Mesh m, /**/ Particle *pp);
+void update_mesh_hst(const Solid *ss_hst, const int ns, const Mesh m, /**/ Particle *pp);
+void update_mesh_dev(const Solid *ss_dev, const int ns, const Mesh m, /**/ Particle *pp);
 
-    void dump(const int it, const Solid *ss, const Solid *ssbb, int nsolid, const int *mcoords);
+void dump(const int it, const Solid *ss, const Solid *ssbb, int nsolid, const int *mcoords);
 }
