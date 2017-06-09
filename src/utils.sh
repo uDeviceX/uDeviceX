78c0743bd16657b5ddcf9ba27b2fa7e0e43ddf37
Sergey Litvinov <slitvinov@gmail.com>
2017-06-09 18:24:20 +0200

    Update utils.sh

--- src/utils.sh
+++ src/utils.sh
@@ -1,10 +1,27 @@
-dhost () ( # dispatch hostname
-    h=`hostname`
+inc ()  { #
+          # inc HOST.file
+          #   source `daint.file' on daint and `panda.file' on panda/falcon
+    . `inc0 "$1"`
+}
+
+dhost () { # dispatch hostname
     awk '
        BEGIN {
 	   h = ARGV[1]
 	   if (h ~ /^daint/)  print "daint"
 	   else               print "panda"
        }
-    ' "$h"
-)
+    ' `hostname`
+}
+
+
+inc0 () {
+    awk '
+       BEGIN {
+	   h = ARGV[1]; f = ARGV[2]
+	   gsub(/HOST/, h, f)
+	   print f
+       }
+    ' `dhost` "$1"
+}
+
