a84839ad5ef52268d2815e403634b5decc0e27fd
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-09 11:37:29 +0200

    toward multinodes on daint

--- src/run.gx.sh
+++ src/run.gx.sh
@@ -7,7 +7,13 @@ setup() {
 }
 
 pre() {
+    NX=1  NY=1  NZ=1
+    #NX=2  NY=2  NZ=2
     XS=40 YS=52 ZS=20
+    LX=$((${NX}*${XS}))
+    LY=$((${NY}*${YS}))
+    LZ=$((${NZ}*${ZS}))
+
     df=1.0
 
     D="-XS=$XS -YS=$YS -ZS=$ZS"
@@ -15,11 +21,10 @@ pre() {
     radius=3
     fraction=0.5
     sc=1 ang=0
-    plcmt.ro $XS $YS $ZS $radius $fraction $sc $ang ic_solid.txt rbcs-ic.txt
+    plcmt.ro $LX $LY $LZ $radius $fraction $sc $ang ic_solid.txt rbcs-ic.txt
 
     rm -rf diag.txt h5 bop ply solid-ply solid_diag*txt
     cp sdf/gx/small.rot.dat sdf.dat
-    cp data/sphere_R1.ply mesh_solid.ply
     cp data/sphere_R1.ply mesh_solid.ply
     cp cells/rbc.496.off  rbc.off
 
