c850f7da9a6b6b22350fe1148d917e01d3c573de
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-10 18:24:37 +0200

    remove unnoying tabs

--- src/gx.daint.sh
+++ src/gx.daint.sh
@@ -6,18 +6,18 @@ run () {
     n=gx-packed
     
     cat <<-EOF > runme.sh
-	#!/bin/bash -l
-	#SBATCH --partition=low
-	#SBATCH --job-name=$n
-	#SBATCH --time=02:00:00
-	#SBATCH --ntasks=${NN}
-	#SBATCH --ntasks-per-node=1
-	#SBATCH --output=output.txt
-	#SBATCH --error=error.txt
-	#SBATCH --constraint=gpu
-	:
-    module load cray-hdf5-parallel cudatoolkit daint-gpu GSL
-	srun --export ALL -u -n ${NN} ./udx ${NX} ${NY} ${NZ}
+#!/bin/bash -l
+#SBATCH --partition=low
+#SBATCH --job-name=$n
+#SBATCH --time=02:00:00
+#SBATCH --ntasks=${NN}
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#SBATCH --constraint=gpu
+:
+module load cray-hdf5-parallel cudatoolkit daint-gpu GSL
+srun --export ALL -u -n ${NN} ./udx ${NX} ${NY} ${NZ}
 EOF
     sbatch runme.sh
 }
