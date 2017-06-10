3581838932b46db147023d24c94b365f88116b07
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-10 19:24:39 +0200

    ntasks/n

--- src/gx.daint.sh
+++ src/gx.daint.sh
@@ -10,7 +10,7 @@ run () {
 #SBATCH --partition=low
 #SBATCH --job-name=$n
 #SBATCH --time=02:00:00
-#SBATCH --ntasks=${NN}
+#SBATCH --nnode=${NN}
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=output.txt
 #SBATCH --error=error.txt
