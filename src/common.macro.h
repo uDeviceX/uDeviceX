d4489f4955f4152072891b8b66037bbfc5a7092b
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-11 19:34:25 +0200

    minor

--- src/common.macro.h
+++ src/common.macro.h
@@ -5,7 +5,9 @@ W_WALL,  /* becomes wall particle */
 W_DEEP   /* deep inside the wall */
 };
 
-#define allsync() do { CC(cudaDeviceSynchronize()); MC(MPI_Barrier(m::cart)); \
-        if (m::rank == 0) fprintf(stderr, "%s : l %d\n", __FILE__, __LINE__); \
+#define allsync() do {                                                  \
+        CC(cudaDeviceSynchronize()); MC(MPI_Barrier(m::cart));          \
+        if (m::rank == 0)                                               \
+        fprintf(stderr, "%s : %d\n", __FILE__, __LINE__);               \
     } while (0)
 //#define allsync() do { CC(cudaDeviceSynchronize()); } while (0)
