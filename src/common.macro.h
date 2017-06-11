1ae86fcaaa5b42b227b60fea8a2210f37a212074
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-11 21:13:23 +0200

    no sync

--- src/common.macro.h
+++ src/common.macro.h
@@ -4,10 +4,11 @@ W_BULK,  /* remains in bulk */
 W_WALL,  /* becomes wall particle */
 W_DEEP   /* deep inside the wall */
 };
-
+/*
 #define allsync() do {                                                  \
         CC(cudaDeviceSynchronize()); MC(MPI_Barrier(m::cart));          \
         if (m::rank == 0)                                               \
         fprintf(stderr, "%s : %d\n", __FILE__, __LINE__);               \
     } while (0)
-//#define allsync() do { CC(cudaDeviceSynchronize()); } while (0)
+    #define allsync() do { CC(cudaDeviceSynchronize()); } while (0)*/
+#define allsync() do {  } while (0)
