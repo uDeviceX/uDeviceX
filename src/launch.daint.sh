988aa09cb83915519f924cbc54b53195dd24d489
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-11 19:29:18 +0200

    use c.sh

--- src/launch.daint.sh
+++ src/launch.daint.sh
@@ -7,7 +7,5 @@ fi
 
 # need ri (git@gitlab.ethz.ch:mavt-cse/ri.git)
 
-RUN=run.gx.disk.sh
-
 r $1 ^ cd sdf/gx/ '&&' sh copy.sh
-r $1 ^ cp .cache.Makefile.amlucas.daint .cache.Makefile '&&' sh run.gx.disk.sh
+r $1 ^ cp .cache.Makefile.amlucas.daint .cache.Makefile '&&' sh c.sh
