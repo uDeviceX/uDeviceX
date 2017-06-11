28bc23c233af09a1314e11a7104e24ab205256db
amlucas <lucas.amoudruz@wanadoo.fr>
2017-06-11 11:30:37 +0200

    assert r::n

--- src/sim.impl.h
+++ src/sim.impl.h
@@ -520,6 +520,7 @@ void dumps_diags(int it) {
 
 void run0(float driving_force0, bool wall_created, int it) {
     safety::bound(o::pp, o::n);
+    assert(r::n <= MAX_PART_NUM);
     distr_solvent();
     if (solids0) distr_solid();
     if (rbcs)    distr_rbc();
