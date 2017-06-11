3389e8ae66f45e2a38bb1eeffb720e6c2ec6de38
Sergey Litvinov <slitvinov@gmail.com>
2017-06-11 12:35:42 +0200

    Cannot get a crash!

--- src/c.sh
+++ src/c.sh
@@ -8,29 +8,22 @@
 inc ./gx.HOST.sh
 
 pre() {
+    clean
     nv=498
+    sdf=/tmp/googlex/8/150.sdf
+    rbc=cells/sph.$nv.off
+    sld=data/cylinder.ply
+    copy $sdf $rbc $sld
+
     NX=2  NY=2  NZ=1
     XS=40 YS=52 ZS=20
-    NN=$((${NX}*${NY}*${NZ}))
-    LX=$((${NX}*${XS}))
-    LY=$((${NY}*${YS}))
-    LZ=$((${NZ}*${ZS}))
+    geom # set NN, LX, LY, LZ, Domain
 
-    df=1.0
-    D="-XS=$XS -YS=$YS -ZS=$ZS"
-    
-    fraction=0.2 radius=2.3 sc=0.2 ang=0
+    df=10.0
+    fraction=0.2 radius=2.3 sc=0.2 ang=rnd
     plcmt.ro $LX $LY $LZ $radius $fraction $sc $ang ic_solid.txt rbcs-ic.txt
 
-    rm -rf diag.txt h5 bop ply solid-ply solid_diag*txt
-    cp sdf/gx/small.rot.dat sdf.dat
-    #cp ~/geoms/128.dat sdf.dat
-    cp data/cylinder.ply mesh_solid.ply
-    cp cells/sph.$nv.off  rbc.off
-
-    #ply.sxyz xs ys zs in.ply > out.ply
-
-    argp .conf.gx.base.h $D                   \
+    argp .conf.gx.base.h $Domain              \
 	 RBCnv=$nv                            \
 	 rbcs solids contactforces            \
 	 tend=3000.0 part_freq=100            \
--- src/gx.generic.sh
+++ src/gx.generic.sh
@@ -8,3 +8,38 @@ compile() {
     { make clean && make -j ; } > /dev/null
 }
 
+clean () {
+    rm -rf diag.txt h5 bop ply solid-ply solid_diag*txt
+}
+
+cp0() {
+    local a=$1 b=$2
+    if test ! -r "$a"; then err 'cannot read %s' $a; exit; fi
+}
+
+err () {
+    printf '%s' 'gx.gneric: '      | cat >&2
+    printf "$@"                    | cat >&2
+    printf '\n'                    | cat >&2
+    exit 1
+}
+
+
+copy() {
+    if test $# = 0; then return; fi
+    cp0   $1  sdf.dat               ; shift
+
+    if test $# = 0; then return; fi
+    cp0   $1  rbc.off               ; shift
+
+    if test $# = 0; then return; fi
+    cp0   $1  mesh_solid.ply        ; shift
+}
+
+geom () {
+    NN=$((${NX}*${NY}*${NZ}))
+    LX=$((${NX}*${XS}))
+    LY=$((${NY}*${YS}))
+    LZ=$((${NZ}*${ZS}))
+    Domain="-XS=$XS -YS=$YS -ZS=$ZS"
+}
