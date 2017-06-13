b02fada735132e708c47c6142445a4a944fac107
Sergey Litvinov <slitvinov@gmail.com>
2017-06-13 13:25:28 +0200

    minor

--- src/common.h
+++ src/common.h
@@ -60,7 +60,7 @@ inline void cudaAssert(cudaError_t code, const char *file, int line) {
 
 #define MSG00(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
 #define MSG(fmt, ...) MSG00("%03d: ", m::rank), MSG00(fmt, ##__VA_ARGS__), MSG00("\n")
-#define MSG0(fmt, ...) do { if (m::rank == 0) MSG(fmt, ##__VA_ARGS__); } while (0);
+#define MSG0(fmt, ...) do { if (m::rank == 0) MSG(fmt, ##__VA_ARGS__); } while (0)
 
 #define ERR(fmt, ...) do { fprintf(stderr, "%03d: ERROR: %s: %d" fmt, m::rank, __FILE__, __LINE__, ##__VA_ARGS__); exit(1); } while(0)
 
