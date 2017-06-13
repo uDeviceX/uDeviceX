974b44c536d5c4509da3bfafd9e6062b3e94b8cb
Sergey Litvinov <slitvinov@gmail.com>
2017-06-13 20:52:51 +0200

    remove unused file

--- src/dpd/cell-lists.h
+++
@@ -1,5 +0,0 @@
-void build_clists_vanilla(float * const device_xyzuvw, int np, const float rc,
-                          const int xcells, const int ycells, const int zcells,
-                          const float xdomainstart, const float ydomainstart, const float zdomainstart,
-                          int * const host_order, int * device_cellsstart, int * device_cellscount,
-                          std::pair<int, int *> * nonemptycells = NULL, const float * const src_device_xyzuvw = NULL);
