655957c26d7c461d93f14865c168a54e7904943f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-13 12:14:06 +0200

    format ic_solid; use ERR

--- src/ic_solid.impl.h
+++ src/ic_solid.impl.h
@@ -4,55 +4,39 @@ enum {X, Y, Z};
 
 //#define DEBUG_MSG
     
-static int read_coms(const char * fname, /**/ float* coms)
-{
+static int read_coms(const char * fname, /**/ float* coms) {
     int nsolids = 0;
-        
-    if (m::rank == 0)
-    {
+    if (m::rank == 0) {
         FILE *f = fopen(fname, "r"); 
 
-        if (f == NULL)
-        {
-            fprintf(stderr, "Could not %s. aborting.\n", fname);
-            exit(1);
-        }
+        if (f == NULL) 
+        ERR("Could not %s. aborting.\n", fname);
     
         float x, y, z;
         int i = 0;
         
-        while (fscanf(f, "%f %f %f\n", &x, &y, &z) == 3)
-        {
+        while (fscanf(f, "%f %f %f\n", &x, &y, &z) == 3) {
             coms[3*i + X] = x;
             coms[3*i + Y] = y;
             coms[3*i + Z] = z;
-
             i++;
             assert(i < MAX_SOLIDS);
         }
         nsolids = i;
     }
-
     MC( MPI_Bcast(&nsolids, 1,     MPI_INT,   0, m::cart) );
     MC( MPI_Bcast(coms, 3*nsolids, MPI_FLOAT, 0, m::cart) );
-
     return nsolids;
 }
 
 /* bbox: minx, maxx, miny, maxy, minz, maxz */
-static int duplicate_PBC(const float *bbox, int n, /**/ float *coms)
-{
+static int duplicate_PBC(const float *bbox, int n, /**/ float *coms) {
     struct f3 {float x[3];};
-
     const int Lg[3] = {XS * m::dims[X], YS * m::dims[Y], ZS * m::dims[Z]};
-        
     int id = n;
-    for (int j = 0; j < n; ++j)
-    {
+    for (int j = 0; j < n; ++j) {
         f3 r0 = {coms[3*j + X], coms[3*j + Y], coms[3*j + Z]};
-            
         std::vector<f3> dupls;
-
         dupls.push_back(r0);
 
         auto duplicate = [&](int d, int sign) {
@@ -62,8 +46,7 @@ static int duplicate_PBC(const float *bbox, int n, /**/ float *coms)
             dupls.insert(dupls.end(), dupls2.begin(), dupls2.end());
         };
             
-        for (int d = 0; d < 3; ++d)
-        {
+        for (int d = 0; d < 3; ++d) {
             if (r0.x[d] + bbox[2*d+0] < 0)
             duplicate(d, +1);
                 
@@ -72,8 +55,7 @@ static int duplicate_PBC(const float *bbox, int n, /**/ float *coms)
         }
 
         // k from 1: do not reinsert the original
-        for (int k = 1; k < (int) dupls.size(); ++k)
-        {
+        for (int k = 1; k < (int) dupls.size(); ++k) {
             for (int d = 0; d < 3; ++d)
             coms[3*id + d] = dupls[k].x[d];
 
@@ -83,8 +65,7 @@ static int duplicate_PBC(const float *bbox, int n, /**/ float *coms)
     return id;
 }
 
-static void make_local(const int n, /**/ float *coms)
-{
+static void make_local(const int n, /**/ float *coms) {
     const int L[3] = {XS, YS, ZS};
     int mi[3];
     for (int c = 0; c < 3; ++c) mi[c] = (m::coords[c] + 0.5) * L[c];
@@ -98,25 +79,19 @@ static void make_local(const int n, /**/ float *coms)
 
 static void count_pp_inside(const Particle *s_pp, const int n, const float *coms, const int ns,
                             const int *tt, const float *vv, const int nt,
-                            /**/ int *tags, int *rcounts)
-{
+                            /**/ int *tags, int *rcounts) {
     for (int j = 0; j < ns; ++j) rcounts[j] = 0;
     
-    for (int ip = 0; ip < n; ++ip)
-    {
+    for (int ip = 0; ip < n; ++ip) {
         Particle p = s_pp[ip]; float *r0 = p.r;
-
         int tag = -1;
-        
-        for (int j = 0; j < ns; ++j)
-        {
+        for (int j = 0; j < ns; ++j) {
             const float *com = coms + 3*j;
             const float r[3] = {r0[X]-com[X], r0[Y]-com[Y], r0[Z]-com[Z]};
 
             const float r2 = r[X]*r[X] + r[Y]*r[Y] + r[Z]*r[Z];     
 
-            if (r2 < R*R && collision::inside_1p(r, vv, tt, nt))
-            {
+            if (r2 < R*R && collision::inside_1p(r, vv, tt, nt)) {
                 assert(tag == -1);
                 ++rcounts[j];
                 tags[ip] = tag = j;
@@ -129,19 +104,17 @@ static void count_pp_inside(const Particle *s_pp, const int n, const float *coms
 
 #ifdef DEBUG_MSG
     for (int j = 0; j < ns; ++j)
-    printf("[%d] Found %d particles in solid %d\n", m::rank, rcounts[j], j);
+    MSG("Found %d particles in solid %d", rcounts[j], j);
 #endif
 }
 
 #undef R
 
-static void elect(const int *rcounts, const int ns, /**/ int *root, int *idmax)
-{
+static void elect(const int *rcounts, const int ns, /**/ int *root, int *idmax) {
     int localmax[2] = {0, m::rank}, globalmax[2] = {0, m::rank}, idmax_ = 0;
 
     for (int j = 0; j < ns; ++j)
-    if (localmax[0] < rcounts[j])
-    {
+    if (localmax[0] < rcounts[j]) {
         localmax[0] = rcounts[j];
         idmax_ = j;
     }
@@ -152,12 +125,10 @@ static void elect(const int *rcounts, const int ns, /**/ int *root, int *idmax)
     *idmax = idmax_;
 }
 
-static void kill(const int idmax, const int *tags, /**/ int *s_n, Particle *s_pp, int *r_n, Particle *r_pp)
-{
+static void kill(const int idmax, const int *tags, /**/ int *s_n, Particle *s_pp, int *r_n, Particle *r_pp) {
     int scount = 0, rcount = 0;
 
-    for (int ip = 0; ip < *s_n; ++ip)
-    {
+    for (int ip = 0; ip < *s_n; ++ip) {
         Particle p = s_pp[ip];
         const int tag = tags[ip];
 
@@ -171,8 +142,7 @@ static void kill(const int idmax, const int *tags, /**/ int *s_n, Particle *s_pp
     *r_n = rcount;
 }
 
-static void share_parts(const int root, /**/ Particle *pp, int *n)
-{
+static void share_parts(const int root, /**/ Particle *pp, int *n) {
     // set to global coordinates and then convert back to local
     const int L[3] = {XS, YS, ZS};
     int mi[3];
@@ -186,10 +156,7 @@ static void share_parts(const int root, /**/ Particle *pp, int *n)
     std::vector<Particle> recvbuf(MAX_PSOLID_NUM);
 
     if (*n >= MAX_PSOLID_NUM)
-    {
-        fprintf(stderr, "(ic_solid) Number of solid particles too high for the buffer. exiting\n");
-        exit(1);
-    }
+    ERR("(ic_solid) Number of solid particles too high for the buffer. exiting\n");
         
     MC( MPI_Gather(n, 1, MPI_INT, counts.data(), 1, MPI_INT, root, m::cart) );
 
@@ -202,8 +169,7 @@ static void share_parts(const int root, /**/ Particle *pp, int *n)
 
     MC( MPI_Gatherv(pp, *n, Particle::datatype(), recvbuf.data(), counts.data(), displs.data(), Particle::datatype(), root, m::cart) );
 
-    if (m::rank == root)
-    {
+    if (m::rank == root) {
         *n = displs.back() + counts.back();
         for (int i = 0; i < *n; ++i) pp[i] = recvbuf[i];
     }
@@ -236,10 +202,7 @@ void init(const char *fname, const Mesh m, /**/ int *ns, int *nps, float *rr0, S
     make_local(nsolid, coms);
 
     if (nsolid == 0)
-    {
-        fprintf(stderr, "No solid provided. Aborting...\n");
-        exit (1);
-    }
+    ERR("No solid provided. Aborting...\n");
 
     const int npp0 = *s_n;
     int *tags = new int[npp0];
@@ -281,14 +244,12 @@ void init(const char *fname, const Mesh m, /**/ int *ns, int *nps, float *rr0, S
     
     // filter coms to keep only the ones in my domain
     int id = 0;
-    for (int j = 0; j < nsolid; ++j)
-    {
+    for (int j = 0; j < nsolid; ++j) {
         const float *com = coms + 3*j;
 
         if (-XS/2 <= com[X] && com[X] < XS/2 &&
             -YS/2 <= com[Y] && com[Y] < YS/2 &&
-            -ZS/2 <= com[Z] && com[Z] < ZS/2 )
-        {
+            -ZS/2 <= com[Z] && com[Z] < ZS/2 ) {
             ss[id] = model;
             
             for (int d = 0; d < 3; ++d)
