8e564b5b5983d34106eac12d721bd7af3e4dd2aa
Sergey Litvinov <slitvinov@gmail.com>
2017-06-13 08:08:07 +0200

    Defend options with --compiler-options

--- src/sys/common.mk
+++ src/sys/common.mk
@@ -3,6 +3,7 @@ GSL_CXXFLAGS = `gsl-config --cflags`
 GSL_LDFLAGS  = -L${GSL_DIR}/lib -Wl,-rpath -Wl,${GSL_DIR}/lib
 GSL_LIBS     = `gsl-config --libs`
 
-NVCC = ${NVCC_BIN} ${HDF5_CXXFLAGS} ${MPI_CXXFLAGS} ${GSL_CXXFLAGS}
-LDFLAGS =          ${HDF5_LDFLAGS}  ${MPI_LDFLAGS}  ${GSL_LDFLAGS} ${NVCC_LDFALGS}
-LIBS =             ${HDF5_LIBS}     ${MPI_LIBS}     ${GSL_LIBS}    
+NVCC = ${NVCC_BIN} --compiler-options \
+	  '${HDF5_CXXFLAGS} ${MPI_CXXFLAGS} ${GSL_CXXFLAGS}'
+LDFLAGS = ${HDF5_LDFLAGS}  ${MPI_LDFLAGS}  ${GSL_LDFLAGS} ${NVCC_LDFALGS}
+LIBS =    ${HDF5_LIBS}     ${MPI_LIBS}     ${GSL_LIBS}
