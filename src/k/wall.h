0a40c246d38f2f8df6223990cbae36c5dc5f971c
Sergey Litvinov <slitvinov@gmail.com>
2017-06-15 00:47:24 +0200

    more constant

--- src/k/wall.h
+++ src/k/wall.h
@@ -6,7 +6,7 @@ __device__ int minmax(int lo, int hi, int a) { return min(hi, max(lo, a)); }
 
 __global__ void interactions_3tpp(const float2 *const pp, const int np,
                                   const int w_n, float *const acc,
-                                  const float seed, const int type, const int *start) {
+                                  const float seed, const int type, const int *const start) {
     int gid = threadIdx.x + blockDim.x * blockIdx.x;
     int pid = gid / 3;
     int zplane = gid % 3;
