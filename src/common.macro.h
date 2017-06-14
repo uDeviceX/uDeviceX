6291d7359406732416d1d610001742ff77586e93
Sergey Litvinov <slitvinov@gmail.com>
2017-06-14 23:13:26 +0200

    Add XE.. from master

--- src/common.macro.h
+++ src/common.macro.h
@@ -1,3 +1,10 @@
+/* wired constants from wall.impl */
+enum {
+  XE = XS + 2*XWM, YE = YS + 2*YWM, ZE = ZS + 2*ZWM,
+                            XTE  = 16*16,
+  _YTE = ceiln(YE*XTE, XE), YTE  = 16*ceiln(_YTE, 16),
+  _ZTE = ceiln(ZE*XTE, XE), ZTE  = 16*ceiln(_ZTE, 16)
+};
 
 enum {  /* used in sorting of bulk particle when wall is created */
 W_BULK,  /* remains in bulk */
