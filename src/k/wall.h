67d10ca1fb169ebd22ad7fe691ac20294327b21c
Sergey Litvinov <slitvinov@gmail.com>
2017-06-15 00:42:06 +0200

    No macro

--- src/k/wall.h
+++ src/k/wall.h
@@ -10,7 +10,7 @@ __device__ int minmax(int lo, int hi, int a) {
 
 __global__ void interactions_3tpp(const float2 *const pp, const int np,
                                   const int w_n, float *const acc,
-                                  const float seed, const int type, const int* start) {
+                                  const float seed, const int type, const int *start) {
     int gid = threadIdx.x + blockDim.x * blockIdx.x;
     int pid = gid / 3;
     int zplane = gid % 3;
@@ -48,18 +48,18 @@ __global__ void interactions_3tpp(const float2 *const pp, const int np,
 
         int cid0 = xbase - 1 + XCELLS * (ybase - 1 + YCELLS * (zbase - 1 + zplane));
 
-        spidbase = tex1Dfetch000(start, cid0);
-        int count0 = tex1Dfetch000(start, cid0 + 3) - spidbase;
+        spidbase = start[cid0];
+        int count0 = start[cid0 + 3] - spidbase;
 
         int cid1 = cid0 + XCELLS;
-        deltaspid1 = tex1Dfetch000(start, cid1);
-        int count1 = tex1Dfetch000(start, cid1 + 3) - deltaspid1;
+        deltaspid1 = start[cid1];
+        int count1 = start,[cid1 + 3] - deltaspid1;
 
         int cid2 = cid0 + XCELLS * 2;
-        deltaspid2 = tex1Dfetch000(start, cid2);
+        deltaspid2 = start[cid2];
         int count2 = cid2 + 3 == NCELLS
             ? w_n
-            : tex1Dfetch000(start, cid2 + 3) - deltaspid2;
+            : start[cid2 + 3] - deltaspid2;
 
         scan1 = count0;
         scan2 = count0 + count1;
