eebdf529a615183f99e985cea971a495d31da69a
Sergey Litvinov <slitvinov@gmail.com>
2017-06-15 00:08:09 +0200

    Copy a version of constants from master

--- src/common.macro.h
+++ src/common.macro.h
@@ -1,7 +1,10 @@
 /* wired constants from wall.impl */
 enum {
-  XE = XS + 2*XWM, YE = YS + 2*YWM, ZE = ZS + 2*ZWM,
-                            XTE  = 16*16,/* texture sizes */
+    XSIZE_WALLCELLS = 2 * XWM + XS,
+    YSIZE_WALLCELLS = 2 * YWM + YS,
+    ZSIZE_WALLCELLS = 2 * ZWM + ZS,
+    XE = XS + 2*XWM, YE = YS + 2*YWM, ZE = ZS + 2*ZWM,
+			    XTE  = 16*16,/* texture sizes */
   _YTE = ceiln(YE*XTE, XE), YTE  = 16*ceiln(_YTE, 16),
   _ZTE = ceiln(ZE*XTE, XE), ZTE  = 16*ceiln(_ZTE, 16)
 };
