b84ae6437c86b3dede20c7cb66022e842cbb35fe
Sergey Litvinov <slitvinov@gmail.com>
2017-06-14 23:20:48 +0200

    Remove defs from wall.decl

--- src/wall.decl.h
+++ src/wall.decl.h
@@ -1,20 +1,3 @@
-enum {
-    XSIZE_WALLCELLS = 2 * XWM + XS,
-    YSIZE_WALLCELLS = 2 * YWM + YS,
-    ZSIZE_WALLCELLS = 2 * ZWM + ZS,
-
-    XTE = 256,
-    _YTEXTURESIZE = ((YS + 2 * YWM) * XTE +
-                     XS + 2 * XWM - 1) /
-    (XS + 2 * XWM),
-
-    YTE = 16 * ((_YTEXTURESIZE + 15) / 16),
-    _ZTEXTURESIZE = ((ZS + 2 * ZWM) * XTE +
-                     XS + 2 * XWM - 1) /
-    (XS + 2 * XWM),
-    ZTE = 16 * ((_ZTEXTURESIZE + 15) / 16),
-};
-
 namespace wall {
 Logistic::KISS* trunk;
 
