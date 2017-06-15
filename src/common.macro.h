3e654295640a24f057c813cdc1f8e5cc05062fa2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-15 10:39:51 +0200

    indent

--- src/common.macro.h
+++ src/common.macro.h
@@ -4,20 +4,20 @@ enum {
     YSIZE_WALLCELLS = 2 * YWM + YS,
     ZSIZE_WALLCELLS = 2 * ZWM + ZS,
     XE = XS + 2*XWM, YE = YS + 2*YWM, ZE = ZS + 2*ZWM,
-			    XTE  = 16*16,/* texture sizes */
-  _YTE = ceiln(YE*XTE, XE), YTE  = 16*ceiln(_YTE, 16),
-  _ZTE = ceiln(ZE*XTE, XE), ZTE  = 16*ceiln(_ZTE, 16)
+    XTE  = 16*16,/* texture sizes */
+    _YTE = ceiln(YE*XTE, XE), YTE  = 16*ceiln(_YTE, 16),
+    _ZTE = ceiln(ZE*XTE, XE), ZTE  = 16*ceiln(_ZTE, 16)
 };
 
 enum {  /* used in sorting of bulk particle when wall is created */
-W_BULK,  /* remains in bulk */
-W_WALL,  /* becomes wall particle */
-W_DEEP   /* deep inside the wall */
+    W_BULK,  /* remains in bulk */
+    W_WALL,  /* becomes wall particle */
+    W_DEEP   /* deep inside the wall */
 };
 /*
-#define allsync() do {                                                  \
-        CC(cudaDeviceSynchronize()); MC(MPI_Barrier(m::cart));          \
-        if (m::rank == 0)                                               \
-        fprintf(stderr, "%s : %d\n", __FILE__, __LINE__);               \
-    } while (0)
+  #define allsync() do {                                                  \
+  CC(cudaDeviceSynchronize()); MC(MPI_Barrier(m::cart));          \
+  if (m::rank == 0)                                               \
+  fprintf(stderr, "%s : %d\n", __FILE__, __LINE__);               \
+  } while (0)
 */
