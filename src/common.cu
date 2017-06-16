684728a607d83955aba675998c933923a1d6204a
Sergey Litvinov <slitvinov@gmail.com>
2017-06-16 22:07:01 +0200

    minor

--- src/common.cu
+++ src/common.cu
@@ -10,18 +10,18 @@ MPI_Datatype Particle::mytype;
 bool Solid::initialized = false;
 MPI_Datatype Solid::mytype;
 
-void diagnostics(Particle * particles, int n, int idstep) {
+void diagnostics(Particle *pp, int n, int idstep) {
     double p[] = {0, 0, 0};
     for(int i = 0; i < n; ++i)
     for(int c = 0; c < 3; ++c)
-    p[c] += particles[i].v[c];
+    p[c] += pp[i].v[c];
 
     MC(MPI_Reduce(m::rank == 0 ? MPI_IN_PLACE : &p,
                   m::rank == 0 ? &p : NULL, 3,
                   MPI_DOUBLE, MPI_SUM, 0, m::cart) );
     double ke = 0;
     for(int i = 0; i < n; ++i)
-    ke += pow(particles[i].v[0], 2) + pow(particles[i].v[1], 2) + pow(particles[i].v[2], 2);
+    ke += pow(pp[i].v[0], 2) + pow(pp[i].v[1], 2) + pow(pp[i].v[2], 2);
 
     MC(MPI_Reduce(m::rank == 0 ? MPI_IN_PLACE : &ke,
                   &ke,
