203bb0251289d6f539c96897343b4f5e036fa864
Sergey Litvinov <slitvinov@gmail.com>
2017-06-16 20:52:44 +0200

    compiles

--- src/o/int.h
+++ src/o/int.h
@@ -2,12 +2,12 @@ struct Quants {
   Particle *pp;
   int       n;
   Clist *cells;
-}
+};
 
 struct TicketZip {
   float4  *zip0;
   ushort4 *zip1;
-}
+};
 
 struct TicketDistr {
   MPI_Comm cart;
@@ -16,12 +16,12 @@ struct TicketDistr {
   MPI_Request send_mesg_req[27], recv_mesg_req[27];
   bool first = true;
   /* Odstr odstr; */
-}
+};
 
 struct Work {
   uchar4 *subi_lo, *subi_re;
   uint   *iidx;
   Particle *pp_re;
-  unsigned char *count_zip
+  unsigned char *count_zip;
   Particle *pp0;
-}
+};
