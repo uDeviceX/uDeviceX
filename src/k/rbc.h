ad6db752c485f90a6b42b585176d03f0d6e0144b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-16 11:06:53 +0200

    indent

--- src/k/rbc.h
+++ src/k/rbc.h
@@ -11,12 +11,12 @@ texture<int4, cudaTextureType1D> Tri;
 #define scn(t) ( (t).y )
 
 __device__ void tt2r(float2 t1, float2 t2, /**/ float3 *r) {
-  r->x = fst(t1); r->y = scn(t1); r->z = fst(t2);
+    r->x = fst(t1); r->y = scn(t1); r->z = fst(t2);
 }
 
 __device__ void ttt2ru(float2 t1, float2 t2, float2 t3, /**/ float3 *r, float3 *u) {
-  r->x = fst(t1); r->y = scn(t1); r->z = fst(t2);
-  u->x = scn(t2); u->y = fst(t3); u->z = scn(t3);
+    r->x = fst(t1); r->y = scn(t1); r->z = fst(t2);
+    u->x = scn(t2); u->y = fst(t3); u->z = scn(t3);
 }
 
 __device__ float3 adj_tris(float2 t0a, float2 t0b, float *av) {
@@ -40,20 +40,20 @@ __device__ float3 adj_tris(float2 t0a, float2 t0b, float *av) {
     if (i3 == -1 && valid) i3 = tex1Dfetch(Adj0, 0 + degreemax * lid);
 
     if (valid) {
-	t0c = tex1Dfetch(Vert,         pid * 3 + 2);
-	t1a = tex1Dfetch(Vert, offset + i2 * 3 + 0);
-	t1b = tex1Dfetch(Vert, offset + i2 * 3 + 1);
-	t1c = tex1Dfetch(Vert, offset + i2 * 3 + 2);
-	t2a = tex1Dfetch(Vert, offset + i3 * 3 + 0);
-	t2b = tex1Dfetch(Vert, offset + i3 * 3 + 1);
+        t0c = tex1Dfetch(Vert,         pid * 3 + 2);
+        t1a = tex1Dfetch(Vert, offset + i2 * 3 + 0);
+        t1b = tex1Dfetch(Vert, offset + i2 * 3 + 1);
+        t1c = tex1Dfetch(Vert, offset + i2 * 3 + 2);
+        t2a = tex1Dfetch(Vert, offset + i3 * 3 + 0);
+        t2b = tex1Dfetch(Vert, offset + i3 * 3 + 1);
 
-	ttt2ru( t0a, t0b, t0c, &r1, &u1);
-	ttt2ru( t1a, t1b, t1c, &r2, &u2);
-	tt2r  ( t2a, t2b,      &r3     );
+        ttt2ru( t0a, t0b, t0c, &r1, &u1);
+        ttt2ru( t1a, t1b, t1c, &r2, &u2);
+        tt2r  ( t2a, t2b,      &r3     );
 
-	f  = tri(r1, r2, r3, av[2 * idrbc], av[2 * idrbc + 1]);
-	f += visc(r1, r2, u1, u2);
-	return f;
+        f  = tri(r1, r2, r3, av[2 * idrbc], av[2 * idrbc + 1]);
+        f += visc(r1, r2, u1, u2);
+        return f;
     }
     return make_float3(-1.0e10f, -1.0e10f, -1.0e10f);
 }
@@ -93,32 +93,32 @@ __device__ float3 adj_dihedrals(float2 t0a, float2 t0b) {
     i2 = tex1Dfetch(Adj0, ((neighid + 1) % degreemax) + degreemax * lid);
 
     if (i2 == -1 && valid) {
-	i2 = tex1Dfetch(Adj0, 0 + degreemax * lid);
-	i3 = tex1Dfetch(Adj0, 1 + degreemax * lid);
+        i2 = tex1Dfetch(Adj0, 0 + degreemax * lid);
+        i3 = tex1Dfetch(Adj0, 1 + degreemax * lid);
     } else {
-	i3 =
-	    tex1Dfetch(Adj0, ((neighid + 2) % degreemax) + degreemax * lid);
-	if (i3 == -1 && valid) i3 = tex1Dfetch(Adj0, 0 + degreemax * lid);
+        i3 =
+            tex1Dfetch(Adj0, ((neighid + 2) % degreemax) + degreemax * lid);
+        if (i3 == -1 && valid) i3 = tex1Dfetch(Adj0, 0 + degreemax * lid);
     }
 
     i4 = tex1Dfetch(Adj1, neighid + degreemax * lid);
 
     if (valid) {
-	t1a = tex1Dfetch(Vert, offset + i1 * 3 + 0);
-	t1b = tex1Dfetch(Vert, offset + i1 * 3 + 1);
-	t2a = tex1Dfetch(Vert, offset + i2 * 3 + 0);
-	t2b = tex1Dfetch(Vert, offset + i2 * 3 + 1);
-	t3a = tex1Dfetch(Vert, offset + i3 * 3 + 0);
-	t3b = tex1Dfetch(Vert, offset + i3 * 3 + 1);
-	t4a = tex1Dfetch(Vert, offset + i4 * 3 + 0);
-	t4b = tex1Dfetch(Vert, offset + i4 * 3 + 1);
+        t1a = tex1Dfetch(Vert, offset + i1 * 3 + 0);
+        t1b = tex1Dfetch(Vert, offset + i1 * 3 + 1);
+        t2a = tex1Dfetch(Vert, offset + i2 * 3 + 0);
+        t2b = tex1Dfetch(Vert, offset + i2 * 3 + 1);
+        t3a = tex1Dfetch(Vert, offset + i3 * 3 + 0);
+        t3b = tex1Dfetch(Vert, offset + i3 * 3 + 1);
+        t4a = tex1Dfetch(Vert, offset + i4 * 3 + 0);
+        t4b = tex1Dfetch(Vert, offset + i4 * 3 + 1);
 
-	tt2r(t1a, t1b, &r1);
-	tt2r(t2a, t2b, &r2);
-	tt2r(t3a, t3b, &r3);
-	tt2r(t4a, t4b, &r4);
+        tt2r(t1a, t1b, &r1);
+        tt2r(t2a, t2b, &r2);
+        tt2r(t3a, t3b, &r3);
+        tt2r(t4a, t4b, &r4);
 
-	return dihedral<1>(r0, r2, r1, r4) + dihedral<2>(r1, r0, r2, r3);
+        return dihedral<1>(r0, r2, r1, r4) + dihedral<2>(r1, r0, r2, r3);
     }
     return make_float3(-1.0e10f, -1.0e10f, -1.0e10f);
 }
@@ -129,18 +129,18 @@ __global__ void force(int nc, float *__restrict__ av, float *ff) {
     int pid = (threadIdx.x + blockDim.x * blockIdx.x) / degreemax;
 
     if (pid < nc * nv) {
-	float2 t0 = tex1Dfetch(Vert, pid * 3 + 0);
-	float2 t1 = tex1Dfetch(Vert, pid * 3 + 1);
+        float2 t0 = tex1Dfetch(Vert, pid * 3 + 0);
+        float2 t1 = tex1Dfetch(Vert, pid * 3 + 1);
 
-	/* all triangles and dihedrals adjusting to vertex `pid` */
-	float3 f = adj_tris(t0, t1, av);
-	f += adj_dihedrals(t0, t1);
+        /* all triangles and dihedrals adjusting to vertex `pid` */
+        float3 f = adj_tris(t0, t1, av);
+        f += adj_dihedrals(t0, t1);
 
-	if (f.x > -1.0e9f) {
-	    atomicAdd(&ff[3 * pid + 0], f.x);
-	    atomicAdd(&ff[3 * pid + 1], f.y);
-	    atomicAdd(&ff[3 * pid + 2], f.z);
-	}
+        if (f.x > -1.0e9f) {
+            atomicAdd(&ff[3 * pid + 0], f.x);
+            atomicAdd(&ff[3 * pid + 1], f.y);
+            atomicAdd(&ff[3 * pid + 2], f.z);
+        }
     }
 }
 
@@ -152,8 +152,8 @@ __DF__ float3 tex2vec(int id) {
 
 __DF__ float2 warpReduceSum(float2 val) {
     for (int offset = warpSize / 2; offset > 0; offset /= 2) {
-	val.x += __shfl_down(val.x, offset);
-	val.y += __shfl_down(val.y, offset);
+        val.x += __shfl_down(val.x, offset);
+        val.y += __shfl_down(val.y, offset);
     }
     return val;
 }
@@ -164,20 +164,20 @@ __global__ void area_volume(float *totA_V) {
     int cid = blockIdx.y;
 
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < nt;
-	 i += blockDim.x * gridDim.x) {
-	int4 ids = tex1Dfetch(Tri, i);
+         i += blockDim.x * gridDim.x) {
+        int4 ids = tex1Dfetch(Tri, i);
 
-	float3 r0(tex2vec(3 * (ids.x + cid * nv)));
-	float3 r1(tex2vec(3 * (ids.y + cid * nv)));
-	float3 r2(tex2vec(3 * (ids.z + cid * nv)));
+        float3 r0(tex2vec(3 * (ids.x + cid * nv)));
+        float3 r1(tex2vec(3 * (ids.y + cid * nv)));
+        float3 r2(tex2vec(3 * (ids.z + cid * nv)));
 
-	a_v.x += area0(r0, r1, r2);
-	a_v.y += volume0(r0, r1, r2);
+        a_v.x += area0(r0, r1, r2);
+        a_v.y += volume0(r0, r1, r2);
     }
     a_v = warpReduceSum(a_v);
     if ((threadIdx.x & (warpSize - 1)) == 0) {
-	atomicAdd(&totA_V[2 * cid + 0], a_v.x);
-	atomicAdd(&totA_V[2 * cid + 1], a_v.y);
+        atomicAdd(&totA_V[2 * cid + 0], a_v.x);
+        atomicAdd(&totA_V[2 * cid + 1], a_v.y);
     }
 }
 #undef fst
