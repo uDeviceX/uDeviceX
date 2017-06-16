be26e22ee165fddbc71736dd88b34a0afcd2c3d7
Sergey Litvinov <slitvinov@gmail.com>
2017-06-16 09:44:25 +0200

    rbc : renaming : v/r

--- src/k/rbc0.h
+++ src/k/rbc0.h
@@ -12,13 +12,13 @@ namespace k_rbc {
      (a).x*(b).y - (a).y*(b).x)
 
 /* forces from one triangle */  
-__DF__ float3 tri(float3 v1, float3 v2, float3 v3, float area, float volume) {
+__DF__ float3 tri(float3 r1, float3 r2, float3 r3, float area, float volume) {
 #include "params/rbc.inc0.h"
     float Ak, A0, n_2, coefArea, coeffVol,
 	r, xx, IbforceI_wcl, kp, IbforceI_pow, ka0, kv0, x0, l0, lmax,
 	kbToverp;
 
-    float3 x21 = v2 - v1, x32 = v3 - v2, x31 = v3 - v1;
+    float3 x21 = r2 - r1, x32 = r3 - r2, x31 = r3 - r1;
     float3 nn = cross(x21, x31); /* normal */
 
     Ak = 0.5 * sqrtf(dot(nn, nn));
@@ -33,7 +33,7 @@ __DF__ float3 tri(float3 v1, float3 v2, float3 v3, float area, float volume) {
     kv0 = RBCkv / (6.0 * RBCtotVolume);
     coeffVol = kv0 * (volume - RBCtotVolume);
     float3 addFArea = coefArea * cross(nn, x32);
-    float3 addFVolume = coeffVol * cross(v3, v2);
+    float3 addFVolume = coeffVol * cross(r3, r2);
 
     r = sqrtf(dot(x21, x21));
     r = r < 0.0001f ? 0.0001f : r;
@@ -55,9 +55,9 @@ __DF__ float3 tri(float3 v1, float3 v2, float3 v3, float area, float volume) {
     return addFArea + addFVolume + (IbforceI_wcl + IbforceI_pow) * x21;
 }
 
-__DF__ float3 visc(float3 v1, float3 v2,
+__DF__ float3 visc(float3 r1, float3 r2,
 					 float3 u1, float3 u2) {
-    float3 du = u2 - u1, dr = v1 - v2;
+    float3 du = u2 - u1, dr = r1 - r2;
     float gammaC = RBCgammaC, gammaT = 3.0 * RBCgammaC;
 
     return gammaT                             * du +
@@ -66,12 +66,12 @@ __DF__ float3 visc(float3 v1, float3 v2,
 
 /* forces from one dihedral */
 template <int update>
-__DF__ float3 dihedral(float3 v1, float3 v2, float3 v3,
-					     float3 v4) {
+__DF__ float3 dihedral(float3 r1, float3 r2, float3 r3,
+					     float3 r4) {
     float overIksiI, overIdzeI, cosTheta, IsinThetaI2, sinTheta_1,
 	beta, b11, b12, phi, sint0kb, cost0kb;
 
-    float3 ksi = cross(v1 - v2, v1 - v3), dze = cross(v3 - v4, v2 - v4);
+    float3 ksi = cross(r1 - r2, r1 - r3), dze = cross(r3 - r4, r2 - r4);
     overIksiI = rsqrtf(dot(ksi, ksi));
     overIdzeI = rsqrtf(dot(dze, dze));
 
@@ -80,7 +80,7 @@ __DF__ float3 dihedral(float3 v1, float3 v2, float3 v3,
 
     sinTheta_1 = copysignf
 	(rsqrtf(max(IsinThetaI2, 1.0e-6f)),
-	 dot(ksi - dze, v4 - v1)); // ">" because the normals look inside
+	 dot(ksi - dze, r4 - r1)); // ">" because the normals look inside
 
     phi = RBCphi / 180.0 * M_PI;
     sint0kb = sin(phi) * RBCkb;
@@ -91,23 +91,23 @@ __DF__ float3 dihedral(float3 v1, float3 v2, float3 v3,
     b12 =  beta * overIksiI * overIdzeI;
 
     if (update == 1) {
-	return b11 * cross(ksi, v3 - v2) + b12 * cross(dze, v3 - v2);
+	return b11 * cross(ksi, r3 - r2) + b12 * cross(dze, r3 - r2);
     } else if (update == 2) {
 	float b22 = -beta * cosTheta * overIdzeI * overIdzeI;
-	return  b11 *  cross(ksi, v1 - v3) +
-	    b12 * (cross(ksi, v3 - v4) + cross(dze, v1 - v3)) +
-	    b22 *  cross(dze, v3 - v4);
+	return  b11 *  cross(ksi, r1 - r3) +
+	    b12 * (cross(ksi, r3 - r4) + cross(dze, r1 - r3)) +
+	    b22 *  cross(dze, r3 - r4);
     } else
     return make_float3(0, 0, 0);
 }
 
-__DF__ float area0(float3 v0, float3 v1, float3 v2) { return 0.5f * abscross(v1 - v0, v2 - v0); }
-__DF__ float volume0(float3 v0, float3 v1, float3 v2) {
+__DF__ float area0(float3 v0, float3 r1, float3 r2) { return 0.5f * abscross(r1 - v0, r2 - v0); }
+__DF__ float volume0(float3 v0, float3 r1, float3 r2) {
   return \
     0.1666666667f *
-    ((v0.x*v1.y-v0.y*v1.x)*v2.z +
-     (v0.z*v1.x-v0.x*v1.z)*v2.y +
-     (v0.y*v1.z-v0.z*v1.y)*v2.x);
+    ((v0.x*r1.y-v0.y*r1.x)*r2.z +
+     (v0.z*r1.x-v0.x*r1.z)*r2.y +
+     (v0.y*r1.z-v0.z*r1.y)*r2.x);
 }
 #undef sq
 #undef abscross2
