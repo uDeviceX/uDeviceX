7faeb8f4f230787192777b7ec91ea1b12517e8e3
Sergey Litvinov <slitvinov@gmail.com>
2017-06-17 14:59:09 +0200

    use wall::exch and sdf::bulk_wall

--- src/wall/imp.h
+++ src/wall/imp.h
@@ -66,100 +66,10 @@ static void exch(/*io*/ Particle *pp, int *n) { /* exchange pp(hst) between proc
 }
 
 int init(Particle *pp, int n, Particle *frozen, int *w_n) {
-    thrust::device_vector<int> keys(n);
-    k_sdf::fill_keys<<<k_cnf(n)>>>(pp, n, thrust::raw_pointer_cast(&keys[0]));
-    thrust::sort_by_key(keys.begin(), keys.end(), thrust::device_ptr<Particle>(pp));
-
-    int nsurvived = thrust::count(keys.begin(), keys.end(), 0);
-    int nbelt = thrust::count(keys.begin() + nsurvived, keys.end(), 1);
-    thrust::device_vector<Particle> solid_local(pp + nsurvived, pp + nsurvived + nbelt);
-    MSG("nsurvived/nbelt : %d/%d", nsurvived, nbelt);
-    dSync();
-
-    DeviceBuffer<Particle> solid_remote;
-    {
-	thrust::host_vector<Particle> local = solid_local;
-
-	int dstranks[26], remsizes[26], recv_tags[26];
-	for (int i = 0; i < 26; ++i) {
-	    int d[3] = {(i + 2) % 3 - 1, (i / 3 + 2) % 3 - 1, (i / 9 + 2) % 3 - 1};
-
-	    recv_tags[i] =
-		(2 - d[0]) % 3 + 3 * ((2 - d[1]) % 3 + 3 * ((2 - d[2]) % 3));
-
-	    int coordsneighbor[3];
-	    for (int c = 0; c < 3; ++c) coordsneighbor[c] = m::coords[c] + d[c];
-	    MC(l::m::Cart_rank(m::cart, coordsneighbor, dstranks + i));
-	}
-
-	// send local counts - receive remote counts
-	{
-	    for (int i = 0; i < 26; ++i) remsizes[i] = -1;
-
-	    MPI_Request reqrecv[26];
-	    for (int i = 0; i < 26; ++i)
-	    MC(l::m::Irecv(remsizes + i, 1, MPI_INTEGER, dstranks[i],
-			 BT_C_WALL + recv_tags[i], m::cart, reqrecv + i));
-
-	    int localsize = local.size();
-	    MPI_Request reqsend[26];
-	    for (int i = 0; i < 26; ++i)
-	    MC(l::m::Isend(&localsize, 1, MPI_INTEGER, dstranks[i], BT_C_WALL + i,
-			 m::cart, reqsend + i));
-	    MPI_Status statuses[26];
-	    MC(l::m::Waitall(26, reqrecv, statuses));
-	    MC(l::m::Waitall(26, reqsend, statuses));
-	}
-
-	std::vector<Particle> remote[26];
-	// send local data - receive remote data
-	{
-	    for (int i = 0; i < 26; ++i) remote[i].resize(remsizes[i]);
-
-	    MPI_Request reqrecv[26];
-	    for (int i = 0; i < 26; ++i)
-	    MC(l::m::Irecv(remote[i].data(), remote[i].size() * 6, MPI_FLOAT,
-			 dstranks[i], BT_P_WALL + recv_tags[i], m::cart,
-			 reqrecv + i));
-	    MPI_Request reqsend[26];
-	    for (int i = 0; i < 26; ++i)
-	    MC(l::m::Isend(local.data(), local.size() * 6, MPI_FLOAT,
-			 dstranks[i], BT_P_WALL + i, m::cart, reqsend + i));
-
-	    MPI_Status statuses[26];
-	    MC(l::m::Waitall(26, reqrecv, statuses));
-	    MC(l::m::Waitall(26, reqsend, statuses));
-	}
-
-	// select particles within my region [-L / 2 - MARGIN, +L / 2 + MARGIN]
-	std::vector<Particle> selected;
-	int L[3] = {XS, YS, ZS};
-	int MARGIN[3] = {XWM, YWM, ZWM};
-
-	for (int i = 0; i < 26; ++i) {
-	    int d[3] = {(i + 2) % 3 - 1, (i / 3 + 2) % 3 - 1, (i / 9 + 2) % 3 - 1};
-	    for (int j = 0; j < (int) remote[i].size(); ++j) {
-		Particle p = remote[i][j];
-		for (int c = 0; c < 3; ++c) p.r[c] += d[c] * L[c];
-		bool inside = true;
-		for (int c = 0; c < 3; ++c)
-		inside &=
-		    p.r[c] >= -L[c] / 2 - MARGIN[c] && p.r[c] < L[c] / 2 + MARGIN[c];
-		if (inside) selected.push_back(p);
-	    }
-	}
-
-	solid_remote.resize(selected.size());
-	cH2D(solid_remote.D, selected.data(), solid_remote.S);
-    }
-
-    *w_n = solid_local.size() + solid_remote.S;
-
-    cD2D(frozen, thrust::raw_pointer_cast(&solid_local[0]), solid_local.size());
-    cD2D(frozen + solid_local.size(), solid_remote.D, solid_remote.S);
-
-    return nsurvived;
-} /* end of ini */
+  sdf::bulk_wall(/*io*/ pp, &n, /*o*/ frozen, w_n); /* sort into bulk-frozen */
+  exch(/*io*/ frozen, w_n);
+  return n;
+}
 
 void build_cells(const int n, Particle *pp, Clist *cells) {if (n) cells->build(pp, n);}
 
