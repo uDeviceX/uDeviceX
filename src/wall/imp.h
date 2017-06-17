f1f7cbe58c8c6046d2cb87afce32f09370fb80e3
Sergey Litvinov <slitvinov@gmail.com>
2017-06-17 14:50:22 +0200

    port exch from master

--- src/wall/imp.h
+++ src/wall/imp.h
@@ -1,3 +1,69 @@
+static void exch(Particle *pp, int *n) { /* excchange pp(hst) between processors */
+  assert(sizeof(Particle) == 6 * sizeof(float)); /* :TODO: dependencies */
+  enum {X, Y, Z};
+  int i, j, c;
+  int dstranks[26], remsizes[26], recv_tags[26];
+  for (i = 0; i < 26; ++i) {
+    int d[3] = {(i + 2) % 3 - 1, (i / 3 + 2) % 3 - 1, (i / 9 + 2) % 3 - 1};
+    recv_tags[i] =
+      (2 - d[0]) % 3 + 3 * ((2 - d[1]) % 3 + 3 * ((2 - d[2]) % 3));
+    int co_ne[3], ranks[3] = {m::coords[X], m::coords[Y], m::coords[Z]};
+    for (c = 0; c < 3; ++c) co_ne[c] = ranks[c] + d[c];
+    l::m::Cart_rank(m::cart, co_ne, dstranks + i);
+  }
+
+  // send local counts - receive remote counts
+  {
+    for (i = 0; i < 26; ++i) remsizes[i] = -1;
+    MPI_Request reqrecv[26], reqsend[26];
+    MPI_Status  statuses[26];
+    for (i = 0; i < 26; ++i)
+      l::m::Irecv(remsizes + i, 1, MPI_INTEGER, dstranks[i],
+	       123 + recv_tags[i], m::cart, reqrecv + i);
+    for (i = 0; i < 26; ++i)
+      l::m::Isend(n, 1, MPI_INTEGER, dstranks[i], 123 + i, m::cart, reqsend + i);
+    l::m::Waitall(26, reqrecv, statuses);
+    l::m::Waitall(26, reqsend, statuses);
+  }
+
+  std::vector<Particle> remote[26];
+  // send local data - receive remote data
+  {
+    for (i = 0; i < 26; ++i) remote[i].resize(remsizes[i]);
+    MPI_Request reqrecv[26], reqsend[26];
+    MPI_Status  statuses[26];
+    for (i = 0; i < 26; ++i)
+      l::m::Irecv(remote[i].data(), remote[i].size() * 6, MPI_FLOAT,
+	       dstranks[i], 321 + recv_tags[i], m::cart,
+	       reqrecv + i);
+    for (i = 0; i < 26; ++i)
+      l::m::Isend(pp, (*n) * 6, MPI_FLOAT,
+	       dstranks[i], 321 + i, m::cart, reqsend + i);
+    l::m::Waitall(26, reqrecv, statuses);
+    l::m::Waitall(26, reqsend, statuses);
+  }
+  l::m::Barrier(m::cart);
+
+  int L[3] = {XS, YS, ZS}, WM[3] = {XWM, YWM, ZWM};
+  float lo[3], hi[3];
+  for (c = 0; c < 3; c ++) {
+    lo[c] = -0.5*L[c] - WM[c];
+    hi[c] =  0.5*L[c] + WM[c];
+  }
+
+  for (i = 0; i < 26; ++i) {
+    int d[3] = {(i + 2) % 3 - 1, (i / 3 + 2) % 3 - 1, (i / 9 + 2) % 3 - 1};
+    for (j = 0; j < remote[i].size(); ++j) {
+      Particle p = remote[i][j];
+      for (c = 0; c < 3; ++c) {
+	p.r[c] += d[c] * L[c];
+	if (p.r[c] < lo[c] || p.r[c] >= hi[c]) goto next;
+      }
+      pp[(*n)++] = p;
+    next: ;
+    }
+  }
+}
 
 int init(Particle *pp, int n, Particle *frozen, int *w_n) {
     thrust::device_vector<int> keys(n);
