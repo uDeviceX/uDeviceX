6e26d56b62e3b846ab8d54b208f53e48fabe7303
Sergey Litvinov <slitvinov@gmail.com>
2017-06-17 21:08:10 +0200

    unpack in distr

--- src/sol/int.h
+++ src/sol/int.h
@@ -26,30 +26,49 @@ struct Work {
   Particle *pp0;
 };
 
-void alloc_work(Work w) {
-  uchar4 *subi_lo = w.subi_lo;
-  uchar4 *subi_re = w.subi_re;
-  uint   *iidx = w.iidx;
-  Particle *pp_re = w.pp_re;
-  unsigned char *count_zip = w.count_zip;
-  Particle *pp0 = w.pp0;
+void alloc_work(Work *w) {
+  uchar4 *subi_lo = w->subi_lo;
+  uchar4 *subi_re = w->subi_re;
+  uint   *iidx = w->iidx;
+  Particle *pp_re = w->pp_re;
+  unsigned char *count_zip = w->count_zip;
+  Particle *pp0 = w->pp0;
 }
 
 /* no alloc_ticketD */
 void create_ticketD(TicketD *t) { t->first = true; }
 
-void alloc_ticketZ(/**/ TicketZ t) {
-  float4  *zip0 = t.zip0;
-  ushort4 *zip1 = t.zip1;
+void alloc_ticketZ(/**/ TicketZ *t) {
+  float4  *zip0 = t->zip0;
+  ushort4 *zip1 = t->zip1;
   mpDeviceMalloc(&zip0);
   mpDeviceMalloc(&zip1);
 }
 
-void create_ticketZ(Quants q, /**/ TicketZ t) {
-  Particle  *pp = q.pp;
-  int         n = q.n;
-  float4  *zip0 = t.zip0;
-  ushort4 *zip1 = t.zip1;
+void create_ticketZ(Quants *q, /**/ TicketZ *t) {
+  Particle  *pp = q->pp;
+  int         n = q->n;
+  float4  *zip0 = t->zip0;
+  ushort4 *zip1 = t->zip1;
   assert(sizeof(Particle) == 6 * sizeof(float)); /* :TODO: implicit dependency */
   sub::zip<<<(n + 1023) / 1024, 1024, 1024 * 6 * sizeof(float)>>>(zip0, zip1, (float*)pp, n);
+}
+
+void distr(Quants *q, TicketD *t, Work *w) {
+  uchar4 *subi_lo = w->subi_lo; /* arrays */
+  uchar4 *subi_re = w->subi_re;
+  uint   *iidx = w->iidx;
+  Particle *pp_re = w->pp_re;
+  unsigned char *count_zip = w->count_zip;
+  Particle *pp0 = w->pp0;
+
+  MPI_Comm cart = t->cart; /* can a copy */
+  
+  int *rank = t->rank; /* arrays */
+  int *send_size_req = t->send_size_req;
+  int *recv_size_req = t->recv_size_req;
+  MPI_Request *send_mesg_req = t->send_mesg_req;
+  MPI_Request *recv_mesg_req = t->recv_mesg_req;
+  
+  bool *first = &t->first; /* shoud be updated */
 }
