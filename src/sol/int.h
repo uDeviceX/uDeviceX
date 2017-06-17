8ef8450a3e82c9793a8a27f4d2035c5bd436cc9e
Sergey Litvinov <slitvinov@gmail.com>
2017-06-17 21:32:38 +0200

    drop a body of distr to int.h

--- src/sol/int.h
+++ src/sol/int.h
@@ -72,25 +72,22 @@ void create_ticketZ(Quants *q, /**/ TicketZ *t) {
   sub::zip<<<(n + 1023) / 1024, 1024, 1024 * 6 * sizeof(float)>>>(zip0, zip1, (float*)pp, n);
 }
 
-void distr(Quants *q, TicketD *t, Work *w) {
-  /*** Q ***/
+void distr(Quants *q, TicketD *td, TicketZ *tz, Work *w) {
   Particle *pp  =  q->pp;
-  int       *n  = &q->n;
+  int       *qn = &q->n;
   Clist *cells  =  q->cells;
 
-  /*** T ***/  
-  MPI_Comm cart = t->cart; /* can be a copy */
-  
-  int *rank = t->rank; /* arrays */
-  int *send_size_req = t->send_size_req;
-  int *recv_size_req = t->recv_size_req;
-  MPI_Request *send_mesg_req = t->send_mesg_req;
-  MPI_Request *recv_mesg_req = t->recv_mesg_req;
-  
-  bool *first = &t->first; /* shoud be updated */
+  MPI_Comm cart = td->cart; /* can be a copy */
+  int *rank = td->rank; /* arrays */
+  int *send_size_req = td->send_size_req;
+  int *recv_size_req = td->recv_size_req;
+  MPI_Request *send_mesg_req = td->send_mesg_req;
+  MPI_Request *recv_mesg_req = td->recv_mesg_req;
+  bool *qfirst = &td->first; /* shoud be updated */
 
-  
-  /*** W ***/
+  float4  *zip0 = tz->zip0;
+  ushort4 *zip1 = tz->zip1;
+
   uchar4 *subi_lo = w->subi_lo; /* arrays */
   uchar4 *subi_re = w->subi_re;
   uint   *iidx = w->iidx;
@@ -98,5 +95,54 @@ void distr(Quants *q, TicketD *t, Work *w) {
   unsigned char *count_zip = w->count_zip;
   Particle *pp0 = w->pp0;
 
-  /* TODO */
+  int n = *qn;
+  bool first = *qfirst;
+  
+  int nbulk, nhalo_padded, nhalo;
+  odstr::post_recv(cart, rank, /**/ recv_size_req, recv_mesg_req);
+  if (n) {
+    odstr::halo(pp, n);
+    odstr::scan(n);
+    odstr::pack(pp, n);
+  }
+  if (!first) {
+    odstr::waitall(send_size_req);
+    odstr::waitall(send_mesg_req);
+  }
+  first = false;
+  nbulk = odstr::send_sz(cart, rank, send_size_req);
+  odstr::send_msg(cart, rank, send_mesg_req);
+
+  CC(cudaMemsetAsync(cells->count, 0, sizeof(int)*XS*YS*ZS));
+  if (n)
+    k_common::subindex_local<false><<<k_cnf(n)>>>
+      (n, (float2*)pp, /*io*/ cells->count, /*o*/ subi_lo);
+
+  odstr::waitall(recv_size_req);
+  odstr::recv_count(&nhalo_padded, &nhalo);
+  odstr::waitall(recv_mesg_req);
+  if (nhalo)
+    odstr::unpack
+      (nhalo_padded, /*io*/ cells->count, /*o*/ subi_re, pp_re);
+
+  k_common::compress_counts<<<k_cnf(XS*YS*ZS)>>>
+    (XS*YS*ZS, (int4*)cells->count, /**/ (uchar4*)count_zip);
+  l::scan::d::scan(count_zip, XS*YS*ZS, /**/ (uint*)cells->start);
+
+  if (n)
+    k_odstr::scatter<<<k_cnf(n)>>>
+      (false, subi_lo,  n, cells->start, /**/ iidx);
+
+  if (nhalo)
+    k_odstr::scatter<<<k_cnf(nhalo)>>>
+      (true, subi_re, nhalo, cells->start, /**/ iidx);
+
+  n = nbulk + nhalo;
+  if (n)
+    k_odstr::gather<<<k_cnf(n)>>>
+      ((float2*)pp, (float2*)pp_re, n, iidx,
+       /**/ (float2*)pp0, zip0, zip1);
+
+  *qn = n;
+  *qfirst = first;
 }
