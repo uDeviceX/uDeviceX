dac2f5975183e6c5a4b889da8fb4668016a75a1d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-19 17:31:21 +0200

    alloc/free quants

--- src/rig/int.h
+++ src/rig/int.h
@@ -11,3 +11,25 @@ struct TicketBB {
 
     Solid *ss;
 };
+
+void alloc_quants(Quants *q) {
+    n - ns = nps = 0;
+    
+    CC(cudaMalloc(&q->pp ,     MAX_PART_NUM * sizeof(Particle)));
+    CC(cudaMalloc(&q->ss ,       MAX_SOLIDS * sizeof(Solid)));
+    CC(cudaMalloc(&q->rr0, 3 * MAX_PART_NUM * sizeof(float)));
+    
+    q->pp_hst  = new Particle[MAX_PART_NUM];
+    q->ss_hst  = new Solid[MAX_SOLIDS];
+    q->rr0_hst = new float[3 * MAX_PART_NUM];
+}
+
+void free_quants(Quants *q) {
+    delete[] q->pp_hst;
+    delete[] q->ss_hst;
+    delete[] q->rr0_hst;
+
+    CC(cudaFree(q->pp));
+    CC(cudaFree(q->ss));
+    CC(cudaFree(q->rr0));
+}
