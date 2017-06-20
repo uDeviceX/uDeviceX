3719f19d0be0ad374d5b23f5e2cbef518675febb
Sergey Litvinov <slitvinov@gmail.com>
2017-06-20 06:39:48 +0200

    clean up dpd/cuda-dpd.h

--- src/dpd/cuda-dpd.h
+++ src/dpd/cuda-dpd.h
@@ -1,11 +1,8 @@
 template<int s>
-inline __device__ float viscosity_function(float x)
-{
-  return sqrtf(viscosity_function<s - 1>(x));
-}
+inline __device__ float viscosity_function(float x) { return sqrtf(viscosity_function<s - 1>(x)); }
 
 template<> inline __device__ float viscosity_function<1>(float x) { return sqrtf(x); }
-template<> inline __device__ float viscosity_function<0>(float x){ return x; }
+template<> inline __device__ float viscosity_function<0>(float x) { return x;        }
 
 void forces_dpd_cuda_nohost(const float4 * const xyzouvwo, const ushort4 * const xyzo_half,
                             float * const axayaz,  const int np,
@@ -14,12 +11,3 @@ void forces_dpd_cuda_nohost(const float4 * const xyzouvwo, const ushort4 * const
                             const float XL, const float YL, const float ZL,
                             const float invsqrtdt,
                             const float seed);
-
-void forces_dpd_cuda(const float * const xp, const float * const yp, const float * const zp,
-                     const float * const xv, const float * const yv, const float * const zv,
-                     float * const xa, float * const ya, float * const za,
-                     const int np,
-                     const float rc,
-                     const float LX, const float LY, const float LZ,
-                     const float invsqrtdt,
-                     const float seed);
