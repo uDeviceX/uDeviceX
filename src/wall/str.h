5901aa9cfd76ca0b75f7341979a098fe1b1735dd
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-20 14:36:59 +0200

    add wall/str::write

--- src/wall/str.h
+++ src/wall/str.h
@@ -13,5 +13,15 @@ void read(const int id, /**/ float4 *pp, int *n) {
 }
 
 void write(const int id, const float4 *pp, const int n) {
-    
+    Particle *pphst, *ppdev;
+    pphst = new Particle[MAX_NUM_PART];
+    if (n) {
+        CC(cudaMalloc(&ppdev, MAXNUM_PART_NUM * sizeof(Particle)));
+        dev::float42particle <<<k_cnf(n)>>> (pp, *n, /**/ ppdev);
+        cD2H(pphst, ppdev, n);
+        CC(cudaFree(ppdev));
+    }
+    restart::write("wall", id, /**/ pphst, n);
+
+    delete[] pphst;
 }
