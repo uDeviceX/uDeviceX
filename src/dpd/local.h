cbda2373393627662d720da1da0589aa410b2993
Sergey Litvinov <slitvinov@gmail.com>
2017-06-20 13:10:54 +0200

    unconst

--- src/dpd/local.h
+++ src/dpd/local.h
@@ -1,7 +1,3 @@
-void flocal0(const float4 * const xyzouvwo, const ushort4 * const xyzo_half,
-	     float * const axayaz,  const int np,
-	     const int * const cellsstart, const int * const cellscount,
-	     const float rc,
-	     const float XL, const float YL, const float ZL,
-	     const float invsqrtdt,
-	     const float seed);
+void flocal0( float4 *  xyzouvwo,  ushort4 *  xyzo_half, float *  axayaz,   int np,
+	      int *  cellsstart,  int *  cellscount,
+	      float rc, float XL,  float YL,  float ZL, float invsqrtdt, float seed);
