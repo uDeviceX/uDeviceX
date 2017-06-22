5f28de3258bc8dec0c926396bbbdf25b6373d346
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-22 12:00:12 +0200

    typedef

--- src/wall/dev.h
+++ src/wall/dev.h
@@ -19,7 +19,9 @@ __global__ void float42particle(const float4 *src, const int n, Particle *dst) {
 
 __device__ int minmax(int lo, int hi, int a) { return min(hi, max(lo, a)); }
 
-__global__ void interactions_3tpp(const sdf::sub::dev::tex3Dca<float> texsdf, const float2 *const pp, const int np, const int w_n,
+typedef const sdf::sub::dev::tex3Dca<float> TexSDF_t;
+
+__global__ void interactions_3tpp(TexSDF_t texsdf, const float2 *const pp, const int np, const int w_n,
                                   float *const acc, const float seed, const int type, const Texo<int> texstart, const Texo<float4> texwpp) {
                                   
 #define start_fetch(i) (texstart.fetch(i))
