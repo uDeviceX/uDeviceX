7a990f491b7b3229cdc4caa719caa7f707d47b23
Sergey Litvinov <slitvinov@gmail.com>
2017-06-22 12:06:20 +0200

    conf.make calls conf.make0

--- cmd/r.sh
+++ cmd/r.sh
@@ -1,6 +1,24 @@
 make > /dev/null
 
 u.conf test_data/double.poiseuille.h <<!
-pushflow
+pushflow # hmmm
+# now run
 run
 !
+
+u.conf test_data/double.poiseuille.h <<!
+run
+pushflow # hmmm
+run
+!
+
+u.conf ../src test_data/double.poiseuille.h <<!
+run
+pushflow # hmmm
+run
+!
+
+
+#u.conf test_data/double.poiseuille.h <<!
+#pushflow # hmmm
+#!
