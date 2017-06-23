438e3a0ffec459d9e175971d52a39286a7058d2a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-23 09:20:40 +0200

    minor

--- src/sim/run.h
+++ src/sim/run.h
@@ -1,14 +1,12 @@
 void run_eq(long te) { /* equilibrate */
-    long it;
     float driving_force0 = 0;
     bool wall0 = false;
-    for (it = 0; it < te; ++it) step(driving_force0, wall0, it);
+    for (long it = 0; it < te; ++it) step(driving_force0, wall0, it);
 }
 
 void run(long ts, long te) {
     dump_strt_templ(); /* :TODO: is it the right place? */
     /* ts, te: time start and end */
-    long it;
     float driving_force0 = pushflow ? driving_force : 0;
-    for (it = ts; it < te; ++it) step(driving_force0, walls, it);
+    for (long it = ts; it < te; ++it) step(driving_force0, walls, it);
 }
