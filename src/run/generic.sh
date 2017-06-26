09bf4daaad928b189571e57d884331ee90d21a5a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-26 10:35:28 +0200

    cmd

--- src/run/generic.sh
+++ src/run/generic.sh
@@ -2,6 +2,7 @@ setup() {
     ( make -C ../tools/rbc install        ) > /dev/null
     ( make -C ../tools install            ) > /dev/null
     ( make -C ../post/build_smesh install ) > /dev/null
+    ( cd ../cmd; make                     ) > /dev/null
 }
 
 compile() {
