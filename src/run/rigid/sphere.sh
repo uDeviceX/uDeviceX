79729bd22acc6d3642becf09a72c42f079b68dc4
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-26 10:51:35 +0200

    add pre and run

--- src/run/rigid/sphere.sh
+++ src/run/rigid/sphere.sh
@@ -5,5 +5,38 @@
 ./configure
 . ./run/generic.sh
 
-clean
+pre() {
+    clean
+
+    sdf=data/sdf/yplates1/yplates.dat
+    rig=data/rig/sphere.ply
+
+    cp $sdf sdf.dat
+    cp $rig mesh_solid.ply
+
+    NX=1  NY=1  NZ=1
+    XS=32 YS=32 ZS=16
+    geom # set NN, LX, LY, LZ, Domain
+    
+    Tend=100
+    gamma_dot=0.01
+    
+    u.conf run/rigid/conf.h <<EOF
+    $Domain tend=$Tend
+    gamma_dot=$gamma_dot
+    field_dumps part_dumps
+    field_freq=4000 parts_freq=1000
+    solids sbounce_back
+    run
+EOF
+    u.strtdir . $NX $NY $NZ
+}
+
+run() {
+    sh runfile
+}
+
 setup
+pre
+compile
+run
