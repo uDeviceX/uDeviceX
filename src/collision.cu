3bee1642709c20a14587789a78dc8fc17834c252
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-26 15:19:42 +0200

    format

--- src/collision.cu
+++ src/collision.cu
@@ -11,8 +11,7 @@ namespace collision
 enum {X, Y, Z};
 #define _HD_ __host__ __device__
 
-static _HD_ bool same_side(const float *x, const float *p, const float *a, const float *b, const float *inplane)
-{
+static _HD_ bool same_side(const float *x, const float *p, const float *a, const float *b, const float *inplane) {
     const float n[3] = {a[Y] * b[Z] - a[Z] * b[Y],
                         a[Z] * b[X] - a[X] * b[Z],
                         a[X] * b[Y] - a[Y] * b[X]};
@@ -23,8 +22,7 @@ static _HD_ bool same_side(const float *x, const float *p, const float *a, const
     return ndx * ndp > 0;
 }
     
-static _HD_ bool in_tetrahedron(const float *x, const float *A, const float *B, const float *C, const float *D)
-{
+static _HD_ bool in_tetrahedron(const float *x, const float *A, const float *B, const float *C, const float *D) {
     const float AB[3] = {B[X] - A[X], B[Y] - A[Y], B[Z] - A[Z]};
     const float AC[3] = {C[X] - A[X], C[Y] - A[Y], C[Z] - A[Z]};
     const float AD[3] = {D[X] - A[X], D[Y] - A[Y], D[Z] - A[Z]};
@@ -39,8 +37,7 @@ static _HD_ bool in_tetrahedron(const float *x, const float *A, const float *B, 
         same_side(x, D, AB, AC, A);
 }
     
-int inside_1p(const float *r, const float *vv, const int *tt, const int nt)
-{
+int inside_1p(const float *r, const float *vv, const int *tt, const int nt) {
     int c = 0;
     float origin[3] = {0, 0, 0};
 #ifdef spdir
@@ -54,14 +51,12 @@ int inside_1p(const float *r, const float *vv, const int *tt, const int nt)
     return c%2;
 }
 
-void inside_hst(const Particle *pp, const int n, const Mesh m, /**/ int *inout)
-{
+void inside_hst(const Particle *pp, const int n, const Mesh m, /**/ int *inout) {
     for (int i = 0; i < n; ++i)
     inout[i] = inside_1p(pp[i].r, m.vv, m.tt, m.nt);
 }
 
-static int inside_1p(const float *r, const Particle *vv, const int *tt, const int nt)
-{
+static int inside_1p(const float *r, const Particle *vv, const int *tt, const int nt) {
     int c = 0;
     float origin[3] = {0, 0, 0};
 #ifdef spdir
@@ -75,14 +70,11 @@ static int inside_1p(const float *r, const Particle *vv, const int *tt, const in
     return c%2;
 }
     
-void inside_hst(const Particle *pp, const int n, const Mesh m, const Particle *i_pp, const int ns, /**/ int *tags)
-{
-    for (int i = 0; i < n; ++i)
-    {
+void inside_hst(const Particle *pp, const int n, const Mesh m, const Particle *i_pp, const int ns, /**/ int *tags) {
+    for (int i = 0; i < n; ++i) {
         tags[i] = -1;
         for (int sid = 0; sid < ns; ++sid)
-        if (inside_1p(pp[i].r, i_pp + m.nv * sid, m.tt, m.nt))
-        {
+        if (inside_1p(pp[i].r, i_pp + m.nv * sid, m.tt, m.nt)) {
             tags[i] = sid;
             break;
         }
@@ -91,15 +83,13 @@ void inside_hst(const Particle *pp, const int n, const Mesh m, const Particle *i
 
 namespace kernels
 {
-__global__ void init_tags(const int n, /**/ int *tags)
-{
+__global__ void init_tags(const int n, /**/ int *tags) {
     const int gid = threadIdx.x + blockIdx.x * blockDim.x;
     if (gid < n) tags[gid] = -1;
 }
 
 // assume ns blocks along y
-__global__ void compute_tags(const Particle *pp, const int n, const Particle *vv, const int nv, const int *tt, const int nt, /**/ int *tags)
-{
+__global__ void compute_tags(const Particle *pp, const int n, const Particle *vv, const int nv, const int *tt, const int nt, /**/ int *tags) {
     const int sid = blockIdx.y;
     const int gid = threadIdx.x + blockIdx.x * blockDim.x;
     if (gid >= n) return;
@@ -113,8 +103,7 @@ __global__ void compute_tags(const Particle *pp, const int n, const Particle *vv
 #endif
 
         
-    for (int i = 0; i < nt; ++i)
-    {
+    for (int i = 0; i < nt; ++i) {
         const int t1 = sid * nv + tt[3*i + 0];
         const int t2 = sid * nv + tt[3*i + 1];
         const int t3 = sid * nv + tt[3*i + 2];
@@ -131,8 +120,7 @@ __global__ void compute_tags(const Particle *pp, const int n, const Particle *vv
 }
 }
     
-void inside_dev(const Particle *pp, const int n, const Mesh m, const Particle *i_pp, const int ns, /**/ int *tags)
-{
+void inside_dev(const Particle *pp, const int n, const Mesh m, const Particle *i_pp, const int ns, /**/ int *tags) {
     if (ns == 0 || n == 0) return;
         
     kernels::init_tags <<< k_cnf(n) >>> (n, /**/ tags);
@@ -143,8 +131,7 @@ void inside_dev(const Particle *pp, const int n, const Mesh m, const Particle *i
     kernels::compute_tags <<< blck, thrd >>> (pp, n, i_pp, m.nv, m.tt, m.nt, /**/ tags);
 }
 
-static void get_bbox(const Particle *pp, const int n, /**/ float3 *minbb, float3 *maxbb)
-{
+static void get_bbox(const Particle *pp, const int n, /**/ float3 *minbb, float3 *maxbb) {
     if (n == 0) return;
 
     const float *r = pp[0].r;
@@ -183,14 +170,12 @@ void get_bbox(const float *rr, const int n, /**/ float3 *minbb, float3 *maxbb) {
     }
 }
 
-void get_bboxes_hst(const Particle *pp, const int nps, const int ns, /**/ float3 *minbb, float3 *maxbb)
-{
+void get_bboxes_hst(const Particle *pp, const int nps, const int ns, /**/ float3 *minbb, float3 *maxbb) {
     for (int i = 0; i < ns; ++i)
     get_bbox(pp + i*nps, nps, /**/ minbb + i, maxbb + i);
 }
 
-void get_bboxes_dev(const Particle *pp, const int nps, const int ns, /**/ float3 *minbb, float3 *maxbb)
-{
+void get_bboxes_dev(const Particle *pp, const int nps, const int ns, /**/ float3 *minbb, float3 *maxbb) {
     if (ns == 0) return;
     minmax(pp, nps, ns, /**/ minbb, maxbb);
 }
