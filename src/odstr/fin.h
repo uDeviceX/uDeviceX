6a491414305bca0ce54cd967966288a8a795297a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-29 15:23:32 +0200

    free ii*

--- src/odstr/fin.h
+++ src/odstr/fin.h
@@ -4,13 +4,22 @@ void Distr::fin() {
         if (i) {
             CC(cudaFreeHost(s.pp_hst_[i]));
             CC(cudaFreeHost(r.pp_hst_[i]));
+
+            if (global_ids) {
+                CC(cudaFreeHost(s.ii_hst_[i]));
+                CC(cudaFreeHost(r.ii_hst_[i]));
+            }
         } else {
-            CC(cudaFree    (s.pp_hst_[i])); /* r.pp_hst_[0] = s.hst_[0] */
+            CC(                cudaFree(s.pp_hst_[i])); /* r.pp_hst_[0] = s.pp_hst_[0] */
+            if (global_ids) CC(cudaFree(s.ii_hst_[i])); /* r.ii_hst_[0] = s.ii_hst_[0] */
         }
     }
 
     CC(cudaFree(s.iidx));
     CC(cudaFree(s.pp_dev)); CC(cudaFree(r.pp_dev));
+    if (global_ids) {
+        CC(cudaFree(s.ii_dev)); CC(cudaFree(r.ii_dev));
+    }
 
     CC(cudaFree(s.size_dev)); CC(cudaFree(s.strt));
     CC(cudaFree(r.strt));
