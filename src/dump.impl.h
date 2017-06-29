941502f170f169d013cd50af5a188cd14b04796a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-29 16:45:43 +0200

    dump::parts_ids

--- src/dump.impl.h
+++ src/dump.impl.h
@@ -26,9 +26,19 @@ static void copy_shift(const Particle *pp, const long n, float *w) {
     }
 }
 
+static void copy_shift_id(const Particle *pp, const int *ii, const long n, float *w) {
+    for (int j = 0; j < n; ++j) {
+        for (int d = 0; d < 3; ++d) {
+            w[7 * j + d]     = pp[j].r[d] + mi[d];
+            w[7 * j + 3 + d] = pp[j].v[d];
+        }
+        w[7 * j + 6] = ii[j];
+    }
+}
+
 #define PATTERN "%s-%05d"
     
-static void header(const long n, const char *name, const int step) {
+static void header(const bool dumpid, const long n, const char *name, const int step) {
     char fname[256] = {0};
     sprintf(fname, DUMP_BASE "/bop/" PATTERN ".bop", name, step / part_freq);
         
@@ -40,9 +50,8 @@ static void header(const long n, const char *name, const int step) {
     fprintf(f, "%ld\n", n);
     fprintf(f, "DATA_FILE: " PATTERN ".values\n", name, step / part_freq);
     fprintf(f, "DATA_FORMAT: float\n");
-    fprintf(f, "VARIABLES: x y z vx vy vz\n");
-    // extra comment
-    fprintf(f, "t = %.6e\n", step * dt);
+    if (dumpid) fprintf(f, "VARIABLES: x y z vx vy vz id\n");
+    else        fprintf(f, "VARIABLES: x y z vx vy vz\n");
     fclose(f);
 }
     
@@ -63,7 +72,31 @@ void parts(const Particle *pp, const long n, const char *name, const int step) {
     MC( MPI_File_set_size(f, 0) );
     MC( MPI_File_get_position(f, &base) ); 
 
-    if (m::rank == 0) header(ntot, name, step);
+    if (m::rank == 0) header(false, ntot, name, step);
+
+    MC( MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, m::cart) );
+    MC( MPI_File_write_at_all(f, base + offset, w_pp, n, Particle::datatype(), &status) ); 
+    MC( MPI_File_close(&f) );
+}
+
+void parts_ids(const Particle *pp, const int *ii, const long n, const char *name, const int step) {
+    copy_shift_id(pp, ii, n, /**/ w_pp);
+        
+    char fname[256] = {0};
+    sprintf(fname, DUMP_BASE "/bop/" PATTERN ".values", name, step / part_freq);
+
+    MPI_File f;
+    MPI_Status status;
+    MPI_Offset base, offset = 0;
+    MPI_Offset len = n * sizeof(Particle);
+
+    long ntot = 0;
+    MC( l::m::Reduce(&n, &ntot, 1, MPI_LONG, MPI_SUM, 0, m::cart) );
+    MC( MPI_File_open(m::cart, fname, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f) );
+    MC( MPI_File_set_size(f, 0) );
+    MC( MPI_File_get_position(f, &base) ); 
+
+    if (m::rank == 0) header(true, ntot, name, step);
 
     MC( MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, m::cart) );
     MC( MPI_File_write_at_all(f, base + offset, w_pp, n, dumptype, &status) ); 
