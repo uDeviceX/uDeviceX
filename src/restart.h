b6ec0fa8f7a3675e30334b721284c54fa4488be3
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-06-30 15:51:13 +0200

    ii prototypes

--- src/restart.h
+++ src/restart.h
@@ -7,6 +7,9 @@ enum {BEGIN=FINAL};
 void write_pp(const char *code, const int id, const Particle *pp, const long n);
 void read_pp(const char *code, const int id, Particle *pp, int *n);
 
+void write_ii(const char *code, const int id, const int *pp, const long n);
+void read_ii(const char *code, const int id, int *pp, int *n);
+
 void write_ss(const char *code, const int id, const Solid *ss, const long n);
 void read_ss(const char *code, const int id, Solid *ss, int *n);
 }
