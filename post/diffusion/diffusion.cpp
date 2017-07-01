35fd90f874cc1ec48b24e28added18a378ddfc9e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-01 15:23:23 +0200

    usage

--- post/diffusion/diffusion.cpp
+++ post/diffusion/diffusion.cpp
@@ -1,10 +1,15 @@
 #include <cstdio>
+#include <cstdlib>
 #include "reader.h"
 
 int main(int argc, char **argv) {
 
-    ReadData dpp, dii;
+    if (argc < 4) {
+        fprintf(stderr, "Usage: po.diffCoeff <inpp-*.bop> -- <inii-*.bop>\n");
+        exit(1);
+    }
     
+    ReadData dpp, dii;    
     init(&dpp); init(&dii);
 
     
