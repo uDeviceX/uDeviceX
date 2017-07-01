b43ff72a91a5163d31bf3dfa3fdfd5cb5c681c02
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-01 15:19:50 +0200

    initial diffusion

---
+++ post/diffusion/diffusion.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include "reader.h"
+
+int main(int argc, char **argv) {
+
+    ReadData dpp, dii;
+    
+    init(&dpp); init(&dii);
+
+    
+    
+    finalize(&dpp); finalize(&dii);
+    
+    return 0;
+}
