b58fe7e2651aa58728171af86c66a112d2aae330
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-03 11:03:59 +0200

    take periodic BC into account

--- post/msd/msd.cpp
+++ post/msd/msd.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cmath>
 
 #include "reader.h"
 
@@ -35,6 +36,22 @@ void pp2rr_sorted(const int *ii, const float *fdata, const int n, const int stri
         const float *r = fdata + j * stride;
         for (int c = 0; c < 3; ++c)
         rr[3*i + c] = r[c];
+    }
+}
+
+enum {XS=16, YS=32, ZS=16};
+
+void glbrr(const float *rrp, const int n, /**/ float *rrc, float *ddL) {
+    const float dL[3] = {XS, YS, ZS};
+    
+    for (int i = 0; i < 3*n; ++i) {
+        const int d = i%3;
+        const float dl = rrp[i] - rrc[i];
+        const float sign = dl > 0 ? 1.f : -1.f;
+        if (fabs(dl) > dL[d]/2) {
+            ddL[i] += sign * dL[d];
+            rrc[i] += sign * dL[d];
+        }
     }
 }
 
@@ -75,27 +92,33 @@ int main(int argc, char **argv) {
 
     const int buffsize = max_index(dii0.idata, dii0.n);
 
-    float *rr0 = new float[3*buffsize];
-    float  *rr = new float[3*buffsize];
+    float *rr0 = new float[3*buffsize]; /* initial  positions     */
+    float *rrc = new float[3*buffsize]; /* current  positions     */
+    float *rrp = new float[3*buffsize]; /* previous positions     */
+    float *ddL = new float[3*buffsize]; /* helper for periodic BC */
 
     memset(rr0, 0, 3*buffsize*sizeof(float));
+    memset(ddL, 0, 3*buffsize*sizeof(float));
     pp2rr_sorted(dii0.idata, dpp0.fdata, dpp0.n, dpp0.nvars, /**/ rr0);
+    memcpy(rrp, rr0, 3*buffsize*sizeof(float));
     
     for (int i = 1; i < nin; ++i) {
         init(&dpp);  init(&dii);
         read_data(ffpp[i], &dpp, ffii[i], &dii);
 
-        memset(rr, 0, 3*buffsize*sizeof(float));
-        pp2rr_sorted(dii.idata, dpp.fdata, dpp.n, dpp.nvars, /**/ rr);
+        memset(rrc, 0, 3*buffsize*sizeof(float));
+        pp2rr_sorted(dii.idata, dpp.fdata, dpp.n, dpp.nvars, /**/ rrc);
+        glbrr(rrp, buffsize, /**/ rrc, ddL);
 
-        const float msd = MSD(rr0, rr, buffsize, dpp.n);
+        const float msd = MSD(rr0, rrc, buffsize, dpp.n);
 
         printf("%f\n", msd);
         
         finalize(&dpp);  finalize(&dii);
+        memcpy(rrp, rrc, 3*buffsize*sizeof(float));
     }
 
-    delete[] rr0; delete[] rr;    
+    delete[] rr0; delete[] rrc;    
     finalize(&dpp0); finalize(&dii0);
     return 0;
 }
