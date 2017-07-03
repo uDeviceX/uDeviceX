09c5337b4c1c1c11d02b0d7f5a3c9fdfd8ad7783
Sergey Litvinov <slitvinov@gmail.com>
2017-07-03 23:38:47 +0200

    create pack_first1 function

--- src/dpd/pack.impl.h
+++ src/dpd/pack.impl.h
@@ -18,14 +18,24 @@ void pack_first0() {
   CC(cudaMemcpyToSymbol(phalo::cellpacks, cellpacks,
 			sizeof(cellpacks), 0, H2D));
 }
-  
+
+void pack_first1() {
+  int i;
+  for (i = 0; i < 26; ++i) srccells0[i] = sendhalos[i]->dcellstarts->D;
+  CC(cudaMemcpyToSymbol(phalo::srccells, srccells0, sizeof(srccells0), 0, H2D));
+  for (i = 0; i < 26; ++i) dstcells0[i] = sendhalos[i]->hcellstarts->DP;
+  CC(cudaMemcpyToSymbol(phalo::dstcells, dstcells0, sizeof(dstcells0), 0, H2D));
+  for (i = 0; i < 26; ++i) srccells1[i] = recvhalos[i]->hcellstarts->DP;
+  CC(cudaMemcpyToSymbol(phalo::srccells, srccells1, sizeof(srccells1), sizeof(srccells1), H2D));
+  for (i = 0; i < 26; ++i) dstcells1[i] = recvhalos[i]->dcellstarts->D;
+  CC(cudaMemcpyToSymbol(phalo::dstcells, dstcells1, sizeof(dstcells1), sizeof(dstcells1), H2D));
+}
+
 void pack(Particle *p, int n, int *cellsstart, int *cellscount) {
   if (firstpost) pack_first0();
 
   if (ncells)
-    phalo::
-      count_all<<<k_cnf(ncells)>>>(cellsstart, cellscount, ncells);
-
+    phalo::count_all<<<k_cnf(ncells)>>>(cellsstart, cellscount, ncells);
   phalo::scan_diego<32><<<26, 32 * 32>>>();
 
   if (firstpost)
@@ -36,30 +46,7 @@ void pack(Particle *p, int n, int *cellsstart, int *cellscount) {
     MC(l::m::Waitall(nsendreq, sendreq, statuses));
     MC(l::m::Waitall(26, sendcountreq, statuses));
   }
-  
-  if (firstpost) {
-    {
-      for (int i = 0; i < 26; ++i) srccells0[i] = sendhalos[i]->dcellstarts->D;
-
-      CC(cudaMemcpyToSymbol(phalo::srccells, srccells0, sizeof(srccells0),
-			    0, H2D));
-
-      for (int i = 0; i < 26; ++i)
-	dstcells0[i] = sendhalos[i]->hcellstarts->DP;
-
-      CC(cudaMemcpyToSymbol(phalo::dstcells, dstcells0, sizeof(dstcells0),
-			    0, H2D));
-    }
-
-    {
-      for (int i = 0; i < 26; ++i) srccells1[i] = recvhalos[i]->hcellstarts->DP;
-      CC(cudaMemcpyToSymbol(phalo::srccells, srccells1, sizeof(srccells1),
-			    sizeof(srccells1), H2D));
-      for (int i = 0; i < 26; ++i) dstcells1[i] = recvhalos[i]->dcellstarts->D;
-      CC(cudaMemcpyToSymbol(phalo::dstcells, dstcells1, sizeof(dstcells1),
-			    sizeof(dstcells1), H2D));
-    }
-  }
+  if (firstpost) pack_first1();
 
   if (ncells) phalo::copycells<0><<<k_cnf(ncells)>>>(ncells);
   _pack_all(p, n, firstpost);
