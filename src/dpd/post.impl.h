bdfc9e6282cf51892b18c6911942ac49fd2803d3
Sergey Litvinov <slitvinov@gmail.com>
2017-07-04 00:28:38 +0200

    never fails II

--- src/dpd/post.impl.h
+++ src/dpd/post.impl.h
@@ -7,13 +7,6 @@ void post(Particle *p, int n) {
         for (int i = 0; i < 26; ++i) {
             int nrequired = required_send_bag_size_host[i];
             bool failed_entry = nrequired > sendhalos[i]->dbuf->C;
-
-            if (failed_entry) {
-                sendhalos[i]->dbuf->resize(nrequired);
-                // sendhalos[i].hbuf.resize(nrequired);
-                sendhalos[i]->scattered_entries->resize(nrequired);
-                succeeded = false;
-            }
         }
 
         for (int i = 0; i < 26; ++i) {
