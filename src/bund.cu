3f5d946b6f6b4d077a17a7469198b3682db3cc5e
Sergey Litvinov <slitvinov@gmail.com>
2017-07-04 16:07:18 +0200

    fix pathes in bund

--- src/bund.cu
+++ src/bund.cu
@@ -137,8 +137,8 @@ namespace dev {
   #include "y/dpd/pack.impl.h"
   #include "y/dpd/post.impl.h"
   #include "y/dpd/recv.impl.h"
-  #include "y/dpd/xy.decl.h"              /* temporary interface to dpd/x and dpd/y */
-  #include "y/dpd/xy.impl.h"
+  #include "y/xy.decl.h"              /* temporary interface to dpd/x and dpd/y */
+  #include "y/xy.impl.h"
 #else
   /* old dpd */
   #include "x/phalo.decl.h"
@@ -147,8 +147,8 @@ namespace dev {
   #include "dpd/forces.h"          /* common */
   #include "x/dpd/ini.h"
   #include "x/dpd/remote.impl.h"
-  #include "x/dpd/xy.decl.h"              /* temporary interface to dpd/x and dpd/y */
-  #include "x/dpd/xy.impl.h"
+  #include "x/xy.decl.h"              /* temporary interface to dpd/x and dpd/y */
+  #include "x/xy.impl.h"
 #endif
 
 #include "collision.h"
