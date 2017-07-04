96a011f27b0af33fda6237d4bda6731efba4292d
Sergey Litvinov <slitvinov@gmail.com>
2017-07-04 16:04:37 +0200

    copy to  x/xy and y/xy

---
+++ src/y/xy.impl.h
@@ -0,0 +1,15 @@
+namespace xy { /* temporary interface to dpd/x and dpd/y */
+void ini() { dpd::ini(); }
+void fin() { dpd::fin(); }
+
+void forces(flu::Quants *q, flu::TicketZ *tz, flu::TicketRND *trnd, /**/ Force *ff) {
+  dpd::pack(q->pp, q->n, q->cells->start, q->cells->count);
+  dpd::flocal(tz->zip0, tz->zip1, q->n,
+	      q->cells->start, q->cells->count,
+	      trnd->rnd,
+	      /**/ ff);
+  dpd::post(q->pp, q->n);
+  dpd::recv();
+  dpd::fremote(q->n, /**/ ff);
+}
+}
