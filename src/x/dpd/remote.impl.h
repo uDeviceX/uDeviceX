b987da5ac0923063e9c4dc7a79b4c2c2b9e02d09
Sergey Litvinov <slitvinov@gmail.com>
2017-07-04 12:33:05 +0200

    unused args

--- src/x/dpd/remote.impl.h
+++ src/x/dpd/remote.impl.h
@@ -3,8 +3,7 @@ void flocal(float4 *zip0, ushort4 *zip1, int n, int *start, int *count, /**/
                         Force *ff) {
     if (n > 0)
       flocal0(zip0, zip1, (float*)ff, n,
-	      start, count, 1, XS, YS, ZS, 1. / sqrt(dt),
-	      local_trunk->get_float());
+	      start, count, 1, XS, YS, ZS, local_trunk->get_float());
 }
 
 void fremote(int n, Force *a) {
