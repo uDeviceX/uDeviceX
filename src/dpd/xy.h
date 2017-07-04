721f4a095f4e16cead81f44ecdfae817f7bd8d53
Sergey Litvinov <slitvinov@gmail.com>
2017-07-04 15:48:45 +0200

    add a body of xyz/forces

--- src/dpd/xy.h
+++ src/dpd/xy.h
@@ -1,5 +1,15 @@
 namespace xy { /* temporary interface to dpd/x and dpd/y */
-  void ini() {}
-  void fin() {}
-  void forces() {}
+void ini() {}
+void fin() {}
+
+void forces(flu::Quants *q, flu::TicketZ *tz, flu::TicketRND *trnd, /**/ Force *ff) {
+  dpd::pack(q->pp, q->n, q->cells->start, q->cells->count);
+  dpd::flocal(tz->zip0, tz->zip1, q->n,
+	      q->cells->start, q->cells->count,
+	      trnd->rnd,
+	      /**/ ff);
+  dpd::post(q->pp, q->n);
+  dpd::recv();
+  dpd::fremote(q->n, /**/ ff);
+}
 }
