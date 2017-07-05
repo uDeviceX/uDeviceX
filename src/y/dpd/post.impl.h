05fc1f509d4a3ef6347c5c526c03b63089caebfc
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-05 18:24:35 +0200

    no second chance

--- src/y/dpd/post.impl.h
+++ src/y/dpd/post.impl.h
@@ -33,31 +33,12 @@ void post(MPI_Comm cart, Particle *pp, SendHalo* sendhalos[], int n) {
         nsendreq = 0;
 
         for (int i = 0; i < 26; ++i) {
-            int expected = sendhalos[i]->expected;
-
-            if (expected == 0) continue;
-
             int count = sendhalos[i]->hbuf->S;
 
-            MC(l::m::Isend(sendhalos[i]->hbuf->D, expected, Particle::datatype(),
+            MC(l::m::Isend(sendhalos[i]->hbuf->D, count, Particle::datatype(),
                            dstranks[i], BT_P_DPD + i, cart, sendreq + nsendreq));
 
             ++nsendreq;
-
-            if (count > expected) {
-
-                int difference = count - expected;
-
-                int d[3] = {(i + 2) % 3 - 1, (i / 3 + 2) % 3 - 1, (i / 9 + 2) % 3 - 1};
-                printf("extra message from rank %d to rank %d in the direction of %d "
-                       "%d %d! difference %d, expected is %d\n",
-                       m::rank, dstranks[i], d[0], d[1], d[2], difference, expected);
-
-                MC(l::m::Isend(sendhalos[i]->hbuf->D + expected, difference,
-                               Particle::datatype(), dstranks[i], BT_P2_DPD + i,
-                               cart, sendreq + nsendreq));
-                ++nsendreq;
-            }
         }
     }
 }
