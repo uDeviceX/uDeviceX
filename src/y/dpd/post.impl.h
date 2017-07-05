f85141d976b3caa503bc974e20fec79651abd36c
Sergey Litvinov <slitvinov@gmail.com>
2017-07-05 14:12:59 +0200

    Add check_size

--- src/y/dpd/post.impl.h
+++ src/y/dpd/post.impl.h
@@ -1,4 +1,20 @@
 namespace dpd {
+
+bool check_size(SendHalo* sendhalos[]) {
+  bool succeeded = true;
+  for (int i = 0; i < 26; ++i) {
+    int nrequired = required_send_bag_size_host[i];
+    bool failed_entry = nrequired > sendhalos[i]->dbuf->C;
+    
+    if (failed_entry) {
+      sendhalos[i]->dbuf->resize(nrequired);
+      sendhalos[i]->scattered_entries->resize(nrequired);
+      succeeded = false;
+    }
+  }
+  return succeeded;
+}
+  
 void post(MPI_Comm cart, Particle *pp, SendHalo* sendhalos[], int n) {
   {
     CC(cudaEventSynchronize(evfillall));
