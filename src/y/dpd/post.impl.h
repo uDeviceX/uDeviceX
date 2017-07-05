1e5950b534effa92b7b2f094b933ca5e746bb12a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-05 18:26:16 +0200

    format

--- src/y/dpd/post.impl.h
+++ src/y/dpd/post.impl.h
@@ -18,28 +18,26 @@ void post(MPI_Comm cart, Particle *pp, SendHalo* sendhalos[], int n) {
                     H2H);
     dSync(); /* was CC(cudaStreamSynchronize(downloadstream)); */
 
-    {
-        for (int i = 0, c = 0; i < 26; ++i)
-        if (sendhalos[i]->expected)
-        MC(l::m::Isend(sendhalos[i]->hcellstarts->D,
-                       sendhalos[i]->hcellstarts->S, MPI_INTEGER, dstranks[i],
-                       BT_CS_DPD + i, cart, sendcellsreq + c++));
+    for (int i = 0, c = 0; i < 26; ++i)
+    if (sendhalos[i]->expected)
+    MC(l::m::Isend(sendhalos[i]->hcellstarts->D,
+                   sendhalos[i]->hcellstarts->S, MPI_INTEGER, dstranks[i],
+                   BT_CS_DPD + i, cart, sendcellsreq + c++));
 
-        for (int i = 0, c = 0; i < 26; ++i)
-        if (sendhalos[i]->expected)
-        MC(l::m::Isend(&sendhalos[i]->hbuf->S, 1, MPI_INTEGER, dstranks[i],
-                       BT_C_DPD + i, cart, sendcountreq + c++));
+    for (int i = 0, c = 0; i < 26; ++i)
+    if (sendhalos[i]->expected)
+    MC(l::m::Isend(&sendhalos[i]->hbuf->S, 1, MPI_INTEGER, dstranks[i],
+                   BT_C_DPD + i, cart, sendcountreq + c++));
 
-        nsendreq = 0;
+    nsendreq = 0;
 
-        for (int i = 0; i < 26; ++i) {
-            int count = sendhalos[i]->hbuf->S;
+    for (int i = 0; i < 26; ++i) {
+        int count = sendhalos[i]->hbuf->S;
 
-            MC(l::m::Isend(sendhalos[i]->hbuf->D, count, Particle::datatype(),
-                           dstranks[i], BT_P_DPD + i, cart, sendreq + nsendreq));
+        MC(l::m::Isend(sendhalos[i]->hbuf->D, count, Particle::datatype(),
+                       dstranks[i], BT_P_DPD + i, cart, sendreq + nsendreq));
 
-            ++nsendreq;
-        }
+        ++nsendreq;
     }
 }
 }
