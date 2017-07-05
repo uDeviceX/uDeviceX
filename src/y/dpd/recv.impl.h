7bf7436f4dcc546e339e36cbded64632f3933c38
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-05 18:28:39 +0200

    no second chance for recver as well

--- src/y/dpd/recv.impl.h
+++ src/y/dpd/recv.impl.h
@@ -9,30 +9,16 @@ void wait_recv() {
 void recv(MPI_Comm cart, RecvHalo* recvhalos[]) {
     for (int i = 0; i < 26; ++i) {
         int count = recv_counts[i];
-        int expected = recvhalos[i]->expected;
-        int difference = count - expected;
 
-        if (count <= expected) {
-            recvhalos[i]->hbuf->resize(count);
-            recvhalos[i]->dbuf->resize(count);
-        } else {
-            printf("RANK %d waiting for RECV-extra message: count %d expected %d "
-                   "(difference %d) from rank %d\n",
-                   m::rank, count, expected, difference, dstranks[i]);
-            recvhalos[i]->hbuf->preserve_resize(count);
-            recvhalos[i]->dbuf->resize(count);
-            MPI_Status status;
-            MPI_Recv(recvhalos[i]->hbuf->D + expected, difference,
-                     Particle::datatype(), dstranks[i], BT_P2_DPD + recv_tags[i],
-                     cart, &status);
-        }
+        recvhalos[i]->hbuf->resize(count);
+        recvhalos[i]->dbuf->resize(count);
     }
 
     for (int i = 0; i < 26; ++i)
     CC(cudaMemcpyAsync(recvhalos[i]->dbuf->D, recvhalos[i]->hbuf->D,
                        sizeof(Particle) * recvhalos[i]->hbuf->S,
                        H2D));
-
+    
     for (int i = 0; i < 26; ++i)
     CC(cudaMemcpyAsync(recvhalos[i]->dcellstarts->D,
                        recvhalos[i]->hcellstarts->D,
