696b80bf767e84830bb41ec148b12662a6c7476a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-06 17:20:26 +0200

    rm x and y

--- src/y/xy.decl.h
+++
@@ -1,9 +0,0 @@
-namespace xy { /* temporary interface to dpd/x and dpd/y */
-  l::rnd::d::KISS *interrank_trunks[26];
-  bool interrank_masks[26];
-  MPI_Comm cart;
-  bool first;
-
-  dpd::SendHalo* sendhalos[26];
-  dpd::RecvHalo* recvhalos[26];
-}
