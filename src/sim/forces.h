9065fe63b505ae26d8f25c4b111cc0243d95cf54
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-06 17:07:41 +0200

    forces in sim

--- src/sim/forces.h
+++ src/sim/forces.h
@@ -14,7 +14,23 @@ void forces_rbc() {
 }
 
 void forces_dpd() {
-  xy::forces(&o::q, &o::tz, &o::trnd, /**/ o::ff); /* defined in [xy]/dpd/xy.impl.h */
+    //xy::forces(&o::q, &o::tz, &o::trnd, /**/ o::ff); /* defined in [xy]/dpd/xy.impl.h */
+
+    dpdr::gather_cells(o::q.cells->start, o::q.cells->count, /**/ &o::h::ts);
+    if (o::h::tc.first) {
+        dpdr::post_expected_recv(&o::h::tc, &o::h::tr);
+        o::h::tc.first = false;
+    }
+    dpdr::copy_cells(&o::h::ts);
+    dpdr::pack(o::q.pp, /**/ o::h::ts);
+
+    dpd::flocal(o::tz.zip0, o::tz.zip1, o::q.n, o::q.cells->start, o::q.cells->count, o::trnd.rnd, /**/ o::ff);
+    
+    dpdr::post(&o::h::tc, &o::h::ts);
+    dpdr::wait_recv(&o::h::tc);
+    dpdr::recv(&o::h::tr);
+    dpdr::post_expected_recv(&o::h::tc, &o::h::tr);
+    dpdr::fremote(o::h::trnd, o::h::ts, o::h::tr, /**/ o::ff);
 }
 
 void clear_forces(Force* ff, int n) {
