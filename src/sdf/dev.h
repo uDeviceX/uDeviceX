a2b992cb384cada167e69ca655c3d1d8c9b648ce
Sergey Litvinov <slitvinov@gmail.com>
2017-07-06 11:10:24 +0200

    format/renaming

--- src/sdf/dev.h
+++ src/sdf/dev.h
@@ -31,34 +31,39 @@ struct tex3Dca {
 };
 
 __device__ float sdf(const tex3Dca<float> texsdf, float x, float y, float z) {
-    int L[3] = {XS, YS, ZS};
-    int MARGIN[3] = {XWM, YWM, ZWM};
-    int TE[3] = {XTE, YTE, ZTE};
+    int c;
+    float t;
+    float s000, s001, s010, s100, s101, s011, s110, s111;
+    float s00x, s01x, s10x, s11x;
+    float s0yx, s1yx;
+    float szyx;
 
     float tc[3], lmbd[3], r[3] = {x, y, z};
-    for (int c = 0; c < 3; ++c) {
-        float t =
-            TE[c] * (r[c] + L[c] / 2 + MARGIN[c]) / (L[c] + 2 * MARGIN[c]) - 0.5;
+    int L[3] = {XS, YS, ZS};
+    int M[3] = {XWM, YWM, ZWM}; /* margin */
+    int T[3] = {XTE, YTE, ZTE}; /* texture */
 
+    for (c = 0; c < 3; ++c) {
+        t = T[c] * (r[c] + L[c] / 2 + M[c]) / (L[c] + 2 * M[c]) - 0.5;
         lmbd[c] = t - (int)t;
         tc[c] = (int)t + 0.5;
     }
 #define tex0(ix, iy, iz) (texsdf.fetch(tc[0] + ix, tc[1] + iy, tc[2] + iz))
-    float s000 = tex0(0, 0, 0), s001 = tex0(1, 0, 0), s010 = tex0(0, 1, 0);
-    float s011 = tex0(1, 1, 0), s100 = tex0(0, 0, 1), s101 = tex0(1, 0, 1);
-    float s110 = tex0(0, 1, 1), s111 = tex0(1, 1, 1);
+    s000 = tex0(0, 0, 0), s001 = tex0(1, 0, 0), s010 = tex0(0, 1, 0);
+    s011 = tex0(1, 1, 0), s100 = tex0(0, 0, 1), s101 = tex0(1, 0, 1);
+    s110 = tex0(0, 1, 1), s111 = tex0(1, 1, 1);
 #undef tex0
 
 #define wavrg(A, B, p) A*(1-p) + p*B /* weighted average */
-    float s00x = wavrg(s000, s001, lmbd[0]);
-    float s01x = wavrg(s010, s011, lmbd[0]);
-    float s10x = wavrg(s100, s101, lmbd[0]);
-    float s11x = wavrg(s110, s111, lmbd[0]);
+    s00x = wavrg(s000, s001, lmbd[0]);
+    s01x = wavrg(s010, s011, lmbd[0]);
+    s10x = wavrg(s100, s101, lmbd[0]);
+    s11x = wavrg(s110, s111, lmbd[0]);
 
-    float s0yx = wavrg(s00x, s01x, lmbd[1]);
+    s0yx = wavrg(s00x, s01x, lmbd[1]);
 
-    float s1yx = wavrg(s10x, s11x, lmbd[1]);
-    float szyx = wavrg(s0yx, s1yx, lmbd[2]);
+    s1yx = wavrg(s10x, s11x, lmbd[1]);
+    szyx = wavrg(s0yx, s1yx, lmbd[2]);
 #undef wavrg
     return szyx;
 }
