14d0bad1336b6a63c71403843375976149cd7995
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-06 11:43:25 +0200

    more in ticket s halo

--- src/dpdr/int.h
+++ src/dpdr/int.h
@@ -16,6 +16,9 @@ struct Ticketrnd {
 };
 
 struct TicketShalo {
+    bool first;
+    int ncells;                /* total number of cells in the halo              */
+    
     int27 fragstarts;          /* cumulative sum of number of cells for each fragment */
 
     intp26 str, cnt, cum;      /* see /doc/remote.md                              */
