905815191d2a209a0e30872cc043d0c453d2a591
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-06 11:18:06 +0200

    adding ticketcomm

--- src/dpdr/int.h
+++ src/dpdr/int.h
@@ -1,5 +1,12 @@
-struct TicketCom {
+typedef Sarray<int,  26> int26;
+typedef Sarray<int,  27> int27;
+typedef Sarray<int*, 26> intp26;
+typedef Sarray<Particle*, 26> Particlep26;
 
+struct TicketCom {
+    MPI_Request sendreq[26], sendcellsreq[26], sendcountreq[26];
+    MPI_Request recvreq[26], recvcellsreq[26], recvcountreq[26];
+    int recv_tags[26], recv_counts[26], dstranks[26];
 };
 
 struct Ticketrnd {
