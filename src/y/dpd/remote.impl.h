972a4667c178b57c735a486be4179d253ee0be19
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-06 09:25:46 +0200

    format

--- src/y/dpd/remote.impl.h
+++ src/y/dpd/remote.impl.h
@@ -11,16 +11,16 @@ void upd_bag(SendHalo* sendhalos[]) {
 }
 
 void post_expected_recv(MPI_Comm cart, RecvHalo* recvhalos[]) {
-    for (int i = 0, c = 0; i < 26; ++i) {
-        MC(l::m::Irecv(recvhalos[i]->hbuf->D, recvhalos[i]->expected,
-                       Particle::datatype(), dstranks[i], BT_P_DPD + recv_tags[i],
-                       cart, recvreq + c++));
-    }
+    for (int i = 0, c = 0; i < 26; ++i)
+    MC(l::m::Irecv(recvhalos[i]->hbuf->D, recvhalos[i]->expected,
+                   Particle::datatype(), dstranks[i], BT_P_DPD + recv_tags[i],
+                   cart, recvreq + c++));
+    
     for (int i = 0, c = 0; i < 26; ++i)
     MC(l::m::Irecv(recvhalos[i]->hcellstarts->D,
                    recvhalos[i]->hcellstarts->S, MPI_INTEGER, dstranks[i],
                    BT_CS_DPD + recv_tags[i], cart, recvcellsreq + c++));
-  
+    
     for (int i = 0, c = 0; i < 26; ++i)
     MC(l::m::Irecv(recv_counts + i, 1, MPI_INTEGER, dstranks[i],
                    BT_C_DPD + recv_tags[i], cart, recvcountreq + c++));
