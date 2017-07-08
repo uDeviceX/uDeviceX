457158e09f192c92999a0bf6cd3146a2b36cdfa7
Sergey Litvinov <slitvinov@gmail.com>
2017-07-08 10:50:13 +0200

    use local remove

--- src/sim/generic.h
+++ src/sim/generic.h
@@ -17,7 +17,7 @@ void remove_rbcs() {
   int nc0;
   r::q.nc = sdf::who_stays(w::qsdf, r::q.pp, r::q.n, nc0 = r::q.nc, r::q.nv, /**/ stay);
   r::q.n = r::q.nc * r::q.nv;
-  Cont::remove(r::q.pp, r::q.nv, stay, r::q.nc);
+  remove(r::q.pp, r::q.nv, stay, r::q.nc);
   MSG("%d/%d RBCs survived", r::q.nc, nc0);
 }
 
@@ -27,14 +27,14 @@ void remove_solids() {
   int nip = s::q.ns * s::q.m_dev.nv;
   s::q.ns = sdf::who_stays(w::qsdf, s::q.i_pp, nip, ns0 = s::q.ns, s::q.m_dev.nv, /**/ stay);
   s::q.n  = s::q.ns * s::q.nps;
-  Cont::remove(s::q.pp,       s::q.nps,      stay, s::q.ns);
-  Cont::remove(s::q.pp_hst,   s::q.nps,      stay, s::q.ns);
+  remove(s::q.pp,       s::q.nps,      stay, s::q.ns);
+  remove(s::q.pp_hst,   s::q.nps,      stay, s::q.ns);
 
-  Cont::remove(s::q.ss,       1,           stay, s::q.ns);
-  Cont::remove(s::q.ss_hst,   1,           stay, s::q.ns);
+  remove(s::q.ss,       1,           stay, s::q.ns);
+  remove(s::q.ss_hst,   1,           stay, s::q.ns);
 
-  Cont::remove(s::q.i_pp,     s::q.m_dev.nv, stay, s::q.ns);
-  Cont::remove(s::q.i_pp_hst, s::q.m_hst.nv, stay, s::q.ns);
+  remove(s::q.i_pp,     s::q.m_dev.nv, stay, s::q.ns);
+  remove(s::q.i_pp_hst, s::q.m_hst.nv, stay, s::q.ns);
   MSG("sim.impl: %d/%d Solids survived", s::q.ns, ns0);
 }
 
