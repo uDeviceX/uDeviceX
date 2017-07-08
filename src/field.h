a2a0e9d829ce6da2d25b9cf56a372f86c64466d9
Sergey Litvinov <slitvinov@gmail.com>
2017-07-08 17:38:07 +0200

    minor

--- src/field.h
+++ src/field.h
@@ -1,7 +1,7 @@
 namespace field {
 void ini_dims(const char *path, int N[3], float extent[3]);
 void ini_data(const char *path, const int n, float *grid_data);
-void sample(const float rlo[3], const float dr[3], const int nsize[3], const int N[3], const float *grid_data, float *out);
+void sample(const float org[3], const float spa[3], const int N1[3], const int N0[3], const float *D0, float *D1);
 void scale(int N[3], float s, float *D);
 void dump(const int N[], const float extent[], const float* grid_data);
 }
