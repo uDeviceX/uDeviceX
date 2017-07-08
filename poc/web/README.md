b9ec4bceaa283d62f8519d4d9f86ecad8f2e725e
Sergey Litvinov <slitvinov@gmail.com>
2017-07-08 23:41:50 +0200

    add hello word

--- poc/web/README.md
+++ poc/web/README.md
@@ -1,1 +1,10 @@
 # web interface
+
+## Hello word
+Run
+
+    python -m CGIHTTPServer
+	
+Open in browser
+
+    firefox http://localhost:8000/cgi-bin/hello.sh
---
+++ poc/web/cgi-bin/hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'Content-Type: text/html'
+echo
+echo '<html>'
+echo  '<head><title>Hello from shell</title></head>'
+echo '<body>'
+echo '<h2>Hello from shell</h2>'
+echo '</body></html>'
+
