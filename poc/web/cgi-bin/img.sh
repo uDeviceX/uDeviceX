ee5dda7c084651c98d1b921a27317805d6513995
Sergey Litvinov <slitvinov@gmail.com>
2017-07-08 23:54:44 +0200

    show img

---
+++ poc/web/cgi-bin/img.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'Content-Type: text/html'
+echo
+echo '<html>'
+echo  '<head><title>Hello from shell</title></head>'
+echo '<body>'
+echo '<img src="../img/success.jpg">'
+echo '</body></html>'
