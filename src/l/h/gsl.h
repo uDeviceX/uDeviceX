a23cf02c0f383b7960e80c6892a09eada3d3ed1a
Sergey Litvinov <slitvinov@gmail.com>
2017-07-08 14:34:43 +0200

    add inv3x32

--- src/l/h/gsl.h
+++ src/l/h/gsl.h
@@ -19,6 +19,34 @@ static void inv3x3_s0(double *m0, double *minv0) {
 	gsl_permutation_free(p);
 }
 
+/* inverts symmetric matrix m[6] (see poc/3x3) */
+void inv3x32(float *m, /**/ float *r) {
+  enum {XX, XY, XZ, YY, YZ, ZZ};
+
+  double xx, yy, zz, xy, xz, yz;
+  double yz2, xz2, xy2;
+  double mx, my, mz; /* minors */
+  double d, i; /* determinant and its inverse */
+
+  xx = m[XX]; yy = m[YY]; zz = m[ZZ];
+  xy = m[XY]; xz = m[XZ];
+  yz = m[YZ];
+  yz2 = yz*yz; xz2 = xz*xz; xy2 = xy*xy;
+
+  mx  = yy*zz-yz2;
+  my  = xy*zz-xz*yz;
+  mz  = xy*yz-xz*yy;
+  d   = mz*xz-my*xy+mx*xx;
+  i   = 1/d;
+
+  r[XX] =  mx*i;
+  r[XY] = -my*i;
+  r[XZ] =  mz*i;
+  r[YY] = i*(xx*zz-xz2);
+  r[YZ] = i*(xy*xz-xx*yz);
+  r[ZZ] = i*(xx*yy-xy2);
+}
+
 void inv3x3(float *m0, float *minv0) {
 	double m[DIM*DIM], minv[DIM*DIM];
 	int i;
