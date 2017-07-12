e5df4831750015a59dbaeb66831c0c3ba13041e7
Sergey Litvinov <slitvinov@gmail.com>
2017-07-12 11:13:17 +0200

    minor

--- src/bipsbatch.decl.h
+++ src/bipsbatch.decl.h
@@ -1,12 +1,12 @@
 namespace bipsbatch {
+
 enum HaloType { HALO_BULK = 0, HALO_FACE = 1, HALO_EDGE = 2, HALO_CORNER = 3 };
 
 struct BatchInfo {
     float *xdst;
     float2 *xsrc;
     float seed;
-    int ndst, nsrc, mask, *cellstarts, *scattered_entries, dx, dy, dz, xcells,
-        ycells, zcells;
+    int ndst, nsrc, mask, *cellstarts, *scattered_entries, dx, dy, dz, xcells, ycells, zcells;
     HaloType halotype;
 };
 
