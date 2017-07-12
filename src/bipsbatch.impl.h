b0e7d92c2529646d2891c7bb45ef47c1b64e663d
Sergey Litvinov <slitvinov@gmail.com>
2017-07-12 11:11:49 +0200

    format

--- src/bipsbatch.impl.h
+++ src/bipsbatch.impl.h
@@ -1,25 +1,14 @@
 namespace bipsbatch {
-void interactions(BatchInfo infos[26], /**/ float *acc) {
-
-    CC(cudaMemcpyToSymbolAsync(batchinfos, infos, sizeof(BatchInfo) * 26, 0,
-                               H2D));
-
+void interactions(BatchInfo infos[26], /**/ float *ff) {
+    CC(cudaMemcpyToSymbolAsync(batchinfos, infos, sizeof(BatchInfo) * 26, 0, H2D));
     static unsigned int hstart_padded[27];
-
+    int nt; /* threads */
+    int i;
     hstart_padded[0] = 0;
-    for (int i = 0; i < 26; ++i)
-    hstart_padded[i + 1] =
-        hstart_padded[i] + 16 * (((unsigned int)infos[i].ndst + 15) / 16);
-
-    CC(cudaMemcpyToSymbolAsync(start, hstart_padded, sizeof(hstart_padded), 0,
-                               H2D));
-
-    int nthreads = 2 * hstart_padded[26];
-
+    for (i = 0; i < 26; ++i) hstart_padded[i + 1] = hstart_padded[i] + 16 * (((unsigned int)infos[i].ndst + 15) / 16);
+    CC(cudaMemcpyToSymbolAsync(start, hstart_padded, sizeof(hstart_padded), 0, H2D));
+    nt = 2 * hstart_padded[26];
     dSync();
-
-    if (nthreads)
-    interaction_kernel<<<k_cnf(nthreads)>>> (acc);
+    if (nt) interaction_kernel<<<k_cnf(nt)>>>(ff);
 }
-
 }
