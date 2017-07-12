25080789c00d33a7a40b5b2b951eab0328d6af55
Sergey Litvinov <slitvinov@gmail.com>
2017-07-12 22:59:00 +0200

    minor

--- src/bipsbatch.impl.h
+++ src/bipsbatch.impl.h
@@ -8,13 +8,13 @@ static void get_start(Frag frag[26], /**/ unsigned int start[27]) {
 
 void interactions(Frag frag_hst[26], /**/ float *ff) {
     static unsigned int start_hst[27];
-    int nt; /* threads */
+    int n; /* number of threads */
     get_start(frag_hst, /**/ start_hst);
-    nt = 2 * start_hst[26];
+    n = 2 * start_hst[26];
     
     CC(cudaMemcpyToSymbolAsync(ffrag_dev, frag_hst, sizeof(Frag) * 26, 0, H2D));
     CC(cudaMemcpyToSymbolAsync(start_dev, start_hst, sizeof(start_hst), 0, H2D));
     dSync();
-    if (nt) k_bipsbatch::force<<<k_cnf(nt)>>>(ffrag_dev, start_dev, ff);
+    if (n) k_bipsbatch::force<<<k_cnf(n)>>>(ffrag_dev, start_dev, ff);
 }
 }
