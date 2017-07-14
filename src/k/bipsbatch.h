880fe364ea3a0c47083ca04c67d22082b7abe99a
Sergey Litvinov <slitvinov@gmail.com>
2017-07-14 18:53:03 +0200

    compile p2m

--- src/k/bipsbatch.h
+++ src/k/bipsbatch.h
@@ -65,10 +65,78 @@ static void p2r(const Part p, /**/ float *x, float *y, float *z) {
 
 static __device__ Map p2map(const Frag frag, const Part p) {
     float x, y, z;
+    int org0, org1, org2;
+    uint cnt0, cnt1, cnt2;
+    int count1, count2;
+    int basecid;
+    int xcid, ycid, zcid;
+    int xl, yl, zl; /* low */
+    int xs, ys, zs; /* size */
+    int dx, dy, dz;
+    int row, col, ncols;
+    int* start;
     Map m;
+
     p2r(p, &x, &y, &z);
+    dx = frag.dx; dy = frag.dy; dz = frag.dz;
 
+    basecid = 0; xs = 1; ys = 1; zs = 1;
+    if (dz == 0) {
+        zcid = (int)(z + ZS / 2);
+        zl = max(0, -1 + zcid);
+        zs = min(frag.zcells, zcid + 2) - zl;
+        basecid = zl;
+    }
+    basecid *= frag.ycells;
 
+    if (dy == 0) {
+        ycid = (int)(y + YS / 2);
+        yl = max(0, -1 + ycid);
+        ys = min(frag.ycells, ycid + 2) - yl;
+        basecid += yl;
+    }
+    basecid *= frag.xcells;
+
+    if (dx == 0) {
+        xcid = (int)(x + XS / 2);
+        xl = max(0, -1 + xcid);
+        xs = min(frag.xcells, xcid + 2) - xl;
+        basecid += xl;
+    }
+
+    row = col = ncols = 1;
+    if (frag.type == FACE) {
+        row = dz ? ys : zs;
+        col = dx ? ys : xs;
+        ncols = dx ? frag.ycells : frag.xcells;
+    } else if (frag.type == EDGE)
+        col = max(xs, max(ys, zs));
+
+    start = frag.start + basecid;
+    org0 = __ldg(start);
+    cnt0 = __ldg(start + col) - org0;
+    start += ncols;
+
+    org1   = org2 = 0;
+    count1 = count2 = 0;
+    if (row > 1) {
+        org1   = __ldg(start);
+        count1 = __ldg(start + col) - org1;
+        start += ncols;
+    }
+    if (row > 2) {
+        org2   = __ldg(start);
+        count2 = __ldg(start + col) - org2;
+    }
+    cnt1 = cnt0 + count1;
+    cnt2 = cnt1 + count2;
+
+    org1 -= cnt0;
+    org2 -= cnt1;
+    
+
+    m.org0 = org0; m.org1 = org1; m.org2 = org2;
+    m.cnt0 = cnt0; m.cnt1 = cnt1; m.cnt2 = cnt2;
     return m;
 }
 
