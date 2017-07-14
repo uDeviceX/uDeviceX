206a93a40520cbe15e24b0ea8102c81ca5941f37
Sergey Litvinov <slitvinov@gmail.com>
2017-07-14 18:46:07 +0200

    fix

--- src/k/bipsbatch.h
+++ src/k/bipsbatch.h
@@ -66,7 +66,7 @@ static void p2r(const Part p, /**/ float *x, float *y, float *z) {
 static __device__ Map p2map(const Frag frag, const Part p) {
     float x, y, z;
     Map m;
-    p2r(p, *x, *y, *z);
+    p2r(p, &x, &y, &z);
 
 
     return m;
