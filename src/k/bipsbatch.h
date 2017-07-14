59766f5be9743cbaca824ac4c1077f7a5a62a63f
Sergey Litvinov <slitvinov@gmail.com>
2017-07-14 21:32:30 +0200

    add m2id

--- src/k/bipsbatch.h
+++ src/k/bipsbatch.h
@@ -18,7 +18,17 @@ struct Map { /* helps to find remote particle */
 };
 
 /* true if `i' bigger than the number of remote particles */
-static __device__ int endp(Map m, uint i) { return i >= m.cnt2; }
+static __device__ int endp(const Map m, uint i) { return i >= m.cnt2; }
+
+static __device__ int m2id(const Map m, uint i) {
+    /* return remote particle id */
+    int m1, m2;
+    uint id;
+    m1 = (int)(i >= m.cnt0);
+    m2 = (int)(i >= m.cnt1);
+    id = i + (m2 ? m.org2 : m1 ? m.org1 : m.org0);
+    return id;
+}
 
 static __device__ void force0(const Rnd rnd, float2 *pp, const Map m, Part p) {
     float x, y, z;
@@ -35,9 +45,6 @@ static __device__ void force0(const Rnd rnd, float2 *pp, const Map m, Part p) {
     float seed = rnd.seed;
     float xforce = 0, yforce = 0, zforce = 0;
     for (uint i = threadIdx.x & 1; !endp(m, i); i += 2) {
-        int m1 = (int)(i >= m.cnt0);
-        int m2 = (int)(i >= m.cnt1);
-        uint spid = i + (m2 ? m.org2 : m1 ? m.org1 : m.org0);
 
         float2 s0 = __ldg(pp + 0 + spid * 3);
         float2 s1 = __ldg(pp + 1 + spid * 3);
