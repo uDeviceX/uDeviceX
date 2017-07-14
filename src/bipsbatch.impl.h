639e5121fc7706925a0fac2597d4f8e55d542424
Sergey Litvinov <slitvinov@gmail.com>
2017-07-14 11:42:04 +0200

    not hst in arguments

--- src/bipsbatch.impl.h
+++ src/bipsbatch.impl.h
@@ -6,15 +6,15 @@ static void get_start(Frag frag[26], /**/ unsigned int start[27]) {
     for (i = 0; i < 26; ++i) start[i + 1] = start[i] + 16 * (((unsigned int)frag[i].ndst + 15) / 16);
 }
 
-void interactions(Frag ffrag_hst[26], Rnd rrnd_hst[26], /**/ float *ff) {
-    static unsigned int start_hst[27];
+void interactions(Frag ffrag[26], Rnd rrnd[26], /**/ float *ff) {
+    static unsigned int start[27];
     int n; /* number of threads */
-    get_start(ffrag_hst, /**/ start_hst);
-    n = 2 * start_hst[26];
+    get_start(ffrag, /**/ start);
+    n = 2 * start[26];
     
-    CC(cudaMemcpyToSymbolAsync(k_bipsbatch::start, start_hst, sizeof(start_hst), 0, H2D));
-    CC(cudaMemcpyToSymbolAsync(k_bipsbatch::ffrag, ffrag_hst, sizeof(Frag) * 26, 0, H2D));
-    CC(cudaMemcpyToSymbolAsync(k_bipsbatch::rrnd,   rrnd_hst, sizeof(Rnd) * 26,  0, H2D));
+    CC(cudaMemcpyToSymbolAsync(k_bipsbatch::start, start, sizeof(start), 0, H2D));
+    CC(cudaMemcpyToSymbolAsync(k_bipsbatch::ffrag, ffrag, sizeof(Frag) * 26, 0, H2D));
+    CC(cudaMemcpyToSymbolAsync(k_bipsbatch::rrnd,   rrnd, sizeof(Rnd) * 26,  0, H2D));
 
     dSync();
     if (n) k_bipsbatch::force<<<k_cnf(n)>>>(ff);
