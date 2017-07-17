08e98df194843723255fc87e3ab8ec3d14062bdb
Sergey Litvinov <slitvinov@gmail.com>
2017-07-17 22:59:35 +0200

    use field of map

--- src/k/fsi/bulk.h
+++ src/k/fsi/bulk.h
@@ -30,73 +30,11 @@ static __device__ void bulk0(float2 *pp, int pid, int zplane, int n, float seed,
     p = pp2p(pp, pid);
     x = p.x; y = p.y; z = p.z;
     if (!p2map(zplane, n, x, y, z, /**/ &m)) return;
-    int cnt0, cnt1, cnt2, org0;
-    int org1, org2;
-
-    {
-        enum {
-            XCELLS = XS,
-            YCELLS = YS,
-            ZCELLS = ZS,
-            XOFFSET = XCELLS / 2,
-            YOFFSET = YCELLS / 2,
-            ZOFFSET = ZCELLS / 2
-        };
-
-        const int xcenter = XOFFSET + (int)floorf(x);
-        const int xstart = max(0, xcenter - 1);
-        const int xcount = min(XCELLS, xcenter + 2) - xstart;
-
-        if (xcenter - 1 >= XCELLS || xcenter + 2 <= 0) return;
-
-        const int ycenter = YOFFSET + (int)floorf(y);
-
-        const int zcenter = ZOFFSET + (int)floorf(z);
-        const int zmy = zcenter - 1 + zplane;
-        const bool zvalid = zmy >= 0 && zmy < ZCELLS;
-
-        int count0 = 0, count1 = 0, count2 = 0;
-
-        if (zvalid && ycenter - 1 >= 0 && ycenter - 1 < YCELLS) {
-            const int cid0 = xstart + XCELLS * (ycenter - 1 + YCELLS * zmy);
-            org0 = tex1Dfetch(texCellsStart, cid0);
-            count0 = ((cid0 + xcount == NCELLS)
-                      ? n
-                      : tex1Dfetch(texCellsStart, cid0 + xcount)) -
-                org0;
-        }
-
-        if (zvalid && ycenter >= 0 && ycenter < YCELLS) {
-            const int cid1 = xstart + XCELLS * (ycenter + YCELLS * zmy);
-            org1 = tex1Dfetch(texCellsStart, cid1);
-            count1 = ((cid1 + xcount == NCELLS)
-                      ? n
-                      : tex1Dfetch(texCellsStart, cid1 + xcount)) -
-                org1;
-        }
-
-        if (zvalid && ycenter + 1 >= 0 && ycenter + 1 < YCELLS) {
-            const int cid2 = xstart + XCELLS * (ycenter + 1 + YCELLS * zmy);
-            org2 = tex1Dfetch(texCellsStart, cid2);
-            count2 = ((cid2 + xcount == NCELLS)
-                      ? n
-                      : tex1Dfetch(texCellsStart, cid2 + xcount)) -
-                org2;
-        }
-
-        cnt0 = count0;
-        cnt1 = count0 + count1;
-        cnt2 = cnt1 + count2;
-
-        org1 -= cnt0;
-        org2 -= cnt1;
-    }
-
     float xforce = 0, yforce = 0, zforce = 0;
-    for (int i = 0; i < cnt2; ++i) {
-        const int m1 = (int)(i >= cnt0);
-        const int m2 = (int)(i >= cnt1);
-        const int spid = i + (m2 ? org2 : m1 ? org1 : org0);
+    for (int i = 0; i < m.cnt2; ++i) {
+        const int m1 = (int)(i >= m.cnt0);
+        const int m2 = (int)(i >= m.cnt1);
+        const int spid = i + (m2 ? m.org2 : m1 ? m.org1 : m.org0);
 
         const int sentry = 3 * spid;
         const float2 stmp0 = tex1Dfetch(texSolventParticles, sentry);
