fa87ee1166ae5c62fe3ef449a7751bf70812d253
Sergey Litvinov <slitvinov@gmail.com>
2017-07-17 22:36:16 +0200

    use Pa

--- src/k/fsi/bulk.h
+++ src/k/fsi/bulk.h
@@ -16,6 +16,7 @@ static __device__ void p2rv(const float2 *p, uint i, /**/
      *x = fst(s0);  *y = scn(s0);  *z = fst(s1);
     *vx = scn(s1); *vy = fst(s2); *vz = scn(s2);
 }
+
 static __device__ Pa pp2p(float2 *pp, int i) {
     Pa p;
     p2rv(pp, i, /**/ &p.x, &p.y, &p.z,   &p.vx, &p.vy, &p.vz);
@@ -24,10 +25,9 @@ static __device__ Pa pp2p(float2 *pp, int i) {
 
 static __device__ void bulk0(float2 *pp, int pid, int zplane, int n, float seed, float *ff0, float *ff1) {
     Pa p;
+    float x, y, z;
     p = pp2p(pp, pid);
-    const float2 dst0 = __ldg(pp + 3 * pid + 0);
-    const float2 dst1 = __ldg(pp + 3 * pid + 1);
-    const float2 dst2 = __ldg(pp + 3 * pid + 2);
+    x = p.x; y = p.y; z = p.z;
     int scan1, scan2, ncandidates, spidbase;
     int deltaspid1, deltaspid2;
 
@@ -41,15 +41,15 @@ static __device__ void bulk0(float2 *pp, int pid, int zplane, int n, float seed,
             ZOFFSET = ZCELLS / 2
         };
 
-        const int xcenter = XOFFSET + (int)floorf(dst0.x);
+        const int xcenter = XOFFSET + (int)floorf(x);
         const int xstart = max(0, xcenter - 1);
         const int xcount = min(XCELLS, xcenter + 2) - xstart;
 
         if (xcenter - 1 >= XCELLS || xcenter + 2 <= 0) return;
 
-        const int ycenter = YOFFSET + (int)floorf(dst0.y);
+        const int ycenter = YOFFSET + (int)floorf(y);
 
-        const int zcenter = ZOFFSET + (int)floorf(dst1.x);
+        const int zcenter = ZOFFSET + (int)floorf(z);
         const int zmy = zcenter - 1 + zplane;
         const bool zvalid = zmy >= 0 && zmy < ZCELLS;
 
@@ -104,10 +104,8 @@ static __device__ void bulk0(float2 *pp, int pid, int zplane, int n, float seed,
         const float myrandnr = l::rnd::d::mean0var1ii(seed, pid, spid);
 
         // check for particle types and compute the DPD force
-        float3 pos1 = make_float3(dst0.x, dst0.y, dst1.x),
-            pos2 = make_float3(stmp0.x, stmp0.y, stmp1.x);
-        float3 vel1 = make_float3(dst1.y, dst2.x, dst2.y),
-            vel2 = make_float3(stmp1.y, stmp2.x, stmp2.y);
+        float3 pos1 = make_float3(x, y, z), pos2 = make_float3(stmp0.x, stmp0.y, stmp1.x);
+        float3 vel1 = make_float3(p.vx, p.vy, p.vz), vel2 = make_float3(stmp1.y, stmp2.x, stmp2.y);
 
         const float3 strength = force(SOLID_TYPE, SOLVENT_TYPE, pos1, pos2,
                                                          vel1, vel2, myrandnr);
