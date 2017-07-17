5ffbad7bb9a2fc39bec11c41621e9287c522bbf8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-17 16:22:05 +0200

    add buf.h

---
+++ src/odstr/buf.h
@@ -0,0 +1,31 @@
+/* pinned buffers (array of pinned buffers) */ 
+template <typename T, int N=27>
+struct Pbufs {
+    T **dev;   /* data on device  */
+    T *dp[N];  /* device pointers */
+    T *hst[N]; /* data on host    */
+};
+
+template <typename T, int N>
+void alloc_pinned(const int i, const int sz, /**/ Pbufs<T, N> *b) {
+    if (sz){
+        CC(cudaHostAlloc(&b->hst[i], sizeof(T) * sz, cudaHostAllocMapped));
+        CC(cudaHostGetDevicePointer(&b->dp[i], b->hst[i], 0));
+    } else {
+        b->hst[i] = NULL;
+    }
+}
+
+template <typename T, int N>
+void alloc_dev(/**/ Pbufs<T, N> *b) {
+    CC(cudaMalloc(&b->dev, SZ_PTR_ARR(b->dp)));
+    CC(cudaMemcpy(b->dev, b->dp, sizeof(b->dp), H2D));
+}
+
+template <typename T, int N>
+void free(Pbufs<T, N> *b) {
+    for (int i = 0; i < N; ++i) {
+        if (b->dp[i] != NULL) CC(cudaFreeHost(b->hst[i]));
+    }
+    CC(cudaFree(b->dev));
+}
