d278b376d368f5fa75b327a0a33fe2a76fa2f2b6
Sergey Litvinov <slitvinov@gmail.com>
2017-07-18 11:01:17 +0200

    minor

--- src/k/fsi/bulk.h
+++ src/k/fsi/bulk.h
@@ -48,9 +48,10 @@ static __device__ float random(uint lid, uint rid, float seed) {
 
 static __device__ void pair(const Pa l, const Pa r, float rnd, /**/ float *fx, float *fy, float *fz) {
     /* pair force ; l, r: local and remote */
-    float3 r1 = make_float3( l.x,  l.y,  l.z), r2 = make_float3( r.x,  r.y,  r.z);
-    float3 v1 = make_float3(l.vx, l.vy, l.vz), v2 = make_float3(r.vx, r.vy, r.vz);
-    float3  f = force(SOLVENT_TYPE, SOLVENT_TYPE, r1, r2, v1, v2, rnd);
+    float3 r1, r2, v1, v2, f;
+    r1 = make_float3( l.x,  l.y,  l.z); r2 = make_float3( r.x,  r.y,  r.z);
+    v1 = make_float3(l.vx, l.vy, l.vz); v2 = make_float3(r.vx, r.vy, r.vz);
+    f = force(SOLVENT_TYPE, SOLVENT_TYPE, r1, r2, v1, v2, rnd);
     *fx = f.x; *fy = f.y; *fz = f.z;
 }
 
