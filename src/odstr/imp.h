4ed001145abbabe108bb2acd1b196ce8ea9319d3
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-18 10:16:18 +0200

    // TODO rm this

--- src/odstr/imp.h
+++ src/odstr/imp.h
@@ -1,8 +1,10 @@
+// TODO rm this
 void Distr::waitall(MPI_Request *reqs) {
     MPI_Status statuses[128]; /* big number */
     l::m::Waitall(26, reqs, statuses) ;
 }
 
+// TODO rm this
 void Distr::post_recv(MPI_Comm cart, int rank[],
                       MPI_Request *size_req, MPI_Request *mesg_req) {
     for(int i = 1, c = 0; i < 27; ++i)
@@ -14,30 +16,36 @@ void Distr::post_recv(MPI_Comm cart, int rank[],
                 BT_P_ODSTR + r.tags[i], cart, mesg_req + c++);
 }
 
+// TODO rm this
 void Distr::post_recv_ii(MPI_Comm cart, int rank[], MPI_Request *ii_req) {
     for(int i = 1, c = 0; i < 27; ++i)
     l::m::Irecv(r.ii.hst[i], MAX_PART_NUM, MPI_INT, rank[i],
                 BT_I_ODSTR + r.tags[i], cart, ii_req + c++);
 }
 
+// TODO rm this
 void Distr::halo(Particle *pp, int n) {
     CC(cudaMemset(s.size_dev, 0,  27*sizeof(s.size_dev[0])));
     dev::halo<<<k_cnf(n)>>>(pp, n, /**/ s.iidx, s.size_dev);
 }
 
+// TODO rm this
 void Distr::scan(int n) {
     dev::scan<<<1, 32>>>(n, s.size_dev, /**/ s.strt, s.size_pin->DP);
     dSync();
 }
 
+// TODO rm this
 void Distr::pack_pp(const Particle *pp, int n) {
     dev::pack<float2, 3> <<<k_cnf(3*n)>>>((float2*)pp, s.iidx, s.strt, /**/ s.pp.dev);
 }
 
+// TODO rm this
 void Distr::pack_ii(const int *ii, int n) {
     dev::pack<int, 1> <<<k_cnf(n)>>>(ii, s.iidx, s.strt, /**/ s.ii.dev);
 }
 
+// TODO rm this
 int Distr::send_sz(MPI_Comm cart, int rank[], MPI_Request *req) {
     for(int i = 0; i < 27; ++i) s.size[i] = s.size_pin->D[i];
     for(int i = 1, cnt = 0; i < 27; ++i)
@@ -46,18 +54,21 @@ int Distr::send_sz(MPI_Comm cart, int rank[], MPI_Request *req) {
     return s.size[0]; /* `n' bulk */
 }
 
+// TODO rm this
 void Distr::send_pp(MPI_Comm cart, int rank[], MPI_Request *req) {
     for(int i = 1, cnt = 0; i < 27; ++i)
     l::m::Isend(s.pp.hst[i], s.size[i] * 6, MPI_FLOAT, rank[i],
                 BT_P_ODSTR + i, cart, &req[cnt++]);
 }
 
+// TODO rm this
 void Distr::send_ii(MPI_Comm cart, int rank[], MPI_Request *req) {
     for(int i = 1, cnt = 0; i < 27; ++i)
     l::m::Isend(s.ii.hst[i], s.size[i], MPI_INT, rank[i],
                 BT_I_ODSTR + i, cart, &req[cnt++]);
 }
 
+// TODO rm this
 void Distr::recv_count(int *nhalo) {
     int i;
     static int size[27], strt[28];
@@ -69,18 +80,22 @@ void Distr::recv_count(int *nhalo) {
     *nhalo = strt[27];
 }
 
+// TODO rm this
 void Distr::unpack_pp(int n, /*o*/ Particle *pp_re) {
     dev::unpack<float2, 3> <<<k_cnf(3*n)>>> (r.pp.dev, r.strt, /**/ (float2*) pp_re);
 }
 
+// TODO rm this
 void Distr::unpack_ii(int n, /*o*/ int *ii_re) {
     dev::unpack<int, 1> <<<k_cnf(n)>>> (r.ii.dev, r.strt, /**/ ii_re);
 }
 
+// TODO rm this
 void Distr::subindex_remote(int n, /*io*/ Particle *pp_re, int *counts, /**/ uchar4 *subi) {
     dev::subindex_remote <<<k_cnf(n)>>> (n, r.strt, /*io*/ (float2*) pp_re, counts, /**/ subi);
 }
 
+// TODO rm this
 void Distr::cancel_recv(MPI_Request *size_req, MPI_Request *mesg_req) {
     for(int i = 0; i < 26; ++i) l::m::Cancel(size_req + i) ;
     for(int i = 0; i < 26; ++i) l::m::Cancel(mesg_req + i) ;
