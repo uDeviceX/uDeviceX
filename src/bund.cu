b0bdc97d53b06bb17896353d45903c05c58e4d26
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-18 14:11:17 +0200

    clean bund

--- src/bund.cu
+++ src/bund.cu
@@ -155,10 +155,7 @@ namespace dev {
 #include "bbhalo.decl.h"
 #include "bbhalo.impl.h"
 
-// #include "dump.decl.h"
-// #include "dump.impl.h"
 #include "dump.h"
-
 #include "l/ply.h"
 
 namespace rig {
