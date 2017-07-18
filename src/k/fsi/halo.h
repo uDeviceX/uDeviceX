3b7f6aad7a71c303003d104e5699a3dc0fd94324
Sergey Litvinov <slitvinov@gmail.com>
2017-07-18 18:20:33 +0200

    useless nesting level

--- src/k/fsi/halo.h
+++ src/k/fsi/halo.h
@@ -20,24 +20,17 @@ __global__ void halo(int n0, int n1, float seed, float *ff1) {
     float2 dst0, dst1, dst2;
     float *dst = NULL;
 
-    {
-        const int code = get_hid(packstarts_padded, localbase);
-        const int unpackbase = localbase - packstarts_padded[code];
+    const int code = get_hid(packstarts_padded, localbase);
+    const int unpackbase = localbase - packstarts_padded[code];
 
-        nunpack = min(32, packcount[code] - unpackbase);
+    nunpack = min(32, packcount[code] - unpackbase);
+    if (nunpack == 0) return;
 
-        if (nunpack == 0) return;
-
-        k_common::read_AOS6f((float2 *)(packstates[code] + unpackbase), nunpack, dst0, dst1,
-                             dst2);
-
-        dst = (float *)(packresults[code] + unpackbase);
-    }
+    k_common::read_AOS6f((float2 *)(packstates[code] + unpackbase), nunpack, dst0, dst1, dst2);
+    dst = (float *)(packresults[code] + unpackbase);
 
     float xforce = 0, yforce = 0, zforce = 0;
-
     const int nzplanes = laneid < nunpack ? 3 : 0;
-
     for (int zplane = 0; zplane < nzplanes; ++zplane) {
         int scan1, scan2, ncandidates, spidbase;
         int deltaspid1, deltaspid2;
