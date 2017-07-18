10f3278fc58e36fe10b06ce4d5a48d44a9c89a04
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-18 11:52:11 +0200

    split: unpack, gather ii

--- src/odstr/int.h
+++ src/odstr/int.h
@@ -15,6 +15,7 @@ struct TicketU { /* unpack ticket */
     uchar4 *subi_re;           /* remote subindices */
     Particle *pp_re;           /* remote particles  */
     int *ii_re;                /* remote ids        */
+    uint *iidx;                /* scatter indices   */
 };
 
 struct Work {
@@ -22,8 +23,8 @@ struct Work {
     uchar4 *subi_re;           /* remote subindices */
     Particle *pp_re;           /* remote particles  */
     int *ii_re;                /* remote ids        */
-
-    uint*iidx;              /* scatter indices   */
+    uint *iidx;                /* scatter indices   */
+    
     unsigned char *count_zip;
 };
 
@@ -180,41 +181,16 @@ void gather_pp(flu::Quants *q, TicketD *td, TicketU *tu, flu::TicketZ *tz, Work 
 }
 
 void unpack_ii(flu::Quants *q, TicketD *td, flu::TicketZ *tz, Work *w) {
-    const int nhalo = td->nhalo, nbulk = td->nbulk;
-    
+    const int nhalo = td->nhalo;
+    if (nhalo) sub::unpack_ii(nhalo, /**/ &td->r, w->ii_re);    
+}
+
+void gather_ii(flu::Quants *q, Work *w) {
     int n = q->n;
-    int *start = q->cells->start;
-    int *count = q->cells->count;
+    int *ii = q->ii, *ii0 = q->ii0;
 
-    Particle *pp = q->pp, *pp0 = q->pp0;
-    int      *ii = q->ii, *ii0 = q->ii0;
-    
-    if (nhalo) {
-        sub::unpack_pp(nhalo, /**/ &td->r, w->pp_re);
-        if (global_ids) sub::unpack_ii(nhalo, /**/ &td->r, w->ii_re);
-        sub::subindex_remote(nhalo, &td->r, /*io*/ w->pp_re, count, /**/ w->subi_re);
-    }
-    
-    k_common::compress_counts<<<k_cnf(XS*YS*ZS)>>>(XS*YS*ZS, (int4*)count, /**/ (uchar4*)w->count_zip);
-    l::scan::d::scan(w->count_zip, XS*YS*ZS, /**/ (uint*)start);
-
-    if (n)
-        sub::dev::scatter<<<k_cnf(n)>>>(false, td->subi_lo,  n, start, /**/ w->iidx);
-
-    if (nhalo)
-        sub::dev::scatter<<<k_cnf(nhalo)>>>(true, w->subi_re, nhalo, start, /**/ w->iidx);
-    n = nbulk + nhalo;
-    if (n) {
-        sub::dev::gather_pp<<<k_cnf(n)>>>((float2*)pp, (float2*)w->pp_re, n, w->iidx,
-                                          /**/ (float2*)pp0, tz->zip0, tz->zip1);
-        if (global_ids) sub::dev::gather_id<<<k_cnf(n)>>>(ii, w->ii_re, n, w->iidx, /**/ ii0);
-    }
-
-    q->n = n;
+    if (n) sub::dev::gather_id<<<k_cnf(n)>>>(ii, w->ii_re, n, w->iidx, /**/ ii0);
 
     /* swap */
-    q->pp = pp0; q->pp0 = pp; 
-    if (global_ids) {
-        q->ii = ii0; q->ii0 = ii;
-    }    
+    q->ii = ii0; q->ii0 = ii;
 }
