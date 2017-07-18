3ebc68fb78fd9ee0582f3d6cec56466bf030eeef
Sergey Litvinov <slitvinov@gmail.com>
2017-07-18 09:01:40 +0200

    add pair()

--- src/k/fsi/bulk.h
+++ src/k/fsi/bulk.h
@@ -42,6 +42,14 @@ static __device__ Pa tex2p(int i) {
     return p;
 }
 
+static __device__ void pair(const Pa l, const Pa r, float rnd, /**/ float *fx, float *fy, float *fz) {
+    /* pair force ; l, r: local and remote */
+    float3 r1 = make_float3( l.x,  l.y,  l.z), r2 = make_float3( r.x,  r.y,  r.z);
+    float3 v1 = make_float3(l.vx, l.vy, l.vz), v2 = make_float3(r.vx, r.vy, r.vz);
+    float3  f = force(SOLVENT_TYPE, SOLVENT_TYPE, r1, r2, v1, v2, rnd);
+    *fx = f.x; *fy = f.y; *fz = f.z;
+}
+
 static __device__ void bulk0(float2 *pp, int pid, int zplane, int n, float seed, float *ff0, float *ff1) {
     Map m;
     Pa l, r; /* "local" and "remote" particles */
