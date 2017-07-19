2c94ead8505823a954f0179aad03311bc50f112b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-19 17:40:42 +0200

    add cancel and wait req

--- src/mcomm/imp.h
+++ src/mcomm/imp.h
@@ -1,5 +1,17 @@
-
 /* Structure containing all kinds of requests a fragment can have */
 struct Reqs {
     MPI_Request pp[26], counts[26];
 };
+
+void cancel_req(Reqs *r) {
+    for (int i = 0; i < 26; ++i) {
+        MC(MPI_Cancel(r->pp     + i));
+        MC(MPI_Cancel(r->counts + i));
+    }
+}
+
+void wait_req(Reqs *r) {
+    MPI_Status ss[26];
+    MC(l::m::Waitall(26, r->pp,     ss));
+    MC(l::m::Waitall(26, r->counts, ss));
+}
