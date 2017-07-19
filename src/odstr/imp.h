fbdcbe123e20e6c0b1290e462ceb7a06a9f6dee8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-19 15:17:56 +0200

    bt_part/btp

--- src/odstr/imp.h
+++ src/odstr/imp.h
@@ -3,7 +3,7 @@ void waitall(MPI_Request *reqs) {
     l::m::Waitall(26, reqs, statuses) ;
 }
 
-void post_recv(const MPI_Comm cart, const int rank[], const int btc, const int bt_part,
+void post_recv(const MPI_Comm cart, const int rank[], const int btc, const int btp,
                MPI_Request *size_req, MPI_Request *mesg_req, Recv *r) {
     for(int i = 1, c = 0; i < 27; ++i)
     l::m::Irecv(r->size + i, 1, MPI_INTEGER, rank[i],
@@ -11,7 +11,7 @@ void post_recv(const MPI_Comm cart, const int rank[], const int btc, const int b
 
     for(int i = 1, c = 0; i < 27; ++i)
     l::m::Irecv(r->pp.hst[i], MAX_PART_NUM, MPI_FLOAT, rank[i],
-                bt_part + r->tags[i], cart, mesg_req + c++);
+                btp + r->tags[i], cart, mesg_req + c++);
 }
 
 void post_recv_ii(const MPI_Comm cart, const int rank[], const int tags[], const int bt, /**/ MPI_Request *ii_req, Pbufs<int> *rii) {
@@ -46,10 +46,10 @@ int send_sz(MPI_Comm cart, const int rank[], const int btc, /**/ Send *s, MPI_Re
     return s->size[0]; /* `n' bulk */
 }
 
-void send_pp(MPI_Comm cart, const int rank[], const int bt_part, /**/ Send *s, MPI_Request *req) {
+void send_pp(MPI_Comm cart, const int rank[], const int btp, /**/ Send *s, MPI_Request *req) {
     for(int i = 1, cnt = 0; i < 27; ++i)
     l::m::Isend(s->pp.hst[i], s->size[i] * 6, MPI_FLOAT, rank[i],
-                bt_part + i, cart, &req[cnt++]);
+                btp + i, cart, &req[cnt++]);
 }
 
 void send_ii(MPI_Comm cart, const int rank[], const int size[], const int bt, /**/ Pbufs<int> *sii, MPI_Request *req) {
