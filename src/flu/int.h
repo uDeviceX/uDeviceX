b39028378f21f3d89586ed244e5164581f53c12b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-19 10:49:29 +0200

    rm ii in flu::quants

--- src/flu/int.h
+++ src/flu/int.h
@@ -1,10 +1,8 @@
 struct Quants {
     Particle *pp, *pp0; /* particles on device  */
-    int      *ii, *ii0; /* global ids on device */
     int       n;        /* particle number      */
     Clist *cells;       /* cell lists           */
     Particle *pp_hst;   /* particles on host    */
-    int *ii_hst;        /* global ids on host   */
 }; 
 
 struct QuantsI {
@@ -24,20 +22,12 @@ struct TicketRND { /* random */
 void alloc_quants(Quants *q) {
     q->n = 0;
     mpDeviceMalloc(&q->pp); mpDeviceMalloc(&q->pp0);
-    if (global_ids) {
-        mpDeviceMalloc(&q->ii); mpDeviceMalloc(&q->ii0);
-        q->ii_hst = new int[MAX_PART_NUM];
-    }
     q->cells = new Clist(XS, YS, ZS);
     q->pp_hst = new Particle[MAX_PART_NUM];
 }
 
 void free_quants(Quants *q) {
     CC(cudaFree(q->pp)); CC(cudaFree(q->pp0));
-    if (global_ids) {
-        CC(cudaFree(q->ii)); CC(cudaFree(q->ii0));
-        delete[] q->ii_hst;
-    }
     delete q->cells;
     delete[] q->pp_hst;
 }
@@ -82,7 +72,6 @@ void free_ticketRND(/**/ TicketRND *t) {
 
 void gen_quants(Quants *q) {
     q->n = sub::gen(q->pp, q->pp_hst);
-    if (global_ids) sub::ii_gen(q->n, q->ii, q->ii_hst);
 }
 
 void gen_ids(const int n, QuantsI *q) {
@@ -91,7 +80,6 @@ void gen_ids(const int n, QuantsI *q) {
 
 void strt_quants(const int id, Quants *q) {
     q->n = sub::strt(id, /**/ q->pp, /* w */ q->pp_hst);
-    if (global_ids) sub::strt_ii(id, /**/ q->ii, /* w */ q->ii_hst);
 }
 
 void strt_ids(const int id, QuantsI *q) {
@@ -100,7 +88,6 @@ void strt_ids(const int id, QuantsI *q) {
 
 void strt_dump(const int id, const Quants q) {
     sub::strt_dump(id, q.n, q.pp, /* w */ q.pp_hst);
-    if (global_ids) sub::strt_dump_ii(id, q.n, q.ii, /* w */ q.ii_hst);
 }
 
 void strt_dump_ids(const int id, const QuantsI q, const int n) {
