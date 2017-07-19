229922bc1b89bf3309c41c999e27ceec47ab0018
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-19 14:15:09 +0200

    distribute tags

--- src/sim/step.h
+++ src/sim/step.h
@@ -3,24 +3,30 @@ void step(float driving_force0, bool wall0, int it) {
     assert(r::q.n <= MAX_PART_NUM);
 
     odstr::post_recv_pp(/**/ &o::td);
-    if (global_ids) odstr::post_recv_ii(&o::td, /**/ &o::ti);
-
+    if (global_ids)    odstr::post_recv_ii(&o::td, /**/ &o::ti);
+    if (multi_solvent) odstr::post_recv_ii(&o::td, /**/ &o::tt);
+    
     odstr::pack_pp(&o::q, /**/ &o::td);
-    if (global_ids) odstr::pack_ii(o::q.n, &o::qi, &o::td, /**/ &o::ti);
+    if (global_ids)    odstr::pack_ii(o::q.n, &o::qi, &o::td, /**/ &o::ti);
+    if (multi_solvent) odstr::pack_ii(o::q.n, &o::qt, &o::td, /**/ &o::tt);
 
     odstr::send_pp(/**/ &o::td);
-    if (global_ids) odstr::send_ii(&o::td, /**/ &o::ti);
-
+    if (global_ids)    odstr::send_ii(&o::td, /**/ &o::ti);
+    if (multi_solvent) odstr::send_ii(&o::td, /**/ &o::tt);
+    
     odstr::bulk(/**/ &o::q, &o::td);
 
     odstr::recv_pp(/**/ &o::td);
-    if (global_ids)odstr::recv_ii(/**/ &o::ti);
-
+    if (global_ids)    odstr::recv_ii(/**/ &o::ti);
+    if (multi_solvent) odstr::recv_ii(/**/ &o::tt);
+    
     odstr::unpack_pp(&o::td, /**/ &o::q, &o::tu, /*w*/ &o::w);
-    if (global_ids) odstr::unpack_ii(&o::td, &o::ti, /**/ &o::tui);
-
+    if (global_ids)    odstr::unpack_ii(&o::td, &o::ti, /**/ &o::tui);
+    if (multi_solvent) odstr::unpack_ii(&o::td, &o::tt, /**/ &o::tut);
+    
     odstr::gather_pp(&o::td, /**/ &o::q, &o::tu, &o::tz);
-    if (global_ids) odstr::gather_ii(o::q.n, &o::tu, &o::tui, /**/ &o::qi);
+    if (global_ids)    odstr::gather_ii(o::q.n, &o::tu, &o::tui, /**/ &o::qi);
+    if (multi_solvent) odstr::gather_ii(o::q.n, &o::tu, &o::tut, /**/ &o::qt);
     
     if (solids0) distr_solid();
     if (rbcs)    distr_rbc();
