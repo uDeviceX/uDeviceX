d7ff0657314eaf2db03570f2aea20ae6a75010dd
Sergey Litvinov <slitvinov@gmail.com>
2017-07-19 17:05:23 +0200

    create fsi/type

--- src/k/fsi/common.h
+++ src/k/fsi/common.h
@@ -1,4 +1,25 @@
 namespace k_fsi {
 static __device__ float fst(float2 p) { return p.x; }
 static __device__ float scn(float2 p) { return p.y; }
+
+static __device__ void tex2rv(int i,
+                              float  *x, float  *y, float  *z,
+                              float *vx, float *vy, float *vz) {
+    float2 s0, s1, s2;
+    i *= 3;
+    s0 = tex1Dfetch(texSolventParticles, i++);
+    s1 = tex1Dfetch(texSolventParticles, i++);
+    s2 = tex1Dfetch(texSolventParticles, i++);
+
+     *x = fst(s0);  *y = scn(s0);  *z = fst(s1);
+    *vx = scn(s1); *vy = fst(s2); *vz = scn(s2);
+}
+
+static __device__ Pa tex2p(int i) {
+    /* texture to particle */
+    Pa p;
+    tex2rv(i, /**/ &p.x, &p.y, &p.z,   &p.vx, &p.vy, &p.vz);
+    return p;
+}
+
 }
---
+++ src/k/fsi/type.h
@@ -0,0 +1,6 @@
+namespace k_fsi {
+struct Pa { /* local particle */
+    float x, y, z;
+    float vx, vy, vz;
+};
+}
