ae017cff1c8953531f3b439e661463f2d0df6b0b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-20 15:37:48 +0200

    simplify buf.h

--- src/dpdr/buf.h
+++ src/dpdr/buf.h
@@ -3,18 +3,7 @@ typedef Sarray<int,  27> int27;
 typedef Sarray<int*, 26> intp26;
 typedef Sarray<Particle*, 26> Particlep26;
 
-struct Sbufs {
-    intp26 str, cnt, cum;      /* cellstarts for each fragment (frag coords)      */
-    Particlep26 pp;            /* buffer of particles for each fragment           */
-    intp26 ii;                 /* scattered indices                               */
-    
-    /* pinned buffers */
-    Particlep26 ppdev, pphst;  /* pinned memory for transfering particles         */
-    intp26 cumdev, cumhst;     /* pinned memory for transfering local cum sum     */
-};
-
-struct Rbufs {
-    
+struct Bbufs { /* basic buffer : common to Send and Recv */
     intp26 cum;                /* cellstarts for each fragment (frag coords)      */
     Particlep26 pp;            /* buffer of particles for each fragment           */
 
@@ -23,12 +12,15 @@ struct Rbufs {
     intp26 cumdev, cumhst;     /* pinned memory for transfering local cum sum     */
 };
 
-static void alloc_Sbuf_frag(const int i, const int est, const int nfragcells, /**/ Sbufs *b) {
-    CC(cudaMalloc(&b->str.d[i], (nfragcells + 1) * sizeof(int)));
-    CC(cudaMalloc(&b->cnt.d[i], (nfragcells + 1) * sizeof(int)));
-    CC(cudaMalloc(&b->cum.d[i], (nfragcells + 1) * sizeof(int)));
+struct Sbufs : Bbufs {
+    intp26 str, cnt;           /* cell starts and counts for each fragment (bulk coords) */
+    intp26 ii;                 /* scattered indices                                      */
+};
 
-    CC(cudaMalloc(&b->ii.d[i], est * sizeof(int)));
+struct Rbufs : Bbufs {};
+
+static void alloc_Bbuf_frag(const int i, const int est, const int nfragcells, /**/ Bbufs *b) {
+    CC(cudaMalloc(&b->cum.d[i], (nfragcells + 1) * sizeof(int)));
     CC(cudaMalloc(&b->pp.d[i], est * sizeof(Particle)));
 
     CC(cudaHostAlloc(&b->pphst.d[i], est * sizeof(Particle), cudaHostAllocMapped));
@@ -36,18 +28,28 @@ static void alloc_Sbuf_frag(const int i, const int est, const int nfragcells, /*
 
     CC(cudaHostAlloc(&b->cumhst.d[i], est * sizeof(int), cudaHostAllocMapped));
     CC(cudaHostGetDevicePointer(&b->cumdev.d[i], b->cumhst.d[i], 0));
-};
+}
 
-static void free_Sbuf_frag(const int i, /**/ Sbufs *b) {
-    CC(cudaFree(b->str.d[i]));
-    CC(cudaFree(b->cnt.d[i]));
+static void free_Bbuf_frag(const int i, /**/ Bbufs *b) {
     CC(cudaFree(b->cum.d[i]));
-
-    CC(cudaFree(b->ii.d[i]));
     CC(cudaFree(b->pp.d[i]));
         
     CC(cudaFreeHost(b->cumhst.d[i]));
     CC(cudaFreeHost(b->pphst.d[i]));
+}
+
+static void alloc_Sbuf_frag(const int i, const int est, const int nfragcells, /**/ Sbufs *b) {
+    alloc_Bbuf_frag(i, est, nfragcells, /**/ b);
+    CC(cudaMalloc(&b->str.d[i], (nfragcells + 1) * sizeof(int)));
+    CC(cudaMalloc(&b->cnt.d[i], (nfragcells + 1) * sizeof(int)));
+    CC(cudaMalloc(&b->ii.d[i], est * sizeof(int)));
+};
+
+static void free_Sbuf_frag(const int i, /**/ Sbufs *b) {
+    free_Bbuf_frag(i, /**/ b);
+    CC(cudaFree(b->str.d[i]));
+    CC(cudaFree(b->cnt.d[i]));
+    CC(cudaFree(b->ii.d[i]));
 }
 
 void alloc_Sbufs(const int26 estimates, const int26 nfragcells, /**/ Sbufs *b) {
@@ -61,22 +63,11 @@ void free_Sbufs(/**/ Sbufs *b) {
 }
 
 static void alloc_Rbuf_frag(const int i, const int est, const int nfragcells, /**/ Rbufs *b) {
-    CC(cudaMalloc(&b->cum.d[i], (nfragcells + 1) * sizeof(int)));
-    CC(cudaMalloc(&b->pp.d[i], est * sizeof(Particle)));
-
-    CC(cudaHostAlloc(&b->pphst.d[i], est * sizeof(Particle), cudaHostAllocMapped));
-    CC(cudaHostGetDevicePointer(&b->ppdev.d[i], b->pphst.d[i], 0));
-
-    CC(cudaHostAlloc(&b->cumhst.d[i], est * sizeof(int), cudaHostAllocMapped));
-    CC(cudaHostGetDevicePointer(&b->cumdev.d[i], b->cumhst.d[i], 0));
+    alloc_Bbuf_frag(i, est, nfragcells, /**/ b);
 };
 
 static void free_Rbuf_frag(const int i, /**/ Rbufs *b) {
-    CC(cudaFree(b->cum.d[i]));
-    CC(cudaFree(b->pp.d[i]));
-        
-    CC(cudaFreeHost(b->cumhst.d[i]));
-    CC(cudaFreeHost(b->pphst.d[i]));
+    free_Bbuf_frag(i, /**/ b);
 }
 
 void alloc_Rbufs(const int26 estimates, const int26 nfragcells, /**/ Rbufs *b) {
