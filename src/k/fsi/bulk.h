e989012dc90c0f9a198dd25c2c842a769e7ea9da
Sergey Litvinov <slitvinov@gmail.com>
2017-07-20 12:21:40 +0200

    move pair to common

--- src/k/fsi/bulk.h
+++ src/k/fsi/bulk.h
@@ -15,28 +15,9 @@ static __device__ Pa pp2p(float2 *pp, int i) {
     return p;
 }
 
-static __device__ void pair0(const Pa l, const Pa r, float rnd, /**/ float *fx, float *fy, float *fz) {
-    /* pair force ; l, r: local and remote */
-    float3 r1, r2, v1, v2, f;
-    r1 = make_float3( l.x,  l.y,  l.z); r2 = make_float3( r.x,  r.y,  r.z);
-    v1 = make_float3(l.vx, l.vy, l.vz); v2 = make_float3(r.vx, r.vy, r.vz);
-    f = force(SOLID_TYPE, SOLVENT_TYPE, r1, r2, v1, v2, rnd); /* TODO: type */
-    *fx = f.x; *fy = f.y; *fz = f.z;
-}
-
 static __device__ int p2map(int zplane, int n, const Pa p, /**/ Map *m) {
     /* particle to map */
     return r2map(zplane, n, p.x, p.y, p.z, m);
-}
-
-static __device__ void pair(const Pa l, const Pa r, float rnd, /**/
-                            float *fx, float *fy, float *fz,
-                            Fo f) {
-    /* f[xyz]: local force; Fo f: remote force */
-    float x, y, z; /* pair force */
-    pair0(l, r, rnd, /**/ &x, &y, &z);
-    *fx += x; *fy += y; *fz += z;
-    atomicAdd(f.x, -x); atomicAdd(f.y, -y); atomicAdd(f.z, -z);
 }
 
 static __device__ void bulk0(const Pa l, int lid, const Map m, float seed, /**/
