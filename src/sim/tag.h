18337810ac5d6a2209e8462b33217ac88a5b5a3e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-20 13:59:09 +0200

    communicate mesh in sim/tag.h

--- src/sim/tag.h
+++ src/sim/tag.h
@@ -1,4 +1,20 @@
 /* set tags of particles according to the RBCs */
 void gen_tags() {
-    collision::get_tags(o::q.pp, o::q.n, r::tt.texvert, r::tt.textri, r::q.nt, r::q.nv, r::q.nc, /**/ o::qt.ii);
+
+    if (mc::tc.first) mcomm::post_recv(/**/ &mc::tc, &mc::tr);
+    mcomm::extents(r::q.pp, r::q.nv, r::q.nc, /**/ &mc::ts);
+    mcomm::pack(r::q.pp, r::q.nv, r::q.nc, /**/ &mc::ts);
+    mcomm::post_send(r::q.nv, &mc::ts, /**/ &mc::tc);
+    mcomm::wait_recv(/**/ &mc::tc);
+    int nm = mcomm::unpack(r::q.nv, /**/ &mc::tr);
+    mcomm::post_recv(/**/ &mc::tc, &mc::tr);
+
+    /* tmp texture object; TODO: make a ticket? */
+    Texo<float2> texvert;
+    texvert.setup((float2*) mc::tr.pp, nm * 3 * r::q.nv);
+
+    collision::get_tags(o::q.pp, o::q.n, texvert, r::tt.textri, r::q.nt, r::q.nv, r::q.nc, /**/ o::qt.ii);
+
+    texvert.destroy();
+    mc::tc.first = false;
 }
