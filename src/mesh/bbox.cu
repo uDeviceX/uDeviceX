451a70f1cf7f35f51981c3c8b2101ec6ccfc1b8b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-21 12:08:11 +0200

    rm already defined functions

--- src/mesh/bbox.cu
+++ src/mesh/bbox.cu
@@ -12,9 +12,6 @@ static void get_bbox(const Particle *pp, const int n, /**/ float3 *minbb, float3
     float3 minb = make_float3(r[0], r[1], r[2]);
     float3 maxb = make_float3(r[0], r[1], r[2]);
 
-    auto min = [](float a, float b) {return a > b ? b : a;};
-    auto max = [](float a, float b) {return a > b ? a : b;};
-    
     for (int i = 1; i < n; ++i) {
         r = pp[i].r;
         minb.x = min(minb.x, r[0]); maxb.x = max(maxb.x, r[0]);
@@ -31,9 +28,6 @@ void get_bbox(const float *rr, const int n, /**/ float3 *minbb, float3 *maxbb) {
         
     float3 minb = make_float3(r[0], r[1], r[2]);
     float3 maxb = make_float3(r[0], r[1], r[2]);
-
-    auto min = [](float a, float b) {return a > b ? b : a;};
-    auto max = [](float a, float b) {return a > b ? a : b;};
 
     for (int i = 1; i < n; ++i) {
         r = rr + 3 * i;
