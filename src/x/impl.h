d1824a0ed1ec99c9703e05f4d29c92a4beb1e134
Sergey Litvinov <slitvinov@gmail.com>
2017-07-22 09:16:50 +0200

    add rex()

--- src/x/impl.h
+++ src/x/impl.h
@@ -2,4 +2,15 @@ namespace x {
 void ini(/*io*/ basetags::TagGen *tg) { rex::ini(tg); }
 void fin() { rex::fin(); }
 
+void rex(std::vector<ParticlesWrap> w) {
+    rex::bind_solutes(w);
+    rex::pack_p();
+    rex::post_p();
+    rex::recv_p();
+
+    rex::halo(); /* fsi::halo(); */
+
+    rex::post_f();
+    rex::recv_f();
+}
 }
