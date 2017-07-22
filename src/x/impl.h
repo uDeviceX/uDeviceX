2ba9a6394d3575cef0f029f1288ddb4b72797a74
Sergey Litvinov <slitvinov@gmail.com>
2017-07-22 15:33:40 +0200

    add nw

--- src/x/impl.h
+++ src/x/impl.h
@@ -3,6 +3,10 @@ void ini(/*io*/ basetags::TagGen *tg) { rex::ini(tg); }
 void fin() { rex::fin(); }
 
 void rex(std::vector<ParticlesWrap> w) {
+    int nw;
+
+    nw = w.size();
+
     rex::pack_p(w);
     rex::post_p(w);
     rex::recv_p(w);
