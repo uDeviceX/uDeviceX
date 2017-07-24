74b4149557738b68ad3869f9d1b1e73bad455e6f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-24 11:41:16 +0200

    flocal

--- src/dpd/x/dev.h
+++ src/dpd/x/dev.h
@@ -14,83 +14,82 @@ __device__ Part tex2Part(const Texo<float2> texpp, const int id) {
     return p;
 }
 
-__global__ void interactions(const Texo<float2> texpp, const int *sstart, int n, /**/ Force *ff) {
+__global__ void flocal(const Texo<float2> texpp, const int *sstart, int n, const float seed, /**/ Force *ff) {
     int gid = threadIdx.x + blockDim.x * blockIdx.x;
     int pid = gid / 3;
     int zplane = gid % 3;
-
+    
 #define start_fetch(id) sstart[id]
     
     if (pid >= n) return;
     
     Part p = tex2Part(texpp, pid);
     /* TODO check from here */
-    uint scan1, scan2, ncandidates, spidbase;
-    int deltaspid1, deltaspid2;
+    uint scan1, scan2, ncandidates;
+    int start0, start1, start2;
 
     {
+        int count0, count1, count2 = 0;
+        
         int xcid = (int)(p.r[X] + XS / 2);
-        int ycid = (int)(p.r[Y] + YS / 2));
-        int zcid = (int)(p.r[Z] + ZS / 2));
-                
+        int ycid = (int)(p.r[Y] + YS / 2);
+        int zcid = (int)(p.r[Z] + ZS / 2);
+    
         enum {
             XCELLS = XS,
             YCELLS = YS,
             ZCELLS = ZS,
             NCELLS = XCELLS * YCELLS * ZCELLS
         };
-/* needs check on boundaries */
-        int cid0 = xcid - 1 + XCELLS * (ycid - 1 + YCELLS * (zcid - 1 + zplane));
+        
+        int xcid0 = max(xcid - 1, 0);
+        int ycid0 = max(ycid - 1, 0);
+        int zcid0 = zcid - 1 + zplane;
 
-        spidbase = start_fetch(cid0);
-        int count0 = start_fetch(cid0 + 3) - spidbase;
+        int dx = (xcid == 0 || xcid == (XCELLS-1)) ? 2 : 3;
+        int dy = (ycid == 0 || ycid == (YCELLS-1)) ? 2 : 3;
 
+        if (zcid0 < 0 || zcid0 >= ZCELLS) return;
+        
+        int cid0 = xcid0 + XCELLS * (ycid + YCELLS * zcid0);
+        start0 = start_fetch(cid0);
+        count0 = start_fetch(cid0 + dx) - start0;
+        
         int cid1 = cid0 + XCELLS;
-        deltaspid1 = start_fetch(cid1);
-        int count1 = start_fetch(cid1 + 3) - deltaspid1;
-
-        int cid2 = cid0 + XCELLS * 2;
-        deltaspid2 = start_fetch(cid2);
-        int count2 = cid2 + 3 == NCELLS
-            ? n
-            : start_fetch(cid2 + 3) - deltaspid2;
+        start1 = start_fetch(cid1);
+        count1 = start_fetch(cid1 + dx) - start1;
+        
+        if (ycid + 1 < YCELLS) {
+            int cid2 = cid0 + XCELLS * 2;
+            start2 = start_fetch(cid2);
+            count2 = start_fetch(cid2 + dx) - start2;
+        }
 
         scan1 = count0;
         scan2 = count0 + count1;
         ncandidates = scan2 + count2;
 
-        deltaspid1 -= scan1;
-        deltaspid2 -= scan2;
+        // start1 -= scan1;
+        // start2 -= scan2;
     }
 
     float xforce = 0, yforce = 0, zforce = 0;
 
-#define zig x
-#define zag y
-#define mf3 make_float3
-    float  x = dst0.zig,  y = dst0.zag,  z = dst1.zig; /* bulk particle  */
-    float vx = dst1.zag, vy = dst2.zig, vz = dst2.zag;
-
     for (int i = 0; i < ncandidates; ++i) {
         int m1 = (int)(i >= scan1);
         int m2 = (int)(i >= scan2);
-        int spid = i + (m2 ? deltaspid2 : m1 ? deltaspid1 : spidbase);
+        int spid = i + (m2 ? start2 : m1 ? start1 : start0);
 
-        const float4 rw = wpp_fetch(spid); /* wall particle */
+        Part sp = tex2Part(texpp, spid); /* source particle */
 
-        float vxw, vyw, vzw;
-        k_wvel::vell(rw.x, rw.y, rw.z, &vxw, &vyw, &vzw);
         float rnd = l::rnd::d::mean0var1ii(seed, pid, spid);
-        float3 strength = force(type, WALL_TYPE,
-                                mf3(x ,  y,  z), mf3(rw.x, rw.y, rw.z),
-                                mf3(vx, vy, vz), mf3( vxw,  vyw,  vzw), rnd);
+        float3 strength = force(SOLVENT_TYPE, SOLVENT_TYPE,
+                                mf3(p.r[X], p.r[Y], p.r[Z]), mf3(ps.r[X], ps.r[Y], ps.r[Z]),
+                                mf3(p.v[X], p.v[Y], p.v[Z]), mf3(ps.v[X], ps.v[Y], ps.v[Z]), rnd);
         xforce += strength.x; yforce += strength.y; zforce += strength.z;
     }
-#undef zig
-#undef zag
-    atomicAdd(acc + 3 * pid + 0, xforce);
-    atomicAdd(acc + 3 * pid + 1, yforce);
-    atomicAdd(acc + 3 * pid + 2, zforce);
+    atomicAdd(ff[pid].f + 0, xforce);
+    atomicAdd(ff[pid].f + 1, yforce);
+    atomicAdd(ff[pid].f + 2, zforce);
 #undef start_fetch
-#undef wpp_fetch
 }
