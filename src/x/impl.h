261193656ec19dff0134f7d72af003ce53f9b4e7
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-07-24 11:57:11 +0200

    use rex0

--- src/x/impl.h
+++ src/x/impl.h
@@ -2,20 +2,21 @@ namespace x {
 void ini(/*io*/ basetags::TagGen *tg) { rex::ini(tg); }
 void fin() { rex::fin(); }
 
-void rex(std::vector<ParticlesWrap> w) {
-    int nw;
-    nw = w.size();
-
-    if (nw) {
-        rex::pack_p(nw);
-        rex::_pack_attempt(w);
-        rex::post_p(w);
-        rex::recv_p(w);
-        rex::halo(); /* fsi::halo(); */
-        rex::_postrecvP();
-    }
-
+void rex0(std::vector<ParticlesWrap> w, int nw) {
+    rex::pack_p(nw);
+    rex::_pack_attempt(w);
+    rex::post_p(w);
+    rex::recv_p(w);
+    rex::halo(); /* fsi::halo(); */
+    rex::_postrecvP();
     rex::post_f(w);
     rex::recv_f(w);
 }
+
+void rex(std::vector<ParticlesWrap> w) {
+    int nw;
+    nw = w.size();
+    if (nw) rex0(w, nw);
+}
+
 }
