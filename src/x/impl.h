765d3997bc81510ddc2051044121381c37918979
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-07-24 11:54:27 +0200

    group nw check

--- src/x/impl.h
+++ src/x/impl.h
@@ -5,14 +5,15 @@ void fin() { rex::fin(); }
 void rex(std::vector<ParticlesWrap> w) {
     int nw;
     nw = w.size();
-    rex::pack_p(nw);
-    rex::_pack_attempt(w);
 
-    if (nw) rex::post_p(w);
-    rex::recv_p(w);
-
-    if (nw) rex::halo(); /* fsi::halo(); */
-    rex::_postrecvP();
+    if (nw) {
+        rex::pack_p(nw);
+        rex::_pack_attempt(w);
+        rex::post_p(w);
+        rex::recv_p(w);
+        rex::halo(); /* fsi::halo(); */
+        rex::_postrecvP();
+    }
 
     rex::post_f(w);
     rex::recv_f(w);
