35f03feefa0c9966109570307a3cff3407ef103d
Sergey Litvinov <slitvinov@gmail.com>
2017-07-24 23:49:58 +0200

    init tags

--- src/x/ticketr.h
+++ src/x/ticketr.h
@@ -1,5 +1,14 @@
 namespace x {
 static void ini_ticketr(TicketR t) {
-
+    enum {X, Y, Z};
+    int i;
+    int d[3];
+    for (i = 0; i < 26; ++i) {
+        i2d(i, /**/ d);
+        t.tags[i] = \
+                 (2 - d[X]) % 3 +
+            3 * ((2 - d[Y]) % 3 +
+            3 * ((2 - d[Z]) % 3));
+    }
 }
 }
