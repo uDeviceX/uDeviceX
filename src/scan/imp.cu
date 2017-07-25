227fb7806c88db3b09b6be232c19dcda36ec3932
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-25 10:31:57 +0200

    add work struct

--- src/scan/imp.cu
+++ src/scan/imp.cu
@@ -4,14 +4,13 @@
 
 #include "scan/int.h"
 
+namespace scan {
 namespace dev {
 #include "scan/dev.h"
 }
 
-static void scan0(const unsigned char *input, int size, uint *output) {
+static void scan0(const unsigned char *input, int size, /**/ uint *output, /*w*/ uint *tmp) {
     enum { THREADS = 128 } ;
-
-    static uint *tmp = NULL;
 
     if (tmp == NULL)
         cudaMalloc(&tmp, sizeof(uint) * (64 * 64 * 64 / THREADS));
@@ -25,8 +24,17 @@ static void scan0(const unsigned char *input, int size, uint *output) {
     dev::gexscan< THREADS / 32 ><<<nblocks, THREADS>>>((uint4 *)input, tmp, (uint4 *)output, size / 16);
 }
 
-void scan(const int *input, int size, /**/ int *output, /*w*/ unsigned char *compressed) {
-    dev::compress <<< k_cnf(size) >>> (size, (const int4*) input, /**/ (uchar4 *) compressed);
+void scan(const int *input, int size, /**/ int *output, /*w*/ Work *w) {
+    dev::compress <<< k_cnf(size) >>> (size, (const int4*) input, /**/ (uchar4 *) w->compressed);
 
-    scan0(compressed, size, /**/ (uint*) output);
+    scan0(w->compressed, size, /**/ (uint*) output, /*w*/ w->tmp);
+}
+
+void alloc_work(/**/ Work *w) {
+
+}
+
+void free_work(/**/ Work *w) {
+
+}
 }
--- src/scan/int.h
+++ src/scan/int.h
@@ -1,1 +1,11 @@
-void scan(const int *input, int size, /**/ int *output, /*w*/ unsigned char *compressed);
+namespace scan {
+struct Work {
+    uint *tmp;
+    unsigned char *compressed;
+};
+
+void scan(const int *input, int size, /**/ int *output, /*w*/ Work *w);
+
+void alloc_work(/**/ Work *w);
+void free_work(/**/ Work *w);
+}
