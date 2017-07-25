7970b59066d98243495cf9fb91e46cdb2349a9ab
Sergey Litvinov <slitvinov@gmail.com>
2017-07-25 15:28:30 +0200

    move decl up

--- src/k/rex/scan.h
+++ src/k/rex/scan.h
@@ -1,9 +1,10 @@
 namespace k_rex {
 __global__ void scan(const int *counts, const int *oldtotalcounts,
                      /**/ int *totalcounts, int *paddedstarts) {
-    int tid = threadIdx.x;
+    int tid, mycount, newcount, myscan, L;
 
-    int mycount = 0;
+    tid = threadIdx.x;
+    mycount = 0;
 
     if (tid < 26) {
         mycount = counts[tid];
@@ -11,7 +12,7 @@ __global__ void scan(const int *counts, const int *oldtotalcounts,
         if (mycount > ccapacities[tid]) failed = true;
 
         if (totalcounts && oldtotalcounts) {
-            int newcount = mycount + oldtotalcounts[tid];
+            newcount = mycount + oldtotalcounts[tid];
             totalcounts[tid] = newcount;
 
             if (newcount > ccapacities[tid]) failed = true;
@@ -19,7 +20,7 @@ __global__ void scan(const int *counts, const int *oldtotalcounts,
     }
 
     if (paddedstarts) {
-        int myscan = mycount = 32 * ((mycount + 31) / 32);
+        myscan = mycount = 32 * ((mycount + 31) / 32);
 
         for (int L = 1; L < 32; L <<= 1)
         myscan += (tid >= L) * __shfl_up(myscan, L);
@@ -28,4 +29,3 @@ __global__ void scan(const int *counts, const int *oldtotalcounts,
     }
 }
 }
-
