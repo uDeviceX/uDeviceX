2c0a017f38ff93167822a6d391ea8e4829439f7e
Sergey Litvinov <slitvinov@gmail.com>
2017-07-25 10:27:57 +0200

    rm one more scope level

--- src/rex/post.h
+++ src/rex/post.h
@@ -47,32 +47,29 @@ void post_p(MPI_Comm cart, int dranks[26], int tags[26], x::TicketTags t) {
     }
     dSync(); /* was CC(cudaStreamSynchronize(downloadstream)); */
 
-    // post the sending of the packs
-    {
-        reqsendC.resize(26);
+    reqsendC.resize(26);
 
-        for (int i = 0; i < 26; ++i)
-            MC(l::m::Isend(send_counts + i, 1, MPI_INTEGER, dranks[i],
-                           t.btc + i, cart, &reqsendC[i]));
+    for (int i = 0; i < 26; ++i)
+        MC(l::m::Isend(send_counts + i, 1, MPI_INTEGER, dranks[i],
+                       t.btc + i, cart, &reqsendC[i]));
 
-        for (int i = 0; i < 26; ++i) {
-            int start = host_packstotalstart->D[i];
-            int count = send_counts[i];
-            int expected = local[i]->expected();
-
-            MPI_Request reqP;
-            MC(l::m::Isend(host_packbuf->D + start, expected * 6, MPI_FLOAT,
-                           dranks[i], t.btp1 + i, cart, &reqP));
-            reqsendP.push_back(reqP);
-
-            if (count > expected) {
-                MPI_Request reqP2;
-                MC(l::m::Isend(host_packbuf->D + start + expected,
-                               (count - expected) * 6, MPI_FLOAT, dranks[i],
-                               t.btp2 + i, cart, &reqP2));
-
-                reqsendP.push_back(reqP2);
-            }
+    for (int i = 0; i < 26; ++i) {
+        int start = host_packstotalstart->D[i];
+        int count = send_counts[i];
+        int expected = local[i]->expected();
+        
+        MPI_Request reqP;
+        MC(l::m::Isend(host_packbuf->D + start, expected * 6, MPI_FLOAT,
+                       dranks[i], t.btp1 + i, cart, &reqP));
+        reqsendP.push_back(reqP);
+        
+        if (count > expected) {
+            MPI_Request reqP2;
+            MC(l::m::Isend(host_packbuf->D + start + expected,
+                           (count - expected) * 6, MPI_FLOAT, dranks[i],
+                           t.btp2 + i, cart, &reqP2));
+            
+            reqsendP.push_back(reqP2);
         }
     }
 }
