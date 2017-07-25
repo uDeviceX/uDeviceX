dc27996fa7055532d62a738496cc3a1164b7565a
Sergey Litvinov <slitvinov@gmail.com>
2017-07-25 15:31:49 +0200

    add scanB

--- src/k/rex/scan.h
+++ src/k/rex/scan.h
@@ -1,4 +1,33 @@
 namespace k_rex {
+__global__ void scanB(const int *counts, const int *oldtotalcounts, /**/ int *totalcounts, int *paddedstarts) {
+    int tid, mycount, newcount, myscan, L;
+
+    tid = threadIdx.x;
+    mycount = 0;
+
+    if (tid < 26) {
+        mycount = counts[tid];
+
+        if (mycount > ccapacities[tid]) failed = true;
+
+        if (totalcounts && oldtotalcounts) {
+            newcount = mycount + oldtotalcounts[tid];
+            totalcounts[tid] = newcount;
+
+            if (newcount > ccapacities[tid]) failed = true;
+        }
+    }
+
+    if (paddedstarts) {
+        myscan = mycount = 32 * ((mycount + 31) / 32);
+
+        for (L = 1; L < 32; L <<= 1)
+        myscan += (tid >= L) * __shfl_up(myscan, L);
+
+        if (tid < 27) paddedstarts[tid] = myscan - mycount;
+    }
+}
+
 __global__ void scan(const int *counts, const int *oldtotalcounts,
                      /**/ int *totalcounts, int *paddedstarts) {
     int tid, mycount, newcount, myscan, L;
