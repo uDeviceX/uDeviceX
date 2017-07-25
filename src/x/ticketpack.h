816491deb33c6a59d6c9b4e08efc83840dc632fd
Sergey Litvinov <slitvinov@gmail.com>
2017-07-25 17:27:30 +0200

    ini and fin for TicketPack

--- src/x/ticketpack.h
+++ src/x/ticketpack.h
@@ -1,2 +1,22 @@
 namespace x {
+static void ini_tickpack(TicketPack *t) {
+    t->packstotalstart = new DeviceBuffer<int>(27);
+    t->host_packstotalstart = new PinnedHostBuffer1<int>(27);
+    t->host_packstotalcount = new PinnedHostBuffer1<int>(26);
+
+    t->packscount = new DeviceBuffer<int>;
+    t->packsstart = new DeviceBuffer<int>;
+    t->packsoffset = new DeviceBuffer<int>;
+
+}
+
+static void fin_tickpack(TicketPack *t) {
+    delete t->packstotalstart;
+    delete t->host_packstotalstart;
+    delete t->host_packstotalcount;
+
+    delete t->packscount;
+    delete t->packsstart;
+    delete t->packsoffset;
+}
 }
