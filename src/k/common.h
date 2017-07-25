0e0654fa7d0f2de6885f7d89f53eb7b69db6e1aa
Sergey Litvinov <slitvinov@gmail.com>
2017-07-25 14:54:05 +0200

    add get_fid to common

--- src/k/common.h
+++ src/k/common.h
@@ -1,4 +1,13 @@
 namespace k_common {
+__device__ inline unsigned int get_fid(const int a[], const int i) {
+    /* where is `i' in sorted a[27]? */
+    int k1, k3, k9;
+    k9 = 9 * ((i >= a[9])           + (i >= a[18]));
+    k3 = 3 * ((i >= a[k9 + 3])      + (i >= a[k9 + 6]));
+    k1 =      (i >= a[k9 + k3 + 1]) + (i >= a[k9 + k3 + 2]);
+    return k9 + k3 + k1;
+}
+
 template<bool project>
 __global__  void subindex_local(const int nparticles, const float2 * particles, int * const partials,
                                 uchar4 * const subindices)
