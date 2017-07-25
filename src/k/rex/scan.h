c6181091632c6c788b9b4d32b5228d85aeaff896
Sergey Litvinov <slitvinov@gmail.com>
2017-07-25 15:35:32 +0200

    rename

--- src/k/rex/scan.h
+++ src/k/rex/scan.h
@@ -1,30 +1,17 @@
 namespace k_rex {
-__global__ void scanB(const int *counts, const int *oldtotalcounts, /**/ int *totalcounts, int *paddedstarts) {
-    int tid, mycount, newcount, myscan, L;
-
+__global__ void scanB(const int *count, /**/ int *start) {
+    int tid, cnt, scan, L;
     tid = threadIdx.x;
-    mycount = 0;
-
+    cnt = 0;
     if (tid < 26) {
-        mycount = counts[tid];
-
-        if (mycount > ccapacities[tid]) failed = true;
-
-        if (totalcounts && oldtotalcounts) {
-            newcount = mycount + oldtotalcounts[tid];
-            totalcounts[tid] = newcount;
-
-            if (newcount > ccapacities[tid]) failed = true;
-        }
+        cnt = count[tid];
+        if (cnt > ccapacities[tid]) failed = true;
     }
-
-    if (paddedstarts) {
-        myscan = mycount = 32 * ((mycount + 31) / 32);
-
+    if (start) {
+        scan = cnt = 32 * ((cnt + 31) / 32);
         for (L = 1; L < 32; L <<= 1)
-        myscan += (tid >= L) * __shfl_up(myscan, L);
-
-        if (tid < 27) paddedstarts[tid] = myscan - mycount;
+        scan += (tid >= L) * __shfl_up(scan, L);
+        if (tid < 27) start[tid] = scan - cnt;
     }
 }
 
