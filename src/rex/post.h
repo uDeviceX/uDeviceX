8b8c648581200866907f5678eefc0dcbc4895cc9
Sergey Litvinov <slitvinov@gmail.com>
2017-07-25 11:42:58 +0200

    rename

--- src/rex/post.h
+++ src/rex/post.h
@@ -1,12 +1,12 @@
 namespace rex {
-void post_wait() { _wait(reqsendC); }
+void post_waitC() { _wait(reqsendC); }
 
 bool post_pre(MPI_Comm cart, int dranks[26], int tags[26], x::TicketTags t) {
     bool packingfailed;
     int i;
 
     if (cnt == 0) _postrecvC(cart, dranks, tags, t);
-    else post_wait();
+    else post_waitC();
 
     for (i = 0; i < 26; ++i) send_counts[i] = host_packstotalcount->D[i];
     packingfailed = false;
