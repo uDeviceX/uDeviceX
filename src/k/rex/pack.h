5c426a02b21f5d924bdf63666a8f4b24a99a7640
Sergey Litvinov <slitvinov@gmail.com>
2017-07-26 11:49:20 +0200

    move decl up

--- src/k/rex/pack.h
+++ src/k/rex/pack.h
@@ -1,24 +1,22 @@
 namespace k_rex {
-__device__ void pack0(const float2 *pp, /**/ float2 *buffer) {
-    int warpid = threadIdx.x >> 5;
-    int npack_padded = g::starts[26];
+__device__ void pack0(const float2 *pp, /**/ float2 *buf) {
+    int warpid, npack_padded, localbase, code, packbase, npack, lane;
+    float2 s0, s1, s2;
+    int entry, pid, entry2;
 
-    for (int localbase = 32 * (warpid + 4 * blockIdx.x); localbase < npack_padded;
+    warpid = threadIdx.x >> 5;
+    npack_padded = g::starts[26];
+    for (localbase = 32 * (warpid + 4 * blockIdx.x); localbase < npack_padded;
          localbase += gridDim.x * blockDim.x) {
-        int code = k_common::fid(g::starts, localbase);
-        int packbase = localbase - g::starts[code];
-
-        int npack = min(32, g::counts[code] - packbase);
-
-        int lane = threadIdx.x & 0x1f;
-
-        float2 s0, s1, s2;
-
+        code = k_common::fid(g::starts, localbase);
+        packbase = localbase - g::starts[code];
+        npack = min(32, g::counts[code] - packbase);
+        lane = threadIdx.x & 0x1f;
         if (lane < npack) {
-            int entry = g::offsets[code] + packbase + lane;
-            int pid = __ldg(g::scattered_indices[code] + entry);
+            entry = g::offsets[code] + packbase + lane;
+            pid = __ldg(g::scattered_indices[code] + entry);
 
-            int entry2 = 3 * pid;
+            entry2 = 3 * pid;
 
             s0 = __ldg(pp + entry2);
             s1 = __ldg(pp + entry2 + 1);
@@ -28,14 +26,13 @@ __device__ void pack0(const float2 *pp, /**/ float2 *buffer) {
             s0.y -= ((code / 3 + 2) % 3 - 1) * YS;
             s1.x -= ((code / 9 + 2) % 3 - 1) * ZS;
         }
-        k_write::AOS6f(buffer + 3 * (g::tstarts[code] + g::offsets[code] + packbase), npack,
-                       s0, s1, s2);
+        k_write::AOS6f(buf + 3 * (g::tstarts[code] + g::offsets[code] + packbase), npack, s0, s1, s2);
     }
 }
 
-__global__ void pack(const float2 *pp, /**/ float2 *buffer) {
+__global__ void pack(const float2 *pp, /**/ float2 *buf) {
     if (g::failed) return;
-    pack0(pp, buffer);
+    pack0(pp, buf);
 }
 
 }
