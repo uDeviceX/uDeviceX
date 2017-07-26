2bb67b90ca1cae8d918baf7d5c52049344435077
Sergey Litvinov <slitvinov@gmail.com>
2017-07-26 07:30:32 +0200

    minor

--- src/k/rex/scan.h
+++ src/k/rex/scan.h
@@ -1,6 +1,6 @@
 namespace k_rex {
 __global__ void scanA(const int *counts, const int *oldtotalcounts,
-                     /**/ int *totalcounts, int *paddedstarts) {
+                     /**/ int *totalcounts, int *starts) {
     int tid, mycount, newcount, myscan, L;
     tid = threadIdx.x;
     mycount = 0;
@@ -14,11 +14,11 @@ __global__ void scanA(const int *counts, const int *oldtotalcounts,
         }
     }
 
-    if (paddedstarts) {
+    if (starts) {
         myscan = mycount = 32 * ((mycount + 31) / 32);
         for (L = 1; L < 32; L <<= 1)
         myscan += (tid >= L) * __shfl_up(myscan, L);
-        if (tid < 27) paddedstarts[tid] = myscan - mycount;
+        if (tid < 27) starts[tid] = myscan - mycount;
     }
 }
 
