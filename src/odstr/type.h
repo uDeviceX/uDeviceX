708adafd20849e29bc535bef4cf600527f2cba8c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-26 11:51:44 +0200

    add type.h

---
+++ src/odstr/type.h
@@ -0,0 +1,31 @@
+namespace odstr {
+namespace sub {
+
+/* pinned buffers (array of pinned buffers) */ 
+template <typename T, int N=27>
+struct Pbufs {
+    T **dev;   /* data on device  */
+    T *dp[N];  /* device pointers */
+    T *hst[N]; /* data on host    */
+};
+
+struct Send {
+    int **iidx;       /* helper indices (device) */
+    int *iidx_[27];   /* helper indices (pinned) */
+    
+    Pbufs<float2> pp; /* Send particles          */
+    
+    int *size_dev, *strt;
+    int size[27];
+    PinnedHostBuffer4<int>* size_pin;
+};
+
+struct Recv {
+    Pbufs<float2> pp; /* Recv  particles          */
+    
+    int *strt;
+    int tags[27], size[27];
+};
+
+}
+}
