0bc0c521799b8754891840755830ed65ed114792
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-26 09:57:31 +0200

    adapt interfaces

--- src/flu/imp.h
+++ src/flu/imp.h
@@ -1,78 +1,18 @@
-int gen0(Particle *pp) { /* generate particle positions and velocities */
-    enum {X, Y, Z};
-    assert(XS * YS * ZS * numberdensity < MAX_PART_NUM);
-  
-    srand48(123456);
-    int iz, iy, ix, l, nd = numberdensity;
-    int n = 0; /* particle index */
-    float x, y, z, dr = 0.99;
-    for (iz = 0; iz < ZS; iz++)
-    for (iy = 0; iy < YS; iy++)
-    for (ix = 0; ix < XS; ix++) {
-        /* edge of a cell */
-        int xlo = -0.5*XS + ix, ylo = -0.5*YS + iy, zlo = -0.5*ZS + iz;
-        for (l = 0; l < nd; l++) {
-            Particle p = Particle();
-            x = xlo + dr * drand48(), y = ylo + dr * drand48(), z = zlo + dr * drand48();
-            p.r[X] = x; p.r[Y] = y; p.r[Z] = z;
+namespace sub {
+int gen0(Particle *pp);
+int gen(Particle *dev, /*w*/ Particle *hst);
 
-            p.v[X] = 0; p.v[Y] = 0; p.v[Z] = 0;
-          
-            pp[n++] = p;
-        }
-    }
+void ii_gen0(const long n, int *ii);
+void ii_gen(const int n, int *ii_dev, int *ii_hst);
 
-    MSG("ic::gen: created %06d solvent particles", n);
-    return n;
+void tags0_gen0(const long n, int *ii);
+void tags0_gen(const int n, int *ii_dev, int *ii_hst);
+
+int strt(const int id, Particle *dev, /*w*/ Particle *hst);
+int strt_ii(const char *subext, const int id, int *dev, /*w*/ int *hst);
+
+void strt_dump(const int id, const int n, const Particle *dev, Particle *hst);
+void strt_dump_ii(const char *subext, const int id, const int n, const int *dev, int *hst);
+
+void zip(const Particle *pp, const int n, /**/ float4 *zip0, ushort4 * zip1);
 }
-
-int gen(Particle *dev, /*w*/ Particle *hst) {
-    int n = gen0(hst);
-    cH2D(dev, hst, n);
-    return n;
-}
-
-void ii_gen0(const long n, int *ii) {
-    long i0 = 0;
-    MC(l::m::Exscan(&n, &i0, 1, MPI_LONG, MPI_SUM, m::cart));
-    for (long i = 0; i < n; ++i) ii[i] = i + i0;
-}
-
-void ii_gen(const int n, int *ii_dev, int *ii_hst) {
-    ii_gen0(n, ii_hst);
-    cH2D(ii_dev, ii_hst, n);
-}
-
-void tags0_gen0(const long n, int *ii) {
-    for (long i = 0; i < n; ++i) ii[i] = -1;
-}
-
-void tags0_gen(const int n, int *ii_dev, int *ii_hst) {
-    tags0_gen0(n, ii_hst);
-    cH2D(ii_dev, ii_hst, n);
-}
-
-int strt(const int id, Particle *dev, /*w*/ Particle *hst) {
-    int n;
-    restart::read_pp("flu", id, hst, &n);
-    if (n) cH2D(dev, hst, n);
-    return n;
-}
-
-int strt_ii(const char *subext, const int id, int *dev, /*w*/ int *hst) {
-    int n;
-    restart::read_ii("flu", subext, id, hst, &n);
-    if (n) cH2D(dev, hst, n);
-    return n;
-}
-
-void strt_dump(const int id, const int n, const Particle *dev, Particle *hst) {
-    if (n) cD2H(hst, dev, n);
-    restart::write_pp("flu", id, hst, n);
-}
-
-void strt_dump_ii(const char *subext, const int id, const int n, const int *dev, int *hst) {
-    if (n) cD2H(hst, dev, n);
-    restart::write_ii("flu", subext, id, hst, n);
-}
-
--- src/flu/int.h
+++ src/flu/int.h
@@ -1,3 +1,5 @@
+namespace flu {
+
 struct Quants {
     Particle *pp, *pp0; /* particles on device  */
     int       n;        /* particle number      */
@@ -19,81 +21,28 @@ struct TicketRND { /* random */
     rnd::KISS *rnd;
 };
 
-void alloc_quants(Quants *q) {
-    q->n = 0;
-    mpDeviceMalloc(&q->pp); mpDeviceMalloc(&q->pp0);
-    q->cells = new clist::Clist(XS, YS, ZS);
-    q->pp_hst = new Particle[MAX_PART_NUM];
-}
+void alloc_quants(Quants *q);
+void free_quants(Quants *q);
 
-void free_quants(Quants *q) {
-    CC(cudaFree(q->pp)); CC(cudaFree(q->pp0));
-    delete q->cells;
-    delete[] q->pp_hst;
-}
+void alloc_quantsI(QuantsI *q);
+void free_quantsI(QuantsI *q);
 
-void alloc_quantsI(QuantsI *q) {
-    mpDeviceMalloc(&q->ii); mpDeviceMalloc(&q->ii0);
-    q->ii_hst = new int[MAX_PART_NUM];
-}
+void alloc_ticketZ(/**/ TicketZ *t);
+void free_ticketZ(/**/ TicketZ *t);
+void get_ticketZ(Quants q, /**/ TicketZ *t);
 
-void free_quantsI(QuantsI *q) {
-    CC(cudaFree(q->ii)); CC(cudaFree(q->ii0));
-    delete[] q->ii_hst;
-}
+void get_ticketRND(/**/ TicketRND *t);
+void free_ticketRND(/**/ TicketRND *t);
 
-void alloc_ticketZ(/**/ TicketZ *t) {
-    mpDeviceMalloc(&t->zip0);
-    mpDeviceMalloc(&t->zip1);
-}
+void gen_quants(Quants *q);
 
-void free_ticketZ(/**/ TicketZ *t) {
-    float4  *zip0 = t->zip0;
-    ushort4 *zip1 = t->zip1;
-    cudaFree(zip0);
-    cudaFree(zip1);
-}
+void gen_ids(const int n, QuantsI *q);
+void gen_tags0(const int n, QuantsI *q);
 
-void get_ticketZ(Quants q, /**/ TicketZ *t) {
-    if (q.n == 0) return;
-    float4  *zip0 = t->zip0;
-    ushort4 *zip1 = t->zip1;
-    assert(sizeof(Particle) == 6 * sizeof(float)); /* :TODO: implicit dependency */
-    sub::dev::zip<<<(q.n + 1023) / 1024, 1024, 1024 * 6 * sizeof(float)>>>(zip0, zip1, (float*)q.pp, q.n);
-}
+void strt_quants(const int id, Quants *q);
+void strt_ii(const char *subext, const int id, QuantsI *q);
 
-void get_ticketRND(/**/ TicketRND *t) {
-    t->rnd = new rnd::KISS(0, 0, 0, 0);
-}
+void strt_dump(const int id, const Quants q);
+void strt_dump_ii(const char *subext, const int id, const QuantsI q, const int n);
 
-void free_ticketRND(/**/ TicketRND *t) {
-    delete t->rnd;
-}
-
-void gen_quants(Quants *q) {
-    q->n = sub::gen(q->pp, q->pp_hst);
-}
-
-void gen_ids(const int n, QuantsI *q) {
-    sub::ii_gen(n, q->ii, q->ii_hst);
-}
-
-void gen_tags0(const int n, QuantsI *q) {
-    sub::tags0_gen(n, q->ii, q->ii_hst);
-}
-
-void strt_quants(const int id, Quants *q) {
-    q->n = sub::strt(id, /**/ q->pp, /* w */ q->pp_hst);
-}
-
-void strt_ii(const char *subext, const int id, QuantsI *q) {
-    sub::strt_ii(subext, id, /**/ q->ii, /* w */ q->ii_hst);
-}
-
-void strt_dump(const int id, const Quants q) {
-    sub::strt_dump(id, q.n, q.pp, /* w */ q.pp_hst);
-}
-
-void strt_dump_ii(const char *subext, const int id, const QuantsI q, const int n) {
-    sub::strt_dump_ii(subext, id, n, q.ii, /* w */ q.ii_hst);
 }
