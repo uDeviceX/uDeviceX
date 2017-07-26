51ee99ad83b2243f39182c4ee6e922060bf16ff5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-26 10:33:58 +0200

    add type.h

---
+++ src/dpdr/type.h
@@ -0,0 +1,30 @@
+typedef Sarray<int,  26> int26;
+typedef Sarray<int,  27> int27;
+typedef Sarray<int*, 26> intp26;
+typedef Sarray<Particle*, 26> Particlep26;
+
+struct Bbufs { /* basic buffer : common to Send and Recv */
+    intp26 cum;                /* cellstarts for each fragment (frag coords)      */
+    Particlep26 pp;            /* buffer of particles for each fragment           */
+
+    /* pinned buffers */
+    Particlep26 ppdev, pphst;  /* pinned memory for transfering particles         */
+    intp26 cumdev, cumhst;     /* pinned memory for transfering local cum sum     */
+};
+
+struct Sbufs : Bbufs {
+    intp26 str, cnt;           /* cell starts and counts for each fragment (bulk coords) */
+    intp26 ii;                 /* scattered indices                                      */
+};
+
+struct Rbufs : Bbufs {};
+
+struct Ibuf {
+    intp26 ii;                  /* int data on device for each fragment    */
+
+    /* pinned buffers */
+    intp26 iihst, iidev;        /* pinned memory for transfering int data  */
+};
+
+struct SIbuf : Ibuf {};
+struct RIbuf : Ibuf {};
