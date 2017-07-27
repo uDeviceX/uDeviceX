09e662fd7d7cf821aac3080ae3cf2db582f37c45
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 12:21:51 +0200

    minor

--- src/k/rex/unpack.h
+++ src/k/rex/unpack.h
@@ -1,5 +1,5 @@
 namespace k_rex {
-__global__ void unpack(/**/ float *forces) {
+__global__ void unpack(/**/ float *ff) {
     int npack_padded = g::starts[26];
 
     for (int gid = threadIdx.x + blockDim.x * blockIdx.x; gid < 3 * npack_padded;
@@ -19,7 +19,7 @@ __global__ void unpack(/**/ float *forces) {
         float myval = __ldg(g::recvbags[code] + component + 3 * entry);
         int dpid = __ldg(g::scattered_indices[code] + entry);
 
-        atomicAdd(forces + 3 * dpid + component, myval);
+        atomicAdd(ff + 3 * dpid + component, myval);
     }
 }
 }
