c28254ed65842146ad9543c01a4cf07c05c56f75
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 22:40:24 +0200

    rename

--- src/rex/post.h
+++ src/rex/post.h
@@ -16,13 +16,13 @@ bool post_check() {
 }
 
 void post_resize() {
-    int newcapacities[26];
-    int *newindices[26];
+    int capacities[26];
+    int *scattered_indices[26];
     int i;
-    for (i = 0; i < 26; ++i) newcapacities[i] = local[i]->capacity();
-    CC(cudaMemcpyToSymbolAsync(k_rex::g::capacities, newcapacities, sizeof(newcapacities), 0, H2D));
-    for (i = 0; i < 26; ++i) newindices[i] = local[i]->scattered_indices->D;
-    CC(cudaMemcpyToSymbolAsync(k_rex::g::scattered_indices, newindices, sizeof(newindices), 0, H2D));
+    for (i = 0; i < 26; ++i) capacities[i] = local[i]->capacity();
+    CC(cudaMemcpyToSymbolAsync(k_rex::g::capacities, capacities, sizeof(capacities), 0, H2D));
+    for (i = 0; i < 26; ++i) scattered_indices[i] = local[i]->scattered_indices->D;
+    CC(cudaMemcpyToSymbolAsync(k_rex::g::scattered_indices, scattered_indices, sizeof(scattered_indices), 0, H2D));
 }
 
 void local_resize() {
