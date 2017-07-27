2ff7600b61c31ab75edf15c1eab76988671fd6ae
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 18:38:06 +0200

    call scan_pad

--- src/k/rex/scan.h
+++ src/k/rex/scan.h
@@ -7,7 +7,7 @@ __device__ void scan_pad(int cnt, int t, /**/ int *starts) {
 }
 
 __global__ void scanA(const int *counts, const int *oldtcounts, /**/ int *tcounts, int *starts) {
-    int t, cnt, newcount, scan, L;
+    int t, cnt, newcount;
     t = threadIdx.x;
     cnt = 0;
     if (t < 26) {
@@ -19,27 +19,18 @@ __global__ void scanA(const int *counts, const int *oldtcounts, /**/ int *tcount
             if (newcount > g::capacities[t]) g::failed = true;
         }
     }
-
-    if (starts) {
-        scan = cnt = 32 * ((cnt + 31) / 32);
-        for (L = 1; L < 32; L <<= 1) scan += (t >= L) * __shfl_up(scan, L);
-        if (t < 27) starts[t] = scan - cnt;
-    }
+    if (starts) scan_pad(cnt, t, /**/ starts);
 }
 
-__global__ void scanB(const int *count, /**/ int *start) {
-    int t, cnt, scan, L;
+__global__ void scanB(const int *count, /**/ int *starts) {
+    int t, cnt;
     t = threadIdx.x;
     cnt = 0;
     if (t < 26) {
         cnt = count[t];
         if (cnt > g::capacities[t]) g::failed = true;
     }
-    if (start) {
-        scan = cnt = 32 * ((cnt + 31) / 32);
-        for (L = 1; L < 32; L <<= 1) scan += (t >= L) * __shfl_up(scan, L);
-        if (t < 27) start[t] = scan - cnt;
-    }
+    if (starts) scan_pad(cnt, t, /**/ starts);
 }
 
 }
