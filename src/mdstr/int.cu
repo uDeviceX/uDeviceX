b1f01eb5839b46730f819f96d9601ccdbfa6e758
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-27 14:24:09 +0200

    fill ticket comm

--- src/mdstr/int.cu
+++ src/mdstr/int.cu
@@ -1,4 +1,4 @@
-#include "int.h"
+#include "mdstr/int.h"
 
 namespace mdstr {
 
--- src/mdstr/int.h
+++ src/mdstr/int.h
@@ -1,9 +1,13 @@
-
 namespace mdstr {
 
 /* communication ticket (generic part) */
 struct TicketC { 
-
+    int btc, btp;                     /* base tags                 */    
+    MPI_Comm cart;                    /* Cartesian communicator    */
+    MPI_Request sreqc[26], rreqc[26]; /* counts requests           */
+    MPI_Request sreqp[26], rreqp[26]; /* particles requests        */
+    int rnk_ne[27];                   /* rank      of the neighbor */
+    int ank_ne[27];                   /* anti-rank of the neighbor */
 };
 
 /* send buffers */
@@ -12,8 +16,8 @@ struct TicketS {
 };
 
 /* recv buffers */
-struct TicketS {
-
+struct TicketR {
+    int counts[27];
 };
 
 void pack();
