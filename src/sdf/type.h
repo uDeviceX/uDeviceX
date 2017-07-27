b41ff30f4a52072a0b28513a3b6fa64241ccd9a2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-27 09:36:04 +0200

    add sdf/type.h

---
+++ src/sdf/type.h
@@ -0,0 +1,31 @@
+/* 3D texture object binded to  cuda array */
+template <typename T>
+struct tex3Dca {
+    cudaTextureObject_t to;
+
+    __device__ __forceinline__
+    const T fetch(const float i, const float j, const float k) const {
+        return tex3D<T>(to, i, j, k);
+    }
+    
+    void setup(cudaArray *ca) {
+        cudaResourceDesc resD;
+        cudaTextureDesc  texD;
+
+        memset(&resD, 0, sizeof(resD));
+        resD.resType = cudaResourceTypeArray;
+        resD.res.array.array = ca;
+        
+        memset(&texD, 0, sizeof(texD));
+        texD.normalizedCoords = 0;
+        texD.filterMode = cudaFilterModePoint;
+        texD.mipmapFilterMode = cudaFilterModePoint;
+        texD.addressMode[0] = cudaAddressModeWrap;
+        texD.addressMode[1] = cudaAddressModeWrap;
+        texD.addressMode[2] = cudaAddressModeWrap;
+
+        CC(cudaCreateTextureObject(&to, &resD, &texD, NULL));
+    }
+
+    void destroy() {CC(cudaDestroyTextureObject(to));}
+};
