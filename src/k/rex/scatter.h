9105ce8acbb8586686802bcc31324fc0ec978037
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 07:42:21 +0200

    use pp2p_coll

--- src/k/rex/scatter.h
+++ src/k/rex/scatter.h
@@ -1,16 +1,21 @@
 namespace k_rex {
+__device__ void pp2xyz_col(const float2 *pp, int n, int i, /**/ float *x, float *y, float *z) {
+
+}
+
 __global__ void scatter(const float2 *pp, const int n, /**/ int *counts) {
     int warpid, lane, base, nsrc, pid;
     int d;
     int xterm, yterm, zterm, fid;
     int myid;
-    float2 s0, s1, s2;
+    Pa p;
     
     warpid = threadIdx.x / warpSize;
     lane   = threadIdx.x % warpSize;
     base   = 32 * (warpid + 4 * blockIdx.x);
     nsrc   = min(32, n - base);
-    k_read::AOS6f(pp + 3 * base, nsrc, s0, s1, s2);
+    p = pp2p_coll(pp, nsrc, base);
+    //    k_read::AOS6f(pp + 3 * base, nsrc, s0, s1, s2);
 
     pid = base + lane;
     if (lane >= nsrc) return;
@@ -20,9 +25,9 @@ __global__ void scatter(const float2 *pp, const int n, /**/ int *counts) {
         HZSIZE = ZS / 2
     };
     int halocode[3] = {
-        -1 + (int)(s0.x >= -HXSIZE + 1) + (int)(s0.x >= HXSIZE - 1),
-        -1 + (int)(s0.y >= -HYSIZE + 1) + (int)(s0.y >= HYSIZE - 1),
-        -1 + (int)(s1.x >= -HZSIZE + 1) + (int)(s1.x >= HZSIZE - 1)};
+        -1 + (int)(p.s0.x >= -HXSIZE + 1) + (int)(p.s0.x >= HXSIZE - 1),
+        -1 + (int)(p.s0.y >= -HYSIZE + 1) + (int)(p.s0.y >= HYSIZE - 1),
+        -1 + (int)(p.s1.x >= -HZSIZE + 1) + (int)(p.s1.x >= HZSIZE - 1)};
     if (halocode[0] == 0 && halocode[1] == 0 && halocode[2] == 0) return;
     // faces
     for (d = 0; d < 3; ++d)
