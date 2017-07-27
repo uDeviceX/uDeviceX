c58fc2db4921df51cbad1b2bbe075e7d1401ad82
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-27 09:34:15 +0200

    adding sdf int.cu

---
+++ src/sdf/int.cu
@@ -0,0 +1,24 @@
+#include "common.h"
+
+#include "sdf/int.h"
+#include "sdf/imp.h"
+
+namespace sdf {
+void alloc_quants(Quants *q) {
+    cudaChannelFormatDesc fmt = cudaCreateChannelDesc<float>();
+    CC(cudaMalloc3DArray(&q->arrsdf, &fmt, make_cudaExtent(XTE, YTE, ZTE)));
+}
+
+void  free_quants(Quants *q) {
+    CC(cudaFreeArray(q->arrsdf));
+    q->texsdf.destroy();
+}
+
+void ini(Quants *q) {
+    sub::ini(q->arrsdf, &q->texsdf);
+}
+
+int who_stays(const Quants q, Particle *pp, int n, int nc, int nv, int *stay) {
+    return sub::who_stays(q.texsdf, pp, n, nc, nv, /**/ stay);
+}
+}
