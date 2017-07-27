5eabff3e30123135ff1ced17ab49893e694750cc
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 07:04:48 +0200

    move decl up

--- src/k/rex/scatter.h
+++ src/k/rex/scatter.h
@@ -1,13 +1,18 @@
 namespace k_rex {
 __global__ void scatter(const float2 *pp, const int n, /**/ int *counts) {
-    int warpid = threadIdx.x / warpSize;
-    int lane   = threadIdx.x % warpSize;
-    int base   = 32 * (warpid + 4 * blockIdx.x);
-    int nsrc   = min(32, n - base);
+    int warpid, lane, base, nsrc, pid;
+    int d;
+    int xterm, yterm, zterm, fid;
+    int myid;
     float2 s0, s1, s2;
+    
+    warpid = threadIdx.x / warpSize;
+    lane   = threadIdx.x % warpSize;
+    base   = 32 * (warpid + 4 * blockIdx.x);
+    nsrc   = min(32, n - base);
     k_read::AOS6f(pp + 3 * base, nsrc, s0, s1, s2);
 
-    int pid = base + lane;
+    pid = base + lane;
     if (lane >= nsrc) return;
     enum {
         HXSIZE = XS / 2,
@@ -20,37 +25,37 @@ __global__ void scatter(const float2 *pp, const int n, /**/ int *counts) {
         -1 + (int)(s1.x >= -HZSIZE + 1) + (int)(s1.x >= HZSIZE - 1)};
     if (halocode[0] == 0 && halocode[1] == 0 && halocode[2] == 0) return;
     // faces
-    for (int d = 0; d < 3; ++d)
+    for (d = 0; d < 3; ++d)
         if (halocode[d]) {
-            int xterm = (halocode[0] * (d == 0) + 2) % 3;
-            int yterm = (halocode[1] * (d == 1) + 2) % 3;
-            int zterm = (halocode[2] * (d == 2) + 2) % 3;
+            xterm = (halocode[0] * (d == 0) + 2) % 3;
+            yterm = (halocode[1] * (d == 1) + 2) % 3;
+            zterm = (halocode[2] * (d == 2) + 2) % 3;
 
-            int fid = xterm + 3 * (yterm + 3 * zterm);
-            int myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
+            fid = xterm + 3 * (yterm + 3 * zterm);
+            myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
 
             if (myid < g::capacities[fid]) g::scattered_indices[fid][myid] = pid;
         }
     // edges
-    for (int d = 0; d < 3; ++d)
+    for (d = 0; d < 3; ++d)
         if (halocode[(d + 1) % 3] && halocode[(d + 2) % 3]) {
-            int xterm = (halocode[0] * (d != 0) + 2) % 3;
-            int yterm = (halocode[1] * (d != 1) + 2) % 3;
-            int zterm = (halocode[2] * (d != 2) + 2) % 3;
+            xterm = (halocode[0] * (d != 0) + 2) % 3;
+            yterm = (halocode[1] * (d != 1) + 2) % 3;
+            zterm = (halocode[2] * (d != 2) + 2) % 3;
 
-            int fid = xterm + 3 * (yterm + 3 * zterm);
-            int myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
+            fid = xterm + 3 * (yterm + 3 * zterm);
+            myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
 
             if (myid < g::capacities[fid]) g::scattered_indices[fid][myid] = pid;
         }
     // one corner
     if (halocode[0] && halocode[1] && halocode[2]) {
-        int xterm = (halocode[0] + 2) % 3;
-        int yterm = (halocode[1] + 2) % 3;
-        int zterm = (halocode[2] + 2) % 3;
+        xterm = (halocode[0] + 2) % 3;
+        yterm = (halocode[1] + 2) % 3;
+        zterm = (halocode[2] + 2) % 3;
 
-        int fid = xterm + 3 * (yterm + 3 * zterm);
-        int myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
+        fid = xterm + 3 * (yterm + 3 * zterm);
+        myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
 
         if (myid < g::capacities[fid]) g::scattered_indices[fid][myid] = pid;
     }
