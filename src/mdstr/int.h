66feba3cc9f8e32726b3226152ca2e32c2851a89
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-27 14:15:44 +0200

    add dummy interface

---
+++ src/mdstr/int.h
@@ -0,0 +1,24 @@
+
+namespace mdstr {
+
+/* communication ticket (generic part) */
+struct TicketC { 
+
+};
+
+/* send buffers */
+struct TicketS {
+
+};
+
+/* recv buffers */
+struct TicketS {
+
+};
+
+void pack();
+void post();
+void wait();
+void unpack();
+
+} // mdstr
