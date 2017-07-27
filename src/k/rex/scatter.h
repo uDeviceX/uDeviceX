37a2b9d7b8cbfdbc205f78c295a7d806d4e1574f
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 15:28:26 +0200

    use xyz2fdir

--- src/k/rex/scatter.h
+++ src/k/rex/scatter.h
@@ -5,27 +5,28 @@ __device__ void pp2xyz_col(const float2 *pp, int n, int i, /**/ float *x, float 
     p2xyz(p, /**/ x, y, z);
 }
 
+__device__ void xyz2fdir(float x, float y, float z, /**/ int fdir[]) {
+    enum {X, Y, Z};
+    enum { HXSIZE = XS / 2, HYSIZE = YS / 2, HZSIZE = ZS / 2 };
+    fdir[X] = -1 + (int)(x >= -HXSIZE + 1) + (int)(x >= HXSIZE - 1);
+    fdir[Y] = -1 + (int)(y >= -HYSIZE + 1) + (int)(y >= HYSIZE - 1);
+    fdir[Z] = -1 + (int)(z >= -HZSIZE + 1) + (int)(z >= HZSIZE - 1);
+}
+
 __device__ void scatter0(const float2 *pp, int pid, float x, float y, float z, /**/ int *counts) {
     int d;
     int xterm, yterm, zterm, fid;
     int myid;
+    int fdir[3]; /* [f]ragment [dir]ection */
+    xyz2fdir(x, y, z, fdir);
     
-    enum {
-        HXSIZE = XS / 2,
-        HYSIZE = YS / 2,
-        HZSIZE = ZS / 2
-    };
-    int halocode[3] = {
-        -1 + (int)(x >= -HXSIZE + 1) + (int)(x >= HXSIZE - 1),
-        -1 + (int)(y >= -HYSIZE + 1) + (int)(y >= HYSIZE - 1),
-        -1 + (int)(z >= -HZSIZE + 1) + (int)(z >= HZSIZE - 1)};
-    if (halocode[0] == 0 && halocode[1] == 0 && halocode[2] == 0) return;
+    if (fdir[0] == 0 && fdir[1] == 0 && fdir[2] == 0) return;
     // faces
     for (d = 0; d < 3; ++d)
-        if (halocode[d]) {
-            xterm = (halocode[0] * (d == 0) + 2) % 3;
-            yterm = (halocode[1] * (d == 1) + 2) % 3;
-            zterm = (halocode[2] * (d == 2) + 2) % 3;
+        if (fdir[d]) {
+            xterm = (fdir[0] * (d == 0) + 2) % 3;
+            yterm = (fdir[1] * (d == 1) + 2) % 3;
+            zterm = (fdir[2] * (d == 2) + 2) % 3;
 
             fid = xterm + 3 * (yterm + 3 * zterm);
             myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
@@ -34,10 +35,10 @@ __device__ void scatter0(const float2 *pp, int pid, float x, float y, float z, /
         }
     // edges
     for (d = 0; d < 3; ++d)
-        if (halocode[(d + 1) % 3] && halocode[(d + 2) % 3]) {
-            xterm = (halocode[0] * (d != 0) + 2) % 3;
-            yterm = (halocode[1] * (d != 1) + 2) % 3;
-            zterm = (halocode[2] * (d != 2) + 2) % 3;
+        if (fdir[(d + 1) % 3] && fdir[(d + 2) % 3]) {
+            xterm = (fdir[0] * (d != 0) + 2) % 3;
+            yterm = (fdir[1] * (d != 1) + 2) % 3;
+            zterm = (fdir[2] * (d != 2) + 2) % 3;
 
             fid = xterm + 3 * (yterm + 3 * zterm);
             myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
@@ -45,10 +46,10 @@ __device__ void scatter0(const float2 *pp, int pid, float x, float y, float z, /
             if (myid < g::capacities[fid]) g::scattered_indices[fid][myid] = pid;
         }
     // one corner
-    if (halocode[0] && halocode[1] && halocode[2]) {
-        xterm = (halocode[0] + 2) % 3;
-        yterm = (halocode[1] + 2) % 3;
-        zterm = (halocode[2] + 2) % 3;
+    if (fdir[0] && fdir[1] && fdir[2]) {
+        xterm = (fdir[0] + 2) % 3;
+        yterm = (fdir[1] + 2) % 3;
+        zterm = (fdir[2] + 2) % 3;
 
         fid = xterm + 3 * (yterm + 3 * zterm);
         myid = g::offsets[fid] + atomicAdd(counts + fid, 1);
