c5cca5c5f9e3ac34f3b7f2c7ff6df9e1407682f2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-27 10:01:44 +0200

    int and imp.h are headers

--- src/sdf/int.h
+++ src/sdf/int.h
@@ -1,22 +1,15 @@
+namespace sdf {
+
 struct Quants {
     cudaArray *arrsdf;
-    sub::dev::tex3Dca<float> texsdf;
+    tex3Dca<float> texsdf;
 };
 
-void alloc_quants(Quants *q) {
-    cudaChannelFormatDesc fmt = cudaCreateChannelDesc<float>();
-    CC(cudaMalloc3DArray(&q->arrsdf, &fmt, make_cudaExtent(XTE, YTE, ZTE)));
-}
+void alloc_quants(Quants *q);
+void  free_quants(Quants *q);
 
-void  free_quants(Quants *q) {
-    CC(cudaFreeArray(q->arrsdf));
-    q->texsdf.destroy();
-}
+void ini(Quants *q);
+int who_stays(const Quants q, Particle *pp, int n, int nc, int nv, int *stay);
 
-void ini(Quants *q) {
-    sub::ini(q->arrsdf, &q->texsdf);
-}
-
-int who_stays(const Quants q, Particle *pp, int n, int nc, int nv, int *stay) {
-    return sub::who_stays(q.texsdf, pp, n, nc, nv, /**/ stay);
+void bounce(const tex3Dca<float> texsdf, int n, /**/ Particle *pp);
 }
