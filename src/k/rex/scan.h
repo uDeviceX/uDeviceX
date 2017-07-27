9229a3afd6b08ddc0c0d28ad24fa641bd57b0a2e
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 18:30:30 +0200

    minor

--- src/k/rex/scan.h
+++ src/k/rex/scan.h
@@ -1,4 +1,7 @@
 namespace k_rex {
+__device__ void scan_pad() {
+}
+
 __global__ void scanA(const int *counts, const int *oldtcounts, /**/ int *tcounts, int *starts) {
     int tid, cnt, newcount, scan, L;
     tid = threadIdx.x;
@@ -15,8 +18,7 @@ __global__ void scanA(const int *counts, const int *oldtcounts, /**/ int *tcount
 
     if (starts) {
         scan = cnt = 32 * ((cnt + 31) / 32);
-        for (L = 1; L < 32; L <<= 1)
-            scan += (tid >= L) * __shfl_up(scan, L);
+        for (L = 1; L < 32; L <<= 1) scan += (tid >= L) * __shfl_up(scan, L);
         if (tid < 27) starts[tid] = scan - cnt;
     }
 }
@@ -31,8 +33,7 @@ __global__ void scanB(const int *count, /**/ int *start) {
     }
     if (start) {
         scan = cnt = 32 * ((cnt + 31) / 32);
-        for (L = 1; L < 32; L <<= 1)
-            scan += (tid >= L) * __shfl_up(scan, L);
+        for (L = 1; L < 32; L <<= 1) scan += (tid >= L) * __shfl_up(scan, L);
         if (tid < 27) start[tid] = scan - cnt;
     }
 }
