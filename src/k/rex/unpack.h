cb6ceb4e26f57a814174cfaedcb7396cf61a48b0
Sergey Litvinov <slitvinov@gmail.com>
2017-07-27 15:58:27 +0200

    move decl up

--- src/k/rex/unpack.h
+++ src/k/rex/unpack.h
@@ -1,23 +1,24 @@
 namespace k_rex {
 __global__ void unpack(/**/ float *ff) {
-    int npack_padded = g::starts[26];
+    int npack_padded, gid, pid, code, lpid, component, entry, myval, dpid;
 
-    for (int gid = threadIdx.x + blockDim.x * blockIdx.x; gid < 3 * npack_padded;
+    npack_padded = g::starts[26];
+    for (gid = threadIdx.x + blockDim.x * blockIdx.x; gid < 3 * npack_padded;
          gid += blockDim.x * gridDim.x) {
-        int pid = gid / 3;
+        pid = gid / 3;
 
         if (pid >= npack_padded) return;
 
-        int code = k_common::fid(g::starts, pid);
-        int lpid = pid - g::starts[code];
+        code = k_common::fid(g::starts, pid);
+        lpid = pid - g::starts[code];
 
         if (lpid >= g::counts[code]) continue;
 
-        int component = gid % 3;
+        component = gid % 3;
 
-        int entry = g::offsets[code] + lpid;
-        float myval = __ldg(g::recvbags[code] + component + 3 * entry);
-        int dpid = __ldg(g::scattered_indices[code] + entry);
+        entry = g::offsets[code] + lpid;
+        myval = __ldg(g::recvbags[code] + component + 3 * entry);
+        dpid = __ldg(g::scattered_indices[code] + entry);
 
         atomicAdd(ff + 3 * dpid + component, myval);
     }
