579b29020460e9a0bb2ee94f59951e20f3d31080
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-28 18:09:34 +0200

    adding generic implementation

--- src/rdstr/imp.cu
+++ src/rdstr/imp.cu
@@ -1,6 +1,16 @@
+#include <cstdio>
+#include <mpi.h>
+#include "l/m.h"
+
 #include "common.h"
+#include "common.mpi.h"
+#include "common.cuda.h"
 
 #include "minmax.h"
+
+#include "mdstr/buf.h"
+#include "mdstr/gen.h"
+
 #include "rdstr/imp.h"
 
 namespace rdstr {
@@ -22,7 +32,29 @@ void get_pos(int n, const float3 *ll, const float3 *hh, /**/ float *rr) {
     }
 }
 
+namespace gen = mdstr::gen;
+typedef gen::pbuf<Particle> Partbuf;
 
+void pack(int *reord[27], const int counts[27], const Particle *pp, int nv, /**/ Partbuf *bpp) {
+    gen::pack <Particle, gen::Device> (reord, counts, pp, nv, /**/ bpp);
+}
+
+void post_send(int nv, const int counts[27], const Partbuf *bpp, MPI_Comm cart, int bt, int rnk_ne[27],
+               /**/ MPI_Request sreq[26]) {
+    gen::post_send(nv, counts, bpp, cart, bt, rnk_ne, /**/ sreq);
+}
+
+void post_recv(MPI_Comm cart, int nmax, int bt, int ank_ne[27], /**/ Partbuf *bpp, MPI_Request rreq[26]) {
+    gen::post_recv(cart, nmax, bt, ank_ne, /**/ bpp, rreq);
+}
+    
+int unpack(int npd, const Partbuf *bpp, const int counts[27], /**/ Particle *pp) {
+    return gen::unpack <Particle, gen::Device> (npd, bpp, counts, /**/ pp);
+}
+
+void shift() {
+    // TODO
+}
 
 } // sub
 } // rdstr
