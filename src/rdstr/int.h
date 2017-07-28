a2d305aa1966ae52b000c009eb3b9787fabe6916
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-28 17:07:42 +0200

    more precise use of using

--- src/rdstr/int.h
+++ src/rdstr/int.h
@@ -11,18 +11,30 @@ struct TicketE {
 using mdstr::ini_ticketC;
 using mdstr::free_ticketC;
 
-using namespace mdstr;
+// TODO change them to custom one
+using mdstr::TicketS;
+using mdstr::TicketR;
 
 void alloc_ticketE(/**/ TicketE *t);
 void free_ticketE(/**/ TicketE *t);
 
+// TODO custom version
+using mdstr::ini_ticketS;
+using mdstr::ini_ticketR;
+using mdstr::free_ticketS;
+using mdstr::free_ticketR;
+
+
 void extents(const Particle *pp, int nc, int nv, /**/ TicketE *t);
 void get_pos(int nc, /**/ TicketE *t);
-// void get_reord(const float *rr, int nm, /**/ TicketS *t);
-// void pack(const Particle *pp, int nv, /**/  TicketS *t);
-// void post_send(int nv, const TicketS *ts, /**/ TicketC *tc);
-// void post_recv(const TicketS *ts, /**/ TicketR *tr, TicketC *tc);
-// void wait_recv(/**/ TicketC *tc);
-// int unpack(int nv, const TicketR *t, /**/ Particle *pp);
+
+using mdstr::get_reord;
+
+// TODO: custom ones
+using mdstr::pack;
+using mdstr::post_send;
+using mdstr::post_recv;
+using mdstr::wait_recv;
+using mdstr::unpack;
 
 } // rdstr
