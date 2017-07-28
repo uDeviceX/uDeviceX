c9a2f98a05f9c3a731a8dc7585377f643f1e7fc5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-28 17:13:29 +0200

    alloc, free buf

--- src/mdstr/buf.h
+++ src/mdstr/buf.h
@@ -6,5 +6,15 @@ struct pbuf { /* pack buffer */
     T *dd[27];
 };
 
+template <typename T>
+void alloc_buf(int nmax, /**/ pbuf<T> *b) {
+    for (int i = 0; i < 27; ++i) b->dd[i] = new T[nmax];
+}
+
+template <typename T>
+void free_buf(/**/ pbuf<T> *b) {
+    for (int i = 0; i < 27; ++i) delete[] b->dd[i];
+}
+
 } // gen
 } // mdstr
