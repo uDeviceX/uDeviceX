805b1ffccec02fb400d87003b2f4c0a89e1b5b26
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-31 09:31:21 +0200

    decl in imp

--- src/rdstr/imp.h
+++ src/rdstr/imp.h
@@ -8,5 +8,16 @@ namespace gen = mdstr::gen;
 typedef gen::pbuf<Particle> Partbuf;
 
 
+void pack(int *reord[27], const int counts[27], const Particle *pp, int nv, /**/ Partbuf *bpp);
+
+void post_send(int nv, const int counts[27], const Partbuf *bpp, MPI_Comm cart, int bt, int rnk_ne[27],
+               /**/ MPI_Request sreq[26]);
+
+void post_recv(MPI_Comm cart, int nmax, int bt, int ank_ne[27], /**/ Partbuf *bpp, MPI_Request rreq[26]);
+
+int unpack(int npd, const Partbuf *bpp, const int counts[27], /**/ Particle *pp);
+
+void shift(int npd, const int counts[27], /**/ Particle *pp);
+
 } // sub
 } // rdstr
