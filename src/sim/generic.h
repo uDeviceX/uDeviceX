631c96e5abdaa199fb20a602c1a8bf5c2a8ad35c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-07-31 10:46:03 +0200

    adapt usage

--- src/sim/generic.h
+++ src/sim/generic.h
@@ -1,13 +1,14 @@
 void distr_rbc() {
     rdstr::extents(r::q.pp, r::q.nc, r::q.nv, /**/ &r::tde);
     rdstr::get_pos(r::q.nc, /**/ &r::tde);
-    rdstr::get_reord(r::tde.rr, r::q.nc, /**/ &r::tds);
-    rdstr::pack(r::q.pp, r::q.nv, /**/ &r::tds);
-    rdstr::post_recv(&r::tds, /**/ &r::tdr, &r::tdc);
-    rdstr::post_send(r::q.nv, &r::tds, /**/ &r::tdc);
-    rdstr::wait_recv(/**/ &r::tdc);
-    r::q.nc = rdstr::unpack(r::q.nv, &r::tdr, /**/ r::q.pp);
+    rdstr::get_reord(r::q.nc, &r::tde, /**/ &r::tdp);
+    rdstr::pack(r::q.pp, r::q.nv, &r::tdp, /**/ &r::tds);
+    rdstr::post_recv(/**/ &r::tdp, &r::tdc, &r::tdr);
+    rdstr::post_send(r::q.nv, &r::tdp, /**/ &r::tdc, &r::tds);
+    rdstr::wait_recv(/**/ &r::tdc, &r::tdr);
+    r::q.nc = rdstr::unpack(r::q.nv, &r::tdr, &r::tdp, /**/ r::q.pp);
     r::q.n = r::q.nc * r::q.nv;
+    rdstr::shift(r::q.nv, &r::tdp, /**/ r::q.pp);
     
     dSync();
     CC( cudaPeekAtLastError() );
