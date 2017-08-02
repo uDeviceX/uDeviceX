ac55734d5c0661f9881e9389794e3ee0e62f3d54
Sergey Litvinov <slitvinov@gmail.com>
2017-08-02 22:56:53 +0200

    create rex/scan.h

--- src/rex/pack.h
+++ src/rex/pack.h
@@ -5,24 +5,6 @@ void pack_clear(int nw, x::TicketPack tp) {
     CC(cudaMemsetAsync(tp.offsets, 0, sizeof(int) * 26 * (nw + 1)));
 }
 
-void scanA(std::vector<ParticlesWrap> w, int nw, x::TicketPack tp) {
-    int i;
-    k_rex::ini<<<1, 1>>>();
-    for (i = 0; i < nw; ++i) {
-        ParticlesWrap it = w[i];
-        if (it.n) {
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, tp.offsets + 26 * i, sizeof(int) * 26, 0, D2D));
-            k_rex::scatter<<<k_cnf(it.n)>>>((float2*)it.p, it.n, /**/ tp.counts + i * 26);
-        }
-        k_rex::scanA<<<1, 32>>>(tp.counts + i * 26, tp.offsets + 26 * i, /**/ tp.offsets + 26 * (i + 1), tp.starts + i * 27);
-    }
-}
-
-void scanB(std::vector<ParticlesWrap> w, x::TicketPack tp) {
-    CC(cudaMemcpyAsync(tp.offsets_hst, tp.offsets + 26 * w.size(), sizeof(int) * 26, H2H));
-    k_rex::scanB<<<1, 32>>>(tp.offsets + 26 * w.size(), /**/ tp.tstarts);
-}
-
 void pack_attempt(std::vector<ParticlesWrap> w, x::TicketPack tp) {
     CC(cudaMemcpyAsync(tp.tstarts_hst, tp.tstarts, sizeof(int) * 27, H2H));
     CC(cudaMemcpyToSymbolAsync(k_rex::g::tstarts, tp.tstarts, sizeof(int) * 27, 0, D2D));
