e8d746914c55294d6ed7c16bea8ed0a637c7af9e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-02 16:39:21 +0200

    format

--- src/mbounce/roots.h
+++ src/mbounce/roots.h
@@ -3,8 +3,7 @@ namespace roots {
 #define SWAP(a,b) do { auto tmp = b ; b = a ; a = tmp ; } while(0)
 
 template<typename real>
-_DH_ bool quadratic(real a, real b, real c, real *h0, real *h1)
-{
+_DH_ bool quadratic(real a, real b, real c, real *h0, real *h1) {
     const int sgnb = b > 0 ? 1 : -1;
     const real D = b*b - 4*a*c;
 
@@ -14,6 +13,7 @@ _DH_ bool quadratic(real a, real b, real c, real *h0, real *h1)
     *h1 = c / (a * *h0);
         
     if (*h0 > *h1) SWAP(*h0, *h1);
+
     return true;
 }
 
@@ -24,8 +24,7 @@ _DH_ bool quadratic(real a, real b, real c, real *h0, real *h1)
 
 template <typename real>
 _DH_ int cubic(real a, real b, real c, 
-               real *x0, real *x1, real *x2)
-{
+               real *x0, real *x1, real *x2) {
     real q = (a * a - 3 * b);
     real r = (2 * a * a * a - 9 * a * b + 27 * c);
 
@@ -38,32 +37,27 @@ _DH_ int cubic(real a, real b, real c,
     real CR2 = 729 * r * r;
     real CQ3 = 2916 * q * q * q;
 
-    if (R == 0 && Q == 0)
-    {
+    if (R == 0 && Q == 0) {
         *x0 = - a / 3 ;
         *x1 = - a / 3 ;
         *x2 = - a / 3 ;
         return 3 ;
     }
-    else if (CR2 == CQ3) 
-    {
+    else if (CR2 == CQ3) {
         /* this test is actually R2 == Q3, written in a form suitable
            for exact computation with integers */
-
+            
         /* Due to finite precision some real roots may be missed, and
            considered to be a pair of complex roots z = x +/- epsilon i
            close to the real axis. */
 
         real sqrtQ = sqrt (Q);
 
-        if (R > 0)
-        {
+        if (R > 0) {
             *x0 = -2 * sqrtQ  - a / 3;
             *x1 = sqrtQ - a / 3;
             *x2 = sqrtQ - a / 3;
-        }
-        else
-        {
+        } else {
             *x0 = - sqrtQ  - a / 3;
             *x1 = - sqrtQ - a / 3;
             *x2 = 2 * sqrtQ - a / 3;
@@ -71,32 +65,29 @@ _DH_ int cubic(real a, real b, real c,
         return 3 ;
     }
     else if (R2 < Q3)
-    {
-        real sgnR = (R >= 0 ? 1 : -1);
-        real ratio = sgnR * sqrt (R2 / Q3);
-        real theta = acos (ratio);
-        real norm = -2 * sqrt (Q);
-        *x0 = norm * cos (theta / 3) - a / 3;
-        *x1 = norm * cos ((theta + 2.0 * M_PI) / 3) - a / 3;
-        *x2 = norm * cos ((theta - 2.0 * M_PI) / 3) - a / 3;
-      
-        /* Sort *x0, *x1, *x2 into increasing order */
-
-        if (*x0 > *x1)
-        SWAP(*x0, *x1) ;
-      
-        if (*x1 > *x2)
         {
-            SWAP(*x1, *x2) ;
-          
-            if (*x0 > *x1)
-            SWAP(*x0, *x1) ;
-        }
+            real sgnR = (R >= 0 ? 1 : -1);
+            real ratio = sgnR * sqrt (R2 / Q3);
+            real theta = acos (ratio);
+            real norm = -2 * sqrt (Q);
+            *x0 = norm * cos (theta / 3) - a / 3;
+            *x1 = norm * cos ((theta + 2.0 * M_PI) / 3) - a / 3;
+            *x2 = norm * cos ((theta - 2.0 * M_PI) / 3) - a / 3;
       
-        return 3;
-    }
-    else
-    {
+            /* Sort *x0, *x1, *x2 into increasing order */
+
+            if (*x0 > *x1)
+                SWAP(*x0, *x1) ;
+      
+            if (*x1 > *x2) {
+                SWAP(*x1, *x2) ;
+          
+                if (*x0 > *x1)
+                    SWAP(*x0, *x1) ;
+            }
+      
+            return 3;
+        } else {
         real sgnR = (R >= 0 ? 1 : -1);
         real A = -sgnR * pow (fabs (R) + sqrt (R2 - Q3), 1.0/3.0);
         real B = Q / A ;
