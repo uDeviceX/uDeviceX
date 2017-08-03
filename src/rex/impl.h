52ae5ba116688bd2f1a75a0c662c9599f50b9677
Sergey Litvinov <slitvinov@gmail.com>
2017-08-03 16:16:02 +0200

    add copy_state

--- src/rex/impl.h
+++ src/rex/impl.h
@@ -1,5 +1,11 @@
 namespace rex {
 
+void copy_state() {
+    int i;
+    for (i = 0; i < 26; ++i)
+        CC(cudaMemcpyAsync(remote[i]->dstate.D, remote[i]->hstate.D, sizeof(Particle) * remote[i]->hstate.S, H2D));
+}
+
 void recv_copy_bags() {
     float *recvbags[26];
     for (int i = 0; i < 26; ++i) recvbags[i] = (float *)local[i]->result->DP;
