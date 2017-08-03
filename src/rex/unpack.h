34df07a9037a927ae33c22784447671514665597
Sergey Litvinov <slitvinov@gmail.com>
2017-08-03 15:46:54 +0200

    mv recv_f

--- src/rex/unpack.h
+++ src/rex/unpack.h
@@ -1,2 +1,13 @@
 namespace rex {
+void recv_f(std::vector<ParticlesWrap> w, x::TicketPack tp) {
+    for (int i = 0; i < (int) w.size(); ++i) {
+        ParticlesWrap it = w[i];
+        if (it.n) {
+            CC(cudaMemcpyToSymbolAsync(k_rex::g::starts,  tp.starts  + 27 * i, sizeof(int) * 27, 0, D2D));
+            CC(cudaMemcpyToSymbolAsync(k_rex::g::counts,  tp.counts  + 26 * i, sizeof(int) * 26, 0, D2D));
+            CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, tp.offsets + 26 * i, sizeof(int) * 26, 0, D2D));
+            k_rex::unpack<<<16 * 14, 128>>>(/**/ (float *)it.f);
+        }
+    }
+}
 }
