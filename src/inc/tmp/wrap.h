0f813e31e45fea0e96f97913ec7d74beb9c79f21
Sergey Litvinov <slitvinov@gmail.com>
2017-08-03 12:27:08 +0200

    minor

--- src/inc/tmp/wrap.h
+++ src/inc/tmp/wrap.h
@@ -1,8 +1,8 @@
-struct ParticlesWrap0 {
+struct ParticlesWrap000 {
     const Particle *p;
     Force *f;
     int n;
-    ParticlesWrap0() : p(NULL), f(NULL), n(0) {}
-    ParticlesWrap0(const Particle *const p, const int n, Force *f)
+    ParticlesWrap000() : p(NULL), f(NULL), n(0) {}
+    ParticlesWrap000(const Particle *const p, const int n, Force *f)
         : p(p), n(n), f(f) {}
 };
