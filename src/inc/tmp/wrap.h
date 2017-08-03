b2fcb0d2a0bfdb6e7e4f3423d5242c9347d2c8ed
Sergey Litvinov <slitvinov@gmail.com>
2017-08-03 15:03:34 +0200

    add particlerex

--- src/inc/tmp/wrap.h
+++ src/inc/tmp/wrap.h
@@ -6,3 +6,12 @@ struct ParticlesWrap {
     ParticlesWrap(const Particle *const p, const int n, Force *f)
         : p(p), n(n), f(f) {}
 };
+
+struct ParticlesRex {
+    const ParticleRex *p;
+    Force *f;
+    int n;
+    ParticlesRex() : p(NULL), f(NULL), n(0) {}
+    ParticlesRex(const Particle *const p, const int n, Force *f)
+        : p(p), n(n), f(f) {}
+};
