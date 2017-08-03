41538ee31b90ecc4fb692aacf64653cf5b14ba53
Sergey Litvinov <slitvinov@gmail.com>
2017-08-03 15:27:52 +0200

    populate recv

--- src/rex/impl.h
+++ src/rex/impl.h
@@ -1,30 +1,5 @@
 namespace rex {
 
-void postrecvC(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
-    for (int i = 0; i < 26; ++i) {
-        MPI_Request reqC;
-        MC(l::m::Irecv(recv_counts + i, 1, MPI_INTEGER, ranks[i], t.btc + tags[i], cart, &reqC));
-        reqrecvC.push_back(reqC);
-    }
-}
-
-void postrecvP(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
-    for (int i = 0; i < 26; ++i) {
-        MPI_Request reqP;
-        remote[i]->pmessage.resize(remote[i]->expected());
-        MC(l::m::Irecv(&remote[i]->pmessage.front(), remote[i]->expected() * 6, MPI_FLOAT, ranks[i], t.btp1 + tags[i], cart, &reqP));
-        reqrecvP.push_back(reqP);
-    }
-}
-
-void postrecvA(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
-    for (int i = 0; i < 26; ++i) {
-        MPI_Request reqA;
-        MC(l::m::Irecv(local[i]->result->D, local[i]->result->S * 3, MPI_FLOAT, ranks[i], t.btf + tags[i], cart, &reqA));
-        reqrecvA.push_back(reqA);
-    }
-}
-
 void recv_p(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
     for (int i = 0; i < 26; ++i) {
         int count = recv_counts[i];
--- src/rex/recv.h
+++ src/rex/recv.h
@@ -1,2 +1,27 @@
 namespace rex {
+void postrecvA(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
+    for (int i = 0; i < 26; ++i) {
+        MPI_Request reqA;
+        MC(l::m::Irecv(local[i]->result->D, local[i]->result->S * 3, MPI_FLOAT, ranks[i], t.btf + tags[i], cart, &reqA));
+        reqrecvA.push_back(reqA);
+    }
+}
+
+void postrecvC(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
+    for (int i = 0; i < 26; ++i) {
+        MPI_Request reqC;
+        MC(l::m::Irecv(recv_counts + i, 1, MPI_INTEGER, ranks[i], t.btc + tags[i], cart, &reqC));
+        reqrecvC.push_back(reqC);
+    }
+}
+
+void postrecvP(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
+    for (int i = 0; i < 26; ++i) {
+        MPI_Request reqP;
+        remote[i]->pmessage.resize(remote[i]->expected());
+        MC(l::m::Irecv(&remote[i]->pmessage.front(), remote[i]->expected() * 6, MPI_FLOAT, ranks[i], t.btp1 + tags[i], cart, &reqP));
+        reqrecvP.push_back(reqP);
+    }
+}
+
 }
