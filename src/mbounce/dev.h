97ac863233a8141a9335eb47bbc8b7d7fdb3caa8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-03 10:58:38 +0200

    add more general bounce

--- src/mbounce/dev.h
+++ src/mbounce/dev.h
@@ -54,6 +54,72 @@ __global__ void bounce_tcells(const Force *ff, const Mesh m, const Particle *i_p
     }
 }
 
+
+__device__ void lin_mom_change(const float v0[3], const float v1[3], /**/ float dP[3]) {
+    dP[X] = -(v1[X] - v0[X]);
+    dP[Y] = -(v1[Y] - v0[Y]);
+    dP[Z] = -(v1[Z] - v0[Z]);
+}
+
+__device__ void ang_mom_change(const float r[3], const float v0[3], const float v1[3], /**/ float dL[3]) {
+    dL[X] = -(r[Y] * v1[Z] - r[Z] * v1[Y]  -  r[Y] * v0[Z] + r[Z] - v0[Y]);
+    dL[X] = -(r[Z] * v1[X] - r[X] * v1[Z]  -  r[Z] * v0[X] + r[X] - v0[Z]);
+    dL[X] = -(r[X] * v1[Y] - r[Y] * v1[X]  -  r[X] * v0[Y] + r[Y] - v0[X]);
+}
+
+__global__ void bounce(const Force *ff, const Mesh m, const Particle *i_pp, const int *tcellstarts, const int *tcellcounts, const int *tids,
+                       const int n, /**/ Particle *pp, Momentum *mm) {
+    const int i = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if (i >= n) return;
+        
+    const Particle p1 = pp[i];
+            
+    Particle p0; rvprev(p1.r, p1.v, ff[i].f, /**/ p0.r, p0.v);
+
+    const int xcid = max(0, min(int (p1.r[X] + XS/2), XS-1));
+    const int ycid = max(0, min(int (p1.r[Y] + YS/2), YS-1));
+    const int zcid = max(0, min(int (p1.r[Z] + ZS/2), ZS-1));
+
+    float h = 2*dt; // must be higher than any valid result
+    float rw[3], vw[3];
+
+    int icol = -1; /* id of the collision triangle */
+        
+    const int cid = xcid + XS * (ycid + YS * zcid);
+    const int start = tcellstarts[cid];
+    const int count = tcellcounts[cid];
+                
+    for (int j = start; j < start + count; ++j) {
+        const int tid = tids[j];
+        const int it  = tid % m.nt;
+        const int mid = tid / m.nt;
+                    
+        if (find_better_intersection(m.tt, it, i_pp + mid * m.nv, &p0, /*io*/ &h, /**/ rw, vw))
+            icol = tid;
+    }
+
+    if (icol != -1) {
+        Particle pn;
+        bounce_back(&p0, rw, vw, h, /**/ &pn);
+
+        float dP[3], dL[3];
+        lin_mom_change(    p1.v, pn.v, /**/ dP);
+        ang_mom_change(rw, p1.v, pn.v, /**/ dL);
+                
+        pp[i] = pn;
+
+        atomicAdd(mm[icol].P + X, dP[X]);
+        atomicAdd(mm[icol].P + Y, dP[Y]);
+        atomicAdd(mm[icol].P + Z, dP[Z]);
+
+        atomicAdd(mm[icol].L + X, dL[X]);
+        atomicAdd(mm[icol].L + Y, dL[Y]);
+        atomicAdd(mm[icol].L + Z, dL[Z]);
+    }
+}
+
+
 static __device__ bool nz(float a) {return fabs(a) > 1e-6f;}
 static __device__ bool nonzero(const Momentum *m) {
     return nz(m->P[0]) && nz(m->P[1]) && nz(m->P[2]) &&
