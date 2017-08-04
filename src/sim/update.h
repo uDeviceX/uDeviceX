db400d177b1f7f5c6c2ef3a327fa423d845af31d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-04 14:18:46 +0200

    add incorrect (commented) bounce_rbc in sim

--- src/sim/update.h
+++ src/sim/update.h
@@ -14,3 +14,11 @@ void bounce() {
     sdf::bounce(&w::qsdf, o::q.n, /**/ o::q.pp);
     // if (rbcs) sdf::bounce(&w::qsdf, r::q.n, /**/ r::q.pp);
 }
+
+/* single node only for now */
+void bounce_rbc() {
+    // TODO
+    // build_tcells_dev(s::q.m_dev, s::t.i_pp, nsbb, /**/ s::t.tcs_dev, s::t.tcc_dev, s::t.tci_dev, /*w*/ &s::ws);
+    // mbounce::bounce_dev(o::ff, s::q.m_dev, s::t.i_pp, s::t.tcs_dev, s::t.tcc_dev, s::t.tci_dev, o::q.n, nsbb*s::q.m_dev.nt, /**/ o::q.pp, &bb::tm);
+    // mbounce::collect_rbc_dev(s::q.m_dev.nt, nsbb, &bb::tm, /**/ s::t.ss);
+}
