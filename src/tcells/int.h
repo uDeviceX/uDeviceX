f3475ed2587f95b48d42cb23e4ce00fa70513a50
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-04 14:36:28 +0200

    add quants

--- src/tcells/int.h
+++ src/tcells/int.h
@@ -1,4 +1,10 @@
 namespace tcells {
+
+struct Quants {
+    int *ss_hst, *cc_hst, *ii_hst; /* starts, counts, ids on hst */ 
+    int *ss_dev, *cc_dev, *ii_dev; /* starts, counts, ids on dev */ 
+};
+
 void build_hst(const Mesh m, const Particle *i_pp, const int ns, /**/ int *starts, int *counts, int *ids);
 void build_dev(const Mesh m, const Particle *i_pp, const int ns, /**/ int *starts, int *counts, int *ids, /*w*/ scan::Work *w);
 }
