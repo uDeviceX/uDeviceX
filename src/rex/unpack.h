133b74c00554a4e36522fd1ae3cb2c66fc3eba8e
Sergey Litvinov <slitvinov@gmail.com>
2017-08-04 22:54:06 +0200

    minor

--- src/rex/unpack.h
+++ src/rex/unpack.h
@@ -2,13 +2,17 @@ namespace rex {
 void unpack(std::vector<ParticlesWrap> w, int nw, x::TicketPack tp) {
     int i, n;
     Force *ff;
+    int *o, *c, *s;
     for (i = 0; i < nw; ++i) {
         n = w[i].n;
         if (n) {
             ff = w[i].f;
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::starts,  tp.starts  + 27 * i, sizeof(int) * 27, 0, D2D));
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::counts,  tp.counts  + 26 * i, sizeof(int) * 26, 0, D2D));
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, tp.offsets + 26 * i, sizeof(int) * 26, 0, D2D));
+            s = tp.starts  + 27 *  i;
+            c = tp.counts  + 26 *  i;
+            o = tp.offsets + 26 *  i;
+            CC(cudaMemcpyToSymbolAsync(k_rex::g::starts,  s, sizeof(int) * 27, 0, D2D));
+            CC(cudaMemcpyToSymbolAsync(k_rex::g::counts,  c, sizeof(int) * 26, 0, D2D));
+            CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, o, sizeof(int) * 26, 0, D2D));
             k_rex::unpack<<<16 * 14, 128>>>(/**/ (float*)ff);
         }
     }
