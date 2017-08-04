5e85b711ae72bb0565c73ab228ba42d39ced8d8e
Sergey Litvinov <slitvinov@gmail.com>
2017-08-04 16:39:06 +0200

    minor

--- src/rex/scan.h
+++ src/rex/scan.h
@@ -1,12 +1,13 @@
 namespace rex {
 void scanA(std::vector<ParticlesWrap> w, int nw, x::TicketPack tp) {
-    int i;
+    int i, n;
     k_rex::ini<<<1, 1>>>();
     for (i = 0; i < nw; ++i) {
         ParticlesWrap it = w[i];
-        if (it.n) {
+        n = w[i].n;
+        if (n) {
             CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, tp.offsets + 26 * i, sizeof(int) * 26, 0, D2D));
-            k_rex::scatter<<<k_cnf(it.n)>>>((float2*)it.p, it.n, /**/ tp.counts + i * 26);
+            k_rex::scatter<<<k_cnf(n)>>>((float2*)it.p, n, /**/ tp.counts + i * 26);
         }
         k_rex::scanA<<<1, 32>>>(tp.counts + i * 26, tp.offsets + 26 * i, /**/ tp.offsets + 26 * (i + 1), tp.starts + i * 27);
     }
