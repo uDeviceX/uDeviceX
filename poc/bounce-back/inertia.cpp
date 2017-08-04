8d39f24499d6c9eae2950e1442126de468251bde
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-04 09:42:53 +0200

    fix inertia.cpp: I

--- poc/bounce-back/inertia.cpp
+++ poc/bounce-back/inertia.cpp
@@ -8,7 +8,7 @@
 enum {X, Y, Z};
 enum {XX, XY, XZ, YY, YZ, ZZ};
 
-float diagI(const int d, const float A[3], const float B[3], const float C[3]) {
+float Moment(const int d, const float A[3], const float B[3], const float C[3]) {
     return 0.25f * (A[d] * A[d] +
                     A[d] * B[d] +
                     B[d] * B[d] +
@@ -18,20 +18,24 @@ float diagI(const int d, const float A[3], const float B[3], const float C[3]) {
 
 void compute_I(const float A[3], const float B[3], const float C[3], /**/ float I[6]) {
 
+    const float Mxx = Moment(X, A, B, C);
+    const float Myy = Moment(Y, A, B, C);
+    const float Mzz = Moment(Z, A, B, C);
+
     const float D1 = C[X] + B[X] + 2 * A[X];
     const float D2 = C[X] + 2 * B[X] + A[X];
     const float D3 = 2 * C[X] + B[X] + A[X];
 
-    I[XX] = diagI(X, A, B, C);
-    I[YY] = diagI(Y, A, B, C);
-    I[ZZ] = diagI(Z, A, B, C);
-
-    I[XY] = 0.125f * (D1 * A[Y] + D2 * B[Y] + D3 * C[Y]);
-    I[XZ] = 0.125f * (D1 * A[Z] + D2 * B[Z] + D3 * C[Z]);
+    const float D4 = C[Y] + B[Y] + 2 * A[Y];
+    const float D5 = C[Y] + 2 * B[Y] + A[Y];
+    const float D6 = 2 * C[Y] + B[Y] + A[Y];
     
-    I[YZ] = 0.125f * ( (C[Y] + B[Y] + 2 * A[Y]) * A[Z] +
-                       (C[Y] + 2 * B[Y] + A[Y]) * B[Z] +
-                       (2 * C[Y] + B[Y] + A[Y]) * C[Z] );    
+    I[XX] = Myy + Mzz;
+    I[XY] = -0.125f * (D1 * A[Y] + D2 * B[Y] + D3 * C[Y]);
+    I[XY] = -0.125f * (D1 * A[Z] + D2 * B[Z] + D3 * C[Z]);
+    I[YY] = Mzz + Mxx;
+    I[YZ] = -0.125f * (D4 * A[Z] + D5 * B[Z] + D6 * C[Z]);
+    I[ZZ] = Myy + Mxx;
 }
 
 void inverse(const float A[6], /**/ float I[6]) {
