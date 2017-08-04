78e66ec12728447e3ef5a55ca94f5346bad0aa36
Sergey Litvinov <slitvinov@gmail.com>
2017-08-04 23:17:34 +0200

    format

--- src/rex/ini.h
+++ src/rex/ini.h
@@ -1,24 +1,20 @@
 namespace rex {
 void ini() {
-    int i;
+    int i, estimate;
+    estimate = 10;
     mpDeviceMalloc(&packbuf);
     Palloc(&host_packbuf, MAX_PART_NUM);
 
     for (i = 0; i < 26; i++) local[i] = new LocalHalo;
     for (i = 0; i < 26; i++) remote[i] = new RemoteHalo;
-        
+
     for (i = 0; i < 26; ++i) {
-        int estimate = 10;
         remote[i]->preserve_resize(estimate);
         local[i]->resize(estimate);
         local[i]->update();
 
-        CC(cudaMemcpyToSymbol(k_rex::g::sizes,
-                              &local[i]->indexes->C, sizeof(int),
-                              sizeof(int) * i, H2D));
-        CC(cudaMemcpyToSymbol(k_rex::g::indexes,
-                              &local[i]->indexes->D, sizeof(int *),
-                              sizeof(int *) * i, H2D));
+        CC(cudaMemcpyToSymbol(k_rex::g::sizes,   &local[i]->indexes->C, sizeof(int),  sizeof(int)  * i, H2D));
+        CC(cudaMemcpyToSymbol(k_rex::g::indexes, &local[i]->indexes->D, sizeof(int*), sizeof(int*) * i, H2D));
     }
 }
 }
