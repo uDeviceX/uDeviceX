976a1efb0ef86f588d9bd1e9f9bc7cedcefd8443
Sergey Litvinov <slitvinov@gmail.com>
2017-08-04 17:19:45 +0200

    minor

--- src/rex/pack.h
+++ src/rex/pack.h
@@ -6,12 +6,13 @@ void clear(int nw, x::TicketPack tp) {
 }
 
 void pack(std::vector<ParticlesWrap> w, int nw, x::TicketPack tp, x::TicketPinned ti) {
+    int n;
     int *o, *c, *s;
     CC(cudaMemcpyAsync(ti.tstarts, tp.tstarts, sizeof(int) * 27, H2H));
     CC(cudaMemcpyToSymbolAsync(k_rex::g::tstarts, tp.tstarts, sizeof(int) * 27, 0, D2D));
     for (int i = 0; i < nw; ++i) {
-        ParticlesWrap it = w[i];
-        if (it.n) {
+        n = w[i].n;
+        if (n) {
             const Particle *pp = w[i].p;
             o = tp.offsets + 26 *  i;
             c = tp.counts  + 26 *  i;
@@ -19,7 +20,7 @@ void pack(std::vector<ParticlesWrap> w, int nw, x::TicketPack tp, x::TicketPinne
             CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, o, sizeof(int) * 26, 0, D2D));
             CC(cudaMemcpyToSymbolAsync(k_rex::g::counts,  c, sizeof(int) * 26, 0, D2D));
             CC(cudaMemcpyToSymbolAsync(k_rex::g::starts,  s, sizeof(int) * 27, 0, D2D));
-            k_rex::pack<<<14 * 16, 128>>>((float2 *)pp, /**/ (float2*)packbuf);
+            k_rex::pack<<<14 * 16, 128>>>((float2*)pp, /**/ (float2*)packbuf);
         }
     }
 }
