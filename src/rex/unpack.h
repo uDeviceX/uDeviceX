53181e294e359bebaa4b01c97160c92343818e5b
Sergey Litvinov <slitvinov@gmail.com>
2017-08-04 22:56:12 +0200

    minor

--- src/rex/unpack.h
+++ src/rex/unpack.h
@@ -7,12 +7,12 @@ void unpack(std::vector<ParticlesWrap> w, int nw, x::TicketPack tp) {
         n = w[i].n;
         if (n) {
             ff = w[i].f;
+            o = tp.offsets + 26 *  i;
             s = tp.starts  + 27 *  i;
             c = tp.counts  + 26 *  i;
-            o = tp.offsets + 26 *  i;
+            CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, o, sizeof(int) * 26, 0, D2D));
             CC(cudaMemcpyToSymbolAsync(k_rex::g::starts,  s, sizeof(int) * 27, 0, D2D));
             CC(cudaMemcpyToSymbolAsync(k_rex::g::counts,  c, sizeof(int) * 26, 0, D2D));
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, o, sizeof(int) * 26, 0, D2D));
             k_rex::unpack<<<16 * 14, 128>>>(/**/ (float*)ff);
         }
     }
