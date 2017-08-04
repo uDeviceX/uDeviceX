85fa35d8850a59c603240b7fa39b7ef7e6727481
Sergey Litvinov <slitvinov@gmail.com>
2017-08-04 17:43:40 +0200

    minor

--- src/fsi/setup.h
+++ src/fsi/setup.h
@@ -1,21 +1,25 @@
 namespace fsi {
+static void setup_first() {
+    k_fsi::texCellsStart.channelDesc = cudaCreateChannelDesc<int>();
+    k_fsi::texCellsStart.filterMode = cudaFilterModePoint;
+    k_fsi::texCellsStart.mipmapFilterMode = cudaFilterModePoint;
+    k_fsi::texCellsStart.normalized = 0;
+
+    k_fsi::texCellsCount.channelDesc = cudaCreateChannelDesc<int>();
+    k_fsi::texCellsCount.filterMode = cudaFilterModePoint;
+    k_fsi::texCellsCount.mipmapFilterMode = cudaFilterModePoint;
+    k_fsi::texCellsCount.normalized = 0;
+
+    k_fsi::texSolventParticles.channelDesc = cudaCreateChannelDesc<float2>();
+    k_fsi::texSolventParticles.filterMode = cudaFilterModePoint;
+    k_fsi::texSolventParticles.mipmapFilterMode = cudaFilterModePoint;
+    k_fsi::texSolventParticles.normalized = 0;
+}
+
 void setup(const Particle *const solvent, const int npsolvent,
            const int *const cellsstart, const int *const cellscount) {
     if (firsttime) {
-        k_fsi::texCellsStart.channelDesc = cudaCreateChannelDesc<int>();
-        k_fsi::texCellsStart.filterMode = cudaFilterModePoint;
-        k_fsi::texCellsStart.mipmapFilterMode = cudaFilterModePoint;
-        k_fsi::texCellsStart.normalized = 0;
-
-        k_fsi::texCellsCount.channelDesc = cudaCreateChannelDesc<int>();
-        k_fsi::texCellsCount.filterMode = cudaFilterModePoint;
-        k_fsi::texCellsCount.mipmapFilterMode = cudaFilterModePoint;
-        k_fsi::texCellsCount.normalized = 0;
-
-        k_fsi::texSolventParticles.channelDesc = cudaCreateChannelDesc<float2>();
-        k_fsi::texSolventParticles.filterMode = cudaFilterModePoint;
-        k_fsi::texSolventParticles.mipmapFilterMode = cudaFilterModePoint;
-        k_fsi::texSolventParticles.normalized = 0;
+        setup_first();
         firsttime = false;
     }
 
