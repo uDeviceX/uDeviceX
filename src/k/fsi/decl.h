93ca3e859e4f4e3451b9f82fb99a8e8d75495645
Sergey Litvinov <slitvinov@gmail.com>
2017-08-04 11:34:12 +0200

    make NCELLS local

--- src/k/fsi/decl.h
+++ src/k/fsi/decl.h
@@ -1,7 +1,6 @@
 namespace k_fsi {
 texture<float2, cudaTextureType1D> texSolventParticles;
 texture<int, cudaTextureType1D> texCellsStart, texCellsCount;
-static const int NCELLS = XS * YS * ZS;
 __constant__ int packstarts_padded[27], packcount[26];
 __constant__ Particle *packstates[26];
 __constant__ Force *packresults[26];
