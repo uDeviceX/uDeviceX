428732cd68cdca13e31760fb2dc860719c98f5f1
Sergey Litvinov <slitvinov@gmail.com>
2017-08-05 20:45:26 +0200

    ok if file directory exists

--- src/os.cu
+++ src/os.cu
@@ -6,11 +6,11 @@
 namespace os {
 void mkdir(const char *path) {
     mode_t mode;
-    int rc;
+    int rc, ok;
     mode = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
     rc = ::mkdir(path, mode);
-
-    if (!rc) {
+    ok = (rc == 0 || rc == EEXIST);
+    if (!ok) {
         fprintf(stderr, "os.cu: mkdir: cannot create directory ‘%s’\n", path);
         fprintf(stderr, "errno: %d\n", errno);
         exit(1);
