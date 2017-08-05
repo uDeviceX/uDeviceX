b7c7d1a7bf2132bbc9a881d1e36a80519b9d07ac
Sergey Litvinov <slitvinov@gmail.com>
2017-08-05 22:37:03 +0200

    use shift0

--- src/io/rbc.impl.h
+++ src/io/rbc.impl.h
@@ -1,3 +1,18 @@
+static void shift0(Particle* f, /**/ Particle* t) {
+    enum {X, Y, Z};
+    int *c;
+    c = m::coords;
+    t->r[X] = f->r[X] + 0.5*XS + c[X]*XS;
+    t->r[Y] = f->r[Y] + 0.5*YS + c[Y]*YS;
+    t->r[Z] = f->r[Z] + 0.5*ZS + c[Z]*ZS;
+}
+
+static void shift(Particle* f, int n, /**/ Particle* t) {
+    /* f, t: from, to */
+    int i;
+    for (i = 0; i < n; i++) shift0(f++, t++);
+}
+
 static void write(const void * const ptr, const int nbytes32, MPI_File f) {
     MPI_Offset base;
     MC(MPI_File_get_position(f, &base));
@@ -11,8 +26,7 @@ static void write(const void * const ptr, const int nbytes32, MPI_File f) {
 }
 
 static void dump0(Particle  *_particles, int *faces,
-                  int nc, int nv, int nt,
-                  const char * filename) {
+                  int nc, int nv, int nt, MPI_File f) {
     std::vector<Particle> particles(_particles, _particles + nc * nv);
     int NPOINTS = 0;
     const int n = particles.size();
@@ -20,12 +34,6 @@ static void dump0(Particle  *_particles, int *faces,
     const int ntriangles = nt * nc;
     int NTRIANGLES = 0;
     l::m::Reduce(&ntriangles, &NTRIANGLES, 1, MPI_INT, MPI_SUM, 0, m::cart) ;
-    MPI_File f;
-    MPI_File_open(m::cart, filename,
-                  MPI_MODE_WRONLY |  MPI_MODE_CREATE,
-                  MPI_INFO_NULL, &f);
-    MPI_File_set_size (f, 0);
-
     std::stringstream ss;
     if (m::rank == 0) {
         ss <<  "ply\n";
@@ -38,12 +46,7 @@ static void dump0(Particle  *_particles, int *faces,
         ss <<  "end_header\n";
     }
     std::string content = ss.str();
-
     write(content.c_str(), content.size(), f);
-    int L[3] = { XS, YS, ZS };
-    for(int i = 0; i < n; ++i)
-    for(int c = 0; c < 3; ++c)
-    particles[i].r[c] += L[c] / 2 + m::coords[c] * L[c];
 
     write(&particles.front(), sizeof(Particle) * n, f);
     int poffset = 0;
@@ -58,6 +61,24 @@ static void dump0(Particle  *_particles, int *faces,
         buf.insert(buf.end(), primitive, primitive + 4);
     }
     write(&buf.front(), sizeof(int) * buf.size(), f);
+}
+
+static void dump1(Particle  *pp, int *faces, int nc, int nv, int nt, MPI_File f) {
+    int sz, n;
+    Particle* pp0;
+    n = nc * nv;
+    sz = n*sizeof(Particle);
+    pp0 = (Particle*)malloc(sz);
+    shift(pp, n, /**/ pp0); /* copy-shift to global coordinates */
+    dump0(pp0, faces, nc, nv, nt, f);
+    free(pp0);
+}
+
+static void dump2(Particle  *pp, int *faces, int nc, int nv, int nt, const char *fn) {
+    MPI_File f;
+    MPI_File_open(m::cart, fn, MPI_MODE_WRONLY |  MPI_MODE_CREATE, MPI_INFO_NULL, &f);
+    MPI_File_set_size(f, 0);
+    dump1(pp, faces, nc, nv, nt, f);
     MPI_File_close(&f);
 }
 
@@ -66,5 +87,5 @@ void rbc_dump(Particle *pp, int *faces, int nc, int nv, int nt, int id) {
     char f[BUFSIZ]; /* file name */
     sprintf(f, fmt, id);
     if (m::rank == 0) os::mkdir(DUMP_BASE "/r");
-    dump0(pp, faces, nc, nv, nt, f);
+    dump2(pp, faces, nc, nv, nt, f);
 }
