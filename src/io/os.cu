c9351c39c51fd7e4e8b2dcea1f08df142fba3cf5
Sergey Litvinov <slitvinov@gmail.com>
2017-08-05 20:27:43 +0200

    add body of mkdir

--- src/io/os.cu
+++ src/io/os.cu
@@ -1,7 +1,17 @@
 #include <sys/stat.h>
-#include "io/os.cu"
+#include <stdio.h>
+#include "io/os.h"
 
 namespace os {
 void mkdir(const char *path) {
+    mode_t mode;
+    int rc;
+    mode = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
+    rc = ::mkdir(path, mode);
+
+    if (!rc) {
+        fprintf(stderr, "os.cu: mkdir: cannot create directory ‘%s’\n", path);
+        exit(1);
+    }
 }
 }
