2c4c275686913a8b20aaa1a5da9f7e80a2485594
Sergey Litvinov <slitvinov@gmail.com>
2017-08-06 19:03:47 +0200

    rename

--- src/io/field/grid.h
+++ src/io/field/grid.h
@@ -1,27 +1,26 @@
-static void grid(FILE * xmf,
-                 const char * const h5path, const char * const *channelnames, int nchannels) {
+static void grid(FILE * f, const char * const path, const char * const *names, int n) {
     enum {X, Y, Z};
     int i;
-    int *d, G[3]; /* global size */
+    int *d, G[3]; /* domain size */
     d = m::dims;
     G[X] = XS*d[X]; G[Y] = YS*d[Y]; G[Z] = ZS*d[Z];
 
-    fprintf(xmf, "   <Grid Name=\"mesh\" GridType=\"Uniform\">\n");
-    fprintf(xmf, "     <Topology TopologyType=\"3DCORECTMesh\" Dimensions=\"%d %d %d\"/>\n", 1 + G[Z], 1 + G[Y], 1 + G[X]);
-    fprintf(xmf, "     <Geometry GeometryType=\"ORIGIN_DXDYDZ\">\n");
-    fprintf(xmf, "       <DataItem Name=\"Origin\" Dimensions=\"3\" NumberType=\"Float\" Precision=\"4\" Format=\"XML\">\n");
-    fprintf(xmf, "        %e %e %e\n", 0.0, 0.0, 0.0);
-    fprintf(xmf, "       </DataItem>\n");
-    fprintf(xmf, "       <DataItem Name=\"Spacing\" Dimensions=\"3\" NumberType=\"Float\" Precision=\"4\" Format=\"XML\">\n");
-    fprintf(xmf, "        %e %e %e\n", 1.0, 1.0, 1.0);
-    fprintf(xmf, "       </DataItem>\n");
-    fprintf(xmf, "     </Geometry>\n");
-    for(i = 0; i < nchannels; ++i) {
-        fprintf(xmf, "     <Attribute Name=\"%s\" AttributeType=\"Scalar\" Center=\"Cell\">\n", channelnames[i]);
-        fprintf(xmf, "       <DataItem Dimensions=\"%d %d %d 1\" NumberType=\"Float\" Precision=\"4\" Format=\"HDF\">\n", G[Z], G[Y], G[X]);
-        fprintf(xmf, "        %s:/%s\n", h5path, channelnames[i]);
-        fprintf(xmf, "       </DataItem>\n");
-        fprintf(xmf, "     </Attribute>\n");
+    fprintf(f, "   <Grid Name=\"mesh\" GridType=\"Uniform\">\n");
+    fprintf(f, "     <Topology TopologyType=\"3DCORECTMesh\" Dimensions=\"%d %d %d\"/>\n", 1 + G[Z], 1 + G[Y], 1 + G[X]);
+    fprintf(f, "     <Geometry GeometryType=\"ORIGIN_DXDYDZ\">\n");
+    fprintf(f, "       <DataItem Name=\"Origin\" Dimensions=\"3\" NumberType=\"Float\" Precision=\"4\" Format=\"XML\">\n");
+    fprintf(f, "        %e %e %e\n", 0.0, 0.0, 0.0);
+    fprintf(f, "       </DataItem>\n");
+    fprintf(f, "       <DataItem Name=\"Spacing\" Dimensions=\"3\" NumberType=\"Float\" Precision=\"4\" Format=\"XML\">\n");
+    fprintf(f, "        %e %e %e\n", 1.0, 1.0, 1.0);
+    fprintf(f, "       </DataItem>\n");
+    fprintf(f, "     </Geometry>\n");
+    for(i = 0; i < n; ++i) {
+        fprintf(f, "     <Attribute Name=\"%s\" AttributeType=\"Scalar\" Center=\"Cell\">\n", names[i]);
+        fprintf(f, "       <DataItem Dimensions=\"%d %d %d 1\" NumberType=\"Float\" Precision=\"4\" Format=\"HDF\">\n", G[Z], G[Y], G[X]);
+        fprintf(f, "        %s:/%s\n", path, names[i]);
+        fprintf(f, "       </DataItem>\n");
+        fprintf(f, "     </Attribute>\n");
     }
-    fprintf(xmf, "   </Grid>\n");
+    fprintf(f, "   </Grid>\n");
 }
