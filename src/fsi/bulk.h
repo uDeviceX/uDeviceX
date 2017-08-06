711f3845b66021a1c89745dbf2fc77793dc02938
Sergey Litvinov <slitvinov@gmail.com>
2017-08-06 19:35:23 +0200

    minor

--- src/fsi/bulk.h
+++ src/fsi/bulk.h
@@ -1,17 +1,9 @@
 namespace fsi {
 void bulk(std::vector<ParticlesWrap> wsolutes) {
     if (wsolutes.size() == 0) return;
-
-    fsi::setup(wsolvent->p, wsolvent->n, wsolvent->cellsstart,
-               wsolvent->cellscount);
-
-
-
-    for (std::vector<ParticlesWrap>::iterator it = wsolutes.begin();
-         it != wsolutes.end(); ++it)
-    if (it->n)
-    k_fsi::bulk<<<k_cnf(3 * it->n)>>>
-        ((float2 *)it->p, it->n, wsolvent->n, local_trunk->get_float(), (float *)it->f, (float *)wsolvent->f);
-
+    fsi::setup(wsolvent->p, wsolvent->n, wsolvent->cellsstart, wsolvent->cellscount);
+    for (std::vector<ParticlesWrap>::iterator it = wsolutes.begin(); it != wsolutes.end(); ++it)
+        if (it->n)
+            k_fsi::bulk<<<k_cnf(3 * it->n)>>>((float2 *)it->p, it->n, wsolvent->n, local_trunk->get_float(), (float *)it->f, (float *)wsolvent->f);
 }
 }
