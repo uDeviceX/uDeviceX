ccb7bbd7ed9498d26725b6947228cd91901fa47a
Sergey Litvinov <slitvinov@gmail.com>
2017-08-07 12:12:06 +0200

    empty functions for ini split

--- src/rex/ini.h
+++ src/rex/ini.h
@@ -1,7 +1,14 @@
 namespace rex {
+static void ini_local(int estimate) { }
+static void ini_remote(int estimate) { }
+static void ini_copy() { }
+
 void ini() {
     int i, estimate;
     estimate = 10;
+    ini_local(estimate);
+    ini_remote(estimate);
+    ini_copy();
 
     for (i = 0; i < 26; i++) local[i] = new LocalHalo;
     for (i = 0; i < 26; i++) remote[i] = new RemoteHalo;
