3bf2e0e85da9feb5946115907c5c8e3f8d9aecdd
Sergey Litvinov <slitvinov@gmail.com>
2017-08-07 20:46:15 +0200

    splimify notation

--- src/sim/forces/dpd.h
+++ src/sim/forces/dpd.h
@@ -1,15 +1,20 @@
 void forces_dpd() {
-    dpdr::gather_cells(o::q.cells->start, o::q.cells->count, /**/ &o::h::ts);
-    if (o::h::tc.first) dpdr::post_expected_recv(&o::h::tc, &o::h::tr);
-    dpdr::copy_cells(&o::h::ts);
-    dpdr::pack(o::q.pp, /**/ &o::h::ts);
-    dpdr::post_send(&o::h::tc, &o::h::ts);
+    using namespace dpdr;
+    using namespace o;
+    int *count = q.cells->count;
+    int *start = q.cells->start;
+    
+    gather_cells(start, count, /**/ &h::ts);
+    if (h::tc.first) post_expected_recv(&h::tc, &h::tr);
+    copy_cells(&h::ts);
+    pack(q.pp, /**/ &h::ts);
+    post_send(&h::tc, &h::ts);
 
-    flocal(o::tz.zip0, o::tz.zip1, o::q.n, o::q.cells->start, o::q.cells->count, o::trnd.rnd, /**/ o::ff);
+    flocal(tz.zip0, tz.zip1, q.n, start, count, trnd.rnd, /**/ ff);
 
-    dpdr::wait_recv(&o::h::tc);
-    dpdr::recv(&o::h::tr);
-    dpdr::post_expected_recv(&o::h::tc, &o::h::tr);
-    dpdr::fremote(o::h::trnd, o::h::ts, o::h::tr, /**/ o::ff);
-    o::h::tc.first = false;
+    wait_recv(&h::tc);
+    recv(&h::tr);
+    post_expected_recv(&h::tc, &h::tr);
+    fremote(h::trnd, h::ts, h::tr, /**/ ff);
+    h::tc.first = false;
 }
