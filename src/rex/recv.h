8e45be47f29d726086c9bc1781470a7813515a12
Sergey Litvinov <slitvinov@gmail.com>
2017-08-07 13:02:32 +0200

    formatting

--- src/rex/recv.h
+++ src/rex/recv.h
@@ -1,25 +1,31 @@
 namespace rex {
 void recvF(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
-    for (int i = 0; i < 26; ++i) {
-        MPI_Request reqA;
+    int i;
+    MPI_Request reqA;
+    for (i = 0; i < 26; ++i) {
         MC(l::m::Irecv(local[i]->ff->D, local[i]->ff->S * 3, MPI_FLOAT, ranks[i], t.btf + tags[i], cart, &reqA));
         reqrecvA.push_back(reqA);
     }
 }
 
 void recvC(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
-    for (int i = 0; i < 26; ++i) {
-        MPI_Request reqC;
-        MC(l::m::Irecv(recv_counts + i, 1, MPI_INTEGER, ranks[i], t.btc + tags[i], cart, &reqC));
+    int i, tag;
+    MPI_Request reqC;
+    for (i = 0; i < 26; ++i) {
+        tag = t.btc + tags[i];
+        MC(l::m::Irecv(recv_counts + i, 1, MPI_INTEGER, ranks[i], tag, cart, &reqC));
         reqrecvC.push_back(reqC);
     }
 }
 
 void recvP(MPI_Comm cart, int ranks[26], int tags[26], x::TicketTags t) {
-    for (int i = 0; i < 26; ++i) {
-        MPI_Request reqP;
-        remote[i]->pmessage.resize(remote[i]->expected());
-        MC(l::m::Irecv(&remote[i]->pmessage.front(), remote[i]->expected() * 6, MPI_FLOAT, ranks[i], t.btp1 + tags[i], cart, &reqP));
+    int i, tag, n;
+    MPI_Request reqP;
+    for (i = 0; i < 26; ++i) {
+        tag = t.btp1 + tags[i];
+        n = remote[i]->expected();
+        remote[i]->pmessage.resize(n);
+        MC(l::m::Irecv(&remote[i]->pmessage.front(), n * 6, MPI_FLOAT, ranks[i], tag, cart, &reqP));
         reqrecvP.push_back(reqP);
     }
 }
