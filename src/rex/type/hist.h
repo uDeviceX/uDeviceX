bb38da0e66a153aada0a94125595d9b5c4a1e8b0
Sergey Litvinov <slitvinov@gmail.com>
2017-08-07 12:44:08 +0200

    minor

--- src/rex/type/hist.h
+++ src/rex/type/hist.h
@@ -1,14 +1,14 @@
 namespace rex {
 class History {
     static const int N = 200;
-    int count, data[N];
+    int cnt, D[N];
 public:
-    History() : count(0) {}
-    void update(int val) { data[count++ % N] = ::max(0, val); }
+    History() : cnt(0) {}
+    void update(int val) { D[cnt++ % N] = ::max(0, val); }
     int max() const {
-        int retval = 0;
-        for (int i = 0; i < min(N, count); ++i) retval = ::max(data[i], retval);
-        return retval;
+        int i, rc = 0;
+        for (i = 0; i < min(N, cnt); ++i) rc = ::max(D[i], rc);
+        return rc;
     }
 };
 }
