8109f76d5d3acb9687911860aaf1756c8fb9ed7d
Sergey Litvinov <slitvinov@gmail.com>
2017-08-07 12:14:33 +0200

    split ini()

--- src/rex/ini.h
+++ src/rex/ini.h
@@ -1,25 +1,33 @@
 namespace rex {
-static void ini_local(int estimate) { }
-static void ini_remote(int estimate) { }
-static void ini_copy() { }
+static void ini_local(int estimate) {
+    int i;
+    for (i = 0; i < 26; i++) {
+        local[i] = new LocalHalo;
+        local[i]->resize(estimate);
+        local[i]->update();
+    }
+}
+
+static void ini_remote(int estimate) {
+    int i;
+    for (i = 0; i < 26; i++) {
+        remote[i] = new RemoteHalo;
+        remote[i]->resize(estimate);
+    }
+}
+static void ini_copy() {
+    int i;
+    for (i = 0; i < 26; ++i) {
+        CC(cudaMemcpyToSymbol(k_rex::g::sizes,   &local[i]->indexes->C, sizeof(int),  sizeof(int)  * i, H2D));
+        CC(cudaMemcpyToSymbol(k_rex::g::indexes, &local[i]->indexes->D, sizeof(int*), sizeof(int*) * i, H2D));
+    }
+}
 
 void ini() {
-    int i, estimate;
+    int estimate;
     estimate = 10;
     ini_local(estimate);
     ini_remote(estimate);
     ini_copy();
-
-    for (i = 0; i < 26; i++) local[i] = new LocalHalo;
-    for (i = 0; i < 26; i++) remote[i] = new RemoteHalo;
-
-    for (i = 0; i < 26; ++i) {
-        remote[i]->resize(estimate);
-        local[i]->resize(estimate);
-        local[i]->update();
-
-        CC(cudaMemcpyToSymbol(k_rex::g::sizes,   &local[i]->indexes->C, sizeof(int),  sizeof(int)  * i, H2D));
-        CC(cudaMemcpyToSymbol(k_rex::g::indexes, &local[i]->indexes->D, sizeof(int*), sizeof(int*) * i, H2D));
-    }
 }
 }
