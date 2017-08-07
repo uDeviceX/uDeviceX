cde840342a5100f4cfa419051d80a0a602e8d480
Sergey Litvinov <slitvinov@gmail.com>
2017-08-07 12:53:06 +0200

    add pack0()

--- src/rex/pack.h
+++ src/rex/pack.h
@@ -5,22 +5,24 @@ void clear(int nw, x::TicketPack tp) {
     CC(cudaMemsetAsync(tp.offsets, 0, sizeof(int) * 26 * (nw + 1)));
 }
 
+static void pack0(std::vector<ParticlesWrap> w, x::TicketPack tp, int i, /**/ Particle *buf) {
+    int *o, *c, *s;
+    const Particle *pp = w[i].p;
+    o = tp.offsets + 26 *  i;
+    c = tp.counts  + 26 *  i;
+    s = tp.starts  + 27 *  i;
+    CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, o, sizeof(int) * 26, 0, D2D));
+    CC(cudaMemcpyToSymbolAsync(k_rex::g::counts,  c, sizeof(int) * 26, 0, D2D));
+    CC(cudaMemcpyToSymbolAsync(k_rex::g::starts,  s, sizeof(int) * 27, 0, D2D));
+    k_rex::pack<<<14 * 16, 128>>>((float2*)pp, /**/ (float2*)buf);
+}
+
 void pack(std::vector<ParticlesWrap> w, int nw, x::TicketPack tp, Particle *buf) {
     int i, n;
-    int *o, *c, *s;
     CC(cudaMemcpyToSymbolAsync(k_rex::g::tstarts, tp.tstarts, sizeof(int) * 27, 0, D2D));
     for (i = 0; i < nw; ++i) {
         n = w[i].n;
-        if (n) {
-            const Particle *pp = w[i].p;
-            o = tp.offsets + 26 *  i;
-            c = tp.counts  + 26 *  i;
-            s = tp.starts  + 27 *  i;
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::offsets, o, sizeof(int) * 26, 0, D2D));
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::counts,  c, sizeof(int) * 26, 0, D2D));
-            CC(cudaMemcpyToSymbolAsync(k_rex::g::starts,  s, sizeof(int) * 27, 0, D2D));
-            k_rex::pack<<<14 * 16, 128>>>((float2*)pp, /**/ (float2*)buf);
-        }
+        if (n) pack0(w, tp, i, /**/ buf);
     }
 }
 
