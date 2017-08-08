f73f08e7093340f8d7ceb251e7feeae36cfb7d0a
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 21:48:22 +0200

    add zero

--- src/kl/safe.h
+++ src/kl/safe.h
@@ -1,3 +1,7 @@
-#define KL_BEFORE(...)
+inline int zero(int i, int j) {
+    return 1;
+}
+
+#define KL_BEFORE(i, j, ...) if (zero(i, j)) continue;
 #define KL_AFTER(s) MSG("safe: %s", s);
 #define KL_CALL(F, C, A) F<<<ESC C>>>A
