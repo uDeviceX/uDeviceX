b82dacf650cc5599eb6b6cdb4b5cec12fce0758c
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-08-08 22:42:52 +0200

    more KL

--- src/fsi/bulk.h
+++ src/fsi/bulk.h
@@ -4,6 +4,8 @@ void bulk(std::vector<ParticlesWrap> wsolutes) {
     fsi::setup(wsolvent->p, wsolvent->n, wsolvent->cellsstart, wsolvent->cellscount);
     for (std::vector<ParticlesWrap>::iterator it = wsolutes.begin(); it != wsolutes.end(); ++it)
         if (it->n)
-            k_fsi::bulk<<<k_cnf(3 * it->n)>>>((float2 *)it->p, it->n, wsolvent->n, local_trunk->get_float(), (float *)it->f, (float *)wsolvent->f);
+            KL(k_fsi::bulk,
+               (k_cnf(3*it->n)),
+               ((float2 *)it->p, it->n, wsolvent->n, local_trunk->get_float(), (float*)it->f, (float*)wsolvent->f));
 }
 }
