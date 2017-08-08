db0f7d3cc86aaf152b8f2f5ec9c22e7540506b71
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-08-08 23:47:43 +0200

    safe only check the size

--- src/kl/safe.h
+++ src/kl/safe.h
@@ -2,7 +2,6 @@ inline int good0(const char s[], dim3 i, dim3 j) {
     int rc;
     rc = i.x>0 && i.y>0 && i.z>0 &&
          j.x>0 && j.y>0 && j.z>0;
-    if (!rc) ERR("s: %s", s);
     return rc;
 }
 inline int good(const char s[], dim3 i, dim3 j)                    { return good0(s, i, j); }
@@ -10,5 +9,5 @@ inline int good(const char s[], dim3 i, dim3 j, int)               { return good
 inline int good(const char s[], dim3 i, dim3 j, int, cudaStream_t) { return good0(s, i, j); }
 
 #define KL_BEFORE(s, C) if (!good(s, ESC C)) continue;
-#define KL_AFTER(s) CC(cudaPeekAtLastError())
+#define KL_AFTER(s)
 #define KL_CALL(F, C, A) F<<<ESC C>>>A
