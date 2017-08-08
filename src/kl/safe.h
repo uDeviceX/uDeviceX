3fb3327d8be80723a04ead6ce47cb46f3dc33ac0
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 21:58:44 +0200

    two zeros

--- src/kl/safe.h
+++ src/kl/safe.h
@@ -2,6 +2,10 @@ inline int zero(dim3 i, dim3 j) {
     return 1;
 }
 
+inline int zero(dim3 i, dim3 j, int n) {
+    return 1;
+}
+
 #define KL_BEFORE(s, C) if (zero(C)) continue;
 #define KL_AFTER(s) MSG("safe: %s", s);
 #define KL_CALL(F, C, A) F<<<ESC C>>>A
