3d20eed0975491831adb97aa9453553765fed712
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 12:44:48 +0200

    format

--- src/dpd/imp/flocal.h
+++ src/dpd/imp/flocal.h
@@ -1,32 +1,32 @@
 void flocal0(float4 *zip0, ushort4 *zip1, int np, int *start, int *count, float seed, float* ff) {
     static InfoDPD c;
-    if( !fdpd_init ) {
+    if(!fdpd_init) {
         setup();
         mps();
         fdpd_init = true;
     }
     tex(zip0, zip1, np, start, count);
-    c.ncells = make_int3( XS, YS, ZS );
+    c.ncells = make_int3(XS, YS, ZS);
     c.nxyz = XS * YS * ZS;
     c.ff = ff;
     c.seed = seed;
 
     if (!is_mps_enabled)
-	CC( cudaMemcpyToSymbolAsync( info, &c, sizeof( c ), 0, cudaMemcpyHostToDevice) );
+	CC(cudaMemcpyToSymbolAsync(info, &c, sizeof(c), 0, cudaMemcpyHostToDevice));
     else
-	CC( cudaMemcpyToSymbol( info, &c, sizeof( c ), 0, cudaMemcpyHostToDevice ) );
+	CC(cudaMemcpyToSymbol(info, &c, sizeof(c), 0, cudaMemcpyHostToDevice));
 
     int np32 = np;
-    if( np32 % 32 ) np32 += 32 - np32 % 32;
-    CC( cudaMemsetAsync( ff, 0, sizeof( float )* np32 * 3) );
+    if(np32 % 32) np32 += 32 - np32 % 32;
+    CC(cudaMemsetAsync(ff, 0, sizeof(float)* np32 * 3));
 
-    if( c.ncells.x % MYCPBX == 0 && c.ncells.y % MYCPBY == 0 && c.ncells.z % MYCPBZ == 0 ) {
-        merged<<< dim3( c.ncells.x / MYCPBX, c.ncells.y / MYCPBY, c.ncells.z / MYCPBZ ), dim3( 32, MYWPB ), 0>>> ();
+    if(XS % MYCPBX == 0 && YS % MYCPBY == 0 && ZS % MYCPBZ == 0) {
+        merged<<<dim3(XS / MYCPBX, YS / MYCPBY, ZS / MYCPBZ), dim3(32, MYWPB), 0>>>();
         CC(cudaPeekAtLastError());
         transpose<<< 28, 1024, 0>>>(np);
         CC(cudaPeekAtLastError());
     } else {
-        fprintf( stderr, "Incompatible grid config\n" );
+        fprintf(stderr, "Incompatible grid config\n");
     }
 }
 
