e064566cd5e0e257924b24d6d974bc1fffe5b253
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-08-08 23:55:19 +0200

    simplify safe

--- src/kl/safe.h
+++ src/kl/safe.h
@@ -1,13 +1,10 @@
-inline int good0(const char s[], dim3 i, dim3 j) {
-    int rc;
-    rc = i.x>0 && i.y>0 && i.z>0 &&
-         j.x>0 && j.y>0 && j.z>0;
-    return rc;
+inline int safe0(dim3 i, dim3 j) {
+    return i.x>0 && i.y>0 && i.z>0   &&   j.x>0 && j.y>0 && j.z>0;
 }
-inline int good(const char s[], dim3 i, dim3 j)                    { return good0(s, i, j); }
-inline int good(const char s[], dim3 i, dim3 j, int)               { return good0(s, i, j); }
-inline int good(const char s[], dim3 i, dim3 j, int, cudaStream_t) { return good0(s, i, j); }
+inline int safe(dim3 i, dim3 j)                    { return safe0(i, j); }
+inline int safe(dim3 i, dim3 j, int)               { return safe0(i, j); }
+inline int safe(dim3 i, dim3 j, int, cudaStream_t) { return safe0(i, j); }
 
-#define KL_BEFORE(s, C) if (!good(s, ESC C)) continue;
+#define KL_BEFORE(s, C) if (!safe(ESC C)) continue;
 #define KL_AFTER(s)
 #define KL_CALL(F, C, A) F<<<ESC C>>>A
