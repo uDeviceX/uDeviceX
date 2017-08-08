f548c6e06d6b33a2647248ee6322edef1a39c4c2
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 13:16:57 +0200

    remove peeks

--- src/sim/forces/dpd.h
+++ src/sim/forces/dpd.h
@@ -3,17 +3,11 @@ void forces_dpd() {
     using namespace o;
     int *count = q.cells->count;
     int *start = q.cells->start;
-    CC(cudaPeekAtLastError());
     gather_cells(start, count, /**/ &h::ts);
-    CC(cudaPeekAtLastError());
     if (h::tc.first) post_expected_recv(&h::tc, &h::tr);
-    CC(cudaPeekAtLastError());
     copy_cells(&h::ts);
-    CC(cudaPeekAtLastError());
     pack(q.pp, /**/ &h::ts);
-    CC(cudaPeekAtLastError());
     post_send(&h::tc, &h::ts);
-    CC(cudaPeekAtLastError());
     flocal(tz.zip0, tz.zip1, q.n, start, count, trnd.rnd, /**/ ff);
 
     wait_recv(&h::tc);
