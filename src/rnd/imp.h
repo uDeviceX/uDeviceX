64878843d428fa1fd9e5703252545af8211b9df0
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 20:22:55 +0200

    split rnd.h

---
+++ src/rnd/imp.h
@@ -0,0 +1,30 @@
+namespace rnd {
+// random number from the ArcSine distribution on [-sqrt(2),sqrt(2)]
+// mean = 0
+// variance = 1
+// can be used directly for DPD
+struct KISS {
+    typedef uint32_t integer;
+    integer x, y, z, c;
+
+    KISS() : x( 0 ), y( 0 ), z( 0 ), c( 0 ) {}
+
+    KISS( integer x_, integer y_, integer z_, integer c_ ) :
+        x( x_ ), y( y_ ), z( z_ ), c( c_ ) {}
+
+    float get_float() {
+        return get_int() / float( std::numeric_limits<integer>::max() );
+    }
+
+    integer get_int() {
+        uint64_t t, a = 698769069ULL;
+        x = 69069 * x + 12345;
+        y ^= ( y << 13 );
+        y ^= ( y >> 17 );
+        y ^= ( y << 5 ); /* y must never be set to zero! */
+        t = a * z + c;
+        c = ( t >> 32 ); /* Also avoid setting z=c=0! */
+        return x + y + ( z = t );
+    }
+};
+}
