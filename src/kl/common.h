bc2e2ca7f4c1ccb769784a9525db9edb6c74f5a7
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 16:33:29 +0200

    minor

--- src/kl/common.h
+++ src/kl/common.h
@@ -1,6 +1,6 @@
 #define ESC(...) __VA_ARGS__
 
-#define KL(F, C, A)                            \
+#define KL(F, C, A)                             \
     do  {                                       \
         KL_BEFORE(#F, ESC C);                   \
         KL_CALL(F, C, A);                       \
