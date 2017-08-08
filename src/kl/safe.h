2dddfc93a8bbf81e02bf4e6c2cf5607ab1f5f4e6
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 21:49:49 +0200

    use dim3

--- src/kl/safe.h
+++ src/kl/safe.h
@@ -1,7 +1,7 @@
-inline int zero(int i, int j) {
+inline int zero(dim3 i, dim3 j) {
     return 1;
 }
 
-#define KL_BEFORE(i, j, ...) if (zero(i, j)) continue;
+#define KL_BEFORE(name, i, j, ...) if (zero(i, j)) continue;
 #define KL_AFTER(s) MSG("safe: %s", s);
 #define KL_CALL(F, C, A) F<<<ESC C>>>A
