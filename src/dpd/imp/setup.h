600b1410186fda03013191e1d9626ca12d6703bf
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 12:25:18 +0200

    make it even more compile

--- src/dpd/imp/setup.h
+++ src/dpd/imp/setup.h
@@ -1,4 +1,4 @@
-static setupt() {
+static void setup() {
     texStartAndCount.channelDesc = cudaCreateChannelDesc<uint2>();
     texStartAndCount.filterMode  = cudaFilterModePoint;
     texStartAndCount.mipmapFilterMode = cudaFilterModePoint;
