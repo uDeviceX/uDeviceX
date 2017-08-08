f34652b89ef919ae879e5ac5717987c05d0dcb1c
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-08-08 23:41:24 +0200

    should be the last kernel

--- src/scan/imp.cu
+++ src/scan/imp.cu
@@ -2,6 +2,10 @@
 #include <common.h>
 #include "common.cuda.h"
 
+#include "m.h"
+#include "conf.common.h"
+#include "kl/kl.h"
+
 #include "scan/int.h"
 #include "scan/dev.h"
 
@@ -11,14 +15,13 @@ enum { THREADS = 128 };
 
 static void scan0(const unsigned char *input, int size, /**/ uint *output, /*w*/ uint *tmp) {
     int nblocks = ((size / 16) + THREADS - 1 ) / THREADS;
-    dev::breduce<THREADS/32 ><<<nblocks, THREADS                >>>((uint4 *)input, tmp, size / 16);
-    dev::bexscan<THREADS>    <<<1, THREADS, nblocks*sizeof(uint)>>>(tmp, nblocks);
-    dev::gexscan<THREADS/32> <<<nblocks, THREAD                 >>>((uint4 *)input, tmp, (uint4 *)output, size / 16);
+    KL(dev::breduce<THREADS/32>, (nblocks, THREADS                ), ((uint4 *)input, tmp, size / 16));
+    KL(dev::bexscan<THREADS>   , (1, THREADS, nblocks*sizeof(uint)), (tmp, nblocks));
+    KL(dev::gexscan<THREADS/32>, (nblocks, THREADS                ), ((uint4 *)input, tmp, (uint4 *)output, size / 16));
 }
 
 void scan(const int *input, int size, /**/ int *output, /*w*/ Work *w) {
-    dev::compress<<<k_cnf(size)>>>(size, (const int4*) input, /**/ (uchar4 *) w->compressed);
-
+    KL(dev::compress, (k_cnf(size)), (size, (const int4*) input, /**/ (uchar4 *) w->compressed));
     scan0(w->compressed, size, /**/ (uint*) output, /*w*/ w->tmp);
 }
 
