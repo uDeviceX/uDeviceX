444d0f98a6f9b992e7213fcc5e4e3b3e16047b3c
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 16:32:34 +0200

    add kl/common.h

---
+++ src/kl/common.h
@@ -0,0 +1,8 @@
+#define ESC(...) __VA_ARGS__
+
+#define KL(F, C, A)                            \
+    do  {                                       \
+        KL_BEFORE(#F, ESC C);                   \
+        KL_CALL(F, C, A);                       \
+        KL_AFTER(#F);                           \
+    } while (0)
