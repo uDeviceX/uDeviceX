186ce580a3a91b2a392469d6411b3704e51080c4
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 21:57:47 +0200

    upd

--- src/kl/safe.h
+++ src/kl/safe.h
@@ -2,6 +2,6 @@ inline int zero(dim3 i, dim3 j) {
     return 1;
 }
 
-#define KL_BEFORE(name, i, j, ...) if (zero(i, j)) continue;
+#define KL_BEFORE(s, C) if (zero(C)) continue;
 #define KL_AFTER(s) MSG("safe: %s", s);
 #define KL_CALL(F, C, A) F<<<ESC C>>>A
