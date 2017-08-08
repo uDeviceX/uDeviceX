448d8aad4b51b01f5f32fd58c8eeca8595aa2195
Sergey Litvinov <slitvinov@gmail.com>
2017-08-08 17:12:40 +0200

    upd poc/kl/kl.h

--- poc/kl/kl.h
+++ poc/kl/kl.h
@@ -1,7 +1,7 @@
 #define ESC(...) __VA_ARGS__
 #define KL_BEFORE(...)
 #define KL_AFTER(s)
-#define KL_CALL(F, C, A) F<<ESC C>>A
+#define KL_CALL(F, C, A) F<<<ESC C>>>A
 
 #define KL(F, C, A)                            \
     do  {                                       \
@@ -11,8 +11,14 @@
     } while (0)
 
 /* examples */
+/* ceiling `m' to `n' (returns the smallest `A' such n*A is not less than `m') */
+#define ceiln(m, n) (   ((m) + (n) - 1)/(n)   )
+/* a common kernel execution configuration */
+#define k_cnf(n) ceiln((n), 128), 128
+
 KL(fun, (i+1, j, k), ())
 KL(fun, (i+1, j, k), (a, b))
+KL(dev::update, (k_cnf(o::q.n)), (dpd_mass, o::q.pp, o::ff, o::q.n));
 
 /* run the command to expand
 
