52c96bcd2ae3808410d1faa6d215ff6b8bec2869
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-08-09 23:18:54 +0200

    one more layer

--- src/rig/ini.h
+++ src/rig/ini.h
@@ -10,7 +10,7 @@ static void ini0(const Mesh m, int nsolid, float *coms, /**/
 
     int root, idmax;
     elect(rcounts, nsolid, /**/ &root, &idmax);
-    
+
     MC(MPI_Bcast(&idmax, 1, MPI_INT, root, l::m::cart));
 
     int rcount = 0;
@@ -27,19 +27,19 @@ static void ini0(const Mesh m, int nsolid, float *coms, /**/
     {
         npsolid = rcount;
         if (!npsolid) ERR("No particles remaining in root node.\n");
-            
+
         for (int d = 0; d < 3; ++d)
         model.com[d] = coms[idmax*3 + d];
-    
+
         solid::ini(r_pp, npsolid, solid_mass, model.com, m, /**/ rr0, &model);
 
         empty_solid(m, /* io */ rr0, &npsolid);
     }
-    
+
     MC(MPI_Bcast(&npsolid,       1,   MPI_INT, root, l::m::cart) );
     MC(MPI_Bcast(rr0,  3 * npsolid, MPI_FLOAT, root, l::m::cart) );
     MC(MPI_Bcast(&model, 1, datatype::solid, root, l::m::cart) );
-    
+
     // filter coms to keep only the ones in my domain
     int id = 0;
     for (int j = 0; j < nsolid; ++j) {
@@ -49,7 +49,7 @@ static void ini0(const Mesh m, int nsolid, float *coms, /**/
             -YS/2 <= com[Y] && com[Y] < YS/2 &&
             -ZS/2 <= com[Z] && com[Z] < ZS/2 ) {
             ss[id] = model;
-            
+
             for (int d = 0; d < 3; ++d)
             ss[id].com[d] = com[d];
 
@@ -64,15 +64,21 @@ static void ini0(const Mesh m, int nsolid, float *coms, /**/
 }
 
 static void ini1(const Mesh m, int nsolid, float *coms, /**/
+                 int *ns, int *nps, float *rr0, Solid *ss, int *s_n, Particle *s_pp, Particle *r_pp,
+                 /*w*/ int *tags, int *rcounts) {
+    ini0(m, nsolid, coms, /**/ ns, nps, rr0, ss, s_n, s_pp, r_pp, /*w*/ tags, rcounts);
+}
+
+static void ini2(const Mesh m, int nsolid, float *coms, /**/
                  int *ns, int *nps, float *rr0, Solid *ss, int *s_n, Particle *s_pp, Particle *r_pp) {
     int *tags = new int[*s_n];
     int *rcounts = new int[nsolid];
-    ini0(m, nsolid, coms, /**/ ns, nps, rr0, ss, s_n, s_pp, r_pp, /*w*/ tags, rcounts);
+    ini1(m, nsolid, coms, /**/ ns, nps, rr0, ss, s_n, s_pp, r_pp, /*w*/ tags, rcounts);
     delete[] rcounts;
     delete[] tags;
 }
 
-static void ini2(const char *fname, const Mesh m, /**/
+static void ini3(const char *fname, const Mesh m, /**/
                  int *ns, int *nps, float *rr0, Solid *ss, int *s_n, Particle *s_pp, Particle *r_pp,
                  /*w*/ float *coms) {
     float3 minbb, maxbb;
@@ -81,13 +87,13 @@ static void ini2(const char *fname, const Mesh m, /**/
     mesh::get_bbox(m.vv, m.nv, /**/ &minbb, &maxbb);
     nsolid = duplicate_PBC(minbb, maxbb, nsolid, /**/ coms);
     make_local(nsolid, /**/ coms);
-    ini1(m, nsolid, coms, /**/ ns, nps, rr0, ss, s_n, s_pp, r_pp);
+    ini2(m, nsolid, coms, /**/ ns, nps, rr0, ss, s_n, s_pp, r_pp);
 }
 
 void ini(const char *fname, const Mesh m, /**/
          int *ns, int *nps, float *rr0, Solid *ss, int *s_n, Particle *s_pp, Particle *r_pp) {
     float *coms = new float[MAX_SOLIDS * 3 * 10];
-    ini2(fname, m, /**/ ns, nps, rr0, ss, s_n, s_pp, r_pp, /*w*/ coms);
+    ini3(fname, m, /**/ ns, nps, rr0, ss, s_n, s_pp, r_pp, /*w*/ coms);
     delete[] coms;
 }
 
