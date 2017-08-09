9b1105a5fb134fb49e87b43121dc7fd2bcade76a
Sergey Litvinov <slitvinov@gmail.com>
2017-08-09 21:35:44 +0200

    trace and peek in trace.peek.h

--- src/kl/trace.peek.h
+++ src/kl/trace.peek.h
@@ -1,3 +1,4 @@
-#define KL_BEFORE(s, C) if (!kl::safe(ESC C)) continue;
-#define KL_AFTER(s) MSG("kl: %s", s);
+#define KL_BEFORE(s, C) MSG("kl: %s", s),       \
+        if (!kl::safe(ESC C)) continue;
+#define KL_AFTER(s) CC(cudaPeekAtLastError());
 #define KL_CALL(F, C, A) F<<<ESC C>>>A
