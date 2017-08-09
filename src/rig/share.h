5b57cea77692a5d41f47ed4e92c04ca35a779465
Litvinov Sergey (panda) <slitvinov@gmail.com>
2017-08-10 00:18:05 +0200

    do coordinates transform in share0

--- src/rig/share.h
+++ src/rig/share.h
@@ -4,15 +4,6 @@ namespace ic {
 
 
 static void share0(const int root, /**/ Particle *pp, int *n) {
-    // set to global coordinates and then convert back to local
-    const int L[3] = {XS, YS, ZS};
-    int mi[3];
-    for (int c = 0; c < 3; ++c) mi[c] = (m::coords[c] + 0.5) * L[c];
-
-    for (int i = 0; i < *n; ++i)
-    for (int c = 0; c < 3; ++c)
-        pp[i].r[c] += mi[c];
-
     std::vector<int> counts(m::size), displs(m::size);
     std::vector<Particle> recvbuf(MAX_PSOLID_NUM);
 
@@ -37,13 +28,18 @@ static void share0(const int root, /**/ Particle *pp, int *n) {
         for (int i = 0; i < *n; ++i) pp[i] = recvbuf[i];
     }
 
-    for (int i = 0; i < *n; ++i)
-    for (int c = 0; c < 3; ++c)
-        pp[i].r[c] -= mi[c];
 }
 
 static void share(const int root, /**/ Particle *pp, int *n) {
+    // set to global coordinates and then convert back to local
+    int i, c;
+    const int L[3] = {XS, YS, ZS};
+    int mi[3];
+    for (c = 0; c < 3; ++c) mi[c] = (m::coords[c] + 0.5) * L[c];
+    for (i = 0; i < *n; ++i) for (c = 0; c < 3; ++c) pp[i].r[c] += mi[c];
     share(root, /**/ pp, n);
+    for (i = 0; i < *n; ++i) for (c = 0; c < 3; ++c) pp[i].r[c] -= mi[c];
+
 }
 
 } // ic
