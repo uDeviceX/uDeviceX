f85049d8ad862269a11c2274df6851121577dee4
Sergey Litvinov <slitvinov@gmail.com>
2017-08-11 00:36:50 +0200

    upd cc/sync.h

--- src/cc/sync.h
+++ src/cc/sync.h
@@ -2,13 +2,6 @@
 
 /* [c]cuda [c]heck */
 #define CC(ans)                                             \
-    do { cudaDeviceSynchronize(); cudaAssert((ans), __FILE__, __LINE__); cudaDeviceSynchronize(); } while (0)
-
-inline void cudaAssert(cudaError_t rc, const char *file, int line) {
-    if (rc != cudaSuccess) {
-        fprintf(stderr, "GPU assert: %s %s %d\n", cudaGetErrorString(rc), file, line);
-        abort();
-    }
-}
-
-
+    do { cudaDeviceSynchronize();                           \
+        cudaAssert((ans), __FILE__, __LINE__);              \
+        cudaDeviceSynchronize(); } while (0)
