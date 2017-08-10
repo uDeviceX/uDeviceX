e567a0d46eaa9ee5f0662bc7a4182f23592150e7
Sergey Litvinov <slitvinov@gmail.com>
2017-08-10 18:20:02 +0200

    unused function; use enum {X, Y, Z}

--- src/sim/dev.h
+++ src/sim/dev.h
@@ -24,27 +24,20 @@ __global__ void update(float mass, Particle* pp, Force* ff, int n) {
 }
 
 __global__ void body_force(float mass, Particle* pp, Force* ff, int n, float driving_force0) {
+    enum {X, Y, Z};
     int pid = threadIdx.x + blockDim.x * blockIdx.x;
     if (pid >= n) return;
 
     float *r = pp[pid].r, *f = ff[pid].f;
 
-    float dy = r[1] - glb::r0[1]; /* coordinate relative to domain
+    float d = r[Y] - glb::r0[Y]; /* coordinate relative to domain
                                      center */
-    if (doublepoiseuille && dy <= 0) driving_force0 *= -1;
-    f[0] += mass*driving_force0;
+    if (doublepoiseuille && d <= 0) driving_force0 *= -1;
+    f[X] += mass*driving_force0;
 }
 
 __global__ void clear_vel(Particle *pp, int n)  {
     int pid = threadIdx.x + blockDim.x * blockIdx.x;
     if (pid >= n) return;
     for(int c = 0; c < 3; ++c) pp[pid].v[c] = 0;
-}
-
-__global__ void ic_shear_velocity(Particle *pp, int n)  {
-    int pid = threadIdx.x + blockDim.x * blockIdx.x;
-    if (pid >= n) return;
-    float z = pp[pid].r[2] - glb::r0[2];
-    float vx = gamma_dot*z, vy = 0, vz = 0;
-    pp[pid].v[0] = vx; pp[pid].v[1] = vy; pp[pid].v[2] = vz;
 }
