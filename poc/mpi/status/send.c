f20d16cfc7b0486d7e1ef899fca5ff043cd78abd
Sergey Litvinov <slitvinov@gmail.com>
2017-08-11 19:46:50 +0200

    import mpi/status

---
+++ poc/mpi/status/send.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <mpi.h>
+
+#define send_cnt  1
+#define recv_cnt 10
+
+#define SEND 0
+#define RECV 1
+
+#define TAG 0
+#define COMM MPI_COMM_WORLD
+
+int rank;
+MPI_Status status[123];
+
+void send() {
+  int dest = RECV;
+  int buf[] = {42};
+  MPI_Send(buf, send_cnt, MPI_INT, dest, TAG, COMM);
+}
+
+
+void recv() {
+  int dest = SEND;
+  int buf[123];
+  MPI_Recv(buf, recv_cnt, MPI_INT, dest, TAG, COMM, status);
+  printf("recv: %d\n", buf[0]);
+}
+
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(COMM, &rank);
+
+  if (rank == SEND) send();
+  else              recv();
+
+  MPI_Finalize();
+  return 0;
+}
