f0a4faf2995863bb7ff948490b7808f3d2b1d3e1
Sergey Litvinov <slitvinov@gmail.com>
2017-08-11 22:09:11 +0200

    restore send/

---
+++ poc/mpi/status/send/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <mpi.h>
+
+#define recv_cnt 10
+
+#define SEND 0
+#define RECV 1
+
+#define TAG 0
+#define COMM MPI_COMM_WORLD
+#define TYPE MPI_INT
+
+#define SZ(a) sizeof(a)/sizeof(a[0])
+
+int buf[123];
+int rank;
+MPI_Status status;
+int count;
+
+void send() {
+    int dest = RECV;
+    int buf[] = {1, 2, 3};
+    MPI_Send(buf, SZ(buf), TYPE, dest, TAG, COMM);
+}
+
+void recv() {
+    int source = SEND;
+    MPI_Recv(buf, recv_cnt, TYPE, source, TAG, COMM, &status);
+}
+
+void cnt() {
+    MPI_Get_count(&status, TYPE, &count);
+    fprintf(stderr, "count: %d\n", count);
+}
+
+void dump() {
+    int i;
+    for (i = 0; i < count; i++)
+        fprintf(stderr, "%d ", buf[i]);
+    fprintf(stderr, "\n");
+}
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(COMM, &rank);
+
+    if (rank == SEND)
+        send();
+    else {
+        recv();
+        cnt();
+        dump();
+    }
+    
+    MPI_Finalize();
+    return 0;
+}
