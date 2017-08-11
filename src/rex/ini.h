755fee596a1ef1823d77a265552ff37c1a3da883
Sergey Litvinov <slitvinov@gmail.com>
2017-08-11 16:33:57 +0200

    add i2d

--- src/rex/ini.h
+++ src/rex/ini.h
@@ -1,4 +1,12 @@
 namespace rex {
+
+static void i2d(int i, /**/ int d[3]) { /* fragment id to directiron */
+    enum {X, Y, Z};
+    d[X] = (i     + 2) % 3 - 1;
+    d[Y] = (i / 3 + 2) % 3 - 1;
+    d[Z] = (i / 9 + 2) % 3 - 1;
+}
+
 static void ini_local(int estimate) {
     int i;
     for (i = 0; i < 26; i++) {
