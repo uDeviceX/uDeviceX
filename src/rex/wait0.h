7c621b4539ef124cdf0b35afa26414988fefaae6
Sergey Litvinov <slitvinov@gmail.com>
2017-08-12 22:31:02 +0200

    minor

--- src/rex/wait0.h
+++ src/rex/wait0.h
@@ -1,12 +1,5 @@
 namespace rex {
 
-namespace r {
-void wait0() {
-    MPI_Status s[26];
-    MC(l::m::Waitall(26, reqrecvP.data(), s));
-}
-}
-
 namespace s {
 void wait0() {
     MPI_Status s[26];
@@ -14,5 +7,11 @@ void wait0() {
 }
 }
 
+namespace r {
+void wait0() {
+    MPI_Status s[26];
+    MC(l::m::Waitall(26, reqrecvP.data(), s));
+}
+}
 
 }
