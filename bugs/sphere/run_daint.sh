09f57715c5b9bc864d7258640db3deacee9ac3a4
Sergey Litvinov <slitvinov@gmail.com>
2017-08-12 19:07:17 +0200

    minor

--- bugs/sphere/run_daint.sh
+++ bugs/sphere/run_daint.sh
@@ -22,15 +22,15 @@ echo $x0 $y0 $z0 > rigs-ic.txt
 cp ${GITROOT}/src/data/rig/sphere.ply mesh_solid.ply
 
 # Create walls
-yl=$((${LY}-1))
-zh=$((${LZ}-1))
+yw=$((${LY}-1))
+zw=$((${LZ}-1))
 n=$((2*${LX}))
 echo "extent ${LX} ${LY} ${LZ}
 N            $n
 obj_margin 2.0
-plane point 0 $yh   0 normal 0 -1 0
+plane point 0 $yw   0 normal 0 -1 0
 plane point 0   1   0 normal 0 1 0
-plane point 0   0 $zh normal 0 0 -1
+plane point 0   0 $zw normal 0 0 -1
 plane point 0   0   1 normal 0 0 1" > channel.tsdf
 ${GITROOT}/tsdf/tsdf channel.tsdf sdf.dat sdf.vti
 
