20b65b5744aedd9534a92eef90c27f0fe1cd2b4a
Sergey Litvinov <slitvinov@gmail.com>
2017-08-12 10:36:22 +0200

    minor

--- src/cc/common.h
+++ src/cc/common.h
@@ -1,7 +1,7 @@
 namespace cc {
 inline void check(cudaError_t rc, const char *file, int line) {
     if (rc != cudaSuccess) {
-        fprintf(stderr, "GPU assert: %s %s %d\n", cudaGetErrorString(rc), file, line);
+        fprintf(stderr, "GPU assert: %s %s:%d\n", cudaGetErrorString(rc), file, line);
         abort();
     }
 }
