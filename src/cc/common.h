b71ff2878026f0bbe00921da7ff4976963988ee6
Sergey Litvinov <slitvinov@gmail.com>
2017-08-12 15:27:07 +0200

    minor

--- src/cc/common.h
+++ src/cc/common.h
@@ -1,7 +1,7 @@
 namespace cc {
 inline void check(cudaError_t rc, const char *file, int line) {
     if (rc != cudaSuccess) {
-        MSG("%s:%d: %s\n", file, line, cudaGetErrorString(rc));
+        MSG("%s:%d: %s", file, line, cudaGetErrorString(rc));
         abort();
     }
 }
