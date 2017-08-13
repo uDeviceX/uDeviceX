ef6adc706e14570c91bedbbc11d16dd42e4a1193
Sergey Litvinov <slitvinov@gmail.com>
2017-08-13 22:07:04 +0200

    rename

--- src/cnt/halo.h
+++ src/cnt/halo.h
@@ -1,6 +1,6 @@
 namespace cnt {
 void halo(ParticlesWrap halos[26]) {
-    int nremote_padded = 0;
+    int n = 0;
     {
         int recvpackcount[26], recvpackstarts_padded[27];
 
@@ -11,7 +11,7 @@ void halo(ParticlesWrap halos[26]) {
         recvpackstarts_padded[0] = 0;
         for (int i = 0, s = 0; i < 26; ++i)
         recvpackstarts_padded[i + 1] = (s += 32 * ((halos[i].n + 31) / 32));
-        nremote_padded = recvpackstarts_padded[26];
+        n = recvpackstarts_padded[26];
         CC(cudaMemcpyToSymbolAsync
            (k_cnt::g::starts, recvpackstarts_padded,
             sizeof(recvpackstarts_padded), 0, H2D));
@@ -24,11 +24,8 @@ void halo(ParticlesWrap halos[26]) {
                                    H2D));
         Force *ff[26];
         for (int i = 0; i < 26; ++i) ff[i] = halos[i].f;
-        CC(cudaMemcpyToSymbolAsync(k_cnt::g::ff, ff,
-                                   sizeof(ff), 0, H2D));
+        CC(cudaMemcpyToSymbolAsync(k_cnt::g::ff, ff, sizeof(ff), 0, H2D));
     }
-
-    KL(k_cnt::halo, (k_cnf(nremote_padded)),
-       (nremote_padded, entries->S, no, rgen->get_float()));
+    KL(k_cnt::halo, (k_cnf(n)), (n, entries->S, no, rgen->get_float()));
 }
 }
