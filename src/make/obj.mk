d2bf9fd672aa3696602f5dfc81c0954bece16e51
Sergey Litvinov <slitvinov@gmail.com>
2017-08-13 17:23:29 +0200

    use makefile/obj.mk

---
+++ src/make/obj.mk
@@ -0,0 +1,9 @@
+OBJ = $B/bund.o $B/clist/imp.o $B/common.mpi.o $B/diag.o $B/dpd/local.o $B/dpdr/imp.o \
+    $B/dpdr/int.o $B/dump.o $B/field.o $B/flu/imp.o $B/flu/int.o $B/glb.o \
+    $B/hforces/imp.o $B/io/field.o $B/io/rbc.o $B/l/linal.o $B/l/m.o $B/l/off.o \
+    $B/l/ply.o $B/m.o $B/main.o $B/mbounce/imp.o $B/mcomm/imp.o $B/mcomm/int.o \
+    $B/mdstr/imp.o $B/mdstr/int.o $B/mesh/bbox.o $B/mesh/collision.o \
+    $B/mesh/dist.o $B/mesh/props.o $B/minmax.o $B/mrescue.o $B/msg.o $B/odstr/imp.o \
+    $B/odstr/int.o $B/os.o $B/rdstr/imp.o $B/rdstr/int.o $B/restart.o $B/rig/imp.o \
+    $B/rig/int.o $B/rnd/imp.o $B/scan/imp.o $B/sdf/imp.o $B/sdf/int.o $B/solid.o \
+    $B/tcells/imp.o $B/tcells/int.o $B/wall/imp.o $B/wall/int.o
