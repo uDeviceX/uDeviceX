73a76935aa1fa050156b71867f310a5307e7c6a1
Sergey Litvinov <slitvinov@gmail.com>
2017-08-14 22:51:53 +0200

    minor

--- bugs/long/small/run.sh
+++ bugs/long/small/run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-u.conf ../../../src  base.h h/`u.host`
-
+s=../../../src
+u.conf $s base.h h/`u.host`
 (u.make clean && u.make -j) > /dev/null
 u.strtdir
 sh runfile
-
-# nvprof ./udx 2>e
-# grep set    e
