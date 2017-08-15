4dff9a00559a8ad9d3c9b0b1993021dca98749f8
Sergey Litvinov <slitvinov@gmail.com>
2017-08-15 23:20:35 +0200

    declare MemsetAsync

--- src/d.h
+++ src/d.h
@@ -6,5 +6,5 @@ cudaError_t MemcpyToSymbol(const void *symbol, const void *src, size_t count, si
 cudaError_t HostAlloc(void **pHost, size_t size, unsigned int flags);
 cudaError_t HostGetDevicePointer(void **pDevice, void *pHost, unsigned int flags);
 cudaError_t Memcpy (void *dst, const void *src, size_t count, enum cudaMemcpyKind kind);
-cudaError_t cudaMemsetAsync (void *devPtr, int value, size_t count, cudaStream_t stream=0);
+cudaError_t MemsetAsync (void *devPtr, int value, size_t count, cudaStream_t stream=0);
 }
