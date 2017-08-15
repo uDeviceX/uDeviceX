aa661fdbd03d05347c13ea43277cd1efedbf101b
Sergey Litvinov <slitvinov@gmail.com>
2017-08-15 22:37:45 +0200

    add a decl of Memcpy

--- src/d.h
+++ src/d.h
@@ -5,4 +5,5 @@ cudaError_t MemcpyToSymbol(const void *symbol, const void *src, size_t count, si
                            enum cudaMemcpyKind kind=cudaMemcpyHostToDevice);
 cudaError_t HostAlloc(void **pHost, size_t size, unsigned int flags);
 cudaError_t HostGetDevicePointer(void **pDevice, void *pHost, unsigned int flags);
+cudaError_t Memcpy (void *dst, const void *src, size_t count, enum cudaMemcpyKind kind);
 }
