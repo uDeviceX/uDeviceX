68f47e43bb72af287fac299bb6398a0c7566b7ad
Sergey Litvinov <slitvinov@gmail.com>
2017-08-15 03:42:09 +0200

    move mpi related functions to odstr/mpi.h

--- src/odstr/imp.cu
+++ src/odstr/imp.cu
@@ -27,6 +27,8 @@
 
 #include "odstr/type.h"
 #include "odstr/imp.h"
+
+#include "odstr/mpi.h"
 #include "odstr/dev.h"
 #include "odstr/buf.h"
 #include "odstr/ini.h"
@@ -34,28 +36,6 @@
 
 namespace odstr {
 namespace sub {
-
-void waitall(MPI_Request *reqs) {
-    MPI_Status statuses[123];
-    l::m::Waitall(26, reqs, statuses) ;
-}
-
-void post_recv(const MPI_Comm cart, const int rank[], const int btc, const int btp,
-               MPI_Request *size_req, MPI_Request *mesg_req, Recv *r) {
-    for(int i = 1, c = 0; i < 27; ++i)
-    l::m::Irecv(r->size + i, 1, MPI_INTEGER, rank[i],
-                btc + r->tags[i], cart, size_req + c++);
-
-    for(int i = 1, c = 0; i < 27; ++i)
-    l::m::Irecv(r->pp.hst[i], MAX_PART_NUM, MPI_FLOAT, rank[i],
-                btp + r->tags[i], cart, mesg_req + c++);
-}
-
-void post_recv_ii(const MPI_Comm cart, const int rank[], const int tags[], const int bt, /**/ MPI_Request *ii_req, Pbufs<int> *rii) {
-    for(int i = 1, c = 0; i < 27; ++i)
-    l::m::Irecv(rii->hst[i], MAX_PART_NUM, MPI_INT, rank[i],
-                bt + tags[i], cart, ii_req + c++);
-}
 
 void halo(const Particle *pp, int n, Send *s) {
     Dzero(s->size_dev, 27);
@@ -75,25 +55,6 @@ void pack_ii(const int *ii, int n, const Send *s, Pbufs<int>* sii) {
     KL((dev::pack<int, 1>), (k_cnf(n)),(ii, s->iidx, s->strt, /**/ sii->dev));
 }
 
-int send_sz(MPI_Comm cart, const int rank[], const int btc, /**/ Send *s, MPI_Request *req) {
-    for(int i = 0; i < 27; ++i) s->size[i] = s->size_pin.D[i];
-    for(int i = 1, cnt = 0; i < 27; ++i)
-    l::m::Isend(s->size + i, 1, MPI_INTEGER, rank[i],
-                btc + i, cart, &req[cnt++]);
-    return s->size[0]; /* `n' bulk */
-}
-
-void send_pp(MPI_Comm cart, const int rank[], const int btp, /**/ Send *s, MPI_Request *req) {
-    for(int i = 1, cnt = 0; i < 27; ++i)
-    l::m::Isend(s->pp.hst[i], s->size[i] * 6, MPI_FLOAT, rank[i],
-                btp + i, cart, &req[cnt++]);
-}
-
-void send_ii(MPI_Comm cart, const int rank[], const int size[], const int bt, /**/ Pbufs<int> *sii, MPI_Request *req) {
-    for(int i = 1, cnt = 0; i < 27; ++i)
-    l::m::Isend(sii->hst[i], size[i], MPI_INT, rank[i],
-                bt + i, cart, &req[cnt++]);
-}
 
 void recv_count(/**/ Recv *r, int *nhalo) {
     int i;
@@ -118,11 +79,6 @@ void subindex_remote(const int n, const Recv *r, /*io*/ Particle *pp_re, int *co
     KL(dev::subindex_remote, (k_cnf(n)), (n, r->strt, /*io*/ (float2*) pp_re, counts, /**/ subi));
 }
 
-/* TODO: this is not used, why? */
-void cancel_recv(/**/ MPI_Request *size_req, MPI_Request *mesg_req) {
-    for(int i = 0; i < 26; ++i) l::m::Cancel(size_req + i) ;
-    for(int i = 0; i < 26; ++i) l::m::Cancel(mesg_req + i) ;
-}
 
 void scatter(bool remote, const uchar4 *subi, const int n, const int *start, /**/ uint *iidx) {
     KL(dev::scatter, (k_cnf(n)),(remote, subi, n, start, /**/ iidx));
