1fccaa9b4b022c576725e1aca56fcd62289d5577
Athena Economides <eceva@ethz.ch>
2017-08-15 11:47:59 +0200

    functions in odstr

--- src/sim/odstr1.h
+++ src/sim/odstr1.h
@@ -1,3 +1,15 @@
+static void gather() {
+    gather_pp(&o::td, /**/ &o::q, &o::tu, &o::tz);
+    if (global_ids)    gather_ii(o::q.n, &o::tu, &o::tui, /**/ &o::qi);
+    if (multi_solvent) gather_ii(o::q.n, &o::tu, &o::tut, /**/ &o::qt);
+}
+
+static void pack() {
+    pack_pp(&o::q, /**/ &o::td);
+    if (global_ids)    pack_ii(o::q.n, &o::qi, &o::td, /**/ &o::ti);
+    if (multi_solvent) pack_ii(o::q.n, &o::qt, &o::td, /**/ &o::tt);
+}
+
 void odstr() {
     using namespace odstr;
 
@@ -7,15 +19,13 @@ void odstr() {
     post_recv_pp(/**/ &o::td);
     if (global_ids)    post_recv_ii(&o::td, /**/ &o::ti);
     if (multi_solvent) post_recv_ii(&o::td, /**/ &o::tt);
-    
-    pack_pp(&o::q, /**/ &o::td);
-    if (global_ids)    pack_ii(o::q.n, &o::qi, &o::td, /**/ &o::ti);
-    if (multi_solvent) pack_ii(o::q.n, &o::qt, &o::td, /**/ &o::tt);
+
+    pack();
 
     send_pp(/**/ &o::td);
     if (global_ids)    send_ii(&o::td, /**/ &o::ti);
     if (multi_solvent) send_ii(&o::td, /**/ &o::tt);
-    
+
     bulk(/**/ &o::q, &o::td);
 
     recv_pp(/**/ &o::td);
@@ -26,7 +36,5 @@ void odstr() {
     if (global_ids)    unpack_ii(&o::td, &o::ti, /**/ &o::tui);
     if (multi_solvent) unpack_ii(&o::td, &o::tt, /**/ &o::tut);
     
-    gather_pp(&o::td, /**/ &o::q, &o::tu, &o::tz);
-    if (global_ids)    gather_ii(o::q.n, &o::tu, &o::tui, /**/ &o::qi);
-    if (multi_solvent) gather_ii(o::q.n, &o::tu, &o::tut, /**/ &o::qt);
+    gather();
 }
