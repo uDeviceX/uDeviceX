117f830a7c401015cb2ec8430390cf4c5f2eca99
Sergey Litvinov <slitvinov@gmail.com>
2017-08-16 18:35:53 +0200

    for cuda q is empty

--- src/d/cuda/q.h
+++ src/d/cuda/q.h
@@ -1,21 +1,2 @@
 /* B.1. Function Type Qualifiers */
-
-/*
-__device__
-__global__
-__host__
-__noinline__
-__forceinline__
-
-*/
-
 /* B.2. Variable Type Qualifiers */
-
-/*
-__device__
-__constant__
-__shared__
-__managed__
-__restrict__
-
-*/
