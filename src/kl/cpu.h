1925e1cc6f1e49cdb5fb4a87444c0e4d263cb0a4
Sergey Litvinov <slitvinov@gmail.com>
2017-08-16 18:58:54 +0200

    add kl/cpu.h

---
+++ src/kl/cpu.h
@@ -0,0 +1,5 @@
+#define KL_BEFORE(s, C) if (!kl::safe(ESC C)) continue;
+#define KL_AFTER(s)
+#define KL_CALL(F, C, A) F<<<ESC C>>>A
+
+namespace kl { inline void msg(int, int, int,   int, int, int) { }; }
