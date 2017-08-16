3a6af28a18f77f54f3520d7b4ae511f7a21e547f
Sergey Litvinov <slitvinov@gmail.com>
2017-08-16 19:54:25 +0200

    add more kernel functions

--- src/d/cpu/ker.h
+++ src/d/cpu/ker.h
@@ -1,10 +1,16 @@
 #define atomicAdd(...) (0)
 
+#define atomicExch(...)
+#define __float2half_rn(...)
 #define __fmaf_rz(...) (0.0)
 #define __ldg(p) (*(p))
+#define __popc(...)
 #define __shfl(...) (0.0)
 #define __shfl_down(...) (0.0)
 #define __shfl_up(...) (0)
 #define __shfl_xor(...) (0)
 
+#define __syncthreads(...)
+
 #define tex1Dfetch(...) (make_float2(0, 0))
+#define tex3D(...)
