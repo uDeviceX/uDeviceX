cf3884535d7e76d88c122d02bd2c0a7971fded0f
Sergey Litvinov <slitvinov@gmail.com>
2017-08-16 22:25:34 +0200

    add lane function to ker

--- src/d/cuda/ker.h
+++ src/d/cuda/ker.h
@@ -2,3 +2,12 @@
 #define F4fetch(t, i)  tex1Dfetch(t, i)
 #define F2fetch(t, i)  tex1Dfetch(t, i)
 #define Tfetch(T, t, i) tex1Dfetch<T>(t, i)
+
+namespace d {
+__inline__ __device__ int lane() {
+    int id;
+    asm volatile ("mov.u32 %0, %%laneid;" : "=r"(id));
+    return id;
+}
+}
+
