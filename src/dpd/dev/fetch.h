12a1c5f3316fbc74d37aa10f6ef24997daced3fc
Sergey Litvinov <slitvinov@gmail.com>
2017-08-16 21:34:01 +0200

    add F4 fetchH4

--- src/dpd/dev/fetch.h
+++ src/dpd/dev/fetch.h
@@ -1,7 +1,7 @@
 static __device__ float4 fetchH4(uint i) {
-    return tex1Dfetch(texParticlesH4, i); /* (sic) type mismatch */
+    return F4fetch(texParticlesH4, i); /* (sic) type mismatch */
 }
 
 static __device__ float4 fetchF4(uint i) {
-    return tex1Dfetch(texParticlesF4, i);
+    return F4fetch(texParticlesF4, i);
 }
