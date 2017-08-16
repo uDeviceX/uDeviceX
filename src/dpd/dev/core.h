8c29e0b4d481d455350ea16ea0b7e7597c50a6c9
Sergey Litvinov <slitvinov@gmail.com>
2017-08-16 20:19:14 +0200

    unconst

--- src/dpd/dev/core.h
+++ src/dpd/dev/core.h
@@ -1,19 +1,19 @@
 static __device__ void core( const uint dststart, const uint pshare, const uint tid, const uint spidext )
 {
     uint item;
-    const uint offset = xmad( tid, 4.f, pshare );
+    uint offset = xmad( tid, 4.f, pshare );
     asm volatile( "ld.volatile.shared.u32 %0, [%1+1024];" : "=r"( item ) : "r"( offset ) : "memory" );
-    const uint2 pid = __unpack_8_24( item );
-    const uint dpid = xadd( dststart, pid.x );
-    const uint spid = pid.y;
+    uint2 pid = __unpack_8_24( item );
+    uint dpid = xadd( dststart, pid.x );
+    uint spid = pid.y;
 
-    const uint dentry = xscale( dpid, 2.f );
-    const uint sentry = xscale( spid, 2.f );
-    const float4 xdest = tex1Dfetch( texParticlesF4,       dentry );
-    const float4 xsrc  = tex1Dfetch( texParticlesF4,       sentry );
-    const float4 udest = tex1Dfetch( texParticlesF4, xadd( dentry, 1u ) );
-    const float4 usrc  = tex1Dfetch( texParticlesF4, xadd( sentry, 1u ) );
-    const float3 f = dpd( dpid, xdest, udest, xsrc, usrc, spid );
+    uint dentry = xscale( dpid, 2.f );
+    uint sentry = xscale( spid, 2.f );
+    float4 xdest = tex1Dfetch( texParticlesF4,       dentry );
+    float4 xsrc  = tex1Dfetch( texParticlesF4,       sentry );
+    float4 udest = tex1Dfetch( texParticlesF4, xadd( dentry, 1u ) );
+    float4 usrc  = tex1Dfetch( texParticlesF4, xadd( sentry, 1u ) );
+    float3 f = dpd( dpid, xdest, udest, xsrc, usrc, spid );
 
     // the overhead of transposition acc back
     // can be completely killed by changing the integration kernel
