17eb0116ccf14456889ec68c8a340f2a2eff548d
Sergey Litvinov <slitvinov@gmail.com>
2017-08-17 13:00:07 +0200

    add ERR in cpu scan

--- src/scan/cpu/imp.h
+++ src/scan/cpu/imp.h
@@ -1,4 +1,5 @@
-enum { THREADS = 128 };
-void scan(const int *input, int size, /**/ int *output, /*w*/ Work *w) { }
-void alloc_work(int size, /**/ Work *w) { }
-void free_work(/**/ Work *w) { }
+void scan(const int* input, int size, /**/ int* output, /*w*/ Work*) {
+    ERR("scan is missing on cpu");
+}
+void alloc_work(int, Work*) { }
+void free_work(Work*) { }
