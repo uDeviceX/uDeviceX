3935b9d4615a7605a35eb0e171d7d4b06c44f9c7
Sergey Litvinov <slitvinov@gmail.com>
2017-08-19 00:26:48 +0200

    Use smaller domain

--- bugs/long/deadlock/run.sh
+++ bugs/long/deadlock/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 s=../../../src
-u.conf $s base.h h/`u.host`
+u=u/solid
+u.conf $s $u base.h h/`u.host`
+mkdir -p u # bug in u.dir
+
 (u.make clean && u.make -j) > /dev/null
 u.strtdir
 sh runfile
