1199cb23719f7b0020e97b22e010984024eaa8b1
Sergey Litvinov <slitvinov@gmail.com>
2017-08-18 12:31:50 +0200

    rm slevel

--- src/make/common.mk
+++ src/make/common.mk
@@ -2,5 +2,4 @@ NVCCFLAGS = --compiler-options \
 	 '${CXXFLAGS}          \
 	  ${HDF5_CXXFLAGS} ${MPI_CXXFLAGS}'
 LIBS =    ${HDF5_LIBS}  ${MPI_LIBS}  ${NVCC_LIBS}
-
 NVCCLIBS  = --linker-options '${HDF5_LIBS}     ${MPI_LIBS}'
