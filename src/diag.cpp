b484f547a3e3951a3bb3077a8c5ac5817e98cf41
Sergey Litvinov <slitvinov@gmail.com>
2017-08-18 02:46:42 +0200

    minor

--- src/diag.cpp
+++ src/diag.cpp
@@ -11,17 +11,18 @@
 
 static float sq(float x) { return x*x; }
 
-void diagnostics(Particle *pp, int n, int idstep) {
+void diagnostics(Particle *pp, int n, int id) {
+    int i, c;
+    double ke, kbt;
+    FILE * f;
     double p[] = {0, 0, 0};
-    for(int i = 0; i < n; ++i)
-    for(int c = 0; c < 3; ++c)
-    p[c] += pp[i].v[c];
+    for (i = 0; i < n; ++i) for (c = 0; c < 3; ++c) p[c] += pp[i].v[c];
 
     MC(l::m::Reduce(m::rank == 0 ? MPI_IN_PLACE : &p,
                     m::rank == 0 ? &p : NULL, 3,
                     MPI_DOUBLE, MPI_SUM, 0, l::m::cart) );
-    double ke = 0;
-    for(int i = 0; i < n; ++i)
+    ke = 0;
+    for (i = 0; i < n; ++i)
         ke += sq(pp[i].v[0]) + sq(pp[i].v[1]) + sq(pp[i].v[2]);
 
     MC(l::m::Reduce(m::rank == 0 ? MPI_IN_PLACE : &ke,
@@ -30,14 +31,14 @@ void diagnostics(Particle *pp, int n, int idstep) {
     MC(l::m::Reduce(m::rank == 0 ? MPI_IN_PLACE : &n,
                     &n, 1, MPI_INT, MPI_SUM, 0, l::m::cart));
 
-    double kbt = 0.5 * ke / (n * 3. / 2);
     if (m::rank == 0) {
+        kbt = 0.5 * ke / (n * 3. / 2);
         static bool firsttime = true;
-        FILE * f = fopen(DUMP_BASE "/diag.txt", firsttime ? "w" : "a");
+        f = fopen(DUMP_BASE "/diag.txt", firsttime ? "w" : "a");
         firsttime = false;
-        if (idstep == 0) fprintf(f, "# TSTEP\tKBT\tPX\tPY\tPZ\n");
-        fprintf(stderr, "%e\t%.10e\t%.10e\t%.10e\t%.10e\n", idstep * dt, kbt, p[0], p[1], p[2]);
-        fprintf(f, "%e\t%.10e\t%.10e\t%.10e\t%.10e\n", idstep * dt, kbt, p[0], p[1], p[2]);
+        if (id == 0) fprintf(f, "# TSTEP\tKBT\tPX\tPY\tPZ\n");
+        fprintf(stderr, "%e\t%.10e\t%.10e\t%.10e\t%.10e\n", id * dt, kbt, p[0], p[1], p[2]);
+        fprintf(f, "%e\t%.10e\t%.10e\t%.10e\t%.10e\n", id * dt, kbt, p[0], p[1], p[2]);
         fclose(f);
     }
 }
