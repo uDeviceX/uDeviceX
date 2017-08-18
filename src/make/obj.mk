8c19a5aae8ab64501b969ece60bfa48d9da0ae20
Sergey Litvinov <slitvinov@gmail.com>
2017-08-18 02:48:44 +0200

    rm common.mpi.cu

--- src/make/obj.mk
+++ src/make/obj.mk
@@ -1,7 +1,7 @@
-O = $B/bund.o $B/cc.o $B/clist/imp.o $B/common.mpi.o $B/d/api.o $B/diag.o $B/dpd/local.o \
-    $B/dpdr/imp.o $B/dpdr/int.o $B/dual/imp.o $B/dump.o $B/field.o $B/flu/imp.o \
-    $B/flu/int.o $B/glb.o $B/hforces/imp.o $B/io/field.o $B/io/rbc.o $B/l/linal.o \
-    $B/l/m.o $B/l/off.o $B/l/ply.o $B/m.o $B/main.o $B/mbounce/imp.o $B/mcomm/imp.o \
+O = $B/bund.o $B/cc.o $B/clist/imp.o $B/d/api.o $B/diag.o $B/dpd/local.o $B/dpdr/imp.o \
+    $B/dpdr/int.o $B/dual/imp.o $B/dump.o $B/field.o $B/flu/imp.o $B/flu/int.o \
+    $B/glb.o $B/hforces/imp.o $B/io/field.o $B/io/rbc.o $B/l/linal.o $B/l/m.o \
+    $B/l/off.o $B/l/ply.o $B/m.o $B/main.o $B/mbounce/imp.o $B/mcomm/imp.o \
     $B/mcomm/int.o $B/mdstr/imp.o $B/mdstr/int.o $B/mesh/bbox.o \
     $B/mesh/collision.o $B/mesh/dist.o $B/mesh/props.o $B/minmax.o $B/mrescue.o \
     $B/msg.o $B/odstr/imp.o $B/odstr/int.o $B/os.o $B/rdstr/imp.o $B/rdstr/int.o \
