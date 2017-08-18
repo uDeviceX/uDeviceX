22e7ae16f68534f3fdd09193cd3a3775c06591e3
Sergey Litvinov <slitvinov@gmail.com>
2017-08-18 16:56:39 +0200

    mv udeps to src/U/x

---
+++ src/U/x/make/dir.mk
@@ -0,0 +1,24 @@
+D = @d () { test -d "$$1" || mkdir "$$1"; } && \
+    d         $B && \
+    d    $B/U/hw && \
+    d   $B/clist && \
+    d       $B/d && \
+    d     $B/dpd && \
+    d    $B/dpdr && \
+    d    $B/dual && \
+    d     $B/flu && \
+    d $B/hforces && \
+    d      $B/io && \
+    d       $B/l && \
+    d $B/mbounce && \
+    d   $B/mcomm && \
+    d   $B/mdstr && \
+    d    $B/mesh && \
+    d   $B/odstr && \
+    d   $B/rdstr && \
+    d     $B/rig && \
+    d     $B/rnd && \
+    d    $B/scan && \
+    d     $B/sdf && \
+    d  $B/tcells && \
+    d    $B/wall
