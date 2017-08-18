9de45cef8cf16c29396cdb4c7371139f0cdb08e4
Sergey Litvinov <slitvinov@gmail.com>
2017-08-18 20:59:14 +0200

    initial import of u/api

---
+++ u/api/main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "msg.h"
+#include "m.h" /* mini-MPI and -device */
+#include "d/api.h"
+
+#include "bund.h"
+#include "glb.h"
+
+#include "inc/dev.h"
+#include "cc.h"
+
+#include "scan/int.h"
+
+#define N 10
+#define value 10
+
+static scan::Work w;
+static int *x, *dev;
+
+static void dump0(int *hst) {
+    int i;
+    for (i = 0; i < N; i++)
+        printf("%d\n", hst[i]);
+}
+
+static void dump(int *dev) {
+    int hst[N];
+    cD2H(hst, dev, N);
+    dump0(hst);
+}
+
+static void main0() {
+    Dset(x, value, N);
+    scan::scan(x, N, dev,  &w);
+    dump(x);
+}
+
+static void main1() {
+    alloc_work(N, &w);
+    Dalloc(&x,   N);
+    Dalloc(&dev, N);    
+    
+    main0();
+
+    free_work(&w);
+    Dfree(x);
+    Dfree(dev);
+}
+
+int main(int argc, char **argv) {
+    m::ini(argc, argv);
+    main1();
+    m::fin();
+}
---
+++ u/api/make/rule.mk
@@ -0,0 +1,7 @@
+$B/../u/scan/main.o: $S/../u/scan/main.cu; $N
+$B/cc.o: $S/cc.cu; $N
+$B/d/api.o: $S/d/api.cu; $N
+$B/l/m.o: $S/l/m.cpp; $X
+$B/m.o: $S/m.cpp; $X
+$B/msg.o: $S/msg.cpp; $X
+$B/scan/imp.o: $S/scan/imp.cu; $N
