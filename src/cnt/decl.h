771cb346606fbf05b0380aa62f77518f6a2612ce
Sergey Litvinov <slitvinov@gmail.com>
2017-08-19 19:52:19 +0200

    rm SmallDeviceBuffer

--- src/cnt/decl.h
+++ src/cnt/decl.h
@@ -2,9 +2,10 @@ namespace cnt {
 const int sz = XS*YS*ZS + 16;
 
 int no; /* number of objects */
-DeviceBuffer<uchar4> *indexes;
+uchar4 *indexes;
+DeviceBuffer<int> *entries;
+
 scan::Work ws;
 int *starts, *counts;
-DeviceBuffer<int> *entries;
 rnd::KISS* rgen;
 }
--- src/inc/tmp/dev.h
+++ src/inc/tmp/dev.h
@@ -1,31 +1,4 @@
 /* TODO: in-transition structure to static allocation */
-template <typename T> struct SmallDeviceBuffer0 {
-  /* `S': size; `D': data */
-  int S; T *D;
-  explicit SmallDeviceBuffer0(int n) : S(n) {CC(cudaMalloc(&D, sizeof(T) * n));}
-  ~SmallDeviceBuffer0()                     {CC(cudaFree(D));}
-  void resize(int n) {S = n;}
-};
-
-/* TODO: in-transition structure to static allocation */
-template <typename T> struct SmallDeviceBuffer1 {
-  /* `S': size; `D': data */
-  int S; T *D;
-  explicit SmallDeviceBuffer1(int n) : S(n) {CC(cudaMalloc(&D, sizeof(T) * n));}
-  ~SmallDeviceBuffer1()                     {CC(cudaFree(D));}
-};
-
-/* TODO: in-transition structure to static allocation */
-template <typename T> struct SmallDeviceBuffer2 {
-  /* `S': size; `D': data */
-  T *D;
-  explicit SmallDeviceBuffer2(int n) : S(n) {CC(cudaMalloc(&D, sizeof(T) * n));}
-  ~SmallDeviceBuffer2()                     {CC(cudaFree(D));}
-private:
-  int S;
-};
-
-/* TODO: in-transition structure to static allocation */
 template <typename T> struct DeviceBuffer1 {
   /* `S': size; `D': data */
   int S; T *D;
