4ecad680dffca0c027ef4cb02c0e8a32b50091a9
Sergey Litvinov <slitvinov@gmail.com>
2017-08-19 21:54:54 +0200

    minor

--- src/io/field/imp.h
+++ src/io/field/imp.h
@@ -1,11 +1,11 @@
-static void header(FILE * xmf) {
-    fprintf(xmf, "<?xml version=\"1.0\" ?>\n");
-    fprintf(xmf, "<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" []>\n");
-    fprintf(xmf, "<Xdmf Version=\"2.0\">\n");
-    fprintf(xmf, " <Domain>\n");
+static void header(FILE *f) {
+    fprintf(f, "<?xml version=\"1.0\" ?>\n");
+    fprintf(f, "<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" []>\n");
+    fprintf(f, "<Xdmf Version=\"2.0\">\n");
+    fprintf(f, " <Domain>\n");
 }
 
-static void epilogue(FILE * xmf) {
-    fprintf(xmf, " </Domain>\n");
-    fprintf(xmf, "</Xdmf>\n");
+static void epilogue(FILE *f) {
+    fprintf(f, " </Domain>\n");
+    fprintf(f, "</Xdmf>\n");
 }
