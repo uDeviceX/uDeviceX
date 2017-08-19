c7b221d20a0b618d0792871aa63cd31c2c7b70d8
Sergey Litvinov <slitvinov@gmail.com>
2017-08-19 11:30:07 +0200

    add n to RemoteHalo

--- src/rex/type/remote.h
+++ src/rex/type/remote.h
@@ -4,10 +4,12 @@ struct RemoteHalo {
     PinnedHostBuffer<Particle> hstate;
     PinnedHostBuffer<Force> ff;
     std::vector<Particle> pp;
+    int n;
 };
 
 namespace re {
 void resize(RemoteHalo *r, int n) {
+    r->n = n;
     r->dstate.resize(n);
     r->hstate.preserve_resize(n);
     r->ff.resize(n);
