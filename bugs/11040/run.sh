7715493972da730a0ce6c732daa9d69a061af754
Sergey Litvinov <slitvinov@gmail.com>
2017-08-20 21:58:45 +0200

    add utilities

--- bugs/11040/run.sh
+++ bugs/11040/run.sh
@@ -2,15 +2,18 @@
 
 set -eu
 
+# load utilities
+. ./u
+
 n=40
 d=/scratch/snx3000/lisergey/u # data
 
 s=../../src
 echo run | u.conf $s u/x base.h
-u.make -j
+u.make -j > make.log
 
-cp $n/sdf.dat .
+safe_cp "$d/$n/sdf.dat" .
 
 x=$n y=1 z=1
 u.strtdir  . $x $y $z
-u.run    $x $y $z ./udx 
+u.run    $x $y $z ./udx
