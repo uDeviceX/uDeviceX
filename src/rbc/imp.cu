60c8609514f649dcc242e177f50ad3245cc589db
Sergey Litvinov <slitvinov@gmail.com>
2017-08-20 17:31:53 +0200

    missing file

---
+++ src/rbc/imp.cu
@@ -0,0 +1,69 @@
+#include <assert.h>
+#include <vector>
+#include <mpi.h>
+#include <stdint.h>
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "d/api.h"
+#include "d/q.h"
+#include "d/ker.h"
+
+#include "l/m.h"
+#include "l/off.h"
+#include "scan/int.h"
+
+#include "rnd/imp.h"
+#include "rnd/dev.h"
+
+#include "common.h"
+#include "msg.h"
+#include "m.h"
+#include "cc.h"
+
+#include "kl.h"
+#include "basetags.h"
+#include "inc/type.h"
+#include "inc/mpi.h"
+#include "inc/dev.h"
+
+#include "dual/type.h"
+#include "dual/int.h"
+#include "texo.h"
+#include "te.h"
+
+#include "inc/tmp/wrap.h"
+#include "inc/tmp/pinned.h"
+#include "io/field.h"
+#include "io/rbc.h"
+#include "bund.h"
+#include "diag.h"
+
+#include "restart.h"
+
+#include "glb.h"
+
+#include "l/float3.h"
+#include "k/read.h"
+#include "k/write.h"
+#include "k/common.h"
+
+#include "clist/int.h"
+
+#include "mcomm/type.h"
+#include "mcomm/int.h"
+
+#include "rbc/imp.h"
+
+namespace rbc {
+namespace sub {
+#include "params/rbc.inc0.h"
+#include "rbc/imp/dev0.h"
+#include "rbc/imp/dev.h"
+#include "rbc/imp/imp.h"
+}
+namespace ic {
+#include "rbc/imp/ic.h"
+}
+}
+
