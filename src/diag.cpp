0d786f3c46f30794162c5ca8f67f3f24d250922f
Sergey Litvinov <slitvinov@gmail.com>
2017-08-21 01:40:07 +0200

    compute max ke

--- src/diag.cpp
+++ src/diag.cpp
@@ -17,19 +17,35 @@ static int reduce(const void *sendbuf0, void *recvbuf, int count, MPI_Datatype d
     return l::m::Reduce(sendbuf, recvbuf, count, datatype, op, root, l::m::cart);
 }
 
+static int sum3(double *v) {
+    return reduce(&v, m::rank == 0 ? &v : NULL, 3, MPI_DOUBLE, MPI_SUM);
+}
+
+static int sum_d(double *v) {
+    return reduce(v, v, 1, MPI_DOUBLE, MPI_SUM);
+}
+
+static int max_d(double *v) {
+    return reduce(v, v, 1, MPI_DOUBLE, MPI_MAX);
+}
+
+static int sum_i(int *v) {
+    return reduce(v, v, 1, MPI_INT, MPI_SUM);
+}
+
 void diagnostics(Particle *pp, int n, int id) {
     int i, c;
-    double ke, kbt;
+    double ke, kbt, km;
     FILE * f;
     double v[] = {0, 0, 0};
     for (i = 0; i < n; ++i) for (c = 0; c < 3; ++c) v[c] += pp[i].v[c];
-    reduce(&v, m::rank == 0 ? &v : NULL, 3, MPI_DOUBLE, MPI_SUM);
+    sum3(v);
 
     ke = 0;
-    for (i = 0; i < n; ++i)
-        ke += sq(pp[i].v[0]) + sq(pp[i].v[1]) + sq(pp[i].v[2]);
-    reduce(&ke, &ke, 1, MPI_DOUBLE, MPI_SUM);
-    reduce(&n, &n, 1, MPI_INT, MPI_SUM);
+    for (i = 0; i < n; ++i) ke += sq(pp[i].v[0]) + sq(pp[i].v[1]) + sq(pp[i].v[2]);
+    km = ke;
+    sum_d(&ke); max_d(&km);
+    sum_i(&n);
 
     if (m::rank == 0) {
         kbt = 0.5 * ke / (n * 3. / 2);
