e315f0bd9c1a30efa403c71474be2dbb3fd0d95f
Sergey Litvinov <slitvinov@gmail.com>
2017-08-20 14:23:52 +0200

    unuse struct

--- src/inc/tmp/wrap.h
+++ src/inc/tmp/wrap.h
@@ -6,12 +6,3 @@ struct ParticlesWrap {
     ParticlesWrap(const Particle *const p, const int n, Force *f)
         : p(p), n(n), f(f) {}
 };
-
-struct ParticlesRex {
-    const Particle *p;
-    Force *f;
-    int n;
-    ParticlesRex() : p(NULL), f(NULL), n(0) {}
-    ParticlesRex(const Particle *const p, const int n, Force *f)
-        : p(p), n(n), f(f) {}
-};
