fcafbc5b23be03b4bc89e74c61535589f15f86b4
amlucas <lucas.amoudruz@wanadoo.fr>
2017-08-21 11:40:33 +0200

    upd u/scan

--- src/u/scan/make/dep.mk
+++ src/u/scan/make/dep.mk
@@ -1,5 +1,5 @@
-$B/cc.o: $S/cc/common.h $S/msg.h
-$B/d/api.o: $S/cc.h $B/conf.h $S/d/api.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/msg.h
+$B/cc.o: $S/cc/common.h $B/conf.h $S/d/api.h $S/inc/conf.h $S/msg.h
+$B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/msg.h
 $B/l/m.o: $S/l/h/m.h $S/l/m.h
 $B/m.o: $S/inc/mpi.h $S/l/m.h $S/m.h
 $B/msg.o: $S/m.h $S/msg.h
