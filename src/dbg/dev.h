61162ee98eb4d28622bcd40a80da65805f2f8caf
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-22 11:50:48 +0200

    soft_check

--- src/dbg/dev.h
+++ src/dbg/dev.h
@@ -3,34 +3,50 @@ namespace dev {
 
 enum {X, Y, Z};
 
-static __device__ void check_pos(float x, int L) {
-    if (x < L/2 || x > L/2) printf("DBG: x = %g (L = %d)\n", x, L);
+static __device__ bool check_pos(float x, int L) {
+    if (x < L/2 || x > L/2) {
+        printf("DBG: x = %g (L = %d)\n", x, L);
+        return false;
+    }
+    return true;
 }
 
-static __device__ void check_vel(float v, int L) {
+static __device__ bool check_vel(float v, int L) {
     float dx = fabs(v * dt);
-    if (dx > L/2) printf("DBG: v = %g (L = %d)\n", v, L);
+    if (dx > L/2) {
+        printf("DBG: v = %g (L = %d)\n", v, L);
+        return false;
+    }
+    return true;
 }
 
-static __device__ void check_p(const Particle *p) {
+static __device__ bool check_p(float  x, float  y, float  z,
+                               float vx, float vy, float vz) {
+    bool ok = true;
+    ok &= check_pos(x, XS);
+    ok &= check_pos(y, YS);
+    ok &= check_pos(z, ZS);
+
+    ok &= check_vel(vx, XS);
+    ok &= check_vel(vy, YS);
+    ok &= check_vel(vz, ZS);    
+
+    return ok;
+}
+
+static __device__ void soft_check_p(const Particle *p) {
     float x, y, z;
     float vx, vy, vz;
     x  = p->r[X];  y = p->r[Y];  z = p->r[Z];
     vx = p->v[X]; vy = p->v[Y]; vz = p->v[Z];
 
-    check_pos(x, XS);
-    check_pos(y, YS);
-    check_pos(z, ZS);
-
-    check_vel(vx, XS);
-    check_vel(vy, YS);
-    check_vel(vz, ZS);    
+    check_p(x, y, z, vx, vy, vz);
 }
 
 __global__ void check_pp(const Particle *pp, int n) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     if (i >= n) return;
-    check_p(pp + i);
+    soft_check_p(pp + i);
 }
 
 } // dev
