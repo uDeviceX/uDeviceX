1f4d8ddae29abfbb130fa54ee739ba2a41e72acf
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-22 11:41:29 +0200

    add oject file

--- src/u/x/make/rule.mk
+++ src/u/x/make/rule.mk
@@ -3,6 +3,7 @@ $B/cc.o: $S/cc.cpp; $X
 $B/clist/imp.o: $S/clist/imp.cu; $N
 $B/common.mpi.o: $S/common.mpi.cpp; $X
 $B/d/api.o: $S/d/api.cu; $N
+$B/dbg.o: $S/dbg.cu; $N
 $B/diag.o: $S/diag.cpp; $X
 $B/dpd/local.o: $S/dpd/local.cu; $N
 $B/dpdr/imp.o: $S/dpdr/imp.cu; $N
