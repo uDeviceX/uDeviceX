1b90b464a916ed565a887e18bd62064e9af4f3d9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-24 13:34:17 +0200

    check/valid

--- src/dbg/dev.h
+++ src/dbg/dev.h
@@ -3,7 +3,7 @@ namespace dev {
 
 enum {X, Y, Z};
 
-static __device__ bool check_pos(float x, int L) {
+static __device__ bool valid_pos(float x, int L) {
     if (x < -L/2 || x > L/2) {
         printf("DBG: x = %g (L = %d)\n", x, L);
         return false;
@@ -11,7 +11,7 @@ static __device__ bool check_pos(float x, int L) {
     return true;
 }
 
-static __device__ bool check_vel(float v, int L) {
+static __device__ bool valid_vel(float v, int L) {
     float dx = fabs(v * dt);
     if (dx > L/2) {
         printf("DBG: v = %g (L = %d)\n", v, L);
@@ -20,37 +20,37 @@ static __device__ bool check_vel(float v, int L) {
     return true;
 }
 
-static __device__ bool check_unpacked_p(float  x, float  y, float  z,
+static __device__ bool valid_unpacked_p(float  x, float  y, float  z,
                                         float vx, float vy, float vz) {
     bool ok = true;
-    ok &= check_pos(x, XS);
-    ok &= check_pos(y, YS);
-    ok &= check_pos(z, ZS);
+    ok &= valid_pos(x, XS);
+    ok &= valid_pos(y, YS);
+    ok &= valid_pos(z, ZS);
 
-    ok &= check_vel(vx, XS);
-    ok &= check_vel(vy, YS);
-    ok &= check_vel(vz, ZS);    
+    ok &= valid_vel(vx, XS);
+    ok &= valid_vel(vy, YS);
+    ok &= valid_vel(vz, ZS);    
 
     return ok;
 }
 
-static __device__ bool check_p(const Particle *p) {
+static __device__ bool valid_p(const Particle *p) {
     float x, y, z;
     float vx, vy, vz;
     x  = p->r[X];  y = p->r[Y];  z = p->r[Z];
     vx = p->v[X]; vy = p->v[Y]; vz = p->v[Z];
 
-    return check_unpacked_p(x, y, z, vx, vy, vz);
+    return valid_unpacked_p(x, y, z, vx, vy, vz);
 }
 
 __global__ void check_pp(const Particle *pp, int n) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     if (i >= n) return;
-    if (!check_p(pp + i)) atomicExch(&error, err::INVALID);
+    if (!valid_p(pp + i)) atomicExch(&error, err::INVALID);
 }
 
 
-static __device__ bool check_acc(float a, int L) {
+static __device__ bool valid_acc(float a, int L) {
     float dx = fabs(a * dt * dt);
     if (dx > L/2) {
         printf("DBG: a = %g (L = %d)\n", a, L);
@@ -59,26 +59,26 @@ static __device__ bool check_acc(float a, int L) {
     return true;
 }
 
-static __device__ bool check_unpacked_f(float fx, float fy, float fz) {
+static __device__ bool valid_unpacked_f(float fx, float fy, float fz) {
     bool ok = true;
-    ok &= check_acc(fx, XS);
-    ok &= check_acc(fy, YS);
-    ok &= check_acc(fz, ZS);
+    ok &= valid_acc(fx, XS);
+    ok &= valid_acc(fy, YS);
+    ok &= valid_acc(fz, ZS);
 
     return ok;
 }
 
-static __device__ bool check_f(const Force *f) {
+static __device__ bool valid_f(const Force *f) {
     float fx, fy, fz;
     fx = f->f[X]; fy = f->f[Y]; fz = f->f[Z];
 
-    return check_unpacked_f(fx, fy, fz);
+    return valid_unpacked_f(fx, fy, fz);
 }
 
 __global__ void check_ff(const Force *ff, int n) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     if (i >= n) return;
-    assert(check_f(ff + i));
+    assert(valid_f(ff + i));
 }
 
 } // dev
