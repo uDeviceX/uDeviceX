448d344f227f405ad36c9e567e9a94ab5559b713
Sergey Litvinov <slitvinov@gmail.com>
2017-08-24 03:07:33 +0200

    use local Pa

--- src/odstr/dev/subindex.h
+++ src/odstr/dev/subindex.h
@@ -12,7 +12,7 @@ __device__ void writePa(Pa *p, int ws, int dwe, /**/ float2 *pp) {
     k_write::AOS6f(/**/ pp + 3*ws, dwe, /*i*/ p->d0, p->d1, p->d2);
 }
 
-__device__ void shiftPa(Pa *p, float r[3]) {
+__device__ void shiftPa(float r[3], Pa *p) {
     enum {X, Y, Z};
     p->d0.x += r[X];   p->d0.y += r[Y];   p->d1.x += r[Z];
 }
@@ -30,12 +30,11 @@ __device__ void Pa2v(Pa *p, /**/ float v[3]) {
 __global__ void subindex(const int n, const int strt[], /*io*/ float2 *pp, int *counts, /**/ uchar4 *subids) {
     enum {X, Y, Z};
     int warp, slot, fid;
-    float2 d0, d1, d2;
     int ws;  /* warp start in global coordinates    */
     int dw;  /* shift relative to `ws' (lane)       */
     int dwe; /* warp or buffer end relative to `ws' */
 
-    float shift[3];
+    float shift[3], r[3], v[3];
     Pa p;
 
     warp = threadIdx.x / warpSize;
@@ -47,32 +46,28 @@ __global__ void subindex(const int n, const int strt[], /*io*/ float2 *pp, int *
     dwe  = min(warpSize, n - ws);
     slot = ws + dw;
     fid  = k_common::fid(strt, slot);
-    
-    k_read::AOS6f(pp + 3*ws, dwe, d0, d1, d2);
-    
+    readPa(pp, ws, dwe, /**/ &p);
     if (dw < dwe) {
         int xi, yi, zi, cid, subindex;
         fid2shift(fid, /**/ shift);
+        shiftPa(shift, &p);
 
-        d0.x += shift[X];
-        d0.y += shift[Y];
-        d1.x += shift[Z];
+        Pa2r(&p, r);
+        xi = x2c(r[X], XS);
+        yi = x2c(r[Y], YS);
+        zi = x2c(r[Z], ZS);
 
-        xi = x2c(d0.x, XS);
-        yi = x2c(d0.y, YS);
-        zi = x2c(d1.x, ZS);
-
-        check_vel(d1.y, XS);
-        check_vel(d2.x, YS);
-        check_vel(d2.y, ZS);
+        Pa2v(&p, v);
+        check_vel(v[X], XS);
+        check_vel(v[Y], YS);
+        check_vel(v[Z], ZS);
 
         cid = xi + XS * (yi + YS * zi);
         subindex = atomicAdd(counts + cid, 1);
 
         subids[slot] = make_uchar4(xi, yi, zi, subindex);
     }
-
-    k_write::AOS6f(pp + 3*ws, dwe, d0, d1, d2);
+    writePa(&p, ws, dwe, /**/ pp);
 }
 
 }}} /* namespace */
