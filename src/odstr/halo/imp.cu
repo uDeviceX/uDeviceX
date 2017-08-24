ce9645ac6003bc7f7e1f0d10a1c2af0315388ff6
Sergey Litvinov <slitvinov@gmail.com>
2017-08-25 00:03:54 +0200

    initial import of halo_map alloc/dealloc

--- src/odstr/halo/imp.cu
+++ src/odstr/halo/imp.cu
@@ -13,12 +13,22 @@
 #include "inc/type.h"
 #include "inc/dev.h"
 #include "inc/macro.h"
-
 #include "kl.h"
+
+#include "int.h"         /* local to this dir */
 
 #include "dev.h"
 
 void halo(const Particle *pp, int n, /**/ int **iidx, int *sizes) {
     Dzero(sizes, 27);
     KL(dev::halo, (k_cnf(n)),(pp, n, /**/ iidx, sizes));
+}
+
+void alloc_halo_map(HaloMap *h) {
+    //    int i;
+    //    for (i = 0; i < 27; ++i)
+    //        Dalloc(h->iidx_[i], estimate(i));
+}
+
+void free_halo_map(HaloMap *h) {
 }
--- src/odstr/halo/int.h
+++ src/odstr/halo/int.h
@@ -1,1 +1,10 @@
+struct HaloMap {
+    int **iidx;
+    int *sizes;
+
+    int *iidx_[27]; /* needed only to free `iidx' */
+};
+
 void halo(const Particle *pp, int n, /**/ int **iidx, int *sizes);
+void alloc_halo_map(HaloMap& h);
+void free_halo_map(HaloMap& h);
