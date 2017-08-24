283a9168c0fb8450bed6e906a8db7e2f5666923f
Sergey Litvinov <slitvinov@gmail.com>
2017-08-24 23:04:49 +0200

    upd units deps

--- src/u/hw/make/dep.mk
+++ src/u/hw/make/dep.mk
@@ -1,4 +1,4 @@
-$B/l/m.o: $S/l/h/m.h $S/l/m.h
-$B/m.o: $S/inc/mpi.h $S/l/m.h $S/m.h
-$B/msg.o: $S/m.h $S/msg.h
-$B/u/hw/main.o: $S/glb.h $S/m.h $S/msg.h
+$B/cc.o: $B/conf.h
+$B/d/api.o: $B/conf.h
+$B/mc.o: $B/conf.h
+$B/scan/imp.o: $B/conf.h
--- src/u/scan/make/dep.mk
+++ src/u/scan/make/dep.mk
@@ -2,4 +2,3 @@ $B/cc.o: $B/conf.h
 $B/d/api.o: $B/conf.h
 $B/mc.o: $B/conf.h
 $B/scan/imp.o: $B/conf.h
-$B/u/scan/main.o: $B/conf.h
