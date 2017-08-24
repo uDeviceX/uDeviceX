6408d76c1b39151f5ae6b5dee06ed4f7d3c302aa
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-24 13:08:31 +0200

    handle error

--- src/dbg/error.h
+++ src/dbg/error.h
@@ -14,11 +14,22 @@ void ini() {
     CC(MemcpyToSymbol(error, NONE, sizeof(ERR_TYPE)));
 }
 
+static void errmsg(ERR_TYPE e) {
+    switch (e) {
+    case INVALID:
+        MSG("DBG: ERR: Invalid");
+        break;
+    case NONE:
+    default:
+        break;
+    };
+}
+
 void handle() {
     dSync();
     ERR_TYPE err;
     CC(MemcpyFromSymbol(&err, error, sizeof(ERR_TYPE)));
-    // TODO
+    errmsg(err);
 }
 
 } // err
