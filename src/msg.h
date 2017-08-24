112eb912f1ffb79bd60fa348b61448a4b316477d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-24 13:16:48 +0200

    indent

--- src/msg.h
+++ src/msg.h
@@ -3,14 +3,14 @@ extern char buf[];
 void print();
 }
 
-#define MSG(fmt, ...)                             \
-    do {                                          \
-        sprintf(msg::buf, fmt, ##__VA_ARGS__);    \
-        msg::print();                             \
+#define MSG(fmt, ...)                           \
+    do {                                        \
+        sprintf(msg::buf, fmt, ##__VA_ARGS__);  \
+        msg::print();                           \
     } while (0)
 
-#define ERR(fmt, ...)                                                   \
-    do {                                                                \
-        MSG(fmt, ##__VA_ARGS__);                                        \
-        exit(1);                                                        \
+#define ERR(fmt, ...)                           \
+    do {                                        \
+        MSG(fmt, ##__VA_ARGS__);                \
+        exit(1);                                \
     } while(0)
