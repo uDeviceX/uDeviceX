ddceec853407bbf08540b89b26bc5c04b2acc6b1
Sergey Litvinov <slitvinov@gmail.com>
2017-08-24 18:53:32 +0200

    use D2TLo

--- src/odstr/dev/gather.h
+++ src/odstr/dev/gather.h
@@ -47,7 +47,7 @@ __device__ void ini_TLo(float2 *pp, float4 *zip0, ushort4 *zip1, /**/ TLo *l) {
     l->pp = pp; l->zip0 = zip0; l->zip1 = zip1;
 }
 
-__device__ void D2TLo(Da *d, int ws, int dw, int dwe, /**/ TLo *l) {
+__device__ void D2TLo(Da *d, int ws, int dw, int dwe, /**/ TLo *l) { /* collective */
     float2 *pp;
     float4  *zip0;
     ushort4 *zip1;
@@ -72,45 +72,25 @@ __device__ void D2TLo(Da *d, int ws, int dw, int dwe, /**/ TLo *l) {
                                      __float2half_rn(d0.y),
                                      __float2half_rn(d1.x),
                                      0);
-    k_write::AOS6f(pp + 3 * ws, dwe, d0, d1, d2);
+    k_write::AOS6f(pp + 3 * ws, dwe, d0, d1, d2); /* collective */
 }
 
 __global__ void gather_pp(const float2  *pp_lo, const float2 *pp_re, int n, const uint *iidx,
                           /**/ float2  *pp, float4  *zip0, ushort4 *zip1) {
     /* pp_lo, pp_re, pp: local, remote and output particles */
     int dw, ws, dwe;
-    float3 s0, s1;
-
     FLo f; /* "from" location */
     Da  d; /* data */
     TLo t; /* "to" location */
     ini_FLo(pp_lo, pp_re, &f);
 
     warpco(&ws, &dw);
-
+    dwe = min(32, n - ws);
     if (ws + dw < n)
         FLo2D(&f, iidx[ws + dw], /**/ &d);
-
-    dwe = min(32, n - ws);
-
-    float2 d0, d1, d2;
-    d0 = d.d0; d1 = d.d1; d2 = d.d2;
-    s0 = make_float3(d0.x, d0.y, d1.x);
-    s1 = make_float3(d1.y, d2.x, d2.y);
-    xchg(dw, &s0, &s1); /* collective */
-
-    if (dw < 2 * dwe)
-        zip0[2 * ws + dw] = make_float4(s0.x, s0.y, s0.z, 0);
-
-    if (dw + 32 < 2 * dwe)
-        zip0[2 * ws + dw + 32] = make_float4(s1.x, s1.y, s1.z, 0);
-
-    if (dw < dwe)
-        zip1[ws + dw] = make_ushort4(__float2half_rn(d0.x),
-                                     __float2half_rn(d0.y),
-                                     __float2half_rn(d1.x),
-                                     0);
-    k_write::AOS6f(pp + 3 * ws, dwe, d0, d1, d2);
+    
+    ini_TLo(pp, zip0, zip1, /**/ &t);
+    D2TLo(&d, ws, dw, dwe, /**/ &t);
 }
 
 }}} /* namespace */
