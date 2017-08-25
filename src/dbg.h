e4eb2f2c6e7ba509ee4b4c52e500310f2a2a3e49
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-25 09:44:23 +0200

    comments

--- src/dbg.h
+++ src/dbg.h
@@ -1,4 +1,7 @@
 namespace dbg {
+/* check if particles are inside domain, and if velocities are within bounds */
 void check_pp(const Particle *pp, int n, const char *M);
+
+/* check if forces are within bounds */
 void check_ff(const    Force *ff, int n, const char *M);
 } // dbg
