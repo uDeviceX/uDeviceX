150d469a311dea004494210bd818cc4dddeb9b4d
Sergey Litvinov <slitvinov@gmail.com>
2017-08-26 19:07:43 +0200

    rex uses frag

--- src/rex/ini.h
+++ src/rex/ini.h
@@ -1,28 +1,6 @@
 namespace rex {
-static void i2d(int i, /**/ int d[3]) { /* fragment id to directiron */
-    enum {X, Y, Z};
-    d[X] = (i     + 2) % 3 - 1;
-    d[Y] = (i / 3 + 2) % 3 - 1;
-    d[Z] = (i / 9 + 2) % 3 - 1;
-}
-
-static int d2sz(int d[3]) { /* direction to size */
-    enum {X, Y, Z};
-    int x, y, z;
-    x = (d[X] == 0 ? XS : 1);
-    y = (d[Y] == 0 ? YS : 1);
-    z = (d[Z] == 0 ? ZS : 1);
-    return x * y * z;
-}
-
-static int i2sz(int i) { /* fragment id to size */
-    int d[3];
-    i2d(i, d);
-    return d2sz(d);
-}
-
 static int i2max(int i) { /* fragment id to maximum size */
-    return MAX_OBJ_DENSITY*i2sz(i);
+    return MAX_OBJ_DENSITY*frag_ncell(i);
 }
 
 static void ini_local() {
