66a95aac3f592d5a66a6ce4c4befc31fa234234e
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 01:59:41 +0200

    add cnt/sdstr.cu

---
+++ src/sdstr/imp.cu
@@ -0,0 +1,84 @@
+#include <assert.h>
+#include <vector>
+#include <mpi.h>
+#include <stdint.h>
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "d/api.h"
+#include "d/q.h"
+#include "d/ker.h"
+
+#include "l/m.h"
+#include "l/off.h"
+#include "scan/int.h"
+
+#include "rnd/imp.h"
+#include "rnd/dev.h"
+
+#include "common.h"
+#include "msg.h"
+#include "m.h"
+#include "cc.h"
+#include "mc.h"
+#include "frag.h"
+
+#include "kl.h"
+#include "basetags.h"
+#include "inc/type.h"
+#include "inc/mpi.h"
+#include "inc/dev.h"
+
+#include "dual/type.h"
+#include "dual/int.h"
+#include "texo.h"
+#include "te.h"
+
+#include "inc/tmp/wrap.h"
+#include "inc/tmp/pinned.h"
+#include "io/field.h"
+#include "io/rbc.h"
+#include "bund.h"
+#include "diag.h"
+
+#include "dbg.h"
+
+#include "restart.h"
+
+#include "glb.h"
+
+#include "k/read.h"
+#include "k/write.h"
+#include "k/common.h"
+#include "k/index.h"
+
+#include "clist/int.h"
+
+#include "mcomm/type.h"
+#include "mcomm/int.h"
+
+#include "rbc/int.h"
+
+#include "mdstr/buf.h"
+#include "mdstr/tic.h"
+#include "mdstr/int.h"
+#include "rdstr/int.h"
+
+#include "field.h"
+
+#include "forces.h"
+
+#include "sdf/type.h"
+#include "sdf/int.h"
+
+#include "wall/int.h"
+
+#include "flu/int.h"
+
+#include "odstr/type.h"
+#include "odstr/int.h"
+#include "cnt/int.h"
+#include "fsi/int.h"
+
+#include "sdstr.decl.h"
+#include "sdstr.impl.h"
--- src/u/x/make/rule.mk
+++ src/u/x/make/rule.mk
@@ -54,6 +54,7 @@ $B/rnd/imp.o: $S/rnd/imp.cpp; $X -I$S/. -I$S/rnd
 $B/scan/imp.o: $S/scan/imp.cu; $N -I$S/. -I$S/scan
 $B/sdf/imp.o: $S/sdf/imp.cu; $N -I$S/. -I$S/sdf
 $B/sdf/int.o: $S/sdf/int.cu; $N -I$S/. -I$S/sdf
+$B/sdstr/imp.o: $S/sdstr/imp.cu; $N -I$S/.
 $B/solid.o: $S/solid.cu; $N -I$S/.
 $B/tcells/imp.o: $S/tcells/imp.cu; $N -I$S/. -I$S/tcells
 $B/tcells/int.o: $S/tcells/int.cpp; $X -I$S/. -I$S/tcells
