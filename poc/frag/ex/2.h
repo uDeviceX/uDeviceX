ce4b263916830d47bdb6b6b90ad1be42873db766
Sergey Litvinov <slitvinov@gmail.com>
2017-08-26 12:01:58 +0200

    frag

--- poc/frag/ex/2.h
+++ poc/frag/ex/2.h
@@ -1,5 +1,5 @@
 #define a(x) (assert(x))
-const int *to_d;
+const int *d; /* direction */
 
-to_d = frag_to_dir[frag_bulk];
-a(to_d[X]  ==  0);  a(to_d[Y] ==  0);  a(to_d[Z] ==  0);
+d = frag_to_dir[frag_bulk];
+a(d[X]  ==  0);  a(d[Y] ==  0);  a(d[Z] ==  0);
