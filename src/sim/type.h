c26d845cbd312bdaaf38ad675af77ce1cfab5e87
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 12:20:22 +0200

    add local Halo struct

--- src/sim/type.h
+++ src/sim/type.h
@@ -1,3 +1,14 @@
-namespace o { namespace h {
+namespace o {
+struct H { /* halo tikets : was h:: */
+    dpdr::TicketCom tc;
+    dpdr::TicketRnd trnd;
+    dpdr::TicketShalo ts;
+    dpdr::TicketRhalo tr;
+
+    /* optional: flags */
+    dpdr::TicketICom tic;
+    dpdr::TicketSIhalo tsi;
+    dpdr::TicketRIhalo tri;
+};
 
 }} /* namespace */
