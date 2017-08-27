c9175cba4d1b950268088e328ac2ed8e9d87d1b9
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 23:04:14 +0200

    upd _release

--- src/fsi/_release/dev/bulk.h
+++ src/fsi/_release/dev/bulk.h
@@ -1,18 +1,17 @@
 namespace dev {
+static __device__ float2 get(const float2 *p) { return __ldg(p); }
 static __device__ void p2rv(const float2 *p, int i, /**/
                             float  *x, float  *y, float  *z,
                             float *vx, float *vy, float *vz) {
     float2 s0, s1, s2;
     p += 3*i;
-    s0 = __ldg(p++); s1 = __ldg(p++); s2 = __ldg(p++);
+    s0 = get(p++); s1 = get(p++); s2 = get(p++);
      *x = fst(s0);  *y = scn(s0);  *z = fst(s1);
     *vx = scn(s1); *vy = fst(s2); *vz = scn(s2);
 }
 
-static __device__ Pa pp2p(float2 *pp, int i) {
-    Pa p;
-    p2rv(pp, i, /**/ &p.x, &p.y, &p.z,   &p.vx, &p.vy, &p.vz);
-    return p;
+static __device__ void pp2p(float2 *pp, int i, /**/ Pa *p) {
+    p2rv(pp, i, /**/ &p->x, &p->y, &p->z,   &p->vx, &p->vy, &p->vz);
 }
 
 static __device__ int p2map(int zplane, int n, const Pa p, /**/ Map *m) {
@@ -48,7 +47,7 @@ static __device__ void bulk2(float2 *pp, int i, int zplane, int n, float seed, /
     Pa p;
     Fo f; /* "local" particle */
     Map m;
-    p = pp2p(pp, i);
+    pp2p(pp, i, /**/ &p);
     f = ff2f(ff, i);
     if (!p2map(zplane, n, p, /**/ &m)) return;
     bulk1(p, f, i, m, seed, /**/ ff0);
