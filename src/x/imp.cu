dde37be8da336aa025f6cbc924bd8dcb62d9bae9
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 02:43:41 +0200

    rm code from x/imp.cu

--- src/x/imp.cu
+++ src/x/imp.cu
@@ -80,9 +80,6 @@
 #include "cnt/int.h"
 #include "fsi/int.h"
 
-#include "sdstr.decl.h"
-#include "sdstr.impl.h"
-
 #include "x/type.h"
   #include "rex/type/remote.h"
   #include "rex/type/local.h"
@@ -118,96 +115,3 @@
 #include "x/ini.h"
 #include "x/fin.h"
 #include "x/imp.h"
-
-#include "dpd/local.h"
-
-namespace dpdx {
-namespace dev {
-#include "dpd/x/dev.h"
-}
-#include "dpd/x/imp.h"
-}
-
-#include "dpdr/type.h"
-#include "dpdr/int.h"
-
-#include "mesh/collision.h"
-#include "mesh/bbox.h"
-
-#include "solid.h"
-#include "tcells/int.h"
-
-#include "mbounce/imp.h"
-#include "mrescue.h"
-
-#include "bbhalo.decl.h"
-#include "bbhalo.impl.h"
-
-#include "dump.h"
-
-#include "rig/int.h"
-
-namespace sim {
-namespace dev {
-#ifdef FORWARD_EULER
-  #include "sim/sch/euler.h"
-#else
-  #include "sim/sch/vv.h"
-#endif
-
-#include "sim/dev.h"
-}
-#include "sim/dec.h"
-#include "sim/ini.h"
-#include "sim/fin.h"
-#include "sim/generic.h"
-#include "sim/dump.h"
-#include "sim/tag.h"
-#include "sim/forces/dpd.h"
-#include "sim/forces.h"
-
-#if   defined(FORCE1)
-  #include "sim/force1.h"
-#elif defined(FORCE0)
-  #include "sim/force0.h"
-#else
-  #error FORCE[01] is undefined
-#endif
-
-
-#define HST (true)
-#define DEV (false)
-#define DEVICE_SOLID
-#ifdef DEVICE_SOLID
-  #include "0dev/sim.impl.h"
-#else
-  #include "0hst/sim.impl.h"
-#endif
-#undef HST
-#undef DEV
-
-#if   defined(UPDATE1)
-  #include "sim/update/release.h"
-#elif defined(UPDATE_SAFE)
-  #include "sim/update/safe.h"
-#else
-  #error UPDATE* is undefined
-#endif
-
-#if   defined(ODSTR1)
-  #include "sim/odstr/release.h"
-#elif defined(ODSTR0)
-  #include "sim/odstr/none.h"
-#elif defined(ODSTR_SAFE)
-  namespace sub {
-    #include "sim/odstr/release.h"
-  }
-  #include "sim/odstr/safe.h"
-#else
-  #error ODSTR* is undefined
-#endif
-
-#include "sim/step.h"
-#include "sim/run.h"
-#include "sim/imp.h"
-}
