d48aa0ec895381782eca7553656ebbe1931df689
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 22:55:52 +0200

    use get for __ldg

--- src/fsi/_safe/dev/bulk.h
+++ src/fsi/_safe/dev/bulk.h
@@ -1,10 +1,12 @@
 namespace dev {
+
+static __device__ float2 get(const float2 *p) { return __ldg(p); }
 static __device__ void p2rv(const float2 *p, int i, /**/
                             float  *x, float  *y, float  *z,
                             float *vx, float *vy, float *vz) {
     float2 s0, s1, s2;
     p += 3*i;
-    s0 = __ldg(p++); s1 = __ldg(p++); s2 = __ldg(p++);
+    s0 = get(p++); s1 = get(p++); s2 = get(p++);
      *x = fst(s0);  *y = scn(s0);  *z = fst(s1);
     *vx = scn(s1); *vy = fst(s2); *vz = scn(s2);
 
