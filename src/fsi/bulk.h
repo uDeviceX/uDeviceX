7133215d75250758fe2df30246b8f24b2e7dc3f5
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 22:37:53 +0200

    clean up bulk

--- src/fsi/bulk.h
+++ src/fsi/bulk.h
@@ -1,10 +1,21 @@
 namespace fsi {
+static void bulk0(ParticlesWrap *w) {
+    int n0, n1;
+    float rnd;
+    rnd = rgen->get_float();
+    n0 = w->n;
+    n1 = wo->n;
+    KL(dev::bulk, (k_cnf(3*n0)), ((float2 *)w->p, n0, n1, rnd, (float*)w->f, (float*)wo->f));
+}
+
 void bulk(std::vector<ParticlesWrap> wr) {
-    if (wr.size() == 0) return;
+    int i, n;
+    ParticlesWrap *w; /* wrap */
+    n = wr.size();
+    w = wr.data(); 
+    
+    if (n == 0) return;
     setup(wo->p, wo->n, wo->cellsstart);
-    for (std::vector<ParticlesWrap>::iterator it = wr.begin(); it != wr.end(); ++it)
-        KL(dev::bulk,
-           (k_cnf(3*it->n)),
-           ((float2 *)it->p, it->n, wo->n, rgen->get_float(), (float*)it->f, (float*)wo->f));
+    for (i = 0; i < n; i++) bulk0(w++);
 }
 }
