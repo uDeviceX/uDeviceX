9b7812b8228344aedffca1b3141b5addb8e96324
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 21:43:42 +0200

    better error report from fsi

--- src/fsi/_safe/dev/pair.h
+++ src/fsi/_safe/dev/pair.h
@@ -23,8 +23,24 @@ static __device__ bool valid_r(float3 r) {
 static __device__ bool valid_v(float3 v) {
     float x, y, z;
     bool verbose = true;
-    f3xyz(r,  &x,  &y,  &z);
+    f3xyz(v,  &x,  &y,  &z);
     return dbg::dev::valid_vel3(x, y, z, verbose);
+}
+
+static __device__ void check(float3 r1, float3 r2, float3 v1, float3 v2) {
+    check1(valid_r(r1), valid_r(r2), valid_v(v1), valid_v(v2));
+}
+
+static __device__ void check1(bool r1, bool r2, bool v1, bool v2) { /* statuses */
+    if (r1 && r2 && v1 && v2) return;
+    check0(r1, r2, v1, v2);
+    assert(0);
+}
+
+#define msg(fmt, ...) printf("%s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+static __device__ void check0(bool r1, bool r2, bool v1, bool v2) {
+    msg("pos: loc, rem: %d %d", r1, r2);
+    msg("vel: loc, rem: %d %d", v1, v2);
 }
 
 static __device__ void pair0(const Pa l, const Pa r, float rnd, /**/ float *fx, float *fy, float *fz) {
@@ -33,10 +49,7 @@ static __device__ void pair0(const Pa l, const Pa r, float rnd, /**/ float *fx, 
     r1 = make_float3( l.x,  l.y,  l.z); r2 = make_float3( r.x,  r.y,  r.z);
     v1 = make_float3(l.vx, l.vy, l.vz); v2 = make_float3(r.vx, r.vy, r.vz);
 
-    valid_r(r1);
-    valid_r(r2);
-    valid_v(v1);
-    valid_v(v2);
+    check(r1, r2, v1, v2);
     
     f = forces::dpd(SOLID_TYPE, SOLVENT_TYPE, r1, r2, v1, v2, rnd); /* TODO: type */
     *fx = f.x; *fy = f.y; *fz = f.z;
