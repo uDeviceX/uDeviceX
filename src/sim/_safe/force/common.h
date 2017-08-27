af227e090e6779decf607776dab9ac6e1ae19f97
Sergey Litvinov <slitvinov@gmail.com>
2017-08-27 19:30:05 +0200

    more dbg

--- src/sim/_safe/force/common.h
+++ src/sim/_safe/force/common.h
@@ -15,7 +15,7 @@ void body_force(float driving_force0) {
     if (pushrbc && rbcs)
         KL(dev::body_force, (k_cnf(r::q.n)), (rbc_mass, r::q.pp, r::ff, r::q.n, driving_force0));
 }
-  
+
 void forces_rbc() {
     if (rbcs)
         rbc::forces(r::q, r::tt, /**/ r::ff);
@@ -26,17 +26,23 @@ void clear_forces(Force* ff, int n) {
 }
 
 void forces_wall() {
+    dbg::check_ff(o::ff, o::q.n, F("B"));
     if (o::q.n)            wall::interactions(w::qsdf, w::q, w::t, SOLVENT_TYPE, o::q.pp, o::q.n,   /**/ o::ff);
     if (solids0 && s::q.n) wall::interactions(w::qsdf, w::q, w::t, SOLID_TYPE, s::q.pp, s::q.n, /**/ s::ff);
     if (rbcs && r::q.n)    wall::interactions(w::qsdf, w::q, w::t, SOLID_TYPE, r::q.pp, r::q.n, /**/ r::ff);
+    dbg::check_ff(o::ff, o::q.n, F("A"));
 }
 
 void forces_cnt(std::vector<ParticlesWrap> *w_r) {
+    dbg::check_ff(o::ff, o::q.n, F("B"));
     cnt::build(*w_r); /* build cells */
     cnt::bulk(*w_r);
+    dbg::check_ff(o::ff, o::q.n, F("A"));
 }
 
 void forces_fsi(SolventWrap *w_s, std::vector<ParticlesWrap> *w_r) {
+    dbg::check_ff(o::ff, o::q.n, F("B"));
     fsi::bind(*w_s);
     fsi::bulk(*w_r);
+    dbg::check_ff(o::ff, o::q.n, F("A"));
 }
