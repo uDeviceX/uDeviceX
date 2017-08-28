9d6330419a7c7c8c160138127fd08bda5e3d1c29
Sergey Litvinov <slitvinov@gmail.com>
2017-08-28 09:57:53 +0200

    SYNC in 0dev/distr.h

--- src/sim/_safe/0dev/distr.h
+++ src/sim/_safe/0dev/distr.h
@@ -1,7 +1,6 @@
 void distr_solid() {
     OO;
     SS;
-    SYNC;
     if (s::q.ns) cD2H(s::q.ss_hst, s::q.ss, s::q.ns);
     sdstr::pack_sendcnt <DEV> (s::q.ss_hst, s::q.i_pp, s::q.ns, s::q.m_dev.nv);
     s::q.ns = sdstr::post(s::q.m_dev.nv);
@@ -9,7 +8,6 @@ void distr_solid() {
     sdstr::unpack <DEV> (s::q.m_dev.nv, /**/ s::q.ss_hst, s::q.i_pp);
     if (s::q.ns) cH2D(s::q.ss, s::q.ss_hst, s::q.ns);
     solid::generate_dev(s::q.ss, s::q.ns, s::q.rr0, s::q.nps, /**/ s::q.pp);
-    SYNC;
     OO;
     SS;
 }
