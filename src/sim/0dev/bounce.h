ecbf1bf6286bd2e9e69aec3fa12f0e3cbaff30d7
Sergey Litvinov <slitvinov@gmail.com>
2017-08-28 09:04:05 +0200

    keep only relevant functions

--- src/sim/0dev/bounce.h
+++ src/sim/0dev/bounce.h
@@ -1,23 +1,3 @@
-void distr_solid() {
-    if (s::q.ns) cD2H(s::q.ss_hst, s::q.ss, s::q.ns);
-    sdstr::pack_sendcnt <DEV> (s::q.ss_hst, s::q.i_pp, s::q.ns, s::q.m_dev.nv);
-    s::q.ns = sdstr::post(s::q.m_dev.nv);
-    s::q.n = s::q.ns * s::q.nps;
-    sdstr::unpack <DEV> (s::q.m_dev.nv, /**/ s::q.ss_hst, s::q.i_pp);
-    if (s::q.ns) cH2D(s::q.ss, s::q.ss_hst, s::q.ns);
-    solid::generate_dev(s::q.ss, s::q.ns, s::q.rr0, s::q.nps, /**/ s::q.pp);
-}
-
-void update_solid0() {
-    solid::update_dev(s::ff, s::q.rr0, s::q.n, s::q.ns, /**/ s::q.pp, s::q.ss);
-    solid::update_mesh_dev(s::q.ss, s::q.ns, s::q.m_dev, /**/ s::q.i_pp);
-
-    // for dump
-    cD2H(s::q.ss_dmp, s::q.ss, s::q.ns);
-
-    solid::reinit_ft_dev(s::q.ns, /**/ s::q.ss);
-}
-
 void bounce_solid(int it) {
     mesh::get_bboxes_dev(s::q.i_pp, s::q.m_dev.nv, s::q.ns, /**/ s::t.minbb_dev, s::t.maxbb_dev);
 
--- src/sim/0hst/bounce.h
+++ src/sim/0hst/bounce.h
@@ -1,28 +1,4 @@
-void distr_solid() {
-    sdstr::pack_sendcnt <HST> (s::q.ss_hst, s::q.i_pp_hst, s::q.ns, s::q.m_hst.nv);
-    s::q.ns = sdstr::post(s::q.m_hst.nv);
-    s::q.n = s::q.ns * s::q.nps;
-    sdstr::unpack <HST> (s::q.m_hst.nv, /**/ s::q.ss_hst, s::q.i_pp_hst);
-    solid::generate_hst(s::q.ss_hst, s::q.ns, s::q.rr0_hst, s::q.nps, /**/ s::q.pp_hst);
-    cH2D(s::q.pp, s::q.pp_hst, 3 * s::q.n);
-}
 
-void update_solid0() {
-    cD2H(s::q.pp_hst, s::q.pp, s::q.n);
-    cD2H(s::ff_hst, s::ff, s::q.n);
-  
-    solid::update_hst(s::ff_hst, s::q.rr0_hst, s::q.n, s::q.ns, /**/ s::q.pp_hst, s::q.ss_hst);
-    solid::update_mesh_hst(s::q.ss_hst, s::q.ns, s::q.m_hst, /**/ s::q.i_pp_hst);
-  
-    // for dump
-    memcpy(s::q.ss_dmp, s::q.ss_hst, s::q.ns * sizeof(Solid));
-  
-    solid::reinit_ft_hst(s::q.ns, /**/ s::q.ss_hst);
-  
-    cH2D(s::q.pp, s::q.pp_hst, s::q.n);
-}
-
-void bounce_solid(int it) {
     mesh::get_bboxes_hst(s::q.i_pp_hst, s::q.m_hst.nv, s::q.ns, /**/ s::t.minbb_hst, s::t.maxbb_hst);
 
     /* exchange solid meshes with neighbours */
