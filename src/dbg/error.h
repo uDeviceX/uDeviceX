91cfa3cf6dc707bc5f69eaad1181734aa908d78f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-28 14:14:35 +0200

    get_err, get_err_str

--- src/dbg/error.h
+++ src/dbg/error.h
@@ -35,6 +35,12 @@ static void ini() {
     CC(d::MemcpyToSymbol(&dev::error, &e, sizeof(err_type)));
 }
 
+static err_type get_err() {
+    err_type e;
+    CC(d::MemcpyFromSymbol(&e, &dev::error, sizeof(err_type)));
+    return e;
+}
+
 static void errmsg(err_type e, const char *fun, const char *msg) {
     if (e != NONE) {
         ERR("DBG: ERR (%s): %s %s", fun, err_str[e], msg);
@@ -42,10 +48,11 @@ static void errmsg(err_type e, const char *fun, const char *msg) {
 }
 
 static void handle(const char *fun, const char *msg) {
-    err_type err;
-    CC(d::MemcpyFromSymbol(&err, &dev::error, sizeof(err_type)));
-    errmsg(err, fun, msg);
+    err_type e = get_err();
+    errmsg(e, fun, msg);
 }
+
+static const char* get_err_str(err_type e) {return err_str[e];}
 
 } // err
 } // dbg
