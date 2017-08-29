13b090af3e6cad938d580f08ca94276dc74e9364
Sergey Litvinov <slitvinov@gmail.com>
2017-08-29 12:07:35 +0200

    copy lisergey database

--- 1/run.sh
+++ 1/run.sh
@@ -4,6 +4,7 @@ set -eu
 
 # load utilities
 . u/`u.host`
+. u/common
 
 n=1
 d=/scratch/snx3000/lisergey/u
@@ -14,13 +15,17 @@ Restart=1
 
 (cd $s/../cmd; make)
 u.conf $s $u base.h <<EOF
-       DBG_PEEK
+       #DBG_PEEK
        RESTART=true
-       ODSTR_SAFE
+       #ODSTR_SAFE
        run
 EOF
-u.make -j > make.log
+# u.make -j > make.log
+
 safe_cp "$d/$n/sdf.dat" .
+
+# copy restart db
+safe_re
 
 x=$n y=1 z=1
 
