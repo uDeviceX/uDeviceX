d58f9207bf6640997bec08da28d5185a199249da
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-29 09:08:20 +0200

    add empty dbg unit

---
+++ src/u/dbg/make/dep.mk
@@ -0,0 +1,3 @@
+$B/cc.o: $B/conf.h
+$B/d/api.o: $B/conf.h
+$B/mc.o: $B/conf.h
---
+++ src/u/dbg/make/rule.mk
@@ -0,0 +1,7 @@
+$B/cc.o: $S/cc.cpp; $X
+$B/d/api.o: $S/d/api.cu; $N
+$B/l/m.o: $S/l/m.cpp; $X
+$B/m.o: $S/m.cpp; $X
+$B/mc.o: $S/mc.cpp; $X
+$B/msg.o: $S/msg.cpp; $X
+$B/u/dbg/main.o: $S/u/dbg/main.cu; $N
