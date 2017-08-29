83fab553cbc891441d90fd1043cd361612899429
Sergey Litvinov <slitvinov@gmail.com>
2017-08-29 16:29:34 +0200

    upd deps for dang

--- src/u/dang/make/rule.mk
+++ src/u/dang/make/rule.mk
@@ -13,7 +13,7 @@ $B/dump/imp.o: $S/dump/imp.cpp; $X -I$S/. -I$S/dump
 $B/field.o: $S/field.cpp; $X -I$S/.
 $B/flu/imp.o: $S/flu/imp.cu; $N -I$S/. -I$S/flu
 $B/flu/int.o: $S/flu/int.cu; $N -I$S/. -I$S/flu
-$B/fsi/imp.o: $S/fsi/imp.cu; $N -I$S/. -I$S/fsi -I$S/fsi/_safe
+$B/fsi/imp.o: $S/fsi/imp.cu; $N -I$S/. -I$S/fsi -I$S/fsi/_dang
 $B/glb.o: $S/glb.cu; $N -I$S/.
 $B/hforces/imp.o: $S/hforces/imp.cu; $N -I$S/. -I$S/hforces
 $B/io/field.o: $S/io/field.cpp; $X -I$S/. -I$S/io
@@ -38,9 +38,9 @@ $B/minmax.o: $S/minmax.cu; $N -I$S/.
 $B/mrescue.o: $S/mrescue.cu; $N -I$S/.
 $B/msg.o: $S/msg.cpp; $X -I$S/.
 $B/odstr/halo/imp.o: $S/odstr/halo/imp.cu; $N -I$S/. -I$S/odstr/halo
-$B/odstr/imp.o: $S/odstr/imp.cu; $N -I$S/. -I$S/odstr -I$S/odstr/_safe
-$B/odstr/int.o: $S/odstr/int.cu; $N -I$S/. -I$S/odstr -I$S/odstr/_safe
-$B/odstr/pack/imp.o: $S/odstr/pack/imp.cu; $N -I$S/. -I$S/odstr/pack -I$S/odstr/pack/_safe
+$B/odstr/imp.o: $S/odstr/imp.cu; $N -I$S/. -I$S/odstr -I$S/odstr/_dang
+$B/odstr/int.o: $S/odstr/int.cu; $N -I$S/. -I$S/odstr -I$S/odstr/_dang
+$B/odstr/pack/imp.o: $S/odstr/pack/imp.cu; $N -I$S/. -I$S/odstr/pack -I$S/odstr/pack/_dang
 $B/os.o: $S/os.cpp; $X -I$S/.
 $B/rbc/imp.o: $S/rbc/imp.cu; $N -I$S/. -I$S/rbc
 $B/rbc/int.o: $S/rbc/int.cu; $N -I$S/. -I$S/rbc
@@ -49,12 +49,12 @@ $B/rdstr/int.o: $S/rdstr/int.cu; $N -I$S/. -I$S/rdstr
 $B/restart.o: $S/restart.cpp; $X -I$S/.
 $B/rig/imp.o: $S/rig/imp.cu; $N -I$S/. -I$S/rig
 $B/rig/int.o: $S/rig/int.cu; $N -I$S/. -I$S/rig
+$B/rigid/imp.o: $S/rigid/imp.cu; $N -I$S/. -I$S/rigid -I$S/rigid/_cuda
 $B/rnd/imp.o: $S/rnd/imp.cpp; $X -I$S/. -I$S/rnd
 $B/scan/imp.o: $S/scan/imp.cu; $N -I$S/. -I$S/scan
 $B/sdf/imp.o: $S/sdf/imp.cu; $N -I$S/. -I$S/sdf
 $B/sdf/int.o: $S/sdf/int.cu; $N -I$S/. -I$S/sdf
-$B/sim/imp.o: $S/sim/imp.cu; $N -I$S/. -I$S/sim -I$S/sim/_safe
-$B/solid.o: $S/solid.cu; $N -I$S/.
+$B/sim/imp.o: $S/sim/imp.cu; $N -I$S/. -I$S/sim -I$S/sim/_dang
 $B/tcells/imp.o: $S/tcells/imp.cu; $N -I$S/. -I$S/tcells
 $B/tcells/int.o: $S/tcells/int.cpp; $X -I$S/. -I$S/tcells
 $B/wall/exch.o: $S/wall/exch.cpp; $X -I$S/. -I$S/wall
