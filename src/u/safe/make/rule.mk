b7c156a2f2d51ce2045227a88bc8949379c1a704
Sergey Litvinov <slitvinov@gmail.com>
2017-08-29 11:15:25 +0200

    upd deps for _safe

--- src/u/safe/make/rule.mk
+++ src/u/safe/make/rule.mk
@@ -38,8 +38,8 @@ $B/minmax.o: $S/minmax.cu; $N -I$S/.
 $B/mrescue.o: $S/mrescue.cu; $N -I$S/.
 $B/msg.o: $S/msg.cpp; $X -I$S/.
 $B/odstr/halo/imp.o: $S/odstr/halo/imp.cu; $N -I$S/. -I$S/odstr/halo
-$B/odstr/imp.o: $S/odstr/imp.cu; $N -I$S/. -I$S/odstr
-$B/odstr/int.o: $S/odstr/int.cu; $N -I$S/. -I$S/odstr
+$B/odstr/imp.o: $S/odstr/imp.cu; $N -I$S/. -I$S/odstr -I$S/odstr/_safe
+$B/odstr/int.o: $S/odstr/int.cu; $N -I$S/. -I$S/odstr -I$S/odstr/_safe
 $B/odstr/pack/imp.o: $S/odstr/pack/imp.cu; $N -I$S/. -I$S/odstr/pack -I$S/odstr/pack/_safe
 $B/os.o: $S/os.cpp; $X -I$S/.
 $B/rbc/imp.o: $S/rbc/imp.cu; $N -I$S/. -I$S/rbc
