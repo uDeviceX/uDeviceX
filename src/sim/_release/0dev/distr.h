e8ab23332d354d845a8b96821d3d9573ab0a9b69
Sergey Litvinov <slitvinov@gmail.com>
2017-08-30 00:26:11 +0200

    add dSync in front of distr_solid

--- src/sim/_release/0dev/distr.h
+++ src/sim/_release/0dev/distr.h
@@ -1,4 +1,5 @@
 void distr_solid() {
+    dSync();
     if (s::q.ns) cD2H(s::q.ss_hst, s::q.ss, s::q.ns);
     sdstr::pack_sendcnt <DEV> (s::q.ss_hst, s::q.i_pp, s::q.ns, s::q.m_dev.nv);
     s::q.ns = sdstr::post(s::q.m_dev.nv);
