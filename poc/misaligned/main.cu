5c5719425d9df268655dd54cea0335d800e4bb3b
Sergey Litvinov <slitvinov@gmail.com>
2017-08-29 17:14:54 +0200

    smaller example

--- poc/misaligned/main.cu
+++ poc/misaligned/main.cu
@@ -13,9 +13,8 @@
 #define k_cnf(n) ceiln((n), 128), 128
 
 #define n 5 /* number of elements */
-float h_A[n], h_B[n], h_C[n]; /* host */
-float *d_A, *d_B, *d_C; /* device */
-#define sz ((n)*sizeof(h_A[0]))
+int  *A;
+char *B;
 
 /*
 __global__ void func (char* stringInput, int stringSize, int* integerInput, char* dummySpace) {
@@ -28,12 +27,12 @@ __global__ void func (char* stringInput, int stringSize, int* integerInput, char
 }
 */
 
-void d_ini() {
-  cudaMalloc(&d_A, sz);
-  cudaMalloc(&d_B, sz);
+void ini() {
+    cudaMalloc(&A, n*sizeof(A[0]));
+    cudaMalloc(&B, n*sizeof(B[0]));
 }
 
 int main() {
-  d_ini();
+  ini();
   //  misaligned<<<k_cnf(n)>>>(d_A, d_B, d_C);
 }
