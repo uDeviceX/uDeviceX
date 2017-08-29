d1b3d6f779e8bd4be3a810ff10261ce4b4816b69
Sergey Litvinov <slitvinov@gmail.com>
2017-08-29 11:51:18 +0200

    upd cmd by 1/run.sh

--- 1/run.sh
+++ 1/run.sh
@@ -12,6 +12,7 @@ u=u/safe
 s=../src
 Restart=1
 
+(cd $s/../cmd; make)
 u.conf $s $u base.h <<EOF
        DBG_PEEK
        RESTART=true
@@ -24,7 +25,7 @@ safe_cp "$d/$n/sdf.dat" .
 x=$n y=1 z=1
 
 if test $Restart -eq 1
-then u.re.peak
+then u.re.pick
 else u.strtdir  . $x $y $z
 fi
 
