18751015399d18bf2b429bdcd60a47a081f27d08
Sergey Litvinov <slitvinov@gmail.com>
2017-08-30 07:18:26 +0200

    copy-past estimate() from ini()

--- src/odstr/halo/_dang/reg.h
+++ src/odstr/halo/_dang/reg.h
@@ -1,10 +1,28 @@
 namespace dev {
+#define i2d(i) { (i + 1) % 3 - 1, (i / 3 + 1) % 3 - 1, (i / 9 + 1) % 3 - 1 }
+static int estimate(const int i) {
+    const int d[3] = i2d(i);
+    int nhalodir[3] =  {
+        d[0] != 0 ? 1 : XS,
+        d[1] != 0 ? 1 : YS,
+        d[2] != 0 ? 1 : ZS
+    };
+    int nhalocells = nhalodir[0] * nhalodir[1] * nhalodir[2];
+    int safety_factor = 4;
+    return numberdensity * safety_factor * nhalocells;
+}
+
+static void check(int size, int fid) {
+
+}
+
 /* [reg]ister a particle */
-static __device__ void reg(int pid, int code, /**/ int *iidx[], int size[]) {
+static __device__ void reg(int pid, int fid, /**/ int *iidx[], int size[]) {
     int entry;
-    if (code > 0) {
-        entry = atomicAdd(size + code, 1);
-        iidx[code][entry] = pid;
+    if (fid > 0) {
+        entry = atomicAdd(size + fid, 1);
+        iidx[fid][entry] = pid;
+        check(size[fid], fid);
     }
 }
 }
