58b7aa143a7f6ad891536a82c69e967427e0da36
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 15:00:54 +0200

    add get()

--- src/k/cnt/code.h
+++ src/k/cnt/code.h
@@ -1,3 +1,11 @@
 namespace k_cnt { /* particle cell code api */
 
+static __device__ void get(int slot, /**/ int *soluteid, int *spid) {
+    CellEntry ce;
+    ce.pid = fetchID(slot);
+    *soluteid = ce.code.w;
+    ce.code.w = 0;
+    *spid = ce.pid;
+}
+
 }
