77568bbafec0c6b6870de0cd0f9ebe25283ebc92
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 16:16:53 +0200

    use assert in set

--- src/k/cnt/code.h
+++ src/k/cnt/code.h
@@ -18,13 +18,14 @@ static __device__ void get(int i, /**/ int *s, int *pid) {
 static __device__ void set(int s, int pid, int i, /**/ CellEntry *cells) {
     /* pid: particle id; s: solute id; i : slot */
     assert(pid < NMAX);
-    //    int c0 = NMAX * s + pid;
-    //    assert( c.pid == c0);
+    int c0 = NMAX * s + pid;
 
     CellEntry c;
     c.pid = pid;
     c.code.w = s;
     cells[i] = c;
+
+    assert(c.pid == c0);
 }
 
 }
