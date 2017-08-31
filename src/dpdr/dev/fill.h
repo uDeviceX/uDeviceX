893e3122bc41cfca68272b944baab8855de57439
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 22:29:16 +0200

    used wrong name

--- src/dpdr/dev/fill.h
+++ src/dpdr/dev/fill.h
@@ -41,9 +41,9 @@ __global__ void fill(const int27 cellpackstarts, const Particle *pp, const intp2
     if (gid + 1 == cellpackstarts.d[fid + 1]) required_bag_size[fid] = dst;
 }
 
-__global__ void fill__ii(const int27 cellpackstarts, const int *ii,
-                         const intp26 fragss, const intp26 fragcc, const intp26 fragcum,
-                         const int26 fragcapacity, /**/ intp26 fragii) {
+__global__ void fill_ii(const int27 cellpackstarts, const int *ii,
+                        const intp26 fragss, const intp26 fragcc, const intp26 fragcum,
+                        const int26 fragcapacity, /**/ intp26 fragii) {
     int gid, fid, hci, tid, src, dst, nsrc;
     int lpid, dpid, spid;
 
