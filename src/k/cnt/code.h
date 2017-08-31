56bd3be79e364d1650f59ace9f9bfa73c252651e
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 16:19:20 +0200

    use module in api

--- src/k/cnt/code.h
+++ src/k/cnt/code.h
@@ -5,27 +5,16 @@ static __device__ void get(int i, /**/ int *s, int *pid) {
     /* pid: particle id; s: solute id; i : slot */
     CellEntry c;
     c.pid = fetchID(i);
-    int pid0 = c.pid % NMAX, s0   = c.pid / NMAX;
-
-    *s = c.code.w;
-    c.code.w = 0;
-    *pid = c.pid;
-
-    assert(*pid == pid0);
-    assert(  *s ==   s0);
+    *pid = c.pid % NMAX;
+    *s   = c.pid / NMAX;
 }
 
 static __device__ void set(int s, int pid, int i, /**/ CellEntry *cells) {
     /* pid: particle id; s: solute id; i : slot */
     assert(pid < NMAX);
-    int c0 = NMAX * s + pid;
-
     CellEntry c;
-    c.pid = pid;
-    c.code.w = s;
+    c.pid = NMAX * s + pid;
     cells[i] = c;
-
-    assert(c.pid == c0);
 }
 
 }
