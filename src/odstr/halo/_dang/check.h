ce2dcffa959821f3ee5896d75ad93774da33ddee
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 13:28:14 +0200

    minor

--- src/odstr/halo/_dang/check.h
+++ src/odstr/halo/_dang/check.h
@@ -3,7 +3,7 @@ static __device__ int  valid(float r, int L) {
     float lo, hi;
     lo = -L/2 - 1;
     hi =  L/2 + 1;
-    return r > hi || r < lo;
+    return r > lo && r < hi;
 };
 static __device__ void check(const float r[3]) {};
 }
