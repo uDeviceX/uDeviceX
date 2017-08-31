a13ff0c2837b40bd1f93f2d8ac731608aff853d2
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 15:28:39 +0200

    spid/pid

--- src/k/cnt/code.h
+++ src/k/cnt/code.h
@@ -1,11 +1,11 @@
 namespace k_cnt { /* particle cell code api */
 
-static __device__ void get(int i, /**/ int *s, int *spid) {
+static __device__ void get(int i, /**/ int *s, int *pid) {
     CellEntry c;
     c.pid = fetchID(i);
     *s = c.code.w;
     c.code.w = 0;
-    *spid = c.pid;
+    *pid = c.pid;
 }
 
 static __device__ void set(int s, int pid, int i, /**/ CellEntry *cells) {
