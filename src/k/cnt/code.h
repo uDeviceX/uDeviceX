86979696720f39292b3aab076bde53ddf6db89ed
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 15:18:21 +0200

    rename parameter in api

--- src/k/cnt/code.h
+++ src/k/cnt/code.h
@@ -1,18 +1,18 @@
 namespace k_cnt { /* particle cell code api */
 
-static __device__ void get(int slot, /**/ int *soluteid, int *spid) {
-    CellEntry ce;
-    ce.pid = fetchID(slot);
-    *soluteid = ce.code.w;
-    ce.code.w = 0;
-    *spid = ce.pid;
+static __device__ void get(int i, /**/ int *s, int *spid) {
+    CellEntry c;
+    c.pid = fetchID(i);
+    *s = c.code.w;
+    c.code.w = 0;
+    *spid = c.pid;
 }
 
-static __device__ void set(int soluteid, int pid, int slot, /**/ CellEntry *entrycells) {
-    CellEntry myentrycell;
-    myentrycell.pid = pid;
-    myentrycell.code.w = soluteid;
-    entrycells[slot] = myentrycell;
+static __device__ void set(int s, int pid, int i, /**/ CellEntry *cells) {
+    CellEntry c;
+    c.pid = pid;
+    c.code.w = s;
+    cells[i] = c;
 }
 
 }
