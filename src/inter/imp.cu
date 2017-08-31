368be196df1f6ee9222deb7f7a966c39c6690611
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-08-31 18:10:05 +0200

    less includes

--- src/inter/imp.cu
+++ src/inter/imp.cu
@@ -12,12 +12,8 @@
 #include "mpi/wrapper.h"
 #include "algo/scan/int.h"
 
-#include "rnd/imp.h"
-#include "rnd/dev.h"
-
 #include "inc/def.h"
 #include "msg.h"
-#include "mpi/glb.h"
 #include "utils/cc.h"
 #include "utils/mc.h"
 #include "frag.h"
@@ -30,58 +26,24 @@
 
 #include "dual/type.h"
 #include "dual/int.h"
-#include "utils/texo.h"
-#include "utils/te.h"
 
 #include "inc/tmp/wrap.h"
 #include "inc/tmp/pinned.h"
 
 #include "dbg/imp.h"
 
-#include "glb.h"
+#include "utils/texo.h"
 
+#include "rnd/imp.h"
 #include "clist/int.h"
-
-
-#include "rbc/int.h"
-
-#include "mdstr/buf.h"
-#include "mdstr/tic.h"
-#include "mdstr/int.h"
-#include "rdstr/int.h"
-
-#include "field.h"
-
-#include "forces/imp.h"
 
 #include "sdf/type.h"
 #include "sdf/int.h"
-
+#include "flu/int.h"
+#include "rbc/int.h"
+#include "rig/int.h"
 #include "wall/int.h"
 
-#include "flu/int.h"
-
-#include "odstr/type.h"
-#include "odstr/int.h"
-#include "cnt/int.h"
-#include "fsi/int.h"
-
-#include "x/int.h"
-#include "dpd/local.h"
-
-#include "dpdr/type.h"
-#include "dpdr/int.h"
-
-#include "mesh/collision.h"
-#include "mesh/bbox.h"
-
-#include "rigid/int.h"
-#include "tcells/int.h"
-
-#include "mbounce/imp.h"
-#include "mrescue.h"
-
-#include "rig/int.h"
 #include "int.h"
 
 /* local */
