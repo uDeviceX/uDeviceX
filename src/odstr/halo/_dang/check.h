560c001b20206eadf13813694c2c1a0c2f396326
Sergey Litvinov <slitvinov@gmail.com>
2017-08-31 13:32:48 +0200

    add report and valid

--- src/odstr/halo/_dang/check.h
+++ src/odstr/halo/_dang/check.h
@@ -1,9 +1,26 @@
 namespace dev {
-static __device__ int  valid(float r, int L) {
+static __device__ int  valid0(float r, int L) {
     float lo, hi;
     lo = -L/2 - 1;
     hi =  L/2 + 1;
     return r > lo && r < hi;
 };
-static __device__ void check(const float r[3]) {};
+
+static __device__ int  valid(const float r[3]) {
+    enum {X, Y, Z};
+    return valid0(r[X], XS) && valid0(r[Y], YS) && valid0(r[Z], ZS);
+};
+
+static __device__ void report(const float r[3]) {
+    enum {X, Y, Z};
+    printf("wild particle: [%g %g %g]\n", r[X], r[Y], r[Z]);
+}
+
+static __device__ void check(const float r[3]) {
+    if (valid(r)) return;
+    report(r);
+    assert(0);
+};
+
+
 }
