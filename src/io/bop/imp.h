8ea0f7581879137841e3e5bd19e4d630b6e5f14f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-01 11:50:04 +0200

    rm unused field

--- src/io/bop/imp.h
+++ src/io/bop/imp.h
@@ -1,8 +1,7 @@
 namespace bop {
 struct Ticket {
     float *w_pp;    // particle workspace
-    int mi[3];      // global coordinates of my origin
-    MPI_Datatype dumptype;
+    int mi[3];      // global coordinates of my originx
 };
 
 void ini(Ticket *t);
