9703bd4cd094f0ff6648b256768e75ea0e90727b
Sergey Litvinov <slitvinov@gmail.com>
2017-09-02 01:14:39 +0200

    split r and get

--- src/hforces/cloud/get.h
+++ src/hforces/cloud/get.h
@@ -6,10 +6,4 @@ void cloudA_get(CloudA c, int i, /**/ force::Pa *p) {
 void cloudB_get(CloudB c, int i, /**/ force::Pa *p) {
 }
 
-void cloudA_r(CloudA c, int i, /**/ float r[3]) {
-}
-
-void cloudB_r(CloudA c, int i, /**/ float r[3]) {
-}
-
 }} /* namespace */
---
+++ src/hforces/cloud/r.h
@@ -0,0 +1,8 @@
+namespace hforces { namespace dev {
+void cloudA_r(CloudA c, int i, /**/ float r[3]) {
+}
+
+void cloudB_r(CloudA c, int i, /**/ float r[3]) {
+}
+
+}} /* namespace */
