1d82eb02a3a2c1f3091cbf5be6afef63dfcac8e2
Sergey Litvinov <slitvinov@gmail.com>
2017-09-01 14:57:29 +0200

    decl Pa a, b; upd deps

--- src/fsi/dev/pair.h
+++ src/fsi/dev/pair.h
@@ -1,6 +1,7 @@
 namespace dev {
 static __device__ void pair0(const Pa l, const Pa r, float rnd, /**/ float *fx, float *fy, float *fz) {
     /* pair force ; l, r: local and remote */
+    Pa a, b;
     float3 r1, r2, v1, v2, f;
     r1 = make_float3( l.x,  l.y,  l.z); r2 = make_float3( r.x,  r.y,  r.z);
     v1 = make_float3(l.vx, l.vy, l.vz); v2 = make_float3(r.vx, r.vy, r.vz);
