af508b6f6f671acc4b72ec7cda9813f69f7a1f5d
Sergey Litvinov <slitvinov@gmail.com>
2017-09-01 10:06:41 +0200

    rename

--- src/forces/imp.h
+++ src/forces/imp.h
@@ -16,7 +16,7 @@ inline __device__ void dpd00(int typed, int types,
     float aij[] = {aij_solv, aij_solid, aij_wall, aij_rbc};
 
     float rij2, invrij, rij;
-    float argwr, wr, rdotv, gamma, sigmaf_pair;
+    float argwr, wr, rdotv, gamma, sigma;
     float f;
     float invr2, t2, t4, t6, lj;
     float aij_pair;
@@ -39,8 +39,8 @@ inline __device__ void dpd00(int typed, int types,
     rdotv = x * vx + y * vy + z * vz;
 
     gamma = 0.5 * (gammadpd[typed] + gammadpd[types]);
-    sigmaf_pair = sqrt(2*gamma*kBT / dt);
-    f = (-gamma * wr * rdotv + sigmaf_pair * rnd) * wr;
+    sigma = sqrt(2*gamma*kBT / dt);
+    f = (-gamma * wr * rdotv + sigma * rnd) * wr;
 
     bool ss = (typed == SOLID_TYPE) && (types == SOLID_TYPE);
     bool sw = (typed == SOLID_TYPE) && (types ==  WALL_TYPE);
