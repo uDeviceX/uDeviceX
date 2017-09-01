9ade905f279fbdcf933da9f5530eb55a438ee8f7
Sergey Litvinov <slitvinov@gmail.com>
2017-09-02 01:12:32 +0200

    upd an interface

---
+++ src/hforces/cloud/imp.h
@@ -0,0 +1,4 @@
+namespace hforces {
+void ini_cloudA(CloudA *c) { };
+void ini_cloudB(CloudB *c) { };
+} /* namespace */
---
+++ src/hforces/cloud/type.h
@@ -0,0 +1,8 @@
+namespace hforces {
+struct CloudA {
+};
+
+struct CloudB {
+};
+
+} /* namespace */
