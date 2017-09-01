ed1092d07e9191da8fbd2f4aa4e4ac1b57afc63b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-01 12:01:22 +0200

    add dump particles with forces

--- src/io/bop/imp.cpp
+++ src/io/bop/imp.cpp
@@ -25,7 +25,7 @@ void ini(Ticket *t) {
     const int L[3] = {XS, YS, ZS};        
     for (int c = 0; c < 3; ++c) t->mi[c] = (m::coords[c] + 0.5) * L[c];
 
-    t->w_pp = new float[6*MAX_PART_NUM];
+    t->w_pp = new float[9*MAX_PART_NUM];
 }
 
 void fin(Ticket *t) {
@@ -40,9 +40,18 @@ static void copy_shift(const Particle *pp, const long n, const int mi[3], /**/ f
     }
 }
 
+static void copy_shift_with_forces(const Particle *pp, const Force *ff, const long n, const int mi[3], /**/ float *w) {
+    for (int j = 0; j < n; ++j)
+    for (int d = 0; d < 3; ++d) {
+        w[9 * j + d]     = pp[j].r[d] + mi[d];
+        w[9 * j + 3 + d] = pp[j].v[d];
+        w[9 * j + 6 + d] = ff[j].f[d];
+    }
+}
+
 #define PATTERN "%s-%05d"
     
-static void header_pp(const long n, const char *name, const int step) {
+static void header_pp(const long n, const char *name, const int step, const char *extrafields = "") {
     char fname[256] = {0};
     sprintf(fname, DUMP_BASE "/bop/" PATTERN ".bop", name, step / part_freq);
         
@@ -54,7 +63,7 @@ static void header_pp(const long n, const char *name, const int step) {
     fprintf(f, "%ld\n", n);
     fprintf(f, "DATA_FILE: " PATTERN ".values\n", name, step / part_freq);
     fprintf(f, "DATA_FORMAT: float\n");
-    fprintf(f, "VARIABLES: x y z vx vy vz\n");
+    fprintf(f, "VARIABLES: x y z vx vy vz %s\n", extrafields);
     fclose(f);
 }
 
@@ -92,6 +101,30 @@ void parts(const Particle *pp, const long n, const char *name, const int step, T
     MC(MPI_File_get_position(f, &base)); 
 
     if (m::rank == 0) header_pp(ntot, name, step);
+
+    MC( MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, m::cart) );
+    MC( MPI_File_write_at_all(f, base + offset, t->w_pp, n, datatype::particle, &status) ); 
+    MC( MPI_File_close(&f) );
+}
+
+void parts_forces(const Particle *pp, const Force *ff, const long n, const char *name, const int step, /*w*/ Ticket *t) {
+    copy_shift_with_forces(pp, ff, n, t->mi, /**/ t->w_pp);
+            
+    char fname[256] = {0};
+    sprintf(fname, DUMP_BASE "/bop/" PATTERN ".values", name, step / part_freq);
+
+    MPI_File f;
+    MPI_Status status;
+    MPI_Offset base, offset = 0;
+    MPI_Offset len = n * (sizeof(Particle) + sizeof(Force));
+
+    long ntot = 0;
+    MC(m::Reduce(&n, &ntot, 1, MPI_LONG, MPI_SUM, 0, m::cart));
+    MC(MPI_File_open(m::cart, fname, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f));
+    MC(MPI_File_set_size(f, 0));
+    MC(MPI_File_get_position(f, &base)); 
+
+    if (m::rank == 0) header_pp(ntot, name, step, "fx fy fz");
 
     MC( MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, m::cart) );
     MC( MPI_File_write_at_all(f, base + offset, t->w_pp, n, datatype::particle, &status) ); 
