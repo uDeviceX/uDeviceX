de503aebc92cd055a61872a51f8a2bc04e133c2a
Sergey Litvinov <slitvinov@gmail.com>
2017-09-01 13:44:22 +0200

    add and interfaces for f2k2p

--- src/forces/pack.h
+++ src/forces/pack.h
@@ -1,4 +1,4 @@
-namespace forces { /* create Pa from all kind of garbage */
+namespace forces { /* create Pa from all kinds of garbage */
 inline __device__ void rvk2p(float r[3], float v[3], int kind, /**/ Pa *p) {
     enum {X, Y, Z};
     p->x = r[X];
@@ -10,6 +10,11 @@ inline __device__ void rvk2p(float r[3], float v[3], int kind, /**/ Pa *p) {
     p->vz = v[Z];
 
     p->kind = kind;
+}
+
+inline __device__ float fst(float2 t) { return t.x; }
+inline __device__ float scn(float2 t) { return t.y; }
+inline __device__ void f2k2p(float2 d0, float2 d1, int kind, /**/ Pa *p) {
 }
 
 }
