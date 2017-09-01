53172185b0941d6c779bb6ae5a49728423d3d3ff
Sergey Litvinov <slitvinov@gmail.com>
2017-09-01 13:04:19 +0200

    minor

--- src/dpd/dev/dpd.h
+++ src/dpd/dev/dpd.h
@@ -7,6 +7,11 @@ static __device__ float3 dpd(int dpid, float4 rdest, float4 udest, float4 rsrc, 
     f2tof3(rdest, &r1); f2tof3(rsrc, &r2);
     f2tof3(udest, &v1); f2tof3(usrc, &v2);
 
-    f = forces::dpd(SOLVENT_TYPE, SOLVENT_TYPE, r1, r2, v1, v2, rnd);
+    f = forces::dpd(SOLVENT_TYPE, SOLVENT_TYPE,
+                    r1,
+                    r2,
+                    v1,
+                    v2,
+                    rnd);
     return f;
 }
