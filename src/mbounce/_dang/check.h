e5dd45f90a21ce405efba7749e28f1cb7721a21f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-01 15:04:04 +0200

    check in dang

--- src/mbounce/_dang/check.h
+++ src/mbounce/_dang/check.h
@@ -2,8 +2,23 @@ namespace mbounce {
 namespace sub {
 namespace dev {
 
-static _HD_ void check_pos(const float r[3]) {}
-static _HD_ void check_vel(const float r[3]) {}
+static _HD_ bool valid(float x, int L) {
+    bool ok = x >= -3*L/2 && x < 3*L/2;
+    if (!ok) printf(": BB : %g not in bounds %d\n", x, L);
+    return ok; 
+}
+
+static _HD_ void check_pos(const float r[3]) {
+    assert(valid(r[X], XS));
+    assert(valid(r[Y], YS));
+    assert(valid(r[Z], ZS));
+}
+
+static _HD_ void check_vel(const float v[3]) {
+    assert(valid(v[X], XS / dt));
+    assert(valid(v[Y], YS / dt));
+    assert(valid(v[Z], ZS / dt));
+}
 
 } // dev
 } // sub
