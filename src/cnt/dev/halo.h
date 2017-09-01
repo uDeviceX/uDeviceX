ebfff7497b2be7a005223a08e041ca23c1e8a980
Sergey Litvinov <slitvinov@gmail.com>
2017-09-01 22:15:33 +0200

    rename warp releated

--- src/cnt/dev/halo.h
+++ src/cnt/dev/halo.h
@@ -4,10 +4,10 @@ __global__ void halo(int nparticles_padded, float seed) {
     float x, y, z;
     forces::Pa a, b;
     float fx, fy, fz;
-    int laneid, warpid, base, pid;
+    int dw, warp, ws, pid;
     int nunpack;
     float2 dst0, dst1, dst2;
-    int fid, unpackbase;
+    int fid, dwe;
     float xforce, yforce, zforce;
     int nzplanes, zplane;
     float *dst = NULL;
@@ -18,24 +18,24 @@ __global__ void halo(int nparticles_padded, float seed) {
     float2 stmp0, stmp1, stmp2;
     float rnd;
 
-    laneid = threadIdx.x % warpSize;
-    warpid = threadIdx.x / warpSize;
-    base = 32 * (warpid + 4 * blockIdx.x);
-    pid = base + laneid;
-    if (base >= nparticles_padded) return;
+    dw = threadIdx.x % warpSize;
+    warp = threadIdx.x / warpSize;
+    ws = 32 * (warp + 4 * blockIdx.x);
+    pid = ws + dw;
+    if (ws >= nparticles_padded) return;
 
-    fid = k_common::fid(g::starts, base);
-    unpackbase = base - g::starts[fid];
-    nunpack = min(32, g::counts[fid] - unpackbase);
+    fid = k_common::fid(g::starts, ws);
+    dwe = ws - g::starts[fid];
+    nunpack = min(32, g::counts[fid] - dwe);
 
     if (nunpack == 0) return;
 
-    k_read::AOS6f((float2*)(g::pp[fid] + unpackbase),
+    k_read::AOS6f((float2*)(g::pp[fid] + dwe),
                   nunpack, dst0, dst1, dst2);
-    dst = (float *)(g::ff[fid] + unpackbase);
+    dst = (float *)(g::ff[fid] + dwe);
     k_read::AOS3f(dst, nunpack, xforce, yforce, zforce);
 
-    nzplanes = laneid < nunpack ? 3 : 0;
+    nzplanes = dw < nunpack ? 3 : 0;
     for (zplane = 0; zplane < nzplanes; ++zplane) {
         x = dst0.x;
         y = dst0.y;
