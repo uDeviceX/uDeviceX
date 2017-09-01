80cb17007617210df4a343219d676d93ad3a9bdb
Sergey Litvinov <slitvinov@gmail.com>
2017-09-01 08:47:32 +0200

    Add F macro; check forces before body forces and update_solvent

--- src/sim/_dang/step.h
+++ src/sim/_dang/step.h
@@ -1,5 +1,6 @@
 #define O dbg::check_pos_pu(o::q.pp, o::q.n, __FILE__, __LINE__, "op"), \
         dbg::check_vv(o::q.pp, o::q.n, __FILE__, __LINE__, "ov")
+#define F dbg::check_ff(o::ff, o::q.n, __FILE__, __LINE__, "of")
 
 void step0(float driving_force0, bool wall0, int it) {
     if (solids0) distr_solid();
@@ -7,8 +8,12 @@ void step0(float driving_force0, bool wall0, int it) {
     forces(wall0);
     dump_diag0(it);
     if (wall0 || solids0) dump_diag_after(it);
+    F;
     body_force(driving_force0);
+    O;
+    F;
     update_solvent();
+    O;
     if (solids0) update_solid();
     if (rbcs)    update_rbc();
     O;
