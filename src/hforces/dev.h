09c272c2dfdf3f8d2531ec9c9840919f9897a254
Sergey Litvinov <slitvinov@gmail.com>
2017-09-01 14:50:49 +0200

    fill Pa a, b

--- src/hforces/dev.h
+++ src/hforces/dev.h
@@ -30,6 +30,9 @@ static __device__ Pa frag2p(const Frag frag, uint i) {
 
 static __device__ void pair(const Pa l, const Pa r, float rnd, /**/ float *fx, float *fy, float *fz) {
     forces::Pa a, b;
+    forces::r3v3k2p(l.x, l.y, l.z, l.vx, l.vy, l.vz, SOLVENT_TYPE, /**/ &a);
+    forces::r3v3k2p(r.x, r.y, r.z, r.vx, r.vy, r.vz, SOLVENT_TYPE, /**/ &b);
+
     forces::dpd0(SOLVENT_TYPE, SOLVENT_TYPE,
                  l.x, l.y, l.z,
                  r.x, r.y, r.z,
@@ -53,7 +56,7 @@ static __device__ void force0(const Rnd rnd, const Frag frag, const Map m, const
     uint i;
     uint lid, rid; /* ids */
     float x, y, z; /* pair force */
-    lid = l.id;    
+    lid = l.id;
 
     *fx = *fy = *fz = 0;
     for (i = threadIdx.x & 1; !endp(m, i); i += 2) {
