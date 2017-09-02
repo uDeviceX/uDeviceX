be24e3efabef77e50f2d55fb3efe469bab5afbbb
Sergey Litvinov <slitvinov@gmail.com>
2017-09-02 12:26:48 +0200

    ra/rb

--- src/dpd/dev/dpd.h
+++ src/dpd/dev/dpd.h
@@ -1,14 +1,14 @@
-static __device__ float3 dpd0(float4 rdest, float4 udest, float4 rsrc, float4 usrc, float rnd) {
+static __device__ float3 dpd0(float4 ra, float4 udest, float4 rb, float4 usrc, float rnd) {
     float fx, fy, fz;
 
     forces::Pa a;
     float r1[3], v1[3];
-    f4tof3(rdest, r1); f4tof3(udest, v1);
+    f4tof3(ra, r1); f4tof3(udest, v1);
     forces::rvk2p(r1, v1, SOLVENT_TYPE, /**/ &a);
 
     forces::Pa b;
     float r2[3], v2[3];
-    f4tof3(rsrc, r2);  f4tof3(usrc, v2);
+    f4tof3(rb, r2);  f4tof3(usrc, v2);
     forces::rvk2p(r2, v2, SOLVENT_TYPE, /**/ &b);
 
     forces::gen(a, b, rnd, &fx, &fy, &fz);
