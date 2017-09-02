8a07c7ad621f970b6eb572e513262a7b6ad346d8
Sergey Litvinov <slitvinov@gmail.com>
2017-09-02 20:42:14 +0200

    pp/ppA

--- src/fsi/dev/bulk.h
+++ src/fsi/dev/bulk.h
@@ -10,8 +10,8 @@ static __device__ void p2rv(const float2 *p, int i, /**/
     *vx = scn(s1); *vy = fst(s2); *vz = scn(s2);
 }
 
-static __device__ void pp2p(float2 *pp, int i, /**/ Pa *p) {
-    p2rv(pp, i, /**/ &p->x, &p->y, &p->z,   &p->vx, &p->vy, &p->vz);
+static __device__ void pp2p(float2 *ppA, int i, /**/ Pa *p) {
+    p2rv(ppA, i, /**/ &p->x, &p->y, &p->z,   &p->vx, &p->vy, &p->vz);
 }
 
 static __device__ int p2map(int zplane, int n, const Pa p, /**/ Map *m) {
@@ -43,22 +43,22 @@ static __device__ void bulk1(const Pa l, const Fo f, int i, const Map m, float s
     atomicAdd(f.z, fz);
 }
 
-static __device__ void bulk2(float2 *pp, int i, int zplane, int n, float seed, /**/ float *ff, float *ff0) {
+static __device__ void bulk2(float2 *ppA, int i, int zplane, int n, float seed, /**/ float *ff, float *ff0) {
     Pa p;
     Fo f; /* "local" particle */
     Map m;
-    pp2p(pp, i, /**/ &p);
+    pp2p(ppA, i, /**/ &p);
     f = ff2f(ff, i);
     if (!p2map(zplane, n, p, /**/ &m)) return;
     bulk1(p, f, i, m, seed, /**/ ff0);
 }
 
-__global__ void bulk(float2 *pp, int n0, int n1, float seed, float *ff, float *ff0) {
+__global__ void bulk(float2 *ppA, int n0, int n1, float seed, float *ff, float *ff0) {
     int gid, i, zplane;
     gid    = threadIdx.x + blockDim.x * blockIdx.x;
     i      = gid / 3;
     zplane = gid % 3;
     if (i >= n0) return;
-    bulk2(pp, i, zplane, n1, seed, ff, ff0);
+    bulk2(ppA, i, zplane, n1, seed, ff, ff0);
 }
 }
