0fc4ad04bd2036f501a1aa00ac30c924c1b14bef
Sergey Litvinov <slitvinov@gmail.com>
2017-09-02 21:41:51 +0200

    decl up

--- src/fsi/halo.h
+++ src/fsi/halo.h
@@ -1,21 +1,21 @@
 namespace fsi {
 void halo(ParticlesWrap halos[26]) {
+    int n, i, s;
+    int counts[26], starts[27];
+    const Particle *pp[26];
     const Particle *ppB = wo->p;
     setup(ppB, wo->n, wo->cellsstart);
-    int n = 0;
-    int counts[26], starts[27];
-    for (int i = 0; i < 26; ++i) counts[i] = halos[i].n;
+    for (i = 0; i < 26; ++i) counts[i] = halos[i].n;
     CC(cudaMemcpyToSymbolAsync(dev::g::counts, counts, sizeof(counts), 0, H2D));
     starts[0] = 0;
-    for (int i = 0, s = 0; i < 26; ++i) starts[i + 1] = (s += 32 * ((halos[i].n + 31) / 32));
+    for (i = s = 0; i < 26; ++i) starts[i + 1] = (s += 32 * ((halos[i].n + 31) / 32));
     n = starts[26];
 
     CC(cudaMemcpyToSymbolAsync(dev::g::starts, starts, sizeof(starts), 0, H2D));
-    const Particle *pp[26];
-    for (int i = 0; i < 26; ++i) pp[i] = halos[i].p;
+    for (i = 0; i < 26; ++i) pp[i] = halos[i].p;
     CC(cudaMemcpyToSymbolAsync(dev::g::pp, pp, sizeof(pp), 0, H2D));
     Force *ff[26];
-    for (int i = 0; i < 26; ++i) ff[i] = halos[i].f;
+    for (i = 0; i < 26; ++i) ff[i] = halos[i].f;
     CC(cudaMemcpyToSymbolAsync(dev::g::ff, ff, sizeof(ff), 0, H2D));
 
     KL(dev::halo, (k_cnf(n)), ((float*)ppB, n, wo->n, rgen->get_float(), /**/ (float*)wo->f));
