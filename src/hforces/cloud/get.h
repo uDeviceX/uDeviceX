35573a42c2f2c7c8ce0a559c6cb77a34af3f41ef
Sergey Litvinov <slitvinov@gmail.com>
2017-09-02 08:35:23 +0200

    add cloudA_get

--- src/hforces/cloud/get.h
+++ src/hforces/cloud/get.h
@@ -1,5 +1,10 @@
 namespace hforces { namespace dev {
-void cloudA_get(CloudA c, int i, /**/ forces::Pa *p) {
+__device__ void cloudA_get(CloudA c, int i, /**/ forces::Pa *p) {
+    float *pp, *r, *v;
+    pp = c.pp;
+    r = &pp[6*i];
+    v = &pp[6*i + 3];
+    rvk2p(r, v, SOLVENT_TYPE, /**/ p);
 }
 
 void cloudB_get(CloudB c, int i, /**/ forces::Pa *p) {
