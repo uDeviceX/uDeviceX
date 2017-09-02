cddbd3e5f84c4743ab09708cf918f833872c9ff9
Sergey Litvinov <slitvinov@gmail.com>
2017-09-02 12:27:25 +0200

    more renaming

--- src/dpd/dev/dpd.h
+++ src/dpd/dev/dpd.h
@@ -1,14 +1,14 @@
-static __device__ float3 dpd0(float4 ra, float4 udest, float4 rb, float4 usrc, float rnd) {
+static __device__ float3 dpd0(float4 ra, float4 va, float4 rb, float4 vb, float rnd) {
     float fx, fy, fz;
 
     forces::Pa a;
     float r1[3], v1[3];
-    f4tof3(ra, r1); f4tof3(udest, v1);
+    f4tof3(ra, r1); f4tof3(va, v1);
     forces::rvk2p(r1, v1, SOLVENT_TYPE, /**/ &a);
 
     forces::Pa b;
     float r2[3], v2[3];
-    f4tof3(rb, r2);  f4tof3(usrc, v2);
+    f4tof3(rb, r2);  f4tof3(vb, v2);
     forces::rvk2p(r2, v2, SOLVENT_TYPE, /**/ &b);
 
     forces::gen(a, b, rnd, &fx, &fy, &fz);
@@ -17,14 +17,14 @@ static __device__ float3 dpd0(float4 ra, float4 udest, float4 rb, float4 usrc, f
 
 
 static __device__ float3 dpd1(uint bid, uint aid, float rnd) {
-    float4 xdest, xsrc, udest, usrc;
+    float4 xdest, xsrc, va, vb;
     xdest = fetchF4(bid);
-    udest = fetchF4(xadd(bid, 1u));
+    va = fetchF4(xadd(bid, 1u));
 
     xsrc  = fetchF4(aid);
-    usrc  = fetchF4(xadd(aid, 1u));
+    vb  = fetchF4(xadd(aid, 1u));
 
-    return dpd0(xdest, udest, xsrc, usrc, rnd);
+    return dpd0(xdest, va, xsrc, vb, rnd);
 }
 
 static __device__ float random(uint i, uint j) {
