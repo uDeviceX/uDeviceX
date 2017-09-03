6b09b41f09db05dc57e81ea8ad8295373884f83a
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 20:22:38 +0200

    compiles

--- src/cnt/int.h
+++ src/cnt/int.h
@@ -1,7 +1,7 @@
 namespace cnt {
+void halo(ParticlesWrap halos[26], Pap26 PP, Fop26 FF, int nn[26]);
 void ini();
 void fin();
 void build(std::vector<ParticlesWrap> wr);
 void bulk(std::vector<ParticlesWrap> wr);
-void halo(ParticlesWrap halos[26]);
 }
