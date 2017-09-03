02e25ffe5d9f2bc6daa7cdd7e6d49a898d0678cf
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 20:08:24 +0200

    add types to replace ParticlesWrap

--- src/inc/tmp/wrap.h
+++ src/inc/tmp/wrap.h
@@ -6,3 +6,7 @@ struct ParticlesWrap {
     ParticlesWrap(const Particle *const p, const int n, Force *f)
         : p(p), n(n), f(f) {}
 };
+
+ /* [pa]ritcle and [fo]rce [p]ointers ;  see also dpdr/ */
+typdef Sarray<Particle*, 26> Pap26;
+typdef Sarray<Force*,    26> Fop26;
