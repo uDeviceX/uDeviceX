e6fd5f6654a33fdba3622149dd660f643a61bf19
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 12:16:58 +0200

    decl up

--- src/cnt/dev/pop.h
+++ src/cnt/dev/pop.h
@@ -2,17 +2,18 @@ __global__ void populate(uchar4 *subindices,
                          int *cellstart, int nparticles,
                          int soluteid, int ntotalparticles,
                          int *entrycells) {
-    int pid;
+    int pid, cellid, mystart, slot;
+    uchar4 subindex;
+
     pid = threadIdx.x + blockDim.x * blockIdx.x;
     if (pid >= nparticles) return;
 
-    uchar4 subindex = subindices[pid];
+    subindex = subindices[pid];
 
     if (subindex.x == 0xff && subindex.y == 0xff && subindex.z == 0xff) return;
 
-    int cellid = subindex.x + XCELLS * (subindex.y + YCELLS * subindex.z);
-    int mystart = __ldg(cellstart + cellid);
-    int slot = mystart + subindex.w;
-
+    cellid = subindex.x + XCELLS * (subindex.y + YCELLS * subindex.z);
+    mystart = __ldg(cellstart + cellid);
+    slot = mystart + subindex.w;
     set(soluteid, pid, slot, /**/ entrycells);
 }
