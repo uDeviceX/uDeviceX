8a995dc9b0e0ff959be25fca208e521f9bf35d51
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 20:15:41 +0200

    declare PP, FF, nn

--- src/rex/halo.h
+++ src/rex/halo.h
@@ -2,6 +2,11 @@ namespace rex {
 void halo(int counts[26], RFrag *remote) {
     int i, n;
     ParticlesWrap halos[26];
+    
+    Pap26 PP;
+    Fop26 FF;
+    int   nn[26];
+    
     for (i = 0; i < 26; ++i) {
         n = counts[i];
         halos[i] = ParticlesWrap(remote[i].pp, n, remote[i].ff);
