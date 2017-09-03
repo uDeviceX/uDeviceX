7f1d7f06e429f13187ab8c46230c021706dca0d3
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 12:45:57 +0200

    rename

--- src/cnt/imp/halo.h
+++ src/cnt/imp/halo.h
@@ -1,23 +1,23 @@
 void halo(ParticlesWrap halos[26]) {
     int i, s, n;
-    int recvpackcount[26], recvpackstarts_padded[27];
+    int counts[26], starts[27];
     Force *ff[26];
-    const Particle *recvpackstates[26];
+    const Particle *pp[26];
     n = 0;
-    for (i = 0; i < 26; ++i) recvpackcount[i] = halos[i].n;
-    CC(cudaMemcpyToSymbolAsync(dev::g::counts, recvpackcount, sizeof(recvpackcount), 0, H2D));
-    recvpackstarts_padded[0] = 0;
+    for (i = 0; i < 26; ++i) counts[i] = halos[i].n;
+    CC(cudaMemcpyToSymbolAsync(dev::g::counts, counts, sizeof(counts), 0, H2D));
+    starts[0] = 0;
     for (i = s = 0; i < 26; ++i)
-        recvpackstarts_padded[i + 1] = (s += 32 * ((halos[i].n + 31) / 32));
-    n = recvpackstarts_padded[26];
+        starts[i + 1] = (s += 32 * ((halos[i].n + 31) / 32));
+    n = starts[26];
     CC(cudaMemcpyToSymbolAsync
-       (dev::g::starts, recvpackstarts_padded,
-        sizeof(recvpackstarts_padded), 0, H2D));
+       (dev::g::starts, starts,
+        sizeof(starts), 0, H2D));
     
-    for (i = 0; i < 26; ++i) recvpackstates[i] = halos[i].p;
+    for (i = 0; i < 26; ++i) pp[i] = halos[i].p;
     
-    CC(cudaMemcpyToSymbolAsync(dev::g::pp, recvpackstates,
-                               sizeof(recvpackstates), 0,
+    CC(cudaMemcpyToSymbolAsync(dev::g::pp, pp,
+                               sizeof(pp), 0,
                                H2D));
     for (i = 0; i < 26; ++i) ff[i] = halos[i].f;
     CC(cudaMemcpyToSymbolAsync(dev::g::ff, ff, sizeof(ff), 0, H2D));
