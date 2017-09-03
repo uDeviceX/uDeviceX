8a345a0f21bb87a5ad435c7edea8fec5dfb8d484
Sergey Litvinov <slitvinov@gmail.com>
2017-09-04 00:38:46 +0200

    less includes

--- src/rex/int.cu
+++ src/rex/int.cu
@@ -6,14 +6,10 @@
 #include "inc/conf.h"
 
 #include "d/api.h"
-#include "d/q.h"
 #include "d/ker.h"
-
 #include "mpi/wrapper.h"
 #include "algo/scan/int.h"
-
 #include "rnd/imp.h"
-#include "rnd/dev.h"
 
 #include "inc/def.h"
 #include "msg.h"
@@ -22,7 +18,6 @@
 #include "utils/mc.h"
 #include "frag.h"
 
-#include "utils/kl.h"
 #include "mpi/basetags.h"
 #include "inc/type.h"
 #include "mpi/type.h"
@@ -45,33 +40,6 @@
 #include "inc/dev/write.h"
 #include "inc/dev/common.h"
 #include "inc/dev/index.h"
-
-#include "clist/int.h"
-
-#include "mcomm/type.h"
-#include "mcomm/int.h"
-
-#include "rbc/int.h"
-
-#include "mdstr/buf.h"
-#include "mdstr/tic.h"
-#include "mdstr/int.h"
-#include "rdstr/int.h"
-
-#include "field.h"
-
-#include "forces/type.h"
-#include "forces/imp.h"
-
-#include "sdf/type.h"
-#include "sdf/int.h"
-
-#include "wall/int.h"
-
-#include "flu/int.h"
-
-#include "odstr/type.h"
-#include "odstr/int.h"
 #include "cnt/int.h"
 #include "fsi/int.h"
 
