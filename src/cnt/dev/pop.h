cb6780344ef160603bc292515e19024dcb3939d8
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 12:20:39 +0200

    rename

--- src/cnt/dev/pop.h
+++ src/cnt/dev/pop.h
@@ -1,16 +1,17 @@
 __global__ void populate(uchar4 *subindices,
-                         int *cellstart, int nparticles,
-                         int soluteid, 
-                         int *entrycells) {
-    int pid, cellid, mystart, slot;
+                         int *starts, int n,
+                         int soluteid,
+                         int *entry) {
+    int pid, id, start, slot;
     uchar4 subindex;
 
     pid = threadIdx.x + blockDim.x * blockIdx.x;
-    if (pid >= nparticles) return;
+    if (pid >= n) return;
     subindex = subindices[pid];
     if (subindex.x == 255 && subindex.y == 255 && subindex.z == 255) return;
-    cellid = subindex.x + XCELLS * (subindex.y + YCELLS * subindex.z);
-    mystart = __ldg(cellstart + cellid);
-    slot = mystart + subindex.w;
-    set(soluteid, pid, slot, /**/ entrycells);
+    id = subindex.x + XCELLS * (subindex.y + YCELLS * subindex.z);
+
+    start = __ldg(starts + id);
+    slot = start + subindex.w;
+    set(soluteid, pid, slot, /**/ entry);
 }
