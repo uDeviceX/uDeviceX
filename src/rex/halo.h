60d6f18c6ef030a0373a411969586db98c75e9f3
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 21:35:20 +0200

    unused

--- src/rex/halo.h
+++ src/rex/halo.h
@@ -1,17 +1,12 @@
 namespace rex {
 void halo(int counts[26], RFrag *remote) {
     int i, n;
-    ParticlesWrap halos[26];
-
     Pap26 PP; /* usage: pp = PP.d[i], n = nn[i]  */
     Fop26 FF;
 
     for (i = 0; i < 26; ++i) {
         PP.d[i] = remote[i].pp;
         FF.d[i] = remote[i].ff;
-
-        n = counts[i];
-        halos[i] = ParticlesWrap(remote[i].pp, n, remote[i].ff);
     }
 
     dSync();
