fed8494e18c7b20d21e944021a998a5a09653461
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 06:07:12 +0200

    rm padding

--- src/fsi/dev/halo.h
+++ src/fsi/dev/halo.h
@@ -9,24 +9,21 @@ static __device__ Pa warp2p(const Particle *pp, int i) {
     return p;
 }
 
-static __device__ void halo0(const float *ppB, Pa A, float *fA, int nb, float seed, int aid, int dw, int dwe, int nunpack,
+static __device__ void halo0(const float *ppB, Pa A, float *fA, int nb, float seed, int aid,
                              Particle *pp, Force *ff,
                              /**/ float *ffB) {
     enum {X, Y, Z};
     Pa B; /* remote particles */
     Fo f;
-    float *dst = NULL;
 
     Map m;
-    int nzplanes;
     int zplane;
     int i;
     int bid; /* remote particle id */
     float xforce, yforce, zforce;
 
     xforce = yforce = zforce = 0;
-    nzplanes = dw < nunpack ? 3 : 0;
-    for (zplane = 0; zplane < nzplanes; ++zplane) {
+    for (zplane = 0; zplane < 3; ++zplane) {
         if (!tex2map(zplane, nb, A.x, A.y, A.z, /**/ &m)) continue;
         for (i = 0; !endp(m, i); ++i) {
             bid = m2id(m, i);
@@ -39,37 +36,27 @@ static __device__ void halo0(const float *ppB, Pa A, float *fA, int nb, float se
     fA[X] = xforce; fA[Y] = yforce; fA[Z] = zforce;
 }
 
-static __device__ void halo1(const float *ppB, int nb, float seed, int aid, int ws, int dw, /**/ float *ffB) {
+static __device__ void halo1(const float *ppB, int nb, float seed, int aid, /**/ float *ffB) {
     int fid; /* fragment id */
-    int start, count;
+    int start;
     Particle *pp;
     Force *ff;
-    int nunpack, dwe;
     Pa A;
     float *fA;
 
-    fid = k_common::fid(g::starts, ws);
+    fid = k_common::fid(g::starts, aid);
     start = g::starts[fid];
-    count = g::counts[fid];
     pp = g::pp[fid];
     ff = g::ff[fid];
-    dwe = ws - start;
-    nunpack = min(32, count - dwe);
-    if (nunpack == 0) return;
-
     A = warp2p(pp, aid - start);
     fA = ff[aid-start].f;
-    halo0(ppB, A, fA, nb, seed, aid, dw, dwe, nunpack, pp, ff, /**/ ffB);
+    halo0(ppB, A, fA, nb, seed, aid, pp, ff, /**/ ffB);
 }
 
 __global__ void halo(const float *ppB, int n0, int nb, float seed, float *ffB) {
-    int dw, warp, ws;
     int i; /* particle id */
-    warp = threadIdx.x / warpSize;
-    dw = threadIdx.x % warpSize;
-    ws = warpSize * warp + blockDim.x * blockIdx.x;
-    if (ws >= n0) return;
     i = threadIdx.x + blockDim.x * blockIdx.x;
-    halo1(ppB, nb, seed, i, ws, dw, /**/ ffB);
+    if (i >= n0) return;
+    halo1(ppB, nb, seed, i, /**/ ffB);
 }
 }
