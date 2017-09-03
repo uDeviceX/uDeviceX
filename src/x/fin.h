7477d40e6d04f0310a2cd920b3920c36cd7a7a03
Sergey Litvinov <slitvinov@gmail.com>
2017-09-03 18:55:32 +0200

    fin_remote() in x::

--- src/x/fin.h
+++ src/x/fin.h
@@ -1,10 +1,24 @@
 namespace x {
-void fin() {
-    rex::fin();
+static void fin_tickets() {
     Pfree0(buf_pi);
-
     fin_ticketcom(tc);
     fin_ticketpack(tp);
     fin_ticketpinned(ti);
+}
+
+static void fin_remote() {
+    int i;
+    rex::RFrag* h;
+    for (i = 0; i < 26; i++) {
+        h = &remote[i];
+        Pfree(h->pp_pi);
+        Pfree(h->ff_pi);
+    }
+}
+
+void fin() {
+    rex::fin();
+    fin_remote();
+    fin_tickets();
 }
 }
