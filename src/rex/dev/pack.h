cb92686a55bd363621d537323f6eae2619f2507d
Sergey Litvinov <slitvinov@gmail.com>
2017-09-04 22:15:16 +0200

    use pid in dev/pack.h

--- src/rex/dev/pack.h
+++ src/rex/dev/pack.h
@@ -1,36 +1,31 @@
 __device__ void
 pack0(const float2 *pp, int fid,
       int count, int offset, int tstart, int *indexes,
-      int wsf, int dw, /**/ float2 *buf)
+      int i, /**/ float2 *buf)
 {
-    int dwe;  /* warp or buffer end relative to `ws' */
     int entry, pid;
     Pa p;
     
-    dwe = min(warpSize, count - wsf);
-    if (dw < dwe) {
-        entry = offset + wsf + dw;
-        pid = __ldg(indexes + entry);
-        p = pp2p(pp, pid);
-        shift(fid, &p); /* shift coordinates */
-    }
+    entry = offset + i;
+    pid = __ldg(indexes + entry);
+    p = pp2p(pp, pid);
+    shift(fid, &p); /* shift coordinates */
     p2pp(p, tstart + entry, /**/ buf);
 }
 
-__device__ void pack1(const float2 *pp, int ws, int dw, /**/ float2 *buf) {
-    int wsf;  /* warp start in fragment coordinates */
+__device__ void pack1(const float2 *pp, int pid, /**/ float2 *buf) {
+    int i;
     int fid;
 
-    fid = k_common::fid(g::starts, ws);
-    wsf = ws - g::starts[fid];
+    fid = k_common::fid(g::starts, pid);
+    i = pid - g::starts[fid];
 
     pack0(pp, fid,
-          g::counts[fid], g::offsets[fid], g::tstarts[fid], g::indexes[fid],
-          wsf, dw, /**/ buf);
+          g::counts[fid], g::offsets[fid], g::tstarts[fid], g::indexes[fid], i, /**/ buf);
 }
 
 __global__ void pack(const float2 *pp, /**/ float2 *buf) {
-    int warp;
+    int warp, pid;
     int lo, hi, step;
     int ws; /* warp start in global coordinates */
     int dw; /* shift relative to `ws' (lane) */
@@ -42,6 +37,8 @@ __global__ void pack(const float2 *pp, /**/ float2 *buf) {
     hi = g::starts[26];
     step = gridDim.x * blockDim.x;
 
-    for (ws = lo; ws < hi; ws += step)
-        pack1(pp, ws, dw, /**/ buf);
+    for (ws = lo; ws < hi; ws += step) {
+        pid = ws + dw;
+        pack1(pp, pid, /**/ buf);
+    }
 }
