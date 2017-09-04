9d7d8d55bbb73816f7e60cd4cb3c0cbc0417563a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-04 11:32:31 +0200

    ini color separately

--- src/cloud/hforces/int.h
+++ src/cloud/hforces/int.h
@@ -2,8 +2,7 @@ namespace hforces {
 inline void ini_cloud(Particle *pp, Cloud *c) {
     c->pp = (float*)pp;
 };
-inline void ini_cloud(Particle *pp, int *cc, Cloud *c) {
-    ini_cloud(pp, c);
+inline void ini_cloud_color(int *cc, Cloud *c) {
     c->cc = cc;
 };
 } /* namespace */
