d67e7bfea7019f705ad35bc0819a9c023877cc08
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-04 12:10:49 +0200

    add ini_cloud_color

--- src/cloud/lforces/int.h
+++ src/cloud/lforces/int.h
@@ -24,3 +24,8 @@ inline void ini_cloud(float4 *zip0, ushort4 *zip1, int n /* dummy c */ ) {
     CC(cudaBindTexture(&offset, &texParticlesF4, zip0, &texParticlesF4.channelDesc, sizeof(float)*8*n));
     CC(cudaBindTexture(&offset, &texParticlesH4, zip1, &texParticlesH4.channelDesc, sizeof(ushort4)*n));
 }
+
+inline void ini_cloud_color(int *cc, int n /* dummy c */ ) {
+    size_t offset;
+    CC(cudaBindTexture(&offset, &texColor, cc, &texColor.channelDesc, sizeof(int) * n));
+}
