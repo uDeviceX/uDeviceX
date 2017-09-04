d663b24c04b361c561f2419427c045ab433eb699
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-04 08:59:13 +0200

    add color to cloud hforces

--- src/cloud/hforces/int.h
+++ src/cloud/hforces/int.h
@@ -2,4 +2,8 @@ namespace hforces {
 inline void ini_cloud(Particle *pp, Cloud *c) {
     c->pp = (float*)pp;
 };
+inline void ini_cloud(Particle *pp, int *cc, Cloud *c) {
+    ini_cloud(pp, c);
+    c->cc = cc;
+};
 } /* namespace */
