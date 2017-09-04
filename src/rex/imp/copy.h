d9fe4c6d328d42c8780e59bd94cb1d4fbda21bfe
Sergey Litvinov <slitvinov@gmail.com>
2017-09-04 19:16:39 +0200

    aD2H

--- src/rex/imp/copy.h
+++ src/rex/imp/copy.h
@@ -4,7 +4,6 @@ void copy_starts(rex::TicketPack tp, /**/ rex::TicketPinned ti) {
 }
 
 void copy_offset(int nw, rex::TicketPack tp, rex::TicketPinned ti) {
-    CC(cudaMemcpyAsync(ti.counts, tp.offsets + 26 * nw, sizeof(int) * 26, D2H));
+    aD2H(ti.counts, tp.offsets + 26 * nw, 26);
 }
-
 }
