053d7692c4cac95593bf22b600a5a3cf449176af
Sergey Litvinov <slitvinov@gmail.com>
2017-09-05 00:35:15 +0200

    printf

--- src/_snow/forces/hook.h
+++ src/_snow/forces/hook.h
@@ -3,6 +3,7 @@ static __device__ void hook(int ca, int cb, float x, float y, float z, float *fx
     bool cond;
     cond = (ca == RED_COLOR || cb == RED_COLOR);
     *fx =  cond ? 1 : 0;
+    printf("snow: %d %d    %g %g %g\n", ca, cb, x, y, z);
 }
 
 } /* namespace */
