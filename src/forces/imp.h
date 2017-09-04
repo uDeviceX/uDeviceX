ce3f70e786b93281336b49252b2f1e77e3d643d1
Sergey Litvinov <slitvinov@gmail.com>
2017-09-04 10:15:22 +0200

    use old version of forces/imp.h

--- src/forces/imp.h
+++ src/forces/imp.h
@@ -1,5 +1,4 @@
 namespace forces {
-enum {LJ_NONE, LJ_ONE, LJ_TWO};
 
 static __device__ float wrf(const int s, float x) {
     if (s == 0) return x;
@@ -36,15 +35,6 @@ inline __device__ bool norm(/*io*/ float *px, float *py, float *pz, /**/ float *
         *px = x; *py = y; *pz = z; *pr = r; *pinvr = invr;
         return OK;
     }
-}
-
-inline __device__ float lj(float invr, float ljsi) {
-    float t2, t4, t6, f;
-    t2 = ljsi * ljsi * invr * invr;
-    t4 = t2 * t2;
-    t6 = t4 * t2;
-    f = ljepsilon * 24 * invr * t6 * (2 * t6 - 1);
-    return cap(f, 0, 1e4);
 }
 
 inline __device__ void dpd00(int typed, int types,
@@ -111,53 +101,12 @@ inline __device__ void dpd0(int typed, int types,
     dpd00(typed, types, dx, dy, dz, dvx, dvy, dvz, rnd, /**/ fx, fy, fz);
 }
 
-inline __device__ void gen1(Pa A, Pa B, float rnd, /**/ float *fx, float *fy, float *fz) {
+inline __device__ void gen(Pa A, Pa B, float rnd, /**/ float *fx, float *fy, float *fz) { /* generic */
     dpd0(A.kind, B.kind,
-         A.x,  A.y,  A.z,  B.x,  B.y,  B.z,
+          A.x,  A.y,  A.z,  B.x,  B.y,  B.z,
          A.vx, A.vy, A.vz, B.vx, B.vy, B.vz,
          rnd,
          fx, fy, fz);
 }
 
-inline __device__ void gen2(Pa A, Pa B, int ca, int cb, int ljkind, float rnd, /**/
-                            float *fx, float *fy, float *fz) {
-    gen1(A, B, rnd, /**/ fx, fy, fz);
-}
-
-inline __device__ void gen3(Pa A, Pa B, float rnd, /**/ float *fx, float *fy, float *fz) {
-    int ljkind; /* call LJ? */
-    int ka, kb;
-    int ca, cb; /* corrected colors */
-    int O, S, W;
-    O = SOLVENT_KIND; S = SOLID_KIND; W = WALL_KIND;
-    ka = A.kind; kb = B.kind;
-    ca = A.color; cb = B.color;
-    ljkind = LJ_NONE;
-
-    if        (ka == O && kb == O) { /* OO */
-        /* no correction */
-    } else if (ka == O && kb == S) { /* OS */
-        cb = ca;
-    } else if (ka == 0 && kb == W) { /* OW */
-        cb = ca;
-    } else if (ka == S && kb == S) { /* SS */
-        ca = cb = RED_COLOR;   ljkind = LJ_TWO;
-    } else if (ka == S && kb == W) { /* SW */
-        ca = cb = RED_COLOR;   ljkind = LJ_ONE;
-    } else {
-        //        assert(0);
-    }
-    gen2(A, B, ca, cb, ljkind, rnd, /**/ fx, fy, fz);
-}
-
-inline __device__ void gen(Pa A, Pa B, float rnd, /**/ float *fx, float *fy, float *fz) {
-    /* sort kind */
-    if (A.kind > B.kind) {
-        gen3(B, A, rnd, /**/ fx, fy, fz);
-        *fx = -(*fx);
-        *fy = -(*fy);
-        *fz = -(*fz);
-    } else
-        gen3(A, B, rnd, /**/ fx, fy, fz);
-}
 } /* namespace */
