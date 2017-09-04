fa9b8c061137e47017615e6e840d54df33854456
Sergey Litvinov <slitvinov@gmail.com>
2017-09-04 20:27:48 +0200

    upd io/field_grid.cpp

--- src/io/fields_grid.cpp
+++ src/io/fields_grid.cpp
@@ -1,7 +1,19 @@
+#include <stdlib.h>
+#include <math.h>
+
 #include <conf.h>
 #include "inc/conf.h"
+
 #include "inc/type.h"
 
+#include "d/api.h"
+
+#include "field.h"
+#include "utils/cc.h"
+#include "inc/dev.h"
+
 void dump_grid(Particle* hst) {
-    
+    int nn = 10;
+    cD2H(hst, hst, nn);
+    h5::dump(hst, nn);
 }
