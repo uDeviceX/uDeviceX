5174dbed0bdfa64670b5eb9e26ee0448798ad43c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-04 12:09:52 +0200

    add flocal_color

--- src/lforces/imp/flocal.h
+++ src/lforces/imp/flocal.h
@@ -1,14 +1,20 @@
-static void flocal0(float4 *zip0, ushort4 *zip1, int np, int *start, int *count, float seed, float *ff) {
-    int nx, ny, nz;
-    if(!fdpd_init) {
+static void setup_flocal() {
+    if (!fdpd_init) {
         setup_cloud();
+        if (multi_solvent) setup_cloud_color();
         setup();
         fdpd_init = true;
     }
+}
+
+static void ini_flocal(float4 *zip0, ushort4 *zip1, int np, int *start, int *count, float seed, float *ff) {
     tex_cells(start, count);
     ini_cloud(zip0, zip1, np);
     set_info(ff, np, seed);
+}
 
+static void launch(int np) {
+    int nx, ny, nz;
     if (XS % MYCPBX == 0 && YS % MYCPBY == 0 && ZS % MYCPBZ == 0) {
         nx = XS / MYCPBX;
         ny = YS / MYCPBY;
@@ -21,8 +27,23 @@ static void flocal0(float4 *zip0, ushort4 *zip1, int np, int *start, int *count,
     }
 }
 
+static void ini_flocal_color(int *cc, int n) {
+    ini_cloud_color(cc, n);
+}
+
 void flocal(float4 *zip0, ushort4 *zip1, int n, int *start, int *count,
 	    rnd::KISS* rnd, /**/ Force *ff) {
     if (n <= 0) return;
-    flocal0(zip0, zip1, n, start, count, rnd->get_float(), (float*)ff);
+    setup_flocal();
+    ini_flocal(zip0, zip1, n, start, count, rnd->get_float(), (float*)ff);
+    launch(n);
+}
+
+void flocal_color(float4 *zip0, ushort4 *zip1, int *colors, int n, int *start, int *count,
+                  rnd::KISS* rnd, /**/ Force *ff) {
+    if (n <= 0) return;
+    setup_flocal();
+    ini_flocal(zip0, zip1, n, start, count, rnd->get_float(), (float*)ff);
+    ini_flocal_color(colors, n);
+    launch(n);
 }
