1fb853036915589db220578a27758954bde038d4
Sergey Litvinov <slitvinov@gmail.com>
2017-09-04 22:27:44 +0200

    use a simple loop in pack

--- src/rex/dev/pack.h
+++ src/rex/dev/pack.h
@@ -1,11 +1,8 @@
-__device__ void
-pack0(const float2 *pp, int fid,
-      int count, int offset, int tstart, int *indexes,
-      int i, /**/ float2 *buf)
-{
+__device__ void pack0(const float2 *pp, int fid,
+                      int count, int offset, int tstart, int *indexes,
+                      int i, /**/ float2 *buf) {
     int entry, pid;
     Pa p;
-    
     entry = offset + i;
     pid = __ldg(indexes + entry);
     p = pp2p(pp, pid);
@@ -25,19 +22,10 @@ __device__ void pack1(const float2 *pp, int pid, /**/ float2 *buf) {
 }
 
 __global__ void pack(const float2 *pp, /**/ float2 *buf) {
-    int warp, pid;
-    int lo, hi, step;
-    int ws; /* warp start in global coordinates */
-    int dw; /* shift relative to `ws' (lane) */
-
-    warp = threadIdx.x / warpSize;
-    dw   = threadIdx.x % warpSize;
-    lo = warpSize * warp + blockDim.x * blockIdx.x;
+    int pid, hi, step;
+    pid = threadIdx.x + blockDim.x * blockIdx.x;
     hi = g::starts[26];
     step = gridDim.x * blockDim.x;
-
-    for (ws = lo; ws < hi; ws += step) {
-        pid = ws + dw;
+    for (  ; pid < hi; pid += step)
         pack1(pp, pid, /**/ buf);
-    }
 }
