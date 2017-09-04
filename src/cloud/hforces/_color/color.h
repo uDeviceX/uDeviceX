3923e8b69a4a9ee1c39279e7046ef60ec7fe0de8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-04 09:06:29 +0200

    c/color

--- src/cloud/hforces/_color/color.h
+++ src/cloud/hforces/_color/color.h
@@ -2,8 +2,8 @@ namespace hforces {
 namespace dev {
 
 inline void cloud_get_color(Cloud c, int i, /**/ forces::Pa *p) {
-    int c = c.cc[i];
-    forces::c2p(c, /**/ p);
+    int color = c.cc[i];
+    forces::c2p(color, /**/ p);
 }
 
 } // dev
