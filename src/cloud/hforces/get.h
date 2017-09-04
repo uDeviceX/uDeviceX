87a461734a69b902c2ea4557d0432ca5549e02f8
Sergey Litvinov <slitvinov@gmail.com>
2017-09-04 10:29:02 +0200

    do not use p->c

--- src/cloud/hforces/get.h
+++ src/cloud/hforces/get.h
@@ -8,6 +8,7 @@ inline __device__ void cloud_get(Cloud c, int i, /**/ forces::Pa *p) {
 }
 
 inline __device__ void cloud_get_color(Cloud c, int i, /**/ forces::Pa *p) {
-    //p->color = c.cc[i];
+    /********************* :TODO: ***********/
+    /* p->color = c.cc[i]; */
 }
 }} /* namespace */
