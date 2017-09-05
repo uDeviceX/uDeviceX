09795150d1b151022af4d151b7d96ae4bba1982c
Sergey Litvinov <slitvinov@gmail.com>
2017-09-05 12:56:20 +0200

    step.h is polymorphic again

---
+++ src/sim/_safe/step.h
@@ -0,0 +1,18 @@
+void step0(float driving_force0, bool wall0, int it) {
+    if (solids0) distr_solid();
+    if (rbcs)    distr_rbc();
+    forces(wall0);
+    dump_diag0(it);
+    if (wall0 || solids0) dump_diag_after(it);
+    body_force(driving_force0);
+    update_solvent();
+    if (solids0) update_solid();
+    if (rbcs)    update_rbc();
+    if (wall0) bounce();
+    if (sbounce_back && solids0) bounce_solid(it);
+}
+
+void step(float driving_force0, bool wall0, int it) {
+    odstr();
+    step0(driving_force0, wall0, it);
+}
