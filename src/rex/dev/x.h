befd796c181d947a37c81e8105f829b5834bd10d
Sergey Litvinov <slitvinov@gmail.com>
2017-09-05 18:16:28 +0200

    rm hardcoded debug block

--- src/rex/dev/x.h
+++ src/rex/dev/x.h
@@ -1,25 +1,9 @@
-/** do as I said not as I do **/
-__device__ int check_p(Pa *p) {
-    if (!isnan(p->s0.x)) return 1;
-    if (!isnan(p->s0.y)) return 2;
-    if (!isnan(p->s1.x)) return 3;
-    return 0;
-}
-#define report(fid)                                             \
-    do {                                                        \
-        printf("%s:%d: i: %d\n", __FILE__, __LINE__, fid);      \
-        assert(0);                                              \
-    }                                                           \
-    while (0);
-/** **/
-
 __device__ float2 get(const float2 *pp) { return *pp; }
 __device__ Pa pp2p(const float2 *pp, int i) {
     /* pp array to a particle */
     Pa p;
     pp += 3*i;
     p.s0 = get(pp++); p.s1 = get(pp++); p.s2 = get(pp++);
-    //    if (check_p(&p)) report(i);
     return p;
 }
 
@@ -41,9 +25,7 @@ __device__ void shift(int fid, Pa *p) {
     enum {X, Y, Z};
     int d[3]; /* coordinate shift */
     i2shift(fid, d);
-    //    if (check_p(p)) report(fid);
     p->s0.x += d[X];
     p->s0.y += d[Y];
     p->s1.x += d[Z];
-    //    if (check_p(p)) report(fid);
 }
