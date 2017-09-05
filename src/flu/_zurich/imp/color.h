62d9f24c68b508d8d3afb9c644fc7573206b15ec
Sergey Litvinov <slitvinov@gmail.com>
2017-09-05 22:41:36 +0200

    add imp/color

--- src/flu/_zurich/imp/color.h
+++ src/flu/_zurich/imp/color.h
@@ -1,1 +1,16 @@
-static void color(Particle *pp, int n) { }
+static void color(Particle *pp, int n) {
+    enum {X, Y, Z};
+    int i;
+    float x, y;
+    Particle p;
+    for (i = 0; i < 0; i++) {
+        p = pp[i];
+        x = p.r[X];
+        y = p.r[Y];
+        if (XS*y+YS*x > 0) {
+            /* RED */
+        } else {
+            /* BLUE */
+        }
+    }
+}
