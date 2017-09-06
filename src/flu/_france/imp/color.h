d63b852f69e7af2a781424d9a2b35cf30fa7d6c1
Sergey Litvinov <slitvinov@gmail.com>
2017-09-06 22:07:33 +0200

    test uses _france; reverse direction

--- src/flu/_france/imp/color.h
+++ src/flu/_france/imp/color.h
@@ -1,19 +1,21 @@
 static void color(Particle *pp, int n, /**/ int *cc) {
-    enum {X};
-    int lx; /* domain */
-    int i;
-    float x;
+    enum {X, Y};
+    int ly; /* domain */
+    int i, b, w, r;
+    float y;
     Particle p;
-    lx = m::lx();
+    ly = m::ly();
 
     enum {B = BLUE_COLOR, W = RED_COLOR, R = RED_COLOR}; /* white is red in France */
-    for (i = 0; i < n; i++) {
+    for (i = b = w = r = 0; i < n; i++) {
         p = pp[i];
-        x = p.r[X];
-        x += 0.5 * lx;
-        x *= 3;
-        if      (x <     lx) cc[i] = B;
-        else if (x < 2 * lx) cc[i] = W;
-        else                 cc[i] = R;
+        y = p.r[Y];
+        y += 0.5 * ly;
+        y *= 3;
+        if      (y <     ly) {cc[i] = B; b++;}
+        else if (y < 2 * ly) {cc[i] = W; w++;}
+        else                 {cc[i] = R; r++;}
     }
+    MSG("color scheme: France");
+    MSG("blue/white/red : %d/%d/%d", b, w, r);
 }
