5f83733a778fc30f9f9b30207a07b0a47b886269
Sergey Litvinov <slitvinov@gmail.com>
2017-09-06 08:36:15 +0200

    add scheme::clear_vel

--- src/sim/update.h
+++ src/sim/update.h
@@ -2,6 +2,11 @@ namespace scheme {
 void move(float mass, Particle *pp, Force *ff, int n) {
     KL(dev::update, (k_cnf(n)), (mass, pp, ff, n));
 }
+
+void clear_vel(Particle *pp, int n) {
+    KL(dev::clear_vel, (k_cnf(n)), (pp, n));
+}
+
 } /* namespace */
 
 void clear_vel() {
