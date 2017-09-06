f0b244e9da06fa6dabeb15a71af1116315f05316
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-06 09:53:57 +0200

    minor

--- src/sim/force/common.h
+++ src/sim/force/common.h
@@ -17,9 +17,9 @@ void clear_forces(Force* ff, int n) {
 }
 
 void forces_wall() {
-    if (o::q.n)            wall::interactions(w::qsdf, w::q, w::t, SOLVENT_KIND, o::q.pp, o::q.n,   /**/ o::ff);
-    if (solids0 && s::q.n) wall::interactions(w::qsdf, w::q, w::t, SOLID_KIND, s::q.pp, s::q.n, /**/ s::ff);
-    if (rbcs && r::q.n)    wall::interactions(w::qsdf, w::q, w::t, SOLID_KIND, r::q.pp, r::q.n, /**/ r::ff);
+    if (o::q.n)            wall::interactions(w::qsdf, w::q, w::t, SOLVENT_KIND, o::q.pp, o::q.n, /**/ o::ff);
+    if (solids0 && s::q.n) wall::interactions(w::qsdf, w::q, w::t, SOLID_KIND  , s::q.pp, s::q.n, /**/ s::ff);
+    if (rbcs && r::q.n)    wall::interactions(w::qsdf, w::q, w::t, SOLID_KIND  , r::q.pp, r::q.n, /**/ r::ff);
 }
 
 void forces_cnt(std::vector<ParticlesWrap> *w_r) {
