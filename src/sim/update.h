e742f32e91673a290a302f5b7399a0b043f41bc7
Sergey Litvinov <slitvinov@gmail.com>
2017-09-06 08:31:06 +0200

    call move()

--- src/sim/update.h
+++ src/sim/update.h
@@ -13,11 +13,11 @@ void move(float mass, Particle *pp, Force *ff, int n) {
 }
 
 void update_solvent() {
-    KL(dev::update, (k_cnf(o::q.n)), (dpd_mass, o::q.pp, o::ff, o::q.n));
+    move(dpd_mass, o::q.pp, o::ff, o::q.n);
 }
 
 void update_rbc() {
-    KL(dev::update, (k_cnf(r::q.n)),  (rbc_mass, r::q.pp, r::ff, r::q.n));
+    move(rbc_mass, r::q.pp, r::ff, r::q.n);
 }
 
 void bounce() {
