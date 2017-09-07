75c666e9adf1cc3addc77b5db89132485d347ef9
Sergey Litvinov <slitvinov@gmail.com>
2017-09-07 21:30:36 +0200

    use corrected cloud fetch

--- src/cloud/lforces/get.h
+++ src/cloud/lforces/get.h
@@ -1,5 +1,7 @@
 static __device__ void tex2rv(uint i, /**/ float r[3], float v[3]) {
     float4 r4, v4;
+
+    i *= 2;
     r4 = fetchF4(i);
     v4 = fetchF4(xadd(i, 1u));
     f4tof3(r4, /**/ r); f4tof3(v4, /**/ v);
@@ -11,7 +13,7 @@ static __device__ void cloud_get(uint i, /**/ forces::Pa *p) { /* local fetch */
     tex2rv(i, /**/ r, v);
     forces::rvk2p(r, v, SOLVENT_KIND, /**/ p);
     if (multi_solvent)
-        p->color = fetchC(i / 2);
+        p->color = fetchC(i);
 }
 
 static __device__ void cloud_pos(/* dummy c */ int i, /**/ float *x, float *y, float *z) {
@@ -23,5 +25,5 @@ static __device__ void cloud_pos(/* dummy c */ int i, /**/ float *x, float *y, f
 /** TODO: aid, bid are 2*pid in local */
 static __device__ void cloud_get2(uint i, /**/ forces::Pa *p) {
     assert(i % 2 == 0);
-    cloud_get(i, /**/ p);
+    cloud_get(i / 2, /**/ p);
 }
