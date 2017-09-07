7a6a237678673a5b0586c72775906d6ffc140100
Sergey Litvinov <slitvinov@gmail.com>
2017-09-07 20:04:02 +0200

    limit z in _snow

--- src/_snow/forces/hook.h
+++ src/_snow/forces/hook.h
@@ -3,7 +3,8 @@ static __device__ void hook(int ca, int cb, float x, float y, float z, float *fx
     bool cond;
     cond = (ca == RED_COLOR || cb == RED_COLOR);
     *fx =  cond ? -1e-6 : 1e-6;
-    printf("%d %d    %g %g %g :snow:\n", ca, cb, x, y, z);
+    if (z > -1 && z < 1)
+        printf("%d %d    %g %g %g :snow:\n", ca, cb, x, y, z);
 }
 
 } /* namespace */
