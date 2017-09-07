bcce783c3e96f80627d31c4309802c396097687f
Athena Economides <eceva@ethz.ch>
2017-09-07 12:52:43 +0200

    bug? rbc mesh

---
+++ bugs/rbc_mesh/git_branch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# copied from: https://gist.github.com/dciccale/5560837
+
+
+# checks if branch has something pending
+function parse_git_dirty() {
+  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
+}
+
+# gets the current git branch
+function parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+# get last commit hash prepended with @ (i.e. @8a323d0)
+function parse_git_hash() {
+  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
+}
+
+# DEMO
+GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+echo ${GIT_BRANCH}
---
+++ bugs/rbc_mesh/launch_daint.sh
@@ -0,0 +1,57 @@
+# !/bin/bash
+#
+# Script for launcing uDeviceX on daint:$SCRATCH
+# run as: ./launch_daint.sh
+#
+# Explanation of script variables:
+# --------------------------------
+# GITROOT:      ??/uDeviceX
+# DEPLOYDIR:    ??/uDeviceX/run/couette
+# COMPILEDIR:   $SCRATCH/tmp.XXXXXXXXX
+# RNAME:        couette_R4_HA0.1_DT0.0001_02Aug2017
+# RUNDIR:       $SCRATCH/UDEVICEX/RNAME
+
+RNAME=GS_solid_rbcs
+
+mkdir -p $SCRATCH/UDEVICEX
+
+GITROOT=`git rev-parse --show-toplevel`
+DEPLOYDIR=`pwd`
+COMPILEDIR=`mktemp -d $SCRATCH/tmp.XXXXXXXXX`
+RNAME=`awk -v n=${RNAME} 'BEGIN {if(length(n) > 0) print n; else print "test_GS"}'`
+RUNDIR=$SCRATCH/UDEVICEX/${RNAME}_`date | awk '{print $2$3"_"$4}'`
+GIT_COMMIT=`./git_branch.sh`
+
+# Copy+compile in $SCRATCH
+cp -r ${GITROOT}/* ${COMPILEDIR}/
+mkdir -p ${COMPILEDIR}/build
+cp ./* ${COMPILEDIR}/build
+
+cd ${COMPILEDIR}/build
+    # Compile utilities
+    (cd ../cmd; make ;                   > /dev/null)
+
+    # Edit configuration script
+    u="u/x"
+    s=../src
+    u.conf $s $u conf.base.h <<EOF
+    run
+EOF
+
+    # Compile
+    { make clean && u.make -j ; } > /dev/null
+
+    # Copy to $RUNDIR
+    mkdir -p ${RUNDIR}
+    cp ${COMPILEDIR}/build/udx ${RUNDIR}/
+    cp ${COMPILEDIR}/build/launch_daint.sh ${RUNDIR}/
+    cp ${COMPILEDIR}/build/run_daint.sh ${RUNDIR}/
+    cp ${COMPILEDIR}/build/conf.h ${RUNDIR}/
+
+cd $RUNDIR
+    . run_daint.sh
+
+cd ${DEPLOYDIR}
+    echo ${GIT_COMMIT} > $RUNDIR/git_commit.txt
+
+rm -rf ${COMPILEDIR}
