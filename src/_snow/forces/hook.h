292c2073f45e1b9a873ce27d5d81d2a887601592
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-07 10:21:11 +0200

    silent hook

--- src/_snow/forces/hook.h
+++ src/_snow/forces/hook.h
@@ -2,8 +2,8 @@ namespace forces {
 static __device__ void hook(int ca, int cb, float x, float y, float z, float *fx, float *fy, float *fz) {
     bool cond;
     cond = (ca == RED_COLOR || cb == RED_COLOR);
-    *fx =  cond ? 1 : 0;
-    printf("snow: %d %d    %g %g %g\n", ca, cb, x, y, z);
+    *fx =  cond ? -1e-6 : 1e-6;
+    //printf("snow: %d %d    %g %g %g\n", ca, cb, x, y, z);
 }
 
 } /* namespace */
