8a8c4484042ac8e14fb7170b477599102db146f1
Sergey Litvinov <slitvinov@gmail.com>
2017-09-07 21:05:20 +0200

    split tex2rv; call cloud_get with i / 2

--- src/cloud/lforces/get.h
+++ src/cloud/lforces/get.h
@@ -1,14 +1,17 @@
+static __device__ void tex2rv(uint i, /**/ float r[3], float v[3]) {
+    float4 r4, v4;
+    r4 = fetchF4(i);
+    v4 = fetchF4(xadd(i, 1u));
+    f4tof3(r4, /**/ r); f4tof3(v4, /**/ v);
+}
+
 static __device__ void cloud_get(uint i, /**/ forces::Pa *p) { /* local fetch */
     /* i: particle index */
-    float4 r, v;
-    float r0[3], v0[3];
-
-    r = fetchF4(i);
-    v = fetchF4(xadd(i, 1u));
-    f4tof3(r, r0); f4tof3(v, v0);
-    forces::rvk2p(r0, v0, SOLVENT_KIND, /**/ p);
-
-    if (multi_solvent) p->color = fetchC(i);
+    float r[3], v[3];
+    tex2rv(i, /**/ r, v);
+    forces::rvk2p(r, v, SOLVENT_KIND, /**/ p);
+    if (multi_solvent)
+        p->color = fetchC(i / 2);
 }
 
 static __device__ void cloud_pos(/* dummy c */ int i, /**/ float *x, float *y, float *z) {
