fd1a91bce1ffb67daf2b0c23586789d06f45f4ef
Sergey Litvinov <slitvinov@gmail.com>
2017-09-08 17:16:18 +0200

    wall in imp.h

--- src/wall/imp.h
+++ src/wall/imp.h
@@ -1,3 +1,5 @@
+namespace wall {
+
 typedef const sdf::tex3Dca<float> TexSDF_t;
 
 void build_cells(const int n, float4 *pp4, clist::Clist *cells);
@@ -7,14 +9,16 @@ void strt_quants(int *w_n, float4 **w_pp);
 
 void gen_ticket(const int w_n, float4 *w_pp, clist::Clist *cells, Texo<int> *texstart, Texo<float4> *texpp);
 
-namespace wall { namespace grey {
+namespace grey {
 void pair(TexSDF_t texsdf, const int type, hforces::Cloud cloud, const int n, const Texo<int> texstart,
                   const Texo<float4> texpp, const int w_n, /**/ rnd::KISS *rnd, Force *ff);
-}}
+}
 
-namespace wall { namespace color {
+namespace color {
 void pair(TexSDF_t texsdf, const int type, hforces::Cloud cloud, const int n, const Texo<int> texstart,
                   const Texo<float4> texpp, const int w_n, /**/ rnd::KISS *rnd, Force *ff);
-}}
+}
 
 void strt_dump_templ(const int n, const float4 *pp);
+
+} /* namespace wall */
