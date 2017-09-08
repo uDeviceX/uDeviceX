0670dcd99d778221d317e8ca9e2151012050c466
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-08 16:21:33 +0200

    inirial import ini

--- src/comm/imp/ini.h
+++ src/comm/imp/ini.h
@@ -1,4 +1,23 @@
-void ini(size_t bsize, int cap[NBAGS], /**/ Bags *b) {
+
+static void estimates(int nfrags, float maxd, /**/ int *counts) {
+    int i, e;
+    for (i = 0; i < nfrags; ++i) {
+        e = frag_ncell(i);
+        e = (int) (e * maxd);
+        counts[i] = e;
+    }
+}
+
+static void alloc_one_frag(int i, /**/ Bags *b) {
+    size_t n = b->bsize * b->counts[i];
+    CC(d::alloc_pinned(&b->hst[i], n));
+    CC(d::HostGetDevicePointer(&b->dev[i], b->hst[i], 0));
+}
+
+void ini_full(size_t bsize, int cap[NBAGS], float maxdensity, /**/ Bags *b) {
+    b->bsize = bsize;
+    estimates(NFRAGS, maxdensity, b->counts);
+
     
 }
 
