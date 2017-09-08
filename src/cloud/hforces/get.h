88f35ddde49e150dc2c3793d9d492eb22cbf60f0
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-08 17:35:50 +0200

    split common part

--- src/cloud/hforces/get.h
+++ src/cloud/hforces/get.h
@@ -1,11 +1,14 @@
 namespace hforces { namespace dev {
-inline __device__ void cloud_get(Cloud c, int i, /**/ forces::Pa *p) {
+inline __device__ void cloud_get_p(Cloud c, int i, /**/ forces::Pa *p) {
     float *pp, *r, *v;
     pp = c.pp;
     r = &pp[6*i];
     v = &pp[6*i + 3];
     forces::rvk2p(r, v, SOLVENT_KIND, /**/ p);
+}
 
+inline __device__ void cloud_get(Cloud c, int i, /**/ forces::Pa *p) {
+    cloud_get_p(c, i, /**/ p);
     if (multi_solvent) p->color = c.cc[i];
 }
 }} /* namespace */
