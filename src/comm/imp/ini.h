c69418bbad1376711138be3b4395308c387f35a7
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-08 15:50:00 +0200

    move comm impleementation to comm/imp

---
+++ src/comm/imp/ini.h
@@ -0,0 +1,7 @@
+void ini(size_t bsize, int cap[NBAGS], /**/ Bags *b) {
+    
+}
+
+void ini(MPI_Comm comm, /**/ Stamp *s) {
+
+}
---
+++ src/comm/imp/main.h
@@ -0,0 +1,18 @@
+void post_recv(Bags *b, Stamp *s) {
+    for (int i = 0; i < NFRAGS; ++i) {
+        size_t c = b->counts[i] * b->bsize; // TODO: capacity
+        MC(m::Irecv(b->hst[i], c, MPI_BYTE, s->ranks[i], s->bt + i, s->cart, s->req + i));
+    }
+}
+
+void post_send(Bags *b, Stamp *s) {
+    for (int i = 0; i < NFRAGS; ++i) {
+        size_t n = b->counts[i] * b->bsize;
+        MC(m::Isend(b->hst[i], n, MPI_BYTE, s->aranks[i], s->bt + i, s->cart, s->req + i));
+    }
+}
+
+void wait_all(Stamp *s) {
+    MPI_Status statuses[NFRAGS];
+    MC(m::Waitall(NFRAGS, s->req, statuses));
+}
