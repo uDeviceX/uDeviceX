a06035f601d6bcfdc26d8f2ff23926c7611d89f5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-08 17:37:37 +0200

    common in cloud

--- src/cloud/hforces/get.h
+++ src/cloud/hforces/get.h
@@ -1,5 +1,5 @@
 namespace hforces { namespace dev {
-inline __device__ void cloud_get_p(Cloud c, int i, /**/ forces::Pa *p) {
+inline __device__ void common(Cloud c, int i, /**/ forces::Pa *p) {
     float *pp, *r, *v;
     pp = c.pp;
     r = &pp[6*i];
@@ -7,8 +7,12 @@ inline __device__ void cloud_get_p(Cloud c, int i, /**/ forces::Pa *p) {
     forces::rvk2p(r, v, SOLVENT_KIND, /**/ p);
 }
 
+inline __device__ void cloud_get_p(Cloud c, int i, /**/ forces::Pa *p) {
+    common(c, i, /**/ p);
+}
+
 inline __device__ void cloud_get(Cloud c, int i, /**/ forces::Pa *p) {
-    cloud_get_p(c, i, /**/ p);
+    common(c, i, /**/ p);
     if (multi_solvent) p->color = c.cc[i];
 }
 }} /* namespace */
