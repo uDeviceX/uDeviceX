7bc429bfe6b9556cf645f07af0972986dc3c2c09
Lucas Amoudruz <amlucas@ethz.ch>
2017-09-11 17:39:27 +0200

    longer tend

--- run/periodic_rbc/conf.base.h
+++ run/periodic_rbc/conf.base.h
@@ -37,7 +37,7 @@
 #define fsiforces               true
 #define walls                   false
 #define wall_creation           1000
-#define tend                    1
+#define tend                    100
 
 /* FLOW TYPE */
 #define pushflow                true
