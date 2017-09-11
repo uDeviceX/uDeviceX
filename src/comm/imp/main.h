4d11b25cfca42dea5693ecf75dcda61c6a0ed78b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-11 14:22:00 +0200

    add recv_counts (in bytes)

--- src/comm/imp/main.h
+++ src/comm/imp/main.h
@@ -1,14 +1,26 @@
 void post_recv(Bags *b, Stamp *s) {
     for (int i = 0; i < NFRAGS; ++i) {
         size_t c = b->capacity[i] * b->bsize;
-        MC(m::Irecv(b->hst[i], c, MPI_BYTE, s->rnks[i], s->bt + i, s->cart, s->req + i));
+        MC(m::Irecv(b->hst[i], c, MPI_BYTE, s->anks[i], s->bt + i, s->cart, s->req + i));
     }
 }
 
 void post_send(Bags *b, Stamp *s) {
     for (int i = 0; i < NFRAGS; ++i) {
         size_t n = b->counts[i] * b->bsize;
-        MC(m::Isend(b->hst[i], n, MPI_BYTE, s->anks[i], s->bt + i, s->cart, s->req + i));
+        MC(m::Isend(b->hst[i], n, MPI_BYTE, s->rnks[i], s->bt + i, s->cart, s->req + i));
+    }
+}
+
+static void recv_bytes_counts(const Stamp *s, /**/ int *counts) {
+    int i, tag, src;
+    MPI_Status stat;
+    
+    for (i = 0; i < NFRAGS; ++i) {
+        tag = s->bt + i;
+        src = s->anks[i];
+        MC(m::Probe(src, tag, s->cart, /**/ &stat));
+        MC(m::Get_count(&stat, MPI_BYTE, counts + i));
     }
 }
 
