0d81e3d67a39b2526613c3297f0cefd49a2090d7
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-12 18:31:28 +0200

    testing send and recv

--- src/u/comm/main.cu
+++ src/u/comm/main.cu
@@ -9,6 +9,19 @@
 
 #include "comm/imp.h"
 
+void fill_bag(int val, int sz, int *ii) {
+    for (int i = 0; i < sz; ++i) ii[i] = val;
+}
+
+void fill_bags(comm::Bags *b) {
+    int c, i;
+    for (i = 0; i < 26; ++i) {
+        c = b->capacity[i] / 2;
+        fill_bag(i, c, (int*) b->hst[i]);
+        b->counts[i] = c;
+    }
+}
+
 int main(int argc, char **argv) {
     m::ini(argc, argv);
     MSG("mpi size: %d", m::size);
@@ -22,7 +35,16 @@ int main(int argc, char **argv) {
     ini_no_bulk(sizeof(int), 1, /**/ &sendB);
     ini_no_bulk(sizeof(int), 1, /**/ &recvB);
     ini(m::cart, /*io*/ &tg, /**/ &stamp);
-    
+
+    fill_bags(&sendB);
+
+    post_recv(&recvB, &stamp);
+    post_send(&sendB, &stamp);
+
+    recv_counts(&stamp, /**/ &recvB);
+
+    wait_recv(&stamp);
+    wait_send(&stamp);
     
     fin(&sendB);
     fin(&recvB);
