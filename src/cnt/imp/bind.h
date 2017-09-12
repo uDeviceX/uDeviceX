d10c83b8e6df3a10775a08f5ed9414de91ba37de
Sergey Litvinov <slitvinov@gmail.com>
2017-09-12 18:58:51 +0200

    decl up

--- src/cnt/imp/bind.h
+++ src/cnt/imp/bind.h
@@ -1,22 +1,22 @@
 static void bind(const int *const starts, const int *const cellentries,
           const int nc, std::vector<ParticlesWrap> wr) {
-    size_t textureoffset = 0;
-
-    if (nc)
-        CC(cudaBindTexture(&textureoffset, &dev::t::id, cellentries,
-                           &dev::t::id.channelDesc,
-                           sizeof(int) * nc));
-    int ncells = XS * YS * ZS;
-    CC(cudaBindTexture(&textureoffset, &dev::t::start, starts,
-                       &dev::t::start.channelDesc, sizeof(int) * ncells));
-    int n = wr.size();
-
-    assert(n <= MAX_OBJ_TYPES);
+    size_t textureoffset;
+    itn ncells, n, i;
     int ns[MAX_OBJ_TYPES];
     float2 *ps[MAX_OBJ_TYPES];
     float *fs[MAX_OBJ_TYPES];
 
-    for (int i = 0; i < n; ++i) {
+    textureoffset = 0;
+    if (nc)
+        CC(cudaBindTexture(&textureoffset, &dev::t::id, cellentries,
+                           &dev::t::id.channelDesc,
+                           sizeof(int) * nc));
+    ncells = XS * YS * ZS;
+    CC(cudaBindTexture(&textureoffset, &dev::t::start, starts,
+                       &dev::t::start.channelDesc, sizeof(int) * ncells));
+    n = wr.size();
+    assert(n <= MAX_OBJ_TYPES);
+    for (i = 0; i < n; ++i) {
         ns[i] = wr[i].n;
         ps[i] = (float2*)wr[i].p;
         fs[i] = (float*)wr[i].f;
