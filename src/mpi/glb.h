022aae2de6dab5e94d051b3f54aaab976df2c3a3
Sergey Litvinov <slitvinov@gmail.com>
2017-09-12 19:14:40 +0200

    add x2g to mpi/glb.h

--- src/mpi/glb.h
+++ src/mpi/glb.h
@@ -1,6 +1,10 @@
 namespace m { /* mini MPI */
 extern int rank, size, coords[], dims[];
 int lx(); int ly(); int lz(); /* domain size */
+
+/* local to global */
+float x2g(float); float y2g(float); float z2g(float);
+
 void ini(int argc, char **argv);
 void fin();
 }
