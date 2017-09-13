8fbc3735f62e9884ee1ad3e37ae2b2622feeb190
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-13 11:33:27 +0200

    include missing files

--- src/distr/flu/imp.cu
+++ src/distr/flu/imp.cu
@@ -1,9 +1,21 @@
+#include <mpi.h>
 #include <conf.h>
+#include "inc/conf.h"
 
+#include "utils/cc.h"
+#include "utils/kl.h"
+#include "d/api.h"
 #include "inc/type.h"
 #include "inc/dev/common.h"
+#include "inc/dev.h"
 #include "frag.h"
+
+
+#include "mpi/basetags.h"
+#include "comm/imp.h"
+
 
 #include "map.h"
 #include "dev.h"
-
+#include "imp/ini.h"
+#include "imp/pack.h"
--- src/distr/flu/imp/pack.h
+++ src/distr/flu/imp/pack.h
@@ -1,13 +1,13 @@
-static void reini_map(Map *m) {
-    CC(d::MemsetAsync(m->counts, 0, 26 * sizeof(int)));
+static void reini_map(Map m) {
+    CC(d::MemsetAsync(m.counts, 0, 26 * sizeof(int)));
 }
 
-void build_map(int n, const Particle *pp, Map *m) {
+void build_map(int n, const Particle *pp, Map m) {
     reini_map(/**/ m);
     KL(dev::build_map, (k_cnf(n)), (pp, n, /**/ m));
     KL(dev::scan_map, (1, 32), (/**/ m));    
 }
 
-void pack(const Map *m, Particle *pp, /**/ dBags *bags) {
+void pack(const Map *m, Particle *pp, /**/ comm::dBags *bags) {
     
 }
