a57e4d93c98de44cdd6f6b3b55caae807524477a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-13 17:55:27 +0200

    add empty imp/ files

---
+++ src/clistx/imp/main.h
@@ -0,0 +1,9 @@
+
+void build(const Particle *pplo, /**/ Particle *pp, Clist *c, /*w*/ Work *w) {
+    
+}
+
+
+void build(const Particle *pplo, const Particle *ppre, /**/ Particle *pp, Clist *c, /*w*/ Work *w) {
+
+}
