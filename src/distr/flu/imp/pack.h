c04c551c4eabdf357c0a4fb5787f0dec3b931b34
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-13 12:37:57 +0200

    add pack in imp

--- src/distr/flu/imp/pack.h
+++ src/distr/flu/imp/pack.h
@@ -8,6 +8,19 @@ void build_map(int n, const Particle *pp, Map m) {
     KL(dev::scan_map, (1, 32), (/**/ m));    
 }
 
-void pack(const Map *m, Particle *pp, /**/ comm::dBags *bags) {
-    
+typedef Sarray <float2*, 26> float2p26;
+
+template <typename T>
+static void bag2Sarray(dBags bags, Sarray<T*, 26> *buf) {
+    for (int i = 0; i < 26; ++i)
+        buf->d[i] = (T*) bags.data[i];
+}
+
+void pack(const Map m, const Particle *pp, int n, /**/ dBags bags) {
+
+    const int S = sizeof(Particle) / sizeof(float2);
+    float2p26 wrap;
+    bag2Sarray(bags, &wrap);
+
+    KL((dev::pack<float2, S>), (k_cnf(S*n)), ((const float2*)pp, m, /**/ wrap));
 }
