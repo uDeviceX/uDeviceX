00f7a2e8a4d9679231ddd39d855ce82306774aad
Sergey Litvinov <slitvinov@gmail.com>
2017-09-13 11:08:10 +0200

    add Clist0: vanial cell list structure

--- src/clist/imp.h
+++ src/clist/imp.h
@@ -1,4 +1,10 @@
 namespace clist {
+struct Clist0 {
+    int LX, LY, LZ;
+    int ncells;
+    int *start, *count;
+};
+
 class Clist {
     int LX, LY, LZ;
     void buildn(Particle *const pp, const int n);
