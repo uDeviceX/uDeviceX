734d7fd40e4226103099c7048ada4c3239b1011a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-14 14:08:12 +0200

    adding interface for distr/flu

--- src/distr/flu/int.h
+++ src/distr/flu/int.h
@@ -1,6 +1,30 @@
 namespace distr {
 namespace flu {
 
+using namespace comm;
+
+struct Pack {
+    Map map;
+    dBags *dpp, *dii, *dcc;
+    hBags *hpp, *hii, *hcc;
+    int nbulk;
+};
+
+struct Comm {
+    Stamp *pp, *ii, *cc;
+};
+
+struct Unpack {
+    hBags *hpp, *hii, *hcc;
+    int *iire, *ccre;
+    Particle *ppre;
+};
+
+void build_map(int n, const Particle *pp, Pack *p);
+
+void pack_pp(const Particle *pp, int n, /**/ Pack *p);
+void pack_ii(const int *ii, int n, /**/ Pack *p);
+void pack_cc(const int *ii, int n, /**/ Pack *p);
 
 } // flu
 } // distr
