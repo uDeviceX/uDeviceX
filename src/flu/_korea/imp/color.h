17d416d3e0bc09b42167366b84048e159ec3a03a
Sergey Litvinov <slitvinov@gmail.com>
2017-09-14 14:30:14 +0200

    add _korea

---
+++ src/flu/_korea/imp/color.h
@@ -0,0 +1,23 @@
+static int min (int x, int y)        { return x < y ? x : y; };
+static int min3(int x, int y, int z) { return min(x, min(y, z)); }
+static void color(Particle *pp, int n, /**/ int *cc) {
+    enum {X, Y, Z};
+    int lx, ly, lz; /* domain */
+    int i, r, g;
+    bool inside;
+    float x, y, z, rad;
+    Particle p;
+    lx = m::lx(); ly = m::ly(); lz = m::lz();
+
+    enum {R = RED_COLOR, G = BLUE_COLOR};
+    rad = 0.25*min3(lx, ly, lz);
+    for (i = r = g = 0; i < n; i++) {
+        p = pp[i];
+        x = p.r[X]; y = p.r[Y]; z = p.r[Z];
+        inside = x*x + y*y + z*z < rad*rad;
+        if (inside) {cc[i] = R; r++;}
+        else        {cc[i] = G; g++;}
+    }
+    MSG("color scheme: Bangladesh");
+    MSG("red/green : %d/%d", r, g);
+}
