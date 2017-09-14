053bf32384d450c9c42c15f2254b22c7341000b2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-14 14:48:43 +0200

    deps

--- src/u/france/make/dir.mk
+++ src/u/france/make/dir.mk
@@ -2,7 +2,6 @@ D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
     d $B/algo       && \
     d $B/algo/scan  && \
     d $B/clist      && \
-    d $B/clistx     && \
     d $B/cnt        && \
     d $B/comm       && \
     d $B/d          && \
