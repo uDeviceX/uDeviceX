90d54c209060ad0539bc2b6629ceea704a0a43f4
Sergey Litvinov <slitvinov@gmail.com>
2017-09-15 15:03:34 +0200

    minor

--- src/scheme/dev/force/4roller.h
+++ src/scheme/dev/force/4roller.h
@@ -1,1 +1,6 @@
-__global__ void force(float, Particle*, Force*, int, float) { }
+__global__ void force(float mass, Particle *pp, Force *ff, int, float) {
+    enum {X, Y, Z};
+    int pid = threadIdx.x + blockDim.x * blockIdx.x;
+    if (pid >= n) return;
+
+}
