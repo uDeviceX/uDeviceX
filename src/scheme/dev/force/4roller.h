522b92ffa77400001cc98b1aa6a2c72cf3b02a5e
Sergey Litvinov <slitvinov@gmail.com>
2017-09-15 15:14:17 +0200

    4roller for one process

--- src/scheme/dev/force/4roller.h
+++ src/scheme/dev/force/4roller.h
@@ -1,6 +1,25 @@
-__global__ void force(float mass, Particle *pp, Force *ff, int, float) {
-    enum {X, Y, Z};
-    int pid = threadIdx.x + blockDim.x * blockIdx.x;
+__global__ void force(float mass, Particle *pp, Force *ff, int n, float driving_force0) {
+    enum {X, Y};
+    int pid;
+    float fx, fy, *f;
+    float x, y, lx, ly, *r;
+    const float PI = 3.141592653589793;
+
+    pid = threadIdx.x + blockDim.x * blockIdx.x;
     if (pid >= n) return;
 
+    r = pp[pid].r;
+    f = ff[pid].f;
+
+    lx = XS; ly = YS;
+    x = r[X] + 0.5*XS; y = r[Y] + 0.5*YS;
+    x /=   lx; y /=   ly;
+    x *= 2*PI; y *= 2*PI;
+
+    fx =  2*sin(x)*cos(y);
+    fy = -2*cos(x)*sin(y);
+    fx *= driving_force0; fy *= driving_force0;
+
+    f[X] += mass * fx;
+    f[Y] += mass * fy;
 }
