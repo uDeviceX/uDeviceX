d6571e31a28b41512194346eb1a47b759cd54228
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-15 11:54:02 +0200

    safer check

--- src/dbg/dev/clist.h
+++ src/dbg/dev/clist.h
@@ -8,8 +8,8 @@ static __device__ int3 cell_coords(int3 L, int cid) {
 
 static __device__ bool at_coord(float x, int c, int L, bool verbose, const char *dir) {
     x += L/2;
-    if ((x < c) || (x >= c + 1)) {
-        if (verbose) printf("Particle out of its cell : %s = %g, c = %d, L = %d\n", dir, x, c, L);
+    if ((x < c) || (x > c + 1)) {
+        if (verbose) printf("Particle out of its cell : %s = %f, c = %d, L = %d\n", dir, x, c, L);
         return false;
     }
     return true;
