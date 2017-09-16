c50339d78d687d8153fb206d4bd1e7963d0a0bf7
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-16 18:03:38 +0200

    include dev in rbc imp

--- src/distr/rbc/imp.cu
+++ src/distr/rbc/imp.cu
@@ -4,9 +4,12 @@
 #include <conf.h>
 #include "inc/conf.h"
 
+#include "frag/imp.h"
+
 #include "mpi/basetags.h"
 #include "comm/imp.h"
 
+#include "distr/map/type.h"
 #include "type.h"
 #include "imp.h"
 
@@ -14,6 +17,8 @@ namespace distr {
 namespace rbc {
 using namespace comm;
 
+#include "distr/map/dev.h"
+#include "dev.h"
 
 } // rbc
 } // distr
