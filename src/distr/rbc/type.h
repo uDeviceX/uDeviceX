098c67ea64efb0771a48fe1a4528665dc4a9fa17
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-16 18:04:22 +0200

    rm rbc::Map

--- src/distr/rbc/type.h
+++ src/distr/rbc/type.h
@@ -3,12 +3,6 @@ namespace rbc {
 
 using namespace comm;
 
-struct Map {
-    int     *counts; /* number of cells leaving in each fragment */
-    int     *starts; /* cumulative sum of the above              */
-    int *ids[NBAGS]; /* indices of leaving cells                 */
-};
-
 struct Pack {
     Map map;
     dBags dpp;
