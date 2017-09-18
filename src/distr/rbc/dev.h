f7d009d3d55022a9b795420cb76e011c0a888765
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-18 10:34:59 +0200

    add shift on device

--- src/distr/rbc/dev.h
+++ src/distr/rbc/dev.h
@@ -36,4 +36,28 @@ __global__ void pack_pp(int nv, const Particle *pp, Map m, /**/ Sarray<Particle*
     buf.d[fid][dst] = pp[src];
 }
 
+
+static __device__ void fid2shift(int id, /**/ int s[3]) {
+    enum {X, Y, Z};
+    s[X] = XS * frag_i2d(id, X);
+    s[Y] = YS * frag_i2d(id, Y);
+    s[Z] = ZS * frag_i2d(id, Z);
+}
+
+static  __device__ void shift_1p(const int s[3], /**/ Particle *p) {
+    enum {X, Y, Z};
+    p->r[X] += s[X];
+    p->r[Y] += s[Y];
+    p->r[Z] += s[Z];
+}
+
+__global__ void shift(int n, const int fid, /**/ Particle *pp) {
+    int i, s[3];
+    i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i >= n) return;
+    
+    fid2shift(fid, /**/ s);
+    shift_1p(s, /**/ pp + i);
+}
+
 } //dev
