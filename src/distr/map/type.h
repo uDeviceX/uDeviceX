278ed131a84d17b1f39929ca7d5ba3623b3ffc91
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-18 11:13:14 +0200

    more entries in map

--- src/distr/map/type.h
+++ src/distr/map/type.h
@@ -3,7 +3,7 @@ namespace distr {
 struct Map {
     int *counts;  /* number of entities leaving in each fragment */
     int *starts;  /* cumulative sum of the above                 */
-    int *ids[26]; /* indices of leaving objects                  */
+    int *ids[27]; /* indices of leaving objects                  */
 };
 
 } // distr
