c1f3d58361ca71be7242a8a677b556126a0ca5fc
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-18 10:37:15 +0200

    shift rbcs in unpack

--- src/distr/rbc/imp/unpack.h
+++ src/distr/rbc/imp/unpack.h
@@ -20,7 +20,10 @@ void unpack_halo(const Unpack *u, /**/ rbc::Quants *q) {
         nc = u->hpp.counts[i];
         n  = nc * nv; 
         size = n * sizeof(Particle);
-        if (nc) CC(d::MemcpyAsync(q->pp + s, u->hpp.data[i], size, H2D));
+        if (nc) {
+            CC(d::MemcpyAsync(q->pp + s, u->hpp.data[i], size, H2D));
+            KL(dev::shift, (k_cnf(n)), (n, i, /**/ q->pp + s));
+        }
         s += n;
         nctot += nc;
     }
