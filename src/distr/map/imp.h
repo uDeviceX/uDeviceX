a3135e0294cb00ed980989e6f33fdedfd46701f5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-09-18 10:55:59 +0200

    alloc more counts in map

--- src/distr/map/imp.h
+++ src/distr/map/imp.h
@@ -1,14 +1,19 @@
 void ini_map(const int capacity[NBAGS], /**/ Map *m) {
-    CC(d::Malloc((void**) &m->counts,  NFRAGS      * sizeof(int)));
-    CC(d::Malloc((void**) &m->starts, (NFRAGS + 1) * sizeof(int)));
+    CC(d::Malloc((void**) &m->counts,  NBAGS      * sizeof(int)));
+    CC(d::Malloc((void**) &m->starts, (NBAGS + 1) * sizeof(int)));
 
-    for (int i = 0; i < NFRAGS; ++i)
-        CC(d::Malloc((void**) &m->ids[i], capacity[i] * sizeof(int)));
+    int i, c;
+    for (i = 0; i < NBAGS; ++i) {
+        c = capacity[i];
+        if (c) CC(d::Malloc((void**) &m->ids[i], c * sizeof(int)));
+        else m->ids[i] = NULL;
+    }
 }
 
 void fin_map(/**/ Map *m) {
     CC(d::Free(m->counts));
     CC(d::Free(m->starts));    
-    for (int i = 0; i < NFRAGS; ++i)
-        CC(d::Free(m->ids[i]));
+    for (int i = 0; i < NBAGS; ++i)
+        if (m->ids[i])
+            CC(d::Free(m->ids[i]));
 }
