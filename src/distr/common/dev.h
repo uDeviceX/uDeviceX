7c6d27c24bf2d0b6bee98de8c643c2421a5f6420
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-19 14:31:22 +0200

    add comon shift to common

---
+++ src/distr/common/dev.h
@@ -0,0 +1,37 @@
+namespace dev {
+
+static __device__ void fid2shift(int id, /**/ int s[3]) {
+    enum {X, Y, Z};
+    s[X] = XS * frag_i2d(id, X);
+    s[Y] = YS * frag_i2d(id, Y);
+    s[Z] = ZS * frag_i2d(id, Z);
+}
+
+static  __device__ void shift_1p(const int s[3], /**/ Particle *p) {
+    enum {X, Y, Z};
+    p->r[X] += s[X];
+    p->r[Y] += s[Y];
+    p->r[Z] += s[Z];
+}
+
+__global__ void shift_one_frag(int n, const int fid, /**/ Particle *pp) {
+    int i, s[3];
+    i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i >= n) return;
+    
+    fid2shift(fid, /**/ s);
+    shift_1p(s, /**/ pp + i);
+}
+
+__global__ void shift_halo(const Sarray<int, 27> starts, /**/ Particle *pp) {
+    int pid, fid, s[3];
+
+    pid = threadIdx.x + blockDim.x * blockIdx.x;
+    if (pid >= starts.d[26]) return;
+    fid = k_common::fid(starts.d, pid);
+    
+    fid2shift(fid, s);
+    shift_1p(s, /**/ pp + pid);
+}
+
+} // dev
