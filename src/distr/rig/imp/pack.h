553436662134d34ec38eaffc2f8b0b12aa3e37b8
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-19 14:50:33 +0200

    pack ss

--- src/distr/rig/imp/pack.h
+++ src/distr/rig/imp/pack.h
@@ -15,8 +15,18 @@ static void pack_pp(const Map m, int ns, int nv, const Particle *ipp, /**/ dBags
     KL((dev::pack_pp_packets), (blck, thrd), (nv, ipp, m, /**/ wrap));
 }
 
-void pack_pp(int ns, int nv, const Particle *ipp, /**/ Pack *p) {
+static void pack_ss(const Map m, int n, const Solid *ss, /**/ dBags bags) {
+
+    Sarray<Solid*, 27> wrap;
+    bag2Sarray(bags, &wrap);
+
+    KL((dev::pack_ss), (k_cnf(n)), (ss, m, /**/ wrap));
+}
+
+
+void pack(int ns, int nv, const Solid *ss, const Particle *ipp, /**/ Pack *p) {
     pack_pp(p->map, ns, nv, ipp, /**/ p->dipp);
+    pack_ss(p->map, ns, ss, /**/ p->dss);
 }
 
 void download(Pack *p) {
