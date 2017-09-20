eb5eb6cfe0ab3e18cc53b749224d816057586689
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-20 17:52:00 +0200

    compile fixes

--- src/exch/obj/dev.h
+++ src/exch/obj/dev.h
@@ -1,12 +1,10 @@
 namespace dev {
 
 enum { MAX_DSTS = 7 };
-enum {BULK, FACE, EDGE, CORN};
-enum {X, Y, Z};
 
 __device__ int map_code(int3 L, const float r[3]) {
     int x, y, z;
-    
+    enum {X, Y, Z};
     x = -1 + (r[X] >= -L.x / 2) + (r[X] >= L.x / 2);
     y = -1 + (r[Y] >= -L.y / 2) + (r[Y] >= L.y / 2);
     z = -1 + (r[Z] >= -L.z / 2) + (r[Z] >= L.z / 2);
@@ -15,7 +13,7 @@ __device__ int map_code(int3 L, const float r[3]) {
 }
 
 __device__ int add_faces(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
-    for (c = 0; c < 3; ++c) {
+    for (int c = 0; c < 3; ++c) {
         if (d[c]) {
             int df[3] = {0, 0, 0}; df[c] = d[c];
             fids[j++] = frag_d32i(df);
@@ -25,6 +23,7 @@ __device__ int add_faces(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
 }
 
 __device__ int add_edges(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
+    enum {X, Y, Z};
     for (int c = 0; c < 3; ++c) {
         int de[3] = {d[X], d[Y], d[Z]}; de[c] = 0;
         if (de[(c + 1) % 3] && de[(c + 2) % 3])
@@ -34,6 +33,7 @@ __device__ int add_edges(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
 }
 
 __device__ int add_cornr(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
+    enum {X, Y, Z};
     if (d[X] && d[Y] && d[Z])
         fids[j++] = frag_d32i(d);
     return j;
@@ -42,9 +42,9 @@ __device__ int add_cornr(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
 __device__ int map_decode(int code, /**/ int fids[MAX_DSTS]) {
     int j = 0;
     const int d[3] = frag_i2d3(code);
-    j = add_faces(j, d, /**/ dsts);
-    j = add_edges(j, d, /**/ dsts);
-    j = add_cornr(j, d, /**/ dsts);
+    j = add_faces(j, d, /**/ fids);
+    j = add_edges(j, d, /**/ fids);
+    j = add_cornr(j, d, /**/ fids);
     return j;
 }
 
