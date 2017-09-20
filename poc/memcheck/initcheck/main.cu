96a6beb2c0502cc1d6b4974eadff1008086d0071
Sergey Litvinov <slitvinov@gmail.com>
2017-09-20 11:36:38 +0200

    upd initcheck/main.cu

--- poc/memcheck/initcheck/main.cu
+++ poc/memcheck/initcheck/main.cu
@@ -1,46 +1,25 @@
 #include <stdio.h>
 
-__device__ int x;
+#define N 10
+int *a;
 
-__global__ void unaligned_kernel(void) {
-    *(int*) ((char*)&x + 1) = 42;
+__global__ void uninit(int *a) {
+    print
 }
 
-__device__ void out_of_bounds_function(void) {
-    *(int*) 0x87654320 = 42;
-}
 
-__global__ void out_of_bounds_kernel(void) {
-    out_of_bounds_function();
-}
-
-void run_unaligned(void) {
-    printf("Running unaligned_kernel\n");
-    unaligned_kernel<<<1,1>>>();
-    printf("Ran unaligned_kernel: %s\n",
-    cudaGetErrorString(cudaGetLastError()));
-    printf("Sync: %s\n", cudaGetErrorString(cudaThreadSynchronize()));
-}
-
-void run_out_of_bounds(void) {
-    printf("Running out_of_bounds_kernel\n");
-    out_of_bounds_kernel<<<1,1>>>();
-    printf("Ran out_of_bounds_kernel: %s\n",
-    cudaGetErrorString(cudaGetLastError()));
+void run_uninit() {
+    uninit<<<1,1>>>(a);
+    cudaGetErrorString(cudaGetLastError());
     printf("Sync: %s\n", cudaGetErrorString(cudaThreadSynchronize()));
 }
 
 int main() {
-    int *devMem;
+    cudaMalloc(&a, N*sizeof(a[0]));
 
-    printf("Mallocing memory\n");
-    cudaMalloc((void**)&devMem, 1024);
-
-    run_unaligned();
-    run_out_of_bounds();
+    run_uninit();
 
     cudaDeviceReset();
-    cudaFree(devMem);
-
+    cudaFree(a);
     return 0;
 }
