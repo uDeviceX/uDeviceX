1ec154af0d8da034be208c6baaa4d4735bf1a401
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-21 17:08:01 +0200

    rm unused typedefs

--- src/inc/tmp/wrap.h
+++ src/inc/tmp/wrap.h
@@ -6,7 +6,3 @@ struct ParticlesWrap {
     ParticlesWrap(const Particle *const p, const int n, Force *f)
         : p(p), n(n), f(f) {}
 };
-
- /* [pa]ritcle and [fo]rce [p]ointers ;  see also dpdr/ */
-typedef Sarray<Particle*, 26> Pap26;
-typedef Sarray<Force*,    26> Fop26;
