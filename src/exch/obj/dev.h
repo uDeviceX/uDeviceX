07c062913586a8b83ad69cc5419b8decce8ca7d8
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-21 08:49:41 +0200

    add static

--- src/exch/obj/dev.h
+++ src/exch/obj/dev.h
@@ -2,7 +2,7 @@ namespace dev {
 
 enum { MAX_DSTS = 7 };
 
-__device__ int map_code(int3 L, const float r[3]) {
+static __device__ int map_code(int3 L, const float r[3]) {
     int x, y, z;
     enum {X, Y, Z};
     x = -1 + (r[X] >= -L.x / 2) + (r[X] >= L.x / 2);
@@ -12,14 +12,14 @@ __device__ int map_code(int3 L, const float r[3]) {
     return frag_d2i(x, y, z);
 }
 
-__device__ void add_to_map(int soluteid, int pid, int fid, Map m) {
+static __device__ void add_to_map(int soluteid, int pid, int fid, Map m) {
     int ientry, centry;
     centry = soluteid * NBAGS + fid;
     ientry = atomicAdd(m.counts + centry, 1);
     m.ids[fid][ientry] = pid;
 }
 
-__device__ int add_faces(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
+static __device__ int add_faces(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
     for (int c = 0; c < 3; ++c) {
         if (d[c]) {
             int df[3] = {0, 0, 0}; df[c] = d[c];
@@ -29,7 +29,7 @@ __device__ int add_faces(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
     return j;
 }
 
-__device__ int add_edges(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
+static __device__ int add_edges(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
     enum {X, Y, Z};
     for (int c = 0; c < 3; ++c) {
         int de[3] = {d[X], d[Y], d[Z]}; de[c] = 0;
@@ -39,14 +39,14 @@ __device__ int add_edges(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
     return j;
 }
 
-__device__ int add_cornr(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
+static __device__ int add_cornr(int j, const int d[3], /**/ int fids[MAX_DSTS]) {
     enum {X, Y, Z};
     if (d[X] && d[Y] && d[Z])
         fids[j++] = frag_d32i(d);
     return j;
 }
 
-__device__ int map_decode(int code, /**/ int fids[MAX_DSTS]) {
+static __device__ int map_decode(int code, /**/ int fids[MAX_DSTS]) {
     int j = 0;
     const int d[3] = frag_i2d3(code);
     j = add_faces(j, d, /**/ fids);
@@ -68,7 +68,7 @@ __global__ void build_map(int3 L, int soluteid, int n, const Particle *pp, /**/ 
         add_to_map(soluteid, pid, fids[j], /**/ map);
 }
 
-__device__ void scan_pad(int cnt, int t, /**/ int *starts) {
+static __device__ void scan_pad(int cnt, int t, /**/ int *starts) {
     int L, scan;
     scan = cnt;
     for (L = 1; L < 32; L <<= 1) scan += (t >= L) * __shfl_up(scan, L);
