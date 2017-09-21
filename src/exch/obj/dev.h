39225a80a2c1028c296484384b5b034fe7a7d159
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-21 08:49:00 +0200

    copy/paste rex/dev/scan

--- src/exch/obj/dev.h
+++ src/exch/obj/dev.h
@@ -68,4 +68,34 @@ __global__ void build_map(int3 L, int soluteid, int n, const Particle *pp, /**/ 
         add_to_map(soluteid, pid, fids[j], /**/ map);
 }
 
+__device__ void scan_pad(int cnt, int t, /**/ int *starts) {
+    int L, scan;
+    scan = cnt;
+    for (L = 1; L < 32; L <<= 1) scan += (t >= L) * __shfl_up(scan, L);
+    if (t < 27) starts[t] = scan - cnt;
+}
+
+__global__ void scanA(const int *counts, const int *oldtcounts, /**/ int *tcounts, int *starts) {
+    int t, cnt, newcount;
+    t = threadIdx.x;
+    cnt = 0;
+    if (t < 26) {
+        cnt = counts[t];
+        if (tcounts && oldtcounts) {
+            newcount = cnt + oldtcounts[t];
+            tcounts[t] = newcount;
+        }
+    }
+    if (starts) scan_pad(cnt, t, /**/ starts);
+}
+
+__global__ void scanB(const int *count, /**/ int *starts) {
+    int t, cnt;
+    t = threadIdx.x;
+    cnt = 0;
+    if (t < 26) cnt = count[t];
+    if (starts) scan_pad(cnt, t, /**/ starts);
+}
+
+
 } // dev
