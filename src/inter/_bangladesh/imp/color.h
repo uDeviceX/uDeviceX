2c138f9957f09eea9e98fa39372d980283d3030c
Sergey Litvinov <slitvinov@gmail.com>
2017-09-21 15:41:40 +0200

    use BANGLADESH_R

--- src/inter/_bangladesh/imp/color.h
+++ src/inter/_bangladesh/imp/color.h
@@ -1,5 +1,3 @@
-static int min (int x, int y)        { return x < y ? x : y; };
-static int min3(int x, int y, int z) { return min(x, min(y, z)); }
 static void color(Particle *pp, int n, /**/ int *cc) {
     enum {X, Y, Z};
     int lx, ly, lz; /* domain */
@@ -8,10 +6,11 @@ static void color(Particle *pp, int n, /**/ int *cc) {
     float x, y, z, rad;
     float x0, y0, z0;
     Particle p;
+    rad = BANGLADESH_R;
+
     lx = m::lx(); ly = m::ly(); lz = m::lz();
     x0 = 0.5*lx; y0 = 0.5*ly; z0 = 0.5*lz;
     enum {R = RED_COLOR, G = BLUE_COLOR};
-    rad = 0.25*min3(lx, ly, lz);
     for (i = r = g = 0; i < n; i++) {
         p = pp[i];
         x = p.r[X]; y = p.r[Y]; z = p.r[Z];
