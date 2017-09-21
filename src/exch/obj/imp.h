f1deed1575d1bfd7ed1e0fb1e9fb3b08d1719002
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-21 12:45:44 +0200

    add dummy function declarations

--- src/exch/obj/imp.h
+++ src/exch/obj/imp.h
@@ -9,10 +9,15 @@ void post_send(Pack *p, Comm *c);
 void wait_recv(Comm *c, Unpack *u);
 void wait_send(Comm *c);
 
+void unpack();
+void pack_ff();
+
 void post_recv_ff(Comm *c, UnpackF *u);
 void post_send_ff(PackF *p, Comm *c);
 void wait_recv_ff(Comm *c, UnpackF *u);
 void wait_send_ff(Comm *c);
+
+void unpack_ff();
 
 } // obj
 } // exch
