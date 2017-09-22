262201c37dfa2caf0570ed866d7338166b2dca0a
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-22 18:06:17 +0200

    use new gen_color

--- src/sim/imp/colors.h
+++ src/sim/imp/colors.h
@@ -1,5 +1,5 @@
 /* set colors of particles according to the RBCs */
-void gen_colors() {
+void gen_colors_old() {
 
     if (mc::tc.first) mcomm::post_recv(/**/ &mc::tc, &mc::tr);
     mcomm::extents(r::q.pp, r::q.nv, r::q.nc, /**/ &mc::ts);
@@ -21,7 +21,7 @@ void gen_colors() {
     mc::tc.first = false;
 }
 
-void gen_colors_new() {
+void gen_colors() {
     int nm, nv, nmhalo;
     nm = r::q.nc;
     nv = r::q.nv;
@@ -41,13 +41,12 @@ void gen_colors_new() {
     wait_recv(&e.c, &e.u);
 
     unpack(nv, &e.u, /**/ &nmhalo, pp + nm * nv);
-    dSync();
     nm += nmhalo;
 
     /* tmp texture object; TODO: make a ticket? */
     Texo<float2> texvert;
     TE(&texvert, (float2*) pp, 3 * nm * nv);
-
+    
     collision::get_colors(o::q.pp, o::q.n, texvert, r::tt.textri, r::q.nt, nv, nm, /**/ o::qc.ii);
 
     texvert.destroy();
