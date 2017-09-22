1dca47547ba6b94af3a811e18be1eca5ded90386
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-22 14:29:06 +0200

    pack

--- src/exch/mesh/dev.h
+++ src/exch/mesh/dev.h
@@ -1,6 +1,6 @@
 namespace dev {
 
-__global__ void build_map(int3 L, int soluteid, int n, const float3 *minext, const float3 *maxext, /**/ Map map) {
+__global__ void build_map(int3 L, int n, const float3 *minext, const float3 *maxext, /**/ Map map) {
     int i, fid, fids[MAX_DSTS], ndsts, j;
     float3 lo, hi;
     i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -13,9 +13,33 @@ __global__ void build_map(int3 L, int soluteid, int n, const float3 *minext, con
     ndsts = map_decode(fid, /**/ fids);
 
     for (j = 0; j < ndsts; ++j)
-        add_to_map(soluteid, i, fids[j], /**/ map);
+        add_to_map(0, i, fids[j], /**/ map);
 }
 
+static __device__ void pack_p(int nv, const Particle *pp, int vid, int frag_mid, int *indices, /**/ Particle *buf) {
+    int dst, src, mid;
+    mid = __ldg(indices + frag_mid);
+    dst = nv * frag_mid + vid;
+    src = nv * mid      + vid;
+    buf[dst] = pp[src];
+}
 
+__global__ void pack_pp_packet(int nv, const Particle *pp, Map map, /**/ Pap26 buf) {
+    int gid, hi, step, fid, mid, vid, frag_mid;
+    gid = threadIdx.x + blockDim.x * blockIdx.x;
+    hi = map.starts[26] * nv;
+    step = gridDim.x * blockDim.x;
+    
+    for (  ; gid < hi; gid += step) {
+        mid = gid / nv; /* mesh id   */
+        vid = gid % nv; /* vertex id */
+        fid = k_common::fid(map.starts, mid);
+
+        /* mesh index in the fragment coordinates */ 
+        frag_mid = mid - map.starts[fid];
+        
+        pack_p(nv, pp, vid, frag_mid, map.ids[fid], /**/ buf.d[fid]);
+    }
+}
 
 } // dev
