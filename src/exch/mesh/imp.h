1b8cf90ea6538d6febd3c803dd947103cc0fe817
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-22 15:26:09 +0200

    com

--- src/exch/mesh/imp.h
+++ src/exch/mesh/imp.h
@@ -9,6 +9,16 @@ void fin(Pack *p);
 void fin(Comm *c);
 void fin(Unpack *u);
 
+void build_map(int nm, int nv, const Particle *pp, /**/ Pack *p);
+void pack(int nv, const Particle *pp, /**/ Pack *p);
+void download(Pack *p);
+
+void post_recv(Comm *c, Unpack *u);
+void post_send(Pack *p, Comm *c);
+void wait_recv(Comm *c, Unpack *u);
+void wait_send(Comm *c);
+
+
 
 } // mesh
 } // exch
