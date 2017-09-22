24db15448d8f0d7272e36107c9164dd2a3d462ed
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-22 17:51:29 +0200

    add gen_colors using mesh exchanger

--- src/sim/imp/colors.h
+++ src/sim/imp/colors.h
@@ -22,23 +22,33 @@ void gen_colors() {
 }
 
 void gen_colors_new() {
+    int nm, nv, nmhalo;
+    nm = r::q.nc;
+    nv = r::q.nv;
 
-    if (mc::tc.first) mcomm::post_recv(/**/ &mc::tc, &mc::tr);
-    mcomm::extents(r::q.pp, r::q.nv, r::q.nc, /**/ &mc::ts);
-    int nbulk = mcomm::map(r::q.nc, /**/ &mc::tm, &mc::ts);
-    mcomm::pack(r::q.pp, r::q.nv, &mc::tm, /**/ &mc::ts);
-    mcomm::post_send(r::q.nv, &mc::ts, /**/ &mc::tc);
-    mcomm::wait_recv(/**/ &mc::tc);
-    int nm = mcomm::unpack(r::q.nv, nbulk, /**/ &mc::tr);
-    mcomm::post_recv(/**/ &mc::tc, &mc::tr);
+    using namespace mc;
+    
+    build_map(nm, nv, r::q.pp, /**/ &e.p);
+    pack(nv, r::q.pp, /**/ &e.p);
+    download(&e.p);
+
+    post_send(&e.p, &e.c);
+    post_recv(&e.c, &e.u);
+
+    CC(d::MemcpyAsync(pp, r::q.pp, nm * nv * sizeof(Particle), D2D));
+    
+    wait_send(&e.c);
+    wait_recv(&e.c, &e.u);
+
+    unpack(nv, &e.u, /**/ &nmhalo, pp + nm * nv);
     dSync();
+    nm += nmhalo;
 
     /* tmp texture object; TODO: make a ticket? */
     Texo<float2> texvert;
-    TE(&texvert, (float2*)mc::tr.pp, nm * 3 * r::q.nv);
+    TE(&texvert, (float2*) pp, 3 * nm * nv);
 
-    collision::get_colors(o::q.pp, o::q.n, texvert, r::tt.textri, r::q.nt, r::q.nv, nm, /**/ o::qc.ii);
+    collision::get_colors(o::q.pp, o::q.n, texvert, r::tt.textri, r::q.nt, nv, nm, /**/ o::qc.ii);
 
     texvert.destroy();
-    mc::tc.first = false;
 }
