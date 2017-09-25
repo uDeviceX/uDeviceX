e5551f914794cefa6270541a421ed3fb530bc320
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-25 09:20:04 +0200

    add momp26 and int27 in type

--- src/inc/type.h
+++ src/inc/type.h
@@ -32,8 +32,10 @@ struct Momentum {
 };
 
 typedef Sarray<int, 26>       int26;
+typedef Sarray<int, 27>       int27;
 typedef Sarray<Particle*, 26> Pap26;
 typedef Sarray<Force*,    26> Fop26;
+typedef Sarray<Momentum*, 26> Mop26;
 
 /* particles wrap */
 struct PaWrap {
