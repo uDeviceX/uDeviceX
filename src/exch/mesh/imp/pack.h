9b8a585ed88f57082339f854acdc52239320ad89
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-25 09:33:44 +0200

    pack_mom

--- src/exch/mesh/imp/pack.h
+++ src/exch/mesh/imp/pack.h
@@ -12,3 +12,27 @@ void download(Pack *p) {
     download_counts(1, p->map, /**/ p->hpp.counts);
 }
 
+
+static int27 scan(const int cc[26]) {
+    int i, s;
+    int27 ss;
+    s = ss.d[0] = 0;
+    for (i = 0; i < 26; ++i) ss.d[i+1] = (s += cc[i]);
+    return ss;
+}
+
+static void pack_mom(const int counts[], const Momentum *mm, /**/ Mop26 buf) {
+    int27 starts = scan(counts);
+    int n = starts.d[26];
+    KL(dev::pack_mom, (k_cnf(n)), (starts, mm, /**/ buf));
+}
+
+void packM(const int counts[], const Momentum *mm, /**/ PackM *p) {
+    Mop26 wrap;
+    bag2Sarray(p->dmm, &wrap);
+    pack_mom(counts, mm, /**/ wrap);
+}
+
+void download(PackM *p) {
+    
+}
