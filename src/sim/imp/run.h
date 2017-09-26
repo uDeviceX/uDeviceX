2fbef9fffdfc0c57a967287f4368e9eca38c58af
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-26 16:01:08 +0200

    distribute solvent after run

--- src/sim/imp/run.h
+++ src/sim/imp/run.h
@@ -2,10 +2,12 @@ void run_eq(long te) { /* equilibrate */
     float driving_force0 = 0;
     bool wall0 = false;
     for (long it = 0; it < te; ++it) step(driving_force0, wall0, it);
+    distribute_flu();
 }
 
 void run(long ts, long te) {
     dump_strt_templ(); /* :TODO: is it the right place? */
     /* ts, te: time start and end */
     for (long it = ts; it < te; ++it) step(FORCE_PAR_A, walls, it);
+    distribute_flu();
 }
