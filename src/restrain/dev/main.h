772c1b4172056a03d4eebead7195c2cfc159b76d
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-26 12:24:41 +0200

    fix

--- src/restrain/dev/main.h
+++ src/restrain/dev/main.h
@@ -5,7 +5,7 @@ static __device__ int indrop;
 static __device__ float3 warpReduceSumf3(float3 v) {
     for (int offset = warpSize>>1; offset > 0; offset >>= 1) {
         v.x += __shfl_down(v.x, offset);
-        v.x += __shfl_down(v.y, offset);
+        v.y += __shfl_down(v.y, offset);
         v.z += __shfl_down(v.z, offset);
     }
     return v;
