195dcbc618cce47fbdff0ba655e4f3bf4841ee3f
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-27 11:07:36 +0200

    P2rP

--- src/meshbb/type.h
+++ src/meshbb/type.h
@@ -12,3 +12,12 @@ __device__ float3 make_real3(float a, float b, float c) {
 __device__ double3 make_real3(double a, double b, double c) {
     return make_double3(a, b, c);
 }
+
+__device__ rPa P2rP(const Particle *p) {
+    enum {X, Y, Z};
+    rPa rp = {
+        .r = make_real3(p->r[X], p->r[Y], p->r[Z]),
+        .v = make_real3(p->v[X], p->v[Y], p->v[Z])
+    };
+    return rp;
+}
