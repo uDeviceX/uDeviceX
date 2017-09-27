917a16ee30697fb9f6e61efa8637bf4a6fa7d2ca
Sergey Litvinov <slitvinov@gmail.com>
2017-09-27 19:17:28 +0200

    do not use float3

--- src/lforces/dev/core.h
+++ src/lforces/dev/core.h
@@ -4,8 +4,6 @@ static __device__ void core( const uint dststart, const uint pshare, const uint 
     forces::Fo f;
     forces::f32f(&fx, &fy, &fz, /**/ &f);
 
-    float3 f0;
-
     uint item;
     uint offset = xmad( tid, 4.f, pshare );
     asm volatile( "ld.volatile.shared.u32 %0, [%1+1024];" : "=r"( item ) : "r"( offset ) : "memory" );
@@ -13,23 +11,23 @@ static __device__ void core( const uint dststart, const uint pshare, const uint 
     uint dpid = xadd( dststart, pid.x );
     uint spid = pid.y;
 
-    f0 = dpd(dpid, spid, /**/ f);
+    dpd(dpid, spid, /**/ f);
 
     // the overhead of transposition acc back
     // can be completely killed by changing the integration kernel
     uint off  = dpid & 0x0000001FU;
     uint base = xdiv( dpid, 1 / 32.f );
     float* acc = info.ff + xmad( base, 96.f, off );
-    atomicAdd( acc     , f0.x );
-    atomicAdd( acc + 32, f0.y );
-    atomicAdd( acc + 64, f0.z );
+    atomicAdd( acc     , fx );
+    atomicAdd( acc + 32, fy );
+    atomicAdd( acc + 64, fz );
 
     if( spid < spidext ) {
         uint off  = spid & 0x0000001FU;
         uint base = xdiv( spid, 1 / 32.f );
         float* acc = info.ff + xmad( base, 96.f, off );
-        atomicAdd( acc     , -f0.x );
-        atomicAdd( acc + 32, -f0.y );
-        atomicAdd( acc + 64, -f0.z );
+        atomicAdd( acc     , -fx );
+        atomicAdd( acc + 32, -fy );
+        atomicAdd( acc + 64, -fz );
     }
 }
--- src/lforces/dev/dpd.h
+++ src/lforces/dev/dpd.h
@@ -1,17 +1,16 @@
-static __device__ float3 dpd0(uint aid, uint bid, float rnd, /**/ forces::Fo f) {
+static __device__ void dpd0(uint aid, uint bid, float rnd, /**/ forces::Fo f) {
     forces::Pa a, b;
 
     cloud_get(aid, &a);
     cloud_get(bid, &b);
     forces::genf(a, b, rnd, /**/ f);
-    return make_float3(*f.x, *f.y, *f.z);
 }
 
 static __device__ float random(uint i, uint j) {
     return rnd::mean0var1ii(info.seed, xmin(i, j), xmax(i, j));
 }
-static __device__ float3 dpd(uint aid, uint bid, /**/ forces::Fo f) {
+static __device__ void dpd(uint aid, uint bid, /**/ forces::Fo f) {
     float rnd;
     rnd = random(aid, bid); /* (sic) */
-    return dpd0(aid, bid, rnd, /**/ f);
+    dpd0(aid, bid, rnd, /**/ f);
 }
