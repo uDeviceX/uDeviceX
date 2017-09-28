7f2f19384cadee4a7ee73da2b75a11aa19c409bd
Sergey Litvinov <slitvinov@gmail.com>
2017-09-28 14:29:32 +0200

    unused function declaration

--- src/wall/force/imp.h
+++ src/wall/force/imp.h
@@ -7,13 +7,6 @@ struct Wa { /* local wall data */
     int w_n;
 };
 
-void build_cells(const int n, float4 *pp4, clist::Clist *cells);
-
-void gen_quants(TexSDF_t texsdf, /**/ int *o_n, Particle *o_pp, int *w_n, float4 **w_pp);
-void strt_quants(int *w_n, float4 **w_pp);
-
-void gen_ticket(const int w_n, float4 *w_pp, clist::Clist *cells, clist::Ticket *tcells, Texo<int> *texstart, Texo<float4> *texpp);
-
 namespace grey {
 void force(TexSDF_t texsdf, hforces::Cloud cloud, const int n, const Texo<int> texstart,
            const Texo<float4> texpp, const int w_n, /**/ rnd::KISS *rnd, Force *ff);
@@ -23,6 +16,4 @@ namespace color {
 void force(TexSDF_t texsdf, hforces::Cloud cloud, const int n, const Texo<int> texstart,
            const Texo<float4> texpp, const int w_n, /**/ rnd::KISS *rnd, Force *ff);
 }
-
-void strt_dump_templ(const int n, const float4 *pp);
-}
+} /* namespace */
