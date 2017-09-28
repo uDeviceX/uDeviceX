9a2d5e93761a0402b84b05c669cdaec174c4a89a
Sergey Litvinov <slitvinov@gmail.com>
2017-09-28 22:04:58 +0200

    fsi calls genf()

--- src/fsi/dev/pair.h
+++ src/fsi/dev/pair.h
@@ -1,5 +1,7 @@
 static __device__ void pair0(const Pa a, const Pa b, float rnd, /**/ float *fx, float *fy, float *fz) {
-    forces::gen(a, b, rnd, /**/ fx, fy, fz);
+    forces::Fo f;
+    forces::f32f(fx, fy, fz, /**/ &f);
+    forces::genf(a, b, rnd, /**/ f);
 }
 
 static __device__ void pair(const Pa a, const Pa b, float rnd, /**/
