fa1bf1fac8bd77c003c9182136631f15121cfca3
Sergey Litvinov <slitvinov@gmail.com>
2017-09-28 21:54:22 +0200

    pid/aid

--- src/wall/force/dev/main.h
+++ src/wall/force/dev/main.h
@@ -1,13 +1,13 @@
 static __global__ void force(hforces::Cloud cloud, int np, float seed, Wa wa, /**/ float *ff) {
     forces::Pa a; /* bulk particle */
-    int gid, pid, zplane;
+    int gid, aid, zplane;
     gid = threadIdx.x + blockDim.x * blockIdx.x;
-    pid = gid / 3;
+    aid    = gid / 3;
     zplane = gid % 3;
 
-    if (pid >= np) return;
-    fetch(cloud, pid, /**/ &a);
+    if (aid >= np) return;
+    fetch(cloud, aid, /**/ &a);
 
     /* call generic function from polymorphic */
-    wall::dev::force0(a, pid, zplane, seed, wa, /**/ ff);
+    wall::dev::force0(a, aid, zplane, seed, wa, /**/ ff);
 }
