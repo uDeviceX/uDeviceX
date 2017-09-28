a4f841a037f477a2adf67310ccddb48cd8c293a7
Sergey Litvinov <slitvinov@gmail.com>
2017-09-28 10:41:40 +0200

    initial import of wall/force

---
+++ src/wall/force/imp.cu
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <conf.h>
+#include "inc/conf.h"
+#include "inc/def.h"
+#include "msg.h"
+#include "glb.h"
+
+#include "d/api.h"
+#include "d/q.h"
+#include "d/ker.h"
+
+#include "utils/cc.h"
+
+#include "inc/type.h"
+#include "inc/dev.h"
+#include "utils/texo.h"
+#include "utils/te.h"
+
+#include "inc/macro.h"
+
+#include "rnd/imp.h"
+#include "rnd/dev.h"
+
+#include "sdf/type.h"
+#include "sdf/int.h"
+#include "sdf/cheap.dev.h"
+
+#include "inc/dev/wvel.h"
+#include "forces/type.h"
+#include "forces/use.h"
+#include "forces/pack.h"
+#include "forces/imp.h"
+
+#include "cloud/hforces/type.h"
+#include "cloud/hforces/get.h"
+
+#include "algo/scan/int.h"
+#include "clist/imp.h"
+#include "io/restart.h"
+
+#include "utils/kl.h"
+#include "exch/imp.h"
+
+#include "imp.h"
+
+namespace wall {
+namespace dev {
+  #include "dev/main.h"
+  namespace map {
+    #include "dev/map/type.h"
+    #include "dev/map/ini.h"
+    #include "dev/map/use.h"
+  }
+  #include "dev/pair0.h"
+}
+
+namespace strt {
+  #include "imp/strt.h"
+}
+#include "imp/main.h"
+
+/*** polymorphic ***/
+namespace grey {
+  namespace dev {
+    #include "dev/fetch/grey.h"
+    #include "dev/pair.h"
+  }
+  #include "imp/pair.h"
+}
+
+namespace color {
+  namespace dev {
+    #include "dev/fetch/color.h"
+    #include "dev/pair.h"
+  }
+  #include "imp/pair.h"
+}
+
+} /* wall */
---
+++ src/wall/force/imp/strt.h
@@ -0,0 +1,28 @@
+void read(/**/ float4 *pp, int *n) {
+    Particle *pphst, *ppdev;
+    pphst = new Particle[MAX_PART_NUM];
+    restart::read_pp("wall", restart::TEMPL, /**/ pphst, n);
+
+    if (*n) {
+        CC(cudaMalloc(&ppdev, MAX_PART_NUM * sizeof(Particle)));
+        cH2D(ppdev, pphst, *n);
+        KL(dev::particle2float4, (k_cnf(*n)), (ppdev, *n, /**/ pp));
+        CC(cudaFree(ppdev));
+    }
+    delete[] pphst;
+}
+
+void write(const float4 *pp, const int n) {
+    Particle *pphst, *ppdev;
+    pphst = new Particle[MAX_PART_NUM];
+    if (n) {
+        CC(cudaMalloc(&ppdev, MAX_PART_NUM * sizeof(Particle)));
+        KL(dev::float42particle , (k_cnf(n)), (pp, n, /**/ ppdev));
+        cD2H(pphst, ppdev, n);
+        CC(cudaFree(ppdev));
+    }
+    restart::write_pp("wall", restart::TEMPL, /**/ pphst, n);
+
+    delete[] pphst;
+}
+
