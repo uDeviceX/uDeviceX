1cdb82e3e87a7b893d7d4d2156c38ebc052b9ab3
Sergey Litvinov <slitvinov@gmail.com>
2017-09-28 03:19:19 +0200

    unuse parameter in imp::pair

--- src/wall/imp/pair.h
+++ src/wall/imp/pair.h
@@ -1,5 +1,5 @@
-void pair(TexSDF_t texsdf, const int type, hforces::Cloud cloud, const int n, const Texo<int> texstart,
-                  const Texo<float4> texpp, const int w_n, /**/ rnd::KISS *rnd, Force *ff) {
+void pair(TexSDF_t texsdf, hforces::Cloud cloud, const int n, const Texo<int> texstart,
+          const Texo<float4> texpp, const int w_n, /**/ rnd::KISS *rnd, Force *ff) {
     KL(dev::pair,
        (k_cnf(3*n)),
        (texsdf, cloud, n, w_n, (float *)ff, rnd->get_float(), texstart, texpp));
