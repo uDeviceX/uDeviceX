ca8594132a565c4b339bc2ef2eb8f2d4a03d4c9c
Sergey Litvinov <slitvinov@gmail.com>
2017-09-28 14:49:21 +0200

    decl "Wa wa"

--- src/wall/int/force.h
+++ src/wall/int/force.h
@@ -1,5 +1,5 @@
 void force(const sdf::Quants qsdf, const Quants q, const Ticket t, hforces::Cloud cloud, const int n, Force *ff) {
-    //    Wa wa; /* local wall data */
+    Wa wa; /* local wall data */
     /*    wa.texsdf = qsdf.texsdf;
     wa.texstart = t.texstart;
     wa.texpp  = t.texpp;
