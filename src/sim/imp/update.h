bcaf106a805398a29c4485e8e0ef35d2aa3eaebe
amlucas <lucas.amoudruz@wanadoo.fr>
2017-09-29 18:31:26 +0200

    rm intermediate bouncer

--- src/sim/imp/update.h
+++ src/sim/imp/update.h
@@ -53,60 +53,6 @@ void bounce_solid_old(long it) {
 #undef DEV
 }
 
-void bounce_solid_v1(long it) {
-#define DEV (false)
-    mesh::get_bboxes_dev(s::q.i_pp, s::q.nv, s::q.ns, /**/ s::t.minbb_dev, s::t.maxbb_dev);
-
-    cD2H(s::t.minbb_hst, s::t.minbb_dev, s::q.ns);
-    cD2H(s::t.maxbb_hst, s::t.maxbb_dev, s::q.ns);
-    cD2H(s::q.ss_hst, s::q.ss, s::q.ns);
-
-    /* exchange solid meshes with neighbours */
-
-    bbhalo::pack_sendcnt <DEV> (s::q.ss_hst, s::q.ns, s::q.i_pp, s::q.nv, s::t.minbb_hst, s::t.maxbb_hst);
-    const int nsbb = bbhalo::post(s::q.nv);
-    bbhalo::unpack <DEV> (s::q.nv, /**/ s::t.ss_hst, s::t.i_pp);
-
-    cH2D(s::t.ss, s::t.ss_hst, nsbb);
-
-    int n, nm, nt, nv, *ss, *cc;
-    int4 *tt;
-    Particle *pp, *i_pp;
-    int3 L = make_int3(XS, YS, ZS);
-    
-    nm = s::q.ns;
-    nt = s::q.nt;
-    nv = s::q.nv;
-    tt = s::q.dtt;
-    i_pp = s::q.i_pp;
-
-    n  = o::q.n;
-    pp = o::q.pp;
-    cc = o::q.cells.counts;
-    ss = o::q.cells.starts;
-    
-    meshbb::reini(n, /**/ bb::bbd);
-    meshbb::find_collisions(nm, nt, nv, tt, i_pp, L, ss, cc, pp, o::ff, /**/ bb::bbd);
-    meshbb::select_collisions(n, /**/ bb::bbd);
-    meshbb::bounce(n, bb::bbd, o::ff, nt, nv, tt, i_pp, /**/ pp, bb::mm);
-
-    meshbb::collect_momentum(nm, nt, nv, tt, i_pp, bb::mm, /**/ s::t.ss);
-    
-    // send back fo, to
-
-    cD2H(s::t.ss_hst, s::t.ss, nsbb);
-
-    bbhalo::pack_back(s::t.ss_hst);
-    bbhalo::post_back();
-    bbhalo::unpack_back(s::q.ss_hst);
-
-    cH2D(s::q.ss, s::q.ss_hst, s::q.ns);
-
-    // for dump
-    memcpy(s::t.ss_dmp, s::q.ss_hst, s::q.ns * sizeof(Solid));
-#undef DEV
-}
-
 void bounce_solid_v2(long it) {
     int n, nm, nt, nv, *ss, *cc, nmhalo, counts[comm::NFRAGS];
     int4 *tt;
