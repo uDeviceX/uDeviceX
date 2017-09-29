13fc9e11329d6d7e442d8f81df7199b7b2f3a1c1
Sergey Litvinov <slitvinov@gmail.com>
2017-09-29 20:27:12 +0200

    use feth_b

--- src/cnt/dev/halo.h
+++ src/cnt/dev/halo.h
@@ -13,6 +13,15 @@ static __device__ void pp2p(Particle *pp, int i, /**/
     forces::rvk2p(r, v, SOLID_KIND, /**/ a);
 }
 
+static __device__ void fetch_b(const float2 *pp, int i, /**/ forces::Pa *b) {
+    float2 s0, s1, s2;
+    pp += 3*i;
+    s0 = __ldg(pp++);
+    s1 = __ldg(pp++);
+    s2 = __ldg(pp  );
+    forces::f2k2p(s0, s1, s2, SOLID_KIND, /**/ b);
+}
+
 __global__ void halo(int n, float seed) {
     enum {X, Y, Z};
     Map m;
@@ -26,10 +35,9 @@ __global__ void halo(int n, float seed) {
     int zplane;
     int i;
     int slot;
-    int objid, bid;
-    int sentry;
-    float2 stmp0, stmp1, stmp2;
+    int objid, bid, sentry;
     float rnd;
+    float *fA;
 
     aid = threadIdx.x + blockDim.x * blockIdx.x;
     if (aid >= n) return;
@@ -38,8 +46,6 @@ __global__ void halo(int n, float seed) {
     start = h::starts[fid];
     pp2p(h::pp[fid], aid - start, &a);
     forces::p2r3(&a, /**/ &x, &y, &z);
-
-    float *fA;
     fA = h::ff[fid][aid - start].f;
     for (zplane = 0; zplane < 3; ++zplane) {
         mapstatus = tex2map(zplane, x, y, z, /**/ &m);
@@ -47,22 +53,18 @@ __global__ void halo(int n, float seed) {
         for (i = 0; !endp(m, i); ++i) {
             slot = m2id(m, i);
             get(slot, &objid, &bid);
-
-            sentry = 3 * bid;
-            stmp0 = __ldg(c::PP[objid] + sentry);
-            stmp1 = __ldg(c::PP[objid] + sentry + 1);
-            stmp2 = __ldg(c::PP[objid] + sentry + 2);
+            fetch_b(c::PP[objid], bid, /**/ &b);
             rnd = rnd::mean0var1ii(seed, aid, bid);
-            forces::f2k2p(stmp0, stmp1, stmp2, SOLID_KIND, /**/ &b);
             forces::gen(a, b, rnd, &fx, &fy, &fz);
             xforce += fx;
             yforce += fy;
             zforce += fz;
+            
+            sentry = 3 * bid;
             atomicAdd(c::FF[objid] + sentry,     -fx);
             atomicAdd(c::FF[objid] + sentry + 1, -fy);
             atomicAdd(c::FF[objid] + sentry + 2, -fz);
         }
     }
-
     fA[X] += xforce; fA[Y] += yforce; fA[Z] += zforce;
 }
