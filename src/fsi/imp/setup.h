cdeb72639f5d682ed6095f4e4770dff7077501c8
Sergey Litvinov <slitvinov@gmail.com>
2017-09-29 11:53:40 +0200

    setup() is static

--- src/fsi/imp/setup.h
+++ src/fsi/imp/setup.h
@@ -5,7 +5,7 @@ static void setup_first() {
     dev::t::start.normalized = 0;
 }
 
-void setup(const int *const cellsstart) {
+static void setup(const int *const cellsstart) {
     size_t offset;
     int nc;
     if (firsttime) {
