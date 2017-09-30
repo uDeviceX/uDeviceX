117fef12cb1e7a5e61a4c623af2ace6169a847e4
Sergey Litvinov <slitvinov@gmail.com>
2017-09-30 12:28:00 +0200

    use idx

--- src/lforces/transpose/dev/main.h
+++ src/lforces/transpose/dev/main.h
@@ -1,4 +1,11 @@
 #define __IMOD(x,y) ((x)-((x)/(y))*(y))
+
+static __device__ uint idx(int a, int b) {
+    uint r = 32 * a  + b;
+    assert(r == xmad( a, 32.f, b));
+    return r;
+}
+
 static __global__ void transpose(const int np, float *ff) {
     uint lane, warpid, base;
     __shared__ volatile float  smem[32][96];
@@ -10,9 +17,9 @@ static __global__ void transpose(const int np, float *ff) {
         smem[warpid][lane   ] =   ff[ base      ];
         smem[warpid][lane + 32] = ff[ base + 32 ];
         smem[warpid][lane + 64] = ff[ base + 64 ];
-        ff[ base      ] = smem[warpid][ xmad( __IMOD( lane + 0, 3 ), 32.f, ( lane + 0 ) / 3 ) ];
-        ff[ base + 32 ] = smem[warpid][ xmad( __IMOD( lane + 32, 3 ), 32.f, ( lane + 32 ) / 3 ) ];
-        ff[ base + 64 ] = smem[warpid][ xmad( __IMOD( lane + 64, 3 ), 32.f, ( lane + 64 ) / 3 ) ];
+        ff[ base      ] = smem[warpid][idx(__IMOD( lane + 0,  3), (lane + 0 )/3)];
+        ff[ base + 32 ] = smem[warpid][idx(__IMOD( lane + 32, 3), (lane + 32)/3)];
+        ff[ base + 64 ] = smem[warpid][idx(__IMOD( lane + 64, 3), (lane + 64)/3)];
     }
 }
 #undef __IMOD
