20bcf2494d37927262bb07581d26c00a510c71e7
Sergey Litvinov <slitvinov@gmail.com>
2017-09-30 12:20:33 +0200

    wrong assert

--- src/lforces/transpose/dev/main.h
+++ src/lforces/transpose/dev/main.h
@@ -1,12 +1,20 @@
 #define __IMOD(x,y) ((x)-((x)/(y))*(y))
+static __device__ uint xmad0(uint u, uint w) {
+    uint r;
+    r = 3 * u  + w;
+    assert(r + 1 == xmad(u, 3.f, w));
+    return r;
+}
+
 static __global__ void transpose(const int np, float *ff) {
+    uint lane, warpid, base;
     __shared__ volatile float  smem[32][96];
-    const uint lane = threadIdx.x % warpSize;
-    const uint warpid = threadIdx.x / warpSize;
+    lane = threadIdx.x % warpSize;
+    warpid = threadIdx.x / warpSize;
 
     for( uint i = ( blockIdx.x * blockDim.x + threadIdx.x ) & 0xFFFFFFE0U; i < np; i += blockDim.x * gridDim.x ) {
-        const uint base = xmad( i, 3.f, lane );
-        smem[warpid][lane   ] = ff[ base      ];
+        base = xmad0(i, lane);
+        smem[warpid][lane   ] =   ff[ base      ];
         smem[warpid][lane + 32] = ff[ base + 32 ];
         smem[warpid][lane + 64] = ff[ base + 64 ];
         ff[ base      ] = smem[warpid][ xmad( __IMOD( lane + 0, 3 ), 32.f, ( lane + 0 ) / 3 ) ];
