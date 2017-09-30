661a41b6cc9af8119733681416dbbcb3acec85f1
Sergey Litvinov <slitvinov@gmail.com>
2017-09-30 13:51:46 +0200

    use get_cid

--- src/lforces/dev/merged.h
+++ src/lforces/dev/merged.h
@@ -131,18 +131,7 @@ static __device__ void merged0(int cid, uint tid, uint pshare) {
 
 static __device__ void merged1(uint it, int cbase, uint tid, uint pshare) {
     int cid;
-    asm( "{  .reg .pred    p;"
-         "   .reg .f32     incf;"
-         "   .reg .s32     inc;"
-         "    setp.lt.f32  p, %2, %3;"
-         "    selp.f32     incf, %4, 0.0, p;"
-         "    add.f32      incf, incf, %5;"
-         "    mov.b32      inc, incf;"
-         "    mul.lo.u32   inc, inc, %6;"
-         "    add.s32 %0,  %1, inc;"
-         "}" :
-         "=r"( cid ) : "r"( cbase ), "f"( u2f( it ) ), "f"( u2f( 2u ) ), "f"( i2f( info.ncells.y ) ), "f"( u2f( ( it & 1u ) ^ ( it >> 1 ) ) ),
-         "r"( info.ncells.x ) );
+    cid = get_cid(it, cbase);
     merged0(cid, tid, pshare);
 }
 
