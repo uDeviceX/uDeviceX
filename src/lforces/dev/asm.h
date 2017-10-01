3de44f2b9d3d702ec60bd559e647a184d8143194
Sergey Litvinov <slitvinov@gmail.com>
2017-10-01 17:56:58 +0200

    minor

--- src/lforces/dev/asm.h
+++ src/lforces/dev/asm.h
@@ -118,5 +118,5 @@ static __device__ void write(uint tid, uint pshare) {
     asm volatile( "{ .reg .u32 tmp;"
                   "   ld.volatile.shared.u32 tmp, [%0+1024+128];"
                   "   st.volatile.shared.u32 [%0+1024], tmp;"
-                  "}" :: "r"( xmad( tid, 4.f, pshare ) ) : "memory" );
+                  "}" :: "r"(xmad(tid, 4.f, pshare)) : "memory");
 }
