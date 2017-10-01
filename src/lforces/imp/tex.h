5b17332dd0f9b09ea1b23274509e1119dafdc985
Sergey Litvinov <slitvinov@gmail.com>
2017-10-01 15:59:14 +0200

    minor

--- src/lforces/imp/tex.h
+++ src/lforces/imp/tex.h
@@ -2,16 +2,16 @@ static void tex_cells(int *start, int *count) {
     const int ncells = XS * YS * ZS;
     static uint2 *start_and_count;
     static int last_nc;
-    if( !start_and_count || last_nc < ncells ) {
-        if( start_and_count ) {
-            cudaFree( start_and_count );
+    if (!start_and_count || last_nc < ncells) {
+        if (start_and_count) {
+            cudaFree(start_and_count);
         }
-        cudaMalloc( &start_and_count, sizeof( uint2 )*ncells );
+        cudaMalloc(&start_and_count, sizeof(uint2)*ncells );
         last_nc = ncells;
     }
 
     size_t offset;
     KL(tex, (64, 512), (start_and_count, start, count, ncells));
-    CC(cudaBindTexture( &offset, &texStartAndCount, start_and_count,
-                        &texStartAndCount.channelDesc, sizeof( uint2 ) * ncells));
+    CC(cudaBindTexture(&offset, &texStartAndCount, start_and_count,
+                       &texStartAndCount.channelDesc, sizeof(uint2) * ncells));
 }
