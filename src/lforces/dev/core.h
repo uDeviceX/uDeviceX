2743cb2ff6c2a0cc004dda20b65f28992fd15b2a
Sergey Litvinov <slitvinov@gmail.com>
2017-10-02 00:03:31 +0200

    acc/ff

--- src/lforces/dev/core.h
+++ src/lforces/dev/core.h
@@ -3,18 +3,18 @@ static __device__ void core0(uint dpid, uint spid, uint spidext) {
     dpd(dpid, spid, /**/ &f);
     uint off  = dpid & 0x0000001FU;
     uint base = xdiv(dpid, 1 / 32.f);
-    float* acc = info.ff + xmad(base, 96.f, off);
-    atomicAdd(acc     , f.x);
-    atomicAdd(acc + 32, f.y);
-    atomicAdd(acc + 64, f.z);
+    float* ff = info.ff + xmad(base, 96.f, off);
+    atomicAdd(ff     , f.x);
+    atomicAdd(ff + 32, f.y);
+    atomicAdd(ff + 64, f.z);
 
     if (spid < spidext) {
         uint off  = spid & 0x0000001FU;
         uint base = xdiv(spid, 1 / 32.f);
-        float* acc = info.ff + xmad(base, 96.f, off);
-        atomicAdd(acc     , -f.x);
-        atomicAdd(acc + 32, -f.y);
-        atomicAdd(acc + 64, -f.z);
+        float* ff = info.ff + xmad(base, 96.f, off);
+        atomicAdd(ff     , -f.x);
+        atomicAdd(ff + 32, -f.y);
+        atomicAdd(ff + 64, -f.z);
     }
 }
 
