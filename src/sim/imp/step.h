646c22426bff11d659f477dead9fc4e0bf645323
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-02 14:39:34 +0200

    no step0

--- src/sim/imp/step.h
+++ src/sim/imp/step.h
@@ -1,20 +1,19 @@
-void step0(float driving_force0, bool wall0, int it) {
+void step(float driving_force0, bool wall0, int it) {
+    distribute_flu();
     if (solids0) distribute_rig();
     if (rbcs)    distribute_rbc();
+
     forces(wall0);
+
     dump_diag0(it);
     if (wall0 || solids0) dump_diag_after(it);
     body_force(driving_force0);
+    
     update_solvent(it);
     if (solids0) update_solid();
     if (rbcs)    update_rbc(it);
-    if (wall0) bounce();
 
+    if (wall0) bounce();
     if (sbounce_back && solids0) bounce_solid_old(it);
     // if (sbounce_back && solids0) bounce_solid_v2(it);
-}
-
-void step(float driving_force0, bool wall0, int it) {
-    distribute_flu();
-    step0(driving_force0, wall0, it);
 }
