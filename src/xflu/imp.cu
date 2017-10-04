07e1b66495075ae2a003a5611a26bd24a143105d
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-04 17:57:49 +0200

    add includes in imp.cu; move functions to imp/

---
+++ src/xflu/imp.cu
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <mpi.h>
+#include <assert.h>
+
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "inc/def.h"
+#include "inc/dev.h"
+#include "inc/type.h"
+
+#include "d/api.h"
+#include "mpi/wrapper.h"
+
+#include "utils/mc.h"
+#include "utils/cc.h"
+#include "utils/kl.h"
+
+#include "msg.h"
+
+#include "algo/scan/int.h"
+#include "clist/imp.h"
+#include "rnd/imp.h"
+
+#include "io/restart.h"
+#include "inter/color.h"
+
+#include "imp.h"
+
+namespace flu {
+
+#include "dev.h"
+
+#include "imp/ini.h"
+#include "imp/fin.h"
+#include "imp/generate.h"
+#include "imp/start.h"
+
+} // flu
