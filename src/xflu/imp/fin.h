b249fa744ef5f580f8588d96a4cebcc47d7e3b4d
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-04 18:00:03 +0200

    use d::

--- src/xflu/imp/fin.h
+++ src/xflu/imp/fin.h
@@ -1,20 +1,20 @@
 void free_quants(Quants *q) {
-    CC(cudaFree(q->pp)); CC(cudaFree(q->pp0));
+    CC(d::Free(q->pp)); CC(d::Free(q->pp0));
     fin(&q->cells);
     fin_ticket(&q->tcells);
     delete[] q->pp_hst;
 }
 
 void free_quantsI(QuantsI *q) {
-    CC(cudaFree(q->ii)); CC(cudaFree(q->ii0));
+    CC(d::Free(q->ii)); CC(d::Free(q->ii0));
     delete[] q->ii_hst;
 }
 
 void free_ticketZ(/**/ TicketZ *t) {
     float4  *zip0 = t->zip0;
     ushort4 *zip1 = t->zip1;
-    cudaFree(zip0);
-    cudaFree(zip1);
+    CC(d::Free(zip0));
+    CC(d::Free(zip1));
 }
 
 void free_ticketRND(/**/ TicketRND *t) {
