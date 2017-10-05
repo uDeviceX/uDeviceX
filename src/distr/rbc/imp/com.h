6ab3e7b4bdb4aaeb5d37a19278be8f48e3ac9fa5
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-05 11:36:02 +0200

    comm rbc ids

--- src/distr/rbc/imp/com.h
+++ src/distr/rbc/imp/com.h
@@ -1,15 +1,19 @@
 void post_recv(Comm *c, Unpack *u) {
     post_recv(&u->hpp, &c->pp);
+    if (rbc_ids) post_recv(&u->hii, &c->ii);
 }
 
 void post_send(Pack *p, Comm *c) {
     post_send(&p->hpp, &c->pp);
+    if (rbc_ids) post_send(&p->hii, &c->ii);
 }
 
 void wait_recv(Comm *c, Unpack *u) {
     wait_recv(&c->pp, &u->hpp);
+    if (rbc_ids) wait_recv(&c->ii, &u->hii);
 }
 
 void wait_send(Comm *c) {
     wait_send(&c->pp);
+    if (rbc_ids) wait_send(&c->ii);
 }
