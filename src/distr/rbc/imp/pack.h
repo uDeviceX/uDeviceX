50bbb076418d1ad2d6d14145d0bb244e35569602
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-05 11:19:56 +0200

    adding pack_ii

--- src/distr/rbc/imp/pack.h
+++ src/distr/rbc/imp/pack.h
@@ -9,8 +9,26 @@ static void pack_pp(const Map m, int nc, int nv, const Particle *pp, /**/ dBags 
     KL((dev::pack_pp_packets), (blck, thrd), (nv, pp, m, /**/ wrap));
 }
 
+/* all data (including map) on host */
+static void pack_ii(const Map m, int nc, const int *ii, /**/ dBags bags) {
+    Sarray<int*, NBAGS> wrap;
+    bag2Sarray(bags, &wrap);
+
+    int i, c, j, src;
+    for (i = 0; i < NBAGS; ++i) {
+        c = m.counts[j];
+        for (j = 0; j < c; ++j) {
+            src = m.ids[i][j];
+            wrap.d[i][j] = ii[src];
+        }
+    }
+}
+
 void pack(int nc, int nv, const Particle *pp, /**/ Pack *p) {
     pack_pp(p->map, nc, nv,  pp, /**/ p->dpp);
+
+    // if (rbc_ids)
+    //     pack_ii(p->hmap, nc, ii, /**/ p->hii);
 }
 
 void download(Pack *p) {
