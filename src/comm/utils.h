475579550572f5df434136819e93b2095fce1191
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-05 11:27:06 +0200

    bag2Sarray works for host bags as well

--- src/comm/utils.h
+++ src/comm/utils.h
@@ -1,10 +1,9 @@
 namespace comm {
 
-template <typename T, int N>
-static void bag2Sarray(dBags bags, Sarray<T*, N> *buf) {
+template <typename BAGT, typename T, int N>
+static void bag2Sarray(BAGT bags, Sarray<T*, N> *buf) {
     for (int i = 0; i < N; ++i)
         buf->d[i] = (T*) bags.data[i];
 }
-
 
 } // comm
