e5cb5caeae4bb68029cb6420755c4f3972b79f72
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 22:09:26 +0200

    rearange dupire

--- src/glb/gdot/imp.cu
+++ src/glb/gdot/imp.cu
@@ -9,8 +9,8 @@ namespace gdot {
 #if   defined(GDOT_FLAT)
   #include "imp/flat.h"
 #elif defined(GDOT_DUPIRE_UP)
-  #include "imp/dupire_up.h"
+  #include "imp/dupire/up.h"
 #elif defined(GDOT_DUPIRE_DOWN)
-  #include "imp/dupire_down.h"
+  #include "imp/dupire/down.h"
 #endif
 }
--- src/u/comm/make/dep.mk
+++ src/u/comm/make/dep.mk
@@ -1,7 +1,7 @@
 $B/comm/imp.o: $S/comm/imp.h $S/comm/imp/fin.h $S/comm/imp/ini.h $S/comm/imp/main.h $B/conf.h $S/d/api.h $S/frag/imp.h $S/inc/conf.h $S/mpi/basetags.h $S/mpi/glb.h $S/mpi/wrapper.h $S/msg.h $S/utils/cc.h $S/utils/mc.h
 $B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/msg.h
 $B/frag/imp.o: $B/conf.h $S/frag/imp.h $S/inc/conf.h
-$B/glb/gdot/imp.o: $B/conf.h $S/glb/gdot/imp.h $S/glb/gdot/imp/dupire_down.h $S/glb/gdot/imp/dupire_up.h $S/glb/gdot/imp/flat.h $S/inc/conf.h
+$B/glb/gdot/imp.o: $B/conf.h $S/glb/gdot/imp.h $S/glb/gdot/imp/dupire/down.h $S/glb/gdot/imp/dupire/up.h $S/glb/gdot/imp/flat.h $S/inc/conf.h $S/msg.h
 $B/glb/imp.o: $B/conf.h $S/d/api.h $S/glb/gdot/imp.h $S/glb/imp.h $S/glb/imp/dec.h $S/glb/imp/main.h $S/glb/imp/util.h $S/inc/conf.h $S/mpi/glb.h
 $B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
