f888547f655dae3f823685fb5a579f3bda896935
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 21:48:44 +0200

    add step

--- src/glb/gdot/imp/dupire_up.h
+++ src/glb/gdot/imp/dupire_up.h
@@ -9,22 +9,22 @@ static float ff(long it, long nsteps) {
     return gd;
 }
 
-static void report0(float gd) {
-    MSG("GDOT_DUPIRE_UP: gd = %g", gd);
+static void report0(long i, long e, float gd) {
+    MSG("GDOT_DUPIRE_UP: gd = %g : step %d/%d", gd, i, e);
 }
 
-static void report(long i, float gd) {
+static void report(long i, long e, float gd) {
     bool cond;
     int freq;
     freq = GDOT_REPORT_FREQ;
     cond = freq > 0 && i % freq == 0;
-    if (cond) report0(gd);
+    if (cond) report0(i, e, gd);
 }
 
 float f0() {               return ff(0, 1); }
 float  f(long i, long e) {
     float gd;
     gd = ff(i, e);
-    report(i, gd);
+    report(i, e, gd);
     return gd;
 }
