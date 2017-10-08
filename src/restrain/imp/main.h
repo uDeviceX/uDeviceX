556b6f105a54b8978a6db5460c786f26e66e539c
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 14:18:54 +0200

    pass vel0

--- src/restrain/imp/main.h
+++ src/restrain/imp/main.h
@@ -1,20 +1,21 @@
-void vel(const int *cc, int color, int n, /**/ Particle *pp) {
+void vel0(dev::Map m, int n, /**/ Particle *pp) {
     enum {X, Y, Z};
-    dev::Map m;
-    m.cc = cc; m.color = color;
 
     float3 v;
     float  u[3];
 
-    assert(color < 10);
-
     reini();
-    KL(dev::sum, (k_cnf(n)), (color, n, pp, cc));
+    KL(dev::sum, (k_cnf(n)), (m.color, n, pp, m.cc));
 
     avg_v(/**/ u);
     v = make_float3(u[X], u[Y], u[Z]);
-    KL(dev::shift, (k_cnf(n)), (color, v, n, cc, /**/ pp));
+    KL(dev::shift, (k_cnf(n)), (m.color, v, n, m.cc, /**/ pp));
 
     stat::setv(u);
 }
 
+void vel(const int *cc, int color, int n, /**/ Particle *pp) {
+    dev::Map m;
+    m.cc = cc; m.color = color;
+    vel0(m, n, /**/ pp);
+}
