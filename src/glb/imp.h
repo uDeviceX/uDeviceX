ee191a02edba684c2ea692edbe43e0b528f7bccb
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 20:42:08 +0200

    deps

--- src/glb/imp.h
+++ src/glb/imp.h
@@ -2,5 +2,5 @@ namespace glb {
 extern __constant__  float r0[3];
 extern __constant__  float gd;
 void sim();            /* simulation wide kernel globals */
-void step(long, long); /* time step kernel globals */
+void step(long s, long e); /* time step kernel globals : timestep [s]tart and [e]nd */
 }
