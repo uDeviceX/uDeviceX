f573b57ea45a5fc5ea75d4b2f4803b24af8ad262
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 08:55:14 +0200

    add mass to gravity

--- src/forces/imp/gravity/main.h
+++ src/forces/imp/gravity/main.h
@@ -1,3 +1,9 @@
 static __device__ void gen(Pa A, Pa B, float rnd, /**/ Fo *f) {
+    enum {O = SOLVENT_KIND, S = SOLID_KIND, W = WALL_KIND};
+    int ka, kb;
+    float m[3];
+    m[0] = 1; m[S] = rbc_mass; m[W] = 1;
+
+    ka = A.kind; kb = B.kind;
     gen2(A, B, rnd, /**/ f);
 }
