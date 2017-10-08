add1300c8dc737490f13c9cc81d8b94c31eb0377
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 21:29:51 +0200

    initil import of dupire_up; add a test

--- src/glb/gdot/imp/dupire_down.h
+++ src/glb/gdot/imp/dupire_down.h
@@ -1,5 +1,5 @@
 float f0() {
-    return 0.5*gamma_dot;
+    return gamma_dot;
 }
 
 float  f(long, long) {
--- src/glb/gdot/imp/dupire_up.h
+++ src/glb/gdot/imp/dupire_up.h
@@ -1,7 +1,13 @@
-float f0() {
-    return 0.5*gamma_dot;
+static float ff(long it, long nsteps) {
+    float gd = 0;
+    int i, n = 8;
+    for (i = 1; i <= n; i++)
+        if (n*it < i*nsteps) {
+            gd =  2.0*i/n*gamma_dot;
+            break
+        }
+    return gd;
 }
 
-float  f(long, long) {
-    return gamma_dot;
-}
+float f0() {               return ff(0, 1); }
+float  f(long s, long e) { return ff(s, e); }
