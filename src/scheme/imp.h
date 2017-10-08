770747af7158142c9c60545bc0baa62f9f716b77
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 15:47:31 +0200

    add QQ and NN in scheme

--- src/scheme/imp.h
+++ src/scheme/imp.h
@@ -1,6 +1,10 @@
 namespace scheme {
 struct Fparams {float a, b, c;};
 
+/* quantaties and sizes */
+struct QQ {Particle *o, *r;};
+struct NN {int o, r;};
+
 void move(float mass, Particle *pp, Force *ff, int n);
 void clear_vel(Particle *pp, int n);
 void force(float mass, Fparams fpar, int n, const Particle* pp, /**/ Force* ff);
