d7bfaef4aae90b4f062d03d2723d0d5a7160cf3c
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 14:29:09 +0200

    minor

--- src/restrain/dev/main.h
+++ src/restrain/dev/main.h
@@ -2,7 +2,7 @@ static __global__ void sum(Map m, int n, const Particle *pp) {
     int i, good;
     i = threadIdx.x + blockDim.x * blockIdx.x;
     good = (i < n) && goodp(m, i);
-    sum0(good, pp, i);
+    sum0(good, pp, i); /* collective */
 }
 
 static __global__ void shift(Map m, float3 v, int n, /**/ Particle *pp) {
