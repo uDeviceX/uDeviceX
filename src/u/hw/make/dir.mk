ad22185cee68bbe456dbf34ac0bff32b022b6978
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 19:58:10 +0200

    deps

--- src/u/hw/make/dir.mk
+++ src/u/hw/make/dir.mk
@@ -1,5 +1,6 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
     d $B/d     && \
+    d $B/glb   && \
     d $B/mpi   && \
     d $B/u/hw  && \
     d $B/utils
--- src/u/hw/make/rule.mk
+++ src/u/hw/make/rule.mk
@@ -1,5 +1,5 @@
 $B/d/api.o: $S/d/api.cu; $N -I$S/. -I$S/d
-$B/glb.o: $S/glb.cu; $N -I$S/.
+$B/glb/imp.o: $S/glb/imp.cu; $N -I$S/. -I$S/glb
 $B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
 $B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
 $B/msg.o: $S/msg.cpp; $X -I$S/.
