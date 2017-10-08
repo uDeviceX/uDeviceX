1b1fb45a9acfeb2b4fd42a190d96de0690d490b5
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 10:39:37 +0200

    split common function to utils

--- src/restrain/dev/main.h
+++ src/restrain/dev/main.h
@@ -1,21 +1,5 @@
-/* restrain drop kernels */
 static __device__ float3 sumv;
 static __device__ int indrop;
-
-static __device__ float3 warpReduceSumf3(float3 v) {
-    for (int offset = warpSize>>1; offset > 0; offset >>= 1) {
-        v.x += __shfl_down(v.x, offset);
-        v.y += __shfl_down(v.y, offset);
-        v.z += __shfl_down(v.z, offset);
-    }
-    return v;
-}
-
-static __device__ int warpReduceSum(int v) {
-    for (int offset = warpSize>>1; offset > 0; offset >>= 1)
-        v += __shfl_down(v, offset);
-    return v;
-}
 
 static __global__ void sum_vel(int color, int n, const Particle *pp, const int *cc) {
     int i, valid, nvalid;
--- src/restrain/imp.cu
+++ src/restrain/imp.cu
@@ -21,6 +21,7 @@
 
 namespace restrain {
 namespace dev {
+#include "dev/util.h"
 #include "dev/main.h"
 }
 #include "imp/main.h"
