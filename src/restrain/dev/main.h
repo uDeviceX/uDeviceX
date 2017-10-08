74614870de56a83dec8be2a642109b869e61f08b
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 14:23:15 +0200

    call goodp

--- src/restrain/dev/main.h
+++ src/restrain/dev/main.h
@@ -1,13 +1,13 @@
 static __global__ void sum(Map m, int n, const Particle *pp) {
     int i, good;
     i = threadIdx.x + blockDim.x * blockIdx.x;
-    good = (i < n) && (m.cc[i] == m.color);
+    good = (i < n) && goodp(m, i);
     sum0(good, pp, i);
 }
 
 static __global__ void shift(Map m, float3 v, int n, /**/ Particle *pp) {
     int i;
     i = threadIdx.x + blockDim.x * blockIdx.x;
-    if (i >= n || m.cc[i] != m.color) return;
+    if (i >= n || !goodp(m, i)) return;
     shift0(i, v, /**/ pp);
 }
