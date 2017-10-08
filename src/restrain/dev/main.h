cfbfda0d552ebc2a90921685537591df53b56260
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 12:46:18 +0200

    add sum0

--- src/restrain/dev/main.h
+++ src/restrain/dev/main.h
@@ -1,17 +1,15 @@
-static __global__ void sum(int color, int n, const Particle *pp, const int *cc) {
-    int i, good, nvalid;
-    i = threadIdx.x + blockDim.x * blockIdx.x;
+static __device__ void sum0(int good, const Particle *pp, int i) {
+    int nvalid;
+    float3 v;
     
-    float3 v = make_float3(0, 0, 0);
-
-    good = (i < n) && (cc[i] == color);
-
     if (good) {
         enum {X, Y, Z};
         const Particle p = pp[i]; 
         v.x = p.v[X];
         v.y = p.v[Y];
         v.z = p.v[Z];
+    } else {
+        v.x = v.y = v.z = 0;
     }
 
     v  = warpReduceSumf3(v);
@@ -23,6 +21,13 @@ static __global__ void sum(int color, int n, const Particle *pp, const int *cc) 
         atomicAdd(&g::v.z, v.z);
         atomicAdd(&g::n, nvalid);
     }
+}
+
+static __global__ void sum(int color, int n, const Particle *pp, const int *cc) {
+    int i, good;
+    i = threadIdx.x + blockDim.x * blockIdx.x;
+    good = (i < n) && (cc[i] == color);
+    sum0(good, pp, i);
 }
 
 static __global__ void shift(int color, float3 v, int n, const int *cc, /**/ Particle *pp) {
