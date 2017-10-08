8059ac144164c28e041d3d24a696ab82d447bbf5
Sergey Litvinov <slitvinov@gmail.com>
2017-10-08 20:07:10 +0200

    deps

--- src/u/hw/make/dep.mk
+++ src/u/hw/make/dep.mk
@@ -1,5 +1,5 @@
 $B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/msg.h
-$B/glb/imp.o: $B/conf.h $S/d/api.h $S/glb/imp.h $S/inc/conf.h $S/mpi/glb.h
+$B/glb/imp.o: $B/conf.h $S/d/api.h $S/glb/imp.h $S/glb/imp/dec.h $S/glb/imp/main.h $S/inc/conf.h $S/mpi/glb.h
 $B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/msg.o: $S/mpi/glb.h $S/msg.h
