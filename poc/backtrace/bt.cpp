70385f9a0538e27801fe4f821006ca415a227ef5
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-13 14:45:30 +0200

    make PRINT_TRACE it a macro

--- poc/backtrace/bt.cpp
+++ poc/backtrace/bt.cpp
@@ -2,27 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Obtain a backtrace and print it to stdout. */
-void print_trace (void) {
-  void *array[10];
-  size_t size;
-  char **strings;
-  size_t i;
+#define PRINT_TRACE() do {                      \
+    void *array[10];                            \
+    size_t size, i;                             \
+    char **strings;                             \
+                                                \
+    size = backtrace (array, 10);               \
+    strings = backtrace_symbols (array, size);  \
+                                                \
+    for (i = 0; i < size; i++)                  \
+        printf ("%s\n", strings[i]);            \
+                                                \
+    free (strings);                             \
+    } while(0)
 
-  size = backtrace (array, 10);
-  strings = backtrace_symbols (array, size);
-
-  printf ("Obtained %zd stack frames.\n", size);
-
-  for (i = 0; i < size; i++)
-     printf ("%s\n", strings[i]);
-
-  free (strings);
-}
 
 /* A dummy function to make the backtrace more interesting. */
 void foo (void) {
-    print_trace();
+    PRINT_TRACE();
 }
 
 void bar (void) {
