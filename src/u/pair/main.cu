230237d3f1bd2dd155b414ea465e3ccf019efaf6
Sergey Litvinov <slitvinov@gmail.com>
2017-10-16 13:25:20 +0200

    add dump

--- src/u/pair/main.cu
+++ src/u/pair/main.cu
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+
 #include <conf.h>
 #include "inc/conf.h"
 
@@ -16,33 +18,48 @@
 #include "forces/use.h"
 #include "forces/imp.h"
 
+typedef forces::Fo Fo;
+typedef forces::Pa Pa;
+
 namespace dev {
-__global__ void main(forces::Pa a, forces::Pa b, float rnd) {
-    forces::Fo f;
+__global__ void main(Pa a, Pa b, float rnd) {
+    Fo f;
     forces::gen(a, b, rnd, /**/ &f);
     printf("%g %g %g\n", f.x, f.y, f.z);
 }
-}
+} /* namespace */
 
 void ini_pa(float x, float y, float z,
             float vx, float vy, float vz,
-            int k, int c, /**/ forces::Pa *a) {
+            int k, int c, /**/ Pa *a) {
     a->x  = x;  a->y  = y;  a->z =  z;
-    a->vx = vx; a->vy = y; a->vz = vz;    
+    a->vx = vx; a->vy = y; a->vz = vz;
     a->kind = k;
     a->color = c;
 }
 
-void pair(forces::Pa a, forces::Pa b, float rnd) {
+void pair(Pa a, Pa b, float rnd) {
     KL(dev::main, (1, 1), (a, b, rnd));
     dSync();
 }
 
+void write_pa(Pa *a) {
+    printf("[ %g %g %g ] [ %g %g %g ] [kc: %d %d]\n",
+           a->x, a->y, a->z, a->vx, a->vy, a->vz, a->kind, a->color);
+}
+
+void read_pa(Pa *a) {
+    scanf("%f %f %f   %f %f %f   %d %d",
+          &a->x, &a->y, &a->z, &a->vx, &a->vy, &a->vz,
+          &a->kind, &a->color);
+    write_pa(a);
+}
+
 void main0() {
-    forces::Pa a, b;
+    Pa a, b;
     float rnd;
-    
-    ini_pa(0,0,0, 0,0,0, SOLID_KIND, BLUE_COLOR, /**/ &a);
+
+    read_pa(&a);
     ini_pa(0.1,0.1,0.1, 0,0,0, SOLID_KIND, BLUE_COLOR, /**/ &b);
     rnd = 0;
     pair(a, b, rnd);
