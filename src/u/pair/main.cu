bcb1e5498780d727aa52b5de21dbfcbfd633055b
Sergey Litvinov <slitvinov@gmail.com>
2017-10-16 13:06:03 +0200

    get particle on device

--- src/u/pair/main.cu
+++ src/u/pair/main.cu
@@ -17,31 +17,34 @@
 #include "forces/imp.h"
 
 namespace dev {
-__global__ void main() {
-    forces::Pa a;
-    forces::Pa b;
+__global__ void main(forces::Pa a, forces::Pa b) {
     forces::Fo f;
     float rnd;
-
-    a.x = a.y = a.z = a.vx = a.vy = a.vz = 0.0;
-    a.kind = SOLID_KIND;
-    a.color = RED_COLOR;    
-
-    b.x = b.y = b.z = b.vx = b.vy = b.vz = 0.0;
-    b.kind = SOLID_KIND;
-    a.color = RED_COLOR;
-
     rnd = 0.0;
-
     forces::gen(a, b, rnd, /**/ &f);
     printf("fo: %g %g %g\n", f.x, f.y, f.z);
 }
 
 }
 
+void ini_pa(float x, float y, float z,
+           float vx, float vy, float vz,
+           int k, int c, /**/ forces::Pa *a) {
+    a->x  = x;  a->y  = y;  a->z =  z;
+    a->vx = vx; a->vy = y; a->vz = vz;    
+    a->kind = k;
+    a->color = c;
+}
+
 int main(int argc, char **argv) {
     m::ini(argc, argv);
-    KL(dev::main, (1, 1), ());
+    
+    forces::Pa a;
+    forces::Pa b;
+    ini_pa(0,0,0, 0,0,0, SOLID_KIND, BLUE_COLOR, /**/ &a);
+    ini_pa(0.1,0.1,0.1, 0,0,0, SOLID_KIND, BLUE_COLOR, /**/ &b);
+    
+    KL(dev::main, (1, 1), (a, b));
     dSync();
     m::fin();
 }
