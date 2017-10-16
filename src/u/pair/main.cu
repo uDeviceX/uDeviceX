11c9a012cbedd717cd894ea660589aaa630fb3fd
Sergey Litvinov <slitvinov@gmail.com>
2017-10-16 13:15:52 +0200

    minor

--- src/u/pair/main.cu
+++ src/u/pair/main.cu
@@ -20,29 +20,36 @@ namespace dev {
 __global__ void main(forces::Pa a, forces::Pa b, float rnd) {
     forces::Fo f;
     forces::gen(a, b, rnd, /**/ &f);
-    printf("fo: %g %g %g\n", f.x, f.y, f.z);
+    printf("%g %g %g\n", f.x, f.y, f.z);
 }
 }
 
 void ini_pa(float x, float y, float z,
-           float vx, float vy, float vz,
-           int k, int c, /**/ forces::Pa *a) {
+            float vx, float vy, float vz,
+            int k, int c, /**/ forces::Pa *a) {
     a->x  = x;  a->y  = y;  a->z =  z;
     a->vx = vx; a->vy = y; a->vz = vz;    
     a->kind = k;
     a->color = c;
 }
 
-int main(int argc, char **argv) {
+void pair(forces::Pa a, forces::Pa b, float rnd) {
+    KL(dev::main, (1, 1), (a, b, rnd));
+    dSync();
+}
+
+void main0() {
     forces::Pa a, b;
     float rnd;
     
-    m::ini(argc, argv);
     ini_pa(0,0,0, 0,0,0, SOLID_KIND, BLUE_COLOR, /**/ &a);
     ini_pa(0.1,0.1,0.1, 0,0,0, SOLID_KIND, BLUE_COLOR, /**/ &b);
     rnd = 0;
-    
-    KL(dev::main, (1, 1), (a, b, rnd));
-    dSync();
+    pair(a, b, rnd);
+}
+
+int main(int argc, char **argv) {
+    m::ini(argc, argv);
+    main0();
     m::fin();
 }
