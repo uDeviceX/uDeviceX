c1e394e3c3463755ac9807cf1281b3ffce943b15
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-17 17:56:31 +0200

    rm unused functions

--- src/fsi/dev/common.h
+++ src/fsi/dev/common.h
@@ -1,5 +1,3 @@
-static __device__ float fst(float2 p) { return p.x; }
-static __device__ float scn(float2 p) { return p.y; }
 static __device__ float random(uint lid, uint rid, float seed) {
     return rnd::mean0var1uu(seed, lid, rid);
 }
