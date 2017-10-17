2d514c7b4749d5674208c6899b13f3a81caa4cd9
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-17 09:41:21 +0200

    include ini_props

--- src/generate/rig/imp.cu
+++ src/generate/rig/imp.cu
@@ -4,8 +4,11 @@
 #include "inc/type.h"
 #include "rig/imp.h"
 
+#include "math/linal.h"
+#include "mesh/props.h"
+
 #include "imp.h"
 
 namespace gen {
-
+#include "imp/ini_props.h"
 }
---
+++ src/generate/rig/imp.h
@@ -0,0 +1,1 @@
+void gen_rig_quants(/* io */ int *on, Particle *opp, /**/ rig::Quants *q);
