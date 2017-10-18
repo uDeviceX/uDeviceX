f9e66959e899aeb5cec8ffda6af02585c22a257d
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-18 11:17:02 +0200

    add helper types

--- src/clist/imp.h
+++ src/clist/imp.h
@@ -1,5 +1,11 @@
 namespace clist {
 
+enum {MAXA=2};                                /* maximum number of array */
+typedef Sarray<uchar4*, MAXA> uchar4pA;       /* uchar4 pointers array               */
+typedef Sarray<int, MAXA> intA;               /* int array                           */
+typedef Sarray<Particle *, MAXA> ParticlepA;  /* particle pointers array             */
+typedef Sarray<int *, MAXA> intpA;            /* particle pointers array             */ 
+
 struct Clist {
     int3 dims;
     int ncells;
