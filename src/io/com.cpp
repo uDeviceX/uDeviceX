178490072d61a2c5d8c9e6f1bc89a3ab15b6d3ac
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-19 10:01:54 +0200

    add swrite

--- src/io/com.cpp
+++ src/io/com.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <vector_types.h>
 #include <mpi.h>
@@ -11,8 +12,25 @@
 // #include "utils/mc.h"
 
 #include "utils/os.h"
+#include "utils/error.h"
 
 #include "msg.h"
+
+enum {MAX_CHAR_PER_LINE = 128};
+
+static int swrite(int n, const int *ii, const float3 *rr, /**/ char *s) {
+    int i, id, c, start;
+    float3 r;
+    for (i = start = 0; i < n; ++i) {
+        id = ii[i];
+        r  = rr[i];
+        c = sprintf(s + start, "%d %g %g %g\n", id, r.x, r.y, r.z);
+        if (c >= MAX_CHAR_PER_LINE)
+            signal_error_extra("buffer too small : %d / %d", c, MAX_CHAR_PER_LINE);
+        start += c;
+    }
+    return start;
+}
 
 static void write(int n, const int *ii, const float3 *rr, /**/ FILE *f) {
     int i, id;
