e7f671cf481431b5620df364a0986b23fc000a74
Sergey Litvinov <slitvinov@gmail.com>
2017-10-19 12:05:34 +0200

    initial version of cudaDeviceProp

--- poc/device.prop/main.cu
+++ poc/device.prop/main.cu
@@ -1,2 +1,18 @@
+#include <stdio.h>
+
+void GetDeviceProperties(struct cudaDeviceProp *prop) {
+    cudaError_t e;
+    int device;
+    device = 0;
+    e = cudaGetDeviceProperties (prop, device);
+    if (e != cudaSuccess) {
+        fprintf(stderr, "GetDeviceProperties failed\n");
+        exit(2);
+    }
+}
+
 int main() {
+    cudaDeviceProp p;
+    GetDeviceProperties(&p);
+    printf("totalGlobalMem = %ld\n", p.totalGlobalMem);
 }
