7de8b3bd9ac7d9cf4a897ead7bb764992f0834e5
Sergey Litvinov <slitvinov@gmail.com>
2017-10-19 16:18:20 +0200

    use MPI_Comm_dup

--- poc/mpi/dup/main.c
+++ poc/mpi/dup/main.c
@@ -12,29 +12,31 @@ MPI_Status status[123];
 
 #define TAG 0
 #define COMM MPI_COMM_WORLD
-MPI_Comm comm;
 
-void send() {
+void send(MPI_Comm comm) {
   int dest = RECV;
   int buf[] = {42};
-  MPI_Send(buf, send_cnt, MPI_INT, dest, TAG, COMM);
+  MPI_Send(buf, send_cnt, MPI_INT, dest, TAG, comm);
 }
 
 
-void recv() {
+void recv(MPI_Comm comm) {
   int dest = SEND;
   int buf[123];
-  MPI_Recv(buf, recv_cnt, MPI_INT, dest, TAG, COMM, status);
+  MPI_Recv(buf, recv_cnt, MPI_INT, dest, TAG, comm, /**/ status);
   printf("recv: %d\n", buf[0]);
 }
 
 
 int main(int argc, char *argv[]) {
+  MPI_Comm comm;
+    
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(COMM, &rank);
+  MPI_Comm_dup(COMM, &comm);
 
-  if (rank == SEND) send();
-  else              recv();
+  if (rank == SEND) send(comm);
+  else              recv(comm);
 
   MPI_Finalize();
   return 0;
