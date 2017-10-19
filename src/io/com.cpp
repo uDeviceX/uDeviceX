a81d0b65d0a03e9926a9209bc6e8cd05a0bb7729
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-19 12:04:12 +0200

    shift com to global coordinates

--- src/io/com.cpp
+++ src/io/com.cpp
@@ -18,12 +18,19 @@
 
 enum {MAX_CHAR_PER_LINE = 128};
 
+static void shift(float3 *r) {
+    r->x = m::x2g(r->x);
+    r->y = m::y2g(r->y);
+    r->z = m::z2g(r->z);
+}
+
 static int swrite(int n, const int *ii, const float3 *rr, /**/ char *s) {
     int i, id, c, start;
     float3 r;
     for (i = start = 0; i < n; ++i) {
         id = ii[i];
         r  = rr[i];
+        shift(/**/ &r);
         c = sprintf(s + start, "%d %g %g %g\n", id, r.x, r.y, r.z);
         if (c >= MAX_CHAR_PER_LINE)
             signal_error_extra("buffer too small : %d / %d", c, MAX_CHAR_PER_LINE);
