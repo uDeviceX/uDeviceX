4a69c2edba8fbc133d4d1ae677e3ae1a0ef22a7b
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-19 10:10:19 +0200

    add com mpi_io

--- src/io/com.cpp
+++ src/io/com.cpp
@@ -9,7 +9,7 @@
 
 #include "mpi/wrapper.h"
 #include "mpi/glb.h"
-// #include "utils/mc.h"
+#include "utils/mc.h"
 
 #include "utils/os.h"
 #include "utils/error.h"
@@ -32,27 +32,34 @@ static int swrite(int n, const int *ii, const float3 *rr, /**/ char *s) {
     return start;
 }
 
-static void write(int n, const int *ii, const float3 *rr, /**/ FILE *f) {
-    int i, id;
-    float3 r;
-    for (i = 0; i < n; ++i) {
-        id = ii[i];
-        r  = rr[i];
-        fprintf(f, "%d %g %g %g\n", id, r.x, r.y, r.z);
-    }
+static void write_mpi(const char *fname, long n, const char *data) {
+    MPI_File f;
+    MPI_Status status;
+    MPI_Offset base, offset = 0;
+    MPI_Offset len = n * sizeof(char);
+
+    MC(MPI_File_open(m::cart, fname, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f));
+    MC(MPI_File_set_size(f, 0));
+    MC(MPI_File_get_position(f, &base)); 
+
+    MC( MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, m::cart) );
+    MC( MPI_File_write_at_all(f, base + offset, data, n, MPI_CHAR, &status) ); 
+    MC( MPI_File_close(&f) );
 }
 
+
 void dump_com(long id, int n, const int *ii, const float3 *rr) {
-    char fname[256] = {0};
-    FILE *f;
+    char fname[256] = {0}, *data;
+    long nchar = 0;
     
-    if (m::rank == 0) {
-        os::mkdir(DUMP_BASE "/com");
-        sprintf(fname, DUMP_BASE "/com/%04ld.txt", id);
-        f = fopen(fname, "w");
-        write(n, ii, rr, /**/ f);
-        fclose(f);
-    } else {
-        ERR("Not implemented yet\n");
-    }
+    data = (char *) malloc(MAX_CHAR_PER_LINE * n * sizeof(char));
+
+    if (m::rank == 0) os::mkdir(DUMP_BASE "/com");
+
+    sprintf(fname, DUMP_BASE "/com/%04ld.txt", id);
+    
+    UC(nchar = swrite(n, ii, rr, /**/ data));
+    write_mpi(fname, nchar, data);
+    
+    free(data);
 }
