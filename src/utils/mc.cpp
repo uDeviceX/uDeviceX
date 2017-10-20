fec956d69a1c6ccc4e5ea9164d4fe51a6657c12a
Sergey Litvinov <slitvinov@gmail.com>
2017-10-20 18:17:29 +0200

    do not use ERR

--- src/utils/mc.cpp
+++ src/utils/mc.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <assert.h>
 #include <mpi.h>
 
 #include <conf.h>
 #include "inc/conf.h"
 #include "mc.h"
-#include "msg.h"
 
 namespace mpicheck {
 void check(int code, const char *file, int line) {
     char error_string[BUFSIZ];
     int sz;
-    if (code != MPI_SUCCESS) return;
+    if (code == MPI_SUCCESS) return;
+
     MPI_Error_string(code, error_string, /**/ &sz);
-    ERR("%s:%d: %s", file, line, error_string);
+    fprintf(stderr, "%s:%d: %s", file, line, error_string);
 }
 } // mpicheck
