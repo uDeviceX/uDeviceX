2e720ce5e441d4efff3dccbded74964f67b02b7c
Sergey Litvinov <slitvinov@gmail.com>
2017-10-20 17:49:58 +0200

    get a error

--- src/u/mpi/main.cu
+++ src/u/mpi/main.cu
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <conf.h>
+#include <mpi.h>
+#include "inc/conf.h"
 
 #include "msg.h"
 #include "mpi/glb.h"
 #include "glb/imp.h"
 
+#include "utils/mc.h"
+#include "mpi/wrapper.h"
+
 int main(int argc, char **argv) {
     m::ini(argc, argv);
-    MSG("mpi size: %d", m::size);
-    MSG("Hello world!");
+    MC(m::Barrier(-1234));
     m::fin();
 }
