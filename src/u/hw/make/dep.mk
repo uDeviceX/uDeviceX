abeef3eac95a02f535456d179a13444999310f51
Sergey Litvinov <slitvinov@gmail.com>
2017-10-20 16:10:37 +0200

    deps

--- src/u/hw/make/dep.mk
+++ src/u/hw/make/dep.mk
@@ -1,8 +1,8 @@
-$B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/msg.h
-$B/glb/gdot/imp.o: $B/conf.h $S/glb/gdot/imp.h $S/glb/gdot/imp/dupire/common.h $S/glb/gdot/imp/dupire/down.h $S/glb/gdot/imp/dupire/up.h $S/glb/gdot/imp/flat.h $S/inc/conf.h $S/msg.h
-$B/glb/imp.o: $B/conf.h $S/d/api.h $S/glb/gdot/imp.h $S/glb/imp.h $S/glb/imp/dec.h $S/glb/imp/main.h $S/glb/imp/util.h $S/inc/conf.h $S/mpi/glb.h
-$B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
+$B/d/api.o: $S/d/cpu/imp.h $S/inc/conf.h $S/d/common.h $S/d/api.h $S/msg.h $B/conf.h $S/d/cuda/imp.h
+$B/glb/gdot/imp.o: $S/inc/conf.h $S/glb/gdot/imp/flat.h $S/glb/gdot/imp.h $S/msg.h $B/conf.h $S/glb/gdot/imp/dupire/common.h $S/glb/gdot/imp/dupire/down.h $S/glb/gdot/imp/dupire/up.h
+$B/glb/imp.o: $S/inc/conf.h $S/glb/imp/util.h $S/glb/gdot/imp.h $S/glb/imp.h $S/d/api.h $B/conf.h $S/glb/imp/main.h $S/glb/imp/dec.h $S/mpi/glb.h
+$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/msg.o: $S/mpi/glb.h $S/msg.h
-$B/u/hw/main.o: $S/glb/imp.h $S/mpi/glb.h $S/msg.h
-$B/utils/mc.o: $B/conf.h $S/inc/conf.h $S/utils/mc.h
+$B/msg.o: $S/msg.h $S/mpi/glb.h
+$B/u/hw/main.o: $S/glb/imp.h $S/msg.h $S/mpi/glb.h
+$B/utils/mc.o: $S/inc/conf.h $S/utils/mc.h $B/conf.h
