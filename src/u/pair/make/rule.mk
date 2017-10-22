71cba2aadda29a1331a7b30c03c585231f2f3439
Sergey Litvinov <slitvinov@gmail.com>
2017-10-22 20:17:39 +0200

    pair/main is cu again

--- src/u/pair/make/rule.mk
+++ src/u/pair/make/rule.mk
@@ -5,7 +5,7 @@ $B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
 $B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
 $B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
 $B/msg.o: $S/msg.cpp; $X -I$S/.
-$B/u/pair/main.o: $S/u/pair/main.cpp; $X -I$S/u/pair -I$S/.
+$B/u/pair/main.o: $S/u/pair/main.cu; $N -I$S/u/pair -I$S/.
 $B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
 $B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
 $B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
