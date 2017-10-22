cb7ae637f10bfdea18314e3d159644c341193744
Sergey Litvinov <slitvinov@gmail.com>
2017-10-22 16:56:22 +0200

    missing static

--- src/io/field/imp/field.h
+++ src/io/field/imp/field.h
@@ -1,4 +1,4 @@
-void fields(const char * const path2h5,
+static void fields(const char * const path2h5,
             const float * const channeldata[],
             const char * const * const channelnames, const int nchannels) {
     int i;
