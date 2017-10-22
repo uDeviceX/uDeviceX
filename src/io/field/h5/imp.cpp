2b0e917469e84597724f9287154aad05388d0404
Sergey Litvinov <slitvinov@gmail.com>
2017-10-22 17:26:29 +0200

    less includes

--- src/io/field/h5/imp.cpp
+++ src/io/field/h5/imp.cpp
@@ -1,15 +1,10 @@
 #include <hdf5.h>
-#include <math.h>
-#include <stdlib.h>
 
 #include <conf.h>
 #include "inc/conf.h"
 
-#include "utils/os.h"
 #include "mpi/glb.h"
 #include "mpi/wrapper.h"
-#include "utils/mc.h"
-#include "inc/type.h"
 #include "imp.h"
 
 namespace h5 {
