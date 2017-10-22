d60c3586ddd51aa947df3cb4e99359639aede7b8
Sergey Litvinov <slitvinov@gmail.com>
2017-10-22 18:59:39 +0200

    deps

--- src/u/france/make/dep.mk
+++ src/u/france/make/dep.mk
@@ -25,7 +25,7 @@ $B/inter/imp.o: $S/algo/scan/int.h $S/inc/conf.h $S/flu/imp.h $S/inc/type.h $S/m
 $B/io/bop/imp.o: $S/utils/os.h $S/inc/conf.h $S/inc/type.h $S/mpi/wrapper.h $S/io/bop/imp.h $S/inc/def.h $S/mpi/type.h $S/msg.h $S/d/api.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/io/com.o: $S/utils/os.h $S/inc/conf.h $S/utils/error.h $S/mpi/wrapper.h $S/msg.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/io/diag.o: $S/inc/conf.h $S/inc/type.h $S/mpi/wrapper.h $S/msg.h $B/conf.h $S/io/diag.h $S/mpi/glb.h
-$B/io/field/h5/imp.o: $S/utils/os.h $S/inc/conf.h $S/inc/type.h $S/mpi/wrapper.h $S/io/field/h5/imp.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
+$B/io/field/h5/imp.o: $S/inc/conf.h $S/mpi/wrapper.h $S/io/field/h5/imp.h $B/conf.h $S/mpi/glb.h
 $B/io/field/imp.o: $S/io/field/imp/scalar.h $S/utils/os.h $S/inc/conf.h $S/io/field/xmf/imp.h $S/inc/type.h $S/mpi/wrapper.h $S/io/field/imp.h $S/io/field/h5/imp.h $S/io/field/imp/dump.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/io/fields_grid.o: $S/inc/conf.h $S/inc/type.h $S/utils/cc.h $S/io/fields_grid.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/io/field/imp.h $S/io/fields_grid/solvent.h $S/io/fields_grid/all.h
 $B/io/field/xmf/imp.o: $S/inc/conf.h $S/io/field/xmf/imp.h $B/conf.h $S/mpi/glb.h
