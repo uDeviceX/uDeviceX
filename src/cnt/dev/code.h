a7105b105cedc2381bc72a9c1723df2a59d65211
amlucas <lucas.amoudruz@wanadoo.fr>
2017-10-25 18:18:43 +0200

    rm unused function

--- src/cnt/dev/code.h
+++ src/cnt/dev/code.h
@@ -1,12 +1,4 @@
 #define NMAX (256*256*256)
-static __device__ void get(int i, /**/ int *s, int *pid) {
-    /* pid: particle id; s: solute id; i : slot */
-    int c;
-    c    = fetchID(i);
-    *pid = c % NMAX;
-    *s   = c / NMAX;
-}
-
 static __device__ void set(int s, int pid, int i, /**/ int *cells) {
     /* pid: particle id; s: solute id; i : slot */
     assert(pid < NMAX);
