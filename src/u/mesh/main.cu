fdad9e4fcf7bc5e0bf7588de63a44b80cb3be984
Sergey Litvinov <slitvinov@gmail.com>
2017-11-01 23:42:22 +0100

    initial import of mesh unit: a copy of pair

---
+++ src/u/mesh/main.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "mpi/glb.h"
+
+#include "utils/cc.h"
+#include "utils/kl.h"
+#include "inc/def.h"
+#include "inc/dev.h"
+#include "d/api.h"
+
+#include "forces/type.h"
+#include "forces/use.h"
+#include "forces/imp.h"
+
+#include "imp/main.h"
