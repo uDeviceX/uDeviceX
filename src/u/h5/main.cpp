5034c0fa66ef15bc74fae612a4088d1a5b24c86c
Sergey Litvinov <slitvinov@gmail.com>
2017-11-03 09:05:39 +0100

    dump to files with different names

--- src/u/h5/main.cpp
+++ src/u/h5/main.cpp
@@ -7,7 +7,7 @@
 #include "io/field/h5/imp.h"
 #include "io/field/xmf/imp.h"
 
-void dump(int nc) {
+void dump(const char *path, int nc) {
     enum {X, Y, Z};
     int sz;
     float *rho, *u[3];
@@ -20,9 +20,9 @@ void dump(int nc) {
     u[Z] = (float*)malloc(sz);
 
     float *data[] = { rho, u[X], u[Y], u[Z] };
-    h5::write("main.h5", data, names, 4);
+    h5::write(path, data, names, 4);
     free(rho); free(u[X]); free(u[Y]); free(u[Z]);
-    if (m::rank == 0) xmf::write("main.h5", names, 4);
+    if (m::rank == 0) xmf::write(path, names, 4);
 }
 
 int ienv(const char *name, int def) {
@@ -38,10 +38,12 @@ void report(int i, int n) {
 
 void main0(int c, char **v) {
     int n, i;
+    char path[BUFSIZ];
     n = ienv("ndump", 1000);
     for (i = 0; i < n; i++) {
         report(i, n);
-        dump(32 * 32 * 32);
+        sprintf(path, "i.%06d.h5", i);
+        dump(path, 32 * 32 * 32);
     }
 }
 
