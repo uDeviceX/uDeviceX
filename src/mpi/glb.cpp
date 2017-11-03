1f6b001a72b87091fbb412ab6dcc55c86d3bb127
Sergey Litvinov <slitvinov@gmail.com>
2017-11-03 07:30:52 +0100

    isolate set_dims

--- src/mpi/glb.cpp
+++ src/mpi/glb.cpp
@@ -41,10 +41,13 @@ float z2g(float r) { /* local to global */
     return (m::coords[Z] + 0.5)*ZS  + r;
 }
 
-void ini(int argc, char **argv) {
+static void set_dims(int argc, char **argv) {
     int i;
     dims[0] = dims[1] = dims[2] = 1;
     for (i = 1; i < argc && i <= 3; i++) dims[i - 1] = atoi(argv[i]);
+}
+void ini(int argc, char **argv) {
+    set_dims(argc, argv);
 
     MPI_Init(&argc, &argv);
     MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
