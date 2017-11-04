7ed3ecd002875abb8822584ec58eace2fe8cee02
Sergey Litvinov <slitvinov@gmail.com>
2017-11-04 19:56:51 +0100

    add global variables to store cubic roots

--- src/meshbb/imp/find_collisions/log_root1.h
+++ src/meshbb/imp/find_collisions/log_root1.h
@@ -1,7 +1,18 @@
+static void log_cubic_reset() {
+    int n = 0;
+    CC(d::MemcpyToSymbol(&dev::ncubicInfo, &n, sizeof(n)));
+}
+
+static void log_cubic_dump() {
+    dev::CubicInfo cci[MAX_CUBIC_INFO];
+}
+
 void find_collisions(int nm, int nt, int nv, const int4 *tt, const Particle *i_pp, int3 L,
                      const int *starts, const int *counts, const Particle *pp, const Force *ff,
                      /**/ BBdata d) {
+    log_cubic_reset();    
     if (!nm) return;
     KL(dev::find_collisions, (k_cnf(nm * nt)),
        (nm, nt, nv, tt, i_pp, L, starts, counts, pp, ff, /**/ d.ncols, d.datacol, d.idcol));
+    log_cubic_dump();
 }
