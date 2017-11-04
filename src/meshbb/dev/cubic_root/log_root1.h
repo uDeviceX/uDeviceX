3e6c8d4f0e5600003fb80e89e1f6bfd308918ff5
Sergey Litvinov <slitvinov@gmail.com>
2017-11-04 19:27:46 +0100

    decl CubicInfo type

--- src/meshbb/dev/cubic_root/log_root1.h
+++ src/meshbb/dev/cubic_root/log_root1.h
@@ -1,4 +1,10 @@
 namespace dev {
+struct CubicInfo {
+    real a, b, c, d;
+    real h; /* root */
+    bool status;
+};
+
 static __device__ bool cubic_root(real a, real b, real c, real d, /**/ real *h) {
     return cubic_root0(a, b, c, d, /**/ h);
 }
