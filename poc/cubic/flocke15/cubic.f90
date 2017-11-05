2ecf646950f08c916e5d1aa5ed094b2449e88b82
Sergey Litvinov <slitvinov@gmail.com>
2017-11-05 08:39:39 +0100

    wip: cubic.f90

--- poc/cubic/flocke15/cubic.f90
+++ poc/cubic/flocke15/cubic.f90
@@ -3,5 +3,17 @@ program cubic
   use SetWorkingPrecision,      ONLY : wp, spKind
 
   implicit none
+  real (kind = wp    ) :: c2,c1,c0
+  integer :: nReal
+  real (kind = wp) :: root (1:3,1:2)
+
+  c2 = 0.00002
+  c1 = 6.7
+  c0 = 8.9
+  call cubicRoots (c2,c1,c0, nReal, root (1:3,1:2))
+
+  write (*, *) nReal
+  write (*, *) root(1, 1), root(2, 1), root(3, 1)
+  write (*, *) root(1, 2), root(2, 2), root(3, 2)
 
 end program cubic
