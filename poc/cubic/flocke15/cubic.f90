acf197631ac6359bf0782398adf708c6b8e649cb
Sergey Litvinov <slitvinov@gmail.com>
2017-11-05 14:34:43 +0100

    better dump

--- poc/cubic/flocke15/cubic.f90
+++ poc/cubic/flocke15/cubic.f90
@@ -1,19 +1,38 @@
+subroutine nxt_str(s)
+  implicit none
+  integer, save      :: i = 1
+  character(len=256) :: s
+
+  call getarg(i, s)
+  i = i + 1
+end subroutine nxt_str
+
+subroutine nxt_real(x)
+  use SetWorkingPrecision,      only : wp
+  implicit none
+  real (kind = wp  ) :: x
+  character(len=256) :: s
+  call nxt_str(s)
+  read(s, *) x
+end subroutine nxt_real
+
 program cubic
-  use Polynomial234RootSolvers, ONLY : cubicRoots
-  use SetWorkingPrecision,      ONLY : wp, spKind
+  use Polynomial234RootSolvers, only : cubicRoots
+  use SetWorkingPrecision,      only : wp, wpformat
 
   implicit none
-  real (kind = wp    ) :: c2,c1,c0
+
+  real (kind = wp    ) :: c3,c2,c1,c0
   integer :: nReal
-  real (kind = wp) :: root (1:3,1:2)
+  real (kind = wp) :: r (1:3,1:2)
 
-  c2 = 0.00002
-  c1 = 6.7
-  c0 = 8.9
-  call cubicRoots (c2,c1,c0, nReal, root (1:3,1:2))
+  call nxt_real(c3)
+  call nxt_real(c2)
+  call nxt_real(c1)
+  call nxt_real(c0)
+  c2 = c2/c3; c1 = c1/c3; c0 = c0/c3
+  call cubicRoots (c2,c1,c0, nReal, r (1:3,1:2))
 
-  write (*, *) nReal
-  write (*, *) root(1, 1), root(2, 1), root(3, 1)
-  write (*, *) root(1, 2), root(2, 2), root(3, 2)
+  write (*, '(6e23.16)') r(1, 1), r(1, 2), r(2, 1), r(2, 2), r(3, 1), r(3, 2)
 
 end program cubic
