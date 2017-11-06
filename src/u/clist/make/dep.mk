3f3e27feda09a90bb046e4a261198c5bd9e9f9d3
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-06 09:16:57 +0100

    deps

--- src/u/clist/make/dep.mk
+++ src/u/clist/make/dep.mk
@@ -1,14 +1,14 @@
-$B/algo/scan/imp.o: $S/algo/scan/int.h $S/inc/conf.h $S/d/q.h $S/utils/cc.h $S/inc/def.h $S/algo/scan/dev.h $S/algo/scan/cpu/imp.h $S/d/api.h $S/msg.h $S/inc/dev.h $B/conf.h $S/utils/kl.h $S/algo/scan/cuda/imp.h $S/d/ker.h
-$B/clist/imp.o: $S/clist/imp/fin.h $S/clist/code.h $S/algo/scan/int.h $S/inc/conf.h $S/inc/type.h $S/clist/imp.h $S/utils/cc.h $S/inc/def.h $S/clist/dev.h $S/clist/imp/ini.h $S/d/api.h $S/inc/dev.h $B/conf.h $S/clist/imp/main.h $S/utils/kl.h
-$B/d/api.o: $S/d/cpu/imp.h $S/inc/conf.h $S/d/common.h $S/d/api.h $S/msg.h $B/conf.h $S/d/cuda/imp.h
-$B/glb/gdot/imp.o: $S/inc/conf.h $S/glb/gdot/imp/flat.h $S/glb/gdot/imp.h $S/msg.h $B/conf.h $S/glb/gdot/imp/dupire/common.h $S/glb/gdot/imp/dupire/down.h $S/glb/gdot/imp/dupire/up.h
-$B/glb/imp.o: $S/inc/conf.h $S/glb/imp/util.h $S/glb/set.h $S/glb/gdot/imp.h $S/d/api.h $B/conf.h $S/glb/imp/main.h $S/glb/imp/dec.h $S/glb/get.h $S/mpi/glb.h
-$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
+$B/algo/scan/imp.o: $S/algo/scan/cpu/imp.h $S/algo/scan/cuda/imp.h $S/algo/scan/dev.h $S/algo/scan/int.h $B/conf.h $S/d/api.h $S/d/ker.h $S/d/q.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/msg.h $S/utils/cc.h $S/utils/kl.h
+$B/clist/imp.o: $S/algo/scan/int.h $S/clist/code.h $S/clist/dev.h $S/clist/imp.h $S/clist/imp/fin.h $S/clist/imp/ini.h $S/clist/imp/main.h $B/conf.h $S/d/api.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/inc/type.h $S/msg.h $S/utils/cc.h $S/utils/kl.h
+$B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/msg.h
+$B/glb/gdot/imp.o: $B/conf.h $S/glb/gdot/imp.h $S/glb/gdot/imp/dupire/common.h $S/glb/gdot/imp/dupire/down.h $S/glb/gdot/imp/dupire/up.h $S/glb/gdot/imp/flat.h $S/inc/conf.h $S/msg.h
+$B/glb/imp.o: $B/conf.h $S/d/api.h $S/glb/gdot/imp.h $S/glb/get.h $S/glb/imp/dec.h $S/glb/imp/main.h $S/glb/imp/util.h $S/glb/set.h $S/inc/conf.h $S/mpi/glb.h
+$B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/msg.o: $S/msg.h $S/mpi/glb.h
-$B/u/clist/main.o: $S/algo/scan/int.h $S/inc/conf.h $S/inc/type.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $S/msg.h $B/conf.h $S/clist/imp.h
-$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/d/api.h $S/msg.h $B/conf.h
+$B/msg.o: $S/mpi/glb.h $S/msg.h
+$B/u/clist/main.o: $S/algo/scan/int.h $S/clist/imp.h $B/conf.h $S/d/api.h $S/inc/conf.h $S/inc/dev.h $S/inc/type.h $S/msg.h $S/utils/cc.h
+$B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/msg.h $S/utils/cc/common.h
 $B/utils/error.o: $S/msg.h $S/utils/error.h
-$B/utils/mc.o: $S/inc/conf.h $S/utils/mc.h $B/conf.h
+$B/utils/mc.o: $B/conf.h $S/inc/conf.h $S/utils/mc.h
 $B/utils/os.o: $S/utils/os.h
