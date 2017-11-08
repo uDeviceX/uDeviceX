40e5c2984fcb3fb4e9042adacf8b1c582e5e9406
Sergey Litvinov <slitvinov@gmail.com>
2017-11-08 13:20:12 +0100

    seems working version: no tests

--- post/paste/main.cpp
+++ post/paste/main.cpp
@@ -20,7 +20,49 @@ void *emalloc(size_t size) {
     return b;
 }
 
-void main2(BopData *f, BopData *i, void *b) {
+void inc0(void **pp, int sz) { /* advance by number of bytes */
+    char *p;
+    p = (char*)*pp;
+    p += sz;
+    *pp = p;
+}
+void *inc(void **p, int sz) {
+    void *p0;
+    p0 = *p;
+    inc0(p, sz);
+    return p0;
+}
+
+void buffer0(int n,   float *f, int nf,   int *i, int ni, /**/ long *pnw, void *b) {
+    int sz, nw;
+    nw = 0;
+    while (n--) {
+        sz = nf*sizeof(float);
+        memcpy(b, f, sz);
+        inc(&b, sz); nw += sz; f += nf;
+
+        sz = ni*sizeof(int);
+        memcpy(b, i, sz);
+        inc(&b, sz); nw += sz; i += ni;
+    }
+    *pnw = nw;
+}
+
+void buffer(BopData *f, BopData *i, /**/ long *pnw, void *b) {
+    long n;
+    int nf, ni;
+    n = f->n;
+    nf = f->nvars;
+    ni = i->nvars;
+    buffer0(n, f->fdata, nf, i->idata, ni, /**/ pnw, b);
+}
+
+
+void main1(BopData *f, BopData *i, void *b) {
+    long nw;
+    buffer(f, i, /**/ &nw, b);
+    //fprintf(stderr, "nw = %ld\n", nw);
+    fwrite(b, 1, nw, stdout);
 }
 
 void main2(BopData *f, BopData *i) {
@@ -32,7 +74,7 @@ void main2(BopData *f, BopData *i) {
     ni = i->nvars;
     sz = n * (nf*sizeof(float) + ni*sizeof(int));
     b = emalloc(sz);
-    main2(f, i, b);
+    main1(f, i, b);
     free(b);
 }
 
@@ -40,11 +82,9 @@ void main3(BopData *f, BopData *i) {
     assert(f->n     == i->n);
     assert(f->nvars == 6); /* x, y, z, vx, vy, vz */
     assert(f->type == FLOAT);
-    
+
     assert(i->nvars == 1); /* color */
     assert(i->type == INT);
-    summary(f);
-    summary(i);
     main2(f, i);
 }
 
@@ -60,6 +100,6 @@ int main(int argc, char **argv) {
     const char *f, *i; /* [f]loat, [i]neger */
     f = argv[1];
     i = argv[2];
-    fprintf(stderr, "<%s> <%s>\n", f, i);
+    //fprintf(stderr, "<%s> <%s>\n", f, i);
     main4(f, i);
 }
