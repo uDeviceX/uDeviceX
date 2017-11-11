fb5fad62255c9c5e492518dd94a12e1f6cc1cab4
Sergey Litvinov <slitvinov@gmail.com>
2017-11-11 14:45:01 +0100

    rm rbc::com declarations from rbc::main

--- src/rbc/main/imp.h
+++ src/rbc/main/imp.h
@@ -18,26 +18,16 @@ struct TicketT {
     Texo <int4> textri;
 };
 
-struct ComHelper {
-    float3 *drr;  /* helper to compute centers of mass on device */
-    float3 *hrr;  /* centers of mass on host                     */
-};
-
 void ini(Quants *q);
 void fin(Quants *q);
 
 void gen_ticket(const Quants q, TicketT *t);
 void fin_ticket(TicketT *t);
 
-void ini(int maxcells, /**/ ComHelper *com);
-void fin(/**/ ComHelper *com);
-
 void gen_quants(const char *r_templ, const char *r_state, Quants *q);
 void strt_quants(const char *r_templ, const int id, Quants *q);
 void strt_dump(const int id, const Quants q);
 
 void forces(const Quants q, const TicketT t, /**/ Force *ff);
-
-void get_com(int nm, int nv, const Particle *pp, /**/ ComHelper *com);
 
 }} /* namespace */
