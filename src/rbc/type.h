75eda301bbca7bc934bc0a81eecfe999fdd2ec22
Sergey Litvinov <slitvinov@gmail.com>
2017-11-11 15:55:30 +0100

    rbc/type.h is commont for rbc/main and rbc/force

---
+++ src/rbc/type.h
@@ -0,0 +1,11 @@
+struct Quants {
+    int n, nc;             /* number of particles, cells            */
+    Particle *pp, *pp_hst; /* vertices particles on host and device */
+    float *av;             /* area and volume on device             */
+
+    int *ii;               /* global ids (on host) */
+    
+    int nt, nv;            /* number of triangles and vertices per mesh */
+    int *adj0, *adj1;      /* adjacency lists on device                 */
+    int4 *tri, *tri_hst;   /* triangles: vertex indices                 */
+};
