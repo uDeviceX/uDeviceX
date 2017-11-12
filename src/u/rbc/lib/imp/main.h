07e2355dc90f68d8d7ac63d9bb392d6b3db3c6f7
Sergey Litvinov <slitvinov@gmail.com>
2017-11-12 17:02:44 +0100

    dump forces

--- src/u/rbc/lib/imp/main.h
+++ src/u/rbc/lib/imp/main.h
@@ -1,10 +1,42 @@
+static void write0(Particle p, Force f0) {
+    enum {X, Y, Z};
+    float *r, *f;
+    r = p.r;
+    f = f0.f;
+    printf("%g %g %g %g %g %g\n", r[X], r[Y], r[Z], f[X], f[Y], f[Z]);
+}
+
+static void write1(int n, Particle *p, Force *f) {
+    int i;
+    for (i = 0; i < n; i++) write0(p[i], f[i]);
+}
+
+static void write(int n, Particle *p, Force *f) {
+    Particle *p_hst;
+    Force *f_hst;
+
+    p_hst = (Particle*) malloc(n*sizeof(Particle));
+    f_hst = (Force*)    malloc(n*sizeof(Force));
+
+    cD2H(p_hst, p, n);
+    cD2H(f_hst, f, n);
+
+    write1(n, p_hst, f_hst);
+
+    free(p_hst);
+    free(f_hst);
+}
+
 static void run0(rbc::Quants q, rbc::force::TicketT t, Force *f) {
     rbc::force::forces(q, t, /**/ f);
+    write(q.n, q.pp, f);
 }
 
 static void run1(rbc::Quants q, rbc::force::TicketT t) {
     Force *f;
     Dalloc(&f, q.n);
+    Dzero(f, q.n);
+
     run0(q, t, f);
     Dfree(f);
 }
