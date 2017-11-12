a19f78fb3430c2b7ab692bc7e9e2e9ad295c041b
Sergey Litvinov <slitvinov@gmail.com>
2017-11-12 16:30:36 +0100

    dump q.n

--- src/u/rbc/lib/imp/main.h
+++ src/u/rbc/lib/imp/main.h
@@ -1,13 +1,20 @@
-static void run0(const char *cell, const char *ic, rbc::Quants q) {
-    rbc::force::TicketT tt;
+static void run0(rbc::Quants q, rbc::force::TicketT t) {
+    Force *f;
+    MSG("q.n = %d\n", q.n);
+    rbc::force::forces(q, t, /**/ f);
+}
+
+static void run1(const char *cell, const char *ic, rbc::Quants q) {
+    rbc::force::TicketT t;
     rbc::main::gen_quants(cell, ic, /**/ &q);
-    rbc::force::gen_ticket(q, &tt);
-    rbc::force::fin_ticket(&tt);
+    rbc::force::gen_ticket(q, &t);
+    run0(q, t);
+    rbc::force::fin_ticket(&t);
 }
 
 void run(const char *cell, const char *ic) {
     rbc::Quants q;
     rbc::main::ini(&q);
-    run0(cell, ic, q);
+    run1(cell, ic, q);
     rbc::main::fin(&q);
 }
