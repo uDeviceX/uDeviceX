798961490d279b45c9ea98daddf666c40a09c3fb
Sergey Litvinov <slitvinov@gmail.com>
2017-11-12 21:06:50 +0100

    add a dummy implimentation of rbc::stretch

---
+++ src/rbc/stretch/imp.cu
@@ -0,0 +1,8 @@
+#include "inc/type.h"
+
+#include "imp.h"
+namespace rbc { namespace sretch {
+#include "imp/type.h"
+#include "imp/main.h"
+
+}} /* namespace */
---
+++ src/rbc/stretch/imp/main.h
@@ -0,0 +1,10 @@
+void ini(const char* path, int nv, /**/ Fo *f) {
+
+}
+void fin(Fo *f) {
+
+}
+
+void apply(int nm, const Particle*, const Fo*, /**/ Force*) {
+
+}
