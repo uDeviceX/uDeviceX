48ed8438e999584731608928d7cdb67d52d38d5c
Sergey Litvinov <slitvinov@gmail.com>
2017-11-13 12:13:04 +0100

    add an interface to map

--- src/rbc/force/dev/map.h
+++ src/rbc/force/dev/map.h
@@ -1,1 +1,11 @@
-/* helps to find indices for faces and dihedrals */
+/* helps to find indices for triangles and dihedrals */
+
+struct Map {
+    int tri, dih; /* == 0 if not valid */
+    int i0, i1, i2, i3, i4;
+};
+
+void ini_map(int md, int nv, int i, const Texo<int> adj0, const Texo<int> adj1, /**/ Map *m) {
+    assert(md == RBCmd);
+    assert(nv == RBCnv);
+}
