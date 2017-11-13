6dc4adc82ce1b28ad69968103657df03617cd25d
Sergey Litvinov <slitvinov@gmail.com>
2017-11-13 20:05:35 +0100

    less includes in adj/imp.cpp

--- src/rbc/adj/imp.cpp
+++ src/rbc/adj/imp.cpp
@@ -2,28 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <vector_types.h>
-#include <mpi.h>
-
-#include <conf.h>
-#include "inc/conf.h"
-
-#include "inc/def.h"
-#include "inc/type.h"
-#include "inc/dev.h"
 
 #include "msg.h"
-
-#include "d/api.h"
-#include "utils/cc.h"
-
-#include "utils/mc.h"
-#include "mpi/wrapper.h"
-#include "mpi/glb.h"
-
-#include "io/off.h"
-#include "io/restart.h"
-
-#include "rbc/type.h"
 #include "imp.h"
 
 namespace rbc { namespace adj {
