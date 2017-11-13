d5957e9b2d4cbe3406bc1eb875a1deaa26424a1e
Sergey Litvinov <slitvinov@gmail.com>
2017-11-13 20:41:02 +0100

    use not valid

--- src/rbc/main/imp/setup.h
+++ src/rbc/main/imp/setup.h
@@ -11,11 +11,12 @@ static void setup(int md, int nt, int nv, const char *r_templ, /**/ int4 *faces,
     int a1[nv*md], a2[nv*md];
     rbc::adj::ini(md, nt, nv, faces, /**/ a1, a2);
 
-    int i;
+    int i, valid;
     rbc::adj::Map m;
-    for (i = 0; i <= nv * md; i++) {
-        rbc::adj::map(md, i, a1, a2, /**/ &m);
-        MSG("i01234: %d %d %d %d %d", m.i0, m.i1, m.i2, m.i3, m.i4);
+    for (i = 0; i < nv * md; i++) {
+        valid = rbc::adj::map(md, i, a1, a2, /**/ &m);
+        if (valid)
+            MSG("i01234: %d %d %d %d %d", m.i0, m.i1, m.i2, m.i3, m.i4);
     }
 
     cH2D(tri, faces, nt);
