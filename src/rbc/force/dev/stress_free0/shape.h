a5a9e079f2aebdb3ff06973dd36d879f40ee51af
Sergey Litvinov <slitvinov@gmail.com>
2017-11-14 11:01:47 +0100

    do nothing in stress_free0/shape.h

--- src/rbc/force/dev/stress_free0/shape.h
+++ src/rbc/force/dev/stress_free0/shape.h
@@ -1,14 +1,5 @@
 struct Shape0 { /* info for one edge :TODO: */
-    float a, b, c;
-    float A;
 };
 
 /* extract edge specific shape info */
-static void __device__ edg_shape(int i, Shape shape, /**/ Shape0 *shape0) {
-    Edg edg;
-    edg = shape.edg[i];
-    shape0->a = edg.a;
-    shape0->b = edg.b;
-    shape0->c = edg.c;
-    shape0->A = edg.A;
-}
+static void __device__ edg_shape(int i, Shape, /**/ Shape0 *) { }
