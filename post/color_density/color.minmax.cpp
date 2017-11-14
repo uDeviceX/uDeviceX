7f813b689840844cd65032eb3ce3975ab32ba4e0
Lucas Amoudruz (daint) <lucas.amoudruz@wanadoo.fr>
2017-11-14 11:45:42 +0100

    add tuning param

--- post/color_density/color.minmax.cpp
+++ post/color_density/color.minmax.cpp
@@ -7,36 +7,39 @@
 
 struct Args {
     char *bov;
-    float T0, T1;
+    float T0, T1, XT;
 };
 
 static void usg() {
-    fprintf(stderr, "usg: u.color.minmax <T0> <T1> <solvent.bop>\n");
+    fprintf(stderr, "usg: u.color.minmax <XT> <T0> <T1> <solvent.bop>\n");
     exit(1);
 }
 
 static void parse(int argc, char **argv, /**/ Args *a) {
-    if (argc != 4) usg();
+    if (argc != 5) usg();
     int iarg = 1;
+    a->XT  = atof(argv[iarg++]);
     a->T0  = atof(argv[iarg++]);
     a->T1  = atof(argv[iarg++]);
     a->bov =      argv[iarg++];
 }
 
-static void minmax(int nx, int ny, const float *d, float T0, float T1, /**/ float *minx, float *maxx) {
+static void minmax(int nx, int ny, const float *d, float T0, float T1, float XT, /**/ float *minx, float *maxx) {
     int ix, iy, i;
     float xmin, xmax;
 
-    xmin = 0;
-    xmax = nx;
+    xmin = nx;
+    xmax = 0;
     
     for (i = iy = 0; iy < ny; ++iy) {
         for (ix = 0; ix < nx; ++ix, ++i) {
             
-            if (ix > xmin && d[i]  > T0) xmin = ix;
-            if (ix < xmax && d[i] <= T1) xmax = ix;
+            if (ix > XT && ix < xmin && d[i] < T0) xmin = ix;
+            if (ix > XT && ix > xmax && d[i] > T1) xmax = ix;
         }
     }
+    if (xmax < xmin) xmax = xmin;
+    
     *minx = xmin;
     *maxx = xmax;
 }
@@ -58,7 +61,7 @@ int main(int argc, char **argv) {
     
     grid = (const float*) bov.data;
 
-    minmax(bov.nx, bov.ny, grid, a.T0, a.T1, /**/ &xmin, &xmax); 
+    minmax(bov.nx, bov.ny, grid, a.T0, a.T1, a.XT, /**/ &xmin, &xmax); 
 
     printf("%g %g\n", xmin, xmax);
     
