a12f2f981113cfb3222ded30ac752d68e76113c8
Sergey Litvinov <slitvinov@gmail.com>
2017-11-14 09:19:54 +0100

    pass shape to a rbc::force

--- src/rbc/force/imp/forces.h
+++ src/rbc/force/imp/forces.h
@@ -1,12 +1,14 @@
-static void apply0(int nc, const Texo<float2> vert, const Texo<int> adj0, const Texo<int> adj1, float* av, /**/ Force *ff){
+static void apply0(int nc, const Texo<float2> vert, const Texo<int> adj0, const Texo<int> adj1, const Shape shape,
+                   float* av, /**/ Force *ff){
     int md, nv;
     md = RBCmd;
     nv = RBCnv;
-    KL(dev::force, (k_cnf(nc*nv*md)), (md, nv, nc, vert, adj0, adj1, av, /**/ (float*)ff));
+    KL(dev::force, (k_cnf(nc*nv*md)), (md, nv, nc, vert, adj0, adj1, shape, av,
+                                       /**/ (float*)ff));
 }
 
 void apply(const Quants q, const TicketT t, /**/ Force *ff) {
     if (q.nc <= 0) return;
     area_volume(q.nc, t.texvert, t.textri, /**/ q.av);
-    apply0(q.nc, t.texvert, t.texadj0, t.texadj1, q.av, /**/ ff);
+    apply0(q.nc, t.texvert, t.texadj0, t.texadj1, q.shape, q.av, /**/ ff);
 }
