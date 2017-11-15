346f19a4408ef7ad2212c2bc1ad5707a42b8fcd7
Sergey Litvinov <slitvinov@gmail.com>
2017-11-15 13:09:46 +0100

    deps; add assert to rbc::rnd::get()

--- src/rbc/rnd/dev/get.h
+++ src/rbc/rnd/dev/get.h
@@ -1,4 +1,5 @@
 namespace rbc { namespace rnd {
 static __device__ float get(const D *d, int i) {
+    assert(i < D->max);
     return D.r[i];
 }} /* namespace */
--- src/u/rbc/rnd/make/dep.mk
+++ src/u/rbc/rnd/make/dep.mk
@@ -3,7 +3,7 @@ $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/msg.o: $S/msg.h $S/mpi/glb.h
-$B/rbc/rnd/imp.o: $S/inc/conf.h $S/rbc/rnd/imp.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $S/msg.h $B/conf.h $S/rbc/rnd/imp/main.h $S/rbc/rnd/type.h $S/rbc/rnd/imp/macro.h $S/mpi/glb.h
+$B/rbc/rnd/imp.o: $S/utils/os.h $S/inc/conf.h $S/rbc/rnd/imp.h $S/rbc/rnd/imp/seed.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $S/msg.h $B/conf.h $S/rbc/rnd/imp/main.h $S/rbc/rnd/type.h $S/rbc/rnd/imp/macro.h $S/mpi/glb.h
 $B/u/rbc/rnd/main.o: $S/inc/conf.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $S/msg.h $B/conf.h $S/rbc/rnd/imp.h $S/mpi/glb.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/d/api.h $S/msg.h $B/conf.h
 $B/utils/error.o: $S/msg.h $S/utils/error.h
