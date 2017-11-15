d9ebfc28c3471dd43e21e3ab95c494ade5466e46
Sergey Litvinov <slitvinov@gmail.com>
2017-11-15 10:44:12 +0100

    initial import of u/rbc/rnd

---
+++ src/u/rbc/rnd/make/dep.mk
@@ -0,0 +1,9 @@
+$B/d/api.o: $S/d/cpu/imp.h $S/inc/conf.h $S/d/common.h $S/d/api.h $S/msg.h $B/conf.h $S/d/cuda/imp.h
+$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
+$B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
+$B/mpi/wrapper.o: $S/mpi/wrapper.h
+$B/u/rbc/rnd/main.o: $S/msg.h $S/mpi/glb.h
+$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/d/api.h $S/msg.h $B/conf.h
+$B/utils/error.o: $S/msg.h $S/utils/error.h
+$B/utils/mc.o: $S/inc/conf.h $S/utils/mc.h $B/conf.h
+$B/utils/os.o: $S/utils/os.h
---
+++ src/u/rbc/rnd/make/obj.mk
@@ -0,0 +1,2 @@
+O = $B/d/api.o $B/mpi/glb.o $B/mpi/type.o $B/mpi/wrapper.o $B/u/rbc/rnd/main.o \
+    $B/utils/cc.o $B/utils/error.o $B/utils/mc.o $B/utils/os.o
