6c2f3a5148af404276483488409c0a08ab698783
Sergey Litvinov <slitvinov@gmail.com>
2017-11-15 21:14:34 +0100

    include gaussrand

--- src/rbc/rnd/api/imp.cpp
+++ src/rbc/rnd/api/imp.cpp
@@ -12,6 +12,7 @@ namespace rbc { namespace rnd { namespace api {
 #if   defined(DEV_CUDA)
   #include "imp/cuda.h"
 #elif defined(DEV_CPU)
+  #include "imp/gaussrand.h"
   #include "imp/cpu.h"
 #else
   #error DEV_* is undefined
