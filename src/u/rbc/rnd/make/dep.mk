28688d43bc318bad70d89cfdba7b0762660753cf
Sergey Litvinov <slitvinov@gmail.com>
2017-11-15 10:46:28 +0100

    more deps

--- src/u/rbc/rnd/make/dep.mk
+++ src/u/rbc/rnd/make/dep.mk
@@ -2,6 +2,7 @@ $B/d/api.o: $S/d/cpu/imp.h $S/inc/conf.h $S/d/common.h $S/d/api.h $S/msg.h $B/co
 $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
+$B/msg.o: $S/msg.h $S/mpi/glb.h
 $B/u/rbc/rnd/main.o: $S/msg.h $S/mpi/glb.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/d/api.h $S/msg.h $B/conf.h
 $B/utils/error.o: $S/msg.h $S/utils/error.h
