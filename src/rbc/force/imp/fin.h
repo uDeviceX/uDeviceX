69f369f1fa0d452d7c9b138d3eb0132bece67dd5
Sergey Litvinov <slitvinov@gmail.com>
2017-11-16 04:50:13 +0100

    fix rnd ini bug

--- src/rbc/force/imp/fin.h
+++ src/rbc/force/imp/fin.h
@@ -1,7 +1,11 @@
+void fin_rnd(rbc::rnd::D *rnd) {
+    rbc::rnd::fin(rnd);
+}
+
 void fin_ticket(TicketT *t) {
     destroy(&t->textri);
     destroy(&t->texadj0);
     destroy(&t->texadj1);
     destroy(&t->texvert);
-    if (RBC_RND) rbc::rnd::fin(t->rnd);
+    if (RBC_RND) fin_rnd(t->rnd);
 }
