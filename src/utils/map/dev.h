5a022054197e0ca431993c476b9f37556b40287f
Sergey Litvinov <slitvinov@gmail.com>
2017-11-17 17:56:14 +0100

    initial import of map/dev.h

---
+++ src/utils/map/dev.h
@@ -0,0 +1,19 @@
+namespace map {
+
+__device__ void ini(Frag frag, float x, float y, float z, /**/ M* m) {
+
+}
+
+__device__ int fst(/**/ M* m) {
+
+}
+
+int nxt(int i, /**/ M* m) {
+
+}
+
+int endp(int i, M* m) {
+
+}
+
+} /* namespace */
