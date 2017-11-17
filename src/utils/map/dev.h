02964da21e04652bf3f0e6d719249d4e376fc633
Sergey Litvinov <slitvinov@gmail.com>
2017-11-17 18:59:10 +0100

    wip: map

--- src/utils/map/dev.h
+++ src/utils/map/dev.h
@@ -1,21 +1,31 @@
 namespace map {
 
-struct Frag;
-struct M;
+struct M {
+    const hforces::Frag *frag;
+    int dx, dy, dz;
+    int x, y, z; /* center */
+    int i;
+    int end;
+};
 
-__device__ void ini(Frag *frag, float x, float y, float z, /**/ M* m) {
+__device__ void ini(hforces::Frag *frag, float x, float y, float z, /**/ M *m) {
+    m->frag = frag;
+    m->x = (int)(x + XS/2);
+    m->y = (int)(y + YS/2);
+    m->z = (int)(y + YS/2);
+    m->dx = m->dy = m->dz = -1;
+    m->i = -1;
 }
 
-__device__ int fst(/**/ M* m) {
-    return 0;
+__device__ int nxt(/**/ M* m) {
+    if (m->i == -1) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
-int nxt(int i, /**/ M* m) {
-    return 0;
-
-}
-
-int endp(int i, M* m) {
+__device__ int endp(int i, M *m) {
     return 0;
 }
 
