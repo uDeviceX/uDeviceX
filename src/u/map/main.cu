4e1eb4c0c80c2d51cb284d2b1656ddbe9f48ae7e
Sergey Litvinov <slitvinov@gmail.com>
2017-11-17 18:03:32 +0100

    add dev part to u/map; upd deps

--- src/u/map/main.cu
+++ src/u/map/main.cu
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <conf.h>
+#include "inc/conf.h"
 
 #include "msg.h"
 #include "mpi/glb.h"
 
+#include "utils/kl.h"
+
+namespace dev {
+#include "dev.h"
+}
+
+void main0() { KL(dev::main, (1, 1), ()); }
+
 int main(int argc, char **argv) {
     m::ini(argc, argv);
     MSG("mpi size: %d", m::size);
+    main0();
     MSG("Hello world!");
     m::fin();
 }
