93cc928f32bdfe9134097b5c4347500f30e672cc
Sergey Litvinov <slitvinov@gmail.com>
2017-11-18 18:23:33 +0100

    rm debug output

--- src/rbc/force/dev/rnd1/main.h
+++ src/rbc/force/dev/rnd1/main.h
@@ -5,6 +5,5 @@ static __device__ void edg_rnd(float *rnd, int i, /**/ Rnd0 *rnd0)  {
 }
 
 static __device__ float3 frnd(float3, float3, const Rnd0 rnd) {
-    printf("rnd: %g\n", rnd);
     return make_float3(0, 0, 0);
 }
