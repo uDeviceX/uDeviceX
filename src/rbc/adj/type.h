4e541a74a9f04e7a56f21447f570ded8c8e0ce7c
Sergey Litvinov <slitvinov@gmail.com>
2017-11-18 20:51:05 +0100

    decl AdjHst

--- src/rbc/adj/type.h
+++ src/rbc/adj/type.h
@@ -1,7 +1,12 @@
 namespace rbc { namespace adj {
-struct Map {
+struct Map { /* one edge info */
     int i0, i1, i2, i3, i4;
     int rbc; /* cell id */
+};
+
+struct AdjHst { /* adjacency lists */
+    int *adj0;
+    int *adj1;
 };
 
 }} /* namespace */
