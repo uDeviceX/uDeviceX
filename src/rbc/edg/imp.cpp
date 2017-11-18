d5fec054359d98db4c72241914c4b27d64d64812
Sergey Litvinov <slitvinov@gmail.com>
2017-11-18 22:41:45 +0100

    fix error

--- src/rbc/edg/imp.cpp
+++ src/rbc/edg/imp.cpp
@@ -10,18 +10,18 @@ void ini(int md, int nv, /**/ int *hx) {
 }
 
 void set(int md, int f, int x, int y,  /**/ int *hx, int *hy) {
-    int j = f*md;
+    int d, j;
+    j = f*md; d = 0;
     while (hx[j] != -1) {
-        j++;
-        if (j >= md) ERR("invalid edg::set call: %d/%d/%d/%d/%d", md, f, x, y, j);
+        j++; d++;
+        if (d >= md) ERR("invalid edg::set call: %d/%d/%d/%d/%d", md, f, x, y, j);
     }
     hx[j] = x; hy[j] = y;
 }
 
 int get(int md, int i, int x, int *hx, int *hy) { /* next */
     int d;
-    d = 0;
-    i *= md;
+    i *= md; d = 0;
     while (hx[i] != x) {
         i++; d++;
         if (d >= md) ERR("invalid edg::get call: %d/%d/%d", md, i, x);
