1cad9968b8e216407493718c0be6e15f5871d96d
Sergey Litvinov <slitvinov@gmail.com>
2017-11-18 22:21:59 +0100

    initial import of edg

---
+++ src/rbc/edj/README.md
@@ -0,0 +1,6 @@
+# edge
+
+set and get `val` on edges
+
+* `md` maximum degree
+* `h[xy]` storage of size `nv*md` where `nv` is a number of vertices
---
+++ src/rbc/edj/imp.h
@@ -0,0 +1,4 @@
+namespace edg {
+void set(int md, int f, int x, int y,  /**/ int *hx, int *hy);
+int  get(int md, int i, int x, int *hx, int *hy);
+}
