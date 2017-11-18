7f9f7a9599f148cffaa8f2e54352e40f225dad2b
Sergey Litvinov <slitvinov@gmail.com>
2017-11-19 00:50:46 +0100

    fetch one number

--- src/rbc/force/dev/rnd1/main.h
+++ src/rbc/force/dev/rnd1/main.h
@@ -1,18 +1,14 @@
 struct Rnd0 { float r; };
 
-static __device__ void edg_rnd(Shape shape, int i0, float* rnd, int  j0, /**/ Rnd0 *rnd0) {
-    /* i: edge index */
+static __device__ void edg_rnd(Shape shape, int i0, float* rnd, int  j, /**/ Rnd0 *rnd0) {
+    /* i0: edge index; j: vertex index */
     assert(i0  < RBCmd * RBCnv);
-    assert(j0 < MAX_CELL_NUM * RBCnv);
-    int i1, j1;
-    float r0, r1;
-    
+    assert(j < MAX_CELL_NUM * RBCnv);
+    int i1;
     i1 = shape.anti[i0];
-    j1 = j0 - i0 + i1;
-    r0 = rnd[j0]; r1 = rnd[j1];
-    rnd0->r = (r0 + r1)/2;
-    
-    assert(j1 < MAX_CELL_NUM * RBCnv);    
+    if (i1 > i0) j = j - i0 + i1;
+    assert(j < MAX_CELL_NUM * RBCnv);
+    rnd0->r = rnd[j];
 }
 
 static __device__ float  frnd0(float rnd) {
