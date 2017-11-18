9503b64cc1dfce3fcc3229e7f7adb37a8c01ae08
Sergey Litvinov <slitvinov@gmail.com>
2017-11-18 22:26:51 +0100

    add ini()

--- src/rbc/edj/README.md
+++ src/rbc/edj/README.md
@@ -3,4 +3,5 @@
 set and get `val` on edges
 
 * `md` maximum degree
-* `h[xy]` storage of size `nv*md` where `nv` is a number of vertices
+* `md` number of vertices
+* `h[xy]` storage of size `nv*md`
--- src/rbc/edj/imp.cu
+++ src/rbc/edj/imp.cu
@@ -1,4 +1,10 @@
 namespace edg {
+void ini(int md, int nv, /**/ int *hx) {
+    int n, i;
+    n = md * nv;
+    for (i = 0; i < n; i ++) hx[i] = -1;
+}
+
 void set(int md, int f, int x, int y,  /**/ int *hx, int *hy) {
     int j = f*md;
     while (hx[j] != -1) j++;
