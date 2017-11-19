c495357274e16355cc1c0bd70320c7a3f1adf2ec
Sergey Litvinov <slitvinov@gmail.com>
2017-11-19 21:27:28 +0100

    deps again

--- src/u/comm/oc/make/rule.mk
+++ src/u/comm/oc/make/rule.mk
@@ -1,8 +1,12 @@
+$B/comm/imp.o: $S/comm/imp.cu; $N -I$S/comm -I$S/.
 $B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
-$B/glb/imp.o: $S/glb/imp.cu; $N -I$S/glb -I$S/.
-$B/glb/wvel/imp.o: $S/glb/wvel/imp.cu; $N -I$S/glb/wvel -I$S/.
+$B/frag/imp.o: $S/frag/imp.cpp; $X -I$S/frag -I$S/.
 $B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
 $B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
 $B/msg.o: $S/msg.cpp; $X -I$S/.
-$B/u/comm/oc/main.o: $S/u/comm/oc/main.cpp; $X -I$S/.
+$B/u/comm/oc/main.o: $S/u/comm/oc/main.cpp; $X -I$S/u/comm/oc -I$S/.
+$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
 $B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
+$B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
