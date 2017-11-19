90958400db1be9780db6402e2bb738ed8a52e25f
Sergey Litvinov <slitvinov@gmail.com>
2017-11-19 21:16:42 +0100

    initial import of oc

--- src/comm/README.md
+++ src/comm/README.md
@@ -1,3 +1,9 @@
 # Comm
 
-Generic communication
+## intro
+
+Generic MPI communication
+
+## OC
+
+OC: c[o]mm [c]heck. A macro to handle comm errors.
---
+++ src/comm/oc/imp.h
@@ -0,0 +1,6 @@
+#define OC(F)                                   \
+    do {                                        \
+        comm::oc::before(__FILE__, __LINE__);   \
+        if ((F) != 0) comm::oc:error();         \
+        comm::oc::after();                      \
+    } while (0)
