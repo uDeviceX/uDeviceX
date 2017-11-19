81da9349bb6aa4ea3e4d8bf9326724f762cc9913
Sergey Litvinov <slitvinov@gmail.com>
2017-11-19 21:33:07 +0100

    upd interface

--- src/comm/oc/imp.h
+++ src/comm/oc/imp.h
@@ -1,9 +1,14 @@
 #define OC(F)                                   \
     do {                                        \
         comm::oc::before(__FILE__, __LINE__);   \
-        if ((F) != 0) comm::oc:error();         \
+        if (comm::oc::error(F))                 \
+            comm::oc::report();                 \
         comm::oc::after();                      \
     } while (0)
 
 namespace comm { namespace oc {
+void before(const char, int);
+void error(int);
+void report();
+void after();
 } } /* namespace */
