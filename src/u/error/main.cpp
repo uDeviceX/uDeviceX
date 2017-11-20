683cd5cb5d9ae461bc042ae160834370021dd7e1
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-20 16:51:49 +0100

    unit error from hw

---
+++ src/u/error/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "msg.h"
+#include "mpi/glb.h"
+
+int main(int argc, char **argv) {
+    m::ini(argc, argv);
+    MSG("mpi size: %d", m::size);
+    MSG("Hello world!");
+    m::fin();
+}
---
+++ src/u/error/make/obj.mk
@@ -0,0 +1,3 @@
+O = $B/comm/imp.o $B/comm/oc/imp.o $B/d/api.o $B/frag/imp.o $B/mpi/glb.o $B/mpi/type.o \
+    $B/mpi/wrapper.o $B/msg.o $B/u/hw/hst/main.o $B/utils/cc.o $B/utils/error.o \
+    $B/utils/halloc.o $B/utils/mc.o $B/utils/os.o
