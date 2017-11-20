121974e04213c054dda285f1a1d0ce78c28104f6
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-20 15:41:08 +0100

    call before and after

--- src/utils/error.h
+++ src/utils/error.h
@@ -1,18 +1,17 @@
 #define signal_error() UdxError::signal(__FILE__, __LINE__)
 #define signal_error_extra(fmt, ...) UdxError::signal_extra(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
-#define report_error() UdxError::report(__FILE__, __LINE__)
-
-
 /* udx check */
 #define UC(f) do {                              \
-    f;                                          \
-    report_error();                             \
+        UdxError::before(__LINE__, __FILE__);   \
+        f;                                      \
+        UdxError::report(__FILE__, __LINE__);   \
+        UdxError::after();                      \
     } while (0)
 
 
 namespace UdxError {
-void before(int line, const char *file);
+void before(const char *file, int line);
 void after();
 
 void signal(const char *file, int line); 
