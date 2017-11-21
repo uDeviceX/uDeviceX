e0d0a6348aeb325c004a046102af77ecf3f4dcae
Sergey Litvinov <slitvinov@gmail.com>
2017-11-21 15:52:08 +0100

    rm unfinished imp of map; deps

--- src/u/map/make/dir.mk
+++
@@ -1,8 +0,0 @@
-D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/d       && \
-    d $B/frag    && \
-    d $B/hforces && \
-    d $B/mpi     && \
-    d $B/rnd     && \
-    d $B/u/map   && \
-    d $B/utils  
