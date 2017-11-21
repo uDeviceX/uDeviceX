621e1c83369ccdaedecef3535b36e6aef3ac2ef2
Sergey Litvinov <slitvinov@gmail.com>
2017-11-21 09:02:28 +0100

    minor

--- src/io/mesh/imp/main.h
+++ src/io/mesh/imp/main.h
@@ -25,8 +25,8 @@ static void write(const void * const ptr, const int nbytes32, MPI_File f) {
     MC(MPI_File_write_at_all(f, base + offset, ptr, nbytes, MPI_CHAR, &status));
     MC(MPI_Allreduce(&nbytes, &ntotal, 1, MPI_OFFSET, MPI_SUM, m::cart) );
     MC(MPI_File_seek(f, ntotal, MPI_SEEK_CUR));
-}
 
+}
 static int reduce(int n0) {
     int n;
     n = 0;
@@ -35,13 +35,11 @@ static int reduce(int n0) {
 }
 /* root predicate */
 static int rootp() { return m::rank == 0; }
-static void write_once(const void * const ptr,
-                       int sz0, MPI_File f) {
+static void write_once(const void * const ptr, int sz0, MPI_File f) {
     int sz;
     sz = (rootp()) ? sz0 : 0;
     write(ptr, sz, f);
 }
-
 static void header(int nc0, int nv, int nt, MPI_File f) {
     int nc; /* total number of cells */
     int sz = 0;
