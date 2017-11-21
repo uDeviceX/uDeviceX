006da5fd3fd8263ce2513286620463c8f3470b37
Sergey Litvinov <slitvinov@gmail.com>
2017-11-21 10:27:59 +0100

    less includes

--- src/io/mesh/write/imp.cpp
+++ src/io/mesh/write/imp.cpp
@@ -1,16 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
-#include <vector_types.h>
 
 #include <conf.h>
 #include "inc/conf.h"
-#include "utils/os.h"
-#include "utils/error.h"
-#include "utils/halloc.h"
+
 #include "mpi/glb.h"
 #include "mpi/wrapper.h"
-#include "inc/type.h"
 #include "utils/mc.h"
 
 #include "imp.h"
