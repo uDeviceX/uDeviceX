5a9525590b92000d8b4fdd0bdd7d3c02c766039a
Sergey Litvinov <slitvinov@gmail.com>
2017-11-21 10:47:30 +0100

    less inc

--- src/io/mesh/imp.cpp
+++ src/io/mesh/imp.cpp
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
 #include <vector_types.h>
 
 #include <conf.h>
 #include "inc/conf.h"
+
 #include "utils/os.h"
 #include "utils/error.h"
 #include "utils/halloc.h"
 #include "mpi/glb.h"
-#include "mpi/wrapper.h"
 #include "inc/type.h"
-#include "utils/mc.h"
 #include "write/imp.h"
 
 #include "imp.h"
