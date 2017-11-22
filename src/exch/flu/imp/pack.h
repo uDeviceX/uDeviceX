9249546b6f8f8c007e9664334ebe4d4421d43119
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-22 09:53:46 +0100

    pack particles

--- src/exch/flu/imp/pack.h
+++ src/exch/flu/imp/pack.h
@@ -1,1 +1,18 @@
 
+void pack(const Cloud *cloud, /**/ Pack *p) {
+    int nc;
+    int26 cc;
+    int27 ss;
+    Pap26 fpp;
+    
+    nc = get_cell_num(/**/ cc.d);
+    scan(NFRAGS, cc.d, /**/ ss.d);
+
+    bag2Sarray(p->dpp, /**/ &fpp);
+    
+    KL( dev::collect_particles,
+        ((nc+1) / 2, 32),
+        (ss, (const Particle*) cloud->pp, p->bss, p->bcc, p->fss, p->cap, /**/ p->bii, fpp, p->counts_dev));
+    
+    
+}
