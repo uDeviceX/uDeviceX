445340e4c4f20d06c349983ef9e468fc6fa0f484
Sergey Litvinov <slitvinov@gmail.com>
2017-11-22 09:34:41 +0100

    no static allocation

--- tools/ply2off.c
+++ tools/ply2off.c
@@ -10,12 +10,10 @@ void usg() {
 #define NVT   3 /* vertices per triangle */
 #define NVMAX 1000000
 #define NVAR  6 /* x, y, z, vx, vy, vz */
-float   buf[NVAR*NVMAX];
-float  ibuf[(NVT+1)*NVMAX];
 FILE* fd;
 
 char line[1024]; /* a line from a file */
-int nv; /* number of vertices */
+int nv, nt; /* number of vertices and triangles */
 
 int commentp() { return strcmp("comment", line) == 0; }
 #define nl() fgets(line, sizeof(line), fd) /* [n]ext [l]ine */
@@ -26,34 +24,54 @@ void header() {
     /* element vertex %nv% */
     sscanf(line, "element vertex %d\n", &nv);
     nl(); nl(); nl(); nl(); nl(); nl(); /* property float [xyzuvw] */
-    nl(); /* element faces */
+    nl(); sscanf(line, "element face %d\n", &nt);
     nl(); /* property list int int vertex_index */
     nl(); /* end_header */
 }
 #undef nl
 
-void vertices() {
-    int iv, ib;
+void verts0(float *buf) {
+    int i, b;
     float x, y, z;
-    fread(buf, NVAR*nv, sizeof(float), fd);
-    for (iv = ib = 0; iv < nv; ++iv) {
-        x = buf[ib++], y = buf[ib++], z = buf[ib++];
+    efread(buf, NVAR*nv, sizeof(float), fd);
+    for (i = b = 0; i < nv; ++i) {
+        x = buf[b++], y = buf[b++], z = buf[b++];
         printf("%g %g %g\n", x, y, z);
-        ib++; ib++; ib++; /* skip vx, vy, vz */
+        b++; b++; b++; /* skip vx, vy, vz */
+     }
+}
+void verts() {
+    float *buf;
+    int sz = nv*NVAR;
+    buf = malloc(nv*sizeof(buf));
+    verts0(buf);
+    free(buf);
+}
+
+void efread(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t r;
+    r = fread(ptr, size, nmemb, stream);
+    if (r == 0) {
+        fprintf(stderr, "ply2off: fails to read: %ld/%ld\n", size, nmemb);
+        exit(2);
     }
 }
 
-void faces() {
-    int iv, ib;
-    float x, y, z;
-    fread(buf, NVAR*nv, sizeof(float), fd);
-    for (iv = ib = 0; iv < nv; ++iv) {
-        x = buf[ib++], y = buf[ib++], z = buf[ib++];
-        printf("%g %g %g\n", x, y, z);
-        ib++; ib++; ib++; /* skip vx, vy, vz */
+void tris0(int *buf) {
+    int i, b;
+    efread(buf, nt*(NVT+1), sizeof(buf[0]), fd);
+    for (i = b = 0; i < nt; ++i) {
+        b++;
+        printf("%d: %d %d %d\n", i, buf[b++], buf[b++], buf[b++]);
     }
 }
-
+void tris() {
+    int *buf;
+    int sz = nt*(NVT+1);
+    buf = malloc(nv*sizeof(buf));
+    tris0(buf);
+    free(buf);
+}
 
 FILE* efopen(const char *p, const char *m) {
     FILE *f;
@@ -68,12 +86,12 @@ FILE* efopen(const char *p, const char *m) {
 void file(const char* fn) {
     fd = efopen(fn, "r");
     header();
-    vertices();
+    verts();
+    tris();
     fclose(fd);
 }
 
 int eq(const char *a, const char *b) { return strcmp(a, b) == 0; }
-
 void help(int c, const char** a) {
     if (c > 1 && eq(a[1], "-h")) usg();
 }
@@ -81,9 +99,6 @@ void help(int c, const char** a) {
 int main(int argc, const char** argv) {
     int i;
     help(argc, argv);
-    for (i = 1; i < argc; i++) {
-        if (i > 1) printf("\n");
-        file(argv[i]);
-    }
+    file(argv[1]);
     return 0;
 }
