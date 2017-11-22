a2999b37e3de3b561dbb34644d460b6f2adbf123
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-22 09:56:12 +0100

    pack colors

--- src/exch/flu/imp/pack.h
+++ src/exch/flu/imp/pack.h
@@ -1,9 +1,9 @@
-
 void pack(const Cloud *cloud, /**/ Pack *p) {
     int nc;
     int26 cc;
     int27 ss;
     Pap26 fpp;
+    intp26 fcc;
     
     nc = get_cell_num(/**/ cc.d);
     scan(NFRAGS, cc.d, /**/ ss.d);
@@ -14,5 +14,12 @@ void pack(const Cloud *cloud, /**/ Pack *p) {
         ((nc+1) / 2, 32),
         (ss, (const Particle*) cloud->pp, p->bss, p->bcc, p->fss, p->cap, /**/ p->bii, fpp, p->counts_dev));
     
-    
+
+    if (multi_solvent) {
+        bag2Sarray(p->dcc, /**/ &fcc);
+
+        KL( dev::collect_colors,
+            ((nc+1) / 2, 32),
+            (ss, cloud->cc, p->bss, p->bcc, p->fss, p->cap, /**/ fcc));        
+    }
 }
