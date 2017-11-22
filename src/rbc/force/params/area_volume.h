fdf8a1be0c65a109440ff11788cfd1a07debe270
Sergey Litvinov <slitvinov@gmail.com>
2017-11-22 07:17:59 +0100

    use a hack for 498

--- src/rbc/force/params/area_volume.h
+++ src/rbc/force/params/area_volume.h
@@ -1,7 +1,17 @@
 #ifndef RBCtotArea
-  #define    RBCtotArea ( 2.3835e-04*(RBCnv)*(RBCnv))
+  #if      RBCnv==498
+    #define RBCtotArea (2.240e-4*(RBCnv)*(RBCnv))
+  #else
+    #define RBCtotArea (2.3835e-04*(RBCnv)*(RBCnv))
+  #endif
 #endif
 
 #ifndef RBCtotVolume
-  #define  RBCtotVolume ( 2.2670e-07*(RBCnv)*(RBCnv)*(RBCnv))
+  #if      RBCnv==498
+    #define RBCtotVolume (2.185e-7*(RBCnv)*(RBCnv)*(RBCnv))
+  #else
+    #define RBCtotVolume (2.2670e-07*(RBCnv)*(RBCnv)*(RBCnv))
+  #endif
 #endif
+
+/* TODO: do not use a hack for 498 */
