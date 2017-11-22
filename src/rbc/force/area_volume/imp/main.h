1726befdbc997c725230edf01f394d0bd502a3f6
Sergey Litvinov <slitvinov@gmail.com>
2017-11-22 13:05:04 +0100

    minor

--- src/rbc/force/area_volume/imp/main.h
+++ src/rbc/force/area_volume/imp/main.h
@@ -11,7 +11,7 @@ void area_volume(int nc, const Texo<float2> texvert, const Texo<int4> textri, /*
 void area_volume_hst(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst) {
     float *dev;
     Dalloc(&dev, 2*nc);
-    area_volume(nc, texvert, textri, dev);
+    area_volume(nc, texvert, textri, /**/ dev);
     cD2H(hst, dev, 2*nc);
     Dfree(dev);
 }
