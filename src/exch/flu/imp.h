537c4e4504f70083f3881849ca248ce5ffd10499
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-22 10:46:15 +0100

    copy functions to interface

--- src/exch/flu/imp.h
+++ src/exch/flu/imp.h
@@ -1,7 +1,11 @@
 namespace exch {
 namespace flu {
 
+void compute_map(const int *start, const int *count, /**/ Pack *p);
+void download_cell_starts(/**/ Pack *p);
 
+void pack(const Cloud *cloud, /**/ Pack *p);
+void download_data(Pack *p);
 
 } // flu
 } // exch
