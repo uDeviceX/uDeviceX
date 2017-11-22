a116ddf90ee2c1a85921f09ff494996561287607
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-22 10:18:34 +0100

    back to previous alloc mode

--- src/exch/flu/imp/ini.h
+++ src/exch/flu/imp/ini.h
@@ -17,9 +17,9 @@ void ini(int maxd, Pack *p) {
     }
     ncs[BULK] = 0;
     
-    UC(ini(PINNED, NONE, sizeof(Particle), cap, /**/ &p->hpp, &p->dpp));
+    UC(ini(PINNED_DEV, NONE, sizeof(Particle), cap, /**/ &p->hpp, &p->dpp));
     if (multi_solvent)
-        UC(ini(PINNED, NONE,  sizeof(int), cap, /**/ &p->hcc, &p->dcc));
+        UC(ini(PINNED_DEV, NONE,  sizeof(int), cap, /**/ &p->hcc, &p->dcc));
 
     UC(ini(PINNED_HST, NONE, sizeof(int), ncs, /**/ &p->hfss, NULL));
 
