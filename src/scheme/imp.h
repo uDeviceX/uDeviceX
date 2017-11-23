42468cc6cd590c19d61e4750ba45700c7158ea82
Sergey Litvinov <slitvinov@gmail.com>
2017-11-23 04:07:16 +0100

    minor

--- src/scheme/imp.h
+++ src/scheme/imp.h
@@ -5,8 +5,8 @@ struct Fparams {float a, b, c;};
 struct QQ {Particle *o, *r;};
 struct NN {int o, r;};
 
-void move(float mass, int n, const Force *ff, Particle *pp);
-void clear_vel(int n, Particle *pp);
-void force(float mass, Fparams fpar, int n, const Particle* pp, /**/ Force* ff);
-void restrain(const int *cc, NN nn, long it, /**/ QQ qq);
+void move(float mass, int n, const Force*, Particle*);
+void clear_vel(int n, Particle*);
+void force(float mass, Fparams, int n, const Particle*, /**/ Force*);
+void restrain(const int *cc, NN, long it, /**/ QQ);
 }
