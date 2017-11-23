f2102a2b4c7d415bcd93b315fedb811bea42ecb5
Sergey Litvinov <slitvinov@gmail.com>
2017-11-23 10:29:13 +0100

    compile FORCE_CONSTANT

--- src/scheme/force/imp/constant.h
+++ src/scheme/force/imp/constant.h
@@ -1,5 +1,5 @@
 void main(float mass, Param par, int n, const Particle *pp, /**/ Force* ff) {
     float ax, ay, az;
-    ax = par.a; bx = par.b; cx = par.c;
+    ax = par.a; ay = par.b; az = par.c;
     KL(dev::main, (k_cnf(n)), (mass, ax, ay, az, n, pp, /**/ ff));
 }
