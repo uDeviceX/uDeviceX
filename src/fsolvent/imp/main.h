585a267b53c1228ddf4f28336a9ac7bb47756b59
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-23 08:53:48 +0100

    importing zip

--- src/fsolvent/imp/main.h
+++ src/fsolvent/imp/main.h
@@ -1,3 +1,9 @@
+static void zip(const int n, const Particle *pp, /**/ float4 *zip0, ushort4 * zip1) {
+    assert(sizeof(Particle) == 6 * sizeof(float)); /* :TODO: implicit dependency */
+    KL(dev::zip, (k_cnf(n)), (n, (float*)pp, zip0, zip1));
+}
+
+
 void prepare(const Cloud *c, /**/ BulkData *b) {
     
 }
