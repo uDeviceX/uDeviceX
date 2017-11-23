86bd7a353d73d85f8a84611e99e63fd3e8f4369e
Sergey Litvinov <slitvinov@gmail.com>
2017-11-23 10:49:37 +0100

    minor

--- src/scheme/force/dev/double_poiseuille.h
+++ src/scheme/force/dev/double_poiseuille.h
@@ -1,13 +1,15 @@
 __global__ void main(float mass, float f0, int n, const Particle *pp, /**/ Force *ff) {
     enum {X, Y};
-    int pid = threadIdx.x + blockDim.x * blockIdx.x;
+    int pid;
+    float d, *f;
+    const float *r;
+
+    pid = threadIdx.x + blockDim.x * blockIdx.x;
     if (pid >= n) return;
 
-    const float *r = pp[pid].r;
-    float       *f = ff[pid].f;
-
-    float d = r[Y] - glb::r0[Y]; /* coordinate relative to domain
-                                     center */
+    r = pp[pid].r;
+    f = ff[pid].f;
+    d = r[Y] - glb::r0[Y]; /* coordinate relative to domain center */
     if (d <= 0) f0 *= -1;
     f[X] += mass * f0;
 }
