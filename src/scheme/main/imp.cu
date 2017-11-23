0eb5d513597f5a2e6e2783aa229e6237912a5e20
Sergey Litvinov <slitvinov@gmail.com>
2017-11-23 05:26:10 +0100

    reorder scheme/main

--- src/scheme/main/imp.cu
+++ src/scheme/main/imp.cu
@@ -19,23 +19,17 @@
 
 namespace scheme {
 namespace dev {
-#ifdef FORWARD_EULER
-  #include "imp/euler.h"
-#else
-  #include "imp/vv.h"
-#endif
 #if   defined(FORCE_NONE)
-  #include "dev/force/none.h"
+  #include "dev/none.h"
 #elif defined(FORCE_CONSTANT)
-  #include "dev/force/constant.h"
+  #include "dev/constant.h"
 #elif defined(FORCE_DOUBLE_POISEUILLE)
-  #include "dev/force/double_poiseuille.h"
+  #include "dev/double_poiseuille.h"
 #elif defined(FORCE_4ROLLER)
-  #include "dev/force/4roller.h"
+  #include "dev/4roller.h"
 #else
   #error FORCE_* is undefined
 #endif
-#include "dev/main.h"
 } /* namespace */
 
 #include "imp/main.h"
--- src/scheme/main/imp.h
+++ src/scheme/main/imp.h
@@ -1,7 +1,4 @@
 namespace scheme {
 struct Fparams {float a, b, c;};
-
-void move(float mass, int n, const Force*, Particle*);
-void clear_vel(int n, Particle*);
 void force(float mass, Fparams, int n, const Particle*, /**/ Force*);
 }
