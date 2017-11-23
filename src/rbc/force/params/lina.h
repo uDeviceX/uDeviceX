6915dbd6049ccc4327e01a0e821d8153dd4dd91f
Sergey Litvinov <slitvinov@gmail.com>
2017-11-23 15:44:56 +0100

    kb is also optional

--- src/rbc/force/params/lina.h
+++ src/rbc/force/params/lina.h
@@ -1,9 +1,13 @@
 #ifndef RBCgammaC
-#define RBCgammaC 10.0
+  #define       RBCgammaC 10.0
 #endif
 
 #ifndef RBCkbT
-#define RBCkbT kBT
+  #define       RBCkbT kBT
+#endif
+
+#ifndef RBCkb
+  #define       RBCkb ( 100.0        )
 #endif
 
 #define         RBCx0 ( 0.457        )
@@ -11,7 +15,6 @@
 #define         RBCka ( 4900.0       )
 #define         RBCkd ( 5000.0       )
 #define         RBCkv ( 5000.0       )
-#define         RBCkb ( 100.0        )
 #define     RBCgammaT ( 0.0          )
 #define       RBCmpow ( 2.0          )
 #define        RBCphi ( 6.97219932574)
