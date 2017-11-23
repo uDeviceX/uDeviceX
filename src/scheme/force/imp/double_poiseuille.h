b31105a948b79f944e6f371833c5cbf6c27c4308
Sergey Litvinov <slitvinov@gmail.com>
2017-11-23 10:19:18 +0100

    compiles

--- src/scheme/force/imp/double_poiseuille.h
+++ src/scheme/force/imp/double_poiseuille.h
@@ -1,5 +1,5 @@
 void main(float mass, Param par, int n, const Particle *pp, /**/ Force* ff) {
-    domain f0;
+    float f0;
     f0 = par.a;
     KL(dev::main, (k_cnf(n)), (mass, f0, n, pp, /**/ ff));
 }
