5025fde22715114f3054f50dee51197886c05808
Sergey Litvinov <slitvinov@gmail.com>
2017-11-23 12:35:51 +0100

    less includes

--- src/clist/imp.cu
+++ src/clist/imp.cu
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <assert.h>
-#include <stdlib.h>
 
 #include <conf.h>
 #include "inc/conf.h"
--- src/comm/oc/imp.cpp
+++ src/comm/oc/imp.cpp
@@ -1,6 +1,5 @@
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
 
 #include "msg.h"
