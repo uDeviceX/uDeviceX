7586e947ed0a989362c35f1940550d2a91297cee
Sergey Litvinov <slitvinov@gmail.com>
2017-11-24 17:15:19 +0100

    deps

--- src/u/h5/make/rule.mk
+++ src/u/h5/make/rule.mk
@@ -1,12 +1,12 @@
-$B/d/api.o: $S/d/api.cu; $N -I$S/. -I$S/d
-$B/glb/imp.o: $S/glb/imp.cu; $N -I$S/. -I$S/glb
-$B/glb/wvel/imp.o: $S/glb/wvel/imp.cu; $N -I$S/. -I$S/glb/wvel
-$B/io/field/h5/imp.o: $S/io/field/h5/imp.cpp; $X -I$S/. -I$S/io/field/h5
-$B/io/field/xmf/imp.o: $S/io/field/xmf/imp.cpp; $X -I$S/. -I$S/io/field/xmf
-$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
-$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
+$B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
+$B/glb/imp.o: $S/glb/imp.cu; $N -I$S/glb -I$S/.
+$B/glb/wvel/imp.o: $S/glb/wvel/imp.cu; $N -I$S/glb/wvel -I$S/.
+$B/io/field/h5/imp.o: $S/io/field/h5/imp.cpp; $X -I$S/io/field/h5 -I$S/.
+$B/io/field/xmf/imp.o: $S/io/field/xmf/imp.cpp; $X -I$S/io/field/xmf -I$S/.
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
 $B/msg.o: $S/msg.cpp; $X -I$S/.
 $B/u/h5/main.o: $S/u/h5/main.cpp; $X -I$S/.
-$B/utils/error.o: $S/utils/error.cpp; $X -I$S/. -I$S/utils
-$B/utils/halloc.o: $S/utils/halloc.cpp; $X -I$S/. -I$S/utils
-$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/. -I$S/utils
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
+$B/utils/halloc.o: $S/utils/halloc.cpp; $X -I$S/utils -I$S/.
+$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
