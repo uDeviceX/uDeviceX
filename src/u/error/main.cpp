0e710a45d2aa78418db94e772ddbdab3c12d87e2
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-24 14:14:02 +0100

    switch for other error kinds

--- src/u/error/main.cpp
+++ src/u/error/main.cpp
@@ -1,19 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "msg.h"
 #include "mpi/glb.h"
 #include "utils/error.h"
 
-void bar() {
-    signal_error_extra("bar failed");
+enum {UDX, MPI, CUDA, NKINDS};
+
+void UDX_bar() {
+    signal_error_extra("udx bar failed");
 }
 
-void foo() {
-    UC(bar());
+void MPI_bar() {
+    signal_error_extra("mpi bar failed");
+}
+
+void CUDA_bar() {
+    signal_error_extra("cuda bar failed");
+}
+
+void foo(int kind) {
+    switch(kind) {
+    case UDX:
+        UC(UDX_bar());
+        break;
+    case MPI:
+        UC(MPI_bar());
+        break;
+    case CUDA:
+        UC(CUDA_bar());
+        break;
+    default:
+        break;
+    };
 }
 
 int main(int argc, char **argv) {
     m::ini(argc, argv);
-    UC(foo());
+
+    const char *ckind = getenv("ERR_KIND");
+    int k = atoi(ckind);
+    printf("%s %d\n", ckind, k);
+    
+    UC(foo(k));
     m::fin();
 }
