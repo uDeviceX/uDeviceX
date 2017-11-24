8cc2673aa99689aadb052783db4c7622823ea900
Sergey Litvinov <slitvinov@gmail.com>
2017-11-24 22:12:09 +0100

    naming

--- src/rbc/gen/imp/main.h
+++ src/rbc/gen/imp/main.h
@@ -3,7 +3,6 @@ static void transform(float* rr0, int nv, float *A, /* output */ Particle *pp) {
     for (int iv = 0; iv < nv; iv++) {
         float  *r = pp[iv].r, *v = pp[iv].v;
         float *r0 = &rr0[3*iv];
-
         for (int c = 0, i = 0; c < 3; c++) {
             r[c] += A[i++]*r0[0]; /* matrix transformation */
             r[c] += A[i++]*r0[1];
@@ -21,7 +20,7 @@ static bool read_A(FILE *f, float A[16]) {
     return true;
 }
 
-static void shift2local(const int orig[3], float A[16]) {
+static void shift(const int orig[3], /**/ float A[16]) {
     int c, j;
     for (c = 0; c < 3; c++) {
         j = 4 * c + 3;
@@ -43,7 +42,7 @@ static void assert_nc(int nc) {
     ERR("nc = %d >= MAX_CELL_NUM = %d", nc, MAX_CELL_NUM);
 }
 
-static int setup_hst0(float *rr0, const char *ic, int nv, Particle *pp) {
+static int main0(float *rr0, const char *ic, int nv, Particle *pp) {
     int c, nc = 0;
     int mi[3], L[3] = {XS, YS, ZS};
     for (c = 0; c < 3; ++c) mi[c] = (m::coords[c] + 0.5) * L[c];
@@ -53,7 +52,7 @@ static int setup_hst0(float *rr0, const char *ic, int nv, Particle *pp) {
     if (f == NULL) ERR("Could not open <%s>\n", ic);
 
     while ( read_A(f, /**/ A) ) {
-        shift2local(mi, /**/ A);
+        shift(mi, /**/ A);
         if ( inside_subdomain(L, A) )
             transform(rr0, nv, A, &pp[nv*(nc++)]);
     }
@@ -76,7 +75,7 @@ int main(const char *cell, const char *ic, int nv, /**/ Particle *pp) {
     UC(emalloc(3*nv*sizeof(float), (void**) &rr0));
 
     vert(cell, nv, /**/ rr0);
-    nc = setup_hst0(rr0, ic, nv, pp);
+    nc = main0(rr0, ic, nv, pp);
 
     free(rr0);
     return nc;
