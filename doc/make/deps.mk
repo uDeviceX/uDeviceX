f181cd1b8e399b7f07c69d6dcbf7b7fbb63fac74
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-25 21:09:47 +0100

    fix deps

--- doc/make/deps.mk
+++ doc/make/deps.mk
@@ -1,31 +1,27 @@
-./units/hw/hst.adoc: 
-./units/hw/dev.adoc: 
-./units/rbc/main.adoc: 
-./units/rbc/force.adoc: 
-./units/rbc/rnd.adoc: 
-./units/mpi.adoc: 
-./units/error.adoc: 
-./units/comm.adoc: 
-./units/meshbb.adoc: 
-./units/x.adoc: 
-./units/pair.adoc: 
-./units/scan.adoc: 
-./main.adoc: 
-./dev/conventions.adoc: ./dev/cstyle/cc.el
-./dev/cstyle/emacs.adoc: 
-./dev/main.adoc: 
-./dev/wrap.adoc: 
-./dev/gource.adoc: 
-./dev/modules/fluforces.adoc: 
-./dev/modules/clist.adoc: 
-./dev/modules/comm.adoc: 
-./dev/modules/utils/error.inc.adoc: 
-./dev/modules/utils/kl.inc.adoc: 
-./dev/modules/utils/main.adoc: ./dev/modules/utils/error.inc.adoc ./dev/modules/utils/cc.inc.adoc ./dev/modules/utils/mc.inc.adoc ./dev/modules/utils/kl.inc.adoc
-./dev/modules/utils/mc.inc.adoc: 
-./dev/modules/utils/cc.inc.adoc: 
-./dev/modules/distr.adoc: 
-./dev/modules/algo/scan.adoc: 
-./dev/modules/d.adoc: 
-./dev/compile.adoc: 
-./index.adoc: 
+./units/hw/hst.html: ./units/hw/hst.adoc 
+./units/hw/dev.html: ./units/hw/dev.adoc 
+./units/rbc/main.html: ./units/rbc/main.adoc 
+./units/rbc/force.html: ./units/rbc/force.adoc 
+./units/rbc/rnd.html: ./units/rbc/rnd.adoc 
+./units/mpi.html: ./units/mpi.adoc 
+./units/error.html: ./units/error.adoc 
+./units/comm.html: ./units/comm.adoc 
+./units/meshbb.html: ./units/meshbb.adoc 
+./units/x.html: ./units/x.adoc 
+./units/pair.html: ./units/pair.adoc 
+./units/scan.html: ./units/scan.adoc 
+./main.html: ./main.adoc 
+./dev/conventions.html: ./dev/conventions.adoc ./dev/cstyle/cc.el
+./dev/cstyle/emacs.html: ./dev/cstyle/emacs.adoc 
+./dev/main.html: ./dev/main.adoc 
+./dev/wrap.html: ./dev/wrap.adoc 
+./dev/gource.html: ./dev/gource.adoc 
+./dev/modules/fluforces.html: ./dev/modules/fluforces.adoc 
+./dev/modules/clist.html: ./dev/modules/clist.adoc 
+./dev/modules/comm.html: ./dev/modules/comm.adoc 
+./dev/modules/utils/main.html: ./dev/modules/utils/main.adoc ./dev/modules/utils/error.inc.adoc ./dev/modules/utils/cc.inc.adoc ./dev/modules/utils/mc.inc.adoc ./dev/modules/utils/kl.inc.adoc
+./dev/modules/distr.html: ./dev/modules/distr.adoc 
+./dev/modules/algo/scan.html: ./dev/modules/algo/scan.adoc 
+./dev/modules/d.html: ./dev/modules/d.adoc 
+./dev/compile.html: ./dev/compile.adoc 
+./index.html: ./index.adoc 
