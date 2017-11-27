18aeecc44e9bab77193492fa512d8d7ae73cb3c2
Sergey Litvinov <slitvinov@gmail.com>
2017-11-27 22:15:57 +0100

    initial import of u/rbc/area_volume

---
+++ src/u/rbc/area_volume/lib/imp.cu
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <assert.h>
+#include <mpi.h>
+
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "utils/error.h"
+#include "utils/halloc.h"
+
+#include "d/api.h"
+#include "msg.h"
+
+#include "inc/def.h"
+#include "inc/type.h"
+#include "inc/dev.h"
+
+#include "utils/te.h"
+#include "utils/texo.h"
+#include "utils/cc.h"
+
+#include "rbc/type.h"
+#include "rbc/force/area_volume/imp.h"
+#include "rbc/main/imp.h"
+#include "rbc/rnd/imp.h"
+#include "rbc/force/imp.h"
+#include "rbc/stretch/imp.h"
+
+#include "scheme/move/imp.h"
+#include "scheme/force/imp.h"
+
+#include "io/mesh/imp.h"
+#include "io/diag.h"
+
+#include "mpi/wrapper.h"
+#include "mpi/glb.h"
+
+#include "imp.h"
+
+namespace stretch {
+#if RBC_STRETCH==true
+  #include "imp/stretch1.h"
+#else
+  #include "imp/stretch0.h"
+#endif
+}
+
+#include "imp/main.h"
---
+++ src/u/rbc/area_volume/lib/imp/main.h
@@ -0,0 +1,64 @@
+static void dump(rbc::Quants q, rbc::force::TicketT t) {
+    float av[2];
+    int n;
+    Particle *pp;
+    static int i = 0;
+    n = q.nc * q.nv;
+    UC(emalloc(n*sizeof(Particle), (void**)&pp));
+    cD2H(pp, q.pp, q.n);
+    io::mesh::rbc(m::cart, pp, q.tri_hst, q.nc, q.nv, q.nt, i++);
+    area_volume::hst(q.nt, q.nv, q.nc, q.pp, q.tri, /**/ av);
+    diagnostics(m::cart, n, pp, i);
+    free(pp);
+}
+
+static int body_force(rbc::Quants q, Force *f) {
+    scheme::force::Param fpar;
+    fpar.a = FORCE_PAR_A;
+    scheme::force::main(rbc_mass, fpar, q.n, q.pp, /**/ f);
+    return 0;
+}
+
+static void run0(rbc::Quants q, rbc::force::TicketT t, rbc::stretch::Fo* stretch, Force *f) {
+    long i;
+    long nsteps = (long)(tend / dt);
+    MSG("will take %ld steps", nsteps);
+    for (i = 0; i < nsteps; i++) {
+        Dzero(f, q.n);
+        rbc::force::apply(q, t, /**/ f);
+        stretch::apply(q.nc, stretch, /**/ f);
+        if (pushrbc) body_force(q, /**/ f);
+        scheme::move::main(rbc_mass, q.n, f, q.pp);
+        if (i % part_freq  == 0) dump(q, t);
+#ifdef RBC_CLEAR_VEL
+        scheme::move::clear_vel(q.n, /**/ q.pp);
+#endif
+    }
+}
+
+static void run1(rbc::Quants q, rbc::force::TicketT t,
+                 rbc::stretch::Fo *stretch) {
+    Force *f;
+    Dalloc(&f, q.n);
+    Dzero(f, q.n);
+    run0(q, t, stretch, f);
+    Dfree(f);
+}
+
+static void run2(const char *cell, const char *ic, rbc::Quants q) {
+    rbc::stretch::Fo *stretch;
+    rbc::force::TicketT t;
+    rbc::main::gen_quants(m::cart, cell, ic, /**/ &q);
+    UC(stretch::ini("rbc.stretch", q.nv, /**/ &stretch));
+    rbc::force::gen_ticket(q, &t);
+    run1(q, t, stretch);
+    stretch::fin(stretch);
+    rbc::force::fin_ticket(&t);
+}
+
+void run(const char *cell, const char *ic) {
+    rbc::Quants q;
+    rbc::main::ini(&q);
+    run2(cell, ic, q);
+    rbc::main::fin(&q);
+}
