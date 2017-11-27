e5c6bbe801d3a7268834342e010883cd4b34b499
Sergey Litvinov <slitvinov@gmail.com>
2017-11-27 14:48:10 +0100

    add an interface to area_volume which does not depend on RBCn[tv]

--- src/rbc/force/area_volume/imp/main.h
+++ src/rbc/force/area_volume/imp/main.h
@@ -1,11 +1,15 @@
-void area_volume(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av) {
+void area_volume0(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av) {
     dim3 avThreads(256, 1);
     dim3 avBlocks(1, nc);
+    Dzero(av, 2*nc);
+    KL(dev::area_volume, (avBlocks, avThreads), (nt, nv, texvert, textri, av));
+}
+
+void area_volume(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av) {
     int nt, nv;
     nt = RBCnt;
     nv = RBCnv;
-    Dzero(av, 2*nc);
-    KL(dev::area_volume, (avBlocks, avThreads), (nt, nv, texvert, textri, av));
+    area_volume0(nt, nv, nc, texvert, textri, /**/ av);
 }
 
 void area_volume_hst(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst) {
