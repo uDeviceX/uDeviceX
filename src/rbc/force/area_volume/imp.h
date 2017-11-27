a95f24ddffca09dc2828687171fc3516e0b27f4d
Sergey Litvinov <slitvinov@gmail.com>
2017-11-27 16:31:00 +0100

    rm variable names in header

--- src/rbc/force/area_volume/imp.h
+++ src/rbc/force/area_volume/imp.h
@@ -3,6 +3,6 @@ struct Ticket;
 void dev(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
 void hst(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst);
 
-void ini(int nmax, Particle *pp, int nt, int4 *tri, /**/ Ticket **t);
-void fin(Ticket* t);
+void ini(int nmax, Particle*, int nt, int4 *tri, /**/ Ticket**);
+void fin(Ticket*);
 }
