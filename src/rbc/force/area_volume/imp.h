c14b68600fa4cf0762985dae95e0a80ee57aa9df
Sergey Litvinov <slitvinov@gmail.com>
2017-11-27 20:28:21 +0100

    textri is not texture

--- src/rbc/force/area_volume/imp.h
+++ src/rbc/force/area_volume/imp.h
@@ -1,4 +1,4 @@
 namespace area_volume {
-void dev(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
-void hst(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst);
+void dev(int nt, int nv, int nc, const Texo<float2> texvert, const int4 *textri, /**/ float *av);
+void hst(int nt, int nv, int nc, const Texo<float2> texvert, const int4 *textri, /**/ float *hst);
 }
--- src/rbc/force/area_volume/imp/main.h
+++ src/rbc/force/area_volume/imp/main.h
@@ -1,11 +1,11 @@
-void dev(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av) {
+void dev(int nt, int nv, int nc, const Texo<float2> texvert, const int4 *textri, /**/ float *av) {
     dim3 avThreads(256, 1);
     dim3 avBlocks(1, nc);
     Dzero(av, 2*nc);
     KL(dev0::dev, (avBlocks, avThreads), (nt, nv, texvert, textri, av));
 }
 
-void hst(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst) {
+void hst(int nt, int nv, int nc, const Texo<float2> texvert, const int4 *textri, /**/ float *hst) {
     float *dev;
     Dalloc(&dev, 2*nc);
     area_volume::dev(nt, nv, nc, texvert, textri, /**/ dev);
