363484c8d062400d1869e94d8a2f3ed19d38226c
Sergey Litvinov <slitvinov@gmail.com>
2017-11-27 14:51:00 +0100

    add area_volume0 and area_volume_hst0 to imp.h

--- src/rbc/force/area_volume/imp.h
+++ src/rbc/force/area_volume/imp.h
@@ -1,2 +1,5 @@
 void area_volume(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
 void area_volume_hst(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
+
+void area_volume0(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
+void area_volume_hst0(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst);
