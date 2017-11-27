5400c1684da8b05648fff67cf04aef410d36226d
Sergey Litvinov <slitvinov@gmail.com>
2017-11-27 15:11:25 +0100

    rm an old interface for area_volume.h

--- src/rbc/force/area_volume/imp.h
+++ src/rbc/force/area_volume/imp.h
@@ -1,5 +1,2 @@
-void area_volume(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
-void area_volume_hst(int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
-
-void area_volume0(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
-void area_volume_hst0(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst);
+void area_volume(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *av);
+void area_volume_hst(int nt, int nv, int nc, const Texo<float2> texvert, const Texo<int4> textri, /**/ float *hst);
