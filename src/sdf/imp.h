18b22a8a278502b48ea74a76c57fdaf8b717c149
Sergey Litvinov <slitvinov@gmail.com>
2017-11-27 22:05:21 +0100

    clean up

--- src/sdf/imp.h
+++ src/sdf/imp.h
@@ -1,10 +1,10 @@
 namespace sdf {
 namespace sub {
 
-void ini(MPI_Comm cart, cudaArray *arrsdf, tex3Dca<float> *texsdf);
-void bulk_wall(const tex3Dca<float> texsdf, /*io*/ Particle *s_pp, int *s_n, /*o*/ Particle *w_pp, int *w_n);
-int  who_stays(const tex3Dca<float> texsdf, Particle *pp, int n, int nc, int nv, /**/ int *stay);
-void bounce(const tex3Dca<float> texsdf, int n, /**/ Particle *pp);
+void ini(MPI_Comm cart, cudaArray *arrsdf, tex3Dca<float>*);
+void bulk_wall(const tex3Dca<float>, /*io*/ Particle *s_pp, int *s_n, /*o*/ Particle *w_pp, int *w_n);
+int  who_stays(const tex3Dca<float>, Particle *pp, int n, int nc, int nv, /**/ int *stay);
+void bounce(const tex3Dca<float>, int n, /**/ Particle *pp);
 
 }
 }
