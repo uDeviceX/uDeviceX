94f6ccd33ed5580b4e0963e29ba7e08229d4c8a5
Sergey Litvinov <slitvinov@gmail.com>
2017-11-28 21:36:19 +0100

    wip: punto2dist

--- post/punto/src/punto2dist.c
+++ post/punto/src/punto2dist.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("42\n");
+#define NMAX 100000
+double x0[NMAX], y0[NMAX], z0[NMAX];
+double x[NMAX], y[NMAX], z[NMAX];
+
+void read(FILE *f, /**/ int *pn, double *x, double *y, double *z) {
+    int i;
+    char s[BUFSIZ];
+    for (i = 0; ; i++) {
+        if (fgets(s, sizeof(s) - 1, f) == '\0') break;
+        if (sscanf(s, "%lf %lf %lf", &x[i], &y[i], &z[i]) != 3) break;
+    }
+    *pn = i;
+}
+
+void main0(FILE *f) {
+    int n0, n;
+    read(f, /**/ &n0, x0, y0, z0);
+    for (;;) {
+        read(f, /**/ &n, x, y, z);
+        if (n == 0) break;
+        if (n != n0) {
+            fprintf(stderr, "punto2dist: n=%d != n0=%d\n", n, n0);
+            exit(2);
+        }
+    }
+}
+
+int main(int c, const char **v) {
+    FILE *f;
+    const char *path;
+    if (c < 2) {
+        fprintf(stderr, "punto2dist: not enough args\n");
+        exit(2);
+    }
+    path = v[1];
+    if ((f = fopen(path, "r")) == NULL) {
+        fprintf(stderr, "punto2dist: fail to open '%s'\n", v[0]);
+        exit(2);
+    }
+
+    main0(f);
 }
