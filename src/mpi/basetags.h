8ccb19bcfb068401aaa7827c3af39599cf2d9d63
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-28 15:25:02 +0100

    rm basetags

--- src/mpi/basetags.h
+++
@@ -1,9 +0,0 @@
-namespace basetags {
-struct TagGen {
-    int bt;
-    static const int stride = 100;
-};
-
-inline void ini(TagGen *tg)    {tg->bt = 0;}
-inline int get_tag(TagGen *tg) {return tg->bt += tg->stride;}
-}
