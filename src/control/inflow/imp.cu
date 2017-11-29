df9053a949a4205a1c719bf7e8e25094181844c3
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-29 10:49:14 +0100

    add empty inflow

---
+++ src/control/inflow/imp.cu
@@ -0,0 +1,14 @@
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "utils/error.h"
+#include "utils/cc.h"
+#include "utils/kl.h"
+#include "d/api.h"
+
+#include "inc/dev.h"
+#include "inc/type.h"
+
+#include "imp.h"
+
+#include "imp/main.h"
