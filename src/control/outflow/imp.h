a9fa0e76dcf127421e54357c142826f82289e253
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-29 10:45:05 +0100

    call plane

--- src/control/outflow/imp.h
+++ src/control/outflow/imp.h
@@ -6,3 +6,4 @@ void ini(int maxp, /**/ Outflow *o);
 void fin(/**/ Outflow *o);
 
 void filter_particles_circle(float R, int n, const Particle *pp, Outflow *o);
+void filter_particles_plane(float3 normal, float3 r, int n, const Particle *pp, Outflow *o);
--- src/control/outflow/imp/main.h
+++ src/control/outflow/imp/main.h
@@ -19,3 +19,19 @@ void filter_particles_circle(float R, int n, const Particle *pp, Outflow *o) {
     
     KL(circle::filter, (k_cnf(n)), (origin, n, pp, params, /**/ o->kk) );
 }
+
+void filter_particles_plane(float3 normal, float3 r, int n, const Particle *pp, Outflow *o) {
+    plane::Params params;
+    float3 origin;
+
+    params.a = normal.x;
+    params.b = normal.y;
+    params.c = normal.z;
+    params.d = - (normal.x * r.x + normal.y * r.y + normal.z * r.z);
+    // TODO
+    origin.x = 0;
+    origin.y = 0;
+    origin.z = 0;
+    
+    KL(plane::filter, (k_cnf(n)), (origin, n, pp, params, /**/ o->kk) );
+}
