b8b79a69e5d087b914042902af94242ba5c89a89
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-29 10:39:13 +0100

    circle is 2D

--- src/control/outflow/dev/circle.h
+++ src/control/outflow/dev/circle.h
@@ -4,11 +4,10 @@ struct Params {
 
 __device__ int predicate(float3 o, Params p, const float r[3]) {
     enum {X, Y, Z};
-    float x, y, z, rsq;
+    float x, y, rsq;
     x = r[X] - o.x;
     y = r[Y] - o.y;
-    z = r[Z] - o.z;
 
-    rsq = x*x + y*y + z*z;
+    rsq = x*x + y*y;
     return rsq > p.Rsq;
 }
