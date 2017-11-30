0b81ae66720a837f6cb4ae1a68f6925461f28d84
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 16:25:31 +0100

    deps

--- src/u/sdf/make/dep.mk
+++ src/u/sdf/make/dep.mk
@@ -10,7 +10,7 @@ $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/msg.o: $S/msg.h $S/mpi/glb.h
 $B/sdf/field/imp.o: $S/inc/conf.h $S/io/field/imp.h $S/utils/error.h $S/inc/type.h $S/sdf/field/imp.h $S/utils/efopen.h $S/msg.h $B/conf.h $S/utils/halloc.h $S/mpi/glb.h
 $B/sdf/imp.o: $S/inc/conf.h $S/utils/error.h $S/inc/type.h $S/sdf/imp.h $S/inc/macro.h $S/utils/cc.h $S/inc/def.h $S/sdf/sub/imp.h $S/inc/dev.h $S/msg.h $S/d/api.h $B/conf.h $S/sdf/imp/main.h $S/utils/halloc.h $S/sdf/imp/type.h $S/sdf/type.h $S/mpi/glb.h $S/d/ker.h
-$B/sdf/sub/imp.o: $S/inc/conf.h $S/inc/dev/wvel.h $S/utils/error.h $S/inc/type.h $S/d/q.h $S/sdf/field/imp.h $S/sdf/sub/imp.h $S/inc/macro.h $S/utils/cc.h $S/inc/def.h $S/sdf/type.h $S/inc/dev.h $S/d/api.h $S/msg.h $B/conf.h $S/sdf/sub/imp/main.h $S/utils/halloc.h $S/sdf/sub/dev/bounce.h $S/utils/kl.h $S/glb/get.h $S/sdf/sub/dev/main.h $S/sdf/sub/dev/cheap.h $S/d/ker.h $S/mpi/glb.h
+$B/sdf/sub/imp.o: $S/sdf/sub/dev/fill.h $S/inc/conf.h $S/inc/dev/wvel.h $S/utils/error.h $S/inc/type.h $S/d/q.h $S/sdf/field/imp.h $S/sdf/sub/imp.h $S/inc/macro.h $S/utils/cc.h $S/inc/def.h $S/sdf/type.h $S/inc/dev.h $S/d/api.h $S/msg.h $B/conf.h $S/sdf/sub/imp/main.h $S/utils/halloc.h $S/sdf/sub/dev/bounce.h $S/utils/kl.h $S/glb/get.h $S/sdf/sub/dev/main.h $S/sdf/sub/dev/cheap.h $S/d/ker.h $S/mpi/glb.h
 $B/u/sdf/main.o: $S/inc/conf.h $S/utils/error.h $S/sdf/imp/type.h $S/inc/type.h $S/mpi/wrapper.h $S/inc/macro.h $S/utils/cc.h $S/u/sdf/dev.h $S/sdf/type.h $S/inc/dev.h $S/msg.h $S/d/api.h $B/conf.h $S/sdf/imp.h $S/utils/kl.h $S/sdf/sub/dev/main.h $S/mpi/glb.h $S/d/ker.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
 $B/utils/efopen.o: $S/utils/error.h $S/utils/efopen.h
