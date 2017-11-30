075d04b18741eb07e6160354947a8f9f8474c777
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 16:11:46 +0100

    move functions to dev/bounce.h

--- src/sdf/sub/dev/bounce.h
+++ src/sdf/sub/dev/bounce.h
@@ -1,3 +1,58 @@
+static __device__ float fst(float2 *t) { return t->x; }
+static __device__ float scn(float2 *t) { return t->y; }
+static __device__ void p2rv(float2 *p, int i, /**/ float r[3], float v[3]) {
+    enum {X, Y, Z};
+    p += 3 * i;
+    r[X] = fst(p);   r[Y] = scn(p++); r[Z] = fst(p);
+    v[X] = scn(p++); v[Y] = fst(p);   v[Z] = scn(p);
+}
+static __device__ void rv2p(float r[3], float v[3], int i, /**/ float2 *p) {
+    enum {X, Y, Z};
+    p += 3 * i;
+    p->x   = r[X]; p++->y = r[Y]; p->x = r[Z];
+    p++->y = v[X]; p->x   = v[Y]; p->y = v[Z];
+}
+
+static __device__ float3 ugrad_sdf(const tex3Dca texsdf, float x, float y, float z) {
+    int L[3] = {XS, YS, ZS};
+    int M[3] = {XWM, YWM, ZWM};
+    int T[3] = {XTE, YTE, ZTE};
+    int tc[3];
+    float fcts[3], r[3] = {x, y, z};
+    for (int c = 0; c < 3; ++c)
+        tc[c] = T[c] * (r[c] + L[c] / 2 + M[c]) / (L[c] + 2 * M[c]);
+    for (int c = 0; c < 3; ++c)
+        fcts[c] = T[c] / (2 * M[c] + L[c]);
+
+#define tex0(ix, iy, iz) (texsdf.fetch(tc[0] + ix, tc[1] + iy, tc[2] + iz))
+    float myval = tex0(0, 0, 0);
+    float gx = fcts[0] * (tex0(1, 0, 0) - myval);
+    float gy = fcts[1] * (tex0(0, 1, 0) - myval);
+    float gz = fcts[2] * (tex0(0, 0, 1) - myval);
+#undef tex0
+
+    return make_float3(gx, gy, gz);
+}
+
+static __device__ float3 grad_sdf(const tex3Dca texsdf, float x, float y, float z) {
+    float gx, gy, gz;
+    int L[3] = {XS, YS, ZS};
+    int M[3] = {XWM, YWM, ZWM};
+    int T[3] = {XTE, YTE, ZTE};
+    float tc[3], r[3] = {x, y, z};
+    for (int c = 0; c < 3; ++c)
+        tc[c] = T[c] * (r[c] + L[c] / 2 + M[c]) / (L[c] + 2 * M[c]) - 0.5;
+
+#define tex0(ix, iy, iz) (texsdf.fetch(tc[0] + ix, tc[1] + iy, tc[2] + iz))
+    gx = tex0(1, 0, 0) - tex0(-1,  0,  0);
+    gy = tex0(0, 1, 0) - tex0( 0, -1,  0);
+    gz = tex0(0, 0, 1) - tex0( 0,  0, -1);
+#undef tex0
+    float ggmag = sqrt(gx*gx + gy*gy + gz*gz);
+    if (ggmag > 1e-6) { gx /= ggmag; gy /= ggmag; gz /= ggmag; }
+    return make_float3(gx, gy, gz);
+}
+
 static __device__ void bounce1(const tex3Dca texsdf, float currsdf,
                                float &x, float &y, float &z,
                                float &vx, float &vy, float &vz) {
