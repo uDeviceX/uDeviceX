9e44455000fa5642ee800249d23142cde8527dc0
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 14:19:41 +0100

    pass x, y, z from comman line to sdf()

--- src/u/sdf/main.cu
+++ src/u/sdf/main.cu
@@ -27,25 +27,47 @@ namespace dev {
 #include "dev.h"
 }
 
-void main0(sdf::Quants *qsdf) {
+struct Part { float x, y, z; };
+static int    argc;
+static char **argv;
+/* left shift */
+void lshift() {
+    argc--;
+    if (argc < 1) ERR("u/sdf: not enough args");
+}
+
+void main0(sdf::Quants *qsdf, Part *p) {
     float x, y, z;
-    x = y = z = 0;
+    x = p->x; y = p->y; z = p->z;
     KL(dev::main, (1, 1), (qsdf->texsdf, x, y, z));
 }
 
-void main1() {
+void main1(Part *p) {
     sdf::Quants *qsdf;
     UC(sdf::alloc_quants(&qsdf));
     UC(sdf::ini(m::cart, qsdf));
-    UC(main0(qsdf));
+    UC(main0(qsdf, p));
     UC(sdf::free_quants(qsdf));
     dSync();
 }
 
-int main(int argc, char **argv) {
+void ini_part(/**/ Part *p) {
+    float x, y, z;
+    x = atof(argv[argc - 1]); lshift();
+    y = atof(argv[argc - 1]); lshift();
+    z = atof(argv[argc - 1]); lshift();
+    p->x = x; p->y = y; p->z = z;
+}
+
+void main2() {
+    Part p;
+    ini_part(/**/ &p);
     m::ini(argc, argv);
-    MSG("mpi size: %d", m::size);
-    UC(main1());
-    MSG("Hello world!");
+    UC(main1(&p));
     m::fin();
+}
+
+int main(int argc0, char **argv0) {
+    argc = argc0; argv = argv0;
+    UC(main2());
 }
