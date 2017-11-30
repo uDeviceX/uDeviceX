3c6e718d6728dffbf172e630327074a5b39292a6
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-30 12:35:51 +0100

    add empty implementation

--- src/control/inflow/imp.h
+++ src/control/inflow/imp.h
@@ -1,4 +1,12 @@
 struct Inflow {
+    float3 *flux;   /* target flux     */
     float *cumflux; /* cumulative flux */
     int n;
 };
+
+void ini(Inflow *i);
+void fin(Inflow *i);
+
+void create_pp(Inflow *i, int *n, Cloud *c);
+
+
---
+++ src/control/inflow/imp/main.h
@@ -0,0 +1,11 @@
+void ini(Inflow *i) {
+    
+}
+
+void fin(Inflow *i) {
+    
+}
+
+void create_pp(Inflow *i, int *n, Cloud *c) {
+
+}
