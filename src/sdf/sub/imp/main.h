004a943c8648edee1c53803cf111cb57eee9f37f
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 16:40:37 +0100

    finger error

--- src/sdf/sub/imp/main.h
+++ src/sdf/sub/imp/main.h
@@ -135,7 +135,7 @@ static int who_stays1(int *keys, int n, int nc, int nv, /*o*/ int *stay) {
 int who_stays(const tex3Dca texsdf, Particle *pp, int n, int nc, int nv, /**/ int *stay) {
     int *keys;
     CC(d::Malloc((void **) &keys, n*sizeof(keys[0])));
-p    KL(dev::fill, (k_cnf(n)), (texsdf, pp, n, keys));
+    KL(dev::fill, (k_cnf(n)), (texsdf, pp, n, keys));
     nc = who_stays1(keys, n, nc, nv, /**/ stay);
     CC(d::Free(keys));
     return nc;
