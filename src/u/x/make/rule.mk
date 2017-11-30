81ab83b4f4cfd70da33adda0df6e2e25b4b89063
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 10:48:45 +0100

    deps

--- src/u/x/make/rule.mk
+++ src/u/x/make/rule.mk
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $B/algo/minmax/imp.o: $S/algo/minmax/imp.cu; $N -I$S/algo/minmax -I$S/.
 $B/algo/scan/imp.o: $S/algo/scan/imp.cu; $N -I$S/algo/scan -I$S/.
 $B/clist/imp.o: $S/clist/imp.cu; $N -I$S/clist -I$S/.
@@ -40,62 +39,17 @@ $B/io/off.o: $S/io/off.cpp; $X -I$S/io -I$S/.
 $B/io/ply.o: $S/io/ply.cpp; $X -I$S/io -I$S/.
 $B/io/restart.o: $S/io/restart.cpp; $X -I$S/io -I$S/.
 $B/io/rig.o: $S/io/rig.cpp; $X -I$S/io -I$S/.
-=======
-$B/algo/minmax/imp.o: $S/algo/minmax/imp.cu; $N -I$S/. -I$S/algo/minmax
-$B/algo/scan/imp.o: $S/algo/scan/imp.cu; $N -I$S/. -I$S/algo/scan
-$B/clist/imp.o: $S/clist/imp.cu; $N -I$S/. -I$S/clist
-$B/cnt/imp.o: $S/cnt/imp.cu; $N -I$S/. -I$S/cnt
-$B/color/flux.o: $S/color/flux.cu; $N -I$S/. -I$S/color
-$B/comm/imp.o: $S/comm/imp.cu; $N -I$S/. -I$S/comm
-$B/control/inflow/imp.o: $S/control/inflow/imp.cu; $N -I$S/. -I$S/control/inflow
-$B/control/outflow/imp.o: $S/control/outflow/imp.cu; $N -I$S/. -I$S/control/outflow
-$B/control/vel/imp.o: $S/control/vel/imp.cu; $N -I$S/. -I$S/control/vel
-$B/d/api.o: $S/d/api.cu; $N -I$S/. -I$S/d
-$B/dbg/imp.o: $S/dbg/imp.cu; $N -I$S/. -I$S/dbg
-$B/distr/flu/imp.o: $S/distr/flu/imp.cu; $N -I$S/. -I$S/distr/flu
-$B/distr/rbc/imp.o: $S/distr/rbc/imp.cu; $N -I$S/. -I$S/distr/rbc
-$B/distr/rig/imp.o: $S/distr/rig/imp.cu; $N -I$S/. -I$S/distr/rig
-$B/exch/flu/imp.o: $S/exch/flu/imp.cu; $N -I$S/. -I$S/exch/flu
-$B/exch/mesh/imp.o: $S/exch/mesh/imp.cu; $N -I$S/. -I$S/exch/mesh
-$B/exch/obj/imp.o: $S/exch/obj/imp.cu; $N -I$S/. -I$S/exch/obj
-$B/field.o: $S/field.cpp; $X -I$S/.
-$B/flu/imp.o: $S/flu/imp.cpp; $X -I$S/. -I$S/flu
-$B/fluforces/bulk/imp.o: $S/fluforces/bulk/imp.cu; $N -I$S/. -I$S/fluforces/bulk
-$B/fluforces/bulk/transpose/imp.o: $S/fluforces/bulk/transpose/imp.cu; $N -I$S/. -I$S/fluforces/bulk/transpose
-$B/fluforces/halo/imp.o: $S/fluforces/halo/imp.cu; $N -I$S/. -I$S/fluforces/halo
-$B/fluforces/imp.o: $S/fluforces/imp.cu; $N -I$S/. -I$S/fluforces
-$B/frag/imp.o: $S/frag/imp.cpp; $X -I$S/. -I$S/frag
-$B/fsi/imp.o: $S/fsi/imp.cu; $N -I$S/. -I$S/fsi
-$B/generate/rig/imp.o: $S/generate/rig/imp.cu; $N -I$S/. -I$S/generate/rig
-$B/glb/imp.o: $S/glb/imp.cu; $N -I$S/. -I$S/glb
-$B/glb/wvel/imp.o: $S/glb/wvel/imp.cu; $N -I$S/. -I$S/glb/wvel
-$B/inter/imp.o: $S/inter/imp.cu; $N -I$S/. -I$S/inter -I$S/inter/_ussr
-$B/io/bop/imp.o: $S/io/bop/imp.cpp; $X -I$S/. -I$S/io/bop
-$B/io/com.o: $S/io/com.cpp; $X -I$S/. -I$S/io
-$B/io/diag.o: $S/io/diag.cpp; $X -I$S/. -I$S/io
-$B/io/field/h5/imp.o: $S/io/field/h5/imp.cpp; $X -I$S/. -I$S/io/field/h5
-$B/io/field/imp.o: $S/io/field/imp.cpp; $X -I$S/. -I$S/io/field
-$B/io/field/xmf/imp.o: $S/io/field/xmf/imp.cpp; $X -I$S/. -I$S/io/field/xmf
-$B/io/fields_grid.o: $S/io/fields_grid.cpp; $X -I$S/. -I$S/io
-$B/io/mesh/imp.o: $S/io/mesh/imp.cpp; $X -I$S/. -I$S/io/mesh
-$B/io/mesh/write/imp.o: $S/io/mesh/write/imp.cpp; $X -I$S/. -I$S/io/mesh/write
-$B/io/off.o: $S/io/off.cpp; $X -I$S/. -I$S/io
-$B/io/ply.o: $S/io/ply.cpp; $X -I$S/. -I$S/io
-$B/io/restart.o: $S/io/restart.cpp; $X -I$S/. -I$S/io
-$B/io/rig.o: $S/io/rig.cpp; $X -I$S/. -I$S/io
->>>>>>> 6e18123d20373f1341a8ed80eb7d480142b8b6d5
 $B/main.o: $S/main.cpp; $X -I$S/.
-$B/math/linal.o: $S/math/linal.cpp; $X -I$S/. -I$S/math
-$B/mesh/bbox.o: $S/mesh/bbox.cu; $N -I$S/. -I$S/mesh
-$B/mesh/collision.o: $S/mesh/collision.cu; $N -I$S/. -I$S/mesh
-$B/mesh/dist.o: $S/mesh/dist.cpp; $X -I$S/. -I$S/mesh
-$B/mesh/props.o: $S/mesh/props.cpp; $X -I$S/. -I$S/mesh
-$B/meshbb/imp.o: $S/meshbb/imp.cu; $N -I$S/. -I$S/meshbb
-$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
-$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/. -I$S/mpi
-$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
+$B/math/linal.o: $S/math/linal.cpp; $X -I$S/math -I$S/.
+$B/meshbb/imp.o: $S/meshbb/imp.cu; $N -I$S/meshbb -I$S/.
+$B/mesh/bbox.o: $S/mesh/bbox.cu; $N -I$S/mesh -I$S/.
+$B/mesh/collision.o: $S/mesh/collision.cu; $N -I$S/mesh -I$S/.
+$B/mesh/dist.o: $S/mesh/dist.cpp; $X -I$S/mesh -I$S/.
+$B/mesh/props.o: $S/mesh/props.cpp; $X -I$S/mesh -I$S/.
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
 $B/msg.o: $S/msg.cpp; $X -I$S/.
-<<<<<<< HEAD
 $B/rbc/adj/imp.o: $S/rbc/adj/imp.cpp; $X -I$S/rbc/adj -I$S/.
 $B/rbc/com/imp.o: $S/rbc/com/imp.cu; $N -I$S/rbc/com -I$S/.
 $B/rbc/edg/imp.o: $S/rbc/edg/imp.cpp; $X -I$S/rbc/edg -I$S/.
@@ -129,37 +83,3 @@ $B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
 $B/wall/exch/imp.o: $S/wall/exch/imp.cpp; $X -I$S/wall/exch -I$S/.
 $B/wall/force/imp.o: $S/wall/force/imp.cu; $N -I$S/wall/force -I$S/.
 $B/wall/imp.o: $S/wall/imp.cu; $N -I$S/wall -I$S/.
-=======
-$B/rbc/adj/imp.o: $S/rbc/adj/imp.cpp; $X -I$S/. -I$S/rbc/adj
-$B/rbc/com/imp.o: $S/rbc/com/imp.cu; $N -I$S/. -I$S/rbc/com
-$B/rbc/edg/imp.o: $S/rbc/edg/imp.cpp; $X -I$S/. -I$S/rbc/edg
-$B/rbc/force/area_volume/imp.o: $S/rbc/force/area_volume/imp.cu; $N -I$S/. -I$S/rbc/force/area_volume
-$B/rbc/force/imp.o: $S/rbc/force/imp.cu; $N -I$S/. -I$S/rbc/force
-$B/rbc/gen/imp.o: $S/rbc/gen/imp.cpp; $X -I$S/. -I$S/rbc/gen
-$B/rbc/main/anti/imp.o: $S/rbc/main/anti/imp.cpp; $X -I$S/. -I$S/rbc/main/anti
-$B/rbc/main/imp.o: $S/rbc/main/imp.cpp; $X -I$S/. -I$S/rbc/main
-$B/rbc/rnd/api/imp.o: $S/rbc/rnd/api/imp.cpp; $X -I$S/. -I$S/rbc/rnd/api
-$B/rbc/rnd/imp.o: $S/rbc/rnd/imp.cpp; $X -I$S/. -I$S/rbc/rnd
-$B/rbc/stretch/imp.o: $S/rbc/stretch/imp.cu; $N -I$S/. -I$S/rbc/stretch
-$B/rig/imp.o: $S/rig/imp.cu; $N -I$S/. -I$S/rig
-$B/rigid/imp.o: $S/rigid/imp.cu; $N -I$S/. -I$S/rigid
-$B/rnd/imp.o: $S/rnd/imp.cpp; $X -I$S/. -I$S/rnd
-$B/scheme/force/imp.o: $S/scheme/force/imp.cu; $N -I$S/. -I$S/scheme/force
-$B/scheme/move/imp.o: $S/scheme/move/imp.cu; $N -I$S/. -I$S/scheme/move
-$B/scheme/restrain/imp.o: $S/scheme/restrain/imp.cpp; $X -I$S/. -I$S/scheme/restrain
-$B/scheme/restrain/sub/imp.o: $S/scheme/restrain/sub/imp.cu; $N -I$S/. -I$S/scheme/restrain/sub
-$B/scheme/restrain/sub/stat/imp.o: $S/scheme/restrain/sub/stat/imp.cpp; $X -I$S/. -I$S/scheme/restrain/sub/stat
-$B/scheme/restrain/sub/sum/imp.o: $S/scheme/restrain/sub/sum/imp.cpp; $X -I$S/. -I$S/scheme/restrain/sub/sum
-$B/sdf/imp.o: $S/sdf/imp.cu; $N -I$S/. -I$S/sdf
-$B/sdf/sub/imp.o: $S/sdf/sub/imp.cu; $N -I$S/. -I$S/sdf/sub
-$B/sim/imp.o: $S/sim/imp.cu; $N -I$S/. -I$S/sim
-$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/. -I$S/utils
-$B/utils/efopen.o: $S/utils/efopen.cpp; $X -I$S/. -I$S/utils
-$B/utils/error.o: $S/utils/error.cpp; $X -I$S/. -I$S/utils
-$B/utils/halloc.o: $S/utils/halloc.cpp; $X -I$S/. -I$S/utils
-$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/. -I$S/utils
-$B/utils/os.o: $S/utils/os.cpp; $X -I$S/. -I$S/utils
-$B/wall/exch/imp.o: $S/wall/exch/imp.cpp; $X -I$S/. -I$S/wall/exch
-$B/wall/force/imp.o: $S/wall/force/imp.cu; $N -I$S/. -I$S/wall/force
-$B/wall/imp.o: $S/wall/imp.cu; $N -I$S/. -I$S/wall
->>>>>>> 6e18123d20373f1341a8ed80eb7d480142b8b6d5
