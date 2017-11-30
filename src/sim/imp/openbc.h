018218cbf8187e57ee1b0d0069b217651e303a7d
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-30 11:25:21 +0100

    call download_ndead in sim

--- src/sim/imp/openbc.h
+++ src/sim/imp/openbc.h
@@ -3,4 +3,5 @@ void mark_outflow(const Flu *f, Outflow *o) {
     normal = make_float3(1, 0, 0);
     r = make_float3(XS/2-1, 0, 0);
     filter_particles_plane(normal, r, f->q.n, f->q.pp, /**/ o);
+    download_ndead(o);
 }
