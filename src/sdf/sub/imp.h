52e3c62f14a3c2f5f57eb612dc5d733bb987aea0
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 21:06:04 +0100

    minor

--- src/sdf/sub/imp.h
+++ src/sdf/sub/imp.h
@@ -1,7 +1,5 @@
-namespace sdf {
-namespace sub {
+namespace sdf { namespace sub {
 void ini(MPI_Comm cart, cudaArray *arrsdf, tex3Dca*);
 void bulk_wall(const tex3Dca, /*io*/ Particle *s_pp, int *s_n, /*o*/ Particle *w_pp, int *w_n);
 int  who_stays(const tex3Dca, Particle *pp, int n, int nc, int nv, /**/ int *stay);
-}
-}
+}}
