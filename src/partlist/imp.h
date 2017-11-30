87e4cdd4184527466705ca6536cde31ed95764ed
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-30 09:54:22 +0100

    move PartList to its own directory

---
+++ src/partlist/imp.h
@@ -0,0 +1,7 @@
+/* structure of particle                           */
+/* optional "deathlist" for particles to be killed */
+
+struct PartList {
+    const Particle *pp;
+    const int *deathlist;
+};
