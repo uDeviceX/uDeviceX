70b4334ecc697399271cd68c1b2c7c70c5bb063f
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 10:43:22 +0100

    create sdf/imp/main.h

--- src/u/sdf/make/dep.mk
+++ src/u/sdf/make/dep.mk
@@ -9,7 +9,7 @@ $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/msg.o: $S/msg.h $S/mpi/glb.h
-$B/sdf/imp.o: $S/inc/conf.h $S/utils/error.h $S/inc/type.h $S/sdf/imp.h $S/inc/macro.h $S/utils/cc.h $S/inc/def.h $S/sdf/sub/imp.h $S/inc/dev.h $S/msg.h $S/d/api.h $B/conf.h $S/sdf/type.h $S/mpi/glb.h $S/d/ker.h
+$B/sdf/imp.o: $S/inc/conf.h $S/utils/error.h $S/inc/type.h $S/sdf/imp.h $S/inc/macro.h $S/utils/cc.h $S/inc/def.h $S/sdf/sub/imp.h $S/inc/dev.h $S/msg.h $S/d/api.h $B/conf.h $S/sdf/imp/main.h $S/sdf/type.h $S/mpi/glb.h $S/d/ker.h
 $B/sdf/sub/imp.o: $S/inc/conf.h $S/inc/dev/wvel.h $S/utils/error.h $S/inc/type.h $S/d/q.h $S/sdf/sub/imp.h $S/inc/macro.h $S/utils/cc.h $S/inc/def.h $S/sdf/type.h $S/inc/dev.h $S/d/api.h $S/msg.h $B/conf.h $S/utils/halloc.h $S/utils/kl.h $S/field.h $S/glb/get.h $S/sdf/sub/dev/main.h $S/sdf/sub/dev/cheap.h $S/d/ker.h $S/mpi/glb.h
 $B/u/sdf/main.o: $S/inc/conf.h $S/inc/type.h $S/mpi/wrapper.h $S/utils/cc.h $S/u/sdf/dev.h $S/sdf/type.h $S/inc/dev.h $S/msg.h $S/d/api.h $B/conf.h $S/utils/kl.h $S/sdf/imp.h $S/mpi/glb.h $S/d/ker.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
