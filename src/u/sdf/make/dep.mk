841e251a902c04e511461ac27eee9ad0aec44d54
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 09:25:20 +0100

    initial import of of u/sdf

---
+++ src/u/sdf/make/dep.mk
@@ -0,0 +1,12 @@
+$B/d/api.o: $S/d/cpu/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/utils/halloc.h $S/d/cuda/imp.h
+$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
+$B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
+$B/mpi/wrapper.o: $S/mpi/wrapper.h
+$B/msg.o: $S/msg.h $S/mpi/glb.h
+$B/u/sdf/main.o: $S/inc/conf.h $S/utils/cc.h $S/u/sdf/dev.h $S/inc/dev.h $S/msg.h $S/d/api.h $B/conf.h $S/utils/kl.h $S/mpi/glb.h
+$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
+$B/utils/efopen.o: $S/utils/error.h $S/utils/efopen.h
+$B/utils/error.o: $S/msg.h $S/utils/error.h
+$B/utils/halloc.o: $S/utils/error.h $S/utils/halloc.h
+$B/utils/mc.o: $S/inc/conf.h $S/utils/error.h $S/utils/mc.h $B/conf.h
+$B/utils/os.o: $S/utils/os.h
---
+++ src/u/sdf/make/obj.mk
@@ -0,0 +1,3 @@
+O = $B/d/api.o $B/mpi/glb.o $B/mpi/type.o $B/mpi/wrapper.o $B/msg.o $B/u/sdf/main.o \
+    $B/utils/cc.o $B/utils/efopen.o $B/utils/error.o $B/utils/halloc.o \
+    $B/utils/mc.o $B/utils/os.o
