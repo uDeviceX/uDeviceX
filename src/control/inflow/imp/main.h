75396aa4d143b62e42abce22ca98e8d043097a99
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-30 14:15:27 +0100

    ini and fin

--- src/control/inflow/imp/main.h
+++ src/control/inflow/imp/main.h
@@ -1,9 +1,22 @@
-void ini(Inflow *i) {
-    
+void ini(int2 nc, Inflow *i) {
+    int n;
+    size_t sz;
+    n = nc.x * nc.y;
+    sz = n * sizeof(curandState_t);
+
+    CC(d::Malloc((void**) &i->rnds, sz));
+
+    sz = n * sizeof(float3);
+    CC(d::Malloc((void**) &i->flux, sz));
+
+    sz = n * sizeof(float);
+    CC(d::Malloc((void**) &i->cumflux, sz));
 }
 
 void fin(Inflow *i) {
-    
+    CC(d::Free(i->rnds));
+    CC(d::Free(i->flux));
+    CC(d::Free(i->cumflux));
 }
 
 void create_pp(Inflow *i, int *n, Cloud *c) {
