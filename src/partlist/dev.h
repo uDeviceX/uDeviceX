ab293710f29c138411271c0f13fa4066b38a66db
amlucas <lucas.amoudruz@wanadoo.fr>
2017-11-30 10:01:44 +0100

    is_alive

--- src/partlist/dev.h
+++ src/partlist/dev.h
@@ -3,3 +3,7 @@ static __device__ bool is_dead(int i, const PartList lp) {
         return lp.deathlist[i] == DEAD;
     return false;
 }
+
+static __device__ bool is_alive(int i, const PartList lp) {
+    return !is_dead(i, lp);
+}
