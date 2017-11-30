238f1d084101db0753257cce9b4deb96b69d72ac
Sergey Litvinov <slitvinov@gmail.com>
2017-11-30 13:59:03 +0100

    call sdf()

--- src/u/sdf/main.cu
+++ src/u/sdf/main.cu
@@ -13,30 +13,38 @@
 #include "inc/type.h"
 #include "inc/dev.h"
 #include "utils/cc.h"
+#include "inc/macro.h"
+#include "utils/kl.h"
 
 #include "sdf/imp.h"
-//#include "sdf/dev.h"
-#include "utils/kl.h"
+#include "sdf/type.h"
+#include "sdf/imp/type.h"
+#include "sdf/sub/dev/main.h"
+
 
 namespace dev {
 #include "dev.h"
 }
 
-void main0() {
+void main0(sdf::Quants *qsdf) {
+    float x, y, z;
+    x = y = z = 0;
+    KL(dev::main, (1, 1), (qsdf->texsdf, x, y, z));
+}
+
+void main1() {
     sdf::Quants *qsdf;
     sdf::alloc_quants(&qsdf);
     sdf::ini(m::cart, qsdf);
-
-    KL(dev::main, (1, 1), ());
-    dSync();
-
+    main0(qsdf);
     sdf::free_quants(qsdf);
+    dSync();
 }
 
 int main(int argc, char **argv) {
     m::ini(argc, argv);
     MSG("mpi size: %d", m::size);
-    main0();
+    main1();
     MSG("Hello world!");
     m::fin();
 }
