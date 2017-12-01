841853ea6a6a793b9c9d4c7be36a9c263f93a544
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-01 18:03:34 +0100

    add params inside Outflow

--- src/control/outflow/imp/type.h
+++ src/control/outflow/imp/type.h
@@ -1,5 +1,19 @@
+enum {
+    TYPE_NONE,
+    TYPE_PLANE,
+    TYPE_CIRCLE
+};
+
+union ParamsU {
+    plane::Params plane;
+    circle::Params circle;
+};
+
 struct Outflow {
     int *kk;        /* die or stay alive?      */
     int *ndead_dev; /* number of kills on dev  */
     int ndead;      /* number of kils          */
+
+    ParamsU params; /* shape parameters */
+    int type;       /* shape type       */
 };
