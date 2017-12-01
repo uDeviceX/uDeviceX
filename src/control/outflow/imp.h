72a3ffba4af19c59f81ffc003c17c5158ced6396
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-01 18:17:51 +0100

    add to interface

--- src/control/outflow/imp.h
+++ src/control/outflow/imp.h
@@ -1,10 +1,15 @@
 struct Outflow;
 
 void ini(int maxp, /**/ Outflow **o);
+void ini_params_circle(float R, Outflow *o);
+void ini_params_plane(int dir, float r0, Outflow *o);
+
 void fin(/**/ Outflow *o);
 
 void filter_particles_circle(float R, int n, const Particle *pp, Outflow *o);
 void filter_particles_plane(float3 normal, float3 r, int n, const Particle *pp, Outflow *o);
+
+void filter_particles(int n, const Particle *pp, /**/ Outflow *o);
 
 void download_ndead(Outflow *o);
 
