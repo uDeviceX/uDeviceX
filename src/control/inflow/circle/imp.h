5312881aa207cce2b6a53487041bbf9085ff43de
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-04 18:28:07 +0100

    ini for params

--- src/control/inflow/circle/imp.h
+++ src/control/inflow/circle/imp.h
@@ -6,6 +6,22 @@ void ini_params_circle(float3 o, float R, float H, float u, bool poiseuille,
 
     pp  = &i->p.circle;
     vpp = &i->vp.circle;
+
+    float3 mi;
+    mi.x = (m::coords[X] + 0.5) * XS;
+    mi.y = (m::coords[Y] + 0.5) * YS;
+    mi.z = (m::coords[Z] + 0.5) * ZS;
+
+    // TODO
+    // shift to local referential
+    // axpy(-1.0, &mi, /**/ &o);
+    
+    pp->o = o;
+    pp->R = R;
+    pp->H = H;
+    // TODO
+    pp->th0 = 0;
+    pp->dth = 2 * M_PI;
     
     vpp->u = u;
     vpp->poiseuille = poiseuille;
