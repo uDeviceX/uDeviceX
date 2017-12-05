270644493932e5b6f33c53b070a0506c4c5bb753
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-05 18:39:20 +0100

    add functions to interface

--- src/control/den/imp.h
+++ src/control/den/imp.h
@@ -1,1 +1,15 @@
 struct DCont;
+struct DContMap;
+
+void ini(int maxp, /**/ DCont **d0);
+void ini(DContMap **m0);
+
+void fin(DCont *d);
+void fin(DContMap *m);
+
+void filter_particles(const DContMap *m, const int *starts, const int *counts, /**/ DCont *d);
+void download_ndead(DCont *d);
+
+
+int* get_deathlist(DCont *d);
+int  get_ndead(DCont *d);
