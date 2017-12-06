337404667679eedd7e39435d1a47da275336b1fc
Sergey Litvinov <slitvinov@gmail.com>
2017-12-06 11:05:57 +0100

    initial importo of ply2vtk

---
+++ poc/ply2vtk/endian.h
@@ -0,0 +1,28 @@
+/* From little endian to bigendian
+   http://www.gamedev.net/page/resources/_/technical/game-programming/writing-endian-independent-code-in-c-r2091 */
+
+float FloatSwap(float f) {
+  union {
+    float f;
+    unsigned char b[4];
+  } dat1, dat2;
+
+  dat1.f = f;
+  dat2.b[0] = dat1.b[3];
+  dat2.b[1] = dat1.b[2];
+  dat2.b[2] = dat1.b[1];
+  dat2.b[3] = dat1.b[0];
+  return dat2.f;
+}
+
+int LongSwap (int i)
+{
+  unsigned char b1, b2, b3, b4;
+
+  b1 = i & 255;
+  b2 = ( i >> 8 ) & 255;
+  b3 = ( i>>16 ) & 255;
+  b4 = ( i>>24 ) & 255;
+
+  return ((int)b1 << 24) + ((int)b2 << 16) + ((int)b3 << 8) + b4;
+}
