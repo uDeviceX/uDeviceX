ea035bc806eb626338d36ea69c3c5dbaed8abc68
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-06 17:10:50 +0100

    add generator for radial tests

---
+++ post/binning/data/gen_radial.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int N = 400;
+
+void header() {
+    FILE *f = fopen("rad.bop", "w");
+    fprintf(f, "%d\n"
+            "DATA_FILE: rad.values\n"
+            "DATA_FORMAT: float\n"
+            "VARIABLES: x y z vx vy vz\n",
+            N);
+    fclose(f);
+}
+
+void body() {
+    FILE *f = fopen("rad.values", "wb");
+    int i, j;
+    float x, y, z;
+    srand(0);
+
+    float pp[6*N];
+    
+    for (i = j = 0; i < N; ++i) {
+        x = drand48();
+        y = drand48();
+        z = drand48();
+        pp[j++] = x;
+        pp[j++] = y;
+        pp[j++] = z;
+
+        pp[j++] = y;
+        pp[j++] = -x;
+        pp[j++] = 0;
+    }
+
+    fwrite(pp, sizeof(float), 6*N, f);
+    
+    fclose(f);
+}
+
+int main() {
+    header();
+    body();
+}
