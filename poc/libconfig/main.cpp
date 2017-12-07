cd0bc0778ac3682f70ae47bb669a6cfadd331daf
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-07 10:07:47 +0100

    add example

--- poc/libconfig/main.cpp
+++ poc/libconfig/main.cpp
@@ -3,7 +3,20 @@
 
 int main(int argc, char **argv) {
     config_t cfg;
+    int i;
     config_init(&cfg);
+
+    for (i = 1; i < argc; ++i) {
+        printf("parsing %s\n", argv[i]);
+        config_read_string(&cfg, argv[i]);
+    }
+
+    int a, b;
+    config_lookup_int(&cfg, "a", &a);
+    config_lookup_int(&cfg, "b", &b);
+
+    printf("a = %d, b = %d\n", a, b);
+    
     config_destroy(&cfg);
     return 0;
 }
