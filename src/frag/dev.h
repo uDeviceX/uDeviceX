5080758ebb3fb4162ea581e1eee95970b15ccbfd
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-07 14:15:16 +0100

    add fid from k_common on frags

---
+++ src/frag/dev.h
@@ -0,0 +1,8 @@
+__device__ inline int get_fid(const int a[], const int i) {
+    /* [f]ragment [id] : where is `i' in sorted a[27]? */
+    int k1, k3, k9;
+    k9 = 9 * ((i >= a[9])           + (i >= a[18]));
+    k3 = 3 * ((i >= a[k9 + 3])      + (i >= a[k9 + 6]));
+    k1 =      (i >= a[k9 + k3 + 1]) + (i >= a[k9 + k3 + 2]);
+    return k9 + k3 + k1;
+}
