aa2dbb06a4b444aa42240e5470658bb33523d509
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-08 15:36:06 +0100

    add union type; add const vel

--- src/wvel/dev.h
+++ src/wvel/dev.h
@@ -1,6 +1,10 @@
-static __device__ void wvel_shear(WvelParam p, Coords c, float3 r, /**/ float3 *v) {
+static __device__ void wvel_cste(WvelPar p, Coords c, float3 r, /**/ float3 *v) {
+    *v = p.cste.u;
+}
+
+static __device__ void wvel_shear(WvelPar p, Coords c, float3 r, /**/ float3 *v) {
     float3 rc; // relative to center
-    float3 gdot = p.p.shear.gdot;
+    float3 gdot = p.shear.gdot;
     local2center(c, r, /**/ &rc);
 
     v->x = gdot.x * rc.x;
@@ -8,12 +12,12 @@ static __device__ void wvel_shear(WvelParam p, Coords c, float3 r, /**/ float3 *
     v->z = gdot.z * rc.z;
 }
 
-static __device__ void bounce_vel(WvelParam p, Coords c, float3 rw, /* io */ float3* v) {
+static __device__ void bounce_vel(Wvel wvel, Coords c, float3 rw, /* io */ float3* v) {
     float3 vw;
     int type;
-    wvel_fun wvel_funs[] = {&wvel_shear};
-    type = p.type;
-    wvel_funs[type](p, c, rw, /**/ &vw);
+    wvel_fun wvel_funs[] = {&wvel_cste, &wvel_shear};
+    type = wvel.type;
+    wvel_funs[type](wvel.p, c, rw, /**/ &vw);
 
     v->x = 2 * vw.x - v->x;
     v->y = 2 * vw.y - v->y;
--- src/wvel/type.h
+++ src/wvel/type.h
@@ -1,12 +1,25 @@
+struct WvelCste {
+    float3 u;
+};
+
 struct WvelShear {
     float3 gdot; // shear rate in all three directions
 };
 
-struct WvelParam {
-    union {
-        WvelShear shear;
-    } p;
+enum {
+    WALL_VEL_CSTE,
+    WALL_VEL_SHEAR,
+};
+
+union WvelPar {
+    WvelCste cste;
+    WvelShear shear;
+};
+
+
+struct Wvel {
+    WvelPar p;
     int type;
 };
 
-typedef void (*wvel_fun) (WvelParam, Coords, float3, /**/ float3*); 
+typedef void (*wvel_fun) (WvelPar, Coords, float3, /**/ float3*); 
