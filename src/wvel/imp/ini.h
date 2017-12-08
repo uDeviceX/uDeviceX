3a808f19fc0730993f3751081cf797ff6638a32c
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-08 17:00:13 +0100

    cleanup; deps; add file

---
+++ src/wvel/imp/ini.h
@@ -0,0 +1,17 @@
+void ini(WvelCste p, Wvel *vw) {
+    vw->type = WALL_VEL_CSTE;
+    vw->p.cste = p;
+    step2params(0, /**/ vw);
+}
+
+void ini(WvelShear p, Wvel *vw) {
+    vw->type = WALL_VEL_SHEAR;
+    vw->p.shear = p;
+    step2params(0, /**/ vw);
+}
+
+void ini(WvelShearSin p, Wvel *vw) {
+    vw->type = WALL_VEL_SHEAR_SIN;
+    vw->p.shearsin = p;
+    step2params(0, /**/ vw);
+}
