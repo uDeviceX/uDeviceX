8b8b22d5ec6a5757d82c0d3c9f5c68a30b4bc40a
Sergey Litvinov <slitvinov@gmail.com>
2017-12-11 19:36:52 +0100

    add env2float functions

--- src/utils/os.cpp
+++ src/utils/os.cpp
@@ -28,4 +28,47 @@ long time() { return ::time(NULL); }
 void   srand(long int seedval) { srand48(seedval); }
 double drand() { return drand48(); }
 
+enum {OK, NOT_SET, NOT_FLOAT};
+static int env2float0(const char *key, /**/ const char **pval, float *px) {
+    float x;
+    if ((*pval = getenv(key)) == NULL)
+        return NOT_SET;
+    if (sscanf(*pval, "%f", &x) != 1)
+        return NOT_FLOAT;
+    return OK;
 }
+
+void env2float(const char *key, float *px) {
+    int status;
+    const char *val;
+    status = env2float0(key, /**/ &val, px);
+    if (status == OK) {
+        /**/
+    } else if (status == NOT_SET) {
+        ERR("env. variable is not set: `%s`", key, val);
+    } else if (status == NOT_FLOAT) {
+        ERR("env. variable `%s = %s` is not float", key, val);
+    } else {
+        ERR("unknown status");
+    }
+    MSG("env %s = %g\n", key, *px);
+}
+
+void env2float_d(const char *key, float def, /**/ float *px) { /* with default */
+    int status;
+    const char *val;
+    status = env2float0(key, /**/ &val, px);
+    if (status == OK) {
+        /**/
+    } else if (status == NOT_SET) {
+        *px = def;
+    } else if  (status == NOT_FLOAT) {
+        ERR("env. variable `%s = %s` is not float", key, val);
+    } else {
+        ERR("unknown status");
+    }
+    MSG("env %s = %g\n", key, *px);    
+}
+
+
+} /* namespace */
--- src/utils/os.h
+++ src/utils/os.h
@@ -3,4 +3,9 @@ void mkdir(const char* path);
 long time();
 void srand(long int seedval);
 double drand();
+
+/* get an environment variable
+   *_d: with default */
+void env2float  (const char*,            float *px);
+void env2float_d(const char*, float def, float *px);
 }
