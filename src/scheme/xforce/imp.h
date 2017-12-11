7b950fd26968719e97a7086f5a87b279bcd63565
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-11 18:23:01 +0100

    add interface

--- src/scheme/xforce/imp.h
+++ src/scheme/xforce/imp.h
@@ -1,1 +1,7 @@
+void ini_none(/**/ FParam *p);
+void ini(FParam_cste_d par, /**/ FParam *p);
+void ini(FParam_dp_d par, /**/ FParam *p);
+void ini(FParam_shear_d par, /**/ FParam *p);
+void ini(FParam_rol_d par, /**/ FParam *p);
+
 void body_force(Coords c, float mass, FParam fpar, int n, const Particle *pp, /**/ Force *ff);
