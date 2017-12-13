7928b2844372286f658c597904bc4c3bf143f774
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-13 09:09:40 +0100

    add structures to be hold on host

--- src/scheme/force/type.h
+++ src/scheme/force/type.h
@@ -42,9 +42,56 @@ union BForceParam_v {
     BForce_rad_v rad;
 };
 
-/* structure containing parameters on host */
-
 struct BForce_v {
     BForceParam_v p;
+    int type;
+};
+
+/* structure containing parameters on host */
+
+/* constant force f */
+struct BForce_cste {
+    float3 a;
+};
+
+/* double poiseuille */
+struct BForce_dp {
+    float a;
+};
+
+/* shear force fx = a * (y - yc) */
+struct BForce_shear {
+    float a;
+};
+
+/* 4 rollers mill */
+struct BForce_rol {
+    float a;
+};
+
+/* radial force decaying as 1/r */
+struct BForce_rad {
+    float a;
+};
+
+enum {
+    BODY_FORCE_NONE,
+    BODY_FORCE_CSTE,
+    BODY_FORCE_DP,
+    BODY_FORCE_SHEAR,
+    BODY_FORCE_ROL,
+    BODY_FORCE_RAD
+};
+
+union BForceParam {
+    BForce_cste cste;
+    BForce_dp dp;
+    BForce_shear shear;
+    BForce_rol rol;
+    BForce_rad rad;
+};
+
+struct BForce {
+    BForceParam p;
     int type;
 };
