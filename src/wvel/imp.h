5dff93256bcd13b30712bf4971562446b1404fda
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-12-13 10:53:10 +0100

    include interface Wvel doc

--- src/wvel/imp.h
+++ src/wvel/imp.h
@@ -1,7 +1,8 @@
+// tag::interface[]
 void ini(WvelCste p, Wvel *vw);
 void ini(WvelShear p, Wvel *vw);
 void ini(WvelShearSin p, Wvel *vw);
 void ini(WvelHS p, Wvel *vw);
 
 void step2params(long it, const Wvel *wv, /**/ Wvel_v *view);
-
+// end::interface[]
