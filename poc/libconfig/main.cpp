60b8415613b4040903282101e564cb2fd482a1cd
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-13 18:14:27 +0100

    adding Config struct

--- poc/libconfig/main.cpp
+++ poc/libconfig/main.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <libconfig.h>
+
+struct Config {
+    config_t args;
+    config_t file;
+};
+
+void conf_ini(/**/ Config *c);
+void conf_read_args(int argc, char **argv, /**/ Config *c);
+void conf_read_file(const char *fname, /**/ Config *c);
+void conf_destroy(/**/ Config *c);
 
 int main(int argc, char **argv) {
     config_t cfg;
@@ -19,4 +30,36 @@ int main(int argc, char **argv) {
     
     config_destroy(&cfg);
     return 0;
+}
+
+void conf_ini(/**/ Config *c) {
+    config_init(&c->args);
+    config_init(&c->file);
+}
+
+void conf_read_args(int argc, char **argv, /**/ Config *c) {
+    enum {MAX_CHAR=100000};
+    char *args, *a;
+    int i;    
+    args = new char[MAX_CHAR];
+    args[0] = '\0';
+
+    for(i = 0; i < argc; ++i) {
+        a = argv[i];
+        strcat(args, a);
+        strcat(args, " ");
+    }
+
+    config_read_string(&c->args, args);
+    
+    delete[] args;    
+}
+
+void conf_read_file(const char *fname, /**/ Config *c) {
+    config_read_file(&c->file, fname);
+}
+
+void conf_destroy(/**/ Config *c) {
+    config_destroy(&c->args);
+    config_destroy(&c->file);
 }
