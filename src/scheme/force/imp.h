38ffbc1533185ac8c48abf7e83683cf8027ff7e6
Sergey Litvinov <slitvinov@gmail.com>
2017-12-13 10:49:54 +0100

    minor

--- src/scheme/force/imp.h
+++ src/scheme/force/imp.h
@@ -5,10 +5,9 @@ void ini(BForce_shear par, /**/ BForce *p);
 void ini(BForce_rol par, /**/ BForce *p);
 void ini(BForce_rad par, /**/ BForce *p);
 
-/* not polymorphic for now        */
+/* TODO not polymorphic for now   */
 /* related to velocity controller */
 void adjust(float3 f, /**/ BForce *fpar);
 
 void get_view(long it, BForce bforce, /**/ BForce_v *view);
 void body_force(Coords c, float mass, BForce_v fpar, int n, const Particle *pp, /**/ Force *ff);
-
