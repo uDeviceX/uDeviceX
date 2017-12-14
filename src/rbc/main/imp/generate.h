bd9026c986e1b3dcba3f6a4aecaae4aef47857f5
Sergey Litvinov <slitvinov@gmail.com>
2017-12-14 21:45:33 +0100

    undo allocation

--- src/rbc/main/imp/generate.h
+++ src/rbc/main/imp/generate.h
@@ -2,10 +2,9 @@ static void gen(const char *cell, const char *ic, int nv, /**/ int *pnc, Particl
     int nc, sz;
     Particle *hst;
     sz = sizeof(Particle)*MAX_CELL_NUM*nv;
-    UC(emalloc(sz, (void**)&hst));
+    UC(emalloc(sz, (void**)hst));
 
     nc = rbc::gen::main(cell, ic, nv, /**/ hst);
-    MSG("nc = %d, nv = %d", nc, nv);
     if (nc) cH2D(dev, hst, nc*nv);
 
     UC(efree(hst));
@@ -13,9 +12,10 @@ static void gen(const char *cell, const char *ic, int nv, /**/ int *pnc, Particl
 }
 
 static void setup_from_pos(MPI_Comm comm, const char *cell, const char *ic, int nv, /**/
-                           Particle *pp, int *pnc, int *pn) {
+                           Particle *pp, int *pnc, int *pn, /* storage */ Particle *pp_hst) {
     int nc;
-    UC(gen(cell, ic, nv, /**/ &nc, pp));
+    nc = rbc::gen::main(cell, ic, nv, pp_hst);
+    if (nc) cH2D(pp, pp_hst, nv * nc);
     MC(m::Barrier(comm));
     *pnc = nc; *pn = nc * nv;
 }
@@ -32,10 +32,10 @@ void gen_quants(MPI_Comm comm, const char *cell, const char *ic, /**/ Quants *q)
     md = RBCmd;
     nt = RBCnt;
     nv = RBCnv;
-    UC(setup(md, nt, nv, cell, /**/
-             q->shape.anti, q->shape.edg, &q->shape.totArea,
-             q->tri_hst, q->tri, q->adj0, q->adj1));
-    UC(setup_from_pos(comm, cell, ic, q->nv, /**/ q->pp, &q->nc, &q->n));
+    setup(md, nt, nv, cell, /**/
+          q->shape.anti, q->shape.edg, &q->shape.totArea,
+          q->tri_hst, q->tri, q->adj0, q->adj1);
+    setup_from_pos(comm, cell, ic, q->nv, /**/ q->pp, &q->nc, &q->n, /*w*/ q->pp_hst);
     if (rbc_ids)
         gen_ids(comm, q->nc, /**/ q->ii);
 }
