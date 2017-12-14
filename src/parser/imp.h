ed90a4f6853cdca0fcd0e9722dd7a77dd7ddc218
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-14 18:12:55 +0100

    adding doc for parser

--- src/parser/imp.h
+++ src/parser/imp.h
@@ -1,5 +1,6 @@
 struct Config;
 
+// tag::interface[]
 void conf_ini(/**/ Config **c);
 void conf_destroy(/**/ Config *c);
 
@@ -10,3 +11,4 @@ void conf_lookup_int(const Config *c, const char *desc, int *a);
 void conf_lookup_float(const Config *c, const char *desc, float *a);
 void conf_lookup_bool(const Config *c, const char *desc, int *a);
 void conf_lookup_string(const Config *c, const char *desc, const char **a);
+// end::interface[]
