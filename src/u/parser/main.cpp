7482c1fdea9300695ce308e80c4ed4567d609477
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-15 17:45:41 +0100

    add simple extract

--- src/u/parser/main.cpp
+++ src/u/parser/main.cpp
@@ -5,6 +5,16 @@
 #include "utils/error.h"
 #include "parser/imp.h"
 
+static void extract(const Config *c) {
+    int a;
+    const char *s;
+
+    UC(conf_lookup_int(c, "a", &a));
+    UC(conf_lookup_string(c, "s", &s));
+
+    printf("%d %s\n", a, s);
+}
+
 int main(int argc, char **argv) {
     Config *cfg;
     m::ini(&argc, &argv);
@@ -12,6 +22,8 @@ int main(int argc, char **argv) {
     conf_ini(/**/ &cfg);
     conf_read(argc, argv, /**/ cfg);
 
+    UC(extract(cfg));
+    
     conf_destroy(/**/ cfg);    
 
     m::fin();
