300aaee754c4960761259c959ec33a0624ebc158
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2017-12-19 17:14:46 +0100

    document linal

--- src/math/linal/imp.h
+++ src/math/linal/imp.h
@@ -1,2 +1,4 @@
+// tag::interface[]
 /* invert symmetric matrix m0[6] */
-void linal_inv3x3(float *m0, /**/ float *minv0); 
+void linal_inv3x3(float *m0, /**/ float *minv0); // <1>
+// end::interface[]
