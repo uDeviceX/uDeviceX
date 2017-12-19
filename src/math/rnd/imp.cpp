87bd78cf8582b1cdffa90b8b04f2f06ac8f33f45
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-19 14:17:03 +0100

    add implementation

---
+++ src/math/rnd/imp.cpp
@@ -0,0 +1,45 @@
+#include <limits>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "utils/error.h"
+#include "utils/imp.h"
+
+#include "imp.h"
+
+typedef uint32_t integer;
+
+struct RNDunif {
+    integer x, y, z, c;
+};
+
+void rnd_ini(int x, int y, int z, int c, /**/ RNDunif **r0) {
+    RNDunif *r;
+    UC(emalloc(sizeof(RNDunif), (void**) r0));
+
+    r = *r0;
+    r->x = x;
+    r->y = y;
+    r->z = z;
+    r->c = c;
+}
+
+void rnd_fin(RNDunif *r) {
+    UC(efree(r));
+}
+
+static integer get_int(RNDunif *r) {
+    uint64_t t, a = 698769069ULL;
+    
+    r->x = 69069 * r->x + 12345;
+    r->y ^= ( r->y << 13 );
+    r->y ^= ( r->y >> 17 );
+    r->y ^= ( r->y << 5 ); /* y must never be set to zero! */
+    t = a * r->z + r->c;
+    r->c = ( t >> 32 ); /* Also avoid setting z=c=0! */
+    return r->x + r->y + ( r->z = t );
+}
+
+float rnd_get(RNDunif *r) {
+    return get_int(r) / float( std::numeric_limits<integer>::max() );
+}
--- src/math/rnd/imp.h
+++ src/math/rnd/imp.h
@@ -3,4 +3,4 @@ struct RNDunif;
 void rnd_ini(int x, int y, int z, int c, /**/ RNDunif **r);
 void rnd_fin(RNDunif *r);
 
-float get(const RNDunif *r);
+float rnd_get(RNDunif *r);
