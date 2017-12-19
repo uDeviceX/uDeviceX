8f4e7f163025b5e464f179811f7647fa06b11f8d
Sergey Litvinov <slitvinov@gmail.com>
2017-12-19 15:04:11 +0100

    unconst simple integers

--- src/io/bop/imp.h
+++ src/io/bop/imp.h
@@ -7,8 +7,8 @@ struct Ticket {
 void ini(Ticket *t);
 void fin(Ticket *t);
 
-void parts(MPI_Comm cart, const Particle *pp, const long n, const char *name, const int step, /*w*/ Ticket *t);
-void parts_forces(MPI_Comm cart, const Particle *pp, const Force *ff, const long n, const char *name, const int step, /*w*/ Ticket *t);
-void ids(MPI_Comm cart, const int *ii, const long n, const char *name, const int step);
-void colors(MPI_Comm cart, const int *ii, const long n, const char *name, const int step);
+void parts(MPI_Comm cart, const Particle *pp, long n, const char *name, int step, /*w*/ Ticket *t);
+void parts_forces(MPI_Comm cart, const Particle *pp, const Force *ff, long n, const char *name, int step, /*w*/ Ticket *t);
+void ids(MPI_Comm cart, const int *ii, long n, const char *name, int step);
+void colors(MPI_Comm cart, const int *ii, long n, const char *name, int step);
 }
