f1181e60d42750ce1ad63194629450161f1adec6
amlucas <lucas.amoudruz@wanadoo.fr>
2017-12-21 12:55:55 +0100

    use enum

--- src/clist/code.h
+++ src/clist/code.h
@@ -1,6 +1,9 @@
 namespace clist {
 
-#define NMAX (256*256*256)
+enum {
+    NMAX = (256*256*256)
+};
+
 static __device__ uint encode_id(int s, int pid) {
     /* pid: particle id; s: array id */
     assert(pid < NMAX);
@@ -12,6 +15,5 @@ static __device__ void decode_id(uint c, /**/ int *s, int *pid) {
     *pid = c % NMAX;
     *s   = c / NMAX;
 }
-#undef NMAX
 
 } // clist
