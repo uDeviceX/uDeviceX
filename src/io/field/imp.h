8158cd7f2cd2159139fa05cd9becbc7dfe71f736
Sergey Litvinov <slitvinov@gmail.com>
2017-12-22 11:45:59 +0100

    minor

--- src/io/field/imp.h
+++ src/io/field/imp.h
@@ -1,4 +1,4 @@
 namespace io { namespace field {
 void dump(MPI_Comm cart, Particle *p, int n);
-void scalar(MPI_Comm cart, float *data, const char *channelname);
+void scalar(MPI_Comm cart, float *data, const char *path);
 }}
