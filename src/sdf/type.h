d8c85bd43d210c166c48240cfec84a538fe741f6
Sergey Litvinov <slitvinov@gmail.com>
2017-12-22 14:56:17 +0100

    back to type

--- src/sdf/type.h
+++ src/sdf/type.h
@@ -1,26 +1,26 @@
 /* 3D texture object binded to  cuda array */
-typedef cudaTextureObject_t tex3Dca;
-static void ini(tex3Dca *to, cudaArray *ca) {
-    cudaResourceDesc resD;
-    cudaTextureDesc  texD;
-
-    memset(&resD, 0, sizeof(resD));
-    resD.resType = cudaResourceTypeArray;
-    resD.res.array.array = ca;
+struct tex3Dca {
+    cudaTextureObject_t to;
     
-    memset(&texD, 0, sizeof(texD));
-    texD.normalizedCoords = 0;
-    texD.filterMode = cudaFilterModePoint;
-    texD.mipmapFilterMode = cudaFilterModePoint;
-    texD.addressMode[0] = cudaAddressModeWrap;
-    texD.addressMode[1] = cudaAddressModeWrap;
-    texD.addressMode[2] = cudaAddressModeWrap;
-    
-    CC(cudaCreateTextureObject(to, &resD, &texD, NULL));
-}
+    void setup(cudaArray *ca) {
+        cudaResourceDesc resD;
+        cudaTextureDesc  texD;
 
-static void fin(tex3Dca *to) {
-    //CC(cudaDestroyTextureObject(*to));
-}
+        memset(&resD, 0, sizeof(resD));
+        resD.resType = cudaResourceTypeArray;
+        resD.res.array.array = ca;
+        
+        memset(&texD, 0, sizeof(texD));
+        texD.normalizedCoords = 0;
+        texD.filterMode = cudaFilterModePoint;
+        texD.mipmapFilterMode = cudaFilterModePoint;
+        texD.addressMode[0] = cudaAddressModeWrap;
+        texD.addressMode[1] = cudaAddressModeWrap;
+        texD.addressMode[2] = cudaAddressModeWrap;
 
+        CC(cudaCreateTextureObject(&to, &resD, &texD, NULL));
+    }
+
+    void destroy() {CC(cudaDestroyTextureObject(to));}
+};
 
