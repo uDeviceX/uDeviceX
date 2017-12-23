19ff187ed6a2d440fba4f74ef589da612bbf8d32
Sergey Litvinov <slitvinov@gmail.com>
2017-12-23 07:36:50 +0100

    add tex3d_copy

--- src/sdf/tex3d/imp.cu
+++ src/sdf/tex3d/imp.cu
@@ -24,4 +24,20 @@ void tex3d_fin(Tex3d *q) {
 }
 
 void tex3d_copy(Array3d *a, /**/ Tex3d *t) {
+    cudaResourceDesc resD;
+    cudaTextureDesc  texD;
+
+    memset(&resD, 0, sizeof(resD));
+    resD.resType = cudaResourceTypeArray;
+    resD.res.array.array = a->a;
+
+    memset(&texD, 0, sizeof(texD));
+    texD.normalizedCoords = 0;
+    texD.filterMode = cudaFilterModePoint;
+    texD.mipmapFilterMode = cudaFilterModePoint;
+    texD.addressMode[0] = cudaAddressModeWrap;
+    texD.addressMode[1] = cudaAddressModeWrap;
+    texD.addressMode[2] = cudaAddressModeWrap;
+
+    CC(cudaCreateTextureObject(&t->t, &resD, &texD, NULL));
 }
