45980fb2663eae33f26c1745b8b883137b1f6778
Sergey Litvinov <slitvinov@gmail.com>
2017-12-23 12:15:56 +0100

    declare to_view

--- src/sdf/imp.h
+++ src/sdf/imp.h
@@ -1,8 +1,10 @@
 struct Sdf;
+struct Sdf_v;
+
 void ini(Sdf**);
 void fin(Sdf*);
-
 void gen(MPI_Comm cart, Sdf*);
+void to_view(Sdf*, Sdf_v*);
 
 void bulk_wall(const Sdf*, /*io*/ Particle *s_pp, int *s_n, /*o*/ Particle *w_pp, int *w_n);
 int who_stays(const Sdf*, Particle *pp, int n, int nc, int nv, int *stay);
