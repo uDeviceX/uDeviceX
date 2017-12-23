8a21e64c0a40b276c418662cb593a2f46e078b60
Sergey Litvinov <slitvinov@gmail.com>
2017-12-23 09:47:23 +0100

    rm ini

--- src/sdf/type.h
+++ src/sdf/type.h
@@ -6,27 +6,9 @@ struct tex3Dca {
 
     void setup(cudaArray *ca) {
         tex3d_ini(&t);
-
-        cudaResourceDesc resD;
-        cudaTextureDesc  texD;
-
-        memset(&resD, 0, sizeof(resD));
-        resD.resType = cudaResourceTypeArray;
-        resD.res.array.array = ca;
-
-        memset(&texD, 0, sizeof(texD));
-        texD.normalizedCoords = 0;
-        texD.filterMode = cudaFilterModePoint;
-        texD.mipmapFilterMode = cudaFilterModePoint;
-        texD.addressMode[0] = cudaAddressModeWrap;
-        texD.addressMode[1] = cudaAddressModeWrap;
-        texD.addressMode[2] = cudaAddressModeWrap;
-
-        CC(cudaCreateTextureObject(&to, &resD, &texD, NULL));
     }
 
     void destroy() {
         tex3d_fin(t);
-        CC(cudaDestroyTextureObject(to));
     }
 };
