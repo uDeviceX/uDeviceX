270a70a8964879ed97c536827a25d005ce10fc76
Sergey Litvinov <slitvinov@gmail.com>
2017-12-23 07:57:24 +0100

    deps

--- src/wall/force/imp.cu
+++ src/wall/force/imp.cu
@@ -30,10 +30,9 @@
 #include "wvel/dev.h"
 
 #include "sdf/type.h"
-
+#include "sdf/dev.h"
 #include "sdf/tex3d/type.h"
 #include "sdf/tex3d/dev.h"
-
 #include "sdf/imp.h"
 
 #include "forces/type.h"
