9747bccdfb08b65ae6e27aa076430a44020b7709
Sergey Litvinov <slitvinov@gmail.com>
2017-12-23 07:19:53 +0100

    wip: tex3d

--- src/sdf/tex3d/imp.h
+++ src/sdf/tex3d/imp.h
@@ -1,4 +1,4 @@
 struct Tex3d;
-void tex3d_ini(Tex3d**, int x, int y, int z);
+void tex3d_ini(Tex3d**);
 void tex3d_fin(Tex3d*);
-void tex3d_copy(int x, int y, int z, float *D, /**/ Tex3d*);
+void tex3d_copy(Array3d*, /**/ Tex3d*);
