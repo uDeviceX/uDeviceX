bc291baa9f861a5972bfa91efadfb37e5fd13635
Sergey Litvinov <slitvinov@gmail.com>
2017-12-23 07:33:54 +0100

    minor

--- src/sdf/tex3d/dev.h
+++ src/sdf/tex3d/dev.h
@@ -1,3 +1,3 @@
-static __device__ float fetch(Tex3d q, float i, float j, float k) {
+static __device__ float tex3d_fetch(Tex3d q, float i, float j, float k) {
     return Ttex3D(float, q->t, i, j, k);
 }
