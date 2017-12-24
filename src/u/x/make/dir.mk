c5f62da5c42162bae0c3e066df6a365fae590c41
Sergey Litvinov <slitvinov@gmail.com>
2017-12-24 21:39:00 +0100

    dep

--- src/u/x/make/dir.mk
+++ src/u/x/make/dir.mk
@@ -51,7 +51,6 @@ D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
     d $B/rbc/rnd                  && \
     d $B/rbc/rnd/api              && \
     d $B/rbc/stretch              && \
-    d $B/rig                      && \
     d $B/rigid                    && \
     d $B/scheme/force             && \
     d $B/scheme/move              && \
