265adca73f2124f6408a46f62b2942000a866b3e
Sergey Litvinov <slitvinov@gmail.com>
2017-12-24 17:18:00 +0100

    minor

--- src/sdf/imp.h
+++ src/sdf/imp.h
@@ -7,5 +7,5 @@ void gen(MPI_Comm cart, Sdf*);
 void to_view(Sdf*, /**/ Sdf_v*);
 
 void bulk_wall(const Sdf*, /*io*/ Particle *s_pp, int *s_n, /*o*/ Particle *w_pp, int *w_n);
-int who_stays(const Sdf*, Particle *pp, int n, int nc, int nv, int *stay);
+int who_stays(const Sdf*, Particle *pp, int n, int nc, int nv, /**/ int *stay);
 void bounce(Wvel_v wv, Coords c, const Sdf*, int n, /**/ Particle*);
