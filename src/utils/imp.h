00468b1ce76a8b1085fd8c5afcd68ca236dcd485
Sergey Litvinov <slitvinov@gmail.com>
2017-12-26 11:24:42 +0100

    minor

--- src/utils/imp.h
+++ src/utils/imp.h
@@ -5,4 +5,4 @@ void efopen(const char *fname, const char *mode, /**/ FILE**);
 void efclose(FILE*);
 void efread(void *ptr, size_t size, size_t nmemb, FILE*);
 void efwrite(const void *ptr, size_t size, size_t nmemb, FILE*);
-void efgets(char *s, int size, FILE);
+void efgets(char *s, int size, FILE*);
