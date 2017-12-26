f1f54d97696d0f5745136ed19a6f1501b94a9d22
Sergey Litvinov <slitvinov@gmail.com>
2017-12-26 09:33:11 +0100

    minor

--- src/utils/imp.h
+++ src/utils/imp.h
@@ -1,5 +1,5 @@
 int emalloc(size_t, /**/ void **);
 void efree(void*);
 
-int efopen(const char *fname, const char *mode, /**/ FILE **f);
-void efclose(FILE *f);
+int efopen(const char *fname, const char *mode, /**/ FILE **);
+void efclose(FILE *);
