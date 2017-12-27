ed635ffedfc549960e5fb8de6a29142d7bd66c5e
Sergey Litvinov <slitvinov@gmail.com>
2017-12-27 22:25:14 +0100

    deps; rename linal test

--- src/u/math/linal/make/dir.mk
+++ src/u/math/linal/make/dir.mk
@@ -1,10 +1,10 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/comm       && \
-    d $B/d          && \
-    d $B/frag       && \
-    d $B/math/linal && \
-    d $B/math/rnd   && \
-    d $B/math/tform && \
-    d $B/mpi        && \
-    d $B/u/linal    && \
-    d $B/utils     
+    d $B/comm         && \
+    d $B/d            && \
+    d $B/frag         && \
+    d $B/math/linal   && \
+    d $B/math/rnd     && \
+    d $B/math/tform   && \
+    d $B/mpi          && \
+    d $B/u/math/linal && \
+    d $B/utils       
--- src/u/math/linal/make/rule.mk
+++ src/u/math/linal/make/rule.mk
@@ -8,7 +8,7 @@ $B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
 $B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
 $B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
 $B/msg.o: $S/msg.cpp; $X -I$S/.
-$B/u/linal/main.o: $S/u/linal/main.cpp; $X -I$S/.
+$B/u/math/linal/main.o: $S/u/math/linal/main.cpp; $X -I$S/.
 $B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
 $B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
 $B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/utils -I$S/.
