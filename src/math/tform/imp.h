5bd3a51dea10205bde3c7ed68c9cb9a09a7b41a8
Sergey Litvinov <slitvinov@gmail.com>
2017-12-27 14:18:16 +0000

    minor

--- src/math/tform/imp.h
+++ src/math/tform/imp.h
@@ -1,6 +1,8 @@
 struct Tform;
 
 // tag::interface[]
-void tform_ini(Tform**);
+void tform_ini(Tform**, float a0[3], float a1[3], float b0[3], float b1[3]);
 void tform_fin(Tform*);
+
+void tform_0to1(Tform*, float a0[3], /**/ float a1[3]);
 // end::interface[]
