3b88662e51395747190f0c8e6c7e9b00bab7ca4f
Sergey Litvinov <slitvinov@gmail.com>
2017-12-27 20:03:46 +0100

    add tform_convert

--- src/math/tform/imp.cpp
+++ src/math/tform/imp.cpp
@@ -42,3 +42,12 @@ void tform_vector(float a0[3], float a1[3],   float b0[3], float b1[3], /**/ Tfo
     r = os(a0[Z], a1[Z], b0[Z], b1[Z], /**/ &t->o[Z], &t->s[Z]);
     if (r != OK) { report(a0, a1,   b0, b1); ERR("tform_vector failed"); }
 }
+
+void tform_convert(Tform *t, float a0[3], /**/ float a1[3]) {
+    enum {X, Y, Z};
+    float *o, *s;
+    o = t->o; s = t->s;
+    a1[X] = s[X]*a0[X] + o[X];
+    a1[Y] = s[Y]*a0[Y] + o[Y];
+    a1[Z] = s[Z]*a0[Z] + o[Z];
+}
