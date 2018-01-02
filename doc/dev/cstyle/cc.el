7116d8544f0249be1471f4840892838463efa931
Sergey Litvinov <slitvinov@gmail.com>
2018-01-02 18:51:39 +0000

    use prefix u

--- doc/dev/cstyle/cc.el
+++ doc/dev/cstyle/cc.el
@@ -1,15 +1,15 @@
-(defun li/c-indent-common ()
+(defun u/c-indent-common ()
   (setq c-basic-offset 4
 	indent-tabs-mode nil))
 
-(defun li/c++-indent ()
-  (li/c-indent-common)
+(defun u/c++-indent ()
+  (u/c-indent-common)
   (c-set-offset 'innamespace [0]))
 
-(defun li/c-indent () (li/c-indent-common))
+(defun u/c-indent () (u/c-indent-common))
 
-(add-hook 'c-mode-hook   'li/c-indent)
-(add-hook 'c++-mode-hook 'li/c++-indent)
+(add-hook 'c-mode-hook   'u/c-indent)
+(add-hook 'c++-mode-hook 'u/c++-indent)
 
 (setq c-default-style
       '((java-mode . "java")
