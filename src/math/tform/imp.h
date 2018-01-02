5655c249c5c954f80a5ee7ee2837d07eb2397152
Sergey Litvinov <slitvinov@gmail.com>
2018-01-02 15:08:00 +0100

    add an implimentation of invert

--- src/math/tform/imp.h
+++ src/math/tform/imp.h
@@ -7,6 +7,7 @@ void tform_fin(Tform*);
 
 void tform_vector(float a0[3], float a1[3],   float b0[3], float b1[3], /**/ Tform*);
 void tform_chain(Tform*, Tform*, /**/ Tform*);
+void tform_inv(Tform*, /**/ Tform*);
 void tform_convert(Tform*, float a0[3], /**/ float a1[3]);
 
 void tform_log(Tform*);
