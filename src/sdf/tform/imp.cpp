fe2de2834ce4f916cbb617cff9b1043e76acf14e
Sergey Litvinov <slitvinov@gmail.com>
2018-01-03 11:44:18 +0100

    wip: tform/imp.cpp

--- src/sdf/tform/imp.cpp
+++ src/sdf/tform/imp.cpp
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+#include "math/tform/imp.h"
 #include "imp.h"
 
-void f();
+void ini_tex2sdf(/**/ Tform *tex2sdf) {
+    /*
+       [tex2g, g2tex]: genfi(-1/2, lo - M,   T-1/2, lo + L + M) $
+       [sdf2g, g2sdf]: genfi(-1/2,      0,   N-1/2,          G) $
+       tex2sdf: chain(tex2g, g2sdf)
+    */
+    Tform *tex2g, *sdf2g, *g2sdf;
+    tform_ini(&tex2g);
+    tform_ini(&sdf2g);
+    tform_ini(&g2sdf);
+
+
+    tform_fin(tex2g);
+    tform_fin(sdf2g);
+    tform_fin(g2sdf);
+}
+
+void ini_sub2tex(/**/ Tform*) {
+
+}
