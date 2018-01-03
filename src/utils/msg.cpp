35ada45d1438130288326a6774b8956db0bc4f69
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-03 14:16:44 +0100

    implement msg

--- src/utils/msg.cpp
+++ src/utils/msg.cpp
@@ -5,16 +5,44 @@
 #include "msg.h"
 
 static int rank;
-static const char *fmt = ".%03d";
-
-// true if master rank
-static void is_master() {return rank == 0;}
 
 void msg_ini(int rnk) {
     rank = rnk;
 }
 
-void msg_print(const char *fmt, ...) {
+static FILE* open(const char *path) {
+    static int fst = 1;
+    if (fst) {
+        fst = 0;
+        return fopen(path, "w");
+    } else {
+        return fopen(path, "a");
+    }
+}
 
+static void is_master(int r) {return r == 0;}
+static void print(const char *msg, FILE *f) {
+    fprintf(f, "%s\n", msg);
+    if (is_master(rank))
+        fprintf(stderr, ": %s\n", msg);
+}
+
+void msg_print(const char *fmt, ...) {
+    char msg[BUFSIZ], name[64];
+    va_list ap;
+    FILE *f;
+
+    // set name of file
+    sprintf(name, ".%03d", rank);
+
+    // form the message
+    va_start(ap, fmt);
+    vsprintf(msg, fmt, ap);
+    va_end(ap);
+
+    // print the message
+    f = open(n);
+    print(f);
+    fclose(f);
 }
 
