be829d67f059cf28127172540e354fad9087101f
Sergey Litvinov <slitvinov@gmail.com>
2018-01-03 10:18:47 +0100

    initial import of u/math/tform/lib: a device version of tform test

--- src/u/math/tform/make/dir.mk
+++ src/u/math/tform/make/dir.mk
@@ -1,10 +1,11 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/comm         && \
-    d $B/d            && \
-    d $B/frag         && \
-    d $B/math/linal   && \
-    d $B/math/rnd     && \
-    d $B/math/tform   && \
-    d $B/mpi          && \
-    d $B/u/math/tform && \
-    d $B/utils       
+    d $B/comm             && \
+    d $B/d                && \
+    d $B/frag             && \
+    d $B/math/linal       && \
+    d $B/math/rnd         && \
+    d $B/math/tform       && \
+    d $B/mpi              && \
+    d $B/u/math/tform     && \
+    d $B/u/math/tform/lib && \
+    d $B/utils           
