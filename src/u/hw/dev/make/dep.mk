1c014886ece304af7fadab1a1e4c1b39c9c1ea9e
Sergey Litvinov <slitvinov@gmail.com>
2018-01-04 21:58:05 +0100

    deps

--- src/u/hw/dev/make/dep.mk
+++ src/u/hw/dev/make/dep.mk
@@ -1,11 +1,11 @@
-$B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/utils/error.h $S/utils/imp.h
-$B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
+$B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/d/cuda/imp.h
+$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/u/hw/dev/main.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/inc/dev.h $S/mpi/glb.h $S/u/hw/dev/dev.h $S/utils/cc.h $S/utils/kl.h $S/utils/msg.h
-$B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/utils/cc/common.h $S/utils/error.h
-$B/utils/error.o: $S/utils/error.h $S/utils/msg.h
+$B/u/hw/dev/main.o: $S/inc/conf.h $S/utils/cc.h $S/u/hw/dev/dev.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/utils/kl.h $S/mpi/glb.h $S/utils/msg.h
+$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
+$B/utils/error.o: $S/utils/msg.h $S/utils/error.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
-$B/utils/mc.o: $B/conf.h $S/inc/conf.h $S/utils/error.h $S/utils/mc.h
+$B/utils/mc.o: $S/inc/conf.h $S/utils/error.h $S/utils/mc.h $B/conf.h
 $B/utils/msg.o: $S/utils/msg.h
-$B/utils/os.o: $S/utils/error.h $S/utils/msg.h $S/utils/os.h
+$B/utils/os.o: $S/utils/os.h $S/utils/error.h $S/utils/msg.h
