283d67946539c83c9c67d6257d25aa16326681e4
Sergey Litvinov <slitvinov@gmail.com>
2018-01-04 11:21:35 +0100

    use sdf_ini

--- src/sdf/tform/imp.cpp
+++ src/sdf/tform/imp.cpp
@@ -27,6 +27,14 @@ static void grid2grid(TGrid *from, TGrid *to, /**/ Tform *t) {
     tform_grid2grid(from->lo, from->hi, from->n,
                     to->lo, to->hi,   to->n, /**/ t);
 }
+static void sdf_ini(const Coords *c, int N[3], /**/ TGrid *t) {
+    enum {X, Y, Z};
+    t->lo[X] = t->lo[Y] = t->lo[Z] = 0;
+    t->hi[X] = xdomain(*c);
+    t->hi[Y] = ydomain(*c);
+    t->hi[Z] = zdomain(*c);
+    t->n = N;
+}
 void tex2sdf_ini(const Coords *c,
                  int T[3], int N[3], int M[3],
                  /**/ Tform *t) {
@@ -45,18 +53,14 @@ void tex2sdf_ini(const Coords *c,
     tex.hi[Z] = zhi(*c) + M[Z];
     tex.n = T;
 
-    sdf.lo[X] = sdf.lo[Y] = sdf.lo[Z] = 0;
-    sdf.hi[X] = xdomain(*c);
-    sdf.hi[Y] = ydomain(*c);
-    sdf.hi[Z] = zdomain(*c);
-    sdf.n = N;
+    sdf_ini(c, N, /**/ &sdf);
 
     UC(grid2grid(&tex, &sdf, /**/ t));
 }
 
 void sub2sdf_ini(const Coords*, int N[3], /**/ Tform* t) {
     TGrid sub, sdf;
-    UC(grid2grid(&tex, &sdf, /**/ t));
+    UC(grid2grid(&sub, &sdf, /**/ t));
 }
 
 void sub2tex_ini(/**/ Tform*) { ERR("not implimented"); }
