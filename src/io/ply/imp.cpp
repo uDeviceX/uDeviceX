79b885baf0a2e787219afb188d07fd9eba4a6c91
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-04 14:11:28 +0100

    mv ply to its own folder

---
+++ src/io/ply/imp.cpp
@@ -0,0 +1,21 @@
+#include <vector_types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "inc/type.h"
+#include "inc/def.h"
+#include "utils/msg.h"
+#include "utils/error.h"
+#include "utils/imp.h"
+
+
+#include "imp.h"
+
+#ifdef PLY_WRITE_ASCII
+  #include "imp/ascii.h"
+#else
+  #include "imp/bin.h"
+#endif
+#include "imp/common.h"
