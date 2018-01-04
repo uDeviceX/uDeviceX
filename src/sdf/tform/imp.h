79e21b2d5ecc3894a1a1da6453a9bc51cc2a8905
Sergey Litvinov <slitvinov@gmail.com>
2018-01-04 09:15:08 +0100

    minor

--- src/sdf/tform/imp.h
+++ src/sdf/tform/imp.h
@@ -1,6 +1,6 @@
 struct Tform;
 struct Coords;
 /* T: texture size, N: sdf size, M: wall margin */
-void ini_tex2sdf(const Coords, const int T[3], const int N[3], const int M[3],
+void ini_tex2sdf(const Coords, int T[3], int N[3], int M[3],
                  /**/ Tform*);
 void ini_sub2tex(/**/ Tform*);
