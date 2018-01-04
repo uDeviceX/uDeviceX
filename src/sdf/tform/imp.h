f49efc7d1323e2dbb28a9fb5602aa8900b684c3f
Sergey Litvinov <slitvinov@gmail.com>
2018-01-04 14:24:32 +0100

    rm out2sdf_ini

--- src/sdf/tform/imp.h
+++ src/sdf/tform/imp.h
@@ -4,5 +4,4 @@ struct Coords;
 /* T: texture size, N: sdf file size, M: wall margin */
 void tex2sdf_ini(const Coords*, const int T[3], const int N[3], const int M[3], /**/ Tform*);
 void sub2sdf_ini(const Coords*, const int N[3], /**/ Tform*);
-void out2sdf_ini(const Coords*, const int N[3], /**/ Tform*);
 void sub2tex_ini(/**/ Tform*);
