489471a421b27ee550316c45bb390715213a1ae0
Sergey Litvinov <slitvinov@gmail.com>
2018-01-05 14:27:26 +0100

    add tform_spacing_dev

--- src/math/tform/dev.h
+++ src/math/tform/dev.h
@@ -6,3 +6,10 @@ static __device__ void tform_convert_dev(Tform_v *t, const float a0[3], /**/ flo
     a1[Y] = s[Y]*a0[Y] + o[Y];
     a1[Z] = s[Z]*a0[Z] + o[Z];
 }
+
+static __device__ void tform_spacing_dev(Tform_v *t, /**/ float s[3]) {
+    enum {X, Y, Z};
+    s[X] = t->s[X];
+    s[Y] = t->s[Y];
+    s[Z] = t->s[Z];
+}
