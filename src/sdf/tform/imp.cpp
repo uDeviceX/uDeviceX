b58b4ad577746ddb468d860518d9f83f05796d13
Sergey Litvinov <slitvinov@gmail.com>
2018-01-05 13:23:46 +0100

    rm unused variables

--- src/sdf/tform/imp.cpp
+++ src/sdf/tform/imp.cpp
@@ -108,7 +108,7 @@ void out2sdf_ini(const Coords *c, const int N[3], /**/ Tform* t) {
 
 void sub2tex_ini(const Coords *c, const int T[3], const int M[3], /**/ Tform *t) {
     enum {X, Y, Z};
-    TGrid g_sub, g_tex;
+    TGrid g_tex;
     Tform *sub, *tex;
     
     if (goodp(T) == BAD) ERR("bad T = [%d %d %d]", T[X], T[Y], T[Z]);
