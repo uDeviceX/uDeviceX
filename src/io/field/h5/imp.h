e5c7067df0f5cd541e728c15bf66e4144aba3c36
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-05 11:49:43 +0100

    use coords interface in field

--- src/io/field/h5/imp.h
+++ src/io/field/h5/imp.h
@@ -2,6 +2,5 @@ namespace h5 {
 void write(Coords coords, MPI_Comm cart, const char *path,
            float *data[],
            const char **names,
-           int n,
-           int sx, int sy, int sz);
+           int n);
 }
--- src/io/field/imp/scalar.h
+++ src/io/field/imp/scalar.h
@@ -1,6 +1,6 @@
 void scalar(Coords coords, MPI_Comm cart, float *D, const char *name) {
     char path[BUFSIZ];
     sprintf(path, DUMP_BASE "/h5/%s.h5", name);
-    UC(h5::write(coords, cart, path, &D, &name, 1, XS, YS, ZS));
-    if (m::is_master(cart)) xmf::write(path, &name, 1, XS, YS, ZS);
+    UC(h5::write(coords, cart, path, &D, &name, 1));
+    if (m::is_master(cart)) xmf::write(path, &name, 1, xs(coords), ys(coords), zs(coords));
 }
