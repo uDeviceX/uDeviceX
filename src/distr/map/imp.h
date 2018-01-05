9f1be76721bc70abbe632371dc634cd436fdf2f7
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-05 15:49:17 +0100

    add separate compilation unit

--- src/distr/map/imp.h
+++ src/distr/map/imp.h
@@ -1,66 +1,8 @@
-void dmap_ini(int nfrags, const int capacity[], /**/ DMap *m) {
-    CC(d::Malloc((void**) &m->counts,  nfrags      * sizeof(int)));
-    CC(d::Malloc((void**) &m->starts, (nfrags + 1) * sizeof(int)));
-
-    CC(d::alloc_pinned((void**) &m->hcounts,  nfrags      * sizeof(int)));
-    
-    int i, c;
-    for (i = 0; i < nfrags; ++i) {
-        c = capacity[i];
-        if (c) CC(d::Malloc((void**) &m->ids[i], c * sizeof(int)));
-    }
-    
-}
-
-void dmap_fin(int nfrags, /**/ DMap *m) {
-    CC(d::Free(m->counts));
-    CC(d::Free(m->starts));
-    CC(d::FreeHost(m->hcounts));    
-    for (int i = 0; i < nfrags; ++i)
-        CC(d::Free(m->ids[i]));
-}
-
-void dmap_reini(int nfrags, /**/ DMap m) {
-    CC(d::MemsetAsync(m.counts, 0, nfrags * sizeof(int)));
-}
-
-void dmap_download_counts(int nfrags, /**/ DMap *m) {
-    CC(d::MemcpyAsync(m->hcounts, m->counts, nfrags * sizeof(int), D2H));
-}
-
-void map_ini_host(int nfrags, const int capacity[], /**/ DMap *m) {
-    UC(emalloc( nfrags      * sizeof(int), (void**) &m->counts));
-    UC(emalloc((nfrags + 1) * sizeof(int), (void**) &m->starts));
-
-    int i, c;
-    for (i = 0; i < nfrags; ++i) {
-        c = capacity[i];
-        if (c)
-            UC(emalloc(c * sizeof(int), (void**) &m->ids[i]));
-    }
-}
-
-void dmap_fin_host(int nfrags, /**/ DMap *m) {
-    free(m->counts);
-    free(m->starts);    
-    for (int i = 0; i < nfrags; ++i)
-        free(m->ids[i]);
-}
-
-void dmap_reini_host(int nfrags, /**/ DMap m) {
-    memset(m.counts, 0, nfrags * sizeof(int));
-}
-
-void dmap_D2H(int nfrags, const DMap *d, /**/ DMap *h) {
-    CC(d::MemcpyAsync(h->counts, d->counts,  nfrags      * sizeof(int), D2H));
-    CC(d::MemcpyAsync(h->starts, d->starts, (nfrags + 1) * sizeof(int), D2H));
-
-    dSync();
-    
-    int i, c;
-    for (i = 0; i < nfrags; ++i) {
-        c = h->counts[i];
-        if (c)
-            CC(d::MemcpyAsync(h->ids[i], d->ids[i], c * sizeof(int), D2H));
-    }
-}
+void dmap_ini(int nfrags, const int capacity[], /**/ DMap *m);
+void dmap_fin(int nfrags, /**/ DMap *m);
+void dmap_reini(int nfrags, /**/ DMap m);
+void dmap_download_counts(int nfrags, /**/ DMap *m);
+void dmap_ini_host(int nfrags, const int capacity[], /**/ DMap *m);
+void dmap_fin_host(int nfrags, /**/ DMap *m);
+void dmap_reini_host(int nfrags, /**/ DMap m);
+void dmap_D2H(int nfrags, const DMap *d, /**/ DMap *h);
