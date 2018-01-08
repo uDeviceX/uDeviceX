7a6f0113066a1fd4e254530113d0930ab1747e0b
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-08 13:41:20 +0100

    read optional arrays in parser unit

--- src/u/parser/main.cpp
+++ src/u/parser/main.cpp
@@ -5,9 +5,11 @@
 #include "utils/error.h"
 #include "parser/imp.h"
 
+enum {MAX_VEC=128};
+
 static void extract(const Config *c) {
-    int a, opt;
-    float f;
+    int i, a, opt, in, fn, ivec[MAX_VEC];
+    float f, fvec[MAX_VEC];
 
     UC(conf_lookup_int(c, "a", &a));
     UC(conf_lookup_float(c, "f", &f));
@@ -16,6 +18,18 @@ static void extract(const Config *c) {
 
     if (conf_opt_int(c, "opt", &opt))
         printf("%d\n", opt);
+
+    if (conf_opt_vint(c, "ivec", &in, ivec)) {
+        for (i = 0; i < in; ++i)
+            printf("%d ", ivec[i]);
+        printf("\n");
+    }
+
+    if (conf_opt_vfloat(c, "fvec", &fn, fvec)) {
+        for (i = 0; i < fn; ++i)
+            printf("%g ", fvec[i]);
+        printf("\n");
+    }
 }
 
 int main(int argc, char **argv) {
