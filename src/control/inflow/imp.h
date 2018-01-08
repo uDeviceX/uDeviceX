4e39ebc822883255b6b9bfb9b2da7e47de11d995
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-01-08 15:37:45 +0100

    adding doc for control modules

--- src/control/inflow/imp.h
+++ src/control/inflow/imp.h
@@ -1,6 +1,11 @@
 struct Inflow;
 
+// tag::mem[]
 void ini(int2 nc, Inflow **i);
+void fin(Inflow *i);
+// end::mem[]
+
+// tag::ini[]
 void ini_velocity(Inflow *i);
 
 void ini_params_plate(Coords c, float3 o, int dir, float L1, float L2,
@@ -8,9 +13,8 @@ void ini_params_plate(Coords c, float3 o, int dir, float L1, float L2,
                       /**/ Inflow *i);
 void ini_params_circle(Coords c, float3 o, float R, float H, float u, bool poiseuille,
                        /**/ Inflow *i);
+// end::ini[]
 
-void fin(Inflow *i);
-
+// tag::int[]
 void create_pp(Inflow *i, int *n, Particle *pp);
-
-
+// end::int[]
