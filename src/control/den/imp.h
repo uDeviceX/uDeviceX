5a3781bfd9d1943fd51f9164cef64c2b82de4e26
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-08 18:05:24 +0100

    add different initialization modes for map in den

--- src/control/den/imp.h
+++ src/control/den/imp.h
@@ -2,7 +2,8 @@ struct DCont;
 struct DContMap;
 
 void den_ini(int maxp, /**/ DCont **d0);
-void den_ini_map(Coords coords, DContMap **m0);
+void den_ini_map_none(Coords coords, DContMap **m0);
+void den_ini_map_circle(Coords coords, float R, DContMap **m0);
 
 void den_fin(DCont *d);
 void den_fin_map(DContMap *m);
--- src/control/den/imp/map.h
+++ src/control/den/imp/map.h
@@ -1,5 +1,16 @@
-static int pred_circle(Coords c, int i, int j, int k) {
+struct Circle {
+    float R;    
+};
+
+struct Plate {
+    // TODO
+};
+
+struct None {};
+
+static int predicate(Coords c, Circle p, int i, int j, int k) {
     enum {X, Y, Z};
+    // TODO: for now assume centered
     float3 r;
     float R;
 
@@ -7,17 +18,21 @@ static int pred_circle(Coords c, int i, int j, int k) {
     local2center(c, r, /**/ &r);
     
     R = sqrt(r.x * r.x + r.y * r.y);
-    return R < OUTFLOW_CIRCLE_R && R >= OUTFLOW_CIRCLE_R - 1;
+    return R < p.R && R >= p.R - 1;
 }
 
-static int predicate(Coords c, int i, int j, int k) {
-    int p = 0;
-    if (OUTFLOW_CIRCLE)
-        p = pred_circle(c, i, j, k);
-    return p;
+static int predicate(Coords c, Plate p, int i, int j, int k) {
+    // TODO
+    return 0;
 }
 
-static void ini_map(Coords coords, int **ids, int *nids) {
+static int predicate(Coords c, None p, int i, int j, int k) {
+    return 0;
+}
+
+
+template <typename T>
+static void ini_map(Coords coords, T p, int **ids, int *nids) {
     int *ii, i, j, k, n;
     size_t sz;
     
@@ -28,7 +43,7 @@ static void ini_map(Coords coords, int **ids, int *nids) {
     for (k = 0; k < ZS; ++k) {
         for (j = 0; j < YS; ++j) {
             for (i = 0; i < XS; ++i) {
-                if (predicate(coords, i, j, k))
+                if (predicate(coords, p, i, j, k))
                     ii[n++] = i + XS * (j + YS * k);
             }
         }
@@ -43,13 +58,22 @@ static void ini_map(Coords coords, int **ids, int *nids) {
     UC(efree(ii));
 }
 
-void den_ini_map(Coords coords, DContMap **m0) {
-    DContMap *m;
-    
+static DContMap *alloc(DContMap **m0) {
     UC(emalloc(sizeof(DContMap), (void**) m0));
-    m = *m0;
+    return *m0;
+}
 
-    ini_map(coords, &m->cids, &m->n);
+void den_ini_map_none(Coords coords, DContMap **m0) {
+    DContMap *m = alloc(m0);
+    None p;
+    ini_map(coords, p, &m->cids, &m->n);
+}
+
+void den_ini_map_circle(Coords coords, float R, DContMap **m0) {
+    DContMap *m = alloc(m0);
+    Circle p;
+    p.R = R;
+    ini_map(coords, p, &m->cids, &m->n);
 }
 
 void den_fin_map(DContMap *m) {
