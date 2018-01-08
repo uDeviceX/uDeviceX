31089fa651732439a4f25c6953bd9e544ba46f27
Sergey Litvinov <slitvinov@gmail.com>
2018-01-08 10:10:02 +0100

    dpes

--- src/u/parser/make/rule.mk
+++ src/u/parser/make/rule.mk
@@ -1,14 +1,14 @@
-$B/comm/imp.o: $S/comm/imp.cpp; $X -I$S/. -I$S/comm
-$B/d/api.o: $S/d/api.cu; $N -I$S/. -I$S/d
-$B/frag/imp.o: $S/frag/imp.cpp; $X -I$S/. -I$S/frag
-$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
-$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/. -I$S/mpi
-$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
-$B/parser/imp.o: $S/parser/imp.cpp; $X -I$S/. -I$S/parser
+$B/comm/imp.o: $S/comm/imp.cpp; $X -I$S/comm -I$S/.
+$B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
+$B/frag/imp.o: $S/frag/imp.cpp; $X -I$S/frag -I$S/.
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
+$B/parser/imp.o: $S/parser/imp.cpp; $X -I$S/parser -I$S/.
 $B/u/parser/main.o: $S/u/parser/main.cpp; $X -I$S/.
-$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/. -I$S/utils
-$B/utils/error.o: $S/utils/error.cpp; $X -I$S/. -I$S/utils
-$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/. -I$S/utils
-$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/. -I$S/utils
-$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/. -I$S/utils
-$B/utils/os.o: $S/utils/os.cpp; $X -I$S/. -I$S/utils
+$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
+$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/utils -I$S/.
+$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
+$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/utils -I$S/.
+$B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
