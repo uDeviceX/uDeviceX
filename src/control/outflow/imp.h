fae2afc384e4908a5b05d03b37c5c3adb54bbe4a
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-01-09 14:33:09 +0100

    document outflow

--- src/control/outflow/imp.h
+++ src/control/outflow/imp.h
@@ -1,14 +1,21 @@
 struct Outflow;
 
+// tag::mem[]
 void ini(int maxp, /**/ Outflow **o);
+void fin(/**/ Outflow *o);
+// end::mem[]
+
+// tag::ini[]
 void ini_params_circle(Coords coords, float3 c, float R, Outflow *o);
 void ini_params_plate(Coords c, int dir, float r0, Outflow *o);
+// end::ini[]
 
-void fin(/**/ Outflow *o);
+// tag::int[]
+void filter_particles(int n, const Particle *pp, /**/ Outflow *o); // <1>
 
-void filter_particles(int n, const Particle *pp, /**/ Outflow *o);
+void download_ndead(Outflow *o);                                   // <2>
 
-void download_ndead(Outflow *o);
+int* get_deathlist(Outflow *o);                                    // <3>
+int  get_ndead(Outflow *o);                                        // <4>
+// tag::int[]
 
-int* get_deathlist(Outflow *o);
-int  get_ndead(Outflow *o);
