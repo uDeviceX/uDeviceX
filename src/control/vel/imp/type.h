504c9da2f058f684c85ff1423f78c5151f9529da
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-09 08:47:13 +0100

    adding types for transformation

--- src/control/vel/imp/type.h
+++ src/control/vel/imp/type.h
@@ -1,17 +1,37 @@
-struct PidVCont {
-    int3 L;                   /* subdomain size                               */
-    float3 target, current;   /* target and current average velocities        */
-    float Kp, Ki, Kd, factor; /* parameters of the pid controller             */
-    float3 olde, sume;        /* previous error, sum of all previous errors   */
-    float3 f;                 /* force estimate                               */
-    long nsamples;            /* number of "pending" avg on grid              */
-    long totncells;           /* total number of cells                        */
+enum {
+    TYPE_NONE,
+    TYPE_CART,
+    TYPE_RAD
+};
+
+struct TCart {};
+struct TRad {
+    float Rlo, Rhi;
+};
+
+/* parameters for transformation */
+union Trans {
     
-    float3 *gridvel;          /* average velocity per grid point  */
-    float3 *avgvel;           /* chunk sums (pinned memory)       */
-    float3 *davgvel;          /* device pointer of the above      */
+};
+
+/* pid velocity controller */
+struct PidVCont {
+    int3 L;                   /* subdomain size                                  */
+    float3 target, current;   /* target and current average velocities           */
+    float Kp, Ki, Kd, factor; /* parameters of the pid controller                */
+    float3 olde, sume;        /* previous error, sum of all previous errors      */
+    float3 f;                 /* force estimate                                  */
+    long nsamples;            /* number of "pending" avg on grid                 */
+    long totncells;           /* total number of cells                           */
+    
+    float3 *gridvel;          /* average velocity per grid point                 */
+    float3 *avgvel;           /* chunk sums (pinned memory)                      */
+    float3 *davgvel;          /* device pointer of the above                     */
 
     MPI_Comm comm;
 
-    FILE *fdump;              /* output file for logging info     */
+    FILE *fdump;              /* output file for logging info                    */
+
+    Trans trans;              /* transformation of the velocity before averaging */
+    int type;                 /* type of the transformation                      */
 };
