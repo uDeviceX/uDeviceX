eeb7f19bc7908e3f5d10ea75537b11c78d5053b6
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-01-09 12:54:01 +0100

    document vcon

--- src/control/vel/imp.h
+++ src/control/vel/imp.h
@@ -1,11 +1,17 @@
 struct PidVCont;
 
+// tag::mem[]
 void vcont_ini(MPI_Comm comm, int3 L, float3 vtarget, float factor, /**/ PidVCont **c);
 void vcont_fin(/**/ PidVCont *cont);
+// end::mem[]
 
+// tag::ini[]
 void vcon_set_cart(/**/ PidVCont *cont);
 void vcon_set_radial(/**/ PidVCont *cont);
+// end::ini[]
 
-void   vcont_sample(Coords coords, int n, const Particle *pp, const int *starts, const int *counts, /**/ PidVCont *c);
-float3 vcont_adjustF(/**/ PidVCont *c);
-void   vcont_log(const PidVCont *c);
+// tag::int[]
+void   vcont_sample(Coords coords, int n, const Particle *pp, const int *starts, const int *counts, /**/ PidVCont *c); // <1>
+float3 vcont_adjustF(/**/ PidVCont *c); // <2>
+void   vcont_log(const PidVCont *c);    // <3>
+// end::int[]
