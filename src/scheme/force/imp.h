a43406815679af8433767819beae4b00fbcbf243
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-01-10 11:59:37 +0100

    upd doc

--- src/scheme/force/imp.h
+++ src/scheme/force/imp.h
@@ -9,15 +9,15 @@ void bforce_fin(BForce *p);
 // end::mem[]
 
 // tag::ini[]
-void bforce_ini_none(/**/ BForce *p);
-void bforce_ini_cste(float3 f, /**/ BForce *p);
-void bforce_ini_dp(float a, /**/ BForce *p);
-void bforce_ini_shear(float a, /**/ BForce *p);
-void bforce_ini_rol(float a, /**/ BForce *p);
-void bforce_ini_rad(float a, /**/ BForce *p);
+void bforce_ini_none(/**/ BForce *p);            // <1>
+void bforce_ini_cste(float3 f, /**/ BForce *p);  // <2>
+void bforce_ini_dp(float a, /**/ BForce *p);     // <3>
+void bforce_ini_shear(float a, /**/ BForce *p);  // <4>
+void bforce_ini_rol(float a, /**/ BForce *p);    // <5>
+void bforce_ini_rad(float a, /**/ BForce *p);    // <6>
 // end::ini[]
 
 // tag::interface[]
-void bforce_adjust(float3 f, /**/ BForce *fpar);
-void bforce_apply(long it, Coords c, float mass, const BForce *bf, int n, const Particle *pp, /**/ Force *ff);
+void bforce_adjust(float3 f, /**/ BForce *fpar); // <1>
+void bforce_apply(long it, Coords c, float mass, const BForce *bf, int n, const Particle *pp, /**/ Force *ff); // <2>
 // end::interface[]
