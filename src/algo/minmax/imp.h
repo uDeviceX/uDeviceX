21bdbaba2d60952dc5e5074cb9eebf40791469f9
Sergey Litvinov <slitvinov@gmail.com>
2018-01-11 11:03:57 +0100

    minor in mainmax; predeclare float3

--- src/algo/minmax/imp.h
+++ src/algo/minmax/imp.h
@@ -1,5 +1,5 @@
 struct Particle;
+struct float3;
 // tag::interface[]
-void minmax(const Particle * const particles, int nparticles_per_body, int nbodies,
-            float3 * minextents, float3 * maxextents);
+void minmax(const Particle *const, int nv, int no, /**/ float3 *min, float3 *max);
 // end::interface[]
