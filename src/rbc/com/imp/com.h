4b2b168c5e24a6055f0869abeae105b6d6107d97
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-11 15:22:01 +0100

    reduce velocity

--- src/rbc/com/imp/com.h
+++ src/rbc/com/imp/com.h
@@ -1,27 +1,34 @@
-static void reini(int nm, /**/ float3 *rr) {
-    if (nm) CC(d::MemsetAsync(rr, 0, nm * sizeof(float3)));
+static void reini(int nm, /**/ float3 *h) {
+    if (nm) CC(d::MemsetAsync(h, 0, nm * sizeof(float3)));
 }
 
-static void reduce(int nm, int nv, const Particle *pp, /**/ float3 *rr) {
+static void reduce(int nm, int nv, const Particle *pp, /**/ float3 *rr, float3 *vv) {
     dim3 thrd(128, 1);
     dim3 blck(ceiln(nv, thrd.x), nm);
     
-    KL(dev::reduce_position, (blck, thrd), (nv, pp, /**/ rr));
+    KL(dev::reduce_props, (blck, thrd), (nv, pp, /**/ rr, vv));
 }
 
-static void download(int nm, const float3 *drr, /**/ float3 *hrr) {
-    if (nm) CC(d::Memcpy(hrr, drr, nm * sizeof(float3), D2H));
+static void download(int nm, const float3 *d, /**/ float3 *h) {
+    if (nm) CC(d::MemcpyAsync(h, d, nm * sizeof(float3), D2H));
 }
 
-static void normalize(int nm, int nv, /**/ float3 *hrr) {
+static void normalize(int nm, int nv, /**/ float3 *h) {
     float fac = 1.f / nv;
     for (int i = 0; i < nm; ++i)
-        scal(fac, hrr + i);
+        scal(fac, h + i);
 }
 
 void get(int nm, int nv, const Particle *pp, /**/ ComProps *com) {
     reini(nm, /**/ com->drr);
-    reduce(nm, nv, pp, /**/ com->drr);
-    download(nm, com->drr, /**/ com->hrr); /* blocking */
+    reini(nm, /**/ com->dvv);
+    
+    reduce(nm, nv, pp, /**/ com->drr, com->dvv);
+
+    download(nm, com->drr, /**/ com->hrr);
+    download(nm, com->dvv, /**/ com->hvv);
+    dSync();
+    
     normalize(nm, nv, /**/ com->hrr);
+    normalize(nm, nv, /**/ com->hvv);
 }
