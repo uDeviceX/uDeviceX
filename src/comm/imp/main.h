6af9acbe418aa58c0a8b5d6f1d3aa5c0a8a7037a
Sergey Litvinov <slitvinov@gmail.com>
2018-01-14 09:54:43 +0100

    use fail_over

--- src/comm/imp/main.h
+++ src/comm/imp/main.h
@@ -8,12 +8,11 @@ int post_recv(hBags *b, Comm *com) {
     return 0;
 }
 
-static void assert_over(int i, long c, long cap) {
+static void fail_over(int i, long c, long cap) {
     enum {X, Y, Z};
     int d[3];
-    if (c <= cap) return;
     d[X] = frag_i2dx(i); d[Y] = frag_i2dy(i); d[Z] = frag_i2dz(i);
-    ERR("over capacity in send, fragment %d = [%d %d %d]: %ld/%ld",
+    ERR("over capacity, fragment %d = [%d %d %d]: %ld/%ld",
         i, d[X], d[Y], d[Z], c, cap);
 }
 int post_send(const hBags *b, Comm *com) {
@@ -23,8 +22,7 @@ int post_send(const hBags *b, Comm *com) {
         cap = b->capacity[i];
         n = c * b->bsize;
         tag = i;
-
-        //        UC(assert_over(i, c, cap));
+        if (c > cap) UC(fail_over(i, c, cap));
         MC(m::Isend(b->data[i], n, MPI_BYTE, com->ranks[i], tag, com->cart, com->sreq + i));
     }
     return 0;
@@ -36,14 +34,14 @@ static void get_counts_bytes(const MPI_Status ss[NFRAGS], /**/ int counts[NFRAGS
 }
 
 static void get_counts(const MPI_Status ss[NFRAGS], /**/ hBags *b) {
-    int i, c, cc[NFRAGS];
+    int cap, i, c, cc[NFRAGS];
     get_counts_bytes(ss, /**/ cc);
 
     for (i = 0; i < NFRAGS; ++i) {
         c = cc[i] / b->bsize;
         b->counts[i] = c;
-        if (c > b->capacity[i])
-            ERR("recv more than capacity in fragment %d : (%ld / %ld)", i, (long) c, (long) b->capacity[i]);
+        cap = b->capacity[i];
+        if (c > cap) UC(fail_over(i, c, cap));
     }
 }
 
