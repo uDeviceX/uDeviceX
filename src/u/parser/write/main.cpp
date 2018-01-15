780dbc4552a3bce58013b46bd3e2800f59d728b3
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-15 09:22:42 +0100

    add small unit for parser: write

---
+++ src/u/parser/write/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include "utils/msg.h"
+#include "mpi/glb.h"
+#include "utils/error.h"
+#include "parser/imp.h"
+
+enum {MAX_VEC=128};
+
+static void set(Config *c) {
+    // {
+    //     const char *desc[] = {"group", "a"};
+    //     conf_set_int(2, desc, 0, c);
+    // }
+
+    {
+        const char *desc[] = {"group", "subgroup", "a"};
+        conf_set_int(3, desc, 5, c);
+    }
+
+    {
+        const char *desc[] = {"a"};
+        conf_set_int(1, desc, 0, c);
+    }
+}
+
+int main(int argc, char **argv) {
+    Config *cfg;
+    m::ini(&argc, &argv);
+
+    conf_ini(/**/ &cfg);
+    conf_read(argc, argv, /**/ cfg);
+
+    UC(set(cfg));
+    conf_write_exe(cfg, stdout);
+    
+    conf_fin(/**/ cfg);    
+
+    m::fin();
+}
