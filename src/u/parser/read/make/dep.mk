2cacbb4b149c452bf9828fe5488b43a2e7c6765b
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-15 09:14:28 +0100

    upd deps

--- src/u/parser/read/make/dep.mk
+++ src/u/parser/read/make/dep.mk
@@ -5,7 +5,7 @@ $B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/parser/imp.o: $S/parser/imp.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
-$B/u/parser/main.o: $S/mpi/glb.h $S/parser/imp.h $S/utils/error.h $S/utils/msg.h
+$B/u/parser/read/main.o: $S/mpi/glb.h $S/parser/imp.h $S/utils/error.h $S/utils/msg.h
 $B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/utils/cc/common.h $S/utils/error.h
 $B/utils/error.o: $S/utils/error.h $S/utils/msg.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
