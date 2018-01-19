989227f56f7aab33b064fb135b0d88366b1a9f81
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-19 09:02:12 +0100

    add const

--- src/rbc/stretch/dev/main.h
+++ src/rbc/stretch/dev/main.h
@@ -1,15 +1,15 @@
-static __device__ void apply0(float *f, /**/ float *fb) {
+static __device__ void apply0(const float *f, /**/ float *fb) {
     enum {X, Y, Z};
     fb[X] += f[X]; fb[Y] += f[Y]; fb[Z] += f[Z];
 }
 
-static __device__ void apply1(int i, int nv, float *f, Force *ff) {
+static __device__ void apply1(int i, int nv, const float *f, Force *ff) {
     int k;
     k = i % nv;
     apply0(&f[3*k], /**/ ff[i].f);
 }
 
-__global__ void apply(int n, int nv, float *f, Force *ff) {
+__global__ void apply(int n, int nv, const float *f, Force *ff) {
     int i;
     i = threadIdx.x + blockDim.x * blockIdx.x;
     if (i >= n) return;
