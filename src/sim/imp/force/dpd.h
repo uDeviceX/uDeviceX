cfdfc7c6a77f205ef2741309d286c261d18f5a23
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-19 14:31:55 +0100

    more UC

--- src/sim/imp/force/dpd.h
+++ src/sim/imp/force/dpd.h
@@ -7,30 +7,30 @@ void forces_dpd(Flu *f) {
 
     FluExch *e = &f->e;
     
-    ini_cloud(f->q.pp, /**/ &cloud);
+    UC(ini_cloud(f->q.pp, /**/ &cloud));
     if (multi_solvent)
-        ini_cloud_color(f->q.cc, /**/ &cloud);
+        UC(ini_cloud_color(f->q.cc, /**/ &cloud));
 
-    eflu_compute_map(start, count, /**/ e->p);
-    eflu_download_cell_starts(/**/ e->p);
-    eflu_pack(&cloud, /**/ e->p);
-    eflu_download_data(/**/ e->p);
+    UC(eflu_compute_map(start, count, /**/ e->p));
+    UC(eflu_download_cell_starts(/**/ e->p));
+    UC(eflu_pack(&cloud, /**/ e->p));
+    UC(eflu_download_data(/**/ e->p));
 
     UC(eflu_post_recv(e->c, e->u));
     UC(eflu_post_send(e->p, e->c));
     
-     prepare(f->q.n, &cloud, /**/ f->bulkdata);
-    bulk_forces(f->q.n, f->bulkdata, start, count, /**/ f->ff);
+    UC(prepare(f->q.n, &cloud, /**/ f->bulkdata));
+    UC(bulk_forces(f->q.n, f->bulkdata, start, count, /**/ f->ff));
 
     dSync();
     UC(eflu_wait_recv(e->c, e->u));
     UC(eflu_wait_send(e->c));
 
-    eflu_unpack(e->u);
+    UC(eflu_unpack(e->u));
 
-    eflu_get_local_frags(e->p, /**/ &lfrags);
-    eflu_get_remote_frags(e->u, /**/ &rfrags);
+    UC(eflu_get_local_frags(e->p, /**/ &lfrags));
+    UC(eflu_get_remote_frags(e->u, /**/ &rfrags));
 
-    prepare(lfrags, rfrags, /**/ f->halodata);
-    halo_forces(f->halodata, /**/ f->ff);
+    UC(prepare(lfrags, rfrags, /**/ f->halodata));
+    UC(halo_forces(f->halodata, /**/ f->ff));
 }
