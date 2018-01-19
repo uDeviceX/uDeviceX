b9b0faa6689d2f96a7d97d16b7a71aeb751efd06
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-19 14:10:45 +0100

    use checks in step

--- src/sim/imp/step.h
+++ src/sim/imp/step.h
@@ -9,6 +9,25 @@ static void check_sizes(Sim *s) {
     UC(check_size(s->flu.q.n , MAX_PART_NUM)); 
 }
 
+static void check_pos_soft(Sim *s) {
+    UC(dbg_check_pos_soft(s->dbg, s->flu.q.n, s->flu.q.pp));
+    if (s->solids0) UC(dbg_check_pos_soft(s->dbg, s->rig.q.n, s->rig.q.pp));
+    if (rbcs)       UC(dbg_check_pos_soft(s->dbg, s->rbc.q.n, s->rbc.q.pp));
+}
+
+static void check_vel(Sim *s) {
+    UC(dbg_check_vel(s->dbg, s->flu.q.n, s->flu.q.pp));
+    if (s->solids0) UC(dbg_check_vel(s->dbg, s->rig.q.n, s->rig.q.pp));
+    if (rbcs)       UC(dbg_check_vel(s->dbg, s->rbc.q.n, s->rbc.q.pp));
+}
+
+static void check_forces(Sim *s) {
+    UC(dbg_check_forces(s->dbg, s->flu.q.n, s->flu.ff));
+    if (s->solids0) UC(dbg_check_forces(s->dbg, s->rig.q.n, s->rig.ff));
+    if (rbcs)       UC(dbg_check_forces(s->dbg, s->rbc.q.n, s->rbc.ff));
+}
+
+
 void step(BForce *bforce, bool wall0, int it, Sim *s) {
     Flu *flu = &s->flu;
     Rbc *rbc = &s->rbc;
@@ -16,6 +35,7 @@ void step(BForce *bforce, bool wall0, int it, Sim *s) {
     Wall *wall = &s->wall;
     
     UC(check_sizes(s));
+    UC(check_pos_soft(s));
     
     UC(distribute_flu(s));
     if (s->solids0) UC(distribute_rig(/**/ rig));
@@ -25,6 +45,8 @@ void step(BForce *bforce, bool wall0, int it, Sim *s) {
     
     forces(wall0, s);
 
+    UC(check_forces(s));
+    
     dump_diag0(s->coords, it, s);
     dump_diag_after(it, wall0, s->solids0, s);
     body_force(it, bforce, s);
@@ -34,6 +56,8 @@ void step(BForce *bforce, bool wall0, int it, Sim *s) {
     if (s->solids0) update_solid(/**/ rig);
     if (rbcs)       update_rbc(it, rbc, s);
 
+    UC(check_vel(s));
+    
     if (s->opt.vcon && !s->equilibrating) {
         sample(s->coords, it, flu, /**/ &s->vcon);
         adjust(it, /**/ &s->vcon, bforce);
@@ -41,8 +65,11 @@ void step(BForce *bforce, bool wall0, int it, Sim *s) {
     }
 
     if (wall0) bounce_wall(s->coords, wall, /**/ flu, rbc);
-
+    
     if (sbounce_back && s->solids0) bounce_solid(it, /**/ &s->bb, rig, flu);
+
+    UC(check_pos_soft(s));
+    UC(check_vel(s));
 
     if (! s->equilibrating) {
         if (s->opt.inflow)     apply_inflow(s->inflow, /**/ flu);
