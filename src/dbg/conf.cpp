148b34603545987b833fa38bef9da2b50099c4e7
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-19 11:58:08 +0100

    conf from cfg

---
+++ src/dbg/conf.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "utils/error.h"
+#include "parser/imp.h"
+
+#include "imp.h"
+
+static void set(const Config *cfg, const char *desc, int kind, Dbg *dbg) {
+    int enabled = 0;
+    UC(conf_lookup_bool(cfg, desc, &enabled));
+    if (enabled) dbg_set(kind, dbg);
+}
+
+void dbg_set_conf(const Config *cfg, Dbg *dbg) {
+    set(cfg, "dbg.pos",      DBG_POS,      dbg);
+    set(cfg, "dbg.pos_soft", DBG_POS_SOFT, dbg);
+    set(cfg, "dbg.vel",      DBG_VEL,      dbg);
+    set(cfg, "dbg.forces",   DBG_FORCES,   dbg);
+    set(cfg, "dbg.colors",   DBG_COLORS,   dbg);
+    set(cfg, "dbg.clist",    DBG_CLIST,    dbg);
+}
