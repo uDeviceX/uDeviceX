4d00cb6033fd44112fa365e8ab435462fbfb285b
Sergey Litvinov <slitvinov@gmail.com>
2018-01-19 11:19:52 +0100

    wip: dflu_status_ini_conf

--- src/distr/flu/status/conf.cpp
+++ src/distr/flu/status/conf.cpp
@@ -6,8 +6,11 @@
 #include "parser/imp.h"
 #include "imp.h"
 
-void dflu_status_ini_conf(const Config *cfg, DFluStatus**) {
+void dflu_status_ini_conf(const Config *cfg, DFluStatus **ps) {
     int dbg;
     UC(conf_lookup_bool(cfg, "dflu.debug", /**/ &dbg));
     msg_print("dflu.debug = %d", dbg);
+
+    if (dbg) dflu_status_ini(ps);
+    else     *ps = NULL;
 }
