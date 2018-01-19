470ec6dce3f1dd613fc2481284138a8d297e42ac
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-19 08:44:24 +0100

    deps

--- src/u/mesh/make/dep.mk
+++ src/u/mesh/make/dep.mk
@@ -1,18 +1,18 @@
-$B/algo/minmax/imp.o: $S/inc/conf.h $S/inc/type.h $S/d/q.h $S/algo/minmax/imp.h $S/utils/cc.h $S/inc/def.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/utils/kl.h $S/d/ker.h $S/utils/msg.h
-$B/algo/scan/imp.o: $S/inc/conf.h $S/d/q.h $S/algo/scan/imp.h $S/utils/cc.h $S/inc/def.h $S/algo/scan/dev.h $S/algo/scan/cpu/imp.h $S/d/api.h $S/inc/dev.h $B/conf.h $S/utils/kl.h $S/algo/scan/cuda/imp.h $S/d/ker.h $S/utils/msg.h
-$B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/d/cuda/imp.h
-$B/io/off/imp.o: $S/utils/imp.h $S/utils/error.h $S/io/off/imp.h
-$B/mesh/bbox.o: $S/inc/type.h $S/algo/minmax/imp.h $S/mesh/bbox.h
-$B/mesh/collision.o: $S/inc/conf.h $S/utils/error.h $S/inc/type.h $S/d/q.h $S/utils/cc.h $S/inc/def.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/utils/texo.h $S/utils/kl.h $S/utils/texo.dev.h $S/utils/te.h $S/mesh/collision.h $S/d/ker.h $S/utils/msg.h
+$B/algo/minmax/imp.o: $S/algo/minmax/imp.h $B/conf.h $S/d/api.h $S/d/ker.h $S/d/q.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/inc/type.h $S/utils/cc.h $S/utils/kl.h $S/utils/msg.h
+$B/algo/scan/imp.o: $S/algo/scan/cpu/imp.h $S/algo/scan/cpu/type.h $S/algo/scan/cuda/imp.h $S/algo/scan/cuda/type.h $S/algo/scan/dev.h $S/algo/scan/imp.h $B/conf.h $S/d/api.h $S/d/ker.h $S/d/q.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/utils/cc.h $S/utils/error.h $S/utils/imp.h $S/utils/kl.h
+$B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/utils/error.h $S/utils/imp.h
+$B/io/off/imp.o: $S/io/off/imp.h $S/utils/error.h $S/utils/imp.h
+$B/mesh/bbox.o: $S/algo/minmax/imp.h $S/inc/type.h $S/mesh/bbox.h
+$B/mesh/collision.o: $B/conf.h $S/d/api.h $S/d/ker.h $S/d/q.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/inc/type.h $S/mesh/collision.h $S/utils/cc.h $S/utils/error.h $S/utils/kl.h $S/utils/msg.h $S/utils/te.h $S/utils/texo.dev.h $S/utils/texo.h
 $B/mesh/dist.o: $S/inc/type.h $S/mesh/dist.h
-$B/mesh/props.o: $S/mesh/props.h $S/inc/type.h
-$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
+$B/mesh/props.o: $S/inc/type.h $S/mesh/props.h
+$B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/u/mesh/main.o: $S/inc/conf.h $S/utils/error.h $S/inc/type.h $S/inc/def.h $S/utils/cc.h $S/d/api.h $S/inc/dev.h $B/conf.h $S/u/mesh/imp/main.h $S/utils/texo.h $S/utils/kl.h $S/io/off/imp.h $S/mesh/collision.h $S/utils/msg.h $S/mpi/glb.h
-$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
-$B/utils/error.o: $S/utils/msg.h $S/utils/error.h
+$B/u/mesh/main.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/inc/type.h $S/io/off/imp.h $S/mesh/collision.h $S/mpi/glb.h $S/u/mesh/imp/main.h $S/utils/cc.h $S/utils/error.h $S/utils/kl.h $S/utils/msg.h $S/utils/texo.h
+$B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/utils/cc/common.h $S/utils/error.h
+$B/utils/error.o: $S/utils/error.h $S/utils/msg.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
-$B/utils/mc.o: $S/inc/conf.h $S/utils/error.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
+$B/utils/mc.o: $B/conf.h $S/inc/conf.h $S/mpi/wrapper.h $S/utils/error.h $S/utils/mc.h
 $B/utils/msg.o: $S/utils/msg.h
-$B/utils/os.o: $S/utils/os.h $S/utils/error.h $S/utils/msg.h
+$B/utils/os.o: $S/utils/error.h $S/utils/msg.h $S/utils/os.h
--- src/u/scan/make/dep.mk
+++ src/u/scan/make/dep.mk
@@ -1,12 +1,12 @@
-$B/algo/scan/imp.o: $S/inc/conf.h $S/d/q.h $S/algo/scan/imp.h $S/utils/cc.h $S/inc/def.h $S/algo/scan/dev.h $S/algo/scan/cpu/imp.h $S/d/api.h $S/inc/dev.h $B/conf.h $S/utils/kl.h $S/algo/scan/cuda/imp.h $S/d/ker.h $S/utils/msg.h
-$B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/d/cuda/imp.h
-$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
+$B/algo/scan/imp.o: $S/algo/scan/cpu/imp.h $S/algo/scan/cpu/type.h $S/algo/scan/cuda/imp.h $S/algo/scan/cuda/type.h $S/algo/scan/dev.h $S/algo/scan/imp.h $B/conf.h $S/d/api.h $S/d/ker.h $S/d/q.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/utils/cc.h $S/utils/error.h $S/utils/imp.h $S/utils/kl.h
+$B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/utils/error.h $S/utils/imp.h
+$B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/u/scan/main.o: $S/inc/conf.h $S/algo/scan/imp.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/mpi/glb.h $S/utils/msg.h
-$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
-$B/utils/error.o: $S/utils/msg.h $S/utils/error.h
+$B/u/scan/main.o: $S/algo/scan/imp.h $B/conf.h $S/d/api.h $S/inc/conf.h $S/inc/dev.h $S/mpi/glb.h $S/utils/cc.h $S/utils/msg.h
+$B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/utils/cc/common.h $S/utils/error.h
+$B/utils/error.o: $S/utils/error.h $S/utils/msg.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
-$B/utils/mc.o: $S/inc/conf.h $S/utils/error.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
+$B/utils/mc.o: $B/conf.h $S/inc/conf.h $S/mpi/wrapper.h $S/utils/error.h $S/utils/mc.h
 $B/utils/msg.o: $S/utils/msg.h
-$B/utils/os.o: $S/utils/os.h $S/utils/error.h $S/utils/msg.h
+$B/utils/os.o: $S/utils/error.h $S/utils/msg.h $S/utils/os.h
