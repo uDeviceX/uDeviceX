a98500c1cce4f6a43376e0ccde942f8f3173f21d
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-19 11:08:31 +0100

    deps

--- src/u/error/make/dep.mk
+++ src/u/error/make/dep.mk
@@ -4,7 +4,8 @@ $B/frag/imp.o: $B/conf.h $S/frag/imp.h $S/inc/conf.h
 $B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/u/error/main.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/cc.h $S/utils/error.h $S/utils/mc.h $S/utils/msg.h
+$B/parser/imp.o: $S/parser/imp.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
+$B/u/error/main.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/parser/imp.h $S/utils/cc.h $S/utils/error.h $S/utils/imp.h $S/utils/mc.h $S/utils/msg.h
 $B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/utils/cc/common.h $S/utils/error.h
 $B/utils/error.o: $S/utils/error.h $S/utils/msg.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
--- src/u/error/make/rule.mk
+++ src/u/error/make/rule.mk
@@ -4,6 +4,7 @@ $B/frag/imp.o: $S/frag/imp.cpp; $X -I$S/. -I$S/frag
 $B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
 $B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/. -I$S/mpi
 $B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
+$B/parser/imp.o: $S/parser/imp.cpp; $X -I$S/. -I$S/parser
 $B/u/error/main.o: $S/u/error/main.cpp; $X -I$S/. -I$S/u/error
 $B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/. -I$S/utils
 $B/utils/error.o: $S/utils/error.cpp; $X -I$S/. -I$S/utils
