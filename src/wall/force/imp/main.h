e5c38a1eff3f03856d7347e5604d44d660c1a9b8
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-22 09:17:40 +0100

    minor doc with enum

--- src/wall/force/imp/main.h
+++ src/wall/force/imp/main.h
@@ -1,5 +1,9 @@
+enum {
+    TPP = 3 /* # threads per particle */
+};
+
 void force(Wvel_v wv, Coords c, Cloud cloud, int n, RNDunif *rnd, Wa wa, /**/ Force *ff) {
     KL(dev::force,
-       (k_cnf(3*n)),
+       (k_cnf(TPP*n)),
        (wv, c, cloud, n, rnd_get(rnd), wa, /**/ (float*)ff));
 }
