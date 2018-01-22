5184f2c52f0ae70ad4e8a1cecc8eb3d2b3f78976
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-22 11:28:17 +0100

    add force dump in punto

--- src/io/punto/imp.cpp
+++ src/io/punto/imp.cpp
@@ -8,6 +8,8 @@
 
 #include "imp.h"
 
+#define VFRMT "%16.10e %16.10e %16.10e"
+
 void punto_dump_pp(long n, const Particle *pp, const char *name) {
     enum {X, Y, Z};
     long i;
@@ -19,8 +21,28 @@ void punto_dump_pp(long n, const Particle *pp, const char *name) {
     for (i = 0; i < n; i++) {
         r = pp[i].r;
         v = pp[i].v;
-        rcode = fprintf(f, "%16.10e %16.10e %16.10e %16.10e %16.10e %16.10e\n",
+        rcode = fprintf(f, VFRMT " " VFRMT "\n",
                         r[X], r[Y], r[Z], v[X], v[Y], v[Z]);
+        if (rcode < 0) ERR("fprintf to file '%s' failed", name);
+    }
+    UC(efclose(f));
+    msg_print("dump %ld particles to '%s'", n, name);
+}
+
+void punto_dump_pp_ff(long n, const Particle *pp, const Force *ff, const char *name) {
+    enum {X, Y, Z};
+    long i;
+    FILE *f;
+    const float *r, *v, *a;
+    int rcode;
+
+    UC(efopen(name, "w", /**/ &f));
+    for (i = 0; i < n; i++) {
+        r = pp[i].r;
+        v = pp[i].v;
+        a = ff[i].f;
+        rcode = fprintf(f, VFRMT " " VFRMT " " VFRMT "\n",
+                        r[X], r[Y], r[Z], v[X], v[Y], v[Z], a[X], a[Y], a[Z]);
         if (rcode < 0) ERR("fprintf to file '%s' failed", name);
     }
     UC(efclose(f));
--- src/io/punto/imp.h
+++ src/io/punto/imp.h
@@ -1,2 +1,4 @@
 struct Particle;
+struct Force;
 void punto_dump_pp(long n, const Particle*, const char *name);
+void punto_dump_pp_ff(long n, const Particle*, const Force*, const char *name);
