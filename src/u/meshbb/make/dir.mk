376d178a3403ba994632eeb21bfc8c5bad0797f6
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-22 17:53:40 +0100

    rm meshbb unit

--- src/u/meshbb/make/dir.mk
+++
@@ -1,9 +0,0 @@
-D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/algo/scan && \
-    d $B/clist     && \
-    d $B/d         && \
-    d $B/io/off    && \
-    d $B/meshbb    && \
-    d $B/mpi       && \
-    d $B/u/meshbb  && \
-    d $B/utils    
