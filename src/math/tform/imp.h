fba8a993cef7dc1f1f72fb5aea01bff221771d1c
Sergey Litvinov <slitvinov@gmail.com>
2018-01-22 16:25:22 +0100

    wip: doc for tform

--- src/math/tform/imp.h
+++ src/math/tform/imp.h
@@ -1,22 +1,31 @@
 struct Tform;
 struct Tform_v;
 
-// tag::interface[]
-void tform_ini(Tform**); // <I>
+// tag::ini[]
+void tform_ini(Tform**);
 void tform_fin(Tform*);
+// end::ini[]
 
-void tform_vector(const float a0[3], const float a1[3],   const float b0[3], const float b1[3], /**/ Tform*); // <C>
+// tag::def[]
+void tform_vector(const float a0[3], const float a1[3],   const float b0[3], const float b1[3], /**/ Tform*);
 void tform_chain(Tform*, Tform*, /**/ Tform*);
 void tform_grid2grid(const float f_lo[3], const float f_hi[3], const int f_n[3],
                      const float t_lo[3], const float t_hi[3], const int t_n[3], /**/
                      Tform*);
 void tform_to_grid(const float a0[3], const float b0[3], const int n[3], /**/ Tform*);
 void tform_from_grid(const float a0[3], const float b0[3], const int n[3], /**/ Tform*);
+// end::def[]
 
-void tform_convert(Tform*, const float a0[3], /**/ float a1[3]); // <U>
 
-void tform_to_view(Tform*, /**/ Tform_v*); // <V>
+// tag::convert[]
+void tform_convert(Tform*, const float a0[3], /**/ float a1[3]);
+// end::convert[]
 
-void tform_log(Tform*); // <L>
+// tag::view[]
+void tform_to_view(Tform*, /**/ Tform_v*);
+// end::view[]
+
+// tag::log[]
+void tform_log(Tform*);
 void tform_dump(Tform*, FILE*);
-// end::interface[]
+// end::log
