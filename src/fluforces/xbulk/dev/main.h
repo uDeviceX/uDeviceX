5014a171da170d624d4449416e16b55feed7ba16
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-23 17:26:38 +0100

    initial import of xbulk

---
+++ src/fluforces/xbulk/dev/main.h
@@ -0,0 +1,19 @@
+__device__ void fetch(BCloud c, int i, forces::Pa *p) {
+    float4 r, v;
+    r = c.pp[2*i + 0];
+    v = c.pp[2*i + 1];
+
+    forces::r3v3k2p(r.x, r.y, r.z,
+                    v.x, v.y, v.z,
+                    SOLVENT_KIND, /**/ p);
+    
+    if (multi_solvent)
+        p->color = c.cc[i];
+}
+
+__global__ void main(int n, BCloud cloud, const int *start, const int *count, RNDunif *rnd, /**/ Force *ff) {
+    int i;
+    i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    
+}
---
+++ src/fluforces/xbulk/imp.h
@@ -0,0 +1,7 @@
+struct BCloud {
+    const float4 *pp
+    const int *cc;
+};
+
+void flocal(int n, BCloud cloud, const int *start, const int *count, RNDunif *rnd, /**/ Force *ff);
+
