6e9e6cd95f849748354b0aeb86dcc5c1357adf4b
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-23 17:51:25 +0100

    add empty map

---
+++ src/fluforces/xbulk/dev/map.h
@@ -0,0 +1,12 @@
+struct Map {
+    int a;
+};
+
+__device__ void map_build(int i, const int *ss, const int *cc, /**/ Map *m) {
+    int s;
+    s = ss[i];
+}
+
+__device__ int map_next(Map m) {
+    
+}
