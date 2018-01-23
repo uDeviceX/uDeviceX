fd54db727c4122c31377f10da26ff1557ccb48a1
Sergey Litvinov <slitvinov@gmail.com>
2018-01-23 10:34:06 +0100

    use static_assert

--- src/fluforces/imp/main.h
+++ src/fluforces/imp/main.h
@@ -1,5 +1,6 @@
 static void zip(const int n, const float *pp, /**/ float4 *zipped_pp, ushort4 *zipped_rr) {
-    assert(sizeof(Particle) == 6 * sizeof(float)); /* :TODO: implicit dependency */
+    static_assert(sizeof(Particle) == 6 * sizeof(float),
+                  "sizeof(Particle) != 6 * sizeof(float)");
     KL(dev::zip, (k_cnf(n)), (n, pp, zipped_pp, zipped_rr));
 }
 
