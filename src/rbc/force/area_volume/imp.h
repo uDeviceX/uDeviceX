66bca1dbf3c9f6c6616d8bd9f36368c620abaa44
Sergey Litvinov <slitvinov@gmail.com>
2018-01-23 17:02:30 +0100

    rm commented code

--- src/rbc/force/area_volume/imp.h
+++ src/rbc/force/area_volume/imp.h
@@ -8,5 +8,4 @@ void area_volume_fin(AreaVolume*);
 
 int4* area_volume_tri(AreaVolume*);
 
-// void area_volume_compute(int nt, int nv, int nc, const Particle*, const int4 *tri, /**/ float *av);
 void area_volume_compute(AreaVolume*, int nc, const Particle*, /**/ float *av);
