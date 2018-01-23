d1c246eacbe5fa66f285efa1e3788178c70a9ff9
Sergey Litvinov <slitvinov@gmail.com>
2018-01-23 12:46:33 +0100

    add message

--- src/fluforces/bulk/transpose/imp/main.h
+++ src/fluforces/bulk/transpose/imp/main.h
@@ -1,4 +1,5 @@
 void transpose(int n, /*io*/ Force *ff) {
-    static_assert(sizeof(Force) == 3*sizeof(float));
+    static_assert(sizeof(Force) == 3*sizeof(float),
+                 "sizeof(Force) != 3*sizeof(float)");
     KL(transpose, (28, 1024), (n, (float*)ff));
 }
