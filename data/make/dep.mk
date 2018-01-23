772bc915b4e245a64f283aa578e4db7d76b71bb2
Sergey Litvinov <slitvinov@gmail.com>
2018-01-23 12:02:35 +0100

    install ply, off and dat (sdf files)

--- data/make/dep.mk
+++ data/make/dep.mk
@@ -1,21 +1,62 @@
-P = \
-cells/rbc/2562.off \
-cells/rbc/1986.off \
-cells/rbc/sc/2562.off \
-cells/rbc/sc/1986.off \
-cells/rbc/sc/642.off \
-cells/rbc/sc/498.off \
-cells/rbc/642.off \
-cells/rbc/498.off \
-cells/main/piramid.off \
-cells/main/side.off \
-cells/sph/2562.off \
-cells/sph/1986.off \
-cells/sph/sc/2562.off \
-cells/sph/sc/1986.off \
-cells/sph/sc/642.off \
-cells/sph/sc/162.off \
-cells/sph/sc/498.off \
-cells/sph/642.off \
-cells/sph/162.off \
+O = \
+cells/rbc/2562.off      \
+cells/rbc/1986.off      \
+cells/rbc/sc/2562.off   \
+cells/rbc/sc/1986.off   \
+cells/rbc/sc/642.off    \
+cells/rbc/sc/498.off    \
+cells/rbc/642.off       \
+cells/rbc/498.off       \
+cells/main/piramid.off  \
+cells/main/side.off     \
+cells/sph/2562.off      \
+cells/sph/1986.off      \
+cells/sph/sc/2562.off   \
+cells/sph/sc/1986.off   \
+cells/sph/sc/642.off    \
+cells/sph/sc/162.off    \
+cells/sph/sc/498.off    \
+cells/sph/642.off       \
+cells/sph/162.off       \
 cells/sph/498.off
+
+S = \
+sdf/pachinko/sdf.dat      \
+sdf/zplates1/main.dat     \
+sdf/black_hole/bh.dat     \
+sdf/yplates1/yplates.dat  \
+sdf/cyl1/cyl.dat          \
+sdf/wall1/wall.dat
+
+R = \
+rig/ellipse.ply               \
+rig/spheres/sphereL2_D63.ply  \
+rig/spheres/sphereL2_D15.ply  \
+rig/spheres/sphereL2_D32.ply  \
+rig/spheres/sphereL2_D36.ply  \
+rig/spheres/sphereL2_D24.ply  \
+rig/spheres/sphereL2_D42.ply  \
+rig/spheres/sphereL2_D48.ply  \
+rig/spheres/sphereL2_D3.ply   \
+rig/spheres/sphereL2_D72.ply  \
+rig/spheres/sphereL2_D54.ply  \
+rig/spheres/sphereL2_D16.ply  \
+rig/spheres/sphereL2_D18.ply  \
+rig/spheres/sphereL2_D21.ply  \
+rig/spheres/sphereL2_D4.ply   \
+rig/spheres/sphereL2_D6.ply   \
+rig/spheres/sphereL2_D8.ply   \
+rig/spheres/sphereL2_D45.ply  \
+rig/spheres/sphereL2_D30.ply  \
+rig/spheres/sphereL2_D27.ply  \
+rig/spheres/sphereL2_D12.ply  \
+rig/spheres/sphereL2_D20.ply  \
+rig/spheres/sphereL2_D9.ply   \
+rig/sphere_R2.ply             \
+rig/cyl.ply                   \
+rig/ellipsoid.ply             \
+rig/oscad_sphere.ply          \
+rig/sphere_R1.ply             \
+rig/cylinder.ply              \
+rig/sphere_R0.7.ply           \
+rig/sphere.ply
