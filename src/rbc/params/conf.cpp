31db80b0e16b44d76c6f1ee234e31bcd319025cd
Sergey Litvinov <slitvinov@gmail.com>
2018-01-24 21:12:26 +0100

    rm `p' from rbc_params_set

--- src/rbc/params/conf.cpp
+++ src/rbc/params/conf.cpp
@@ -9,7 +9,7 @@
 #include "imp.h"
 
 void rbc_params_set_conf(const Config *c, RbcParams *par) {
-    float gc, gt, kbt, kb, phi, ks, p, mpow, x0, ka, kd, kv, dt0;
+    float gc, gt, kbt, kb, phi, ks, mpow, x0, ka, kd, kv, dt0;
 
     UC(conf_lookup_float(c, "rbc.gammaC", &gc));
     UC(conf_lookup_float(c, "rbc.gammaT", &gt));
@@ -17,7 +17,6 @@ void rbc_params_set_conf(const Config *c, RbcParams *par) {
     UC(conf_lookup_float(c, "rbc.kb", &kb));
     UC(conf_lookup_float(c, "rbc.phi", &phi));
     UC(conf_lookup_float(c, "rbc.ks", &ks));
-    UC(conf_lookup_float(c, "rbc.p", &p));
     UC(conf_lookup_float(c, "rbc.x0", &x0));
     UC(conf_lookup_float(c, "rbc.mpow", &mpow));
     UC(conf_lookup_float(c, "rbc.ka", &ka));
