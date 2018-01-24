ea2ec29782c932b2a2cbb553eba6150e603be44a
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-24 08:57:02 +0100

    brutal implementation

--- src/fluforces/xbulk/dev/main.h
+++ src/fluforces/xbulk/dev/main.h
@@ -19,7 +19,14 @@ __device__ int3 get_cid(const forces::Pa *pa) {
     return c;
 }
 
-__device__ void one_cell(int ia, forces::Pa pa, BCloud c, int start, int count, float rnd, /**/ Force *fa, Force *ff) {
+__device__ bool valid_cid(int3 c) {
+    return
+        (c.x >= 0) && (c.x < XS) &&
+        (c.y >= 0) && (c.y < YS) &&
+        (c.z >= 0) && (c.z < ZS);    
+}
+
+__device__ void one_cell(int ia, forces::Pa pa, BCloud c, int start, int count, float rnd, /**/ float fa[3], Force *ff) {
     enum {X, Y, Z};
     int i, ib;
     forces::Pa pb;
@@ -35,9 +42,9 @@ __device__ void one_cell(int ia, forces::Pa pa, BCloud c, int start, int count, 
         
         forces::force(pa, pb, rnd, /**/ &f);
         
-        fa->f[X] += f.x;
-        fa->f[Y] += f.y;
-        fa->f[Z] += f.z;
+        fa[X] += f.x;
+        fa[Y] += f.y;
+        fa[Z] += f.z;
 
         atomicAdd(fb + X, -f.x);
         atomicAdd(fb + Y, -f.y);
@@ -46,15 +53,30 @@ __device__ void one_cell(int ia, forces::Pa pa, BCloud c, int start, int count, 
 }
 
 __global__ void apply(int n, BCloud cloud, const int *start, const int *count, float rnd, /**/ Force *ff) {
-    int ia;
-    int3 cid;
+    enum {X, Y, Z};
+    int ia, ib;
+    int3 ca, cb;
     forces::Pa pa;
+    float fa[3] = {0};
 
     ia = threadIdx.x + blockIdx.x * blockDim.x;
     if (ia >= n) return;
     
     fetch(cloud, ia, &pa);
-    cid = get_cid(&pa);
+    ca = get_cid(&pa);
 
-    
+    for (cb.z = ca.z - 1; cb.z <= ca.z + 1; ++cb.z) {
+        for (cb.y = ca.y - 1; cb.y <= ca.y + 1; ++cb.y) {
+            for (cb.x = ca.x - 1; cb.x <= ca.x + 1; ++cb.x) {
+                if (!valid_cid(cb)) continue;
+                ib = cb.x + XS * (cb.y + YS * cb.z);
+                
+                one_cell(ia, pa, cloud, start[ib], count[ib], rnd, /**/ fa, ff);
+            }        
+        }        
+    }
+
+    atomicAdd(ff[ia].f + X, fa[X]);
+    atomicAdd(ff[ia].f + Y, fa[Y]);
+    atomicAdd(ff[ia].f + Z, fa[Z]);
 }
