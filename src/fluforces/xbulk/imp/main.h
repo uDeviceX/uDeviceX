c1e0bc943becd760ab4e0171455d90511e6d4efa
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-24 15:07:30 +0100

    THR in enum

--- src/fluforces/xbulk/imp/main.h
+++ src/fluforces/xbulk/imp/main.h
@@ -1,9 +1,10 @@
+enum {THR=128};
 
 void flocal(int n, BCloud cloud, const int *start, const int *count, RNDunif *rnd, /**/ Force *ff) {
     float seed;
     seed = rnd_get(rnd);
     KL(flocaldev::apply,
-       (k_cnf(n)),
+       (ceiln((n), THR), THR),
        (n, cloud, start, count, seed, /**/ ff));
 }
 
