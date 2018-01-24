8628b2eb9d9d7b75c226e1ed15865fab4e325e10
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-24 10:04:25 +0100

    add basic interface

--- src/io/punto/imp.h
+++ src/io/punto/imp.h
@@ -1,4 +1,15 @@
 struct Particle;
 struct Force;
+
 void punto_write_pp(long n, const Particle*, const char *name);
 void punto_write_pp_ff(long n, const Particle*, const Force*, const char *name);
+
+struct PuntoRead;
+
+void punto_read_pp(const char *name, PuntoRead **);
+void punto_read_pp_ff(const char *name, PuntoRead **);
+void punto_read_fin(PuntoRead*);
+
+int punto_read_get_n(const PuntoRead *);
+const Particle* punto_read_get_pp(const PuntoRead *);
+const Force*    punto_read_get_ff(const PuntoRead *);
