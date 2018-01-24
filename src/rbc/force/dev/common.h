ab2096fe9a0806ef7e0e57f6c61cdae43a0847ae
Sergey Litvinov <slitvinov@gmail.com>
2018-01-24 12:08:44 +0100

    wlc is a function

--- src/rbc/force/dev/common.h
+++ src/rbc/force/dev/common.h
@@ -25,8 +25,11 @@ static __device__ float3 farea(RbcParams_v par, float3 x21, float3 x31, float3 x
 }
 
 static __device__ float sq(float x) { return x * x; }
+static __device__ float wlc(float kbT, float lmax, float p, float r) {
+    return (kbT*(4*sq(r)-9*lmax*r+6*sq(lmax)))/(4*lmax*p*sq(r-lmax));
+}
 static __device__ float3 fspring(RbcParams_v par, float3 x21, float l0) {
-  #define wlc_r(r) (kbT*(4*sq(r)-9*lmax*r+6*sq(lmax)))/(4*lmax*p*sq(r-lmax))
+  #define wlc_r(r) (wlc(kbT, lmax, p, r))
     float m;
     float  r, fwlc, fpow, lmax, kbT, p, x0;
     float3 f;
