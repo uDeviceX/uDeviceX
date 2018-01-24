4ac845888ef5f1c062b8165fdb2f568e142ed311
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-24 10:38:04 +0100

    deps

--- src/u/rbc/force/make/dep.mk
+++ src/u/rbc/force/make/dep.mk
@@ -11,7 +11,7 @@ $B/io/mesh/imp.o: $B/conf.h $S/coords/imp.h $S/inc/conf.h $S/inc/type.h $S/io/me
 $B/io/mesh/write/imp.o: $B/conf.h $S/inc/conf.h $S/io/mesh/write/imp.h $S/io/mesh/write/imp/main.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/io/off/imp.o: $S/io/off/imp.h $S/utils/error.h $S/utils/imp.h
 $B/io/ply/imp.o: $S/inc/def.h $S/inc/type.h $S/io/ply/imp.h $S/io/ply/imp/ascii.h $S/io/ply/imp/bin.h $S/io/ply/imp/common.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
-$B/io/punto/imp.o: $S/inc/type.h $S/io/punto/imp.h $S/io/punto/imp/dump.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
+$B/io/punto/imp.o: $S/inc/type.h $S/io/punto/imp.h $S/io/punto/imp/dump.h $S/io/punto/imp/read.h $S/io/punto/imp/type.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
 $B/io/restart/imp.o: $B/conf.h $S/coords/imp.h $S/inc/conf.h $S/inc/def.h $S/inc/type.h $S/io/restart/imp.h $S/mpi/glb.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
 $B/io/rig/imp.o: $B/conf.h $S/coords/imp.h $S/inc/conf.h $S/inc/type.h $S/utils/error.h $S/utils/imp.h
 $B/math/linal/imp.o: $S/math/linal/imp.h $S/utils/error.h
