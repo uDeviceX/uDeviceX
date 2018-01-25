8cc4f26391e5fa030246249e73ff819ee593fb26
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-25 08:40:21 +0100

    deps

--- src/u/mpi/make/rule.mk
+++ src/u/mpi/make/rule.mk
@@ -1,11 +1,11 @@
-$B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
-$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
-$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
-$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
+$B/d/api.o: $S/d/api.cu; $N -I$S/. -I$S/d
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
+$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/. -I$S/mpi
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
 $B/u/mpi/main.o: $S/u/mpi/main.cpp; $X -I$S/.
-$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
-$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
-$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/utils -I$S/.
-$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
-$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/utils -I$S/.
-$B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
+$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/. -I$S/utils
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/. -I$S/utils
+$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/. -I$S/utils
+$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/. -I$S/utils
+$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/. -I$S/utils
+$B/utils/os.o: $S/utils/os.cpp; $X -I$S/. -I$S/utils
