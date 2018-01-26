b9419fc3f57d336b666ae1d045029563afe2780a
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-26 12:10:00 +0100

    adding hidden host type

---
+++ src/pair/imp/type.h
@@ -0,0 +1,22 @@
+enum {
+    BLUE,
+    RED,
+    MAX_COL
+}; 
+
+/* maximum of pairwise dpd parameters */
+enum {
+    MAX_PAR = (MAX_COL * (MAX_COL + 1)) / 2
+};
+
+struct PairParams {
+    float a[MAX_PAR]; /* a    : conservative */
+    float g[MAX_PAR]; /* gamma: dissipative  */
+    float s[MAX_PAR]; /* sigma: random       */
+    int ncol;         /* number of colors    */
+
+    bool lj;
+    float lj_s; /* lennard jones sigma   */
+    float lj_e; /* lennard jones epsilon */
+};
+
