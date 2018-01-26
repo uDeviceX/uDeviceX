07181faecfdfd275ef088ff9887c230e94dceddb
Sergey Litvinov <slitvinov@gmail.com>
2018-01-26 17:48:16 +0100

    mv u/h5 to u/io/h5

--- src/u/france/make/dep.mk
+++ src/u/france/make/dep.mk
@@ -59,7 +59,7 @@ $B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/pair/conf.o: $S/pair/imp.h $S/parser/imp.h
-$B/pair/imp.o: $S/pair/imp.h $S/pair/imp/main.h $S/pair/imp/type.h $S/utils/error.h $S/utils/imp.h
+$B/pair/imp.o: $S/pair/imp.h $S/pair/imp/main.h $S/pair/imp/type.h $S/pair/type.h $S/utils/error.h $S/utils/imp.h
 $B/parser/imp.o: $S/parser/imp.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
 $B/rbc/adj/imp.o: $S/rbc/adj/imp.h $S/rbc/adj/imp/anti.h $S/rbc/adj/imp/fin.h $S/rbc/adj/imp/ini.h $S/rbc/adj/imp/map.h $S/rbc/adj/type/common.h $S/rbc/adj/type/hst.h $S/rbc/edg/imp.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
 $B/rbc/com/imp.o: $B/conf.h $S/d/api.h $S/d/ker.h $S/d/q.h $S/inc/conf.h $S/inc/def.h $S/inc/dev.h $S/inc/type.h $S/math/dev.h $S/rbc/com/dev/main.h $S/rbc/com/imp.h $S/rbc/com/imp/com.h $S/rbc/com/imp/fin.h $S/rbc/com/imp/ini.h $S/utils/cc.h $S/utils/kl.h $S/utils/msg.h
--- src/u/io/h5/make/rule.mk
+++ src/u/io/h5/make/rule.mk
@@ -6,7 +6,7 @@ $B/io/field/xmf/imp.o: $S/io/field/xmf/imp.cpp; $X -I$S/. -I$S/io/field/xmf
 $B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
 $B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/. -I$S/mpi
 $B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
-$B/u/h5/main.o: $S/u/h5/main.cpp; $X -I$S/.
+$B/u/io/h5/main.o: $S/u/io/h5/main.cpp; $X -I$S/.
 $B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/. -I$S/utils
 $B/utils/error.o: $S/utils/error.cpp; $X -I$S/. -I$S/utils
 $B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/. -I$S/utils
