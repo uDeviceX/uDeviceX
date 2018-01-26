a47680768d8e12cad480de4c819e89809b704a59
Sergey Litvinov <slitvinov@gmail.com>
2018-01-26 17:45:24 +0100

    plan a new API for io/off

--- src/io/off/imp.h
+++ src/io/off/imp.h
@@ -6,3 +6,14 @@ void off_read_vert(const char *f, int max, /**/ int *nv, float *vert);
 
 /* file to faces : max: maximum face number */
 void off_read_faces(const char *f, int max, /**/ int *nf, int4 *faces);
+
+/***/
+struct OffRead;
+struct int4;
+void off_read(const char *path, OffRead**);
+void off_fin(OffRead*);
+
+int    off_get_n(OffRead*);
+int4  *off_get_tri(OffRead*);
+float *off_get_vert(OffRead*);
+/***/
