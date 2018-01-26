63322a8435e02f070ce1a95ca66818c37fc10bc4
Sergey Litvinov <slitvinov@gmail.com>
2018-01-26 11:54:57 +0100

    add text

--- src/io/txt/imp.h
+++ src/io/txt/imp.h
@@ -1,11 +1,13 @@
 struct Particle;
 struct Force;
-
-void txt_write_pp(long n, const Particle*, const char *name);
-void txt_write_pp_ff(long n, const Particle*, const Force*, const char *name);
-
 struct TxtRead;
 
+// tag::write[]
+void txt_write_pp(long n, const Particle*, const char *name);
+void txt_write_pp_ff(long n, const Particle*, const Force*, const char *name);
+// end::write[]
+
+// tag::read[]
 void txt_read_pp(const char *name, TxtRead **);
 void txt_read_pp_ff(const char *name, TxtRead **);
 void txt_read_fin(TxtRead*);
@@ -13,3 +15,4 @@ void txt_read_fin(TxtRead*);
 int txt_read_get_n(const TxtRead *);
 const Particle* txt_read_get_pp(const TxtRead *);
 const Force*    txt_read_get_ff(const TxtRead *);
+// end::read[]
