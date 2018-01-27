5b96b2da1506d7d0c44d8f582bdabc17a0243036
Sergey Litvinov <slitvinov@gmail.com>
2018-01-27 10:48:53 +0100

    add vert

--- src/io/off/imp/main.h
+++ src/io/off/imp/main.h
@@ -45,20 +45,41 @@ static int sizes(FILE *f, char *s, /**/ int *nv, int *nt) {
     r = sscanf(s, "%d %d %*d", nv, nt);
     return r == 2;
 }
+static int vert(FILE *f, char *s, int n, float *rr) {
+    enum {X, Y, Z};
+    int i;
+    float *r;
+    for (i = 0; i < n; i++) {
+        if (line(f, /**/ s) == ERR) return 0;
+        r = &rr[3*i];
+        if (sscanf(s, "%f %f %f", &r[X], &r[Y], &r[Z]) != 3) return 0;
+    }
+    return 1;
+}
+static int tri(FILE *f, char *s, int n, int4 *tt) {
+    return 1;
+}
 static void read(FILE *f, const char *path, /**/ OffRead *q) {
     int nv, nt;
     char s[SIZE];
-    if (!header(f, /**/ s)) {
+    float *rr;
+    int4  *tt;
+    if (!header(f, s)) {
         msg_print("not an OFF file: '%s'", s);
-        ERR("faild to read: '%s'", path);
+        ERR("failed to read: '%s'", path);
     }
-    if (!sizes(f, /**/ s, &nv, &nt)) {
+    if (!sizes(f, s, &nv, &nt)) {
         msg_print("wrong sizes: '%s'", s);
-        ERR("faild to read: '%s'", path);
+        ERR("failed to read: '%s'", path);
     }
-    /* vert(f);
-    tri(f); */
+    UC(emalloc(3*nv*sizeof(rr[0]), (void**)&rr));
+    UC(emalloc(  nt*sizeof(tt[0]), (void**)&tt));    
+    if (!vert(f, s, nv, /**/ rr))
+        ERR("failed to read vertices: '%s'", path);
+    if (!tri(f, s, nt, /**/ tt))
+        ERR("failed to read triangles: '%s'", tri);
     q->nv = nv; q->nt = nt;
+    q->rr = rr; q->tt = tt;
 }
 
 void off_read(const char *path, OffRead **pq) {
@@ -72,8 +93,8 @@ void off_read(const char *path, OffRead **pq) {
 }
 
 void off_fin(OffRead* q) {
-    //    UC(efree(q->rr));
-    //    UC(efree(q->tt));
+    UC(efree(q->rr));
+    UC(efree(q->tt));
     UC(efree(q));
 }
 
