647b3028aa5bfc8706eb3d9bdcdb98f3f0f70420
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-01-29 14:39:59 +0100

    add move documentation

--- src/scheme/move/imp.h
+++ src/scheme/move/imp.h
@@ -1,3 +1,5 @@
 struct MoveParams;
-void scheme_move_apply(MoveParams* moveparams, float mass, int n, const Force*, Particle*);
-void scheme_move_clear_vel(int n, Particle*);
+// tag::int[]
+void scheme_move_apply(MoveParams* moveparams, float mass, int n, const Force*, Particle*); // <1>
+void scheme_move_clear_vel(int n, Particle*); // <2>
+// end::int[]
--- src/scheme/move/params/imp.h
+++ src/scheme/move/params/imp.h
@@ -4,11 +4,19 @@ struct MoveParams_v {
     float dt0;
 };
 
+// tag::mem[]
 void scheme_move_params_ini(MoveParams **);
 void scheme_move_params_fin(MoveParams *);
+// end::mem[]
 
+// tag::set[]
 void scheme_move_params_set_timestep(float dt0, MoveParams *);
+// end::set[]
 
+// tag::cnf[]
 void scheme_move_params_conf(const Config *c, MoveParams *);
+// end::cnf[]
 
+// tag::int[]
 MoveParams_v scheme_move_params_get_view(const MoveParams *);
+// end::int[]
