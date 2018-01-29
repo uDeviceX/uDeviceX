5ba8f43090d99baa2aa29c12bfe4fd28e80c82c8
Sergey Litvinov <slitvinov@gmail.com>
2018-01-29 11:50:13 +0100

    dpes

--- src/u/io/mesh/make/dir.mk
+++ src/u/io/mesh/make/dir.mk
@@ -1,8 +1,8 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/coords   && \
-    d $B/d        && \
-    d $B/io/off   && \
-    d $B/mpi      && \
-    d $B/parser   && \
-    d $B/u/io/off && \
-    d $B/utils   
+    d $B/coords    && \
+    d $B/d         && \
+    d $B/io/off    && \
+    d $B/mpi       && \
+    d $B/parser    && \
+    d $B/u/io/mesh && \
+    d $B/utils    
