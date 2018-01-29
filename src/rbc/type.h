4a215cb7c2d7c06645f2abc24ad6fd570060a713
Sergey Litvinov <slitvinov@gmail.com>
2018-01-29 20:10:36 +0100

    minor

--- src/rbc/type.h
+++ src/rbc/type.h
@@ -12,14 +12,11 @@ struct Shape {
 
 struct RbcQuants {
     int n, nc;             /* number of particles, cells            */
+    int nt, nv;            /* number of triangles and vertices per mesh */
     Particle *pp, *pp_hst; /* vertices particles on host and device */
     float *av;             /* area and volume on device             */
-
     int *ii;               /* global ids (on host) */
-
-    int nt, nv;            /* number of triangles and vertices per mesh */
     int *adj0, *adj1;      /* adjacency lists on device                 */
     AreaVolume *area_volume; /* a structure for area and volume computations */
-
     Shape shape;
 };
