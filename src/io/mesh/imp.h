c526773d48d5e1abbc27e42777df2b07472a4a1c
Sergey Litvinov <slitvinov@gmail.com>
2018-01-29 16:51:07 +0100

    rm mesh_write_rig

--- src/io/mesh/imp.h
+++ src/io/mesh/imp.h
@@ -4,12 +4,7 @@ struct int4;
 struct MeshWrite;
 struct OffRead;
 
-void mesh_write_rig(MPI_Comm, const Coords*, const Particle*,  const int4 *faces, int nc, int nv, int nt, int id);
-
-/*****/
 void mesh_write_ini(const int4 *tt, int nv, int nt, const char *directory, /**/ MeshWrite**);
 void mesh_write_ini_off(OffRead*, const char *directory, /**/ MeshWrite**);
 void mesh_write_dump(MeshWrite*, MPI_Comm, const Coords*, int nc, const Particle*, int id);
-
 void mesh_write_fin(MeshWrite*);
-/*****/
