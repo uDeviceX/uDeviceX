c5eb5a8b28be149532cae57a56fefd80a148b689
Sergey Litvinov <slitvinov@gmail.com>
2018-01-29 20:00:53 +0100

    minor

--- src/rbc/stretch/imp.h
+++ src/rbc/stretch/imp.h
@@ -1,5 +1,6 @@
 struct RbcStretch;
+struct Force;
 
-void rbc_stretch_ini(const char* path, int nv, /**/ RbcStretch **fp); /* `nv` is for error check */
-void rbc_stretch_fin(RbcStretch *f);
+void rbc_stretch_ini(const char* path, int nv, /**/ RbcStretch**); /* `nv` is for error check */
+void rbc_stretch_fin(RbcStretch*);
 void rbc_stretch_apply(int nm, const RbcStretch*, /**/ Force*);
