aa60ffe3d9bf541a241b9bb4bde7d7ad3890773e
Sergey Litvinov <slitvinov@gmail.com>
2018-01-29 10:42:36 +0100

    predeclare Coords, Particle, int4

--- src/io/mesh/imp.h
+++ src/io/mesh/imp.h
@@ -1,2 +1,6 @@
+struct Coords;
+struct Particle;
+struct int4;
+
 void mesh_write_rbc(MPI_Comm, const Coords*, const Particle*,  const int4 *faces, int nc, int nv, int nt, int id);
 void mesh_write_rig(MPI_Comm, const Coords*, const Particle*,  const int4 *faces, int nc, int nv, int nt, int id);
