4eecd645dfd157b1ac6f40bdb2efe66011cb4ba9
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-30 14:45:35 +0100

    deps

--- src/u/comm/main/make/dep.mk
+++ src/u/comm/main/make/dep.mk
@@ -4,7 +4,7 @@ $B/frag/imp.o: $S/frag/dev.h $S/frag/imp.h
 $B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/u/comm/main/main.o: $S/comm/imp.h $S/frag/imp.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/error.h $S/utils/msg.h
+$B/u/comm/main/main.o: $S/comm/imp.h $B/conf.h $S/frag/imp.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/error.h $S/utils/msg.h
 $B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/utils/cc/common.h $S/utils/error.h
 $B/utils/error.o: $S/utils/error.h $S/utils/msg.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
