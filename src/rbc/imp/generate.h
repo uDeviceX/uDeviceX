9b0ae94f37c469489e09a517aaca3aca629e9b34
Sergey Litvinov <slitvinov@gmail.com>
2018-01-30 12:28:12 +0100

    compiles

--- src/rbc/imp/generate.h
+++ src/rbc/imp/generate.h
@@ -17,7 +17,7 @@ static void gen_ids(MPI_Comm comm, long nc, /**/ int *ii) {
 void rbc_gen_quants(const Coords *coords, MPI_Comm comm, OffRead *off, const char *ic, /**/ RbcQuants *q) {
     int md, nt, nv;
     const float* vv;
-    md = RBCmd; nt = off_gen_nt(off); nv = off_get_nv(off); vv = off_get_vert(off);
+    md = RBCmd; nt = off_get_nt(off); nv = off_get_nv(off); vv = off_get_vert(off);
     setup(md, nt, nv, off, /**/ q);
     setup_from_pos(coords, comm, vv, ic, q->nv, /**/ q->pp, &q->nc, &q->n, /*w*/ q->pp_hst);
     if (rbc_ids)
