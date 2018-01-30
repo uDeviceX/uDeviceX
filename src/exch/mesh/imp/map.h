2aa002bec78bd4f9783ebe10280ac8836ca8cb20
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-30 09:19:18 +0100

    use L inside exch map

--- src/exch/mesh/imp/map.h
+++ src/exch/mesh/imp/map.h
@@ -1,11 +1,11 @@
-static void fill_map(int n, const float3 *lo, const float3 *hi, /**/ EMap map) {
-    int3 L = make_int3(XS-2, YS-2, ZS-2);
-    KL(dev::build_map, (k_cnf(n)), (L, n, lo, hi, /**/ map));
+static void fill_map(int3 L, int n, const float3 *lo, const float3 *hi, /**/ EMap map) {
+    int3 L0 = make_int3(L.x-2, L.y-2, L.z-2);
+    KL(dev::build_map, (k_cnf(n)), (L0, n, lo, hi, /**/ map));
 }
 
 void emesh_build_map(int nm, int nv, const Particle *pp, /**/ EMeshPack *p) {
     emap_reini(1, NFRAGS, /**/ p->map);
     minmax(pp, nv, nm, /**/ p->minext, p->maxext);
-    fill_map(nm, p->minext, p->maxext, /**/ p->map);
+    fill_map(p->L, nm, p->minext, p->maxext, /**/ p->map);
     emap_scan(1, NFRAGS, /**/ p->map);
 }
