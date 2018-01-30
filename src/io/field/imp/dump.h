b4ce63fa422082a1bce0985158f35d42b2ddfb40
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-30 15:03:11 +0100

    use coords instead of XS

--- src/io/field/imp/dump.h
+++ src/io/field/imp/dump.h
@@ -16,21 +16,27 @@ static void zero(float *rho, float *v[3], int n) {
     zero0(v[X], n); zero0(v[Y], n); zero0(v[Z], n);
 }
 
-static void avg(Particle *pp, int n, int nc, /**/
+static void avg(const Coords *coords, Particle *pp, int n, int nc, /**/
                 float *rho, float *u[3]) {
     enum {X, Y, Z};
     int c, i, entry;
     float *r, *v;
+    int Lx, Ly, Lz;
+
+    Lx = xs(coords);
+    Ly = ys(coords);
+    Lz = zs(coords);
+
     zero(rho, u, nc);
     for (i = 0; i < n; ++i) {
         r = pp[i].r;
         v = pp[i].v;
         int index[3] = {
-            minmax(0, XS - 1, (int)(floor(r[X])) + XS / 2),
-            minmax(0, YS - 1, (int)(floor(r[Y])) + YS / 2),
-            minmax(0, ZS - 1, (int)(floor(r[Z])) + ZS / 2)
+            minmax(0, Lx - 1, (int)(floor(r[X])) + Lx / 2),
+            minmax(0, Ly - 1, (int)(floor(r[Y])) + Ly / 2),
+            minmax(0, Lz - 1, (int)(floor(r[Z])) + Lz / 2)
         };
-        entry = index[0] + XS * (index[1] + YS * index[2]);
+        entry = index[0] + Lx * (index[1] + Ly * index[2]);
         rho[entry] += 1;
         for (c = 0; c < 3; ++c) u[c][entry] += v[c];
     }
@@ -47,7 +53,7 @@ static void dump0(const Coords *coords, MPI_Comm cart, Particle *pp, int n, int 
     char path[BUFSIZ];
     const char *names[] = { "density", "u", "v", "w" };
 
-    avg(pp, n, nc, rho, u);
+    avg(coords, pp, n, nc, rho, u);
     if (!directory_exists) {
         if (m::is_master(cart))
             UC(os_mkdir(DUMP_BASE "/h5"));
@@ -66,7 +72,7 @@ void dump(const Coords *coords, MPI_Comm cart, Particle *pp, int n) {
     enum {X, Y, Z};
     int nc, sz;
     float *rho, *u[3];
-    nc = XS * YS * ZS;
+    nc = xs(coords) * ys(coords) * zs(coords);
     sz = nc*sizeof(rho[0]);
 
     UC(emalloc(sz, (void**) &rho));
