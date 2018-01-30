0a24dd29a21e69278a366ac3005ff5b5af3ae45e
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-30 14:06:40 +0100

    deps

--- src/u/comm/main/make/dep.mk
+++ src/u/comm/main/make/dep.mk
@@ -1,6 +1,6 @@
 $B/comm/imp.o: $S/comm/imp.h $S/comm/imp/fin.h $S/comm/imp/ini.h $S/comm/imp/main.h $S/comm/imp/type.h $B/conf.h $S/d/api.h $S/frag/imp.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/cc.h $S/utils/error.h $S/utils/imp.h $S/utils/mc.h $S/utils/msg.h
 $B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/utils/error.h $S/utils/imp.h
-$B/frag/imp.o: $B/conf.h $S/frag/imp.h $S/inc/conf.h
+$B/frag/imp.o: $B/conf.h $S/frag/dev.h $S/frag/imp.h $S/inc/conf.h
 $B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
--- src/u/math/linal/make/dep.mk
+++ src/u/math/linal/make/dep.mk
@@ -1,6 +1,6 @@
 $B/comm/imp.o: $S/comm/imp.h $S/comm/imp/fin.h $S/comm/imp/ini.h $S/comm/imp/main.h $S/comm/imp/type.h $B/conf.h $S/d/api.h $S/frag/imp.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/cc.h $S/utils/error.h $S/utils/imp.h $S/utils/mc.h $S/utils/msg.h
 $B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/utils/error.h $S/utils/imp.h
-$B/frag/imp.o: $B/conf.h $S/frag/imp.h $S/inc/conf.h
+$B/frag/imp.o: $B/conf.h $S/frag/dev.h $S/frag/imp.h $S/inc/conf.h
 $B/math/linal/imp.o: $S/math/linal/imp.h $S/utils/error.h
 $B/math/rnd/imp.o: $S/math/rnd/imp.h $S/utils/error.h $S/utils/imp.h
 $B/math/tform/imp.o: $B/conf.h $S/inc/conf.h $S/math/tform/imp.h $S/math/tform/imp/main.h $S/math/tform/imp/type.h $S/math/tform/type.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
