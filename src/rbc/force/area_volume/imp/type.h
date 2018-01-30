9f99020ea3fb0d0a4ed4e72a82e01c702bca334e
Sergey Litvinov <slitvinov@gmail.com>
2018-01-30 15:57:21 +0100

    fix comment

--- src/rbc/force/area_volume/imp/type.h
+++ src/rbc/force/area_volume/imp/type.h
@@ -1,6 +1,5 @@
 struct AreaVolume {
     int nt, nv; /* number of triangles and vertices in one cell */
     int4 *tri; /* triangles on device */
-
-    float *av; /* output: [area0 volume0 area1 volume1, ..]
+    float *av; /* output: [area0 volume0 area1 volume1, ..] */
 };
