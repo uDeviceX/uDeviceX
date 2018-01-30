882b3f41c5f33d8602cafef6f070f9aba69f6dd1
Sergey Litvinov <slitvinov@gmail.com>
2018-01-30 15:55:52 +0100

    av is managed by area_volume

--- src/rbc/force/area_volume/imp/type.h
+++ src/rbc/force/area_volume/imp/type.h
@@ -1,4 +1,6 @@
 struct AreaVolume {
     int nt, nv; /* number of triangles and vertices in one cell */
     int4 *tri; /* triangles on device */
+
+    float *av; /* output: [area0 volume0 area1 volume1, ..]
 };
