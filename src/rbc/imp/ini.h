b9772f7f24d81f96cdea5a39c2f7d514fffa4bd2
Sergey Litvinov <slitvinov@gmail.com>
2018-01-30 12:42:29 +0100

    minor

--- src/rbc/imp/ini.h
+++ src/rbc/imp/ini.h
@@ -1,11 +1,6 @@
 static void ini_common(RbcQuants *q) {
-    q->n = q->nc = 0;
     Dalloc(&q->pp, MAX_CELL_NUM * RBCnv);
     UC(emalloc(MAX_CELL_NUM * RBCnv * sizeof(Particle), (void**) &q->pp_hst));
-
-    q->nt = RBCnt;
-    q->nv = RBCnv;
-
     UC(area_volume_ini(q->nt, /**/ &q->area_volume));
     Dalloc(&q->adj0, q->nv * RBCmd);
     Dalloc(&q->adj1, q->nv * RBCmd);
@@ -17,6 +12,10 @@ static void ini_edg(RbcQuants *q)  { Dalloc(&q->shape.edg,  q->nv * RBCmd); }
 static void ini_anti(RbcQuants *q) { Dalloc(&q->shape.anti, q->nv * RBCmd); }
 
 void rbc_ini(RbcQuants *q) {
+    q->n = q->nc = 0;
+    q->nt = RBCnt;
+    q->nv = RBCnv;
+
     UC(ini_common(q));
     if (rbc_ids)         UC(ini_ids(q));
     if (RBC_STRESS_FREE) UC(ini_edg (q));
