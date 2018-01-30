fcd00939e869d4ed2ebccd2b6928c874b9eb3bd8
Sergey Litvinov <slitvinov@gmail.com>
2018-01-30 12:29:40 +0100

    rm RBCnv in asserts

--- src/rbc/force/dev/stress_free1/shape.h
+++ src/rbc/force/dev/stress_free1/shape.h
@@ -6,7 +6,6 @@ struct Shape0 {
 
 /* extract edge specific shape info */
 static void __device__ edg_shape(Shape shape, int i, /**/ Shape0 *shape0) {
-    assert ( i < RBCnv * RBCmd);
     Edg edg;
     edg = shape.edg[i];
     shape0->a = edg.a;
