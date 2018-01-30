92f68f21261b39d7e376ed0902719fd11af0f526
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-30 09:31:15 +0100

    use L inside exch obj map

--- src/exch/obj/imp/map.h
+++ src/exch/obj/imp/map.h
@@ -1,23 +1,23 @@
-static void add_wrap_to_map(int wid, int n, const Particle *pp, EMap map) {
-    int3 L = make_int3(XS-2, YS-2, ZS-2);    
-    KL(dev::build_map, (k_cnf(n)), (L, wid, n, pp, /**/ map));
+static void add_wrap_to_map(int3 L, int wid, int n, const Particle *pp, EMap map) {
+    int3 L0 = make_int3(L.x-2, L.y-2, L.z-2);    
+    KL(dev::build_map, (k_cnf(n)), (L0, wid, n, pp, /**/ map));
 }
 
-static void fill_map(int nw, const PaWrap *ww, /**/ EMap map) {
+static void fill_map(int3 L, int nw, const PaWrap *ww, /**/ EMap map) {
     const PaWrap *w;
     for (int i = 0; i < nw; ++i) {
         w = ww + i;
-        add_wrap_to_map(i, w->n, w->pp, /**/ map);
+        add_wrap_to_map(L, i, w->n, w->pp, /**/ map);
     }
 }
 
-static void build_map(int nw, const PaWrap *ww, /**/ EMap map) {
+static void build_map(int3 L, int nw, const PaWrap *ww, /**/ EMap map) {
     emap_reini(nw, NFRAGS, /**/ map);
-    fill_map(nw, ww, /**/ map);
+    fill_map(L, nw, ww, /**/ map);
     emap_scan(nw, NFRAGS, /**/ map);
 }
 
 void eobj_build_map(int nw, const PaWrap *ww, /**/ EObjPack *p) {
     if (!nw) return;
-    build_map(nw, ww, /**/ p->map);
+    build_map(p->L, nw, ww, /**/ p->map);
 }
