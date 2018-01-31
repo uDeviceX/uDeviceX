3c0fd0f4fda64e7260b9d34059db8ea2114245f3
Sergey Litvinov <slitvinov@gmail.com>
2018-01-31 22:18:07 +0100

    minor

--- src/rbc/adj/imp.h
+++ src/rbc/adj/imp.h
@@ -3,7 +3,7 @@ struct Adj_v;
 struct AdjMap;
 struct int4;
 
-void adj_ini(int md, int nt, int nv, const int4 *faces, /**/ Adj**);
+void adj_ini(int md, int nt, int nv, const int4 *tt, /**/ Adj**);
 void adj_fin(Adj*);
 int  adj_get_map(int i, const Adj*, /**/ AdjMap *m);
 void adj_get_anti(int md, int nv, const Adj*, /**/ int *anti);
