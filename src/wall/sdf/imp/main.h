9f3fd6f79b213de649543ba916be17117dbf5001
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-31 09:14:56 +0100

    add texture_grid_size function

--- src/wall/sdf/imp/main.h
+++ src/wall/sdf/imp/main.h
@@ -1,3 +1,22 @@
+static int3 texture_grid_size(int3 L, int3 M) {
+    enum {PAD = 16};
+    int3 T;
+    int x, y, z; /* grid extents in real space */
+    int ty, tz;
+    
+    x = L.x + 2 * M.x;
+    y = L.y + 2 * M.y;
+    z = L.z + 2 * M.z;
+    
+    T.x = PAD * PAD;
+    ty = ceiln(y * x, T.x);
+    tz = ceiln(z * x, T.x);
+    T.y = PAD * ceiln(ty, PAD);
+    T.z = PAD * ceiln(tz, PAD);
+
+    return T;
+};
+
 void sdf_ini(Sdf **pq) {
     Sdf *q;
     UC(emalloc(sizeof(Sdf), (void**)&q));
