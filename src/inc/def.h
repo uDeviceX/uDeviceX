c8e4e400deeccd1355a5c4559f8db3027f1ed204
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-31 17:09:49 +0100

    enum instead of defines in inc/def.h

--- src/inc/def.h
+++ src/inc/def.h
@@ -1,36 +1,37 @@
 /* used in forces.h */
 enum {SOLVENT_KIND, SOLID_KIND, WALL_KIND};
 
-#define N_COLOR (4)
 enum {BLUE_COLOR, RED_COLOR, /* solvent colors */
-      SOLID_COLOR, WALL_COLOR};
+      SOLID_COLOR,
+      WALL_COLOR,
+      N_COLOR};
 
-/* maximum particle number per one processor for static allocation */
-#define MAX_PART_NUM (3*XS*YS*ZS*numberdensity)
+enum {
+    /* maximum number of particles per solid */
+    MAX_PSOLID_NUM = 12000,
 
-/* maximum number of particles per solid */
-#define MAX_PSOLID_NUM 12000
+    /* maximum number of solids per node */
+    MAX_SOLIDS = 40,
 
-/* maximum number of solids per node */
-#define MAX_SOLIDS 40
+    /* maximum number of object types (solid, rbc, ...) */
+    MAX_OBJ_TYPES = 2,
 
-/* maximum number of object types (solid, rbc, ...) */
-#define MAX_OBJ_TYPES 2
+    /* maximum number density of particles of the objects */
+    MAX_OBJ_DENSITY = 30,
 
-/* maximum number density of particles of the objects */
-#define MAX_OBJ_DENSITY (30)
+    /* maximum number of red blood cells per node */
+    MAX_CELL_NUM = 1500,
 
-/* maximum number of red blood cells per node */
-#define MAX_CELL_NUM 1500
+    /* maximum texture size in bytes */
+    MAX_TEXO_SIZE = 100000000,
 
-/* maximum texture size in bytes */
-#define MAX_TEXO_SIZE (100000000)
+    /* safety factor for dpd halo interactions */
+    HSAFETY_FACTOR = 10,
 
-/* safety factor for dpd halo interactions */
-#define HSAFETY_FACTOR 10.f
+    /* safety factor for odist fragments */
+    ODSTR_FACTOR = 3,
+};
 
-/* safety factor for odist fragments */
-#define ODSTR_FACTOR (3)
 
 /* write ascii/bin in l/ply.cu */
 #define PLY_WRITE_ASCII
