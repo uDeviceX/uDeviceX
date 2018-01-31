ed312451caecf62922e430dabf29952f412c2160
amlucas <lucas.amoudruz@wanadoo.fr>
2018-01-31 09:54:18 +0100

    add a macro for emalloc

--- src/utils/imp.h
+++ src/utils/imp.h
@@ -1,6 +1,9 @@
 void emalloc(size_t, /**/ void**);
 void efree(void*);
 
+#define EMALLOC(n, ppdata) UC(emalloc(n * sizeof(**(ppdata)), (void**) (ppdata)))
+#define EFREE(pdata) UC(efree(pdata))
+
 void efopen(const char *fname, const char *mode, /**/ FILE**);
 void efclose(FILE*);
 void efread(void *ptr, size_t size, size_t nmemb, FILE*);
