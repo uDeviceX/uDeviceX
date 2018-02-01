0b93d565d1a0f6cc5c95d6a4f8bb0354708bcf74
Sergey Litvinov <slitvinov@gmail.com>
2018-02-01 16:27:34 +0100

    initil version of rbc/shape api plan

--- src/rbc/shape/imp.h
+++ src/rbc/shape/imp.h
@@ -1,1 +1,11 @@
-void f();
+struct RbcShape;
+struct OffRead;
+
+void rbc_shape_ini(Adj*, float *rr /**/ RbcShape**);
+void rbc_shape_fin(/**/ RbcShape*);
+
+void rbc_shape_edg (RbcShape*, /**/ float*);
+void rbc_shape_area(RbcShape*, /**/ float*);
+
+void rbc_shape_total_area(RbcShape*, /**/ float*);
+void rbc_shape_total_volume(RbcShape*, /**/ float*);
