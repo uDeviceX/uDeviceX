1c5b9e4b00b109a7439f72fc79dafbe9a305b3b5
Sergey Litvinov <slitvinov@gmail.com>
2018-02-01 20:50:30 +0100

    minor

--- src/rbc/shape/imp/main.h
+++ src/rbc/shape/imp/main.h
@@ -2,7 +2,7 @@ void rbc_shape_ini(Adj *adj, float *rr, /**/ RbcShape **pq) {
     int n;
     RbcShape *q;
     n = adj_get_max(adj);
-    
+
     EMALLOC(1, &q);
     EMALLOC(n, &q->edg);
     EMALLOC(n, &q->area);
@@ -14,7 +14,7 @@ void rbc_shape_ini(Adj *adj, float *rr, /**/ RbcShape **pq) {
 void rbc_shape_fin(RbcShape *q) {
     EFREE(q->edg);
     EFREE(q->area);
-    EFREE(q->anti);    
+    EFREE(q->anti);
     EFREE(q);
 }
 
