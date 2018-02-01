44f638ee1f48fd1eaa3dc541d55e52964fa373e1
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-01 10:52:19 +0100

    compile fix

--- src/coords/conf.cpp
+++ src/coords/conf.cpp
@@ -7,8 +7,10 @@
 #include "utils/error.h"
 #include "parser/imp.h"
 
-void coords_ini(MPI_Comm cart, const Config *cfg, Coords **c) {
+#include "ini.h"
+
+void coords_ini_conf(MPI_Comm cart, const Config *cfg, Coords **c) {
     int3 L;
     UC(conf_lookup_int3(cfg, "glb/L", &L));
-    UC(coords_ini(cart, L,x, L.y, L.z, c));
+    UC(coords_ini(cart, L.x, L.y, L.z, c));
 }
