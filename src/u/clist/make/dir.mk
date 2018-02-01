45c2b290daa817a7b5986e66f13e4951d8e8a4a8
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-01 11:13:06 +0100

    clist is free from XS

--- src/u/clist/make/dir.mk
+++ src/u/clist/make/dir.mk
@@ -1,7 +1,9 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
     d $B/algo/scan && \
     d $B/clist     && \
+    d $B/coords    && \
     d $B/d         && \
     d $B/mpi       && \
+    d $B/parser    && \
     d $B/u/clist   && \
     d $B/utils    
--- src/u/clist/make/rule.mk
+++ src/u/clist/make/rule.mk
@@ -1,9 +1,12 @@
 $B/algo/scan/imp.o: $S/algo/scan/imp.cu; $N -I$S/. -I$S/algo/scan
 $B/clist/imp.o: $S/clist/imp.cu; $N -I$S/. -I$S/clist
+$B/coords/conf.o: $S/coords/conf.cpp; $X -I$S/. -I$S/coords
+$B/coords/imp.o: $S/coords/imp.cpp; $X -I$S/. -I$S/coords
 $B/d/api.o: $S/d/api.cu; $N -I$S/. -I$S/d
 $B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/. -I$S/mpi
 $B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/. -I$S/mpi
 $B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/. -I$S/mpi
+$B/parser/imp.o: $S/parser/imp.cpp; $X -I$S/. -I$S/parser
 $B/u/clist/main.o: $S/u/clist/main.cu; $N -I$S/. -I$S/u/clist
 $B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/. -I$S/utils
 $B/utils/error.o: $S/utils/error.cpp; $X -I$S/. -I$S/utils
