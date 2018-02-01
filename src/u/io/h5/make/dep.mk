ee756126161877dda5c07409f47485868b7e5fab
Sergey Litvinov <slitvinov@gmail.com>
2018-02-01 18:29:51 +0100

    deps

--- src/u/io/h5/make/dep.mk
+++ src/u/io/h5/make/dep.mk
@@ -1,17 +1,17 @@
-$B/coords/conf.o: $S/coords/ini.h $S/parser/imp.h $S/utils/error.h $S/utils/imp.h
-$B/coords/imp.o: $B/conf.h $S/coords/imp.h $S/coords/imp/main.h $S/coords/imp/type.h $S/coords/ini.h $S/coords/type.h $S/inc/conf.h $S/mpi/wrapper.h $S/utils/error.h $S/utils/imp.h $S/utils/mc.h
-$B/d/api.o: $B/conf.h $S/d/api.h $S/d/common.h $S/d/cpu/imp.h $S/d/cuda/imp.h $S/inc/conf.h $S/utils/error.h $S/utils/imp.h
-$B/io/field/h5/imp.o: $S/coords/imp.h $S/io/field/h5/imp.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/error.h
-$B/io/field/imp.o: $B/conf.h $S/coords/imp.h $S/inc/conf.h $S/inc/type.h $S/io/field/h5/imp.h $S/io/field/imp.h $S/io/field/imp/dump.h $S/io/field/imp/scalar.h $S/io/field/xmf/imp.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/error.h $S/utils/imp.h $S/utils/mc.h $S/utils/os.h
-$B/io/field/xmf/imp.o: $S/io/field/xmf/imp.h $S/mpi/glb.h $S/utils/error.h $S/utils/imp.h
-$B/mpi/glb.o: $B/conf.h $S/inc/conf.h $S/mpi/glb.h $S/mpi/wrapper.h $S/utils/mc.h
+$B/coords/conf.o: $S/utils/imp.h $S/coords/ini.h $S/utils/error.h $S/parser/imp.h
+$B/coords/imp.o: $S/utils/imp.h $S/inc/conf.h $S/coords/ini.h $S/utils/error.h $S/mpi/wrapper.h $S/coords/imp.h $S/utils/mc.h $B/conf.h $S/coords/imp/main.h $S/coords/imp/type.h $S/coords/type.h
+$B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/d/cuda/imp.h
+$B/io/field/h5/imp.o: $S/utils/error.h $S/mpi/wrapper.h $S/io/field/h5/imp.h $S/mpi/glb.h $S/coords/imp.h
+$B/io/field/imp.o: $S/io/field/imp/scalar.h $S/utils/os.h $S/utils/imp.h $S/inc/conf.h $S/io/field/xmf/imp.h $S/utils/error.h $S/inc/type.h $S/mpi/wrapper.h $S/io/field/imp.h $S/io/field/h5/imp.h $S/io/field/imp/dump.h $S/utils/mc.h $B/conf.h $S/coords/imp.h $S/mpi/glb.h
+$B/io/field/xmf/imp.o: $S/utils/imp.h $S/utils/error.h $S/io/field/xmf/imp.h $S/mpi/glb.h
+$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/parser/imp.o: $S/parser/imp.h $S/utils/error.h $S/utils/imp.h $S/utils/msg.h
-$B/u/io/h5/main.o: $B/conf.h $S/coords/imp.h $S/coords/ini.h $S/inc/conf.h $S/io/field/h5/imp.h $S/io/field/xmf/imp.h $S/mpi/glb.h $S/mpi/wrapper.h $S/parser/imp.h $S/utils/error.h $S/utils/imp.h $S/utils/mc.h $S/utils/msg.h
-$B/utils/cc.o: $B/conf.h $S/d/api.h $S/inc/conf.h $S/utils/cc/common.h $S/utils/error.h
-$B/utils/error.o: $S/utils/error.h $S/utils/msg.h
+$B/parser/imp.o: $S/utils/imp.h $S/utils/error.h $S/parser/imp.h $S/utils/msg.h
+$B/u/io/h5/main.o: $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/io/field/h5/imp.h $S/mpi/wrapper.h $S/coords/ini.h $S/utils/mc.h $B/conf.h $S/parser/imp.h $S/io/field/xmf/imp.h $S/mpi/glb.h $S/utils/msg.h $S/coords/imp.h
+$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
+$B/utils/error.o: $S/utils/msg.h $S/utils/error.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
-$B/utils/mc.o: $B/conf.h $S/inc/conf.h $S/mpi/wrapper.h $S/utils/error.h $S/utils/mc.h
+$B/utils/mc.o: $S/inc/conf.h $S/utils/error.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
 $B/utils/msg.o: $S/utils/msg.h
-$B/utils/os.o: $S/utils/error.h $S/utils/msg.h $S/utils/os.h
+$B/utils/os.o: $S/utils/os.h $S/utils/error.h $S/utils/msg.h
