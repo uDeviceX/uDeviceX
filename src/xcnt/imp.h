f8f926c4227c1ba8e0ebf5879d5e33ef68453d0f
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-02 16:20:53 +0100

    adapt interface

--- src/xcnt/imp.h
+++ src/xcnt/imp.h
@@ -1,5 +1,6 @@
 struct Contact;
 struct int3;
+struct PairParams;
 
 // tag::mem[]
 void cnt_ini(int maxp, int rank, int3 L, /**/ Contact **c);
@@ -8,7 +9,7 @@ void cnt_fin(Contact *c);
 
 // tag::int[]
 void cnt_build_cells(int nw, const PaWrap *pw, /**/ Contact *c); // <1>
-void cnt_bulk(const Contact *c, int nw, PaWrap *pw, FoWrap *fw); // <2>
-void cnt_halo(const Contact *c, int nw, PaWrap *pw, FoWrap *fw, Pap26 PP, Fop26 FF, int counts[26]); // <3>
+void cnt_bulk(const PairParams*, const Contact *c, int nw, PaWrap *pw, FoWrap *fw); // <2>
+void cnt_halo(const PairParams*, const Contact *c, int nw, PaWrap *pw, FoWrap *fw, Pap26 PP, Fop26 FF, int counts[26]); // <3>
 // end::int[]
 
