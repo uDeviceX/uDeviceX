8b70461993ed23de980361036ce60199bf583b7f
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-02 14:20:12 +0100

    adapt interface

--- src/fluforces/xbulk/imp.h
+++ src/fluforces/xbulk/imp.h
@@ -1,9 +1,12 @@
+struct PairParams;
+struct Force;
 struct float4;
 struct int3;
+
 struct BCloud {
     const float4 *pp;
     const int *cc;
 };
 
-void flocal(int3 L, int n, BCloud cloud, const int *start, RNDunif *rnd, /**/ Force *ff);
-
+void flocal       (const PairParams*, int3 L, int n, BCloud cloud, const int *start, RNDunif *rnd, /**/ Force *ff);
+void flocal_colors(const PairParams*, int3 L, int n, BCloud cloud, const int *start, RNDunif *rnd, /**/ Force *ff);
