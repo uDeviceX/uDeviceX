092354c160040a35d69c12fe2d4b5a2cba2c9ee0
Sergey Litvinov <slitvinov@gmail.com>
2018-02-02 18:05:22 +0100

    compute volume in rbc/shape

--- src/rbc/shape/imp/main.h
+++ src/rbc/shape/imp/main.h
@@ -36,6 +36,31 @@ static void compute_area(Adj *adj, const float *rr, /**/ float *o) {
     }
 }
 
+static void compute_total_volume(Adj *adj, const float *rr, /**/ float *pV) {
+    enum {X, Y, Z};
+    int i, i0, i1, i2, valid, n;
+    const float *r0, *r1, *r2;
+    float x0, y0, z0, x1, y1, z1, x2, y2, z2;
+    AdjMap m;
+    double V;
+    n = adj_get_max(adj);
+    for (i = 0; i < n; i++) {
+        valid = adj_get_map(i, adj, /**/ &m);
+        if (!valid) continue;
+        i0 = m.i0; i1 = m.i1; i2 = m.i2;
+        r0 = &rr[3*i0]; r1 = &rr[3*i1]; r2 = &rr[3*i2];
+        x0 = r0[X]; y0 = r0[Y]; z0 = r0[Z];
+        x1 = r1[X]; y1 = r1[Y]; z1 = r1[Z];
+        x2 = r2[X]; y2 = r2[Y]; z2 = r2[Z];
+        V += (x0*y1-x1*y0)*z2+x2*(y0*z1-y1*z0)+y2*(x1*z0-x0*z1);
+    }
+
+    V /= 6; /* from the formula */
+    V /= 3; /* every triangle was counted three times */
+    msg_print("V: %g", V);
+    *pV = V;
+}
+
 static void compute_total_area(Adj *adj, const float* area, /**/ float *pA) {
     int n, i, valid;
     AdjMap m;
@@ -48,7 +73,7 @@ static void compute_total_area(Adj *adj, const float* area, /**/ float *pA) {
         if (!valid) continue;
         A += area[i];
     }
-    A /= 3;
+    A /= 3; /* every triangle was counted three times */
 
     *pA = A;
 }
@@ -64,6 +89,8 @@ void rbc_shape_ini(Adj *adj, const float *rr, /**/ RbcShape **pq) {
     compute_edg(adj, rr, /**/ q->edg);
     compute_area(adj, rr, /**/ q->area);
     compute_total_area(adj, q->area, /**/ &q->A);
+    compute_total_volume(adj, rr, /**/ &q->V);
+    
     msg_print("A: %g", q->A);
 
     *pq = q;
