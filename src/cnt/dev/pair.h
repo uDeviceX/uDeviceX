d143d540b0d19a511ecc04198595832880c12070
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-02 16:21:35 +0100

    rm old cnt

--- src/cnt/dev/pair.h
+++
@@ -1,6 +0,0 @@
-static __device__ void pair(forces::Pa a, forces::Pa b, float rnd, /**/
-                            float *fx, float *fy, float *fz) {
-    forces::Fo f;
-    forces::gen(a, b, rnd, /**/ &f);
-    *fx = f.x; *fy = f.y; *fz = f.z;
-}
--- src/cnt/imp/type.h
+++
@@ -1,6 +0,0 @@
-struct Contact {
-    Clist cells;    /* cell lists              */
-    ClistMap *cmap; /* helper to build clists  */
-    RNDunif *rgen;  /* random number generator */
-    int3 L;         /* dimensions of subdomain */
-};
