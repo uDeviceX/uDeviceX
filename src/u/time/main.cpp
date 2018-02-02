7ff690b793fe0a3ceea60a781637a234e43b8203
Sergey Litvinov <slitvinov@gmail.com>
2018-02-02 23:21:36 +0100

    print less

--- src/u/time/main.cpp
+++ src/u/time/main.cpp
@@ -10,11 +10,12 @@ int main(int argc, char **argv) {
     m::ini(&argc, &argv);
     msg_ini(m::rank);
     Time *time;
-    ts = 0; dt = 0.01; dump = 0.78;
+    ts = 0; dt = 0.01; dump = 0.25;
     time_ini(ts, /**/ &time);
     for (i = 0; i < 1000; i++) {
         time_step(time, dt);
-        printf("%g %d\n", time_current(time), time_cross(time, dump));
+        if (time_cross(time, dump))
+            printf("%g\n", time_current(time));
     }
     time_fin(time);
     m::fin();
