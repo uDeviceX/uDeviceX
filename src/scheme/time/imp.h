ee351297367247d641bc8e9ba0fe7f679451ca0f
Sergey Litvinov <slitvinov@gmail.com>
2018-02-02 23:30:55 +0100

    minor

--- src/scheme/time/imp.h
+++ src/scheme/time/imp.h
@@ -3,7 +3,7 @@ void time_ini(float start, /**/ Time**);
 void time_fin(Time*);
 
 void time_step(Time*, float dt);
-int time_cross(Time*, float interval); /* do we just cross
+int time_cross(Time*, float interval); /* did we just cross
                                           `n*interval', where `n' is
                                           an integer? */
 float time_current(Time*);
