566754c8596a3c90f800d19a642fc640392a1adf
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-02 17:47:34 +0100

    xpair/pair

--- src/u/xpair/make/dir.mk
+++
@@ -1,7 +0,0 @@
-D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/d       && \
-    d $B/mpi     && \
-    d $B/pair    && \
-    d $B/parser  && \
-    d $B/u/xpair && \
-    d $B/utils  
