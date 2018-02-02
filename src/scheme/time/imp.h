0a603ceff0fe0c0ec0b057ff6385700d5fc25a5e
Sergey Litvinov <slitvinov@gmail.com>
2018-02-02 23:22:51 +0100

    comments

--- src/scheme/time/imp.h
+++ src/scheme/time/imp.h
@@ -1,10 +1,11 @@
 struct Time;
 struct Force;
 
-void time_ini(float ts, /**/ Time**);
+void time_ini(float start, /**/ Time**);
 void time_fin(Time*);
 
 void time_step(Time*, float dt);
-int time_cross(Time*, float t); /* do we just cross `n*t', where `n'
-                                   is an integer? */
+int time_cross(Time*, float interval); /* do we just cross
+                                          `n*interval', where `n' is an
+                                          integer? */
 float time_current(Time*);
