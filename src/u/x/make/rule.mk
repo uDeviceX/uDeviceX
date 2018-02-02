fce00772a50ba9cc485086aa91c8e9ea6495c015
Sergey Litvinov <slitvinov@gmail.com>
2018-02-02 22:26:07 +0100

    deps

--- src/u/x/make/rule.mk
+++ src/u/x/make/rule.mk
@@ -86,7 +86,6 @@ $B/scheme/move/params/conf.o: $S/scheme/move/params/conf.cpp; $X -I$S/scheme/mov
 $B/scheme/move/params/imp.o: $S/scheme/move/params/imp.cpp; $X -I$S/scheme/move/params -I$S/.
 $B/scheme/restrain/conf.o: $S/scheme/restrain/conf.cpp; $X -I$S/scheme/restrain -I$S/.
 $B/scheme/restrain/imp.o: $S/scheme/restrain/imp.cu; $N -I$S/scheme/restrain -I$S/.
-$B/scheme/time/.#imp.o: $S/scheme/time/.#imp.cpp; $X -I$S/scheme/time -I$S/.
 $B/scheme/time/imp.o: $S/scheme/time/imp.cpp; $X -I$S/scheme/time -I$S/.
 $B/sim/imp.o: $S/sim/imp.cpp; $X -I$S/sim -I$S/.
 $B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
