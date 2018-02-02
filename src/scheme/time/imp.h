889c6002c2bf8cb1d3e441153857b4836d428eef
Sergey Litvinov <slitvinov@gmail.com>
2018-02-02 23:23:17 +0100

    do not need Force

--- src/scheme/time/imp.h
+++ src/scheme/time/imp.h
@@ -1,11 +1,9 @@
 struct Time;
-struct Force;
-
 void time_ini(float start, /**/ Time**);
 void time_fin(Time*);
 
 void time_step(Time*, float dt);
 int time_cross(Time*, float interval); /* do we just cross
-                                          `n*interval', where `n' is an
-                                          integer? */
+                                          `n*interval', where `n' is
+                                          an integer? */
 float time_current(Time*);
