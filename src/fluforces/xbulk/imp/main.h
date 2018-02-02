33fadb4b1813ae10130de86128d49539c5c20664
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-02 14:18:03 +0100

    add wrappers for color or normal dpd bulk

--- src/fluforces/xbulk/imp/main.h
+++ src/fluforces/xbulk/imp/main.h
@@ -1,15 +1,5 @@
 enum {THR=128};
 
-template<typename Par>
-void oflocal_common(Par params, int3 L, int n, BCloud cloud, const int *start, RNDunif *rnd, /**/ Force *ff) {
-    float seed;
-    if (n <= 0) return;
-    seed = rnd_get(rnd);
-    KL(flocaldev::apply_unroll,
-       (ceiln((n), THR), THR),
-       (params, L, n, cloud, start, seed, /**/ ff));
-}
-
 static void tbcloud_ini(int n, BCloud cloud, TBCloud *tc) {
     setup0((float4*) cloud.pp, 2*n, &tc->pp);
     if (multi_solvent) setup0(   (int*) cloud.cc,   n, &tc->cc);    
@@ -20,7 +10,6 @@ static void tbcloud_fin(TBCloud *tc) {
     if (multi_solvent) destroy(&tc->cc);
 }
 
-// try with textures
 template<typename Par>
 void flocal_common(Par params, int3 L, int n, BCloud cloud, const int *start, RNDunif *rnd, /**/ Force *ff) {    
     float seed;
@@ -37,3 +26,14 @@ void flocal_common(Par params, int3 L, int n, BCloud cloud, const int *start, RN
     tbcloud_fin(&tc);
 }
 
+void flocal(const PairParams *params, int3 L, int n, BCloud cloud, const int *start, RNDunif *rnd, /**/ Force *ff) {
+    PairDPD pv;
+    pair_get_view_dpd(params, &pv);
+    flocal_common(pv, L, n, cloud, start, rnd, /**/ ff);
+}
+
+void flocal_colors(const PairParams *params, int3 L, int n, BCloud cloud, const int *start, RNDunif *rnd, /**/ Force *ff) {
+    PairDPDC pv;
+    pair_get_view_dpd_color(params, &pv);
+    flocal_common(pv, L, n, cloud, start, rnd, /**/ ff);
+}
