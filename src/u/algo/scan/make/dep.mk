b1a7914f4547c005f169d1db7714d872eff151b5
Sergey Litvinov <slitvinov@gmail.com>
2018-02-03 12:27:16 +0100

    deps

--- src/u/algo/scan/make/dep.mk
+++ src/u/algo/scan/make/dep.mk
@@ -3,7 +3,7 @@ $B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/co
 $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h $S/mpi/glb.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
-$B/u/scan/main.o: $S/inc/conf.h $S/algo/scan/imp.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/mpi/glb.h $S/utils/msg.h
+$B/u/algo/scan/main.o: $S/inc/conf.h $S/algo/scan/imp.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/mpi/glb.h $S/utils/msg.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
 $B/utils/error.o: $S/utils/msg.h $S/utils/error.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
--- src/u/algo/scan/make/dir.mk
+++ src/u/algo/scan/make/dir.mk
@@ -1,6 +1,6 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/algo/scan && \
-    d $B/d         && \
-    d $B/mpi       && \
-    d $B/u/scan    && \
-    d $B/utils    
+    d $B/algo/scan   && \
+    d $B/d           && \
+    d $B/mpi         && \
+    d $B/u/algo/scan && \
+    d $B/utils      
