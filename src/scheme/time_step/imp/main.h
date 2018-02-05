78ddbeb84473599d148d183b6f49e3ec979a673f
Sergey Litvinov <slitvinov@gmail.com>
2018-02-05 12:58:08 +0100

    wip: time_step

--- src/scheme/time_step/imp/main.h
+++ src/scheme/time_step/imp/main.h
@@ -1,6 +1,15 @@
 void time_step_accel_ini(/**/ TimeStepAccel **pq) {
     TimeStepAccel *q;
     EMALLOC(1, &q);
+    q->k = 0;
     *pq = q;
 }
 void time_step_accel_fin(TimeStepAccel *q) { EFREE(q); }
+void time_step_accel_push(TimeStepAccel *q, float m, int n, Force *ff) {
+    int k;
+    k = q->k;
+    if (k + 1 == MAX_ACCEL_NUM)
+        ERR("k + 1 = %d < %d = MAX_ACCEL_NUM", k + 1, MAX_ACCEL_NUM);
+    q->mm[k] = m; q->nn[k] = n; q->fff[k] = ff;
+    q->k = k + 1;
+}
--- src/scheme/time_step/imp/type.h
+++ src/scheme/time_step/imp/type.h
@@ -3,6 +3,7 @@ struct TimeStepAccel {
     float  mm[MAX_ACCEL_NUM];
     int    nn[MAX_ACCEL_NUM];
     Force* fff[MAX_ACCEL_NUM];
+    int k;
 };
 
 struct TimeStep {
