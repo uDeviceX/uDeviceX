faddb20d168366c500f4b1df9b4492ffe7643511
Sergey Litvinov <slitvinov@gmail.com>
2018-02-06 17:37:28 +0100

    mkdir h5

--- src/io/field/imp/scalar.h
+++ src/io/field/imp/scalar.h
@@ -1,6 +1,7 @@
 void scalar(const Coords *coords, MPI_Comm cart, float *D, const char *name) {
     char path[BUFSIZ];
     sprintf(path, DUMP_BASE "/h5/%s.h5", name);
+    if (m::is_master(cart)) UC(os_mkdir(DUMP_BASE "/h5"));
     UC(h5_write(coords, cart, path, &D, &name, 1));
     if (m::is_master(cart)) xmf_write(path, &name, 1, xs(coords), ys(coords), zs(coords));
 }
