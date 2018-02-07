3f090115a24be917287a3289c84ee77445f9a904
Sergey Litvinov <slitvinov@gmail.com>
2018-02-07 12:29:49 +0100

    include parser in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@
 #include "mpi/glb.h"
 #include "mpi/wrapper.h"
 #include "scheme/time/imp.h"
+#include "parser/imp.h"
 #include "sim/imp.h"
 
 int main(int argc, char **argv) {
     Sim *sim;
     Time *time;
+    Config *cfg;
     float tend;
 
     m::ini(&argc, &argv);
@@ -23,6 +25,7 @@ int main(int argc, char **argv) {
     if (RESTART) sim_strt(sim, time, tend);
     else         sim_gen(sim, time, tend);
     sim_fin(sim, time);
+
     m::fin();
     msg_print("end");
 }
