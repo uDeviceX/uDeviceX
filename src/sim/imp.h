f645f727254f4acca6d8e149e97fd208ff54d75a
Sergey Litvinov <slitvinov@gmail.com>
2018-02-07 12:50:38 +0100

    add TimeSeg

--- src/sim/imp.h
+++ src/sim/imp.h
@@ -1,7 +1,12 @@
 struct Sim;
 struct Time;
 struct Config;
+struct TimeSeg; /* time segments: tend, dumps, restarts, ... */
+
 void sim_ini(Config*, MPI_Comm, Time*, /**/ Sim**, float *tend);
 void sim_gen(Sim*, Config*, Time*, float tend);
 void sim_strt(Sim*, Config*, Time*, float tend);
 void sim_fin(Sim*, Time*);
+
+void time_seg_ini(Config*, /**/ TimeSeg**);
+void time_seg_fin(TimeSeg*);
