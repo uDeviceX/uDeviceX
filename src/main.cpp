e1ebae9901e5915873b5d1bcb2b1fd577249a454
Sergey Litvinov <slitvinov@gmail.com>
2018-02-07 12:31:33 +0100

    read cfg in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "inc/conf.h"
 
 #include "utils/msg.h"
+#include "utils/error.h"
 #include "mpi/glb.h"
 #include "mpi/wrapper.h"
 #include "scheme/time/imp.h"
@@ -20,12 +21,14 @@ int main(int argc, char **argv) {
     m::ini(&argc, &argv);
     msg_ini(m::rank);
     msg_print("mpi rank/size: %d/%d", m::rank, m::size);
+    UC(conf_ini(&cfg));
 
     sim_ini(argc, argv, m::cart, /**/ &sim, &time, &tend);
     if (RESTART) sim_strt(sim, time, tend);
     else         sim_gen(sim, time, tend);
     sim_fin(sim, time);
 
+    UC(conf_fin(cfg));
     m::fin();
     msg_print("end");
 }
