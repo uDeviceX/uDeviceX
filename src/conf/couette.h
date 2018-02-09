35500e50f497a448261103d687b37e471e76995d
Sergey Litvinov <slitvinov@gmail.com>
2018-02-09 19:53:22 +0100

    clean up conf/*.h

--- src/conf/couette.h
+++ src/conf/couette.h
@@ -1,28 +1,28 @@
-#define numberdensity    10
-#define kBT              1e-7
-#define flu_mass         1.0
-#define rbc_mass         0.5
-#define solid_mass       0.5
+#define   numberdensity              10
+#define             kBT            1e-7
+#define        flu_mass             1.0
+#define        rbc_mass             0.5
+#define      solid_mass             0.5
 
-#define adpd_b         2.667
-#define adpd_r          2.667
-#define adpd_br         2.667
+#define          adpd_b           2.667
+#define          adpd_r           2.667
+#define         adpd_br           2.667
 
-#define gdpd_b    8.0
-#define gdpd_r     8.0
-#define gdpd_br    8.0
+#define          gdpd_b             8.0
+#define          gdpd_r             8.0
+#define         gdpd_br             8.0
 
-#define global_ids       (false)
-#define multi_solvent    (false)
+#define      global_ids           false
+#define   multi_solvent           false
 
-#define ljsigma          0.3
-#define ljepsilon        0.444
+#define         ljsigma             0.3
+#define       ljepsilon           0.444
 
-#define fsiforces        (true)
-#define contactforces    (false)
-#define strt_dumps       (false)
-#define strt_freq        (5000)
-#define pushsolid        (false)
-#define pushrbc          (false)
-#define walls            (false)
-#define RBCnv            (498)
+#define       fsiforces            true
+#define   contactforces           false
+#define      strt_dumps           false
+#define       strt_freq            5000
+#define       pushsolid           false
+#define         pushrbc           false
+#define           walls           false
+#define           RBCnv             498
--- src/conf/lina.h
+++ src/conf/lina.h
@@ -1,29 +1,29 @@
-#define numberdensity      10
-#define flu_mass          1.0
-#define rbc_mass         0.25
-#define solid_mass        1.0
+#define   numberdensity              10
+#define        flu_mass             1.0
+#define        rbc_mass            0.25
+#define      solid_mass             1.0
 
-#define adpd_b     2.66667
-#define adpd_br    2.66667
-#define adpd_r     2.66667
-#define gdpd_b       11.5
-#define gdpd_br      8.25
-#define gdpd_r          5
-#define kBT    0.00444302
+#define          adpd_b         2.66667
+#define         adpd_br         2.66667
+#define          adpd_r         2.66667
+#define          gdpd_b            11.5
+#define         gdpd_br            8.25
+#define          gdpd_r               5
+#define             kBT      0.00444302
 
-#define multi_solvent           true
-#define color_freq              500
-#define contactforces           false
-#define ljsigma                 0.3
-#define ljepsilon               0.44
-#define walls                   false
+#define   multi_solvent            true
+#define      color_freq             500
+#define   contactforces           false
+#define         ljsigma             0.3
+#define       ljepsilon            0.44
+#define           walls           false
 
-#define dump_all_fields         true
-#define strt_dumps              true
-#define strt_freq               2000000
+#define dump_all_fields            true
+#define      strt_dumps            true
+#define       strt_freq         2000000
 
-#define           RBC_RND       true
-#define         fsiforces       true
+#define         RBC_RND            true
+#define       fsiforces            true
 
-#define              rbcs       true
-#define             RBCnv       642
+#define            rbcs            true
+#define           RBCnv             642
