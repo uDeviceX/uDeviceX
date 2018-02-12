49ea2c8584953427a472a0fd08e965fbbc5f4f50
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 19:21:48 +0100

    upd a test ref

--- data/hdr/default.h
+++ data/hdr/default.h
@@ -1,15 +1,8 @@
-#define numberdensity    10
-#define kBT              4.44302e-8
-#define flu_mass         1.0
-#define rbc_mass         0.5
-#define solid_mass       0.5
+#define   numberdensity              10
+#define             kBT       0.0444302
+#define        flu_mass             1.0
+#define        rbc_mass             0.5
+#define      solid_mass             0.5
 
-#define global_ids       (false)
-#define multi_solvent    (false)
-
-#define strt_dumps       (false)
-#define strt_freq        (5000)
-#define pushsolid        (false)
-#define pushrbc          (false)
-#define walls            (false)
-#define RBCnv            (498)
+#define      strt_dumps           false
+#define       strt_freq            5000
--- data/hdr/lina.h
+++ data/hdr/lina.h
@@ -1,19 +1,23 @@
-#define numberdensity      10
-#define flu_mass          1.0
-#define rbc_mass         0.25
-#define solid_mass        1.0
+#define   numberdensity              10
+#define             kBT      0.00444302
+#define        flu_mass             1.0
+#define        rbc_mass            0.25
+#define      solid_mass             1.0
 
-#define kBT    0.00444302
+// #define          gdpd_b            11.5
+// #define          gdpd_r               5
+// #define         gdpd_br            8.25
 
-#define multi_solvent           true
-#define color_freq              500
-#define walls                   false
+#define      global_ids           false
+#define   multi_solvent            true
 
-#define dump_all_fields         true
-#define strt_dumps              true
-#define strt_freq               2000000
+#define      color_freq             500
+#define dump_all_fields            true
+#define         RBC_RND            true
+#define            rbcs            true
 
-#define           RBC_RND       true
-
-#define              rbcs       true
-#define             RBCnv       642
+#define      strt_dumps            true
+#define       strt_freq         2000000
+#define       pushsolid           false
+#define         pushrbc           false
+#define           walls           false
