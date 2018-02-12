720de2c0634f6537f13ebd6ab67346516e7b4034
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 16:17:51 +0100

    minor

--- src/rbc/force/imp.h
+++ src/rbc/force/imp.h
@@ -3,5 +3,5 @@ struct RbcForce;
 struct Force;
 void rbc_force_ini(const RbcQuants*, RbcForce**);
 void rbc_force_fin(RbcForce*);
-void rbc_force_apply(RbcForce*, const RbcParams *p, float dt, const RbcQuants*, /**/ Force *ff);
+void rbc_force_apply(RbcForce*, const RbcParams*, float dt, const RbcQuants*, /**/ Force*);
 void rbc_force_stat(/**/ float *pArea, float *pVolume);
