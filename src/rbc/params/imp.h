ab2f0faa7f4994c74894decbd6461e0c16b583d1
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 17:25:20 +0100

    add rbc_params_set_tot_area_volume

--- src/rbc/params/imp.h
+++ src/rbc/params/imp.h
@@ -5,6 +5,7 @@ struct RbcParams_v {
     float kb, phi;
     float ks, x0, mpow;
     float ka, kd, kv;
+    float totArea, totVolume;
 };
 
 void rbc_params_ini(RbcParams **);
@@ -14,6 +15,7 @@ void rbc_params_set_fluct(float gammaC, float gammaT, float kBT0, RbcParams *);
 void rbc_params_set_bending(float kb, float phi, RbcParams *);
 void rbc_params_set_spring(float ks, float x0, float mpow , RbcParams *);
 void rbc_params_set_area_volume(float ka, float kd, float kv, RbcParams *);
+void rbc_params_set_tot_area_volume(float totArea, float totVolume, RbcParams *);
 
 void rbc_params_set_conf(const Config *c, RbcParams *);
 
