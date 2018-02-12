2094056d0450b069de106fab0b6e8475bd09473b
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-12 13:36:13 +0100

    update data/hdr; use default in io tests

--- data/hdr/default.h
+++ data/hdr/default.h
@@ -4,16 +4,6 @@
 #define rbc_mass         0.5
 #define solid_mass       0.5
 
-#define adpd_b         2.6666667
-#define adpd_r          2.6666667
-#define adpd_br         2.6666667
-#define gdpd_b    8.0
-#define gdpd_r     8.0
-#define gdpd_br    8.0
-
-#define ljsigma          0.3
-#define ljepsilon        0.44
-
 #define global_ids       (false)
 #define multi_solvent    (false)
 
--- data/hdr/lina.h
+++ data/hdr/lina.h
@@ -3,19 +3,11 @@
 #define rbc_mass         0.25
 #define solid_mass        1.0
 
-#define adpd_b     2.66667
-#define adpd_br    2.66667
-#define adpd_r     2.66667
-#define gdpd_b       11.5
-#define gdpd_br      8.25
-#define gdpd_r          5
 #define kBT    0.00444302
 
 #define multi_solvent           true
 #define color_freq              500
 #define contactforces           false
-#define ljsigma                 0.3
-#define ljepsilon               0.44
 #define walls                   false
 
 #define dump_all_fields         true
