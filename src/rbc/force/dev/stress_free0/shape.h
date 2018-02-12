53d794adcd826542105142a717835c04057601c6
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 17:15:11 +0100

    pass totVolume to host

--- src/rbc/force/dev/stress_free0/shape.h
+++ src/rbc/force/dev/stress_free0/shape.h
@@ -1,8 +1,9 @@
-struct Shape0 { float a0, A0, totArea; };
+struct Shape0 { float a0, A0, totArea, totVolume; };
 
 /* extract edge specific shape info */
 static void __device__ edg_shape(Shape shape, int, /**/ Shape0 *shape0) {
     shape0->a0      = shape.a0;
     shape0->A0      = shape.A0;
     shape0->totArea = shape.totArea;
+    shape0->totVolume = shape.totVolume;
 }
