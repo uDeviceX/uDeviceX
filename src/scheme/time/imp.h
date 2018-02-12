0120a5d0023567c1c61c8458730185a9bdda9426
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 15:40:12 +0100

    rm comments from a header

--- src/scheme/time/imp.h
+++ src/scheme/time/imp.h
@@ -4,11 +4,9 @@ struct Time;
 void time_ini(float start, /**/ Time**);
 void time_fin(Time*);
 
-void time_next(Time*, float dt); /* "register" a step */
-int  time_cross(Time*, float interval); /* did we just cross
-                                           `n*interval', where `n' is
-                                           an integer? */
-float time_current (Time*);   /* current time */
-float time_dt (Time*);  /* current dt */
-float time_dt0(Time*);  /* previous dt */
+void time_next(Time*, float dt);
+int  time_cross(Time*, float interval);
+float time_current (Time*);
+float time_dt (Time*);
+float time_dt0(Time*);
 // end::interface[]
