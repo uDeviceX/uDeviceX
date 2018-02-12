f72ba52de6b50584bc6feaa8ac08c22a51b6fea8
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 10:39:27 +0100

    = shape.A[i]

--- src/rbc/force/dev/stress_free1/shape.h
+++ src/rbc/force/dev/stress_free1/shape.h
@@ -7,6 +7,6 @@ struct Shape0 {
 /* extract edge specific shape info */
 static void __device__ edg_shape(Shape shape, int i, /**/ Shape0 *shape0) {
     shape0->a = shape.a[i];
-    shape0->A = shape.a[i];
+    shape0->A = shape.A[i];
     shape0->totArea = shape.totArea;
 }
