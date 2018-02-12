9b864366b5a3f1c1d66067b67e77a4fbd81601ab
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 18:25:09 +0100

    read area and volume for conf.cfg

--- src/rbc/force/dev/stress_free0/force.h
+++ src/rbc/force/dev/stress_free0/force.h
@@ -2,8 +2,8 @@ __device__ real3 tri(RbcParams_v par, real3 r1, real3 r2, real3 r3, Shape0 shape
     real a0, A0, totArea, totVolume;
     A0 = shape0.A0;
     a0 = shape0.a0;
-    totArea = shape0.totArea;
-    totVolume = shape0.totVolume;
+    totArea = par.totArea;
+    totVolume = par.totVolume;
     return tri0(par, r1, r2, r3,   a0, A0, totArea, totVolume,   area, volume);
 }
 
--- src/rbc/force/dev/stress_free1/force.h
+++ src/rbc/force/dev/stress_free1/force.h
@@ -2,8 +2,8 @@ __device__ real3 tri(RbcParams_v par, real3 r1, real3 r2, real3 r3, Shape0 shape
     real l0, A0, totArea, totVolume;
     l0 = shape.a;
     A0 = shape.A;
-    totArea = shape.totArea;
-    totVolume = shape.totVolume;
+    totArea = par.totArea;
+    totVolume = par.totVolume;
     return tri0(par, r1, r2, r3,   l0, A0, totArea, totVolume,   area, volume);
 }
 
