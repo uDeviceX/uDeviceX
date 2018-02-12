9c5d9c7d2ed1029634eeca69668554197c048efd
Sergey Litvinov <slitvinov@gmail.com>
2018-02-12 16:47:02 +0100

    copy a0 and A0 to Shape0

--- src/rbc/force/dev/stress_free0/shape.h
+++ src/rbc/force/dev/stress_free0/shape.h
@@ -1,5 +1,7 @@
-struct Shape0 { /* info for one edge :TODO: */
-};
+struct Shape0 { float a0, A0; };
 
 /* extract edge specific shape info */
-static void __device__ edg_shape(Shape, int i, /**/ Shape0*) { }
+static void __device__ edg_shape(Shape shape, int, /**/ Shape0 *shape0) {
+    shape0->a0 = shape.a0;
+    shape0->A0 = shape.A0;
+}
