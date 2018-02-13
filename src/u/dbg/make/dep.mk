9544c7cdd29f9a97a6a5137ff58cf484fdd26d31
Sergey Litvinov <slitvinov@gmail.com>
2018-02-13 11:45:10 +0100

    deps

--- src/u/dbg/make/dep.mk
+++ src/u/dbg/make/dep.mk
@@ -8,7 +8,7 @@ $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/parser/imp.o: $S/utils/imp.h $S/utils/error.h $S/parser/imp.h $S/utils/msg.h
-$B/u/dbg/main.o: $S/inc/conf.h $S/utils/error.h $S/inc/type.h $S/dbg/imp.h $S/mpi/wrapper.h $S/coords/ini.h $S/utils/cc.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/utils/kl.h $S/parser/imp.h $S/coords/imp.h $S/mpi/glb.h $S/utils/msg.h
+$B/u/dbg/main.o: $S/inc/conf.h $S/utils/error.h $S/inc/type.h $S/dbg/imp.h $S/mpi/wrapper.h $S/coords/ini.h $S/utils/cc.h $S/inc/dev.h $S/utils/mc.h $S/d/api.h $B/conf.h $S/utils/kl.h $S/parser/imp.h $S/coords/imp.h $S/mpi/glb.h $S/utils/msg.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
 $B/utils/error.o: $S/utils/msg.h $S/utils/error.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
--- src/u/sdf/make/dep.mk
+++ src/u/sdf/make/dep.mk
@@ -11,7 +11,7 @@ $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/parser/imp.o: $S/utils/imp.h $S/utils/error.h $S/parser/imp.h $S/utils/msg.h
-$B/u/sdf/main.o: $S/coords/type.h $S/inc/conf.h $S/wall/sdf/imp.h $S/utils/error.h $S/inc/type.h $S/wall/sdf/dev.h $S/mpi/wrapper.h $S/wall/sdf/imp/type.h $S/coords/ini.h $S/utils/cc.h $S/u/sdf/dev.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/math/tform/dev.h $S/math/tform/type.h $S/utils/kl.h $S/wall/sdf/type.h $S/parser/imp.h $S/wall/sdf/tex3d/type.h $S/wall/wvel/type.h $S/coords/imp.h $S/mpi/glb.h $S/utils/msg.h $S/d/ker.h
+$B/u/sdf/main.o: $S/coords/type.h $S/inc/conf.h $S/wall/sdf/imp.h $S/utils/error.h $S/inc/type.h $S/wall/sdf/dev.h $S/mpi/wrapper.h $S/wall/sdf/imp/type.h $S/coords/ini.h $S/utils/cc.h $S/u/sdf/dev.h $S/utils/mc.h $S/inc/dev.h $S/d/api.h $B/conf.h $S/math/tform/dev.h $S/math/tform/type.h $S/utils/kl.h $S/wall/sdf/type.h $S/parser/imp.h $S/wall/sdf/tex3d/type.h $S/wall/wvel/type.h $S/coords/imp.h $S/mpi/glb.h $S/utils/msg.h $S/d/ker.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
 $B/utils/error.o: $S/utils/msg.h $S/utils/error.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
