f508882eaddebbcc9eda72b081ffc3cb2de934b4
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-13 11:33:41 +0100

    do not use m::cart in glb; separate cart creation

--- src/mpi/glb.h
+++ src/mpi/glb.h
@@ -1,4 +1,6 @@
 namespace m { /* mini MPI */
 void ini(int *argc, char ***argv);
+void get_dims(int *argc, char ***argv, int dims[3]);
+void get_cart(MPI_Comm comm, const int dims[3], /**/ MPI_Comm *cart);
 void fin();
 }
