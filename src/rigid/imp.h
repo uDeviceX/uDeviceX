cc28a3c68ffc9d2fd52e1ea7a456fca93e0fd988
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-13 12:48:35 +0100

    sketch interface for Pininfo

--- src/rigid/imp.h
+++ src/rigid/imp.h
@@ -1,3 +1,8 @@
+struct RigPinInfo;
+void rig_ini_pininfo(RigPinInfo **);
+void rig_fin_pininfo(RigPinInfo *);
+void rig_set_pininfo(int3 com, int3 axis, RigPinInfo *);
+
 void rig_reinit_ft(const int nsolid, /**/ Solid *ss);
 void rig_update(float dt, int n, const Force *ff, const float *rr0, int nsolid, /**/ Particle *pp, Solid *ss);
 void rig_generate(int ns, const Solid *ss, int nps, const float *rr0, /**/ Particle *pp);
