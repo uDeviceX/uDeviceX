a05b550da9bbfd380ca50206c823979edeb4ae53
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-02-14 09:01:35 +0100

    document rigid module

--- src/rigid/imp.h
+++ src/rigid/imp.h
@@ -2,21 +2,28 @@ struct RigPinInfo;
 struct int3;
 struct Config;
 
+// tag::enum[]
 enum {NOT_PERIODIC = -1};
+// end::enum[]
 
-void rig_ini_pininfo(RigPinInfo **);
-void rig_fin_pininfo(RigPinInfo *);
-void rig_set_pininfo(int3 com, int3 axis, RigPinInfo *);
-void rig_set_pdir(int pdir, RigPinInfo *);
-void rig_set_pininfo_conf(const Config *cfg, RigPinInfo *);
+// tag::pin[]
+void rig_ini_pininfo(RigPinInfo **); // <1>
+void rig_fin_pininfo(RigPinInfo *); // <2>
+void rig_set_pininfo(int3 com, int3 axis, RigPinInfo *); // <3>
+void rig_set_pdir(int pdir, RigPinInfo *); // <4>
+void rig_set_pininfo_conf(const Config *cfg, RigPinInfo *); // <5>
 
-int rig_get_pdir(const RigPinInfo *);
+int rig_get_pdir(const RigPinInfo *); // <6>
+// end::pin[]
 
 struct Particle;
 struct Solid;
 struct Force;
 
-void rig_reinit_ft(const int nsolid, /**/ Solid *ss);
-void rig_update(const RigPinInfo *pi, float dt, int n, const Force *ff, const float *rr0, int nsolid, /**/ Particle *pp, Solid *ss);
-void rig_generate(int ns, const Solid *ss, int nps, const float *rr0, /**/ Particle *pp);
-void rig_update_mesh(float dt, int ns, const Solid *ss, int nv, const float *vv, /**/ Particle *pp);
+// tag::upd[]
+void rig_reinit_ft(const int ns, /**/ Solid *ss); // <1>
+void rig_update(const RigPinInfo *pi, float dt, int n, const Force *ff, const float *rr0, int ns, /**/ Particle *pp, Solid *ss); // <2>
+void rig_generate(int ns, const Solid *ss, int nps, const float *rr0, /**/ Particle *pp); // <3>
+void rig_update_mesh(float dt, int ns, const Solid *ss, int nv, const float *vv, /**/ Particle *pp); // <4>
+// end::upd[]
+
