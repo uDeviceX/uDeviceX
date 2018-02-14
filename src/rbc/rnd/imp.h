141ea14e05908adf7b9b2497782be38bdf5c8d84
Sergey Litvinov <slitvinov@gmail.com>
2018-02-14 15:42:26 +0100

    rm SEED_ENV

--- src/rbc/rnd/imp.h
+++ src/rbc/rnd/imp.h
@@ -1,5 +1,5 @@
 struct RbcRnd;
-enum {ENV = -2, TIME = -1}; /* special seeds */
+enum {SEED_TIME = -1}; /* magic seed: ini from "time" */
 void rbc_rnd_ini(int n, long seed, RbcRnd**);
 void rbc_rnd_fin(RbcRnd*);
 void rbc_rnd_gen(RbcRnd*, int n, /**/ float**);
