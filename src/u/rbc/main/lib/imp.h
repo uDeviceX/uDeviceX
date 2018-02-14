166a4c61e2d98e8999f8191bd644a9eb8330c2c0
Sergey Litvinov <slitvinov@gmail.com>
2018-02-14 16:38:55 +0100

    fix u/rbc/main

--- src/u/rbc/main/lib/imp.h
+++ src/u/rbc/main/lib/imp.h
@@ -1,2 +1,3 @@
-void run(MPI_Comm cart, float dt, float te,
-         const Coords*, float part_freq, const BForce*, MoveParams*, const char *cell, const char *ic, const RbcParams*);
+void run(MPI_Comm cart, float dt, float te, int seed,
+         const Coords*, float part_freq, const BForce*, MoveParams*,
+         const char *cell, const char *ic, const RbcParams*);
--- src/u/rbc/main/main.cpp
+++ src/u/rbc/main/main.cpp
@@ -21,6 +21,7 @@
 #include "lib/imp.h"
 
 int main(int argc, char **argv) {
+    int seed;
     Config *cfg;
     Coords *coords;
     BForce *bforce;
@@ -42,6 +43,7 @@ int main(int argc, char **argv) {
     UC(conf_read(argc, argv, cfg));
     UC(conf_lookup_float(cfg, "time.dt", &dt));
     UC(conf_lookup_float(cfg, "time.end", &te));
+    UC(conf_lookup_int(cfg, "rbc.seed", &seed));
 
     UC(rbc_params_ini(&par));
     UC(rbc_params_set_conf(cfg, par));
@@ -54,7 +56,7 @@ int main(int argc, char **argv) {
     UC(scheme_move_params_ini(&moveparams));
     UC(scheme_move_params_conf(cfg, /**/moveparams));
 
-    run(cart, dt, te, coords, part_freq, bforce, moveparams, "rbc.off", "rbcs-ic.txt", par);
+    run(cart, dt, te, seed, coords, part_freq, bforce, moveparams, "rbc.off", "rbcs-ic.txt", par);
     UC(coords_fin(coords));
 
     UC(bforce_fin(bforce));
