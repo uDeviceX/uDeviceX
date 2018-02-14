933316c10c3580e70d340adebb35242c4a026e19
Sergey Litvinov <slitvinov@gmail.com>
2018-02-14 22:21:20 +0100

    rm commented code

--- src/conf/default.h
+++ src/conf/default.h
@@ -1,5 +1,4 @@
 #define   numberdensity              10
-//#define             kBT       0.0444302
 #define        flu_mass             1.0
 #define        rbc_mass             0.5
 #define      solid_mass             0.5
