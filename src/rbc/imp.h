8252fd0034fb1510533039184d768bec29382bfd
Sergey Litvinov <slitvinov@gmail.com>
2018-02-14 11:38:18 +0100

    useless const

--- src/rbc/imp.h
+++ src/rbc/imp.h
@@ -6,4 +6,4 @@ void rbc_fin(RbcQuants*);
 
 void rbc_gen_quants(const Coords*, MPI_Comm comm, OffRead*, const char *ic, RbcQuants*);
 void rbc_strt_quants(const Coords*, OffRead*, const int id, RbcQuants*);
-void rbc_strt_dump(const Coords*, const int id, const RbcQuants *q);
+void rbc_strt_dump(const Coords*, int id, const RbcQuants *q);
