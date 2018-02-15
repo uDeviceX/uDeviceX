8b6fcc50faa1bd9aba3a70927a290de91e5ae523
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-15 12:19:37 +0100

    adding farray

---
+++ src/farray/imp.h
@@ -0,0 +1,24 @@
+struct FoArray {
+    bool stress;
+    float *ff;
+    float *ss; /* stress */
+};
+
+struct FoArray_v;
+struct FoSArray_v;
+
+struct Force;
+
+// tag::push[]
+void farray_push_pp(Force *ff, FoArray *a);
+void farray_push_ss(float *ss, FoArray *a);
+// end::push[]
+
+// tag::get[]
+bool farray_has_stress(const FoArray *a);
+// end::get[]
+
+// tag::view[]
+void farray_get_view(const FoArray *a, FoArray_v *v);  // <1>
+void farray_get_view(const FoArray *a, PFoSArray_v *v); // <2>
+// end::view[]
