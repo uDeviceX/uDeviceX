f69b61db450f4d6a56e8489c796bc51a0bcd37f9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-02-15 12:01:21 +0100

    add documentation for exch: flu and common

--- src/exch/common/imp.h
+++ src/exch/common/imp.h
@@ -1,10 +1,14 @@
 struct int3;
 
+// tag::struct[]
 struct PackHelper {
     int *starts;
     int *offsets;
     int *indices[NFRAGS];
 };
+// end::struct[]
 
-void ecommon_pack_pp(const Particle *pp, PackHelper ph, /**/ Pap26 buf);
-void ecommon_shift_one_frag(int3 L, int n, const int fid, /**/ Particle *pp);
+// tag::int[]
+void ecommon_pack_pp(const Particle *pp, PackHelper ph, /**/ Pap26 buf); // <1>
+void ecommon_shift_one_frag(int3 L, int n, const int fid, /**/ Particle *pp); // <2>
+// end::int[]
--- src/exch/flu/imp.h
+++ src/exch/flu/imp.h
@@ -3,6 +3,7 @@ struct EFluComm;
 struct EFluUnpack;
 struct PaArray;
 
+// tag::mem[]
 void eflu_pack_ini(bool colors, int3 L, int maxd, EFluPack **p);
 void eflu_comm_ini(bool colors, MPI_Comm comm, /**/ EFluComm **c);
 void eflu_unpack_ini(bool colors, int3 L, int maxd, EFluUnpack **u);
@@ -10,18 +11,26 @@ void eflu_unpack_ini(bool colors, int3 L, int maxd, EFluUnpack **u);
 void eflu_pack_fin(EFluPack *p);
 void eflu_comm_fin(EFluComm *c);
 void eflu_unpack_fin(EFluUnpack *u);
+// end::mem[]
 
+// tag::map[]
 void eflu_compute_map(const int *start, const int *count, /**/ EFluPack *p);
 void eflu_download_cell_starts(/**/ EFluPack *p);
+// end::map[]
 
+// tag::pack[]
 void eflu_pack(const PaArray *parray, /**/ EFluPack *p);
 void eflu_download_data(EFluPack *p);
+// end::pack[]
 
+// tag::com[]
 void eflu_post_recv(EFluComm *c, EFluUnpack *u);
 void eflu_post_send(EFluPack *p, EFluComm *c);
 void eflu_wait_recv(EFluComm *c, EFluUnpack *u);
 void eflu_wait_send(EFluComm *c);
+// tag::end[]
 
+// tag::unpack[]
 void eflu_unpack(EFluUnpack *u);
 
 using flu::LFrag26;
@@ -29,3 +38,5 @@ using flu::RFrag26;
 
 void eflu_get_local_frags(const EFluPack *p, /**/ LFrag26 *lfrags);
 void eflu_get_remote_frags(const EFluUnpack *u, /**/ RFrag26 *rfrags);
+// end::unpack[]
+
