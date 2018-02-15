1b81e9480f99ed4d7aad3eefe09b1912b82d08e5
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-15 14:45:32 +0100

    rm old functions

--- src/fluforces/bulk/dev/main.h
+++ src/fluforces/bulk/dev/main.h
@@ -49,84 +49,9 @@ static __device__ void loop_pp(Par params, int ia, PairPa pa, Parray parray, int
     }
 }
 
-template<typename Par, typename Parray>
-__global__ void apply_simplest(Par params, int3 L, int n, Parray parray, const int *start, float seed, /**/ Force *ff) {
-    enum {X, Y, Z};
-    int ia, ib;
-    int3 ca, cb;
-    PairPa pa;
-    float fa[3] = {0};
-
-    ia = threadIdx.x + blockIdx.x * blockDim.x;
-    if (ia >= n) return;
-    
-    fetch(parray, ia, &pa);
-    ca = get_cid(L, &pa);
-
-    for (cb.z = ca.z - 1; cb.z <= ca.z + 1; ++cb.z) {
-        for (cb.y = ca.y - 1; cb.y <= ca.y + 1; ++cb.y) {
-            for (cb.x = ca.x - 1; cb.x <= ca.x + 1; ++cb.x) {
-                if (!valid_cid(L, cb)) continue;
-                ib = cb.x + L.x * (cb.y + L.y * cb.z);
-                
-                loop_pp(params, ia, pa, parray, start[ib], start[ib + 1], seed, /**/ fa, ff);
-            }        
-        }        
-    }
-
-    atomicAdd(ff[ia].f + X, fa[X]);
-    atomicAdd(ff[ia].f + Y, fa[Y]);
-    atomicAdd(ff[ia].f + Z, fa[Z]);
-}
-
-template <typename Par, typename Parray>
-__global__ void apply_smarter(Par params, int3 L, int n, Parray parray, const int *start, float seed, /**/ Force *ff) {
-    enum {X, Y, Z};
-    int ia, dy, dz;
-    int enddy, enddx;
-    int startx, endx;
-    int bs, be, cid0;
-    int3 ca, cb;
-    PairPa pa;
-    float fa[3] = {0};
-
-    ia = threadIdx.x + blockIdx.x * blockDim.x;
-    if (ia >= n) return;
-    
-    fetch(parray, ia, &pa);
-    ca = get_cid(L, &pa);
-
-    for (dz = -1; dz <= 0; ++dz) {
-        cb.z = ca.z + dz;
-        if (!valid_c(cb.z, L.z)) continue;
-        
-        enddy = dz ? 1 : 0;
-            
-        for (dy = -1; dy <= enddy; ++dy) {
-            cb.y = ca.y + dy;
-            if (!valid_c(cb.y, L.y)) continue;
-
-            enddx = (dz == 0 && dy == 0) ? 0 : 1;
-            
-            startx =    max(     0, ca.x - 1    );
-            endx   = 1 + min(L.x-1, ca.x + enddx);
-
-            cid0 = L.x * (cb.y + L.y * cb.z);
-
-            bs = start[cid0 + startx];
-            be = start[cid0 + endx];
-
-            loop_pp(params, ia, pa, parray, bs, be, seed, /**/ fa, ff);
-        }        
-    }
-
-    atomicAdd(ff[ia].f + X, fa[X]);
-    atomicAdd(ff[ia].f + Y, fa[Y]);
-    atomicAdd(ff[ia].f + Z, fa[Z]);
-}
-
 template <typename Par, typename PArray>
-__device__ void one_row(Par params, int3 L, int dz, int dy, int ia, int3 ca, PairPa pa, PArray parray, const int *start, float seed, /**/ PairFo *fa, FoArray_v farray) {
+static __device__ void one_row(Par params, int3 L, int dz, int dy, int ia, int3 ca, PairPa pa, PArray parray, const int *start, float seed,
+                               /**/ PairFo *fa, FoArray_v farray) {
     int3 cb;
     int enddx, startx, endx, cid0, bs, be;
     cb.z = ca.z + dz;
