7a57093dc50477a3df258be30d7794ac7297b80d
Sergey Litvinov <slitvinov@gmail.com>
2018-02-16 16:34:48 +0100

    add a new interface

--- src/wall/sdf/field/imp.h
+++ src/wall/sdf/field/imp.h
@@ -1,5 +1,6 @@
 struct Coords;
 struct Tform;
+struct Field;
 
 void sdf_field_ini_dims(const char *path, /**/ int N[3],    float ext[3]);
 void sdf_field_ini_data(const char *path, int n, /**/ float *D);
@@ -7,3 +8,14 @@ void sdf_field_sample(Tform*, const int N0[3], const float *D0, const int N1[3],
             /**/ float *D1);
 void sdf_field_scale(const int N[3], float s, /**/ float *D);
 void sdf_field_dump(const Coords*, MPI_Comm cart, const int N[3], const float *D);
+
+/* new interface */
+void field_ini(const char *path, /**/ Field**);
+void field_sample(const Field*, const int N[3], /**/ Field**);
+
+void field_size(const Field*, /**/ int N[3]);
+void field_extension(const Field*, /**/ float ext[3]);
+void field_data(const Field* /**/ float**);
+
+void field_scale(Field*, float scale);
+void field_fin(Field*);
