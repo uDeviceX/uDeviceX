b242dea8630f49545b7729b2735ae3331f14c2d5
Sergey Litvinov <slitvinov@gmail.com>
2018-02-16 16:44:43 +0100

    compile

--- src/wall/sdf/field/imp.h
+++ src/wall/sdf/field/imp.h
@@ -15,7 +15,7 @@ void field_sample(const Field*, Tform*, const int N[3], /**/ Field**);
 
 void field_size(const Field*, /**/ int N[3]);
 void field_extension(const Field*, /**/ float ext[3]);
-void field_data(const Field* /**/ float**);
+void field_data(const Field*, /**/ float**);
 
 void field_dump(const Field*, const Coords*, MPI_Comm cart);
 void field_scale(Field*, float scale);
