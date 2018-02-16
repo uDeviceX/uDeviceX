a8f8e5320dc2f300158f4aed54efbf9d1b7cefb1
Sergey Litvinov <slitvinov@gmail.com>
2018-02-16 18:18:43 +0100

    fscanf/sscanf

--- src/wall/sdf/field/imp/ini.h
+++ src/wall/sdf/field/imp/ini.h
@@ -2,22 +2,18 @@ void get_flo3(FILE *f, /**/ float *x, float *y, float *z) {
     int r;
     char s[2048];
     UC(efgets(s, sizeof(s), f));
-    r = fscanf(f, "%f %f %f", x, y, z);
-    if (r != 3) {
-        msg_print("expect three floats, got");
-        ERR("%s", s);
-    }
+    r = sscanf(s, "%f %f %f", x, y, z);
+    if (r != 3)
+        ERR("expect three floats, got '%s'", s);
 }
 
 void get_int3(FILE *f, /**/ int *x, int *y, int *z) {
     int r;
     char s[2048];
     UC(efgets(s, sizeof(s), f));
-    r = fscanf(f, "%d %d %d", x, y, z);
-    if (r != 3) {
-        msg_print("expect three integers, got");
-        ERR("%s", s);
-    }
+    r = sscanf(s, "%d %d %d", x, y, z);
+    if (r != 3)
+        ERR("expect three integers, got '%s'", s);
 }
 
 void field_ini(const char *path, /**/ Field **pq) {
