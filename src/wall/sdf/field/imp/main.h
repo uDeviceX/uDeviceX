100c03fd12d7479c622e2bb445c62eef3987f712
Sergey Litvinov <slitvinov@gmail.com>
2018-02-16 18:26:54 +0100

    rm unused code

--- src/wall/sdf/field/imp/main.h
+++ src/wall/sdf/field/imp/main.h
@@ -1,34 +1,3 @@
-static void skip_line(FILE *f) {
-    char l[BUFSIZ];
-    UC(efgets(l, sizeof(l), f));
-}
-
-void sdf_field_ini_dims(const char *path, /**/ int N[3], float ext[3]) {
-    FILE *f;
-    char l[BUFSIZ];
-    UC(efopen(path, "r", /**/ &f));
-    UC(efgets(l, sizeof(l), f));
-    sscanf(l, "%f %f %f", &ext[0], &ext[1], &ext[2]);
-    UC(efgets(l, sizeof(l), f));
-    sscanf(l, "%d %d %d", &N[0], &N[1], &N[2]);
-    UC(efclose(f));
-}
-
-void sdf_field_ini_data(const char *path, int n, /**/ float *D) { /* read sdf file */
-    FILE *f;
-    UC(efopen(path, "r", /**/ &f));
-    skip_line(f); skip_line(f);
-    UC(efread(D, sizeof(D[0]), n, f));
-    UC(efclose(f));
-}
-
-void sdf_field_scale(const int N[3], float s, /**/ float *D) {
-    enum {X, Y, Z};
-    int i, n;
-    n = N[X]*N[Y]*N[Z];
-    for (i = 0; i < n; i++) D[i] *= s;
-}
-
 static void dump0(const Coords *coords, const int N0[3], const float *D0, /**/ float *D1) {
     int L[3] = {xs(coords), ys(coords), zs(coords)};
     Tform *t;
@@ -50,7 +19,6 @@ void sdf_field_dump(const Coords *coords, MPI_Comm cart, const int N[], const fl
     UC(dump1(coords, cart, N, D, /*w*/ W));
     UC(efree(W));
 }
-
 
 /* new */
 void field_size(const Field *q, /**/ int N[3]) {
