f0e980e49af333ad0dc13e8c3ba62da0fde3d16c
Sergey Litvinov <slitvinov@gmail.com>
2018-02-17 20:32:08 +0100

    pre-declare more structs

--- src/color/flux.h
+++ src/color/flux.h
@@ -1,3 +1,6 @@
 struct int3;
+struct Particle;
+struct Coords;
 
-void color_linear_flux(const Coords *coords, int3 L, int dir, int color, int n, const Particle *pp, int *cc);
+void color_linear_flux(const Coords*, int3 L, int dir, int color, int n, const Particle*, /**/
+                       int*);
