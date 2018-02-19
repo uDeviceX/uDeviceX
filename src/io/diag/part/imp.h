ab46b6c1c6a2942cc471502146c82bf43e7464a1
Sergey Litvinov <slitvinov@gmail.com>
2018-02-19 17:37:37 +0100

    add tags in io/diag

--- src/io/diag/part/imp.h
+++ src/io/diag/part/imp.h
@@ -1,7 +1,8 @@
 struct Particle;
 struct DiagPart;
 
+// tag::interface[]
 void diag_part_ini(const char *path, /**/ DiagPart**);
 void diag_part_fin(DiagPart*);
-
 void diag_part_apply(DiagPart*, MPI_Comm, float time, int n, const Particle*);
+// end::interface[]
