7f3144dad6eebb348ad4f284be332652c7e8abe9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-02-19 17:36:34 +0100

    upd wvel doc

--- src/wall/wvel/imp.h
+++ src/wall/wvel/imp.h
@@ -8,11 +8,13 @@ struct WvelHS_v;
 struct float3;
 struct Config;
 
+// tag::enum[]
 enum {
     WALL_VEL_V_CSTE,
     WALL_VEL_V_SHEAR,
     WALL_VEL_V_HS,
 };
+// end::enum[]
 
 // tag::mem[]
 void wvel_ini(Wvel **wv);
@@ -34,12 +36,16 @@ void wvel_set_timestep(float dt, Wvel *vw);
 void wvel_set_conf(const Config *cfg, Wvel *vw);
 // end::cnf[]
 
-// tag::int[]
-void wvel_get_step(float dt, long it, const Wvel *wv, /**/ WvelStep *view);
-// end::int[]
+// tag::step[]
+void wvel_get_step(float dt, long it, const Wvel *wv, /**/ WvelStep *);
+// end::step[]
 
+// tag::get[]
 int wvel_get_type(const WvelStep *w);
+// end::get[]
 
+// tag::view[]
 void wvel_get_view(const WvelStep *w, /**/ WvelCste_v *v);
 void wvel_get_view(const WvelStep *w, /**/ WvelShear_v *v);
 void wvel_get_view(const WvelStep *w, /**/ WvelHS_v *v);
+// end::view[]
