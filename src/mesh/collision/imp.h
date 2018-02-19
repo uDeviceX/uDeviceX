52e514d82ad4d3464fcdb9cfd3d23522c0d2e4a8
Sergey Litvinov <slitvinov@gmail.com>
2018-02-19 15:46:32 +0100

    minor

--- src/mesh/collision/imp.h
+++ src/mesh/collision/imp.h
@@ -5,6 +5,7 @@ struct int4;
 int collision_inside_1p(int spdir, const float *r, const float *vv, const int4 *tt, const int nt);
 
 void collision_get_colors(const Particle *pp, int n,
-                          const Particle *i_pp, const int4 *tri,
-                          int nt, int nv, int nm,
+                          const Particle *i_pp,
+                          const int4 *tri, int nt, int nv,
+                          int nm,
                           const float3 *minext, const float3 *maxext, /**/ int *cc);
