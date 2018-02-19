460e2a8eb5c87744961ed08be6af9e74cf397eec
Sergey Litvinov <slitvinov@gmail.com>
2018-02-19 18:09:38 +0100

    use tag::

--- src/io/diag/mesh/imp.h
+++ src/io/diag/mesh/imp.h
@@ -1,6 +1,8 @@
 struct DiagMesh;
 struct MeshRead;
 
+// tag::interface[]
 void diag_mesh_ini(const char *path, MeshRead*, DiagMesh**);
 void diag_mesh_fin(DiagMesh*);
 void diag_mesh_apply(DiagMesh*, MPI_Comm, float time, int nc, Particle*);
+// end::interface[]
