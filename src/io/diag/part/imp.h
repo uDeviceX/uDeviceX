47181ff9893fbdac92b8dcacab42161adee238b4
Sergey Litvinov <slitvinov@gmail.com>
2018-02-19 16:58:54 +0100

    minor

--- src/io/diag/part/imp.h
+++ src/io/diag/part/imp.h
@@ -1,7 +1,7 @@
 struct Particle;
 struct DiagPart;
 
-void diag_part_ini(/**/ const char *path, DiagPart**);
+void diag_part_ini(const char *path, /**/ DiagPart**);
 void diag_part_fin(DiagPart*);
 
 void diag(MPI_Comm comm, float time, int n, const Particle *pp);
