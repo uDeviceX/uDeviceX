16688008e1f80d0a452d28abaa5589391ae64fa3
Sergey Litvinov <slitvinov@gmail.com>
2018-02-19 11:42:13 +0100

    add EMEMCPY

--- src/utils/imp.h
+++ src/utils/imp.h
@@ -1,8 +1,9 @@
 void emalloc(size_t, /**/ void**);
 void efree(void*);
 
-#define EMALLOC(n, ppdata) UC(emalloc(n * sizeof(**(ppdata)), (void**) (ppdata)))
+#define EMALLOC(n, ppdata) UC(emalloc((n)*sizeof(**(ppdata)), (void**)(ppdata)))
 #define EFREE(pdata) UC(efree(pdata))
+#define EMEMCPY(n, src, dest) memcpy((void*)(dest), (const void*)(src), (n)*sizeof(*(dest)))
 
 void efopen(const char *fname, const char *mode, /**/ FILE**);
 void efclose(FILE*);
