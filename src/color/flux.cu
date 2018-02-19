4d9c835f5b04576d52b4d9d3317ce674c0a543f7
Sergey Litvinov <slitvinov@gmail.com>
2018-02-19 13:48:53 +0100

    minor

--- src/color/flux.cu
+++ src/color/flux.cu
@@ -20,7 +20,6 @@ __global__ void linear_flux(int3 L, int dir, int color, int n, const Particle *p
     Particle p;
     i = threadIdx.x + blockIdx.x * blockDim.x;
     if (i >= n) return;
-    
     p = pp[i];
     const int HL[] = {L.x/2, L.y/2, L.z/2};
 
@@ -31,7 +30,6 @@ __global__ void linear_flux(int3 L, int dir, int color, int n, const Particle *p
 
 void color_linear_flux(const Coords *coords, int3 L, int dir, int color, int n, const Particle *pp, int *cc) {
     assert(dir >= 0 && dir <= 2);
-        
     if (is_end(coords, dir))
         KL(color_dev::linear_flux, (k_cnf(n)), (L, dir, color, n, pp, cc));
 }
