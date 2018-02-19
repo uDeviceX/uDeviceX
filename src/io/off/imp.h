0300a67acb9b82b50dff592dc3e31842ec2450c6
Sergey Litvinov <slitvinov@gmail.com>
2018-02-19 13:55:08 +0100

    initial import of off

--- src/io/off/imp.h
+++ src/io/off/imp.h
@@ -1,16 +1,15 @@
 struct int4;
 struct MeshRead;
 
-/* off files
-   [1] https://en.wikipedia.org/wiki/OFF_(file_format) */
-
+// tag::interface[]
 void mesh_read_off(const char *path, MeshRead**);
 void mesh_read_ply(const char *path, MeshRead**);
 void mesh_fin(MeshRead*);
 
 int mesh_get_nt(MeshRead*);
 int mesh_get_nv(MeshRead*);
-int mesh_get_md(MeshRead*); /* maximum vertex degree */
+int mesh_get_md(MeshRead*);
 
 const int4  *mesh_get_tri(MeshRead*);
 const float *mesh_get_vert(MeshRead*);
+// end::interface[]
