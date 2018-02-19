f27b7c2486adb9bf1a86b5876a4524823e7e3d3e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-02-19 11:00:17 +0100

    upd doc for velocity controller

--- src/control/vel/imp.h
+++ src/control/vel/imp.h
@@ -10,15 +10,16 @@ void vcont_ini(MPI_Comm comm, int3 L, /**/ PidVCont **c);
 void vcont_fin(/**/ PidVCont *cont);
 // end::mem[]
 
-void vcont_set_params(float factor, float Kp, float Ki, float Kd, /**/ PidVCont *c);
-void vcont_set_target(float3 vtarget, /**/ PidVCont *c);
+// tag::set[]
+void vcont_set_params(float factor, float Kp, float Ki, float Kd, /**/ PidVCont *c); // <1>
+void vcont_set_target(float3 vtarget, /**/ PidVCont *c);  // <2>
+void vcont_set_cart(/**/ PidVCont *c);  // <3>
+void vcont_set_radial(/**/ PidVCont *c);  // <4>
+// end::set[]
 
-// tag::ini[]
-void vcont_set_cart(/**/ PidVCont *c);
-void vcont_set_radial(/**/ PidVCont *c);
-// end::ini[]
-
+// tag::cnf[]
 void vcont_set_conf(const Config *cfg, /**/ PidVCont *c);
+// end::cnf[]
 
 // tag::int[]
 void   vcont_sample(const Coords *coords, int n, const Particle *pp, const int *starts, const int *counts, /**/ PidVCont *c); // <1>
