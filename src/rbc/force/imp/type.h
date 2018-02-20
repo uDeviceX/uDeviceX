f6b30b6b65c2d63303c2edd3e68859e729cf7e08
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-20 10:42:27 +0100

    idem on host

--- src/rbc/force/imp/type.h
+++ src/rbc/force/imp/type.h
@@ -1,1 +1,20 @@
-struct RbcForce { RbcRnd *rnd; };
+struct StressFree {
+    float *ll; /* eq. spring lengths */
+    float *aa; /* eq. triangle areas */
+};
+
+struct StressFul {
+    float l0; /* eq. spring length */
+    float a0; /* eq. triangle area */
+};
+
+union SFreeInfo {
+    StressFul sful;
+    StressFree sfree;
+};
+
+struct RbcForce {
+    RbcRnd *rnd;
+    
+    SFreeInfo sinfo;
+};
