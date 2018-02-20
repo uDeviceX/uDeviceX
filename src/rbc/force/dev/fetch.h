e92ee8b37e0a697f493da8ee5209def2ba59078a
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-20 14:22:01 +0100

    forgot to add fetch.h

---
+++ src/rbc/force/dev/fetch.h
@@ -0,0 +1,17 @@
+struct StressInfo {
+    real l0, a0;
+};
+
+static __device__ StressInfo fetch_stress_info(int, StressFul_v sv) {
+    StressInfo si;
+    si.l0 = sv.l0;
+    si.a0 = sv.a0;
+    return si;
+}
+
+static __device__ StressInfo fetch_stress_info(int i, StressFree_v sv) {
+    StressInfo si;
+    si.l0 = sv.ll[i];
+    si.a0 = sv.aa[i];
+    return si;
+}
