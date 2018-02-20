590e1f214e19eaa69bc6de89a685748106915431
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-20 10:42:02 +0100

    adding type for stressfree/stressful shapes

---
+++ src/rbc/force/type.h
@@ -0,0 +1,9 @@
+struct StressFree_v {
+    float *ll; /* eq. spring lengths */
+    float *aa; /* eq. triangle areas */
+};
+
+struct StressFul_v {
+    float l0; /* eq. spring length */
+    float a0; /* eq. triangle area */
+};
