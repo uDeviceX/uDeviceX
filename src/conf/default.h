d4722223b60ad57632db3dce89bb6c64175b9995
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-20 17:52:42 +0100

    rbc_mass:adapt tests and default cfg

--- src/conf/default.h
+++ src/conf/default.h
@@ -1,2 +1,1 @@
 #define   numberdensity              10
-// #define        rbc_mass             0.5
--- src/conf/lina.h
+++ src/conf/lina.h
@@ -1,5 +1,4 @@
 #define   numberdensity              10
-// #define        rbc_mass            0.25
 
 #define      color_freq             500
 #define dump_all_fields            true
