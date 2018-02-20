994a24a5823b4fb01bfccf091427c18f1e2d7242
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-20 17:50:02 +0100

    solid_mass:adapt tests

--- src/conf/lina.h
+++ src/conf/lina.h
@@ -1,7 +1,5 @@
 #define   numberdensity              10
-// #define        flu_mass             1.0
 // #define        rbc_mass            0.25
-// #define      solid_mass             1.0
 
 #define      color_freq             500
 #define dump_all_fields            true
