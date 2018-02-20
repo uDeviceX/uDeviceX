6a3fb2465ae8fdf9dd8a64f475c60eac6bcddfce
Sergey Litvinov <slitvinov@gmail.com>
2018-02-20 17:39:48 +0100

    add tags in rbc/com

--- src/rbc/com/imp.h
+++ src/rbc/com/imp.h
@@ -1,6 +1,9 @@
 struct RbcCom;
 struct float3;
 struct Particle;
+
+// tag::interface[]
 void rbc_com_ini(int nv, int max_cell, /**/ RbcCom**);
 void rbc_com_fin(/**/ RbcCom*);
 void rbc_com_compute(RbcCom*, int nm, const Particle*, /**/ float3 **rr, float3 **vv);
+// end::interface[]
