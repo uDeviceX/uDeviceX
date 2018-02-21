2ff4f7967e4ffe26ef4c1cc106394749ccf1ce53
Sergey Litvinov <slitvinov@gmail.com>
2018-02-21 11:51:55 +0100

    add dummy impl for mesh/positions

--- src/mesh/positions/imp.h
+++ src/mesh/positions/imp.h
@@ -1,8 +1,8 @@
 struct Particle;
 struct Positions;
 
-void Positions_float_ini(int n, float*, /**/, Positions**);
-void Positions_particle_ini(int n, Particle* /**/, Positions**);
+void Positions_float_ini(int n, float*, /**/ Positions**);
+void Positions_particle_ini(int n, Particle*, /**/ Positions**);
 void Positions_fin(Positions*);
 
 void Positions_get(int i, /**/ float r[3]);
---
+++ src/mesh/positions/imp/main.h
@@ -0,0 +1,13 @@
+void Positions_float_ini(int, float*, /**/ Positions**) {
+
+}
+
+void Positions_particle_ini(int, Particle*, /**/ Positions**) {
+}
+
+void Positions_fin(Positions*) {
+}
+
+void Positions_get(int, /**/ float*) {
+
+}
