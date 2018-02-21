af42d12e95c4c2350ed97ac4d667987881f64b07
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-21 10:39:29 +0100

    add type

--- src/io/rig/imp.cpp
+++ src/io/rig/imp.cpp
@@ -8,50 +8,6 @@
 #include "utils/imp.h"
 #include "coords/imp.h"
 
-enum {X, Y, Z};
-
-static void write_v(FILE *f, const float v[3]) {
-    fprintf(f, "%+.6e %+.6e %+.6e ", v[X], v[Y], v[Z]);
-}
-
-void io_rig_dump(float dt, const int it, const Solid *ss, const Solid *ssbb, int ns, const Coords *c) {
-    enum {X, Y, Z};
-    static bool first = true;
-    char fname[256];
-    float com[3];
-    FILE *fp;
-    int j;
-    const Solid *s, *sbb;
-
-    for (j = 0; j < ns; ++j) {
-        s   = ss   + j;
-        sbb = ssbb + j;
-            
-        sprintf(fname, DUMP_BASE "/solid_diag_%04d.txt", (int) s->id);
-        if (first) UC(efopen(fname, "w", /**/ &fp));
-        else       UC(efopen(fname, "a", /**/ &fp));
-
-        fprintf(fp, "%+.6e ", dt*it);
-
-        // shift to global coordinates
-        com[X] = xl2xg(c, s->com[X]);
-        com[Y] = yl2yg(c, s->com[Y]);
-        com[Z] = zl2zg(c, s->com[Z]);
-            
-        write_v(fp, com);
-        write_v(fp, s->v );
-        write_v(fp, s->om);
-        write_v(fp, s->fo);
-        write_v(fp, s->to);
-        write_v(fp, s->e0);
-        write_v(fp, s->e1);
-        write_v(fp, s->e2);
-        write_v(fp, sbb->fo);
-        write_v(fp, sbb->to);
-        fprintf(fp, "\n");
-        
-        UC(efclose(fp));
-    }
-
-    first = false;
-}
+#include "imp.h"
+#include "imp/type.h"
+#include "imp/main.h"
---
+++ src/io/rig/imp/type.h
@@ -0,0 +1,3 @@
+struct IoRig {
+    char mode[2]; // fopen mode
+};
