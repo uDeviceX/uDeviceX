18b78504bf1180e45963cfe553db288fc9c31029
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-22 10:59:39 +0100

    use local opt in sim/imp/main

--- src/sim/imp/main.h
+++ src/sim/imp/main.h
@@ -9,26 +9,27 @@ static void gen(Time *time, float tw, const Coords *coords, Wall *w, Sim *s) { /
     Flu *flu = &s->flu;
     Rbc *rbc = &s->rbc;
     Rig *rig = &s->rig;
-    bool dump_sdf = s->opt.dump_field;
+    const Opt *opt = &s->opt;
+    bool dump_sdf = opt->dump_field;
     long maxp_wall = get_max_parts_wall(coords);
-
+    
     InterWalInfos winfo;
     InterFluInfos finfo;
     InterRbcInfos rinfo;
     InterRigInfos sinfo;
 
-    winfo.active = s->opt.wall;
+    winfo.active = opt->wall;
     winfo.sdf = w->sdf;
     finfo.q = &flu->q;
-    rinfo.active = s->opt.rbc;
+    rinfo.active = opt->rbc;
     rinfo.q = &rbc->q;
-    sinfo.active = s->opt.rig;
+    sinfo.active = opt->rig;
     sinfo.q = &rig->q;
     sinfo.pi = rig->pininfo;
     sinfo.mass = rig->mass;
     
     run_eq(time, tw, s);
-    if (s->opt.wall) {
+    if (opt->wall) {
         dSync();
         UC(sdf_gen(coords, s->cart, dump_sdf, /**/ w->sdf));
         MC(m::Barrier(s->cart));
@@ -37,7 +38,7 @@ static void gen(Time *time, float tw, const Coords *coords, Wall *w, Sim *s) { /
     inter_freeze(coords, s->cart, winfo, /*io*/ finfo, rinfo, sinfo);
     clear_vel(s);
 
-    if (s->opt.flucolors) {
+    if (opt->flucolors) {
         Particle *pp = flu->q.pp;
         int n = flu->q.n;
         int *cc = flu->q.cc;
@@ -50,29 +51,30 @@ void sim_gen(Sim *s, const Config *cfg, Time *time, TimeSeg *time_seg) {
     Rbc *rbc = &s->rbc;
     Wall *wall = &s->wall;
     MeshRead *cell = s->rbc.cell;
-
+    const Opt *opt = &s->opt;
+    
     UC(flu_gen_quants(s->coords, s->gen_color, &flu->q));
     UC(flu_build_cells(&flu->q));
-    if (s->opt.fluids)  flu_gen_ids  (s->cart, flu->q.n, &flu->q);
-    if (s->opt.rbc) {
+    if (opt->fluids)  flu_gen_ids  (s->cart, flu->q.n, &flu->q);
+    if (opt->rbc) {
         rbc_gen_quants(s->coords, s->cart, cell, "rbcs-ic.txt", /**/ &rbc->q);
-        if (s->opt.flucolors) gen_colors(rbc, &s->colorer, /**/ flu);
+        if (opt->flucolors) gen_colors(rbc, &s->colorer, /**/ flu);
     }
     MC(m::Barrier(s->cart));
-    if (s->opt.wall || s->opt.rig) {
+    if (opt->wall || opt->rig) {
         s->solids0 = false;
         gen(time, time_seg->wall, s->coords, /**/ wall, s);
         dSync();
-        if (s->opt.wall && wall->q.n) UC(wall_gen_ticket(&wall->q, wall->t));
-        s->solids0 = s->opt.rig;
-        if (s->opt.rbc && s->opt.flucolors) gen_colors(rbc, &s->colorer, /**/ flu);
+        if (opt->wall && wall->q.n) UC(wall_gen_ticket(&wall->q, wall->t));
+        s->solids0 = opt->rig;
+        if (opt->rbc && opt->flucolors) gen_colors(rbc, &s->colorer, /**/ flu);
         run(cfg, time, time_seg->wall, time_seg->end, s);
     } else {
-        s->solids0 = s->opt.rig;
+        s->solids0 = opt->rig;
         run(cfg, time, 0, time_seg->end, s);
     }
     /* final strt dump*/
-    if (s->opt.dump_strt) dump_strt0(RESTART_FINAL, s);
+    if (opt->dump_strt) dump_strt0(RESTART_FINAL, s);
 }
 
 void sim_strt(Sim *s, const Config *cfg, Time *time, TimeSeg *time_seg) {
@@ -82,7 +84,7 @@ void sim_strt(Sim *s, const Config *cfg, Time *time, TimeSeg *time_seg) {
     Wall *wall = &s->wall;
     MeshRead *cell = s->rbc.cell;
     const Opt *opt = &s->opt;
-    bool dump_sdf = s->opt.dump_field;
+    bool dump_sdf = opt->dump_field;
     long maxp_wall = get_max_parts_wall(s->coords);
 
     /*Q*/
