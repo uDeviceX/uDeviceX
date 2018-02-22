d847b3e966d927b4dc57616f9edb3cb4dffb0a69
Sergey Litvinov <slitvinov@gmail.com>
2018-02-22 13:07:05 +0100

    initial import of u/mesh/volume

---
+++ src/u/mesh/volume/make/dep.mk
@@ -0,0 +1,11 @@
+$B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/d/cuda/imp.h
+$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
+$B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
+$B/mpi/wrapper.o: $S/mpi/wrapper.h
+$B/u/mesh/volume/main.o: $S/mpi/wrapper.h $S/utils/mc.h $S/mpi/glb.h $S/utils/msg.h
+$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
+$B/utils/error.o: $S/utils/msg.h $S/utils/error.h
+$B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
+$B/utils/mc.o: $S/inc/conf.h $S/utils/error.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
+$B/utils/msg.o: $S/utils/msg.h
+$B/utils/os.o: $S/utils/os.h $S/utils/error.h $S/utils/msg.h
