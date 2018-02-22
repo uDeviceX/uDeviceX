58cdc834467fa702d2f5c40dbf011be9acdac49a
Sergey Litvinov <slitvinov@gmail.com>
2018-02-22 12:56:32 +0100

    add a dummy version of mesh/volume

---
+++ src/mesh/volume/imp/main.h
@@ -0,0 +1,2 @@
+void mesh_volume_fin(MeshVolume*) {
+}
--- src/u/scheme/time_step/make/dep.mk
+++ src/u/scheme/time_step/make/dep.mk
@@ -1,4 +1,4 @@
-$B/algo/force_stat/imp.o: $S/utils/error.h $S/inc/type.h $S/algo/force_stat/imp.h $S/algo/force_stat/imp/main.h $S/utils/msg.h
+$B/algo/force_stat/imp.o: $S/utils/error.h $S/inc/type.h $S/algo/force_stat/imp.h $S/d/api.h $S/algo/force_stat/imp/main.h $S/utils/msg.h
 $B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/d/cuda/imp.h
 $B/io/txt/imp.o: $S/utils/imp.h $S/utils/error.h $S/inc/type.h $S/io/txt/imp.h $S/io/txt/imp/dump.h $S/io/txt/imp/read.h $S/io/txt/imp/type.h $S/utils/msg.h
 $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
