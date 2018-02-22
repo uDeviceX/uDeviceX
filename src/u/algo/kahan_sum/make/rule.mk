a0e43d4490c3d28c1de470ef7c82391899b3ff71
Sergey Litvinov <slitvinov@gmail.com>
2018-02-22 14:46:14 +0100

    initial import of u/algo/kahan_sum (a copy of u/hw/hst)

---
+++ src/u/algo/kahan_sum/make/rule.mk
@@ -0,0 +1,11 @@
+$B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
+$B/u/algo/kahan_sum/main.o: $S/u/algo/kahan_sum/main.cpp; $X -I$S/.
+$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
+$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/utils -I$S/.
+$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
+$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/utils -I$S/.
+$B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
