68754a812ec67b3c22e919e7ae9f74c35300285f
Sergey Litvinov <slitvinov@gmail.com>
2018-02-22 12:57:51 +0100

    restore a part of imp/main.h

--- src/scheme/time_step/imp/main.h
+++ src/scheme/time_step/imp/main.h
@@ -18,5 +18,49 @@ void time_step_accel_reset(TimeStepAccel *q) { q->k = 0; }
 
 void time_step_ini(Config *c, /**/ TimeStep **pq) {
     const char *type;
-     tlog[q->type](q); }
+    TimeStep *q;
+    EMALLOC(1, &q);
+    UC(conf_lookup_string(c, "time.type", &type));
+    if      (same_str(type, "const")) {
+        q->type = CONST;
+        UC(conf_lookup_float(c, "time.dt", &q->dt));
+    } else if (same_str(type, "disp")) {
+        q->type = DISP;
+        q->k    = 0;
+        UC(conf_lookup_float(c, "time.dt", &q->dt));
+        UC(conf_lookup_float(c, "time.dx", &q->dx));
+    } else
+        ERR("unrecognised time.type: '%s'", type);
+    *pq = q;
+}
+void time_step_fin(TimeStep *q) {EFREE(q); }
+
+static float const_dt(TimeStep *q, MPI_Comm, TimeStepAccel*) { return q->dt; }
+static float disp_dt(TimeStep *q, MPI_Comm comm, TimeStepAccel *a) {
+    float dt_new, dx, dt_max, accel;
+    dt_max = q->dt;
+    dx     = q->dx;
+    accel = accel_max(comm, a, /**/ q->accel);
+    q->k  = a->k;
+    if (accel == 0) return dt_max;
+    dt_new = sqrtf(2.*dx/accel);
+    return (dt_new > dt_max ? dt_max : dt_new);
+}
+float time_step_dt(TimeStep *q, MPI_Comm comm, TimeStepAccel *a) {
+    return dt[q->type](q, comm, a);
+}
+
+static void const_log(TimeStep *q) { msg_print("time_step: const: dt = %g", q->dt); }
+static void  disp_log(TimeStep *q) {
+    int i;
+    float accel;
+    float dx;
+    dx = q->dx;
+    msg_print("time_step: disp: dt = %g dx = %g", q->dt, q->dx);
+    for (i = 0; i < q->k; i++) {
+        accel = q->accel[i];
+        msg_print("  accel:%g, dt_new:%g", accel, sqrtf(2.*dx/accel));
+    }
+}
+void time_step_log(TimeStep *q) { tlog[q->type](q); }
 float time_step_dt0(TimeStep *q) { return q->dt; };
