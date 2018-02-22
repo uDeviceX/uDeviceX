e0f9864281c19582b2249e09ed5b7475a46b8f63
Sergey Litvinov <slitvinov@gmail.com>
2018-02-22 13:44:11 +0100

    deps

--- src/u/mesh/volume/make/dep.mk
+++ src/u/mesh/volume/make/dep.mk
@@ -5,7 +5,7 @@ $B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
 $B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
 $B/mpi/wrapper.o: $S/mpi/wrapper.h
 $B/parser/imp.o: $S/utils/imp.h $S/utils/error.h $S/parser/imp.h $S/utils/msg.h
-$B/u/mesh/volume/main.o: $S/utils/error.h $S/mpi/wrapper.h $S/mesh/volume/imp.h $S/utils/mc.h $S/io/off/imp.h $S/parser/imp.h $S/mpi/glb.h $S/utils/msg.h
+$B/u/mesh/volume/main.o: $S/utils/error.h $S/mpi/wrapper.h $S/mesh/volume/imp.h $S/utils/mc.h $S/io/off/imp.h $S/parser/imp.h $S/mesh/positions/imp.h $S/mpi/glb.h $S/utils/msg.h
 $B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
 $B/utils/error.o: $S/utils/msg.h $S/utils/error.h
 $B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
