a14330f96b6217cfd2f20573fb3bc4ae8e0652f9
Sergey Litvinov <slitvinov@gmail.com>
2018-02-22 20:21:28 +0100

    minor

--- src/math/linal/imp.h
+++ src/math/linal/imp.h
@@ -1,4 +1,4 @@
 // tag::interface[]
 /* invert symmetric matrix m0[6] */
-void linal_inv3x3(const float *m0, /**/ float *minv0); // <1>
+void linal_inv3x3(const float m0[6], /**/ float minv0[6]); // <1>
 // end::interface[]
