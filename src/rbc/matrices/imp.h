b2ff00c3fa648a402333049583d9454e00429d1f
Sergey Litvinov <slitvinov@gmail.com>
2018-02-27 15:59:13 +0100

    return a pointer to a matrix

--- src/rbc/matrices/imp.h
+++ src/rbc/matrices/imp.h
@@ -6,7 +6,7 @@ void matrices_read(const char *path, /**/ Matrices**);
 void matrices_read_r(const char *path, /**/ Matrices**);
 void matrices_ini_filter(Matrices*, Coords*, /**/ Matrices**);
 
-void matrices_get(Matrices*, int i, /**/ double M[4*4]);
+void matrices_get(Matrices*, int i, /**/ double**);
 void matrices_get_r(Matrices*, int i, /**/ double r[3]);
 int  matrices_get_n(Matrices*);
 
