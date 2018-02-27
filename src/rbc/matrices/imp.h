eda5099de860bd224c1708e26882cf52134c0078
Sergey Litvinov <slitvinov@gmail.com>
2018-02-27 15:58:24 +0100

    add _read and read_r to matrices API

--- src/rbc/matrices/imp.h
+++ src/rbc/matrices/imp.h
@@ -2,12 +2,13 @@ struct Matrices;
 struct Coords;
 
 // tag::interface[]
-void matrices_ini_file(const char *path, /**/ Matrices**);
+void matrices_read(const char *path, /**/ Matrices**);
+void matrices_read_r(const char *path, /**/ Matrices**);
 void matrices_ini_filter(Matrices*, Coords*, /**/ Matrices**);
 
-int  matrices_get_n(Matrices*);
 void matrices_get(Matrices*, int i, /**/ double M[4*4]);
 void matrices_get_r(Matrices*, int i, /**/ double r[3]);
+int  matrices_get_n(Matrices*);
 
 void matrices_fin(Matrices*);
 // end::interface[]
