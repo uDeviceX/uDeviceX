65858c6fc10583268649789a04c4242ba717b982
Sergey Litvinov <slitvinov@gmail.com>
2018-02-27 17:33:41 +0100

    add read_matrix

--- src/rbc/matrices/imp/main.h
+++ src/rbc/matrices/imp/main.h
@@ -1,5 +1,12 @@
 static const int MAX_N = 999999;
 
+static int read_matrix(FILE *f, double A[16]) {
+    int i;
+    for (i = 0; i < 16; i++)
+        if (fscanf(f, "%lf", &A[i]) != 1) return 0;
+    return 1;
+}
+
 void matrices_read(const char *path, /**/ Matrices **pq) {
     int n;
     FILE *f;
@@ -9,7 +16,7 @@ void matrices_read(const char *path, /**/ Matrices **pq) {
     UC(efopen(path, "r", /**/ &f));
     UC(efclose(f));
     n = 0;
-    //    while (read_matrix(f, /**/ q->m.) ) {
+    while (read_matrix(f, /**/ q->m[n++].D)) ;
     q->n = n;
 
     *pq = q;
