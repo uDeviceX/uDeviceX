eda2e2d523312b7f9f16f94c0944b87708a08d79
Sergey Litvinov <slitvinov@gmail.com>
2018-02-27 15:22:25 +0100

    initial import of Matrices

---
+++ src/rbc/matrices/imp.h
@@ -0,0 +1,11 @@
+struct Matrices;
+struct Coords;
+
+void matrices_ini_file(const char *path, /**/ Matrices**);
+void matrices_ini_filter(Matrices*, Coords*, /**/ Matrices**);
+
+int  matrices_get_n(Matrices*);
+void matrices_get(Matrices*, int i, /**/ double M[4*4]);
+void matrices_get_r(Matrices*, int i, /**/ double r[3]);
+
+void matrices_fin(Matrices*);
