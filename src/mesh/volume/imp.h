2fe79b0b4393e4f950f2de688409d9408bfbc080
Sergey Litvinov <slitvinov@gmail.com>
2018-02-27 09:25:19 +0100

    wip: volume.adoc

--- src/mesh/volume/imp.h
+++ src/mesh/volume/imp.h
@@ -3,9 +3,9 @@ struct Positions;
 struct MeshRead;
 
 // tag::interface[]
-void mesh_volume_ini(MeshRead*, /**/ MeshVolume**);
+void mesh_volume_ini(MeshRead*, /**/ MeshVolume**); // <1>
 void mesh_volume_fin(MeshVolume*);
 
-void  mesh_volume_apply(MeshVolume*, int nm, Positions*, /**/ double**);
-double mesh_volume_apply0(MeshVolume*, Positions*);
+double mesh_volume_apply0(MeshVolume*, Positions*);                      // <2>
+void  mesh_volume_apply(MeshVolume*, int nm, Positions*, /**/ double**); // <3>
 // end::interface[]
