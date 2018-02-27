96776d8984ab1b5781f33645deac511e32151497
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-02-27 18:32:27 +0100

    adding doc for meshbb

--- src/meshbb/imp.h
+++ src/meshbb/imp.h
@@ -5,19 +5,21 @@ struct Momentum;
 struct Solid;
 struct int4;
 
+// tag::mem[]
 void meshbb_ini(int maxpp, /**/ MeshBB **d);
 void meshbb_fin(/**/ MeshBB *d);
+// end::mem[]
+
 
 void meshbb_reini(int n, /**/ MeshBB *d);
-void meshbb_find_collisions(float dt,
-                            int nm, int nt, int nv, const int4 *tt, const Particle *i_pp, int3 L,
+void meshbb_find_collisions(float dt, int nm, int nt, int nv, const int4 *tt, const Particle *i_pp, int3 L,
                             const int *starts, const int *counts, const Particle *pp, const Force *ff, /**/ MeshBB *d);
-void meshbb_select_collisions(float dt,
-                              int n, /**/ MeshBB *d);
+void meshbb_select_collisions(float dt, int n, /**/ MeshBB *d);
 void meshbb_bounce(float dt, float mass,
                    int n, const MeshBB *d, const Force *ff, int nt, int nv, const int4 *tt,
                    const Particle *i_pp, /**/ Particle *pp, Momentum *mm);
-void meshbb_collect_rig_momentum(float dt,
-                                 int ns, int nt, int nv, const int4 *tt, const Particle *pp, const Momentum *mm, /**/ Solid *ss);
-void meshbb_collect_rbc_momentum(float dt,
-                                 int nc, int nt, int nv, const int4 *tt, const Particle *pp, const Momentum *mm, /**/ Force *ff);
+
+// tag::collect[]
+void meshbb_collect_rig_momentum(float dt, int ns, int nt, int nv, const int4 *tt, const Particle *pp, const Momentum *mm, /**/ Solid *ss);
+void meshbb_collect_rbc_momentum(float dt, int nc, int nt, int nv, const int4 *tt, const Particle *pp, const Momentum *mm, /**/ Force *ff);
+// end::collect[]
