a6e5b86cfa28175ebcb79e83f021776af4fe1bc4
Sergey Litvinov <slitvinov@gmail.com>
2018-02-27 13:48:58 +0100

    add a body of tri_hst::area_kahan

--- src/math/tri/dev.h
+++ src/math/tri/dev.h
@@ -11,6 +11,23 @@
 #endif
 
 BEGIN
+
+_S_ void swap(double *a, double *b) {
+    double t;
+    t = *a; *a = *b; *b = t;
+}
+_S_ int less(double *a, double *b) { return (*a) < (*b); }
+_S_ void  sort3(double *a, double *b, double *c) {
+    if (less(c, b)) swap(c, b);
+    if (less(b, a)) swap(b, a);
+    if (less(c, b)) swap(c, b);
+}
+
+_I_ double area_kahan(double a, double b, double c) {
+    sort3(&c, &b, &a); /* a > b > c */
+    return sqrt((a+(b+c))*(c-(a-b))*(c+(a-b))*(a+(b-c)))/4;
+}
+
 END
 
 #undef _I_
--- src/math/tri/imp.cpp
+++ src/math/tri/imp.cpp
@@ -1,5 +1,7 @@
+#include <math.h>
+
 #include "imp.h"
 
-#define FRAG_HOST
+#define TRI_HOST
 #include "dev.h"
-#undef FRAG_HOST
+#undef TRI_HOST
