e938e2e97e47497f852565f3b31c6b00fa06a650
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-27 10:44:39 +0100

    add static

--- post/common/pp_id.h
+++ post/common/pp_id.h
@@ -1,10 +1,10 @@
-int separator(int argc, char **argv) {
+static int separator(int argc, char **argv) {
     for (int i = 1; i < argc; ++i)
     if (strcmp("--", argv[i]) == 0) return i;
     return -1;
 }
 
-void read_data(const char *fpp, BopData *dpp, const char *fii, BopData *dii) {
+static void read_data(const char *fpp, BopData *dpp, const char *fii, BopData *dii) {
     char fdname[CBUFSIZE];
 
     bop_read_header(fpp, dpp, fdname);
@@ -19,13 +19,13 @@ void read_data(const char *fpp, BopData *dpp, const char *fii, BopData *dii) {
     if (dii->type != INT   && dii->type != IASCII) ERR("expected int data form <%s>\n", fii);
 }
 
-int max_index(const int *ii, const int n) {
+static int max_index(const int *ii, const int n) {
     int m = -1;
     for (int i = 0; i < n; ++i) m = m < ii[i] ? ii[i] : m;
     return m;
 }
 
-void pp2rr_sorted(const int *ii, const float *fdata, const int n, const int stride, /**/ float *rr) {
+static void pp2rr_sorted(const int *ii, const float *fdata, const int n, const int stride, /**/ float *rr) {
     for (int j = 0; j < n; ++j) {
         const int i = ii[j];
         const float *r = fdata + j * stride;
