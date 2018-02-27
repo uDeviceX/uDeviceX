6a0393a0c8528454866759187f54035a468e5209
Sergey Litvinov <slitvinov@gmail.com>
2018-02-27 17:20:28 +0100

    open/close file in matrices_read

--- src/rbc/matrices/imp/main.h
+++ src/rbc/matrices/imp/main.h
@@ -1,9 +1,17 @@
 static const int MAX_N = 999999;
 
-void matrices_read(const char*, /**/ Matrices **pq) {
+void matrices_read(const char *path, /**/ Matrices **pq) {
+    int n;
+    FILE *f;
     Matrices *q;
     EMALLOC(1, &q);
     EMALLOC(MAX_N, &q->m);
+    UC(efopen(path, "r", /**/ &f));
+    UC(efclose(f));
+    n = 0;
+    //    while (read_matrix(f, /**/ q->m.) ) {
+    q->n = n;
+
     *pq = q;
 }
 
