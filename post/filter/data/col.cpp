80d6bf507f15e0a33c33174c4b707254507bfa0e
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-28 12:14:56 +0100

    add 0 and 1 colors

--- post/filter/data/col.cpp
+++ post/filter/data/col.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-enum{N=8100};
+enum{
+    N = 8100,
+    T = 200
+};
 
 int main() {
     FILE *f = fopen("colors.values", "wb");
@@ -8,7 +11,7 @@ int main() {
     int cc[N], i;
     
     for (i = 0; i < N; ++i) {
-        cc[i] = 1;
+        cc[i] = i > T ? 1 : 0;
     }
     fwrite(cc, sizeof(int), N, f);
     
