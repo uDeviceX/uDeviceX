cfe65601f7b8ff68339c92cfe4a20a1e5aa6fbbc
amlucas <lucas.amoudruz@wanadoo.fr>
2018-02-28 11:56:00 +0100

    implement collect(first version)

--- post/filter/color.cpp
+++ post/filter/color.cpp
@@ -52,7 +52,8 @@ static void get_n(int tag, long n, const int *pred, long *nmatch) {
     *nmatch = j;
 }
 
-static void collect(int tag, long n, const int *pred, int stride, const float *in, float *out) {
+template <typename T>
+static void collect(int tag, long n, const int *pred, int stride, const T *in, T *out) {
     long i, j, c;
     for (i = j = 0; i < n; ++i) {
         if (pred[i] == tag) {
@@ -63,8 +64,73 @@ static void collect(int tag, long n, const int *pred, int stride, const float *i
     }
 }
 
-static void filter() {
-    
+static void filter(int tag, const BopData *cc, const BopData *pp, const BopData *ii,
+                   BopData *outpp, BopData *outii) {
+    long n, nout;
+    const int *pred;
+
+    BPC( bop_get_n(cc, &n) );
+    pred = (const int*) bop_get_data(cc);
+    get_n(tag, n, pred, /**/ &nout);
+
+    BPC( bop_set_n(nout, outpp) );
+    BPC( bop_set_vars(6, "x y z vx vy vz", outpp) );
+    BPC( bop_set_type(BopFLOAT, outpp) );
+    BPC( bop_alloc(outpp) );
+
+    const float *pp_in  = (const float*) bop_get_data(pp);
+    float       *pp_out =       (float*) bop_get_data(outpp);
+
+    collect(tag, n, pred, 6, pp_in, /**/ pp_out);
+
+    if (ii) {
+        BPC( bop_set_n(nout, outii) );
+        BPC( bop_set_vars(1, "ids", outii) );
+        BPC( bop_set_type(BopINT, outii) );
+        BPC( bop_alloc(outii) );
+
+        const int *ii_in  = (const int*) bop_get_data(ii);
+        int       *ii_out =       (int*) bop_get_data(outii);
+
+        collect(tag, n, pred, 1, ii_in, /**/ ii_out);        
+    }
+}
+
+static void read(Args a, BopData *cc, BopData *pp, BopData *ii) {
+    char fdname[FILENAME_MAX];
+
+    BPC( bop_read_header(a.cc, cc, fdname) );
+    BPC( bop_alloc(cc) );
+    BPC( bop_read_values(fdname, cc) );
+
+    BPC( bop_read_header(a.pp, pp, fdname) );
+    BPC( bop_alloc(pp) );
+    BPC( bop_read_values(fdname, pp) );
+
+    if (a.ii) {
+        BPC( bop_read_header(a.ii, ii, fdname) );
+        BPC( bop_alloc(ii) );
+        BPC( bop_read_values(fdname, ii) );
+    }
+}
+
+static void getfname(const char *base, const char *specific, char *name) {
+    strcpy(name, base);
+    strcat(name, specific);
+}
+
+static void write(Args a, const BopData *pp, const BopData *ii) {
+    char fname[FILENAME_MAX];
+
+    getfname(a.out, ".pp", fname);
+    BPC( bop_write_header(fname, pp) );
+    BPC( bop_write_values(fname, pp) );
+
+    if (a.ii) {
+        getfname(a.out, ".ii", fname);
+        BPC( bop_write_header(fname, ii) );
+        BPC( bop_write_values(fname, ii) );
+    }
 }
 
 int main(int argc, char **argv) {
@@ -73,17 +139,27 @@ int main(int argc, char **argv) {
     
     parse(argc, argv, /**/ &a);
 
-    BPC( bop_ini(&pp) );
     BPC( bop_ini(&cc) );
-    if (a.ii)
+    BPC( bop_ini(&pp) );
+    BPC( bop_ini(&outpp) );
+    if (a.ii) {
         BPC( bop_ini(&ii) );
+        BPC( bop_ini(&outii) );
+    }
 
+    read(a, /**/ cc, pp, ii);
+
+    filter(a.color, cc, pp, ii, /**/ outpp, outii);
+
+    write(a, outpp, outii);
     
-    
+    BPC( bop_fin(cc) );    
     BPC( bop_fin(pp) );
-    BPC( bop_fin(cc) );
-    if (a.ii)
+    BPC( bop_fin(outpp) );
+    if (a.ii) {
         BPC( bop_fin(ii) );
+        BPC( bop_fin(outii) );
+    }
     
     return 0;
 }
