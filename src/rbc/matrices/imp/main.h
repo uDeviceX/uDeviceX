e41b91832bde1ac0fdcebde7bd789345bb461c07
Sergey Litvinov <slitvinov@gmail.com>
2018-02-28 09:48:16 +0100

    count number of matrices

--- src/rbc/matrices/imp/main.h
+++ src/rbc/matrices/imp/main.h
@@ -41,17 +41,19 @@ static int good(const Coords *c, const double A[16]) {
         zlo(c) < r[Z] && r[Z] <= zhi(c);
 }
 void matrices_read_filter(const char *path, const Coords *c, /**/ Matrices **pq) {
-    int n;
+    int m, n;
     FILE *f;
     Matrices *q;
     EMALLOC(1, &q);
     EMALLOC(MAX_N, &q->m);
     UC(efopen(path, "r", /**/ &f));
-    n = 0;
+    n = m = 0;
     while (read_matrix(f, /**/ q->m[n].D)) {
+        m++;
         if (good(c, q->m[n].D)) n++;
         if (n > MAX_N) ERR("n=%d > MAX_N=%d", n, MAX_N);
     }
+    msg_print("took %d/%d matrices", n, m);
     UC(efclose(f));
     q->n = n;
     *pq = q;
