2a74b59f5fcc8594474e303a4a3618665420a41f
Sergey Litvinov <slitvinov@gmail.com>
2018-02-28 09:51:34 +0100

    initial import of u/rbc/gen

---
+++ src/u/rbc/gen/make/dir.mk
@@ -0,0 +1,11 @@
+D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
+    d $B/algo/kahan_sum && \
+    d $B/conf           && \
+    d $B/d              && \
+    d $B/io/mesh_read   && \
+    d $B/math/tri       && \
+    d $B/mesh/area      && \
+    d $B/mesh/positions && \
+    d $B/mpi            && \
+    d $B/u/rbc/gen      && \
+    d $B/utils         
