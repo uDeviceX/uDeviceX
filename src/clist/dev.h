5ef0e2ec76bf9972aa43a775f14af2ccec6e80ef
amlucas <lucas.amoudruz@wanadoo.fr>
2018-03-01 10:40:29 +0100

    use _I_ and _S_

--- src/clist/dev.h
+++ src/clist/dev.h
@@ -1,7 +1,9 @@
+#define _I_ static __device__
+
 enum { CLIST_NMAX = (256*256*256) };
 
 // tag::int[]
-static __device__ uint clist_encode_id(int s, int pid)
+_I_ uint clist_encode_id(int s, int pid)
 // end::int[]
 {
     /* pid: particle id; s: array id */
@@ -10,10 +12,12 @@ static __device__ uint clist_encode_id(int s, int pid)
 }
 
 // tag::int[]
-static __device__ void clist_decode_id(uint c, /**/ int *s, int *pid)
+_I_ void clist_decode_id(uint c, /**/ int *s, int *pid)
 // end::int[]
 {
     /* pid: particle id; s: array id; c : code */
     *pid = c % CLIST_NMAX;
     *s   = c / CLIST_NMAX;
 }
+
+#undef _I_
