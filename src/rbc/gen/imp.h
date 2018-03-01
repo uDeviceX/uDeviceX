77f0ec4c5cc52f7a49e19dc1db7a55f3eac3bb39
Sergey Litvinov <slitvinov@gmail.com>
2018-03-01 09:42:02 +0100

    fill Particle* not return Particle*

--- src/rbc/gen/imp.h
+++ src/rbc/gen/imp.h
@@ -5,4 +5,4 @@ struct Matrices;
 /* vv : vertices of a template : x y z x y z, ...   */
 int rbc_gen(const Coords*, const float *vv, const char *ic, int nv, /**/ Particle*);
 
-void rbc_gen0(const Coords*, int nv, const float *vv, const Matrices*, /**/ int *pn, Particle**);
+void rbc_gen0(const Coords*, int nv, const float *vv, const Matrices*, /**/ int *pn, Particle*);
