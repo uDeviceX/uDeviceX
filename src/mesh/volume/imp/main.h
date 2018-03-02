8eeb7cfb2b2f94e35b9dc477992cb001ab781aae
Sergey Litvinov <slitvinov@gmail.com>
2018-03-02 11:48:52 +0100

    fix a bug with offset in mesh/volume

--- src/mesh/volume/imp/main.h
+++ src/mesh/volume/imp/main.h
@@ -58,13 +58,13 @@ static double M0(double a[3], double b[3], double c[3]) {
              -a[Y]*(b[X]*c[Z]-b[Z]*c[X])
              +a[Z]*(b[X]*c[Y]-b[Y]*c[X]));
 }
-static void get(Positions *rr, int i, /**/ double *r1) {
+static void get(double *rr, int i, /**/ double *r1) {
     enum {X, Y, Z};
-    float r0[3];
-    UC(positions_get(rr, i, /**/ r0));
+    double *r0;
+    r0 = &rr[3*i];
     r1[X] = r0[X]; r1[Y] = r0[Y]; r1[Z] = r0[Z];
 }
-static double volume(int nt, int4 *tt, Positions *rr) {
+static double volume(int nt, int4 *tt, double *rr) {
     enum {X, Y, Z};
     int i, ia, ib, ic;
     double a[3], b[3], c[3], sum;
@@ -86,7 +86,7 @@ double mesh_volume_apply0(MeshVolume *q, Positions *positions) {
     int4 *tt;
     nv = q->nv; nt = q->nt; rr = q->rr; tt = q->tt; offset = 0;
     UC(to_com(nv, offset, positions, /**/ rr));
-    return volume(nt, tt, positions);
+    return volume(nt, tt, rr);
 }
 
  void mesh_volume_apply(MeshVolume *q, int m, Positions *positions, double *volume0) {
@@ -96,7 +96,7 @@ double mesh_volume_apply0(MeshVolume *q, Positions *positions) {
     nv = q->nv; nt = q->nt; rr = q->rr; tt = q->tt; offset = 0;    
     for (i = 0; i < m; i++) {
         UC(to_com(nv, offset, positions, /**/ rr));
-        UC(volume0[i] = volume(nt, tt, positions));
+        UC(volume0[i] = volume(nt, tt, rr));
         offset += nv;
     }
 }
