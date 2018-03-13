255ed04fdde7e66f29606073d84f1f7464f8f8f2
Sergey Litvinov <slitvinov@gmail.com>
2018-03-13 10:49:52 +0100

    create imp/main.h

--- src/algo/edg/README.md
+++
@@ -1,7 +0,0 @@
-# edge
-
-set and get `val` on edges
-
-* `md` maximum degree
-* `md` number of vertices
-* `h[xy]` storage of size `nv*md`
--- src/algo/edg/imp.cpp
+++ src/algo/edg/imp.cpp
@@ -4,28 +4,4 @@
 #include "utils/error.h"
 #include "imp.h"
 
-void edg_ini(int md, int nv, /**/ int *hx) {
-    int n, i;
-    n = md * nv;
-    for (i = 0; i < n; i ++) hx[i] = -1;
-}
-
-void edg_set(int md, int f, int x, int y,  /**/ int *hx, int *hy) {
-    int d, j;
-    j = f*md; d = 0;
-    while (hx[j] != -1) {
-        j++; d++;
-        if (d >= md) ERR("invalid edg_set call: %d/%d/%d/%d/%d", md, f, x, y, j);
-    }
-    hx[j] = x; hy[j] = y;
-}
-
-int edg_get(int md, int i, int x, const int *hx, const int *hy) { /* next */
-    int d;
-    i *= md; d = 0;
-    while (hx[i] != x) {
-        i++; d++;
-        if (d >= md) ERR("invalid edg_get call: %d/%d/%d", md, i, x);
-    }
-    return hy[i];
-}
+#include "imp/main.h"
