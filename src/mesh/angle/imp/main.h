3f77b2af18e9acf00666b78b0be71406a4928a0e
Sergey Litvinov <slitvinov@gmail.com>
2018-03-13 15:33:23 +0100

    clean up angle/imp

--- src/mesh/angle/imp/main.h
+++ src/mesh/angle/imp/main.h
@@ -1,4 +1,4 @@
-struct Q { int4 *d; };
+struct Q { int4 *d; }; /* queue */
 static void q_push(Q *q, int i0, int i1, int i2, int i3) {
     int4 *d;
     d = q->d;
@@ -49,42 +49,14 @@ void mesh_angle_ini(MeshRead *mesh, MeshAngle **pq) {
     UC(nd = mesh_read_get_ne(mesh));
     UC(md = mesh_read_get_md(mesh));
     EMALLOC(1, &q);
-    EMALLOC(nt, &q->tt);
     EMALLOC(nd, &q->dd);
     UC(ini_dd(nt, mesh_read_get_tri(mesh), nv, md, /**/ q->dd));
-    q->nv = nv; q->nt = nt; q->nd = nd;
-    EMEMCPY(nt, mesh_read_get_tri(mesh), q->tt);
-
+    q->nd = nd;
     *pq = q;
 }
 
 void mesh_angle_fin(MeshAngle *q) {
-    EFREE(q->dd); EFREE(q->tt); EFREE(q);
+    EFREE(q->dd); EFREE(q);
 }
-static void get(Positions *p, int i, double d[3]) {
-    enum {X, Y, Z};
-    float f[3];
-    UC(positions_get(p, i, /**/ f));
-    d[X] = f[X]; d[Y] = f[Y]; d[Z] = f[Z];
-}
-static double area(int4 t, Positions *p, int offset) {
-    int ia, ib, ic;
-    double a[3], b[3], c[3];
-    ia = t.x; ib = t.y; ic = t.z;
-    UC(get(p, ia + offset, /**/ a));
-    UC(get(p, ib + offset, /**/ b));
-    UC(get(p, ic + offset, /**/ c));
-    return tri_hst::kahan_area(a, b, c);
-}
-
-void mesh_angle_apply(MeshAngle *q, int m, Positions *p, double *area0) {
-    int i, j, k, nt, nv, offset;
-    int4 *tt;
-    nt = q->nt; tt = q->tt; nv = q->nv; offset = 0;
-
-    for (k = i = 0; i < m; i++) {
-        for (j = 0; j < nt; j++)
-            area0[k++] = area(tt[j], p, offset);
-        offset += nv;
-    }
+void mesh_angle_apply(MeshAngle*, int, Positions*, /**/ double*) {
 }
