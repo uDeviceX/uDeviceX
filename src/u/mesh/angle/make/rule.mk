9d4eda15c217af09d144df254e0bf9cb8a3fc5c0
Sergey Litvinov <slitvinov@gmail.com>
2018-03-13 14:11:42 +0100

    initial import of mesh/angle

---
+++ src/u/mesh/angle/make/rule.mk
@@ -0,0 +1,17 @@
+$B/algo/kahan_sum/imp.o: $S/algo/kahan_sum/imp.cpp; $X -I$S/algo/kahan_sum -I$S/.
+$B/conf/imp.o: $S/conf/imp.cpp; $X -I$S/conf -I$S/.
+$B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
+$B/io/mesh_read/imp.o: $S/io/mesh_read/imp.cpp; $X -I$S/io/mesh_read -I$S/.
+$B/math/tri/imp.o: $S/math/tri/imp.cpp; $X -I$S/math/tri -I$S/.
+$B/mesh/area/imp.o: $S/mesh/area/imp.cpp; $X -I$S/mesh/area -I$S/.
+$B/mesh/positions/imp.o: $S/mesh/positions/imp.cpp; $X -I$S/mesh/positions -I$S/.
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
+$B/u/mesh/area/main.o: $S/u/mesh/area/main.cpp; $X -I$S/.
+$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
+$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/utils -I$S/.
+$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
+$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/utils -I$S/.
+$B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
