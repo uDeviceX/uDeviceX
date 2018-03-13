a556466c4b35b6c05ab17bae40be7b736f0017da
Sergey Litvinov <slitvinov@gmail.com>
2018-03-13 10:48:32 +0100

    initial import of edg.adoc

--- src/algo/edg/imp.h
+++ src/algo/edg/imp.h
@@ -1,3 +1,5 @@
+// tag::interface[]
 void edg_ini(int md, int nv,                     int *hx);
-void edg_set(int md, int i, int j, int val, /**/ int *hx,       int *hy);
-int  edg_get(int md, int i, int j,         const int *hx, const int *hy);
+void edg_set(int md, int i, int j, int val, /**/ int *hx,       int *hy); // <1>
+int  edg_get(int md, int i, int j,         const int *hx, const int *hy); // <2>
+// end::interface
