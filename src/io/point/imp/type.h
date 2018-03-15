fd1df15851abcf0bf8f3b28ba75e05a376b1d5bd
Sergey Litvinov <slitvinov@gmail.com>
2018-03-15 17:11:08 +0100

    add int

--- src/io/point/imp/type.h
+++ src/io/point/imp/type.h
@@ -3,8 +3,11 @@
 struct IOPointConf {
     int nn[N_MAX];
     char keys[N_MAX][FILENAME_MAX];
+    int i;
 };
 
 struct IOPoint {
+    int maxn;
     IOPointConf c;
+    char path[FILENAME_MAX];
 };
