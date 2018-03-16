d1b300afef26fba55758bc3276e2e44de56389a7
Sergey Litvinov <slitvinov@gmail.com>
2018-03-16 17:02:05 +0100

    split tags

--- src/io/point/imp.h
+++ src/io/point/imp.h
@@ -1,13 +1,15 @@
 struct IOPointConf;
 struct IOPoint;
 
-// tag::interface[]
+// tag::conf[]
 void io_point_conf_ini(/**/ IOPointConf**);
 void io_point_conf_fin(IOPointConf*);
 void io_point_conf_push(IOPointConf*, const char *keys);
+// end::conf[]
 
+// tag::point[]
 void io_point_ini(int maxn, const char *path, IOPointConf*, /**/ IOPoint**);
 void io_point_fin(IOPoint*);
 void io_point_push(IOPoint*, int n, const double *data, const char *keys);
 void io_point_write(IOPoint*, MPI_Comm comm, int id);
-// end::interface[]
+// end::point[]
