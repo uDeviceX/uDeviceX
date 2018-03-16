99089fa7389efcdded190ce0e4f352454ce393ad
Sergey Litvinov <slitvinov@gmail.com>
2018-03-16 09:09:48 +0100

    clean up u/io/point

--- src/u/io/point/make/dir.mk
+++ src/u/io/point/make/dir.mk
@@ -1,11 +1,8 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/conf         && \
-    d $B/coords       && \
-    d $B/d            && \
-    d $B/io/field     && \
-    d $B/io/field/h5  && \
-    d $B/io/field/xmf && \
-    d $B/io/point     && \
-    d $B/mpi          && \
-    d $B/u/io/point   && \
-    d $B/utils       
+    d $B/conf       && \
+    d $B/coords     && \
+    d $B/d          && \
+    d $B/io/point   && \
+    d $B/mpi        && \
+    d $B/u/io/point && \
+    d $B/utils     
