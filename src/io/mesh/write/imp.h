2f40ea226320d038f3f2b0e1b177c3da5eac4352
Sergey Litvinov <slitvinov@gmail.com>
2018-03-17 22:19:17 +0100

    minor

--- src/io/mesh/write/imp.h
+++ src/io/mesh/write/imp.h
@@ -1,12 +1,12 @@
 struct WriteFile;
 
-int write_file_open(MPI_Comm, const char*, /**/ WriteFile**);
+int write_file_open(MPI_Comm, const char *path, /**/ WriteFile**);
 int write_file_close(WriteFile*);
 
-void write_all(MPI_Comm cart, const void *const, const int sz, WriteFile*);
-int write_master(MPI_Comm cart, const void *const, int sz, WriteFile*);
+void write_all(MPI_Comm, const void *const, const int sz, WriteFile*);
+int write_master(MPI_Comm, const void *const, int sz, WriteFile*);
 
-int write_shift_indices(MPI_Comm cart, int, /**/ int*);
-int write_reduce(MPI_Comm cart, int, /**/ int*);
+int write_shift_indices(MPI_Comm, int, /**/ int*);
+int write_reduce(MPI_Comm, int, /**/ int*);
 
 
