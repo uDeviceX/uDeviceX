2e5a5db1e7f36cbc0d623cd87ad86da3a3baca10
Sergey Litvinov <slitvinov@gmail.com>
2018-03-17 20:42:40 +0100

    u/mesh/tri_area is a copy of u/mesh/angle

---
+++ src/u/mesh/tri_area/make/dir.mk
@@ -0,0 +1,13 @@
+D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
+    d $B/algo/edg       && \
+    d $B/algo/kahan_sum && \
+    d $B/conf           && \
+    d $B/d              && \
+    d $B/io/mesh_read   && \
+    d $B/math/tri       && \
+    d $B/mesh/angle     && \
+    d $B/mesh/angle/edg && \
+    d $B/mesh/positions && \
+    d $B/mpi            && \
+    d $B/u/mesh/angle   && \
+    d $B/utils         
