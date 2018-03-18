35f9509d8dddee002213cc3551426868d288e041
Sergey Litvinov <slitvinov@gmail.com>
2018-03-18 14:34:01 +0100

    declare positions_edge and positions_center

--- src/mesh/vectors/imp.h
+++ src/mesh/vectors/imp.h
@@ -3,7 +3,11 @@ struct Vectors;
 
 // tag::interface[]
 void vectors_float_ini(int n, const float*, /**/ Vectors**);
+
 void vectors_postions_ini(int n, const Particle*, /**/ Vectors**);
+void vectors_postions_edge(int n, const Particle*, /**/ Vectors**);
+void vectors_postions_center(int n, const Particle*, /**/ Vectors**);
+
 void vectors_velocities_ini(int n, const Particle*, /**/ Vectors**);
 void vectors_zero_ini(int n, /**/ Vectors**);
 
