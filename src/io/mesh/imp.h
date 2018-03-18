892836ff821f8e9871d182d26dcb5c233d8c110e
Sergey Litvinov <slitvinov@gmail.com>
2018-03-18 17:37:36 +0100

    use vectors

--- src/io/mesh/imp.h
+++ src/io/mesh/imp.h
@@ -3,10 +3,12 @@ struct Particle;
 struct int4;
 struct MeshWrite;
 struct MeshRead;
+struct Vectors;
 
 void mesh_write_ini(const int4 *tt, int nv, int nt, const char *directory, /**/ MeshWrite**);
 void mesh_write_ini_off(MeshRead*, const char *directory, /**/ MeshWrite**);
 
 void mesh_write_particles(MeshWrite*, MPI_Comm, const Coords*, int nm, const Particle*, int id);
+void mesh_write_vectros(MeshWrite*, MPI_Comm, int nm, Vectors *pos, Vectors *vel, int id);
 
 void mesh_write_fin(MeshWrite*);
