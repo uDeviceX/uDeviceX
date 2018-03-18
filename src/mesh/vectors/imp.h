765d0ee7d0d8e440f92de2cf84f7ddf45acadc46
Sergey Litvinov <slitvinov@gmail.com>
2018-03-18 14:36:15 +0100

    positions_edge and positions_center take Coords

--- src/mesh/vectors/imp.h
+++ src/mesh/vectors/imp.h
@@ -1,13 +1,12 @@
 struct Particle;
 struct Vectors;
+struct Coords;
 
 // tag::interface[]
 void vectors_float_ini(int n, const float*, /**/ Vectors**);
-
 void vectors_postions_ini(int n, const Particle*, /**/ Vectors**);
-void vectors_postions_edge(int n, const Particle*, /**/ Vectors**);
-void vectors_postions_center(int n, const Particle*, /**/ Vectors**);
-
+void vectors_postions_edge  (Coords*, int n, const Particle*, /**/ Vectors**);
+void vectors_postions_center(Coords*, int n, const Particle*, /**/ Vectors**);
 void vectors_velocities_ini(int n, const Particle*, /**/ Vectors**);
 void vectors_zero_ini(int n, /**/ Vectors**);
 
