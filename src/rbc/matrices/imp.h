90239d138d21b45665311f88f57be974ab8c9b4f
Sergey Litvinov <slitvinov@gmail.com>
2018-03-19 14:59:22 +0100

    add tag

--- src/rbc/matrices/imp.h
+++ src/rbc/matrices/imp.h
@@ -2,13 +2,13 @@ struct Matrices;
 struct Coords;
 
 // tag::interface[]
-void matrices_read(const char *path, /**/ Matrices**);
-void matrices_read_filter(const char *path, const Coords*, /**/ Matrices**);
+void matrices_read(const char *path, /**/ Matrices**); // <1>
+void matrices_read_filter(const char *path, const Coords*, /**/ Matrices**); // <2>
 
-void matrices_get(const Matrices*, int i, /**/ double**);
-void matrices_get_r(const Matrices*, int i, /**/ double r[3]);
-int  matrices_get_n(const Matrices*);
-void matrices_log(const Matrices*);
+void matrices_get(const Matrices*, int i, /**/ double**); // <3>
+void matrices_get_r(const Matrices*, int i, /**/ double r[3]); // <4>
+int  matrices_get_n(const Matrices*); // <5>
+void matrices_log(const Matrices*);   // <6>
 
 void matrices_fin(Matrices*);
 // end::interface[]
