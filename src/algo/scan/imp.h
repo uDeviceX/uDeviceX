07220759066ad30f4b807e423cefa92177ccf556
Sergey Litvinov <slitvinov@gmail.com>
2018-03-20 11:38:39 +0100

    minor

--- src/algo/scan/imp.h
+++ src/algo/scan/imp.h
@@ -1,7 +1,7 @@
 struct Scan;
 
 // tag::interface[]
-void scan_apply(const int *input, int size, /**/ int *output, /*w*/ Scan *); // <1>
-void scan_ini(int size, /**/ Scan **);                                       // <2>
-void scan_fin(Scan*);                                                        // <3>
+void scan_apply(const int *input, int size, /**/ int *output, /*w*/ Scan*); // <1>
+void scan_ini(int size, /**/ Scan**);                                       // <2>
+void scan_fin(Scan*);                                                       // <3>
 // end::interface[]
