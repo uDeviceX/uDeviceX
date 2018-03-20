85f2e4b30dc07a615636dd8f253babdc873dda43
Sergey Litvinov <slitvinov@gmail.com>
2018-03-20 11:10:20 +0100

    minor

--- src/algo/edg/imp.h
+++ src/algo/edg/imp.h
@@ -3,4 +3,4 @@ void edg_ini(int md, int nv,                     int *hx);
 void edg_set(int md, int i, int j, int val, /**/ int *hx,       int *hy); // <1>
 int  edg_get(int md, int i, int j,         const int *hx, const int *hy); // <2>
 int  edg_valid(int md, int i, int j,         const int *hx); // <3>
-// end::interface
+// end::interface[]
