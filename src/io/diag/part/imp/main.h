ad4e9060317b4e3f6b42e4d891264e2f61c59d10
Sergey Litvinov <slitvinov@gmail.com>
2018-03-22 14:02:52 +0100

    minor

--- src/io/diag/part/imp/main.h
+++ src/io/diag/part/imp/main.h
@@ -31,7 +31,7 @@ static int sum_i(MPI_Comm comm, int *v) {
 }
 void diag_part_apply(DiagPart *q, MPI_Comm comm, float time, int n, const Particle *pp) {
     if (time < 0) ERR("time = %g < 0", time);
-    if (q->stamp != STAMP_GOOD) ERR("'q' was not initilized");
+    if (q->stamp != STAMP_GOOD) ERR("'q' was not initialized");
     enum {X, Y, Z};
     int i, c;
     double k, km, ke; /* particle, total, and maximum kinetic energies */
