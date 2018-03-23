7fb85ff8c55ef2e7d32de4ac9284d6a716046f1b
Sergey Litvinov <slitvinov@gmail.com>
2018-03-23 11:39:04 +0100

    initial import of KeyList

--- src/algo/key_list/imp/type.h
+++ src/algo/key_list/imp/type.h
@@ -1,1 +1,8 @@
-struct KeyList { };
+enum {MAX_NK = 100 };
+
+struct KeyList {
+    int nk; /* number of keys */
+    int  ww[MAX_NK]; /* width */
+    int  mark[MAX_NK];
+    char keys[MAX_NK][FILENAME_MAX];
+};
