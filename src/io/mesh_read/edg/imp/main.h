4bcc158ac35fe92b4c73de3cdc9c3bc6c8c806b2
Sergey Litvinov <slitvinov@gmail.com>
2018-03-25 13:03:51 +0200

    copy edg to mesh_read

---
+++ src/io/mesh_read/edg/imp/main.h
@@ -0,0 +1,35 @@
+void e_ini(int md, int nv, /**/ Edg **pq) {
+    Edg *q;
+    EMALLOC(1, &q);
+    EMALLOC(md*nv, &q->hx);
+    EMALLOC(md*nv, &q->hy);
+    edg_ini(md, nv, /**/ q->hx);
+    q->md = md; q->nv = nv;
+    *pq = q;
+}
+
+void e_fin(Edg *q) {
+    EFREE(q->hx);
+    EFREE(q->hy);
+    EFREE(q);
+}
+
+void e_set(Edg *q, int i, int j, int val) {
+    int md, *hx, *hy;
+    md = q->md; hx = q->hx; hy = q->hy;
+    UC(edg_set(md, i, j, val, hx, hy));
+}
+
+int e_get(Edg *q, int i, int j) {
+    int md;
+    const int *hx, *hy;
+    md = q->md; hx = q->hx; hy = q->hy;
+    return edg_get(md, i, j, hx, hy);
+}
+
+int e_valid(Edg *q, int i, int j) {
+    int md;
+    const int *hx;
+    md = q->md; hx = q->hx;
+    return edg_valid(md, i, j, hx);
+}
