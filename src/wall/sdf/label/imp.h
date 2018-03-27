73f10fa78a8fb7d8d0bbf48126f13afd52721c5c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-03-27 13:46:14 +0200

    doc: sdf/label

--- src/wall/sdf/label/imp.h
+++ src/wall/sdf/label/imp.h
@@ -1,7 +1,12 @@
 enum {
+    // tag::enum[]
     LABEL_BULK,  /* remains in bulk */
     LABEL_WALL,  /* becomes wall particle */
     LABEL_DEEP   /* deep inside the wall */
+    // end::enum[]
 };
-void wall_label_dev(const Sdf *sdf, int n, const Particle*, /**/ int *labels);
-void wall_label_hst(const Sdf *sdf, int n, const Particle*, /**/ int *labels);
+
+// tag::int[]
+void wall_label_dev(const Sdf *sdf, int n, const Particle*, /**/ int *labels); // <1>
+void wall_label_hst(const Sdf *sdf, int n, const Particle*, /**/ int *labels); // <2>
+// end::int[]
