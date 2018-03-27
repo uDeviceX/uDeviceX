845552da061585c1a19710f86d6b89dc9b3e40fc
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-03-27 14:00:12 +0200

    doc: sdf/tform

--- src/wall/sdf/tform/imp.h
+++ src/wall/sdf/tform/imp.h
@@ -1,7 +1,10 @@
 struct Tform;
 struct Coords;
 
+// tag::int[]
 /* T: texture size, N: sdf file size, M: wall margin */
-void tex2sdf_ini(const Coords*, const int T[3], const int N[3], const int M[3], /**/ Tform*);
-void out2sdf_ini(const Coords*, const int N[3], /**/ Tform*);
-void sub2tex_ini(const Coords*, const int T[3], const int M[3], /**/ Tform*);
+void tex2sdf_ini(const Coords*, const int T[3], const int N[3], const int M[3], /**/ Tform*); // <1>
+void out2sdf_ini(const Coords*, const int N[3], /**/ Tform*); // <2>
+void sub2tex_ini(const Coords*, const int T[3], const int M[3], /**/ Tform*); // <3>
+// end::int[]
+
