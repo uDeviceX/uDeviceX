4de35f4d837452694a3f6ce5216970744ecdada1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-03-27 12:32:12 +0200

    mv bounce doc to submodule of sdf

--- src/wall/sdf/bounce/imp.h
+++ src/wall/sdf/bounce/imp.h
@@ -3,4 +3,7 @@ struct WvelStep;
 struct Coords;
 struct Particle;
 
+// tag::int[]
 void bounce_back(float dt, const WvelStep *wv, const Coords *c, const Sdf *sdf, int n, /**/ Particle *pp);
+// end::int[]
+
