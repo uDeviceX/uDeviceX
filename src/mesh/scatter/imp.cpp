1b9d14ce88444d5e2f109f126a5b76bbfa5ae16a
Sergey Litvinov <slitvinov@gmail.com>
2018-03-27 15:07:02 +0200

    add imp.cpp

---
+++ src/mesh/scatter/imp.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "utils/imp.h"
+#include "utils/error.h"
+#include "utils/msg.h"
+
+#include "io/mesh_read/imp.h"
+#include "mesh/scalars/imp.h"
+
+#include "imp.h"
+#include "imp/type.h"
+#include "imp/main.h"
