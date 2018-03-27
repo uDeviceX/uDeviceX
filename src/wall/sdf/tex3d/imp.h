ca68df1d0528d1378b21f2e61db5f1b0ed1c104f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-03-27 13:54:18 +0200

    doc: sdf/tex3d

--- src/wall/sdf/tex3d/imp.h
+++ src/wall/sdf/tex3d/imp.h
@@ -1,7 +1,13 @@
 struct Tex3d;
 struct Tex3d_v;
 struct Array3d;
+
+// tag::mem[]
 void tex3d_ini(Tex3d**);
 void tex3d_fin(Tex3d*);
-void tex3d_copy(const Array3d*, /**/ Tex3d*);
-void tex3d_to_view(const Tex3d*, /**/ Tex3d_v*);
+// end::mem[]
+
+// tag::int[]
+void tex3d_copy(const Array3d*, /**/ Tex3d*);    // <1>
+void tex3d_to_view(const Tex3d*, /**/ Tex3d_v*); // <2>
+// end::int[]
--- src/wall/sdf/tex3d/type.h
+++ src/wall/sdf/tex3d/type.h
@@ -1,2 +1,2 @@
-struct Tex3d_v { cudaTextureObject_t t; };
-struct Tex3d   { cudaTextureObject_t t; };
+struct Tex3d_v { cudaTextureObject_t t; }; // <1>
+struct Tex3d   { cudaTextureObject_t t; }; // <2>
