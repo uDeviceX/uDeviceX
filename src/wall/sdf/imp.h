d4bc85495604b267e26f8a4048b563056b81c3d5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-03-27 11:40:37 +0200

    document sdf

--- src/wall/sdf/imp.h
+++ src/wall/sdf/imp.h
@@ -5,11 +5,21 @@ struct Coords;
 struct Particle;
 struct int3;
 
+// tag::mem[]
 void sdf_ini(int3 L, Sdf**);
 void sdf_fin(Sdf*);
-void sdf_gen(const Coords*, MPI_Comm cart, bool dump, Sdf*);
-void sdf_to_view(const Sdf*, /**/ Sdf_v*);
+// end::mem[]
 
-void sdf_bulk_wall(const Sdf*, /*io*/ int *s_n, Particle *s_pp, /*o*/ int *w_n, Particle *w_pp);
-int  sdf_who_stays(const Sdf*, int n, const Particle*, int nc, int nv, /**/ int *stay);
+// tag::int[]
+void sdf_gen(const Coords*, MPI_Comm cart, bool dump, Sdf*); // <1>
+void sdf_to_view(const Sdf*, /**/ Sdf_v*);                   // <2>
+// end::int[]
+
+// tag::tools[]
+void sdf_bulk_wall(const Sdf*, /*io*/ int *s_n, Particle *s_pp, /*o*/ int *w_n, Particle *w_pp); // <1>
+int  sdf_who_stays(const Sdf*, int n, const Particle*, int nc, int nv, /**/ int *stay);          // <2>
+// end::tools[]
+
+// tag::bounce[]
 void sdf_bounce(float dt, const WvelStep*, const Coords*, const Sdf*, int n, /**/ Particle*);
+// end::bounce[]
