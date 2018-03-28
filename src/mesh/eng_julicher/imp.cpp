0125001b5c65fb7a8df294054ba1fc0f382b6ffa
Sergey Litvinov <slitvinov@gmail.com>
2018-03-28 17:29:41 +0200

    add mesh/eng_julicher

---
+++ src/mesh/eng_julicher/imp.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <vector_types.h>
+
+#include "utils/imp.h"
+#include "utils/error.h"
+#include "utils/msg.h"
+
+#include "io/mesh_read/imp.h"
+#include "algo/vectors/imp.h"
+
+#include "math/tri/imp.h"
+
+#include "imp.h"
+#include "imp/type.h"
+#include "imp/main.h"
---
+++ src/mesh/eng_julicher/imp/type.h
@@ -0,0 +1,4 @@
+struct MeshEngJulicher {
+    int4 *tt;
+    int nv, nt;
+};
