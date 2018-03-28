320a533ef4b0eaea525cc104101c7b8c37a322c9
Sergey Litvinov <slitvinov@gmail.com>
2018-03-28 17:31:51 +0200

    add u/mesh/eng_julicher

---
+++ src/u/mesh/eng_julicher/make/dep.mk
@@ -0,0 +1,23 @@
+$B/algo/edg/imp.o: $S/utils/error.h $S/algo/edg/imp.h $S/algo/edg/imp/main.h
+$B/algo/vectors/imp.o: $S/utils/imp.h $S/utils/error.h $S/inc/type.h $S/algo/vectors/imp.h $S/algo/vectors/imp/main.h $S/algo/vectors/imp/type.h $S/math/tform/imp.h $S/coords/imp.h
+$B/conf/imp.o: $S/utils/imp.h $S/utils/error.h $S/conf/imp/set.h $S/conf/imp.h $S/conf/imp/main.h $S/conf/imp/type.h $S/conf/imp/get.h $S/utils/msg.h
+$B/coords/conf.o: $S/utils/imp.h $S/coords/ini.h $S/utils/error.h $S/conf/imp.h
+$B/coords/imp.o: $S/utils/imp.h $S/inc/conf.h $S/coords/ini.h $S/utils/error.h $S/mpi/wrapper.h $S/coords/imp.h $S/utils/mc.h $B/conf.h $S/coords/imp/main.h $S/coords/imp/type.h $S/coords/type.h
+$B/d/api.o: $S/d/cpu/imp.h $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/d/common.h $S/d/api.h $B/conf.h $S/d/cuda/imp.h
+$B/io/mesh_read/edg/imp.o: $S/utils/imp.h $S/utils/error.h $S/io/mesh_read/edg/imp/main.h $S/algo/edg/imp.h $S/io/mesh_read/edg/imp/type.h $S/utils/msg.h
+$B/io/mesh_read/imp.o: $S/utils/imp.h $S/utils/error.h $S/io/mesh_read/imp/ply.h $S/io/mesh_read/imp.h $S/io/mesh_read/imp/main.h $S/io/mesh_read/imp/type.h $S/io/mesh_read/edg/imp.h $S/io/mesh_read/imp/off.h $S/utils/msg.h
+$B/io/write/imp.o: $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/mpi/wrapper.h $S/io/write/imp.h $S/utils/mc.h $B/conf.h $S/io/write/imp/main.h $S/io/write/imp/type.h
+$B/math/tform/imp.o: $S/utils/imp.h $S/inc/conf.h $S/utils/error.h $S/math/tform/imp.h $B/conf.h $S/math/tform/imp/main.h $S/math/tform/imp/type.h $S/math/tform/type.h $S/utils/msg.h
+$B/math/tri/imp.o: $S/math/tri/imp.h $S/math/tri/dev.h
+$B/mesh/vert_area/imp.o: $S/utils/imp.h $S/utils/error.h $S/math/tri/imp.h $S/mesh/vert_area/imp.h $S/io/mesh_read/imp.h $S/algo/vectors/imp.h $S/mesh/vert_area/imp/main.h $S/mesh/vert_area/imp/type.h $S/utils/msg.h
+$B/mpi/glb.o: $S/inc/conf.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
+$B/mpi/type.o: $S/inc/conf.h $S/inc/type.h $S/mpi/type.h $S/utils/mc.h
+$B/mpi/wrapper.o: $S/mpi/wrapper.h
+$B/u/mesh/vert_area/main.o: $S/utils/imp.h $S/utils/error.h $S/conf/imp.h $S/mpi/wrapper.h $S/io/mesh_read/imp.h $S/algo/vectors/imp.h $S/utils/mc.h $S/mesh/vert_area/imp.h $S/mpi/glb.h $S/utils/msg.h
+$B/utils/cc.o: $S/utils/cc/common.h $S/inc/conf.h $S/utils/error.h $S/d/api.h $B/conf.h
+$B/utils/error.o: $S/utils/msg.h $S/utils/error.h
+$B/utils/imp.o: $S/utils/error.h $S/utils/imp.h
+$B/utils/mc.o: $S/inc/conf.h $S/utils/error.h $S/mpi/wrapper.h $S/utils/mc.h $B/conf.h
+$B/utils/msg.o: $S/utils/msg.h
+$B/utils/os.o: $S/utils/os.h $S/utils/error.h $S/utils/msg.h
+$B/utils/string/imp.o: $S/utils/error.h $S/utils/string/imp.h
---
+++ src/u/mesh/eng_julicher/make/rule.mk
@@ -0,0 +1,23 @@
+$B/algo/edg/imp.o: $S/algo/edg/imp.cpp; $X -I$S/algo/edg -I$S/.
+$B/algo/vectors/imp.o: $S/algo/vectors/imp.cpp; $X -I$S/algo/vectors -I$S/.
+$B/conf/imp.o: $S/conf/imp.cpp; $X -I$S/conf -I$S/.
+$B/coords/conf.o: $S/coords/conf.cpp; $X -I$S/coords -I$S/.
+$B/coords/imp.o: $S/coords/imp.cpp; $X -I$S/coords -I$S/.
+$B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
+$B/io/mesh_read/edg/imp.o: $S/io/mesh_read/edg/imp.cpp; $X -I$S/io/mesh_read/edg -I$S/.
+$B/io/mesh_read/imp.o: $S/io/mesh_read/imp.cpp; $X -I$S/io/mesh_read -I$S/.
+$B/io/write/imp.o: $S/io/write/imp.cpp; $X -I$S/io/write -I$S/.
+$B/math/tform/imp.o: $S/math/tform/imp.cpp; $X -I$S/math/tform -I$S/.
+$B/math/tri/imp.o: $S/math/tri/imp.cpp; $X -I$S/math/tri -I$S/.
+$B/mesh/vert_area/imp.o: $S/mesh/vert_area/imp.cpp; $X -I$S/mesh/vert_area -I$S/.
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/type.o: $S/mpi/type.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
+$B/u/mesh/vert_area/main.o: $S/u/mesh/vert_area/main.cpp; $X -I$S/.
+$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
+$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/utils -I$S/.
+$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
+$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/utils -I$S/.
+$B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
+$B/utils/string/imp.o: $S/utils/string/imp.cpp; $X -I$S/utils/string -I$S/.
