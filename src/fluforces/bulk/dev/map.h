7f97d3335b4a2c61f00bbd3b13e96e3723857f6e
Lucas Amoudruz (daint) <lucas.amoudruz@wanadoo.fr>
2018-04-04 12:10:09 +0200

    adding map

---
+++ src/fluforces/bulk/dev/map.h
@@ -0,0 +1,79 @@
+struct Map {
+    int org0, org1, org2, org3, org4;
+    int str0, str1, str2, str3, str4;
+};
+
+_I_ int map_end(const Map *m, int i) {
+    return i >= m->str4;
+}
+
+_I_ int map_getid(const Map *m, int i) {
+    int m1, m2, m3, m4, id;
+    m1 = (i >= m->str0);
+    m2 = (i >= m->str1);
+    m3 = (i >= m->str2);
+    m4 = (i >= m->str3);
+    id = i + (m4 ? m->org4 :
+              m3 ? m->org3 :
+              m2 ? m->org2 :
+              m1 ? m->org1 :
+              m->org0);
+    return id;
+}
+
+_S_ int2 part_map_bounds(int3 L, int dz, int dy, int3 ca, const int *start) {
+    int3 cb;
+    int2 bounds;
+    int enddx, startx, endx, cid0;
+    cb.z = ca.z + dz;
+    cb.y = ca.y + dy;
+    if (!valid_c(cb.z, L.z)) return;
+    if (!valid_c(cb.y, L.y)) return;
+
+    /* dx runs from -1 to enddx */
+    enddx = (dz == 0 && dy == 0) ? 0 : 1;
+
+    startx =     max(    0, ca.x - 1    );
+    endx   = 1 + min(L.x-1, ca.x + enddx);
+
+    cid0 = L.x * (cb.y + L.y * cb.z);
+
+    bounds.x = start[cid0 + startx];
+    bounds.y = start[cid0 + endx];
+
+    return bounds;
+}
+
+_I_ void map_build(int3 L, int3 ca, const int *start, Map *m) {
+#define ONE_PART(dz, dy) part_map_bounds (L, dz, dy, ca, start)
+    int2 b;   /* bounds */
+    int s; /* start, count */
+
+    b = ONE_PART(-1, -1);
+    m->org0 = b.x;
+    s = b.y - b.x;
+    m->str0 = s;
+    
+
+    b = ONE_PART(-1,  0);
+    m->org1 = b.x - s;
+    s += b.y - b.x;
+    m->str1 = s;
+    
+    b = ONE_PART(-1,  1);
+    m->org2 = b.x - s;
+    s += b.y - b.x;
+    m->str2 = s;
+
+    b = ONE_PART( 0, -1);
+    m->org3 = b.x - s;
+    s += b.y - b.x;
+    m->str3 = s;
+
+    b = ONE_PART( 0,  1);
+    m->org4 = b.x - s;
+    s += b.y - b.x;
+    m->str4 = s;
+    
+#undef ONE_PART    
+}
