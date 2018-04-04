177c9e6935204595bdfdfda336157bb1ec3d3396
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-04 10:05:31 +0200

    use more EMALLOC/EFREE

--- src/flu/imp/fin.h
+++ src/flu/imp/fin.h
@@ -1,16 +1,16 @@
 static void fin_ii(int *ii, int *ii0, int *ii_hst) {
     CC(d::Free(ii));
     CC(d::Free(ii0));
-    UC(efree(ii_hst));
+    EFREE(ii_hst);
 }
 
 void flu_fin(FluQuants *q) {
     CC(d::Free(q->pp));
     CC(d::Free(q->pp0));
-    clist_fin(&q->cells);
-    clist_fin_map(q->mcells);
-    UC(efree(q->pp_hst));
+    UC(clist_fin(&q->cells));
+    UC(clist_fin_map(q->mcells));
+    EFREE(q->pp_hst);
 
-    if (q->ids)    fin_ii(q->ii, q->ii0, q->ii_hst);
-    if (q->colors) fin_ii(q->cc, q->cc0, q->cc_hst);
+    if (q->ids)    UC(fin_ii(q->ii, q->ii0, q->ii_hst));
+    if (q->colors) UC(fin_ii(q->cc, q->cc0, q->cc_hst));
 }
