91650bd7a1a43acfde8ad172cb057ef3a02d2a15
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-04-04 11:19:16 +0200

    doc: add algo/utils

--- src/algo/utils/dev.h
+++ src/algo/utils/dev.h
@@ -1,11 +1,20 @@
+#define _S_ static __device__
+#define _I_ static __device__
+
+// tag::int[]
 template <typename T>
-static __device__ T warpReduceSum(T v) {
+_I_ T warpReduceSum(T v)  // <1>
+// end::int[]
+{
     for (int offset = warpSize>>1; offset > 0; offset >>= 1)
         v += __shfl_down(v, offset);
     return v;
 }
 
-static __device__ float2 warpReduceSum(float2 val) {
+// tag::int[]
+_I_ float2 warpReduceSum(float2 val)  // <2>
+// end::int[]
+{
     for (int offset = warpSize / 2; offset > 0; offset /= 2) {
         val.x += __shfl_down(val.x, offset);
         val.y += __shfl_down(val.y, offset);
@@ -13,7 +22,10 @@ static __device__ float2 warpReduceSum(float2 val) {
     return val;
 }
 
-static __device__ float3 warpReduceSum(float3 val) {
+// tag::int[]
+_I_ float3 warpReduceSum(float3 val)  // <3>
+// end::int[]
+{
     for (int offset = warpSize/2; offset > 0; offset /= 2) {
         val.x += __shfl_down(val.x, offset);
         val.y += __shfl_down(val.y, offset);
