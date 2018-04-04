2cc8056f1a2c1fd0a2078e7f0e41343d3c7d0b4e
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-04 17:44:23 +0200

    adapting interface

--- src/flu/imp/start.h
+++ src/flu/imp/start.h
@@ -1,41 +1,41 @@
 #define CODE "flu"
-#define COLEXT "colors"
-#define IDSEXT "ids"
+#define COL CODE ".colors"
+#define IDS CODE ".ids"
 
-static int strt_pp(const Coords *coords, const int id, Particle *dev, /*w*/ Particle *hst) {
+static int strt_pp(MPI_Comm comm, const int id, Particle *dev, /*w*/ Particle *hst) {
     int n;
-    restart_read_pp(coords, CODE, id, hst, &n);
+    restart_read_pp(comm, CODE, id, &n, hst);
     if (n) cH2D(dev, hst, n);
     return n;
 }
 
-static int strt_ii(const Coords *coords, const char *subext, const int id, int *dev, /*w*/ int *hst) {
+static int strt_ii(MPI_Comm comm, const char *code, const int id, int *dev, /*w*/ int *hst) {
     int n;
-    restart_read_ii(coords, CODE, subext, id, hst, &n);
+    restart_read_ii(comm, code, id, &n, hst);
     if (n) cH2D(dev, hst, n);
     return n;
 }
 
-void flu_strt_quants(const Coords *coords, const int id, FluQuants *q) {
-    q->n =             strt_pp(coords,         id, /**/ q->pp, /* w */ q->pp_hst);
-    if (q->ids)    strt_ii(coords, IDSEXT, id, /**/ q->ii, /* w */ q->ii_hst);
-    if (q->colors) strt_ii(coords, COLEXT, id, /**/ q->cc, /* w */ q->cc_hst);
+void flu_strt_quants(MPI_Comm comm, const int id, FluQuants *q) {
+    q->n =         strt_pp(comm,      id, /**/ q->pp, /* w */ q->pp_hst);
+    if (q->ids)    strt_ii(comm, IDS, id, /**/ q->ii, /* w */ q->ii_hst);
+    if (q->colors) strt_ii(comm, COL, id, /**/ q->cc, /* w */ q->cc_hst);
 }
 
-static void strt_dump_pp(const Coords *coords, const int id, const int n, const Particle *dev, Particle *hst) {
+static void strt_dump_pp(MPI_Comm comm, const int id, const int n, const Particle *dev, Particle *hst) {
     if (n) cD2H(hst, dev, n);
-    restart_write_pp(coords, CODE, id, hst, n);
+    restart_write_pp(comm, CODE, id, n, hst);
 }
 
-static void strt_dump_ii(const Coords *coords, const char *subext, const int id, const int n, const int *dev, int *hst) {
+static void strt_dump_ii(MPI_Comm comm, const char *code, const int id, const int n, const int *dev, int *hst) {
     if (n) cD2H(hst, dev, n);
-    restart_write_ii(coords, CODE, subext, id, hst, n);
+    restart_write_ii(comm, code, id, n, hst);
 }
 
-void flu_strt_dump(const Coords *coords, const int id, const FluQuants *q) {
-    strt_dump_pp(coords, id, q->n, q->pp, /* w */ q->pp_hst);
-    if (q->ids)    strt_dump_ii(coords, IDSEXT, id, q->n, q->ii, /* w */ q->ii_hst);
-    if (q->colors) strt_dump_ii(coords, COLEXT, id, q->n, q->cc, /* w */ q->cc_hst);
+void flu_strt_dump(MPI_Comm comm, const int id, const FluQuants *q) {
+    strt_dump_pp(comm, id, q->n, q->pp, /* w */ q->pp_hst);
+    if (q->ids)    strt_dump_ii(comm, IDS, id, q->n, q->ii, /* w */ q->ii_hst);
+    if (q->colors) strt_dump_ii(comm, COL, id, q->n, q->cc, /* w */ q->cc_hst);
 }
 
 #undef CODE
--- src/rbc/imp/start.h
+++ src/rbc/imp/start.h
@@ -1,39 +1,39 @@
 #define CODE "rbc"
-#define IDEXT "ids"
+#define IDS CODE ".ids"
 
-static void setup_from_strt(const Coords *coords, int nv, int id, /**/ Particle *pp, int *nc, int *n, /*w*/ Particle *pp_hst) {
-    restart_read_pp(coords, CODE, id, pp_hst, n);
+static void setup_from_strt(MPI_Comm comm, int nv, int id, /**/ Particle *pp, int *nc, int *n, /*w*/ Particle *pp_hst) {
+    restart_read_pp(comm, CODE, id, n, pp_hst);
     *nc = *n / nv;
 
     if (*n) cH2D(pp, pp_hst, *n);
 }
 
-static void ids_from_strt(const Coords *coords, int id, /**/ int *ii) {
+static void ids_from_strt(MPI_Comm comm, int id, /**/ int *ii) {
     int nc;
-    restart_read_ii(coords, CODE, IDEXT, id, ii, &nc);
+    restart_read_ii(comm, IDS, id, &nc, ii);
 }
 
-void rbc_strt_quants(const Coords *coords, MeshRead *off, int id, RbcQuants *q) {
+void rbc_strt_quants(MPI_Comm comm, MeshRead *off, int id, RbcQuants *q) {
     int nv;
     nv = mesh_read_get_nv(off);
-    setup_from_strt(coords, nv, id, /**/ q->pp, &q->nc, &q->n, /*w*/ q->pp_hst);
-    if (q->ids) ids_from_strt(coords, id, /**/ q->ii);
+    setup_from_strt(comm, nv, id, /**/ q->pp, &q->nc, &q->n, /*w*/ q->pp_hst);
+    if (q->ids) ids_from_strt(comm, id, /**/ q->ii);
 }
 
-static void strt_dump(const Coords *coords, int id, int n, const Particle *pp, /*w*/ Particle *pp_hst) {
+static void strt_dump(MPI_Comm comm, int id, int n, const Particle *pp, /*w*/ Particle *pp_hst) {
     if (n) cD2H(pp_hst, pp, n);
-    restart_write_pp(coords, CODE, id, pp_hst, n);
+    restart_write_pp(comm, CODE, id, n, pp_hst);
 }
 
-static void strt_dump_ii(const Coords *coords, int id, int nc, const int *ii) {
-    restart_write_ii(coords, CODE, IDEXT, id, ii, nc);
+static void strt_dump_ii(MPI_Comm comm, int id, int nc, const int *ii) {
+    restart_write_ii(comm, IDS, id, nc, ii);
 }
 
-void rbc_strt_dump(const Coords *coords, int id, const RbcQuants *q) {
-    strt_dump(coords, id, q->n, q->pp, /*w*/ q->pp_hst);
+void rbc_strt_dump(MPI_Comm comm, int id, const RbcQuants *q) {
+    strt_dump(comm, id, q->n, q->pp, /*w*/ q->pp_hst);
     if (q->ids)
-        strt_dump_ii(coords, id, q->nc, q->ii);
+        strt_dump_ii(comm, id, q->nc, q->ii);
 }
 
 #undef CODE
-#undef IDEXT
+#undef IDS
