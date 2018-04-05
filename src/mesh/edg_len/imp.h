f15721f638cc0a6dadaf5d24447cba5316d74156
Sergey Litvinov <slitvinov@gmail.com>
2018-04-05 17:32:35 +0200

    wip: mesh/scatter

--- src/mesh/edg_len/imp.h
+++ src/mesh/edg_len/imp.h
@@ -3,7 +3,7 @@ struct MeshRead;
 struct Vectors;
 
 // tag::interface[]
-void mesh_edg_len_ini(MeshRead*, MeshEdgLen**);
+void mesh_edg_len_ini(MeshRead*, MeshEdgLen**); // <1>
 void mesh_edg_len_fin(MeshEdgLen*);
-void mesh_edg_len_apply(MeshEdgLen*, int nm, Vectors*, /**/ double*);
+void mesh_edg_len_apply(MeshEdgLen*, int nm, Vectors*, /**/ double*); // <2>
 // end::interface[]
