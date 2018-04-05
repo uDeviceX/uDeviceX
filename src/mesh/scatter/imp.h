e3d1ec5bd6fc7bae74f499197c553561dac30762
Sergey Litvinov <slitvinov@gmail.com>
2018-04-05 17:33:58 +0200

    wip: scatter doc

--- src/mesh/scatter/imp.h
+++ src/mesh/scatter/imp.h
@@ -3,7 +3,7 @@ struct MeshRead;
 struct Scalars;
 
 // tag::interface[]
-void mesh_scatter_ini(MeshRead*, MeshScatter**);
+void mesh_scatter_ini(MeshRead*, MeshScatter**); // <1>
 void mesh_scatter_fin(MeshScatter*);
-void mesh_scatter_edg2vert(MeshScatter*, int nm, Scalars*, /**/ double*);
+void mesh_scatter_edg2vert(MeshScatter*, int nm, Scalars*, /**/ double*); // <2>
 // end::interface[]
