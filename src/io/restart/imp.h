0c84ecb5d5ce627edb61ac8bcfc3ec9b86186b01
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-04-05 09:46:01 +0200

    doc: adding restart

--- src/io/restart/imp.h
+++ src/io/restart/imp.h
@@ -2,14 +2,20 @@ struct Coords;
 struct Particle;
 struct Solid;
 
+// tag::id[]
 enum {RESTART_TEMPL=-1,
       RESTART_FINAL=-2};
 enum {RESTART_BEGIN=RESTART_FINAL};
+// end::id[]
 
-void restart_write_pp(MPI_Comm, const char *code, int id, long n, const Particle *pp);
-void restart_write_ii(MPI_Comm, const char *code, int id, long n, const int *ii);
-void restart_write_ss(MPI_Comm, const char *code, int id, long n, const Solid *ss);
+// tag::write[]
+void restart_write_pp(MPI_Comm, const char *code, int id, long n, const Particle *pp); // <1>
+void restart_write_ii(MPI_Comm, const char *code, int id, long n, const int *ii);      // <2>
+void restart_write_ss(MPI_Comm, const char *code, int id, long n, const Solid *ss);    // <3>
+// end::write[]
 
+// tag::read[]
 void restart_read_pp(MPI_Comm, const char *code, int id, int *n, Particle *pp);
 void restart_read_ii(MPI_Comm, const char *code, int id, int *n, int *ii);
 void restart_read_ss(MPI_Comm, const char *code, int id, int *n, Solid *ss);
+// end::read[]
