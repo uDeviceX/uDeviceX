d0d9616f97db592cd9f70254cbb666e3bf385fa9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-04-05 17:01:55 +0200

    doc: update control/outflow

--- src/control/outflow/imp.h
+++ src/control/outflow/imp.h
@@ -10,12 +10,14 @@ void outflow_ini(int maxp, /**/ Outflow **o);
 void outflow_fin(/**/ Outflow *o);
 // end::mem[]
 
-// tag::ini[]
+// tag::set[]
 void outflow_set_params_circle(const Coords*, float3 c, float R, Outflow *o);
 void outflow_set_params_plate(const Coords*, int dir, float r0, Outflow *o);
-// end::ini[]
+// end::set[]
 
+// tag::cfg[]
 void outflow_set_conf(const Config *cfg, const Coords*, Outflow *);
+// end::cfg[]
 
 // tag::int[]
 void outflow_filter_particles(int n, const Particle *pp, /**/ Outflow *o); // <1>
