428c725e3d9f40f38386c477660ca0dd7ad716ca
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-05 15:58:24 +0200

    more UC

--- src/sim/imp/colors.h
+++ src/sim/imp/colors.h
@@ -7,9 +7,9 @@ void gen_colors(const Rbc *r, Colorer *c, Flu *f) {
     nm = r->q.nc;
     nv = r->q.nv;
 
-    emesh_build_map(nm, nv, r->q.pp, /**/ c->e.p);
-    emesh_pack(nv, r->q.pp, /**/ c->e.p);
-    emesh_download(c->e.p);
+    UC(emesh_build_map(nm, nv, r->q.pp, /**/ c->e.p));
+    UC(emesh_pack(nv, r->q.pp, /**/ c->e.p));
+    UC(emesh_download(c->e.p));
 
     UC(emesh_post_send(c->e.p, c->e.c));
     UC(emesh_post_recv(c->e.c, c->e.u));
@@ -20,7 +20,7 @@ void gen_colors(const Rbc *r, Colorer *c, Flu *f) {
     UC(emesh_wait_send(c->e.c));
     UC(emesh_wait_recv(c->e.c, c->e.u));
 
-    emesh_unpack(nv, c->e.u, /**/ &nmhalo, c->pp + nm * nv);
+    UC(emesh_unpack(nv, c->e.u, /**/ &nmhalo, c->pp + nm * nv));
     nm += nmhalo;
 
     /* compute extents */
@@ -34,5 +34,5 @@ void gen_colors(const Rbc *r, Colorer *c, Flu *f) {
 void recolor_flux(const Coords *c, const Recolorer *opt, Flu *f) {
     int3 L = make_int3(xs(c), ys(c), zs(c));
     if (opt->flux_active)
-        color_linear_flux(c, L, opt->flux_dir, RED_COLOR, f->q.n, f->q.pp, /**/ f->q.cc);
+        UC(color_linear_flux(c, L, opt->flux_dir, RED_COLOR, f->q.n, f->q.pp, /**/ f->q.cc));
 }
