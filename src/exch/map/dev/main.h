55a9c809014b2fdaff3dcab1a8d84a968c3d3fc6
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-06 10:26:53 +0200

    rename

--- src/exch/map/dev/main.h
+++ src/exch/map/dev/main.h
@@ -7,14 +7,14 @@ static __device__ void warpexscan(int cnt, int t, /**/ int *starts) {
     if (t < 27) starts[t] = scan - cnt;
 }
 
-__global__ void scan2d(const int *counts, const int *oldtcounts, /**/ int *tcounts, int *starts) {
-    int t, cnt, newcnt;
+__global__ void scan2d(const int *counts, const int *offsets, /**/ int *nextoffsets, int *starts) {
+    int t, cnt, nextoffset;
     t = threadIdx.x;
     cnt = 0;
     if (t < 26) {
         cnt = counts[t];
-        newcnt = cnt + oldtcounts[t];
-        tcounts[t] = newcnt;
+        nextoffset = cnt + offsets[t];
+        nextoffsets[t] = nextoffset;
     }
     if (starts) warpexscan(cnt, t, /**/ starts);
 }
