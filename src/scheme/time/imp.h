d5b83b44b41d39b5fa71375ebbaba8083abf85fb
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-06 17:33:59 +0200

    rename Time/TimeLine

--- src/scheme/time/imp.h
+++ src/scheme/time/imp.h
@@ -1,11 +1,11 @@
-struct Time;
+struct TimeLine;
 
 // tag::interface[]
-void time_ini(float start, /**/ Time**);
-void time_fin(Time*);
+void time_line_ini(float start, /**/ TimeLine**);
+void time_line_fin(TimeLine*);
 
-void time_next(Time*, float dt);
-int  time_cross(Time*, float interval);
-float time_current (Time*);
-long  time_iteration(Time*);
+void time_line_next(TimeLine*, float dt);
+int  time_line_cross(TimeLine*, float interval);
+float time_line_current (TimeLine*);
+long  time_line_iteration(TimeLine*);
 // end::interface[]
--- src/scheme/time/imp/main.h
+++ src/scheme/time/imp/main.h
@@ -1,13 +1,13 @@
-void time_ini(float t, /**/ Time** pq) {
-    Time *q;
+void time_line_ini(float t, /**/ TimeLine** pq) {
+    TimeLine *q;
     EMALLOC(1, &q);
     q->t = q->t0 = t;
     q->iteration = 0;
     *pq = q;
 }
-void time_fin(Time *q) { EFREE(q); }
+void time_line_fin(TimeLine *q) { EFREE(q); }
 
-void time_next(Time *q, float dt) {
+void time_line_next(TimeLine *q, float dt) {
     if (dt < 0)
         ERR("dt = %g < 0", dt);
     if (q->iteration == 0) q->dt0 = dt;
@@ -20,9 +20,9 @@ void time_next(Time *q, float dt) {
     q->iteration++;
 }
 
-float time_current(Time *q) { return q->t; }
-long  time_iteration(Time *q) { return q->iteration; }
-int time_cross(Time *q, float i) {
+float time_line_current(TimeLine *q) { return q->t; }
+long  time_line_iteration(TimeLine *q) { return q->iteration; }
+int time_line_cross(TimeLine *q, float i) {
     float t, t0, f;
     if (q->iteration == 0) return 0;
     else if (i < 0)   ERR("i < 0");
