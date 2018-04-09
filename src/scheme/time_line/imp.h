0545b1ce1baedfdff136bb28e8613d7c485836a8
Sergey Litvinov <slitvinov@gmail.com>
2018-04-09 11:22:11 +0200

    upd doc for time_line

--- src/scheme/time_line/imp.h
+++ src/scheme/time_line/imp.h
@@ -4,9 +4,9 @@ struct TimeLine;
 void time_line_ini(float start, /**/ TimeLine**);
 void time_line_fin(TimeLine*);
 
-void time_line_advance(float dt, TimeLine*);
+void time_line_advance(float dt, TimeLine*); // <1>
 
-int   time_line_cross(const TimeLine*, float interval);
+int   time_line_cross(const TimeLine*, float interval); // <2>
 float time_line_get_current (const TimeLine*);
 long  time_line_get_iteration(const TimeLine*);
 // end::interface[]
