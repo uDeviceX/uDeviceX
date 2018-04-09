0051c08ada215807b3c9cda36e6cb407c2513f34
Sergey Litvinov <slitvinov@gmail.com>
2018-04-09 11:19:00 +0200

    better vtk docs

--- src/io/vtk/imp.h
+++ src/io/vtk/imp.h
@@ -6,19 +6,19 @@ struct Vectors;
 struct Scalars;
 
 // tag::conf[]
-void vtk_conf_ini(MeshRead*, /**/ VTKConf**);
+void vtk_conf_ini(MeshRead*, /**/ VTKConf**); // <1>
 void vtk_conf_fin(VTKConf*);
-void vtk_conf_tri(VTKConf*, const char *keys);
-void vtk_conf_vert(VTKConf*, const char *keys);
+void vtk_conf_vert(VTKConf*, const char *keys); // <2>
+void vtk_conf_tri(VTKConf*, const char *keys); // <3>
 // end::conf[]
 
 // tag::main[]
-void vtk_ini(MPI_Comm, int maxm, char const *path, VTKConf*, /**/ VTK**);
-void vtk_points(VTK*, int nm, const Vectors*);
+void vtk_ini(MPI_Comm, int maxm, char const *dir, VTKConf*, /**/ VTK**); // <1>
+void vtk_points(VTK*, int nm, const Vectors*); // <2>
 
-void vtk_tri(VTK*, int nm, const Scalars*, const char *keys);
-void vtk_vert(VTK*, int nm, const Scalars*, const char *keys);
+void vtk_vert(VTK*, int nm, const Scalars*, const char *keys); // <3>
+void vtk_tri(VTK*, int nm, const Scalars*, const char *keys);  // <4>
 
-void vtk_write(VTK*, MPI_Comm, int id);
+void vtk_write(VTK*, MPI_Comm, int id); // <5>
 void vtk_fin(VTK*);
 // tag::main[]
