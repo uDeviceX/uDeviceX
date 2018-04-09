cebcea89943c08b1d7d9063e63ff5338da35a88e
Sergey Litvinov <slitvinov@gmail.com>
2018-04-09 09:30:14 +0200

    do not to pass ARCH if compile c++ files

--- src/make/main.mk
+++ src/make/main.mk
@@ -27,7 +27,7 @@ LIBS      += -lcudart -lcurand -lnvToolsExt
 
 LOG = @echo $< $@;
 N  = $(LOG) $(NVCC)  $(ARCH) $(NVCCFLAGS) --compiler-options '$(NCFLAGS)'     $< -c -o $@
-X  = $(LOG) $(NVCC)  $(ARCH)              --compiler-options '$(XCFLAGS)'     $< -c -o $@
+X  = $(LOG) $(NVCC)                       --compiler-options '$(XCFLAGS)'     $< -c -o $@
 L  = $(LOG) $(NVCC)  $(ARCH) -dlink $O $(NVCCLIBS) -o $B/gpuCode.o && \
 	$(LINK)  $B/gpuCode.o $O $(LIBS) -o $@
 
