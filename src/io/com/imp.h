ef3d9fe8c7f80722b1d1638abf76824665caf0e0
Sergey Litvinov <slitvinov@gmail.com>
2018-04-09 10:45:35 +0200

    predecl Coords and float3

--- src/io/com/imp.h
+++ src/io/com/imp.h
@@ -1,1 +1,3 @@
-void io_com_dump(MPI_Comm comm, const Coords *coords, long id, int n, const int *ii, const float3 *rr);
+struct Coords;
+struct float3;
+void io_com_dump(MPI_Comm, const Coords*, long id, int n, const int *ii, const float3 *rr);
