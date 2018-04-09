4747c9c12636f8f5b3a5fbb03775d8d4a1ad0e11
Sergey Litvinov <slitvinov@gmail.com>
2018-04-09 13:16:26 +0200

    a bit of doc about io/write

--- src/io/write/imp.h
+++ src/io/write/imp.h
@@ -3,8 +3,8 @@ struct WriteFile;
 int write_file_open(MPI_Comm, const char *path, /**/ WriteFile**);
 int write_file_close(WriteFile*);
 
-void write_all(MPI_Comm, const void *const, int sz, WriteFile*);
-int write_master(MPI_Comm, const void *const, int sz, WriteFile*);
+void write_all(MPI_Comm, const void *const, int nbytes, WriteFile*); // <1>
+int write_master(MPI_Comm, const void *const, int nbytes, WriteFile*); // <2>
 
 int write_shift_indices(MPI_Comm, int, /**/ int*);
 int write_reduce(MPI_Comm, int, /**/ int*);
