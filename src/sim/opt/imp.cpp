faee753aeebfc2e29454ab392e9f07e44a5dce39
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-09 15:54:46 +0200

    simplify bool read in opt

--- src/sim/opt/imp.cpp
+++ src/sim/opt/imp.cpp
@@ -20,12 +20,16 @@ static int get_shifttype(const Config *c, const char *desc) {
     return -1;
 }
 
-void opt_read_common(const Config *c, Opt *o) {
+static void lookup_bool(const Config *c, const char *desc, bool *res) {
     int b;
+    UC(conf_lookup_bool(c, desc, &b));
+    *res = b;
+}
+
+void opt_read_common(const Config *c, Opt *o) {
     const char *s;
 
-    UC(conf_lookup_bool(c, "flu.ids", &b));
-    o->fluids = b;
+    UC(lookup_bool(c, "flu.ids", &o->fluids));
 
     UC(conf_lookup_string(c, "dump.strt_base_dump", &s));
     strcpy(o->strt_base_dump, s);
@@ -33,16 +37,13 @@ void opt_read_common(const Config *c, Opt *o) {
     UC(conf_lookup_string(c, "dump.strt_base_read", &s));
     strcpy(o->strt_base_read, s);
 
-    UC(conf_lookup_bool(c, "dump.field", &b));
-    o->dump_field = b;
+    UC(lookup_bool(c, "dump.field", &o->dump_field));
     UC(conf_lookup_float(c, "dump.freq_field", &o->freq_field));
     
-    UC(conf_lookup_bool(c, "dump.strt", &b));
-    o->dump_strt = b;
+    UC(lookup_bool(c, "dump.strt", &o->dump_strt));
     UC(conf_lookup_float(c, "dump.freq_strt", &o->freq_strt));
 
-    UC(conf_lookup_bool(c, "dump.parts", &b));
-    o->dump_parts = b;
+    UC(lookup_bool(c, "dump.parts", &o->dump_parts));
     UC(conf_lookup_float(c, "dump.freq_parts", &o->freq_parts));
 }
 
@@ -66,60 +67,36 @@ void opt_set_eq(Opt *o) {
 }
 
 void opt_read(const Config *c, Opt *o) {
-    int b;
-    UC(conf_lookup_bool(c, "fsi.active", &b));
-    o->fsi = b;
-    UC(conf_lookup_bool(c, "cnt.active", &b));
-    o->cnt = b;
+    UC(lookup_bool(c, "fsi.active", &o->fsi));
+    UC(lookup_bool(c, "cnt.active", &o->cnt));
 
-    UC(conf_lookup_bool(c, "flu.colors", &b));
-    o->flucolors = b;
-    UC(conf_lookup_bool(c, "flu.ids", &b));
-    o->fluids = b;
-    UC(conf_lookup_bool(c, "flu.stresses", &b));
-    o->fluss = b;
+    UC(lookup_bool(c, "flu.colors", &o->flucolors));
+    UC(lookup_bool(c, "flu.ids", &o->fluids));
+    UC(lookup_bool(c, "flu.stresses", &o->fluss));
 
-    UC(conf_lookup_bool(c, "rbc.active", &b));
-    o->rbc = b;
-    UC(conf_lookup_bool(c, "rbc.ids", &b));
-    o->rbcids = b;
-    UC(conf_lookup_bool(c, "rbc.stretch", &b));
-    o->rbcstretch = b;
+    UC(lookup_bool(c, "rbc.active", &o->rbc));
+    UC(lookup_bool(c, "rbc.ids", &o->rbcids));
+    UC(lookup_bool(c, "rbc.stretch", &o->rbcstretch));
     o->rbcshifttype = get_shifttype(c, "rbc.shifttype");
 
-    UC(conf_lookup_bool(c, "rig.active", &b));
-    o->rig = b;
-    UC(conf_lookup_bool(c, "rig.bounce", &b));
-    o->rig_bounce = b;
-    UC(conf_lookup_bool(c, "rig.empty_pp", &b));
-    o->rig_empty_pp = b;
+    UC(lookup_bool(c, "rig.active", &o->rig));
+    UC(lookup_bool(c, "rig.bounce", &o->rig_bounce));
+    UC(lookup_bool(c, "rig.empty_pp", &o->rig_empty_pp));
     o->rigshifttype = get_shifttype(c, "rig.shifttype");
 
-    UC(conf_lookup_bool(c, "wall.active", &b));
-    o->wall = b;
-    
-    UC(conf_lookup_bool(c, "outflow.active", &b));
-    o->outflow = b;
-    UC(conf_lookup_bool(c, "inflow.active", &b));
-    o->inflow = b;
-    UC(conf_lookup_bool(c, "denoutflow.active", &b));
-    o->denoutflow = b;
-    UC(conf_lookup_bool(c, "vcon.active", &b));
-    o->vcon = b;
+    UC(lookup_bool(c, "wall.active", &o->wall));    
+    UC(lookup_bool(c, "outflow.active", &o->outflow));
+    UC(lookup_bool(c, "inflow.active", &o->inflow));
+    UC(lookup_bool(c, "denoutflow.active", &o->denoutflow));
+    UC(lookup_bool(c, "vcon.active", &o->vcon));
 
-    UC(conf_lookup_bool(c, "dump.rbc_com", &b));
-    o->dump_rbc_com = b;
+    UC(lookup_bool(c, "dump.rbc_com", &o->dump_rbc_com));
     UC(conf_lookup_float(c, "dump.freq_rbc_com", &o->freq_rbc_com));
-
-    UC(conf_lookup_bool(c, "dump.forces", &b));
-    o->dump_forces = b;
+    UC(lookup_bool(c, "dump.forces", &o->dump_forces));
     
     UC(conf_lookup_int(c, "flu.recolor_freq", &o->recolor_freq));
 
-    UC(conf_lookup_bool(c, "flu.push", &b));
-    o->push_flu = b;
-    UC(conf_lookup_bool(c, "rbc.push", &b));
-    o->push_rbc = b;
-    UC(conf_lookup_bool(c, "rig.push", &b));
-    o->push_rig = b;
+    UC(lookup_bool(c, "flu.push", &o->push_flu));
+    UC(lookup_bool(c, "rbc.push", &o->push_rbc));
+    UC(lookup_bool(c, "rig.push", &o->push_rig));
 }
