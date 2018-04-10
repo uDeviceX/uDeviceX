3b719bc8c9f0968d6c4fedeaa9aaf8455c7a4d4d
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-10 10:28:38 +0200

    rm unused run_eq

--- src/sim/imp/run.h
+++ src/sim/imp/run.h
@@ -1,15 +1,3 @@
-static void run_eq(float te, Sim *s) { /* equilibrate */
-    float dt;
-    s->equilibrating = true;
-    dt = utils_get_dt0(s);
-    while (time_line_get_current(s->time.t) < te) {
-        UC(step(s->time.t, dt, 0.0, s));
-        time_line_advance(dt, s->time.t);
-        dt = utils_get_dt(s, s->time.t);
-    }
-    UC(distribute_flu(/**/ s));
-}
-
 static void pre_run(const Config *cfg, Sim *s) {
     s->equilibrating = false;
     
