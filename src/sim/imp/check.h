f83d47d6c9293d8f85050a1724f01f746be0fc8c
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-10 08:55:43 +0200

    use actice_rbc

--- src/sim/imp/check.h
+++ src/sim/imp/check.h
@@ -4,7 +4,7 @@ static void check_size(long n, long nmax) {
 }
 
 static void check_sizes(Sim *s) {
-    if (s->opt.rbc) UC(check_size(s->rbc.q.nc, MAX_CELL_NUM));
+    if (active_rbc(s)) UC(check_size(s->rbc.q.nc, MAX_CELL_NUM));
     UC(check_size(s->flu.q.n , s->flu.q.maxp)); 
 }
 
@@ -16,12 +16,12 @@ static void check_vel(float dt, Sim *s) {
     const Coords *c = s->coords;
     UC(dbg_check_vel(dt, c, "flu", s->dbg, s->flu.q.n, s->flu.q.pp));
     if (active_rig(s))  UC(dbg_check_vel(dt, c, "rig", s->dbg, s->rig.q.n, s->rig.q.pp));
-    if (s->opt.rbc)     UC(dbg_check_vel(dt, c, "rbc", s->dbg, s->rbc.q.n, s->rbc.q.pp));
+    if (active_rbc(s))  UC(dbg_check_vel(dt, c, "rbc", s->dbg, s->rbc.q.n, s->rbc.q.pp));
 }
 
 static void check_forces(float dt, Sim *s) {
     const Coords *c = s->coords;
     UC(dbg_check_forces(dt, c, "flu.ff", s->dbg, s->flu.q.n, s->flu.q.pp, s->flu.ff));
     if (active_rig(s)) UC(dbg_check_forces(dt, c, "rig.ff", s->dbg, s->rig.q.n, s->rig.q.pp, s->rig.ff));
-    if (s->opt.rbc)    UC(dbg_check_forces(dt, c, "rbc.ff", s->dbg, s->rbc.q.n, s->rbc.q.pp, s->rbc.ff));
+    if (active_rbc(s)) UC(dbg_check_forces(dt, c, "rbc.ff", s->dbg, s->rbc.q.n, s->rbc.q.pp, s->rbc.ff));
 }
