7d6c76c662c851578dcdf3a8e0626d41a6d916a1
Sergey Litvinov <slitvinov@gmail.com>
2018-04-11 09:19:25 +0200

    add missing tag::interface

--- src/io/write/imp.h
+++ src/io/write/imp.h
@@ -1,5 +1,6 @@
 struct WriteFile;
 
+// tag::interface[]
 int write_file_open(MPI_Comm, const char *path, /**/ WriteFile**);
 int write_file_close(WriteFile*);
 
@@ -8,3 +9,4 @@ int write_master(MPI_Comm, const void *const, int nbytes, WriteFile*); // <2>
 
 int write_shift_indices(MPI_Comm, int, /**/ int*);
 int write_reduce(MPI_Comm, int, /**/ int*);
+// end::interface[]
