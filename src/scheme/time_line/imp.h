7f26e2a08ee587ba52e222d5c0d6cd84fd16e3fb
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-04-11 12:09:42 +0200

    doc: better time_line

--- src/scheme/time_line/imp.h
+++ src/scheme/time_line/imp.h
@@ -1,12 +1,16 @@
 struct TimeLine;
 
-// tag::interface[]
+// tag::mem[]
 void time_line_ini(float start, /**/ TimeLine**);
 void time_line_fin(TimeLine*);
+// end::mem[]
 
-void time_line_advance(float dt, TimeLine*); // <1>
-int   time_line_cross(const TimeLine*, float interval); // <2>
+// tag::int[]
+void time_line_advance(float dt, TimeLine*);           // <1>
+int  time_line_cross(const TimeLine*, float interval); // <2>
+// end::int[]
 
-float time_line_get_current (const TimeLine*);
-long  time_line_get_iteration(const TimeLine*);
-// end::interface[]
+// tag::get[]
+float time_line_get_current (const TimeLine*);  // <1>
+long  time_line_get_iteration(const TimeLine*); // <2>
+// end::get[]
