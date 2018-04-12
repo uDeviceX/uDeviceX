860b5c0e1be8934c358ab4fadb1f627655a96c44
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-12 10:47:36 +0200

    fix compilation u/scheme/time

--- src/u/scheme/time/make/dir.mk
+++ src/u/scheme/time/make/dir.mk
@@ -1,5 +1,8 @@
 D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
+    d $B/conf             && \
+    d $B/coords           && \
     d $B/d                && \
+    d $B/io/restart       && \
     d $B/mpi              && \
     d $B/scheme/time_line && \
     d $B/u/scheme/time    && \
--- src/u/scheme/time/make/obj.mk
+++ src/u/scheme/time/make/obj.mk
@@ -1,4 +1,5 @@
-O = $B/d/api.o $B/mpi/glb.o $B/mpi/wrapper.o $B/scheme/time_line/imp.o \
+O = $B/conf/imp.o $B/coords/conf.o $B/coords/imp.o $B/d/api.o $B/io/restart/imp.o \
+    $B/mpi/glb.o $B/mpi/wrapper.o $B/scheme/time_line/imp.o \
     $B/u/scheme/time/main.o $B/utils/cc.o $B/utils/error.o $B/utils/imp.o \
     $B/utils/mc.o $B/utils/msg.o $B/utils/nvtx/imp.o $B/utils/os.o \
     $B/utils/string/imp.o
