c24f7bfed9d0b78bd355909adcc765e04a0b006d
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-12 08:50:06 +0200

    add start to interface in time_line

--- src/scheme/time_line/imp.h
+++ src/scheme/time_line/imp.h
@@ -10,6 +10,9 @@ void time_line_advance(float dt, TimeLine*);           // <1>
 int  time_line_cross(const TimeLine*, float interval); // <2>
 // end::int[]
 
+void time_line_strt_dump(MPI_Comm, const char *base, int id, const TimeLine*);
+void time_line_strt_read(const char *base, int id, TimeLine*);
+
 // tag::get[]
 float time_line_get_current (const TimeLine*);  // <1>
 long  time_line_get_iteration(const TimeLine*); // <2>
