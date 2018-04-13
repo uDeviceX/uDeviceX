e453902f467b5ad2370c71b11124334b63095273
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-13 13:36:34 +0200

    sample at given frequency

--- src/sim/imp/addons.h
+++ src/sim/imp/addons.h
@@ -99,3 +99,11 @@ static void sample(Sim *s) {
     UC(grid_sampler_data_push(flu->q.n, flu->q.pp, flu->ss, sam->d));
     UC(grid_sampler_add(sam->d, sam->s));
 }
+
+static void field_sample(Sim *s) {
+    const Opt *opt = &s->opt;
+    float freq = opt->freq_field / opt->sampler_npdump;
+    const TimeLine *time = s->time.t;
+    if (opt->dump_field && time_line_cross(time, freq))
+        sample(s);
+}
