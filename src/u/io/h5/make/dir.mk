7437b95a7370a409a925c40f57d161cdd5867145
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-13 14:37:42 +0200

    rm obsolete unit

--- src/u/io/h5/make/dir.mk
+++
@@ -1,9 +0,0 @@
-D = @d () { test -d "$$1" || mkdir -p -- "$$1"; } && \
-    d $B/conf         && \
-    d $B/coords       && \
-    d $B/d            && \
-    d $B/mpi          && \
-    d $B/u/io/h5      && \
-    d $B/utils        && \
-    d $B/utils/nvtx   && \
-    d $B/utils/string
