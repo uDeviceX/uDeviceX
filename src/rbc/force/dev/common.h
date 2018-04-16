8e5264f892a0ccab806413a5625d38d88ed3cbbb
Sergey Litvinov <slitvinov@gmail.com>
2018-04-16 19:24:15 +0200

    fix other forces

--- src/rbc/force/dev/common.h
+++ src/rbc/force/dev/common.h
@@ -39,8 +39,8 @@ static __device__ float3 fspring(const RbcParams_v *par, float3 x21, float l0, /
     }
     fwlc =   wlc_r(r); /* make fwlc + fpow = 0 for r = l0 */
     fpow = - wlc_r(l0) * powf(l0, m + 1) / powf(r, m + 1);
-    axpy(fwlc + fpow, &x21, /**/ &f);
-    return f;
+    scal(fwlc + fpow, /*io*/ &x21);
+    return x21;
   #undef wlc_r
 }
 
@@ -60,16 +60,16 @@ static __device__ float3 ftri(const RbcParams_v *par, float3 r1, float3 r2, floa
     diff(&r3, &r1, /**/ &x31);
 
     fa = farea(par, x21, x31, x32, si.a0, par->totArea, area);
-    add(&fa, /**/ &f);
+    add(&fa, /*io*/ &f);
 
     fv = fvolume(par, r2, r3, par->totVolume, volume);
-    add(&fv, /**/ &f);
+    add(&fv, /*io*/ &f);
 
     fs = fspring(par, x21, si.l0, &spring_status);
     if (spring_status != SPRING_OK)
         report_tri(r1, r2, r3);
     
-    add(&fs, /**/ &f);
+    add(&fs, /*io*/ &f);
 
     return f;
 }
@@ -100,12 +100,12 @@ static __device__ float  frnd0(float dt, const RbcParams_v *par, float rnd) {
 }
 
 static __device__ float3 frnd(float dt, const RbcParams_v *par, float3 r1, float3 r2, Rnd1Info rnd) {
-    float3 dr, f;
+    float3 dr;
     float r, f0;
     diff(&r1, &r2, /**/ &dr);
     r = sqrtf(dot<float>(&dr, &dr));
     f0 = frnd0(dt, par, rnd.r);
-    axpy(f0/r, &dr, /**/ &f);
-    return f;
+    scal(f0/r, /*io*/ &dr);
+    return dr;
 }
 
