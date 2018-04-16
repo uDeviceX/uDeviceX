d81c2643b4a2d1891d4e22c025f8ed64adfaf487
Sergey Litvinov <slitvinov@gmail.com>
2018-04-16 14:56:14 +0200

    wip: testing a new formula

--- src/u/mesh/force/main.cpp
+++ src/u/mesh/force/main.cpp
@@ -8,9 +8,20 @@
 #include "mpi/glb.h"
 #include "mpi/wrapper.h"
 #include "mesh/force/kantor1/imp.h"
+#include "mesh/force/kantor0/imp.h"
 #include "utils/mc.h"
+#include "utils/imp.h"
 
 typedef double3 (*Fun)(double, double, double3, double3, double3, double3);
+enum {KANTOR0, KANTOR1};
+static Fun Dih_A[] = {
+    force_kantor0_hst::dih_a,
+    force_kantor1_hst::dih_a,
+};
+static Fun Dih_B[] = {
+    force_kantor0_hst::dih_b,
+    force_kantor1_hst::dih_b,
+};
 
 void force(Fun fun,
             double phi, double kb,
@@ -43,21 +54,38 @@ void read3(int *pc, char ***pv, /**/ double a[3]) {
     *pc = c; *pv = v;
 }
 
+void read_type(int *pc, char ***pv, /**/ int *ptype) {
+    int c, type;
+    char **v;
+    c = *pc; v = *pv;
+
+    if (c < 0) ERR("not enough arguments");
+    if      (same_str(v[0], "kantor0")) type = KANTOR0;
+    else if (same_str(v[0], "kantor1")) type = KANTOR1;
+    else ERR("unknown type '%s'", v[0]);
+    shift(&c, &v);
+    
+    *pc = c; *pv = v; *ptype = type;
+}
+
 void main0(int *argc, char ***argv) {
     enum {X, Y, Z};
+    int type;
     double phi, kb;
     double a[3], b[3], c[3], d[3], fa[3], fb[3];
-
+    
+    UC(read_type(argc, argv, &type));
     UC(read3(argc, argv, /**/ a));
     UC(read3(argc, argv, /**/ b));
     UC(read3(argc, argv, /**/ c));
     UC(read3(argc, argv, /**/ d));
     
     phi = 0; kb = 1;
-    UC(force(force_kantor1_hst::dih_a, phi, kb, a, b, c, d, /**/ fa));
-    UC(force(force_kantor1_hst::dih_b, phi, kb, a, b, c, d, /**/ fb));
-    printf("%.16g %.16g %.16g %.16g %.16g %.16g\n",
-           fa[X], fa[Y], fa[Z], fb[X], fb[Y], fb[Z]);
+    UC(force(Dih_A[type], phi, kb, a, b, c, d, /**/ fa));
+    printf("%.16g %.16g %.16g \n", fa[X], fa[Y], fa[Z]);
+    //    UC(force(Dih_B[type], phi, kb, a, b, c, d, /**/ fb));
+    //    printf("%.16g %.16g %.16g %.16g %.16g %.16g\n",
+    //           fa[X], fa[Y], fa[Z], fb[X], fb[Y], fb[Z]);
 }
 
 int main(int argc, char **argv) {
