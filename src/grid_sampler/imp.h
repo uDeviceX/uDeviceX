192098505fa6718ffbfc3d8c8e7f1a71b7bbe35e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-04-16 14:25:31 +0200

    adding grid_sampler doc

--- src/grid_sampler/imp.h
+++ src/grid_sampler/imp.h
@@ -3,16 +3,24 @@ struct GridSampleData;
 
 struct Particle;
 
+// tag::data_mem[]
 void grid_sampler_data_ini(GridSampleData **);
 void grid_sampler_data_fin(GridSampleData  *);
+// end::data_mem[]
 
+// tag::data_int[]
 void grid_sampler_data_reset(GridSampleData *);
 void grid_sampler_data_push(long n, const Particle *pp, const float *ss, GridSampleData *);
+// end::data_int[]
 
 
-void grid_sampler_ini(bool stress, int3 L, int3 N, GridSampler**);
-void grid_sampler_fin(GridSampler*);
+// tag::mem[]
+void grid_sampler_ini(bool stress, int3 L, int3 N, GridSampler**); // <1>
+void grid_sampler_fin(GridSampler*); // <2>
+// end::mem[]
 
-void grid_sampler_reset(GridSampler*);
-void grid_sampler_add(const GridSampleData*, GridSampler*);
-void grid_sampler_dump(MPI_Comm, const char *dir, long id, GridSampler*);
+// tag::int[]
+void grid_sampler_reset(GridSampler*); // <1>
+void grid_sampler_add(const GridSampleData*, GridSampler*); // <2>
+void grid_sampler_dump(MPI_Comm, const char *dir, long id, GridSampler*); // <3>
+// end::int[]
