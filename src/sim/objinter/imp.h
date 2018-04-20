f2a546635a267ebcdb3a3638c466447747efe98b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-04-20 13:45:15 +0200

    doc: include interface

--- src/sim/objinter/imp.h
+++ src/sim/objinter/imp.h
@@ -4,7 +4,11 @@ struct Config;
 struct Opt;
 struct PFarrays;
 
+// tag::mem[]
 void obj_inter_ini(const Config*, const Opt*, MPI_Comm, float dt, int maxp, /**/ ObjInter**);
 void obj_inter_fin(ObjInter*);
+// end::mem[]
 
+// tag::int[]
 void obj_inter_forces(ObjInter*, PFarrays *flu, int *flu_start, PFarrays *obj);
+// end::int[]
