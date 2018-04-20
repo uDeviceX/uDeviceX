bf902a946062604af8f39ef2ba62b39f5eec86bc
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-20 10:51:35 +0200

    adapt interface

--- src/sim/objinter/imp.h
+++ src/sim/objinter/imp.h
@@ -6,4 +6,4 @@ struct PFAarrays;
 void obj_inter_ini(MPI_Comm, int maxp, const Opt*, /**/ ObjInter**);
 void obj_inter_fin(ObjInter*);
 
-void obj_inter_forces(ObjInter*, PFarrays *flu, PFarrays *obj);
+void obj_inter_forces(ObjInter*, PFarrays *flu, int *flu_start, PFarrays *obj);
