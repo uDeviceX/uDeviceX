a48849021dec906f6f8eba6aca155734ddaba2dc
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-20 11:35:10 +0200

    upd interface

--- src/sim/objinter/imp.h
+++ src/sim/objinter/imp.h
@@ -2,9 +2,9 @@ struct ObjInter;
 
 struct Config;
 struct Opt;
-struct PFAarrays;
+struct PFarrays;
 
-void obj_inter_ini(const Config *, const Opt*, MPI_Comm, int maxp, /**/ ObjInter**);
+void obj_inter_ini(const Config*, const Opt*, MPI_Comm, float dt, int maxp, /**/ ObjInter**);
 void obj_inter_fin(ObjInter*);
 
 void obj_inter_forces(ObjInter*, PFarrays *flu, int *flu_start, PFarrays *obj);
