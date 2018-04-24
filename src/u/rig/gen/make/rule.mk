bcc2b4bf5121d8bbb8c54f26e7c3f2c834967025
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-24 17:50:57 +0200

    clone hw to rig/gen

---
+++ src/u/rig/gen/make/rule.mk
@@ -0,0 +1,12 @@
+$B/d/api.o: $S/d/api.cu; $N -I$S/d -I$S/.
+$B/mpi/glb.o: $S/mpi/glb.cpp; $X -I$S/mpi -I$S/.
+$B/mpi/wrapper.o: $S/mpi/wrapper.cpp; $X -I$S/mpi -I$S/.
+$B/u/hw/hst/main.o: $S/u/hw/hst/main.cpp; $X -I$S/.
+$B/utils/cc.o: $S/utils/cc.cpp; $X -I$S/utils -I$S/.
+$B/utils/error.o: $S/utils/error.cpp; $X -I$S/utils -I$S/.
+$B/utils/imp.o: $S/utils/imp.cpp; $X -I$S/utils -I$S/.
+$B/utils/mc.o: $S/utils/mc.cpp; $X -I$S/utils -I$S/.
+$B/utils/msg.o: $S/utils/msg.cpp; $X -I$S/utils -I$S/.
+$B/utils/nvtx/imp.o: $S/utils/nvtx/imp.cpp; $X -I$S/utils/nvtx -I$S/.
+$B/utils/os.o: $S/utils/os.cpp; $X -I$S/utils -I$S/.
+$B/utils/string/imp.o: $S/utils/string/imp.cpp; $X -I$S/utils/string -I$S/.
