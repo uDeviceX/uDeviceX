ca6737dccae46b113b318c0f01f41889e9bf8a57
amlucas <lucas.amoudruz@wanadoo.fr>
2018-04-26 13:31:21 +0200

    objects_gen_freeze

--- src/sim/objects/imp/gen.h
+++ src/sim/objects/imp/gen.h
@@ -50,3 +50,31 @@ void objects_remove_from_wall(const Sdf *sdf, Objects *o) {
     if (o->mbr) remove_mbr(sdf, o->mbr);
     if (o->rig) remove_rig(sdf, o->rig);
 }
+
+static void gen_freeze_mbr(MPI_Comm cart, Mbr *m) {
+    UC(rbc_gen_freeze(cart, /**/ &m->q));
+}
+
+static void gen_freeze_rig(const Coords *coords, MPI_Comm cart, bool empty_pp, int numdensity, PFarrays *flu, Rig *r) {
+    const MeshRead *mesh = r->mesh;
+    long ln;
+    int n;
+    PaArray p;
+    FoArray f;
+    if (pfarrays_size(flu) != 1)
+        ERR("Must have only one particle array for solvent");
+    pfarrays_get(0, flu, &ln, &p, &f);
+    n = ln;
+    
+    UC(rig_gen_freeze(coords, empty_pp, numdensity, r->mass, r->pininfo,
+                      cart, mesh, (Particle*) p.pp, &n, /**/ &r->q));
+    
+    pfarrays_clear(flu);
+    pfarrays_push(flu, n, p, f);
+}
+
+void objects_gen_freeze(PFarrays *flu, Objects *o) {
+    const Opt *opt = &o->opt;
+    if (o->mbr) gen_freeze_mbr(o->cart, o->mbr);
+    if (o->rig) gen_freeze_rig(o->coords, o->cart, opt->rig.empty_pp, opt->params.numdensity, flu, o->rig);
+}
