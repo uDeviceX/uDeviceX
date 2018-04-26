5617d5b9735c44f578a8a182a71e03b561364c8f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-04-26 11:58:31 +0200

    adding doc for rig/gen

--- src/rig/gen/imp.h
+++ src/rig/gen/imp.h
@@ -2,6 +2,7 @@ struct Coords;
 struct RigPinInfo;
 struct MeshRead;
 
+// tag:struct[]
 struct RigGenInfo {
     float mass;
     int numdensity;
@@ -25,5 +26,8 @@ struct RigInfo {
     Solid *ss;
     Particle *pp;
 };
+// end::struct[]
 
+// tag::int[]
 void rig_gen_from_solvent(const Coords*, MPI_Comm, RigGenInfo, /*io*/ FluInfo, /**/ RigInfo);
+// end::int[]
