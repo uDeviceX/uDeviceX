c2059df7c9d1995194eba40b8c6db476931ebb0d
amlucas <lucas.amoudruz@wanadoo.fr>
2018-05-02 16:46:06 +0200

    use objects check functions

--- src/sim/imp/check.h
+++ src/sim/imp/check.h
@@ -4,9 +4,8 @@ _S_ void check_size(long n, long nmax) {
 }
 
 _I_ void check_sizes(Sim *s) {
-    // TODO
-    // if (active_rbc(s)) UC(check_size(s->rbc.q.nc, MAX_CELL_NUM));
-    UC(check_size(s->flu.q.n , s->flu.q.maxp)); 
+    UC(check_size(s->flu.q.n , s->flu.q.maxp));
+    UC(objects_check_size(s->obj));
 }
 
 _I_ void check_pos_soft(Sim *s) {
@@ -16,15 +15,11 @@ _I_ void check_pos_soft(Sim *s) {
 _I_ void check_vel(float dt, Sim *s) {
     const Coords *c = s->coords;
     UC(dbg_check_vel(dt, c, "flu", s->dbg, s->flu.q.n, s->flu.q.pp));
-    // TODO
-    // if (active_rig(s))  UC(dbg_check_vel(dt, c, "rig", s->dbg, s->rig.q.n, s->rig.q.pp));
-    // if (active_rbc(s))  UC(dbg_check_vel(dt, c, "rbc", s->dbg, s->rbc.q.n, s->rbc.q.pp));
+    UC(objects_check_vel(s->obj, s->dbg, dt));
 }
 
 _I_ void check_forces(float dt, Sim *s) {
     const Coords *c = s->coords;
     UC(dbg_check_forces(dt, c, "flu.ff", s->dbg, s->flu.q.n, s->flu.q.pp, s->flu.ff));
-    // TODO
-    // if (active_rig(s)) UC(dbg_check_forces(dt, c, "rig.ff", s->dbg, s->rig.q.n, s->rig.q.pp, s->rig.ff));
-    // if (active_rbc(s)) UC(dbg_check_forces(dt, c, "rbc.ff", s->dbg, s->rbc.q.n, s->rbc.q.pp, s->rbc.ff));
+    UC(objects_check_forces(s->obj, s->dbg, dt));
 }
