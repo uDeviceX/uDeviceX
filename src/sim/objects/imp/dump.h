4a22a9b35a19cded68347f603c9b983ea8c4c197
amlucas <lucas.amoudruz@wanadoo.fr>
2018-05-17 10:16:46 +0200

    wip: arrays of mbr and rig: dump.h

--- src/sim/objects/imp/dump.h
+++ src/sim/objects/imp/dump.h
@@ -8,11 +8,17 @@ static void dump_mesh_rig(MPI_Comm cart, const Coords *coords, Particle *pp, lon
     UC(mesh_write_particles(r->mesh_write, cart, coords, r->q.ns, pp, id));
 }
 
+/* TODO different namings */
 void objects_mesh_dump(Objects *obj) {
+    int i;
     Dump *d = obj->dump;
     if (!obj->active) return;
-    if (obj->mbr) UC(dump_mesh_mbr(obj->cart, obj->coords, d->pp, d->id, obj->mbr));
-    if (obj->rig) UC(dump_mesh_rig(obj->cart, obj->coords, d->pp, d->id, obj->rig));
+
+    for (i = 0; i < obj->nmbr; ++i)
+        UC(dump_mesh_mbr(obj->cart, obj->coords, d->pp, d->id, obj->mbr[i]));
+
+    for (i = 0; i < obj->nrig; ++i)
+        UC(dump_mesh_rig(obj->cart, obj->coords, d->pp, d->id, obj->rig[i]));
     ++ d->id;
 }
 
@@ -32,11 +38,13 @@ static void dump_diag_rig(float t, const Coords *coords, Rig *r, Dump *d) {
     UC(io_rig_dump(coords, t, q->ns, q->ss_dmp, q->ss_dmp_bb, d->rig));
 }
 
+/* TODO different namings */
 void objects_diag_dump(float t, Objects *obj) {
+    int i;
     Dump *d = obj->dump;
     if (!obj->active) return;
-    if (obj->mbr) dump_diag_mbr(obj->cart, obj->coords, obj->mbr, d);
-    if (obj->rig) dump_diag_rig(t,         obj->coords, obj->rig, d);
+    for (i = 0; i < obj->nmbr; ++i) dump_diag_mbr(obj->cart, obj->coords, obj->mbr[i], d);
+    for (i = 0; i < obj->nrig; ++i) dump_diag_rig(t,         obj->coords, obj->rig[i], d);
     ++ d->id_diag;
 }
 
@@ -46,18 +54,24 @@ static void dump_part_rig(MPI_Comm cart, const Coords *coords, long id, Rig *r, 
     UC(io_bop_parts(cart, coords, q->n, q->pp_hst, "solid", id, bop));
 }
 
+/* TODO different namings */
 void objects_part_dump(long id, Objects *o, IoBop *bop) {
+    int i;
     if (!o->active) return;    
-    if (o->rig) dump_part_rig(o->cart, o->coords, id, o->rig, bop);
+    for (i = 0; i < o->nrig; ++i) dump_part_rig(o->cart, o->coords, id, o->rig[i], bop);
 }
 
+/* TODO different namings */
 void objects_strt_templ(const char *base, Objects *o) {
+    int i;
     if (!o->active) return;
-    if (o->rig) UC(rig_strt_dump_templ(o->cart, base, &o->rig->q));
+    for (i = 0; i < o->nrig; ++i) UC(rig_strt_dump_templ(o->cart, base, &o->rig[i]->q));
 }
 
+/* TODO different namings */
 void objects_strt_dump(const char *base, long id, Objects *o) {
+    int i;
     if (!o->active) return;
-    if (o->mbr) UC(rbc_strt_dump(o->cart, base, id, &o->mbr->q));
-    if (o->rig) UC(rig_strt_dump(o->cart, base, id, &o->rig->q));
+    for (i = 0; i < o->nmbr; ++i) UC(rbc_strt_dump(o->cart, base, id, &o->mbr[i]->q));
+    for (i = 0; i < o->nrig; ++i) UC(rig_strt_dump(o->cart, base, id, &o->rig[i]->q));
 }
