c766c7ad44db3f1df69c10520f560c7877a346c6
amlucas <lucas.amoudruz@wanadoo.fr>
2018-05-18 14:01:18 +0200

    recolor mor many mbr

--- src/sim/objects/imp/recolor.h
+++ src/sim/objects/imp/recolor.h
@@ -1,3 +1,7 @@
+static void reini_colors(PFarray *flu) {
+    UC(collision_label_ini(flu->n, BLUE_COLOR, /**/ (int*) flu->p.cc));
+}
+
 static int exchange_mesh_mbr(Mbr *m, Particle *pp) {
     int nm, nv, nmhalo;
     const RbcQuants *q = &m->q;
@@ -30,7 +34,6 @@ static void recolor_flu_from_mbr(Mbr *m, PFarray *flu) {
 
     UC(minmax(c->pp_mesh, nv, nm, /**/ c->lo, c->hi));
 
-    UC(collision_label_ini(flu->n, BLUE_COLOR, /**/ (int*) flu->p.cc));
     UC(collision_label(NOT_PERIODIC, flu->n, (const Particle*) flu->p.pp, m->tri, nv, nm,
                        c->pp_mesh, c->lo, c->hi, RED_COLOR, /**/ (int*) flu->p.cc));
 }
@@ -43,5 +46,7 @@ void objects_recolor_flu(Objects *obj, PFarray *flu) {
     if (!obj->nmbr) return;
     if (flu->n == 0) return;
     if (obj->nmbr > 1) ERR("not implemented yet for more than one membrane type");
-    for (i = 0; i < obj->nmbr; ++i) recolor_flu_from_mbr(obj->mbr[i], flu);
+
+    UC(reini_colors(flu));
+    for (i = 0; i < obj->nmbr; ++i) UC(recolor_flu_from_mbr(obj->mbr[i], flu));
 }
