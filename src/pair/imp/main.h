9b7b600554cfd03f92ede5b240e7f41c1063f935
amlucas <lucas.amoudruz@wanadoo.fr>
2018-05-22 14:38:39 +0200

    check from state before returning views

--- src/pair/imp/main.h
+++ src/pair/imp/main.h
@@ -39,8 +39,21 @@ void pair_set_adhesion(float k1, float k2, PairParams *p) {
     p->k2 = k2;
 }
 
+static void check_dpd(const PairParams *p) {
+    if (!p->dpd) ERR("Must have dpd parameters");
+}
+
+static void check_lj(const PairParams *p) {
+    if (!p->lj) ERR("Must have lj parameters");
+}
+
+static void check_adhesion(const PairParams *p) {
+    if (!p->adhesion) ERR("Must have adhesion parameters");
+}
+
 void pair_compute_dpd_sigma(float kBT, float dt, PairParams *p) {
     int i, npar, ncol;
+    UC(check_dpd(p));
     ncol = p->ncolors;
     npar = get_npar(ncol);
 
@@ -50,6 +63,7 @@ void pair_compute_dpd_sigma(float kBT, float dt, PairParams *p) {
 
 void pair_get_view_dpd(const PairParams *p, PairDPD *v) {
     enum {PID=0};
+    UC(check_dpd(p));
     v->a = p->a[PID];
     v->g = p->g[PID];
     v->s = p->s[PID];
@@ -59,6 +73,7 @@ void pair_get_view_dpd(const PairParams *p, PairDPD *v) {
 void pair_get_view_dpd_color(const PairParams *p, PairDPDC *v) {
     size_t sz;
     int ncol, npar;
+    UC(check_dpd(p));
     ncol = v->ncolors = p->ncolors;
     npar = get_npar(ncol);
     sz = npar * sizeof(float);
@@ -70,6 +85,7 @@ void pair_get_view_dpd_color(const PairParams *p, PairDPDC *v) {
 
 void pair_get_view_dpd_mirrored(const PairParams *p, PairDPDCM *v) {
     int i, j, nc;
+    UC(check_dpd(p));
     nc = v->ncolors = p->ncolors;
     for (i = 0; i < nc; ++i) {
         j = (i+1) * (i+2) / 2 - 1; /* take all i-i pairs */
@@ -82,6 +98,8 @@ void pair_get_view_dpd_mirrored(const PairParams *p, PairDPDCM *v) {
 
 void pair_get_view_dpd_lj(const PairParams *p, PairDPDLJ *v) {
     enum {PID=0};
+    UC(check_dpd(p));
+    UC(check_lj(p));
     v->a = p->a[PID];
     v->g = p->g[PID];
     v->s = p->s[PID];
@@ -91,6 +109,7 @@ void pair_get_view_dpd_lj(const PairParams *p, PairDPDLJ *v) {
 }
 
 void pair_get_view_adhesion(const PairParams *p, PairAdhesion *v) {
+    UC(check_adhesion(p));
     v->k1 = p->k1;
     v->k2 = p->k2;
 }
