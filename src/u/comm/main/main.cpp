e21fec6e8d36bc107fb6d785d90016ac414bb11e
amlucas <lucas.amoudruz@wanadoo.fr>
2018-05-24 09:12:08 +0200

    adapt unit

--- src/u/comm/main/main.cpp
+++ src/u/comm/main/main.cpp
@@ -33,10 +33,14 @@ void fill_bags(hBags *b) {
     }
 }
 
-void comp(const int *a, const int *b, int n) {
-    for (int i = 0; i < n; ++i)
-        if (a[i] != b[i])
-            ERR("%d != %d for i = %d\n", a[i], b[i], i);
+void comp(const int *aa, const int *bb, int n) {
+    int i, a, b;
+    for (i = 0; i < n; ++i) {
+        a = aa[i];
+        b = bb[i];
+        if (a != b)
+            ERR("%d != %d for i = %d\n", a, b, i);
+    }
 }
 
 void compare(const hBags *sb, const hBags *rb) {
@@ -54,6 +58,7 @@ void compare(const hBags *sb, const hBags *rb) {
 int main(int argc, char **argv) {
     hBags sendB, recvB;
     Comm *comm;
+    CommBuffer *scommbuf, *rcommbuf;
     int capacity[NBAGS];
     float maxdensity = 26.f;
     int3 L;
@@ -82,15 +87,21 @@ int main(int argc, char **argv) {
     UC(comm_bags_ini(HST_ONLY, NONE, sizeof(int), capacity, /**/ &sendB, NULL));
     UC(comm_bags_ini(HST_ONLY, NONE, sizeof(int), capacity, /**/ &recvB, NULL));
     UC(comm_ini(cart, /**/ &comm));
+    UC(comm_buffer_ini(1, &sendB, &scommbuf));
+    UC(comm_buffer_ini(1, &recvB, &rcommbuf));
 
     fill_bags(&sendB);
 
-    UC(comm_post_recv(&recvB, comm));
-    UC(comm_post_send(&sendB, comm));
+    UC(comm_buffer_set(1, &sendB, scommbuf));
 
-    UC(comm_wait_recv(comm, &recvB));
+    UC(comm_post_recv(rcommbuf, comm));
+    UC(comm_post_send(scommbuf, comm));
+
+    UC(comm_wait_recv(comm, rcommbuf));
     UC(comm_wait_send(comm));
 
+    UC(comm_buffer_get(rcommbuf, 1, &recvB));
+    
     compare(&sendB, &recvB);
 
     msg_print("Passed");
@@ -98,7 +109,9 @@ int main(int argc, char **argv) {
     UC(comm_bags_fin(HST_ONLY, NONE, &sendB, NULL));
     UC(comm_bags_fin(HST_ONLY, NONE, &recvB, NULL));
     UC(comm_fin(/**/ comm));
-
+    UC(comm_buffer_fin(scommbuf));
+    UC(comm_buffer_fin(rcommbuf));
+    
     UC(coords_fin(coords));
     UC(conf_fin(cfg));
 
