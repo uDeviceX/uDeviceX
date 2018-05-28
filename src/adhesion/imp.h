f6537e43d0d18312c75a1a641f3a52b3954761d5
amlucas <lucas.amoudruz@wanadoo.fr>
2018-05-28 17:25:10 +0200

    adding adhesion module interface

---
+++ src/adhesion/imp.h
@@ -0,0 +1,13 @@
+struct Adhesion;
+
+void adhesion_ini(Adhesion**);
+void adhesion_fin(Adhesion*);
+
+void adhesion_link();
+void adhesion_force();
+
+/* TODO 
+  communication getters
+  or
+  visible struct
+ */
