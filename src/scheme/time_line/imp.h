d130c9e6f68f2f5db54d9bdb2ea9dc399116f2c1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-05-31 09:47:22 +0200

    doc: upd time line

--- src/scheme/time_line/imp.h
+++ src/scheme/time_line/imp.h
@@ -10,10 +10,12 @@ void time_line_advance(float dt, TimeLine*);           // <1>
 int  time_line_cross(const TimeLine*, float interval); // <2>
 // end::int[]
 
-void time_line_strt_dump(MPI_Comm, const char *base, int id, const TimeLine*);
-void time_line_strt_read(const char *base, int id, TimeLine*);
+// tag::strt[]
+void time_line_strt_dump(MPI_Comm, const char *base, int id, const TimeLine*); // <1>
+void time_line_strt_read(const char *base, int id, TimeLine*); // <2>
+// end::strt[]
 
 // tag::get[]
-float time_line_get_current (const TimeLine*);  // <1>
+float time_line_get_current  (const TimeLine*); // <1>
 long  time_line_get_iteration(const TimeLine*); // <2>
 // end::get[]
