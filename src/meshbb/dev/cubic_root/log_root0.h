735714edb0a8882723acd8d2e0bc7b710920f674
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-05 15:27:20 +0200

    rm log roots

--- src/meshbb/dev/cubic_root/log_root0.h
+++
@@ -1,3 +0,0 @@
-static __device__ bool cubic_root(real dt, real a, real b, real c, real d, /**/ real *h) {
-    return cubic_root0(dt, a, b, c, d, /**/ h);
-}
--- src/meshbb/imp/find_collisions/log_root0.h
+++
@@ -1,8 +0,0 @@
-void meshbb_find_collisions(float dt,
-                            int nm, MeshInfo mi, const Particle *i_pp, int3 L,
-                            const int *starts, const int *counts, const Particle *pp, const Force *ff,
-                            /**/ MeshBB *d) {
-    if (!nm) return;
-    KL(meshbb_dev::find_collisions, (k_cnf(nm * mi.nt)),
-       (dt, nm, mi.nt, mi.nv, mi.tt, i_pp, L, starts, counts, pp, ff, /**/ d->ncols, d->datacol, d->idcol));
-}
