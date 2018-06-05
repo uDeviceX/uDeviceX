b66375bf045d16d228956e94f1a3b3dfbae5a676
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-05 10:14:48 +0200

    use generic write in io/com

--- src/io/com/imp.cpp
+++ src/io/com/imp.cpp
@@ -14,6 +14,8 @@
 #include "utils/os.h"
 #include "utils/error.h"
 
+#include "io/write/imp.h"
+
 #include "imp.h"
 
 enum {MAX_CHAR_PER_LINE = 128};
@@ -36,20 +38,11 @@ static int swrite(const Coords *coords, int n, const int *ii, const float3 *rr, 
 }
 
 static void write_mpi(MPI_Comm comm, const char *fname, long n, const char *data) {
-    MPI_File f;
-    MPI_Status status;
-    MPI_Offset base, offset = 0;
-    MPI_Offset len = n * sizeof(char);
-
-    MC(MPI_File_open(comm, fname, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f));
-    MC(MPI_File_set_size(f, 0));
-    MC(MPI_File_get_position(f, &base)); 
-
-    MC( MPI_Exscan(&len, &offset, 1, MPI_OFFSET, MPI_SUM, comm) );
-    MC( MPI_File_write_at_all(f, base + offset, data, n, MPI_CHAR, &status) ); 
-    MC( MPI_File_close(&f) );
+    WriteFile *f;
+    UC(write_file_open(comm, fname, &f));
+    UC(write_all(comm, data, n, f));
+    UC(write_file_close(f));
 }
-
 
 void io_com_dump(MPI_Comm comm, const Coords *coords, const char *name, long id, int n, const int *ii, const float3 *rr) {
     char fname[FILENAME_MAX] = {0}, *data;
