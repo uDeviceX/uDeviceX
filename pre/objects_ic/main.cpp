a9b44cc40bcb311228abb838f420f8d8ea7e27a9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-05 08:41:58 +0200

    add inertia

--- pre/objects_ic/main.cpp
+++ pre/objects_ic/main.cpp
@@ -13,6 +13,7 @@ struct Args {
 
 struct Particles {
     real *xx, *yy, *zz;
+    real *vx, *vy, *vz;
     real *fx, *fy, *fz;
 };
 
@@ -46,24 +47,34 @@ static void parse(int c, char **v, Args *a) {
     a->r = atof(*v);
 }
 
+static real sq(real a) {return a*a;}
+static real cu(real a) {return a*a*a;}
+
 static void clear_forces(int n, Particles *p) {
     for (int i = 0; i < n; ++i)
         p->fx[i] = p->fy[i] = p->fz[i] = 0;
+}
+
+static void clear_vel(int n, Particles *p) {
+    for (int i = 0; i < n; ++i)
+        p->vx[i] = p->vy[i] = p->vz[i] = 0;
 }
 
 static real gen(int L) {
     return L * (drand48() - 0.5);
 }
 
+static void ini_array(int n, real **a) {
+    *a = (real*) malloc(n*sizeof(real));
+}
+
 static void particles_ini(int n, int Lx, int Ly, int Lz, Particles *p) {
-    size_t sz = n * sizeof(real);
-    int i; 
-    p->xx = (real*) malloc(sz);
-    p->yy = (real*) malloc(sz);
-    p->zz = (real*) malloc(sz);
-    p->fx = (real*) malloc(sz);
-    p->fy = (real*) malloc(sz);
-    p->fz = (real*) malloc(sz);
+    int i;
+    ini_array(n, &p->xx); ini_array(n, &p->yy); ini_array(n, &p->zz);
+    ini_array(n, &p->vx); ini_array(n, &p->vy); ini_array(n, &p->vz);
+    ini_array(n, &p->fx); ini_array(n, &p->fy); ini_array(n, &p->fz);
+    clear_vel(n, p);
+    clear_forces(n, p);
 
     for (i = 0; i < n; ++i) {
         p->xx[i] = gen(Lx);
@@ -74,11 +85,12 @@ static void particles_ini(int n, int Lx, int Ly, int Lz, Particles *p) {
 
 static void particles_fin(Particles *p) {
     free(p->xx); free(p->yy); free(p->zz);
+    free(p->vx); free(p->vy); free(p->vz);
     free(p->fx); free(p->fy); free(p->fz);
 }
 
-static real periodic_advance(real dt, real x, real f, int L) {
-    x += dt * f;
+static real periodic_advance(real x, real dx, int L) {
+    x += dx;
     if (x < - 0.5 * L) x += L;
     if (x >=  0.5 * L) x -= L;
     return x;
@@ -86,9 +98,13 @@ static real periodic_advance(real dt, real x, real f, int L) {
 
 static void advance(real dt, int Lx, int Ly, int Lz, int n, Particles *p) {
     for (int i = 0; i < n; ++i) {
-        p->xx[i] = periodic_advance(dt, p->xx[i], p->fx[i], Lx);
-        p->yy[i] = periodic_advance(dt, p->yy[i], p->fy[i], Ly);
-        p->zz[i] = periodic_advance(dt, p->zz[i], p->fz[i], Lz);
+        p->vx[i] += dt * p->fx[i];
+        p->vy[i] += dt * p->fy[i];
+        p->vz[i] += dt * p->fz[i];
+        
+        p->xx[i] = periodic_advance(p->xx[i], dt * p->vx[i], Lx);
+        p->yy[i] = periodic_advance(p->yy[i], dt * p->vy[i], Ly);
+        p->zz[i] = periodic_advance(p->zz[i], dt * p->vz[i], Lz);
     }
 }
 
@@ -115,8 +131,8 @@ static void periodic_dr(int Lx, int Ly, int Lz, const real ri[], const real rj[]
 }
 
 static real yang_w0(real q) {
-    if      (q < 1) return pow(2 - q, 3) - 4 * pow(1 - q, 3);
-    else if (q < 2) return pow(2 - q, 3);
+    if      (q < 1) return cu(2 - q) - 4 * cu(1 - q);
+    else if (q < 2) return cu(2 - q);
     else            return 0;
 }
 
@@ -126,9 +142,10 @@ static real yang_w3(real rc, real r) {
     return yang_coeff_3d * yang_w0(q) / (rc * rc * rc);
 }
 
-static real dpda(real rc, real r) {
+static real kernel(real rc, real r) {
     if (r < rc) return 1.0 - r / rc;
     return 0;
+    // return yang_w3(rc, r);
 }
 
 static real force(real rc, const real dr[], real f[]) {
@@ -136,14 +153,14 @@ static real force(real rc, const real dr[], real f[]) {
     f[X] = f[Y] = f[Z] = 0;
     real f0, rsq, r, inv_r;
 
-    rsq = dr[X] * dr[X] + dr[Y] * dr[Y] + dr[Z] * dr[Z];
+    rsq = sq(dr[X]) + sq(dr[Y]) + sq(dr[Z]);
     r = sqrt(rsq);
     if (r > rc) return r;
     
     inv_r = 1.0 / r;
     
     // f0 = yang_w3(rc, r);
-    f0 = dpda(rc, r);
+    f0 = kernel(rc, r);
     f0 *= inv_r;
 
     f[X] = dr[X] * f0;
@@ -156,7 +173,7 @@ static real interactions(real rc, int Lx, int Ly, int Lz, int n, Particles *p) {
     enum {X, Y, Z, D};
     int i, j;
     real ri[D], rj[D], dr[D], f[D], fi[D], r, rmin;
-    rmin = sqrt(Lx * Lx + Ly * Lz + Lz * Lz);
+    rmin = sqrt(sq(Lx) + sq(Ly) + sq(Lz));
 
     // printf("%d\n", n);
     
@@ -185,6 +202,27 @@ static real interactions(real rc, int Lx, int Ly, int Lz, int n, Particles *p) {
     return rmin;
 }
 
+static real temperature(int n, const Particles *p) {
+    real vsq, sum_vsq = 0;
+    int i;
+    for (i = 0; i < n; ++i) {
+        vsq = sq(p->vx[i]) + sq(p->vy[i]) + sq(p->vz[i]);
+        sum_vsq += vsq;
+    }
+    return sum_vsq / (3*n);
+}
+
+static void rescale(real T0, real T, int n, Particles *p) {
+    real s;
+    int i;
+    s = sqrt(T0 / T);
+    for (i = 0; i < n; ++i) {
+        p->vx[i] *= s;
+        p->vy[i] *= s;
+        p->vz[i] *= s;
+    }
+}
+
 static void dump_xyz(const char *fname, int n, const Particles *p) {
     FILE *f = fopen(fname, "w");
     fprintf(f, "%d\n\n", n);
@@ -198,7 +236,7 @@ static void dump_xyz(const char *fname, int n, const Particles *p) {
 static void check_density(Args a) {
     real den, Vs, Vb;
     Vb = a.Lx * a.Ly * a.Lz;
-    Vs = a.n * 4.0 * M_PI * pow(a.r, 3) / 3.0;
+    Vs = a.n * 4.0 * M_PI * cu(a.r) / 3.0;
     den = Vs / Vb;
     fprintf(stderr, "%g\n", den);
 }
@@ -209,11 +247,11 @@ int main(int argc, char **argv) {
     long seed = 12345;
     bool converged = false;
     int step = 0;
-    real rmin;
+    real rmin, T, T0;
 
-    static const real dt = 0.15;
+    static const real dt = 0.1;
     static const int max_steps = 15000;
-    static const int freq = 1000;
+    static const int freq = 100;
 
     parse(argc, argv, &a);
     check_density(a);
@@ -221,12 +259,18 @@ int main(int argc, char **argv) {
     
     particles_ini(a.n, a.Lx, a.Ly, a.Lz, &p);
 
+    rmin = a.r;
+
     while (!converged) {
         clear_forces(a.n, &p);
-        rmin = interactions(2.1 * a.r, a.Lx, a.Ly, a.Lz, a.n, &p);
+        rmin = interactions(2.5 * a.r, a.Lx, a.Ly, a.Lz, a.n, &p);
         advance(dt, a.Lx, a.Ly, a.Lz, a.n, &p);
         ++ step;
 
+        T = temperature(a.n, &p);
+        T0 = 0.1 / (sqrt(step + 10));
+        rescale(T0, T, a.n, &p);
+        
         converged = (a.r < rmin / 2);
         printf("%g\n", rmin / 2);
         
