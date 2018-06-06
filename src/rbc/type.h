aa70dae8c1184a330fa23ee6a28d2f4c7c1ea218
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-06 13:46:02 +0200

    indent

--- src/rbc/type.h
+++ src/rbc/type.h
@@ -3,12 +3,12 @@ struct Particle;
 
 // tag::quants[]
 struct RbcQuants {
-    int n, nc;             /* number of particles, cells                */
-    int nt, nv;            /* number of triangles and vertices per mesh */
-    int md;                /* maximum valence of the mesh               */
-    Particle *pp, *pp_hst; /* vertices particles on host and device     */
-    bool ids;              /* global ids active ?                       */
-    int *ii;               /* global ids on host                        */
+    int n, nc;               /* number of particles, cells                */
+    int nt, nv;              /* number of triangles and vertices per mesh */
+    int md;                  /* maximum valence of the mesh               */
+    Particle *pp, *pp_hst;   /* vertices particles on host and device     */
+    bool ids;                /* global ids active ?                       */
+    int *ii;                 /* global ids on host                        */
     AreaVolume *area_volume; /* to compute area and volume */
 };
 // end::quants[]
