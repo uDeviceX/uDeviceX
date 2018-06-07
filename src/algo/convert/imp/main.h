da22c4548c3752c7a6387501792f561a77a2d62f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-07 11:59:31 +0200

    add zip inside algo/convert

--- src/algo/convert/imp/main.h
+++ src/algo/convert/imp/main.h
@@ -5,3 +5,9 @@ void convert_pp2rr_current (long n, const Particle *pp, Positioncp *rr) {
 void convert_pp2rr_previous(long n, const Particle *pp, Positioncp *rr) {
     KL(convert_dev::pp2rr_previous, (k_cnf(n)), (n, pp, rr));
 }
+
+void convert_pp2f4_pos(int n, const float *pp, /**/ float4 *zpp) {
+    static_assert(sizeof(Particle) == 6 * sizeof(float),
+                  "sizeof(Particle) != 6 * sizeof(float)");
+    KL(convert_dev::pp2f4_pos, (k_cnf(n)), (n, pp, zpp));
+}
