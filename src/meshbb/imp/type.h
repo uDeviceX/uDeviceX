1d429b54ae3bc6e0de043644a340699d54a4b547
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-07 08:41:05 +0200

    rm meshbb

--- src/meshbb/imp/type.h
+++
@@ -1,5 +0,0 @@
-struct MeshBB {
-    int *ncols;       /* number of possible collisions per particle      */
-    float4 *datacol;  /* list of data related to collisions per particle */
-    int *idcol;       /* list of triangle colliding ids per particle     */
-};
