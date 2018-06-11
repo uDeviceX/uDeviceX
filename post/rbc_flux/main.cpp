1fa5564434497cf3db7a02b3fc6b445570f5f200
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-11 13:36:10 +0200

    read sequence

--- post/rbc_flux/main.cpp
+++ post/rbc_flux/main.cpp
@@ -4,22 +4,71 @@
 #include "type.h"
 #include "com.h"
 
+struct Arg {
+    int Lx, Ly, Lz;
+    int nfiles;
+    char **fnames;
+};
+
+static void usg() {
+    fprintf(stderr, "usage: u.post.rbc.flux <Lx> <Ly> <Lz> <com-00.txt> <com-01.txt> ...\n");
+    exit(1);
+}
+
+static bool shift(int *c, char ***v) {
+    (*c) --;
+    (*v) ++;
+    return *c > 0;
+}
+
+static void parse(int c, char **v, Arg *a) {
+    if (!shift(&c, &v)) usg();
+    a->Lx = atof(*v);
+    if (!shift(&c, &v)) usg();
+    a->Ly = atof(*v);
+    if (!shift(&c, &v)) usg();
+    a->Lz = atof(*v);
+
+    if (!shift(&c, &v)) usg();
+    a->fnames = v;
+    a->nfiles = c;
+
+    if (c < 2) usg();        
+}
+
+template <typename T>
+static void swap(T *a, T *b) {
+    T c = *a;
+    *a = *b;
+    *b = c;
+}
+
 int main(int argc, char **argv ) {
-
-    Com *cc;
-    int n;
-
+    Arg a;
+    Com *cc0, *cc1;
+    int i, n;
     FILE *f;
 
-    f = fopen(argv[1], "r");
-    read(f, &n, &cc);
-    
-    sort_by_id(n, cc);
+    parse(argc, argv, &a);
 
-    print_cc(n, cc, stderr);
 
+    f = fopen(a.fnames[0], "r");
+    read(f, &n, &cc0);
+    sort_by_id(n, cc0);
     fclose(f);
-    free(cc);
+    
+    for (i = 1; i < a.nfiles; ++i) {
+        f = fopen(a.fnames[i], "r");
+        read(f, &n, &cc1);
+        fclose(f);
+        
+        sort_by_id(n, cc1);
+
+        swap(&cc0, &cc1);
+        
+        free(cc1);
+    }
+    free(cc0);
     
     return 0;
 }
