9466e28bc1c82ec2a782a5dd267b10f52577b64c
Sergey Litvinov <slitvinov@gmail.com>
2018-06-19 14:37:37 +0200

    dump avaible memory

--- src/d/cuda/debug/alloc.h
+++ src/d/cuda/debug/alloc.h
@@ -1,13 +1,27 @@
+#include <unistd.h>
 static size_t b2mb(size_t byte) { return byte / 1000000; }
 
+size_t memory() {
+    size_t p, sz;
+    p  = sysconf(_SC_PHYS_PAGES);
+    sz = sysconf(_SC_PAGE_SIZE);
+    return p * sz;
+}
+
 int alloc_pinned(void **pHost, size_t size) {
+    cudaError_t status;
     msg_print("[alloc_pinned] %ld MB", b2mb(size));
-    return R(cudaHostAlloc(pHost, size, cudaHostAllocMapped));
+    status = cudaHostAlloc(pHost, size, cudaHostAllocMapped);
+    msg_print("host memory: %ld MG", b2mb(memory()));
+    return R(status);
 }
 
 int Malloc(void **devPtr, size_t size) {
+    cudaError_t status;
     msg_print("[Malloc] %ld MB", b2mb(size));
-    return R(cudaMalloc(devPtr, size));
+    status = cudaMalloc(devPtr, size);
+    msg_print("host memory: %ld MG", b2mb(memory()));
+    return R(status);
 }
 
 int Free (void *devPtr) {
