8b23f0d3b18caac978331169a980b67665397204
Sergey Litvinov <slitvinov@gmail.com>
2018-06-22 15:13:19 +0200

    initial version of lib

---
+++ make/lib.mk
@@ -0,0 +1,24 @@
+# input:
+# TRG: target base name
+# O: object files
+# I: include files (headers)
+# CFLAGS, U_CFLAGS, CC:
+# LIB: where to install lib$(TRG).a
+# INC: where to install $I
+#
+# requres u.install
+
+L = lib$(TRG).a
+
+$L: $O; ar rv $@ $H && ranlib $@
+%.o: %.c; $(CC) $(CFLAGS) $(U_CFLAGS) -c -o $@ $<
+
+install: $L $H
+	@printf 'install %s' "$L"
+	@printf 'install %s' "$I"
+	u.install $L $(LIB)
+	u.install $I $(INC)
+
+clean: rm -f $O $L
+
+.PHONY: clean install
