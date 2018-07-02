beb0d0c05fca11eb943668abd7e24c0ec6fecc7e
Sergey Litvinov <slitvinov@gmail.com>
2018-07-02 09:49:17 +0200

    add Makefile and setup

---
+++ poc/libconfig/python/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name="uconf",
+      author="Christian Aichinger, Sergey Litvinov, Lucas Amoudruz",
+      py_modules=["uconf"],
+      description="udx configuration utilities",
+      license="MIT"
+)
