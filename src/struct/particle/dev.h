d5bb7ba025aec3af475b003de86eb9790a6fcc29
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-07-12 16:00:42 +0200

    fix dev fetch

--- src/struct/particle/dev.h
+++ src/struct/particle/dev.h
@@ -1,13 +1,13 @@
-static float3 fetch_pos(int i, const Particle *pp) {
+static __device__ float3 fetch_pos(int i, const Particle *pp) {
     enum {X, Y, Z};
     float3 r;
-    r.x = pp.r[X]; r.y = pp.r[Y]; r.z = pp.r[Z];
-    return v;
+    r.x = pp[i].r[X]; r.y = pp[i].r[Y]; r.z = pp[i].r[Z];
+    return r;
 }
 
-static float3 fetch_vel(int i, const Particle *pp) {
+static __device__ float3 fetch_vel(int i, const Particle *pp) {
     enum {X, Y, Z};
     float3 v;
-    v.x = pp.v[X]; v.y = pp.v[Y]; v.z = pp.v[Z];
+    v.x = pp[i].v[X]; v.y = pp[i].v[Y]; v.z = pp[i].v[Z];
     return v;
 }
