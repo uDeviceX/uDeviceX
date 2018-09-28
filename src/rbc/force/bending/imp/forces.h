0c13c3c3484300066106ca0821a3b237decef1fb
Sergey Litvinov <slitvinov@gmail.com>
2018-09-28 14:56:29 +0200

    minor

--- src/rbc/force/bending/imp/forces.h
+++ src/rbc/force/bending/imp/forces.h
@@ -2,7 +2,8 @@ void rbc_bending_apply(RbcForce *t, const RbcParams *par, float /* dt */ , const
     int nc, nv, md;
     RbcParams_v parv;
     if (q->nc <= 0) return;
-    if (!d::is_device_pointer(q->pp))  ERR("`q->pp` is not a device pointer");
+    if (!d::is_device_pointer(q->pp))
+        ERR("`q->pp` is not a device pointer");
     md = q->md; nv = q->nv; nc = q->nc;
     parv = rbc_params_get_view(par);
     KL(rbc_bending_dev::force, (k_cnf(q->nc*nv*md)), (parv, md, nv, nc, q->pp, *t->adj_v, /**/ (float*)ff));    
