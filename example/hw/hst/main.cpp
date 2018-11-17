8a1fa2473ba332358bbd827fe4c4d1d1f19e59c5
Sergey Litvinov <slitvinov@gmail.com>
2018-11-17 22:36:50 +0100

    add hww/hst

---
+++ example/hw/hst/main.cpp
@@ -0,0 +1,25 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#include "utils/mc.h"
+#include "utils/msg.h"
+#include "mpi/glb.h"
+#include "mpi/wrapper.h"
+#include "utils/mc.h"
+
+int main(int argc, char **argv) {
+    int rank, size, dims[3];
+    MPI_Comm cart;
+    m::ini(&argc, &argv);
+    m::get_dims(&argc, &argv, dims);
+    m::get_cart(MPI_COMM_WORLD, dims, &cart);
+
+    MC(m::Comm_rank(cart, &rank));
+    MC(m::Comm_size(cart, &size));
+    msg_ini(rank);
+    msg_print("mpi size: %d", size);
+    msg_print("Hello world!");
+
+    MC(m::Barrier(cart));
+    m::fin();
+}
