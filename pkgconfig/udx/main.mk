bebf09262ab2ffd702f185d732d14c1e8d0c7ac4
Sergey Litvinov <slitvinov@gmail.com>
2018-11-17 23:36:06 +0100

    Group -rpath together

--- pkgconfig/udx/main.mk
+++ pkgconfig/udx/main.mk
@@ -1,9 +1,9 @@
 PREFIX    = $(HOME)
 BIN       = $(PREFIX)/bin
-ARCH     ?= -arch compute_35 -code sm_35
+ARCH      ?= -arch compute_35 -code sm_35
 
-NVCCFLAGS  += $(ARCH) -use_fast_math -restrict
-NVCCLIBS   +=  $(NVCC_LIBS) -lcudart -lcurand -lnvToolsExt
+NVCCFLAGS  += -use_fast_math -restrict
+NVCCLIBS   += $(NVCC_LIBS) -lcudart -lcurand -lnvToolsExt
 
 P = \
 udx_dep.pc\
@@ -18,13 +18,15 @@ udx_cpu.pc: udx_cpu.pc.in
 	sed "s|@PREFIX@|$(PREFIX)|g" $< > $@
 
 udx_cuda.pc: udx_cuda.pc.in
-	CFLAGS="$(NVCCFLAGS)" LIBS="$(NVCCLIBS)"; \
+	CFLAGS="$(ARCH) $(NVCCFLAGS)" LIBS="$(ARCH) `./coma $(NVCCLIBS)`"; \
 	sed -e "s|@PREFIX@|$(PREFIX)|g" \
             -e "s|@CFLAGS@|$$CFLAGS|g" \
             -e "s|@LIBS@|$$LIBS|g" $< > $@
 
 install: $P
 	u.install $P "$(PKG)"
+
+show:; @echo $(NVCCLIBS)
 
 PHONY: clean
 clean:; rm -rf $P
