0864a76f584192a735165b1c91e75dde2f90f48d
Sergey Litvinov <slitvinov@gmail.com>
2018-11-17 10:13:58 +0100

    use __CUDACC_VER_MAJOR__

--- src/algo/utils/shfl.h
+++ src/algo/utils/shfl.h
@@ -1,5 +1,16 @@
 #define _I_ static __device__
 
+#if __CUDACC_VER_MAJOR__ >= 9
+template <typename T>
+_I_ T shfl_down(T val, unsigned int width) {
+    return __shfl_down_sync(0xFFFFFFFF, val, width);
+}
+
+template <typename T>
+_I_ T shfl_up(T val, unsigned int width) {
+    return __shfl_up_sync(0xFFFFFFFF, val, width);
+}
+#else
 template <typename T>
 _I_ T shfl_down(T val, unsigned int width) {
     return __shfl_down(val, width);
@@ -9,5 +20,6 @@ template <typename T>
 _I_ T shfl_up(T val, unsigned int width) {
     return __shfl_up(val, width);
 }
+#endif
 
 #undef _I_
