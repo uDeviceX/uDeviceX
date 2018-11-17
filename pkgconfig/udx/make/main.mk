120b8b25fd7f323b60443fd7c1675824231b106e
Sergey Litvinov <slitvinov@gmail.com>
2018-11-17 16:44:45 +0100

    minor

--- pkgconfig/udx/make/main.mk
+++ pkgconfig/udx/make/main.mk
@@ -30,10 +30,10 @@ LIBS      += -lcudart -lcurand -lnvToolsExt
 LOG = @echo $< $@;
 N  = $(LOG) $(NVCC)  $(ARCH) $(NVCCFLAGS)        --compiler-options '$(NCFLAGS)' $< -c -o $@
 X  = $(LOG) $(NVCC)  -Wno-deprecated-gpu-targets --compiler-options '$(XCFLAGS)' $< -c -o $@
-DL = $(LOG) $(NVCC) $(ARCH) -dlink `../tools/unmain $^` -o $@
-L  = $(LOG) $(LINK) $^ $(LIBS) -o $@
-A  = $(LOG) ar r $@ `../tools/unmain $^` && ranlib $@
-$B/udx: $B/main.o $B/libudx_cpu.a $B/libudx_cuda.a; $L
+DL = $(LOG) $(NVCC) $(ARCH) -dlink `u.unmain $^` -o $@
+L  = $(LOG) $(LINK) $^ $(LIBS) -o `u.main`
+A  = $(LOG) ar r $@ `u.unmain $^` && ranlib $@
+$B/udx: $B/libudx_cpu.a $B/libudx_cuda.a; $L
 $B/gpu.o: $O; $(DL)
 
 $B/libudx_cpu.a:  $O;       $A
