eb5f2dc2c5eeb399a4dc0e3d8411efda3919787b
Sergey Litvinov <slitvinov@gmail.com>
2018-11-18 09:35:14 +0100

    copy-paste u/rbc; use example.mk

---
+++ example/rbc/bending/main.cu
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <mpi.h>
+
+#include <conf.h>
+#include "inc/conf.h"
+
+#include "inc/def.h"
+#include "inc/type.h"
+#include "inc/dev.h"
+
+#include "coords/type.h"
+#include "coords/ini.h"
+
+#include "utils/imp.h"
+#include "utils/msg.h"
+#include "utils/error.h"
+#include "utils/texo.h"
+#include "utils/cc.h"
+#include "utils/mc.h"
+
+#include "d/api.h"
+
+#include "rbc/params/imp.h"
+#include "rbc/type.h"
+#include "rbc/imp.h"
+#include "rbc/force/imp.h"
+#include "rbc/force/bending/imp.h"
+
+#include "io/mesh_read/imp.h"
+
+#include "mpi/glb.h"
+#include "mpi/wrapper.h"
+
+#include "conf/imp.h"
+
+static void write0(Particle p, Force f0) {
+    enum {X, Y, Z};
+    float *r, *f;
+    r = p.r;
+    f = f0.f;
+    printf("%g %g %g %g %g %g\n", r[X], r[Y], r[Z], f[X], f[Y], f[Z]);
+}
+
+static void write1(int n, Particle *p, Force *f) {
+    int i;
+    for (i = 0; i < n; i++) write0(p[i], f[i]);
+}
+
+static void write(int n, Particle *p, Force *f) {
+    Particle *p_hst;
+    Force *f_hst;
+
+    EMALLOC(n, &p_hst);
+    EMALLOC(n, &f_hst);
+
+    cD2H(p_hst, p, n);
+    cD2H(f_hst, f, n);
+
+    write1(n, p_hst, f_hst);
+
+    EFREE(p_hst);
+    EFREE(f_hst);
+}
+
+int main(int argc, char **argv) {
+    Config *cfg;
+    Coords *coords;
+    RbcParams *par;
+    Bending *bending;
+    const char *cell, *ic;
+    MPI_Comm cart;
+    int dims[3];
+    MeshRead *off;
+    RbcQuants q;
+    bool ids = false;
+    Force *f;
+    float kb, phi;
+    
+    m::ini(&argc, &argv);
+    m::get_dims(&argc, &argv, dims);
+    m::get_cart(MPI_COMM_WORLD, dims, &cart);
+
+    UC(conf_ini(&cfg));
+    UC(conf_read(argc, argv, cfg));
+    UC(coords_ini_conf(cart, cfg, &coords));
+    UC(conf_lookup_string(cfg, "rbc.cell", &cell));
+    UC(conf_lookup_string(cfg, "rbc.ic", &ic));
+    UC(conf_lookup_float(cfg, "rbc.kb", &kb));
+    UC(conf_lookup_float(cfg, "rbc.phi", &phi));
+
+    UC(rbc_params_ini(&par));
+    UC(rbc_params_set_bending(kb, phi, /**/ par));
+
+    UC(mesh_read_ini_off(cell, /**/ &off));
+    UC(rbc_ini(MAX_CELL_NUM, ids, off, &q));
+    UC(rbc_gen_mesh(coords, cart, off, ic, /**/ &q));
+
+    Dalloc(&f, q.n); Dzero(f, q.n);
+
+    UC(bending_juelicher_ini(off, &bending));
+    UC(bending_apply(bending, par, &q, f));
+    write(q.n, q.pp, f);
+    
+    Dfree(f);
+    UC(bending_fin(bending));
+    rbc_fin(&q);
+    mesh_read_fin(off);
+    
+    UC(rbc_params_fin(par));
+    UC(conf_fin(cfg));
+    UC(coords_fin(coords));
+    MC(m::Barrier(cart));
+
+    dSync();
+    m::fin();
+}
---
+++ example/rbc/gen/main.cpp
@@ -0,0 +1,111 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#include "utils/mc.h"
+#include "utils/msg.h"
+#include "mpi/glb.h"
+#include "mpi/wrapper.h"
+#include "utils/mc.h"
+#include "utils/error.h"
+#include "conf/imp.h"
+#include "io/mesh_read/imp.h"
+#include "mesh/gen/matrices/imp.h"
+#include "mesh/gen/imp.h"
+#include "mesh/area/imp.h"
+#include "mesh/volume/imp.h"
+#include "algo/vectors/imp.h"
+#include "utils/imp.h"
+#include "inc/type.h"
+
+#define MAX_N 99999
+#define MAX_M 20
+static Particle pp[MAX_N];
+static double output[MAX_M];
+
+enum {AREA, VOLUME};
+struct MeshQuant {
+    int type;
+    union {
+        MeshArea *area;
+        MeshVolume *volume;
+    };
+};
+
+static void q_ini(const char *type, MeshRead *mesh, /**/ MeshQuant **pq) {
+    MeshQuant *q;
+    EMALLOC(1, &q);
+    if (same_str(type, "area")) {
+        q->type = AREA;
+        mesh_area_ini(mesh, &q->area);
+    } else if (same_str(type, "volume")) {
+        q->type = VOLUME;
+        mesh_volume_ini(mesh, &q->volume);
+    } else
+        ERR("unknown type '%s'", type);
+    *pq = q;
+}
+
+static void q_apply(MeshQuant *q, int nm, Vectors *positions, double *out) {
+    if (q->type == AREA)
+        UC(mesh_area_apply(q->area, nm, positions, /**/ out));
+    else if (q->type == VOLUME)
+        UC(mesh_volume_apply(q->volume, nm, positions, /**/ out));
+    else ERR("unknown q->type");
+}
+
+static void q_fin(MeshQuant *q) {
+    if (q->type == AREA) mesh_area_fin(q->area);
+    else if (q->type == VOLUME) mesh_volume_fin(q->volume);
+    else ERR("unknown q->type");
+    EFREE(q);
+}
+
+void main0(const char *cell, const char *ic, const char *quant) {
+    int i, nm, n, nv;
+    Matrices *matrices;
+    MeshRead *mesh;
+    MeshQuant *mesh_quant;
+    Vectors *positions;
+    const float *verts;
+    UC(mesh_read_ini_off(cell, /**/ &mesh));
+    UC(matrices_read(ic, &matrices));
+    nv = mesh_read_get_nv(mesh);
+    verts = mesh_read_get_vert(mesh);
+    mesh_gen_from_matrices(nv, verts, matrices, /**/ &n, pp);
+    nm = n / nv;
+    vectors_postions_ini(n, pp, /**/ &positions);
+    UC(q_ini(quant, mesh, &mesh_quant));
+    UC(q_apply(mesh_quant, nm, positions, /**/ output));
+    for (i = 0; i < nm; i++)
+        printf("%g\n", output[i]);
+    
+    UC(vectors_fin(positions));
+    UC(q_fin(mesh_quant));
+    UC(matrices_fin(matrices));
+    UC(mesh_read_fin(mesh));
+}
+
+int main(int argc, char **argv) {
+    const char *cell, *ic, *quant;
+    Config *cfg;
+    int rank, size, dims[3];
+    MPI_Comm cart;
+    m::ini(&argc, &argv);
+    m::get_dims(&argc, &argv, dims);
+    m::get_cart(MPI_COMM_WORLD, dims, &cart);
+
+    MC(m::Comm_rank(cart, &rank));
+    MC(m::Comm_size(cart, &size));
+
+    UC(conf_ini(&cfg));
+    UC(conf_read(argc, argv, cfg));
+    UC(conf_lookup_string(cfg, "cell", &cell));
+    UC(conf_lookup_string(cfg, "ic", &ic));
+    UC(conf_lookup_string(cfg, "q", &quant));
+
+    main0(cell, ic, quant);
+
+    UC(conf_fin(cfg));
+    MC(m::Barrier(cart));
+    m::fin();
+}
