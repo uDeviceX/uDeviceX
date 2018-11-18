c16a09f855f21d24b4d3939f4515e9d85cfceffe
Sergey Litvinov <slitvinov@gmail.com>
2018-11-18 10:07:36 +0100

    use NCC to compile *.cpp files

--- make/example.mk
+++ make/example.mk
@@ -3,6 +3,6 @@ UDX_CXXFLAGS = $(shell u.pkg-config udx_cpu --cflags)
 UDX_NCCFLAGS = $(shell u.pkg-config udx_cuda --cflags)
 UDX_LDFLAGS  = $(shell u.pkg-config udx_cpu --libs) $(shell u.pkg-config udx_cuda --libs)
 
-%.o: %.cpp; $(CXX) $(CXXFLAGS) $(UDX_CXXFLAGS) $< -c -o $@
+%.o: %.cpp; $(NCC) $(CXXFLAGS) $(UDX_NCCFLAGS) $< -c -o $@
 %.o: %.cu;  $(NCC) $(CXXFLAGS) $(UDX_NCCFLAGS) $< -c -o $@
 %: %.o; $(LINK) $^ $(UDX_LDFLAGS) $(LDLIBS) -o $@
